972a2f8a23bebd366ee5dca444c86bdb7c606006
jbloom22 <jbloom@broadinstitute.org>
2015-10-06 14:05:10 -0400

    playing with breeze for linreg

--- src/main/scala/org/broadinstitute/k3/methods/LinRegBreeze.scala
+++ src/main/scala/org/broadinstitute/k3/methods/LinRegBreeze.scala
@@ -3,20 +3,25 @@ import breeze.linalg._
 import breeze.stats._
 import breeze.numerics._
 
-object LinRegTemp {
+object LinRegBreeze {
   def main(args: Array[String]): Unit = {
 
-    val X = DenseMatrix((0.0, 1.0, 0.0, 0.0, 0.0), (1.0, 2.0, 1.0, 0.0, 1.0), (0.0, 1.0, 1.0, 0.0, 1.0))
+    val X = DenseMatrix(
+      (0.0, 1.0, 0.0, 0.0, 0.0),
+      (1.0, 2.0, 1.0, 0.0, 1.0),
+      (0.0, 1.0, 1.0, 0.0, 1.0))
 
     val y = DenseVector(0.0, 1.0, 1.0)
 
-    val C = DenseMatrix((0.0, 1.0), (2.0, 3.0), (4.0, 5.0))
+    val C = DenseMatrix(
+      (0.0, 1.0),
+      (2.0, 3.0),
+      (4.0, 5.0))
 
     val mv = meanAndVariance(X(::, *))
     val mu = mv.map(_.mean).toDenseVector
     val sigma = mv.map(_.stdDev).toDenseVector
     val Xstd = ((X(*, ::) - mu): DenseMatrix[Double])(*,::) :/ sigma
-
 
     println("X")
     println(X)
@@ -24,6 +29,11 @@ object LinRegTemp {
     println(Xstd)
     println("y")
     println(y)
+
+    val b = X \ y
+    println("b")
+    println(b)
+
     println("C")
     println(C)
 
@@ -31,27 +41,18 @@ object LinRegTemp {
     for (i <- 0 until X.cols) {
       println(X(::, i))
     }
-
     val Q = qr.reduced.justQ(C)
 
     println("Q")
     println(Q)
 
-    val X00 = new DenseMatrix(3, 1, Array(0.0, 1.0, 0.0))
-    println(X00)
-    val X0 = X(::, 0).toDenseMatrix.t
-    println(X0)
-    println(X00.majorStride)
-    println(X0 == X00)
-    println(y)
-    val beta00 = X00 \ y
-    println(beta00)
-    println(X0)
-    println(y)
-    val beta0 = X0 \ y
-    println(y)
-    println(beta0)
-
 
   }
 }
+
+//  Breeze bug
+//  val X = DenseMatrix((1.0, 0.0), (0.0, 1.0))
+//  val y = DenseVector(0.0, 1.0)
+//  val X0 = X(::, 0).toDenseMatrix.t
+//  val b = X \ y
+//  println(X0.majorStride)
