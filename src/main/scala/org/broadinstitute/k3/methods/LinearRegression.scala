f009c2e1cfea1e946ea12bc825865ee8670d6df9
jbloom22 <jbloom@broadinstitute.org>
2015-10-13 20:02:47 -0400

    linear regression now computes t, b, se, p

--- src/main/scala/org/broadinstitute/k3/methods/LinearRegression.scala
+++ src/main/scala/org/broadinstitute/k3/methods/LinearRegression.scala
@@ -4,8 +4,8 @@ import scala.collection.mutable.ListBuffer
 import scala.io.Source
 import java.io.File
 import breeze.linalg._
-import breeze.stats.meanAndVariance
 import org.apache.spark.rdd.RDD
+import org.apache.commons.math3.distribution.TDistribution
 
 import org.broadinstitute.k3.variant._
 import org.broadinstitute.k3.Utils._
@@ -40,7 +40,7 @@ object CovariateData {
 
 case class CovariateData(sampleOfRow: Array[Int], covariateOfCol: Array[String], data: DenseMatrix[Double])
 
-case class LinRegOutput(nMissing: Int, beta: Double, stdError: Double, t: Double)
+case class LinRegOutput(nMissing: Int, beta: Double, stdError: Double, t: Double, p: Double)
 
 object LinearRegression {
   def name = "LinearRegression"
@@ -52,14 +52,13 @@ object LinearRegression {
     val k = cov.data.cols
     require(n - k - 2 > 0)
 
-    val rowOfSample = cov.sampleOfRow.zipWithIndex.toMap
-
     // extract the phenotype vector y
+    val rowOfSample = cov.sampleOfRow.zipWithIndex.toMap
     val y = DenseVector.zeros[Double](n)
     for (i <- 0 until n)
      y(i) = ped.phenoOf(cov.sampleOfRow(i)).toString.toDouble
 
-    // augment the covariate matrix with ones vector, and compute q.t, project y
+    // augment the covariate matrix with ones vector, and compute q.t, and yy = yp dot yp
     val qt = qr.reduced.justQ(DenseMatrix.horzcat(DenseMatrix.ones[Double](n, 1), cov.data)).t
     val qty = qt * y
     val yy = (y dot y) - (qty dot qty) // = yp dot yp for yp = (I - q * q.t) y
@@ -67,6 +66,7 @@ object LinearRegression {
     val sc = vds.sparkContext
     val rowOfSampleBc = sc.broadcast(rowOfSample)
     val isPhenotypedBc = sc.broadcast((0 until vds.nSamples).map(rowOfSample.isDefinedAt).toArray)
+
     val qtBc = sc.broadcast(qt)
     val qtyBc = sc.broadcast(qty)
     val yyBc = sc.broadcast(yy)
@@ -80,9 +80,9 @@ object LinearRegression {
           val qt = qtBc.value
           val n = qt.cols
           val k = qt.rows
-          val x = SparseVector.zeros[Double](n)
 
           // replace missing values with mean
+          val x = SparseVector.zeros[Double](n)
           val missingRowsBuffer = new ListBuffer[Int]()
           var gtSum = 0
           var gtSumSq = 0
@@ -99,38 +99,33 @@ object LinearRegression {
                 missingRowsBuffer += row
           }
           assert(gtSum > 0) //FIXME: better error handling
-
-          // fill in missing gt with mean
           val missingRows = missingRowsBuffer.toList
           val nMissing = missingRows.length
           val mu = gtSum.toDouble / (n - nMissing)
           missingRows.foreach(row => x(row) = mu)
 
+          // divide x by norm of centered x
           val sum = gtSum + nMissing * mu
           val sumSq = gtSumSq + nMissing * mu * mu
-          println(x)
           x :/= math.sqrt(sumSq - sum * sum / n)
 
+          // calculate dot products between xp and yp
           val qtx = qt * x
           val qty = qtyBc.value
-
-          // these turn out to be unnecessary!
-          // val xp = x - qt.t * (qt * x)
-          // val yp = y - qt.t * (qt * y)
-
           val xx = (x dot x) - (qtx dot qtx) // = xp dot xp for xp = (I - q * q.t) x
           val xy = (x dot y) - (qtx dot qty) // = xp dot yp
           val yy = yyBc.value                // = yp dot yp
 
-
-          val sign = if (xy > 0) 1 else -1
+          // compute regression coef and stats for 2-sided t test with d degrees of freedom
+          val signT = if (xy > 0) 1 else -1
           val d = n - k - 2
-          val chi2 = d * xy * xy / (xx * yy - xy * xy)
-          val t = sign * math.sqrt(chi2)
           val b = xy / xx
-          val se = b / t // = math.sqrt(|y - bx|^2 / (d * xx))
+          val t = signT * math.sqrt(d * xy * xy / (xx * yy - xy * xy))
+          val se = b / t // = sqrt( 1/d * |y - bx|^2 / xx )
+          val tDist = new TDistribution(null, d.toDouble)
+          val p = 2 * tDist.cumulativeProbability(-1 * signT * t)
 
-          LinRegOutput(nMissing, b, se, t)
+          LinRegOutput(nMissing, b, se, t, p)
         }
       }
     )
@@ -141,8 +136,8 @@ case class LinearRegression(lr: RDD[(Variant, LinRegOutput)]) {
 
   def write(filename: String) {
     def toLine(v: Variant, lro: LinRegOutput) = v.contig + "\t" + v.start + "\t" + v.ref + "\t" + v.alt +
-      "\t" + lro.nMissing + "\t" + lro.beta + lro.stdError + "\t" + lro.t
+      "\t" + lro.nMissing + "\t" + lro.beta + lro.stdError + "\t" + lro.t + "\t" + lro.p
     lr.map((toLine _).tupled)
-      .writeTable(filename, "CHR\tPOS\tREF\tALT\tMISS\tBETA\tSE\tT\n")
+      .writeTable(filename, "CHR\tPOS\tREF\tALT\tMISS\tBETA\tSE\tT\tP\n")
   }
 }
