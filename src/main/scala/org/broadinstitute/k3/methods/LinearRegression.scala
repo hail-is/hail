8004a68b79be23ff0cc89eed35f7a12a2ef7e97d
jbloom22 <jbloom@broadinstitute.org>
2015-10-27 19:13:51 -0400

    switched to aggregateByKey

--- src/main/scala/org/broadinstitute/k3/methods/LinearRegression.scala
+++ src/main/scala/org/broadinstitute/k3/methods/LinearRegression.scala
@@ -40,88 +40,71 @@ object CovariateData {
 
 case class CovariateData(sampleOfRow: Array[Int], covariateOfCol: Array[String], data: DenseMatrix[Double])
 
-case class LinRegOutput(nMissing: Int, beta: Double, t: Double, p: Double)
+case class LinRegStat(nMissing: Int, beta: Double, se: Double, t: Double, p: Double)
 
 object LinearRegression {
   def name = "LinearRegression"
 
   def apply(vds: VariantDataset, ped: Pedigree, cov: CovariateData): LinearRegression = {
     require(ped.phenoDefinedForAll)
-
+    val rowOfSample = cov.sampleOfRow.zipWithIndex.toMap
+    val isPhenotyped: Array[Boolean] = (0 until vds.nSamples).map(rowOfSample.isDefinedAt).toArray
+    
     val n = cov.data.rows
     val k = cov.data.cols
     val d = n - k - 2
     require(d > 0)
-
-    // extract the phenotype vector y
-    val rowOfSample = cov.sampleOfRow.zipWithIndex.toMap
-    val y = DenseVector.zeros[Double](n)
-    for (row <- 0 until n)
-     y(row) = ped.phenoOf(cov.sampleOfRow(row)).toString.toDouble
-
-    // augment covariate matrix with 1s vector, compute q.t and yyp = yp dot yp
-    val qt = qr.reduced.justQ(DenseMatrix.horzcat(DenseMatrix.ones[Double](n, 1), cov.data)).t
-    val qty = qt * y
-    val yyp = (y dot y) - (qty dot qty)
-
+    
     val sc = vds.sparkContext
     val rowOfSampleBc = sc.broadcast(rowOfSample)
-    val isPhenotypedBc = sc.broadcast((0 until vds.nSamples).map(rowOfSample.isDefinedAt).toArray)
-
-    val yBc = sc.broadcast(y)
-    val qtBc = sc.broadcast(qt)
-    val qtyBc = sc.broadcast(qty)
-    val yypBc = sc.broadcast(yyp)
-
+    val isPhenotypedBc = sc.broadcast(isPhenotyped)
     val tDistBc = sc.broadcast(new TDistribution(null, d.toDouble))
+
+    val yArray = (0 until n).toArray.map(row => ped.phenoOf(cov.sampleOfRow(row)).toString.toDouble)
+    val covAndOnesVector = DenseMatrix.horzcat(cov.data, DenseMatrix.ones[Double](n, 1))
+    val y = DenseVector[Double](yArray)
+    val qt = qr.reduced.justQ(covAndOnesVector).t
+    val qty = qt * y
+    
+    val yBc =   sc.broadcast(y)
+    val qtBc =  sc.broadcast(qt)
+    val qtyBc = sc.broadcast(qty)
+    val yypBc = sc.broadcast((y dot y) - (qty dot qty))
+    
+    type LinRegBuilder = (List[(Int, Int)], Int, Int, Double, List[Int])
+
+    def zeroValue: LinRegBuilder = (List(), 0, 0, 0.0, List())
+
+    def seqOp(l: LinRegBuilder, rowGt: (Int, Option[Int])): LinRegBuilder = {
+      val (sparseX, sumX, sumXX, sumXY, missRows) = l
+      val (row, gt) = rowGt
+      gt match {
+        case Some(0) => (                 sparseX, sumX    , sumXX    , sumXY                     ,        missRows)
+        case Some(1) => ((row, gt.get) :: sparseX, sumX + 1, sumXX + 1, sumXY +     yBc.value(row),        missRows)
+        case Some(2) => ((row, gt.get) :: sparseX, sumX + 2, sumXX + 4, sumXY + 2 * yBc.value(row),        missRows)
+        case None    => (                 sparseX, sumX    , sumXX    , sumXY                     , row :: missRows)
+      }
+    }
+
+    def combOp(l1: LinRegBuilder, l2: LinRegBuilder): LinRegBuilder =
+      (l1._1 ++ l2._1, l1._2 + l2._2, l1._3 + l2._3, l1._4 + l2._4, l1._5 ++ l2._5)
 
     new LinearRegression(vds
       .filterSamples(s => isPhenotypedBc.value(s))
-      .mapWithKeys{ (v, s, g) => (v, (rowOfSampleBc.value(s), g.call.map(_.gt))) }
-      .combineByKey[(List[(Int, Int)], Int, Int, Double, List[Int])](
-        (rowGt: (Int, Option[Int])) => {
-        val (row, gt) = rowGt
-        val missingRows: List[Int] = List()
-        if (gt.isDefined)
-          if (gt.get != 0)
-            (List((row, gt.get)), gt.get, gt.get * gt.get, gt.get * yBc.value(row), List())
-          else
-            (List(), 0, 0, 0.0, List())
-        else
-          (List(), 0, 0, 0.0, List(row))
-      },
-      (t: (List[(Int, Int)], Int, Int, Double, List[Int]), rowGt: (Int, Option[Int])) => {
-        val (x0, sumX, sumXX, sumXY, missingRows) = t
-        val (row, gt) = rowGt
-        if (gt.isDefined)
-          if (gt.get != 0)
-            ((row, gt.get) :: x0, sumX + gt.get, sumXX + gt.get * gt.get, sumXY + gt.get * yBc.value(row), missingRows)
-          else
-            (x0, sumX, sumXX, sumXY, missingRows)
-        else
-          (x0, sumX, sumXX, sumXY, row :: missingRows)
-        },
-      (t: (List[(Int, Int)], Int, Int, Double, List[Int]), u: (List[(Int, Int)], Int, Int, Double, List[Int])) => (t._1 ++ u._1, t._2 + u._2, t._3 + u._3, t._4 + u._4, t._5 ++ u._5))
-      .mapValues{ case (x0, sumX, sumXX, sumXY, missingRows) => {
+      .mapWithKeys[(Variant, (Int, Option[Int]))]{ (v, s, g) => (v, (rowOfSampleBc.value(s), g.call.map(_.gt))) }
+      .aggregateByKey[LinRegBuilder](zeroValue)(seqOp, combOp)
+      .mapValues{ case (sparseX, sumX, sumXX, sumXY, missRows) =>
         assert(sumX > 0) //FIXME: better error handling
-        val n = qtBc.value.cols
-        val d = n - qtBc.value.rows - 1 // qt includes vector of 1s
 
-        val nMissing = missingRows.length
-        val mu = sumX.toDouble / (n - nMissing)
-        val xSort = x0.sortBy(_._1)
-        val (index, data) = xSort.unzip
-        val x = new SparseVector[Double](index.toArray, data.map(_.toDouble).toArray, n)
+        val (rows, gts) = sparseX.sortBy(_._1).unzip
+        val x = new SparseVector[Double](rows.toArray, gts.map(_.toDouble).toArray, n)
 
-        missingRows.foreach(row => x(row) = mu)
+        val nMiss = missRows.length
+        val meanX = sumX.toDouble / (n - nMiss)
+        missRows.foreach(row => x(row) = meanX)
 
-        // these lines are useful for normalization, unnecessary for regression
-        // val sum = gtSum + nMissing * mu
-        // val sumSqCentered = math.sqrt(sumSq - sum * sum / n) // = (x - mu) dot (x - mu)
-
-        // compute regression coef and stats for 2-sided t test with d degrees of freedom
-        val xx = sumXX + mu * mu * nMissing
-        val xy = sumXY + mu * missingRows.map(row => yBc.value(row)).sum
+        val xx = sumXX + meanX * meanX * nMiss
+        val xy = sumXY + meanX * missRows.map(row => yBc.value(row)).sum
 
         val qtx = qtBc.value * x
         val qty = qtyBc.value
@@ -135,117 +118,18 @@ object LinearRegression {
         val t = b / se
         val p = 2 * tDistBc.value.cumulativeProbability(- math.abs(t))
 
-        LinRegOutput(nMissing, b, t, p)
-        }
+        LinRegStat(nMiss, b, se, t, p)
       }
     )
   }
 }
 
-
-// FIXME: Deprecated!
-object LinearRegressionGroupByKey {
-  def name = "LinearRegression"
-
-  def apply(vds: VariantDataset, ped: Pedigree, cov: CovariateData): LinearRegression = {
-    require(ped.phenoDefinedForAll)
-
-    val n = cov.data.rows
-    val k = cov.data.cols
-    val d = n - k - 2
-    require(d > 0)
-
-    // extract the phenotype vector y
-    val rowOfSample = cov.sampleOfRow.zipWithIndex.toMap
-    val y = DenseVector.zeros[Double](n)
-    for (row <- 0 until n)
-      y(row) = ped.phenoOf(cov.sampleOfRow(row)).toString.toDouble
-
-    // augment covariate matrix with 1s vector, compute q.t and yyp = yp dot yp
-    val qt = qr.reduced.justQ(DenseMatrix.horzcat(DenseMatrix.ones[Double](n, 1), cov.data)).t
-    val qty = qt * y
-    val yyp = (y dot y) - (qty dot qty)
-
-    val sc = vds.sparkContext
-    val rowOfSampleBc = sc.broadcast(rowOfSample)
-    val isPhenotypedBc = sc.broadcast((0 until vds.nSamples).map(rowOfSample.isDefinedAt).toArray)
-
-    val yBc = sc.broadcast(y)
-    val qtBc = sc.broadcast(qt)
-    val qtyBc = sc.broadcast(qty)
-    val yypBc = sc.broadcast(yyp)
-
-    val tDistBc = sc.broadcast(new TDistribution(null, d.toDouble))
-
-    new LinearRegression(vds
-      .filterSamples(s => isPhenotypedBc.value(s))
-      .mapWithKeys{ (v, s, g) => (v, (rowOfSampleBc.value(s), g.call.map(_.gt))) }
-      .groupByKey()
-      .mapValues{
-        gts => {
-          val qt = qtBc.value
-          val n = qt.cols
-          val d = n - qt.rows - 1 // qt includes vector of 1s
-
-          // replace missing values with mean
-          val y = yBc.value
-          val x = SparseVector.zeros[Double](n)
-          val missingRowsBuffer = new ListBuffer[Int]()
-          var sumX = 0
-          var sumXX = 0
-          var sumXY = 0.0
-          gts.foreach{
-            case (row, gt) =>
-              if (gt.isDefined) {
-                if (gt.get != 0) {
-                  sumX += gt.get
-                  sumXX += gt.get * gt.get
-                  sumXY += gt.get * y(row)
-                  x(row) = gt.get.toDouble
-                }
-              }
-              else
-                missingRowsBuffer += row
-          }
-          assert(sumX > 0) //FIXME: better error handling
-          val missingRows = missingRowsBuffer.toList
-          val nMissing = missingRows.length
-          val mu = sumX.toDouble / (n - nMissing)
-          missingRows.foreach(row => x(row) = mu)
-
-          // these lines are useful for normalization, unnecessary for regression
-          // val sum = gtSum + nMissing * mu
-          // val sumSqCentered = math.sqrt(sumSq - sum * sum / n) // = (x - mu) dot (x - mu)
-
-          // compute regression coef and stats for 2-sided t test with d degrees of freedom
-          val xx = sumXX + mu * mu * nMissing
-          val xy = sumXY + mu * missingRows.map(row => y(row)).sum
-
-          val qtx = qt * x
-          val qty = qtyBc.value
-
-          val xxp = xx - (qtx dot qtx)
-          val xyp = xy - (qtx dot qty)
-          val yyp = yypBc.value
-
-          val b = xyp / xxp
-          val se = math.sqrt((yyp / xxp - b * b) / d)
-          val t = b / se
-          val p = 2 * tDistBc.value.cumulativeProbability(- math.abs(t))
-
-          LinRegOutput(nMissing, b, t, p)
-        }
-      }
-    )
-  }
-}
-
-case class LinearRegression(lr: RDD[(Variant, LinRegOutput)]) {
+case class LinearRegression(lr: RDD[(Variant, LinRegStat)]) {
 
   def write(filename: String) {
-    def toLine(v: Variant, lro: LinRegOutput) = v.contig + "\t" + v.start + "\t" + v.ref + "\t" + v.alt +
-      "\t" + lro.nMissing + "\t" + lro.beta + "\t" + lro.t + "\t" + lro.p
+    def toLine(v: Variant, lrs: LinRegStat) = v.contig + "\t" + v.start + "\t" + v.ref + "\t" + v.alt +
+      "\t" + lrs.nMissing + "\t" + lrs.beta + "\t" + lrs.se + "\t" + lrs.t + "\t" + lrs.p
     lr.map((toLine _).tupled)
-      .writeTable(filename, "CHR\tPOS\tREF\tALT\tMISS\tBETA\tT\tP\n")
+      .writeTable(filename, "CHR\tPOS\tREF\tALT\tMISS\tBETA\tSE\tT\tP\n")
   }
 }
