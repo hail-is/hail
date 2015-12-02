b0b7c90384c2bdc2459f744b6480eab8543723d6
jbloom22 <jbloom@broadinstitute.org>
2015-12-02 00:37:56 -0500

    rewrote eval to use FilterOption, but getting error: unable to cast Booleen to FilterOption

--- src/test/scala/org/broadinstitute/hail/methods/FilterSuite.scala
+++ src/test/scala/org/broadinstitute/hail/methods/FilterSuite.scala
@@ -24,25 +24,27 @@ class FilterSuite extends SparkSuite {
     assert(!highGQ.exists { case (v, s, g) => g.call.exists(c => c.gq < 20) })
     assert(highGQ.count{ case (v, s, g) => g.call.exists(c => c.gq >= 20) } == 31260)
 
+
     val vds2 = TestRDDBuilder.buildRDD(1, 1, sc, vsmtype = "sparky")
     val state2 = State("", sc, sqlContext, vds2)
 
     val nVariants = vds2.nVariants
-    assert(FilterVariants.run(state2, Array("--remove", "-c", "Some(Array(1,2)).size2 == 2"))
+    assert(FilterVariants.run(state2, Array("--remove", "-c", "Array(1,2).size === 2"))
       .vds.nVariants == 0)
 
-    assert(FilterVariants.run(state2, Array("--remove", "-c", "true == true"))
+/*
+    assert(FilterVariants.run(state2, Array("--remove", "-c", "true === true"))
       .vds.nVariants == 0)
 
-    assert(FilterVariants.run(state2, Array("--remove", "-c", "true == Some(true)"))
+    assert(FilterVariants.run(state2, Array("--remove", "-c", "true === Some(true)"))
       .vds.nVariants == 0)
 
-    assert(FilterVariants.run(state2, Array("--remove", "-c", "Some(true) == Some(true)"))
+    assert(FilterVariants.run(state2, Array("--remove", "-c", "Some(true) === Some(true)"))
       .vds.nVariants == 0)
 
-    assert(FilterVariants.run(state2, Array("--remove", "-c", "Some(true) == true"))
+    assert(FilterVariants.run(state2, Array("--remove", "-c", "Some(true) === true"))
       .vds.nVariants == 0)
-
+*/
 
 /*
     assert(FilterVariants.run(state, Array("--remove", "-c", "val a: Option[Double] = Some(2.5);val b: Option[Double] = Some(2.4); a > b"))
