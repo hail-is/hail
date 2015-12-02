253e3847a831ffa7bdecc3eb6b5c5de56c721900
jbloom22 <jbloom@broadinstitute.org>
2015-12-01 22:47:22 -0500

    rewrote Filter to use FilterOption values everywhere

--- src/main/scala/org/broadinstitute/hail/methods/Filter.scala
+++ src/main/scala/org/broadinstitute/hail/methods/Filter.scala
@@ -12,64 +12,59 @@ class FilterString(val s: String) extends AnyVal {
 }
 
 class FilterOption[T](val ot: Option[T]) extends AnyVal {
-  //override def toString: Option[String] = ot.map(_.toString)
-  def ===(that: FilterOption[T]): FilterOption[Boolean] = this.ot.flatMap(t => new FilterOption[Boolean](that.ot.map(_ == t)))
-  def !==(that: FilterOption[T]): FilterOption[Boolean] = new FilterOption[Boolean]((this === that).ot.map(!_))
+  override def toString: String = if (ot.isDefined) ot.get.toString else "NA"
+  def ===(that: FilterOption[T]): FilterOption[Boolean] = new FilterOption(this.ot.flatMap(t => that.ot.map(_ == t)))
+  def !==(that: FilterOption[T]): FilterOption[Boolean] = new FilterOption((this === that).ot.map(!_))
 }
 
 class FilterBooleanOption(val ob: Option[Boolean]) extends AnyVal {
-  def &&(ob2: Option[Boolean]): Option[Boolean] = ob.flatMap(b => ob2.map(b && _))
-  def ||(ob2: Option[Boolean]): Option[Boolean] = ob.flatMap(b => ob2.map(b || _))
-  def unary_!(): Option[Boolean] = ob.map(! _)
+  def &&(that: FilterBooleanOption): FilterOption[Boolean] = new FilterOption(this.ob.flatMap(b => that.ob.map(b && _)))
+  def ||(that: FilterBooleanOption): FilterOption[Boolean] = new FilterOption(this.ob.flatMap(b => that.ob.map(b || _)))
+  def unary_!(): FilterOption[Boolean] = new FilterOption(this.ob.map(! _))
 }
 
 class FilterStringOption(val os: Option[String]) extends AnyVal {
-  def toInt: Option[Int] = os.map(_.toInt)
-  def toDouble: Option[Double] = os.map(_.toDouble)
-  def +(os2: Option[String]) = os.flatMap(s => os2.map(s + _))
-  def apply(i: Int) = os.map(_(i))
+  def toInt: FilterOption[Int] = new FilterOption(this.os.map(_.toInt))
+  def toDouble: FilterOption[Double] = new FilterOption(this.os.map(_.toDouble))
+  def +(that: FilterStringOption) = new FilterOption(this.os.flatMap(s => that.os.map(s + _)))
+  def apply(i: Int): FilterOption[Char] = new FilterOption(this.os.map(_(i)))
 }
 
 class FilterArrayOption[T](val oa: Option[Array[T]]) extends AnyVal {
-  def apply(i: Int): Option[T] = oa.map(_(i))
-  def size2: Option[Int] = oa.map(_.size)
-//  def ++(oa2: Option[Array[T]]): Option[Array[T]] = oa.flatMap(a => oa2.map(a ++ _))
+  def apply(i: Int): FilterOption[T] = new FilterOption(this.oa.map(_(i)))
+  def size: FilterOption[Int] = new FilterOption(oa.map(_.size))
+//  def ++(that: FilterArrayOption[T]): FilterOption[Array[T]] = new FilterOption(this.oa.flatMap(a => that.oa.map(a ++ _)))
 }
 
+// FIXME: replace with compare
 class FilterOrderedOption[T <: Ordered[T]](val ot: Option[T]) extends AnyVal {
-  def >(ot2: Option[T]): Option[Boolean] = ot.flatMap(t => ot2.map(t > _))
-  def <(ot2: Option[T]): Option[Boolean] = ot.flatMap(t => ot2.map(t < _))
-  def >=(ot2: Option[T]): Option[Boolean] = ot.flatMap(t => ot2.map(t >= _))
-  def <=(ot2: Option[T]): Option[Boolean] = ot.flatMap(t => ot2.map(t <= _))
+  def >(that: FilterOrderedOption[T]): FilterOption[Boolean] = new FilterOption(this.ot.flatMap(t => that.ot.map(t > _)))
+  def <(that: FilterOrderedOption[T]): FilterOption[Boolean] = new FilterOption(this.ot.flatMap(t => that.ot.map(t < _)))
+  def >=(that: FilterOrderedOption[T]): FilterOption[Boolean] = new FilterOption(this.ot.flatMap(t => that.ot.map(t >= _)))
+  def <=(that: FilterOrderedOption[T]): FilterOption[Boolean] = new FilterOption(this.ot.flatMap(t => that.ot.map(t <= _)))
 }
 
 class FilterNumericOption[T <: scala.math.Numeric[T]#Ops](val ot: Option[T]) extends AnyVal {
-  def +(ot2: Option[T]): Option[T] = ot.flatMap(t => ot2.map(t + _))
-  def -(ot2: Option[T]): Option[T] = ot.flatMap(t => ot2.map(t - _))
-  def *(ot2: Option[T]): Option[T] = ot.flatMap(t => ot2.map(t * _))
-  def unary_-(): Option[T] = ot.map(-_)
-  def abs: Option[T] = ot.map(_.abs())
-  def signum: Option[Int] = ot.map(_.signum())
-  def toInt: Option[Int] = ot.map(_.toInt())
-  def toLong: Option[Long] = ot.map(_.toLong())
-  def toFloat: Option[Float] = ot.map(_.toFloat())
-  def toDouble: Option[Double] = ot.map(_.toDouble())
-
+  def +(that: FilterNumericOption[T]): FilterOption[T] = new FilterOption(this.ot.flatMap(t => that.ot.map(t + _)))
+  def -(that: FilterNumericOption[T]): FilterOption[T] = new FilterOption(this.ot.flatMap(t => that.ot.map(t - _)))
+  def *(that: FilterNumericOption[T]): FilterOption[T] = new FilterOption(this.ot.flatMap(t => that.ot.map(t * _)))
+  def unary_-(): FilterOption[T] = new FilterOption(this.ot.map(-_))
+  def abs: FilterOption[T] = new FilterOption(this.ot.map(_.abs()))
+  def signum: FilterOption[Int] = new FilterOption(this.ot.map(_.signum()))
+  def toInt: FilterOption[Int] = new FilterOption(this.ot.map(_.toInt()))
+  def toLong: FilterOption[Long] = new FilterOption(this.ot.map(_.toLong()))
+  def toFloat: FilterOption[Float] = new FilterOption(this.ot.map(_.toFloat()))
+  def toDouble: FilterOption[Double] = new FilterOption(this.ot.map(_.toDouble()))
 }
 
 
-
-
 object FilterUtils {
-/*  implicit def toFilterString(s: String): FilterString = new FilterString(s)
-  implicit def toFilterOption[T](t: T): FilterOption[T] = {println("converted a thing: " + t); new FilterOption[T](Some(t))}
-  implicit def toFilterStringOption(s: String): FilterStringOption = new FilterStringOption(Some(s))
-  implicit def toFilterBooleanOption(b: Boolean): FilterBooleanOption = {println("converted a thing: " + b); new FilterBooleanOption(Some(b))}
-  implicit def toFilterArrayOption[T](a: Array[T]): FilterArrayOption[T] = {println("converted a thing: " + a); new FilterArrayOption[T](Some(a))}
-  implicit def toFilterArrayOption[T](a: Option[Array[T]]): FilterArrayOption[T] = {println("converted a thing: " + a); new FilterArrayOption[T](a)}
-  implicit def toFilterOrderedOption[T <: Ordered[T]](t: T): FilterOrderedOption[T] = new FilterOrderedOption[T](Some(t))
-  implicit def toFilterNumericOption[T <: scala.math.Numeric[T]#Ops](t: T): FilterNumericOption[T] = new FilterNumericOption[T](Some(t))
-*/
+  implicit def toFilterOption[T](t: T): FilterOption[T] = {println("converted to FO: " + t); new FilterOption(Some(t))}
+  implicit def toFilterBooleanOption(fo: FilterOption[Boolean]): FilterBooleanOption = {println("converted to FOB: " + fo); new FilterBooleanOption(fo.ot)}
+  implicit def toFilterStringOption(fo: FilterOption[String]): FilterStringOption = {println("converted to FOS: " + fo); new FilterStringOption(fo.ot)}
+  implicit def toFilterArrayOption[T](fo: FilterOption[Array[T]]): FilterArrayOption[T] = {println("converted to FOA: " + fo); new FilterArrayOption[T](fo.ot)}
+  implicit def toFilterOrderedOption[T <: Ordered[T]](fo: FilterOption[T]): FilterOrderedOption[T] = {println("converted to FOO: " + fo); new FilterOrderedOption[T](fo.ot)}
+  implicit def toFilterNumericOption[T <: scala.math.Numeric[T]#Ops](fo: FilterOption[T]): FilterNumericOption[T] = {println("converted to FON: " + fo); new FilterNumericOption[T](fo.ot)}
 }
 
 class Evaluator[T](t: String)(implicit tct: ClassTag[T])
