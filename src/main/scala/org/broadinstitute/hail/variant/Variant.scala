c58db750a5380de458f8268638f6c0a29935b85c
jbloom22 <jbloom@broadinstitute.org>
2016-01-13 13:23:22 -0500

    added ploidy in variant and mendelerrors

--- src/main/scala/org/broadinstitute/hail/variant/Variant.scala
+++ src/main/scala/org/broadinstitute/hail/variant/Variant.scala
@@ -1,9 +1,16 @@
 package org.broadinstitute.hail.variant
 
+
 object VariantType extends Enumeration {
   type VariantType = Value
   val SNP, MNP, Insertion, Deletion, Complex = Value
 }
+
+object Ploidy extends Enumeration {
+  type Ploidy = Value
+  val Auto, HemiX, HemiY = Value
+}
+
 
 case class Variant(contig: String,
                    // FIXME: 0- or 1-based?
@@ -33,12 +40,21 @@ case class Variant(contig: String,
 
   // PAR regions of sex chromosomes: https://en.wikipedia.org/wiki/Pseudoautosomal_region
   // Boundaries for build GRCh37: http://www.ncbi.nlm.nih.gov/projects/genome/assembly/grc/human/
-  def inParX(pos: Int): Boolean = (60001 <= pos && pos <= 2699520) || (154931044 <= pos && pos <= 155260560)
-  def inParY(pos: Int): Boolean = (10001 <= pos && pos <= 2649520) || ( 59034050 <= pos && pos <=  59363566)
-  def inParX: Boolean = inParX(start)
-  def inParY: Boolean = inParY(start)
-  def isHemizygous(sex: Sex.Sex): Boolean = (sex == Sex.Male) &&
-      (contig == "X" && !inParX(start)) || (contig == "Y" && !inParY(start))
+  def inParX: Boolean = (60001 <= start && start <= 2699520) || (154931044 <= start && start <= 155260560)
+  def inParY: Boolean = (10001 <= start && start <= 2649520) || ( 59034050 <= start && start <=  59363566)
+
+  import Ploidy._
+
+  def ploidy(sex: Sex.Sex): Ploidy =
+    if (sex == Sex.Male)
+      if (contig == "X" && !inParX)
+        HemiX
+      else if (contig == "Y" && !inParY)
+        HemiY
+      else
+        Auto
+    else
+      Auto
 
   def isSNP: Boolean = (ref.length == 1 && alt.length == 1) ||
       (ref.length == alt.length && nMismatch == 1)
