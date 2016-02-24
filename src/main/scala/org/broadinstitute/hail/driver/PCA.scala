1c202f66cdeb372ac682e2bdae7e7dc41d68ca87
Alex Bloemendal <bloem@broadinstitute.org>
2016-02-23 19:36:01 -0500

    Done, except for sorting the variants in the loading output.

--- src/main/scala/org/broadinstitute/hail/driver/PCA.scala
+++ src/main/scala/org/broadinstitute/hail/driver/PCA.scala
@@ -6,6 +6,7 @@ import org.broadinstitute.hail.Utils._
 
 object PCA extends Command {
   def name = "pca"
+
   def description = "Compute PCA on the matrix of genotypes"
 
   class Options extends BaseOptions {
@@ -22,33 +23,51 @@ object PCA extends Command {
     var e: Boolean = _
 
   }
+
   def newOptions = new Options
 
   def run(state: State, options: Options): State = {
+
     val vds = state.vds
+    val filename = options.output.replaceAll(".tsv", "")
 
     val (scores, loadings, eigenvalues) = (new SamplePCA(options.k, options.l, options.e))(vds)
 
-    writeTextFile(options.output, state.hadoopConf) { s =>
+    writeTextFile(filename + ".tsv", state.hadoopConf) { s =>
       s.write("sample")
       for (i <- 0 until options.k)
         s.write("\t" + "PC" + (i + 1))
       s.write("\n")
-
-
       for (i <- 0 until vds.nLocalSamples) {
         s.write(vds.sampleIds(vds.localSamples(i)))
         for (j <- 0 until options.k)
-          s.write("\t" + scores(i,j))
+          s.write("\t" + scores(i, j))
         s.write("\n")
       }
     }
 
-//    if (options.l)
-//      writeTextFile(options.output, state.hadoopConf) { s =>
-//        s.write("chrom" + "\t" + "pos" + "\t" + "ref" + "\t")
-//        for (i <- 0 until options.k)
-//      }
+    if (options.l) {
+      val vls = loadings.collect() //FIXME: Sort!
+      writeTextFile(filename + ".loadings.tsv", state.hadoopConf) { s =>
+        s.write("chrom\t pos\t ref\t alt")
+        for (i <- 0 until options.k)
+          s.write("\t" + "PC" + (i + 1))
+        s.write("\n")
+        for (i <- 0 until vds.nVariants.toInt) {
+          val (v, l) = vls(i)
+          s.write(v.contig + "\t" + v.start + "\t" + v.ref + "\t" + v.alt)
+          for (j <- 0 until options.k)
+            s.write("\t" + l(j))
+          s.write("\n")
+        }
+      }
+    }
+
+    if (options.e)
+      writeTextFile(filename + ".eigen.tsv", state.hadoopConf) { s =>
+        for (i <- 0 until options.k)
+          s.write(eigenvalues(i) + "\n")
+      }
 
     state
   }
