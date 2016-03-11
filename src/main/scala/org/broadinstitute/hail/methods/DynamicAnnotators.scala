9aaad0b287f93f6e945894600e7304ab26de6d6f
tpoterba <tpoterba@broadinstitute.org>
2016-03-11 16:37:10 -0500

    placeholder

---
+++ src/main/scala/org/broadinstitute/hail/methods/DynamicAnnotators.scala
@@ -0,0 +1,20 @@
+package org.broadinstitute.hail.methods
+
+import org.broadinstitute.hail.variant.VariantDataset
+
+object DynamicAnnotators {
+
+  object SampleAnnotator {
+
+
+    def apply(vds: VariantDataset, cond: String): VariantDataset = {
+
+
+
+     ???
+    }
+
+  }
+
+
+}
