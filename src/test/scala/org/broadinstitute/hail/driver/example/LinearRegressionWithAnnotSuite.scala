5a58bfff9bf292f022b70cf45f858aacdce7636b
jbloom22 <jbloom@broadinstitute.org>
2016-04-08 00:25:24 -0400

    getting the hang of queries, can now produce y vector and cov matrix

--- src/test/scala/org/broadinstitute/hail/driver/example/LinearRegressionWithAnnotSuite.scala
+++ src/test/scala/org/broadinstitute/hail/driver/example/LinearRegressionWithAnnotSuite.scala
@@ -1,8 +1,11 @@
 package org.broadinstitute.hail.driver.example
 
+import breeze.linalg.{DenseMatrix, DenseVector}
 import org.broadinstitute.hail.SparkSuite
 import org.broadinstitute.hail.driver.{AnnotateSamples, ImportVCF, State}
 import org.testng.annotations.Test
+
+import scala.collection.mutable
 
 class LinearRegressionWithAnnotSuite extends SparkSuite {
   @Test def test() {
@@ -24,8 +27,31 @@ class LinearRegressionWithAnnotSuite extends SparkSuite {
 
     s.vds.metadata.sampleAnnotations.foreach(println)
 
-    val qCov = s.vds.querySA("cov", "Cov1")
+    val qCov1 = s.vds.querySA("cov", "Cov1")
+    val qCov2 = s.vds.querySA("cov", "Cov2")
     val qPheno = s.vds.querySA("pheno", "Pheno")
+
+    val cov1 = s.vds.sampleAnnotations.map(qCov1)
+    val cov2 = s.vds.sampleAnnotations.map(qCov1)
+    val pheno = s.vds.sampleAnnotations.map(qPheno)
+
+    val sampleMask = Range(0, s.vds.nSamples).map(s => cov1(s).isDefined && cov2(s).isDefined && pheno(s).isDefined)
+
+    val filtVds = s.vds.filterSamples((s, sa) => sampleMask(s))
+
+    val cov1Data = cov1.zipWithIndex.filter(x => sampleMask(x._2)).map(_._1.get.asInstanceOf[Double])
+    val cov2Data = cov1.zipWithIndex.filter(x => sampleMask(x._2)).map(_._1.get.asInstanceOf[Double])
+    val covArray = (cov1Data ++ cov2Data).toArray
+
+    val yArray = pheno.zipWithIndex.filter(x => sampleMask(x._2)).map(_._1.get.asInstanceOf[Double]).toArray
+
+    val n = yArray.size
+
+    val y: DenseVector[Double] = DenseVector(yArray)
+    val cov: DenseMatrix[Double] = new DenseMatrix(n, 2, covArray)
+
+    println(y)
+    println(cov)
 
     /*
     s = CaseControlCount.run(s, Array[String]())
