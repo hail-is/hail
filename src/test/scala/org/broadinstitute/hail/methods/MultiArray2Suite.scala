e1eee6570c4334f13064a26ed6620004bf8584d3
Jackie Goldstein <jigold@broadinstitute.org>
2016-05-05 13:57:30 -0400

    first test

--- src/test/scala/org/broadinstitute/hail/methods/MultiArray2Suite.scala
+++ src/test/scala/org/broadinstitute/hail/methods/MultiArray2Suite.scala
@@ -6,7 +6,7 @@ import org.testng.annotations.Test
 
 class MultiArray2Suite extends SparkSuite{
   @Test def test() = {
-    assert(5 == 23)
+    assert(5 == 24)
     // test multiarray of size 0 will be created
     val ma0 = MultiArray2.fill[Int](0, 0)(0)
 
