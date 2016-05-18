7dcd6b1f75cbcbb441cd161b5ce40c1f39b688f1
Jackie Goldstein <jigold@broadinstitute.org>
2016-05-18 14:24:59 -0400

    first pass with double as type

--- src/main/scala/org/broadinstitute/hail/driver/CreateGroup.scala
+++ src/main/scala/org/broadinstitute/hail/driver/CreateGroup.scala
@@ -8,26 +8,22 @@ import org.broadinstitute.hail.variant._
 import org.kohsuke.args4j.{Option => Args4jOption}
 import scala.collection.mutable.ArrayBuffer
 
-class GenotypeStreamCombiner(nSamples: Int, aggregator: (Option[Double], Option[Double]) => Option[Double]) extends Serializable {
-  val resultVector = Array.fill[Option[Double]](nSamples)(None)
+class GenotypeStreamCombiner[T](nSamples: Int, aggregator: (Option[T], Option[T]) => Option[T]) extends Serializable {
+  val resultVector = Array.fill[Option[T]](nSamples)(None)
 
-  def addCount(gs: Iterable[Genotype], ec: EvalContext, f: () => Option[Any], t: (Any) => Double): GenotypeStreamCombiner = {
+  def addCount(gs: Iterable[Genotype], ec: EvalContext, f: () => Option[Any], t: (Any) => T): GenotypeStreamCombiner[T] = {
     require(gs.size == nSamples)
+
     gs.zipWithIndex.foreach{case (g: Genotype, i: Int) =>
       ec.setAll(g)
-      val x = f().map{t}
-      resultVector(i) = (resultVector(i) ++ x).reduceOption(_ + _)
+      resultVector(i) = aggregator(resultVector(i), f().map(t))
     }
     this
   }
 
-  def combineCounts(other: GenotypeStreamCombiner): GenotypeStreamCombiner = {
+  def combineCounts(other: GenotypeStreamCombiner[T]): GenotypeStreamCombiner[T] = {
     other.resultVector.zipWithIndex.foreach{case (g2, i) => resultVector(i) = aggregator(resultVector(i), g2)}
     this
-  }
-
-  override def toString: String = {
-    resultVector.mkString(",")
   }
 }
 
@@ -35,11 +31,11 @@ object CreateGroup extends Command {
 
   class Options extends BaseOptions {
 
-    @Args4jOption(required = false, name = "-k", aliases = Array("--keys"),
+    @Args4jOption(required = false, name = "-k", aliases = Array("--groupkeys"),
       usage = "comma-separated list of annotations to be used as grouping variable(s) (must be attribute of va)")
     var groupKeys: String = _
 
-    @Args4jOption(required = false, name = "-v", aliases = Array("--variable"),
+    @Args4jOption(required = false, name = "-v", aliases = Array("--groupvalues"),
       usage = "genotype variable to group (must be attribute of g)")
     var groupValues: String = _
 
@@ -59,9 +55,6 @@ object CreateGroup extends Command {
 
   def run(state: State, options: Options): State = {
     val vds = state.vds
-    val sc = state.sc
-    val vas = vds.vaSignature
-    val sas = vds.saSignature
 
     def toDouble(t: BaseType, code: String): Any => Double = t match {
       case TInt => _.asInstanceOf[Int].toDouble
@@ -73,7 +66,6 @@ object CreateGroup extends Command {
     }
 
     val symTab = Map("g" -> (0, TGenotype))
-
     val ec = EvalContext(symTab)
 
     val (xT, xQ) = Parser.parse(options.groupValues, ec)
@@ -81,14 +73,14 @@ object CreateGroup extends Command {
 
     val aggregator = options.aggregator match {
       case "sum" => (a: Option[Double], b: Option[Double]) => (a ++ b).reduceOption(_ + _)
-      case "carrier" => (a: Option[Double], b: Option[Double]) => (a ++ b).reduceOption[Double]{case (d1, d2) => math.min(math.min(d1, d2), 1.0)}
+      case "carrier" => (a: Option[Double], b: Option[Double]) => (a ++ b).reduceOption[Double]{case (d1, d2) => math.min(math.max(d1, d2), 1.0)}
       case _ => fatal(s"Option for '-a' ${options.aggregator} not recognized.")
     }
-    
+
     val queriers = options.groupKeys.split(",").map{vds.queryVA(_)._2}
 
     state.copy(group = vds.rdd.map{case (v, va, gs) => (queriers.map{_(va).get}.toIndexedSeq, gs)}
-      .aggregateByKey(new GenotypeStreamCombiner(vds.nSamples, aggregator))(
+      .aggregateByKey(new GenotypeStreamCombiner[Double](vds.nSamples, aggregator))(
         (comb, gs) => comb.addCount(gs, ec, xQ, xToDouble), (comb1, comb2) => comb1.combineCounts(comb2))
       .map{case (k, v) => (k, v.resultVector)}
     )
