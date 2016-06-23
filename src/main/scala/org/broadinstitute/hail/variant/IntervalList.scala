dde4d1b111670ef4de45228ddbc304f00e43b106
tpoterba <tpoterba@broadinstitute.org>
2016-06-23 13:46:27 -0400

    Rewrote interval lists to support future work in interval aggregation

--- src/main/scala/org/broadinstitute/hail/variant/IntervalList.scala
+++ src/main/scala/org/broadinstitute/hail/variant/IntervalList.scala
@@ -1,95 +1,201 @@
 package org.broadinstitute.hail.variant
 
-import java.io.File
-import java.util.TreeMap
-import org.broadinstitute.hail.annotations.Annotation
-
-import scala.collection.mutable
-import scala.io.Source
-import scala.collection.JavaConverters._
 import org.apache.hadoop
 import org.broadinstitute.hail.Utils._
+import org.broadinstitute.hail.annotations.Annotation
+import org.broadinstitute.hail.check.Gen
 
-case class Interval(contig: String, start: Int, end: Int, identifier: Option[Annotation] = None)
+import scala.math.max
 
-object IntervalList {
+case class GenomicIndex(contig: String, position: Int)
 
-  val intervalRegex = """([^:]*)[:\t](\d+)[\-\t](\d+)""".r
+case class GenomicInterval(contig: String, start: Int, end: Int) {
+  require(start <= end)
 
-  def apply(intervals: Traversable[Interval]): IntervalList = {
-    val m = mutable.Map[String, TreeMap[Int, (Int, Option[Annotation])]]()
-    intervals.foreach { case Interval(contig, start, end, identifier) =>
-      m.getOrElseUpdate(contig, new TreeMap[Int, (Int, Option[Annotation])]()).put(start, (end, identifier))
-    }
-    new IntervalList(m)
-  }
+  def interval: Interval = Interval(start, end)
+}
 
-  def read(filename: String,
-    hConf: hadoop.conf.Configuration): IntervalList = {
+case class Interval(start: Int, end: Int) {
+  require(start <= end)
 
-    readLines(filename, hConf) { s =>
-      IntervalList(
-        s.filter(line => !line.value.isEmpty && line.value(0) != '@')
-          .map(_.transform { l =>
-            l.value match {
-              case intervalRegex(contig, start_str, end_str) =>
-                Interval(contig, start_str.toInt, end_str.toInt)
-              case line => line.split("\t") match {
-                case Array(contig, start, end, direction, target) =>
-                  // FIXME proper input error handling
-                  assert(direction == "+" || direction == "-")
-                  Interval(contig, start.toInt, end.toInt)
+  def contains(position: Int): Boolean = position >= start && position <= end
+}
 
-                case _ => fatal("invalid interval format" +
-                  "\n  expected: `chr:start-end' or `chr  start  end  strand  target' (tab-separated) or `chr  start  end' (tab-separated)")
-              }
-            }
-          })
-          .toTraversable)
+object Interval {
+  def gen(min: Int, max: Int): Gen[Interval] = Gen.zip(Gen.choose(min, max), Gen.choose(min, max))
+    .filter { case (x, y) => x <= y }
+    .map { case (x, y) => Interval(x, y) }
+}
+
+case class GenomicIntervalSet(trees: Map[String, IntervalSet]) extends Serializable {
+
+  def contains(gi: GenomicIndex): Boolean = contains(gi.contig, gi.position)
+
+  def contains(contig: String, pos: Int): Boolean = trees
+    .get(contig)
+    .exists(_.contains(pos))
+
+  def query(contig: String, pos: Int): Set[GenomicInterval] = trees
+    .get(contig)
+    .map(_.query(pos).map(i => GenomicInterval(contig, i.start, i.end)))
+    .getOrElse(Set.empty[GenomicInterval])
+
+  def query(g: GenomicIndex): Set[GenomicInterval] = query(g.contig, g.position)
+
+  def write(filename: String, hConf: hadoop.conf.Configuration) {
+    writeTextFile(filename, hConf) { fw =>
+      for (g <- trees.flatMap { case (contig, m) => m.intervals.map(i => GenomicInterval(contig, i.start, i.end)) })
+        fw.write(g.contig + ":" + g.start + "-" + g.end + "\n")
     }
   }
 }
 
-class IntervalList(private val m: mutable.Map[String, TreeMap[Int, (Int, Option[Annotation])]]) extends Serializable {
-  def contains(p: (String, Int)): Boolean = {
-    val (contig, pos) = p
-    m.get(contig) match {
-      case Some(t) =>
-        val entry = t.floorEntry(pos)
-        if (entry != null)
-          pos <= entry.getValue()._1
-        else
-          false
-      case None => false
+object GenomicIntervalSet {
+
+  def apply(intervals: Set[GenomicInterval]): GenomicIntervalSet = {
+    GenomicIntervalSet(intervals.groupBy(_.contig)
+      .mapValues(_.map(_.interval).toArray)
+      .mapValues(IntervalSet(_))
+      .force)
+
+  }
+
+  val intervalRegex = """([^:]*)[:\t](\d+)[\-\t](\d+)""".r
+
+  def read(filename: String, hConf: hadoop.conf.Configuration): GenomicIntervalSet = {
+    readLines(filename, hConf) { s =>
+
+      val intervals = s
+        .filter(line => !line.value.isEmpty && line.value(0) != '@')
+        .map(_.transform { line => line.value match {
+          case intervalRegex(contig, start_str, end_str) => GenomicInterval(contig, start_str.toInt, end_str.toInt)
+          case str => str.split("\t") match {
+            case Array(contig, start, end, direction, _) =>
+              assert(direction == "+" || direction == "-")
+              GenomicInterval(contig, start.toInt, end.toInt)
+            case _ => fatal(
+              """invalid interval format.  Acceptable formats:
+                |  `chr:start-end'
+                |  `chr  start  end' (tab-separated)
+                |  `chr  start  end  strand  target' (tab-separated, strand is `+' or `-')
+              """.stripMargin)
+          }
+        }
+        })
+        .toSet
+
+      GenomicIntervalSet(intervals)
     }
   }
 
-  def query(p: (String, Int)): Option[Annotation] = {
-    val (contig, pos) = p
-    m.get(contig) match {
-      case Some(t) =>
-        val entry = t.floorEntry(pos)
-        if (entry != null)
-          if (pos <= entry.getValue._1)
-            entry.getValue._2
-          else
-            None
-        else
-          None
-      case None => None
+  def readWithMap(filename: String, hConf: hadoop.conf.Configuration): (GenomicIntervalSet, Map[GenomicInterval, Annotation]) = {
+    readLines(filename, hConf) { s =>
+
+      val m = s
+        .filter(line => !line.value.isEmpty && line.value(0) != '@')
+        .map(_.transform { line => line.value match {
+          case str => str.split("\t") match {
+            case Array(contig, start, end, direction, target) =>
+              assert(direction == "+" || direction == "-")
+              (GenomicInterval(contig, start.toInt, end.toInt), target)
+            case _ => fatal(
+              """invalid interval format.  Acceptable formats:
+                |  `chr:start-end'
+                |  `chr  start  end' (tab-separated)
+                |  `chr  start  end  strand  target' (tab-separated, strand is `+' or `-')
+              """.stripMargin)
+          }
+        }
+        })
+        .toMap
+        .force
+
+      (GenomicIntervalSet(m.keySet), m)
+    }
+  }
+}
+
+
+case class IntervalTreeNode(i: Interval,
+  left: Option[IntervalTreeNode],
+  right: Option[IntervalTreeNode],
+  maximum: Int) {
+
+  def contains(position: Int): Boolean = {
+    if (i.contains(position))
+      true
+    else if (position < i.start) {
+      //      println("going left")
+      left.exists(_.contains(position))
+    }
+    else {
+      val rt = if (position <= maximum)
+        right.exists(_.contains(position))
+      else false
+      val lft = if (left.exists(node => node.maximum >= position))
+        left.exists(_.contains(position))
+      else false
+      rt || lft
     }
   }
 
-  def write(filename: String, hConf: hadoop.conf.Configuration) {
-    writeTextFile(filename, hConf) { fw =>
-      for ((contig, t) <- m;
-           entry <- t.entrySet().asScala)
-        fw.write(contig + ":" + entry.getKey() + "-" + entry.getValue()._1 + "\n")
+  def query(position: Int): List[Interval] = {
+    val lft = if (left.exists(_.maximum >= position))
+      left.map(_.query(position)).getOrElse(Nil)
+    else Nil
+    val rt = if (position >= i.start && position <= maximum)
+      right.map(_.query(position)).getOrElse(Nil)
+    else Nil
+    val l = if (i.contains(position))
+      i :: lft ::: rt
+    else lft ::: rt
+    l
+  }
+
+  def intervals: List[Interval] = i :: left.map(_.intervals).getOrElse(Nil) ::: right.map(_.intervals).getOrElse(Nil)
+}
+
+case class IntervalSet(root: IntervalTreeNode) extends Serializable {
+  def contains(position: Int): Boolean = root.contains(position)
+
+  def query(position: Int): Set[Interval] = root.query(position).toSet
+
+  def intervals: Set[Interval] = root.intervals.toSet
+}
+
+object IntervalSet {
+  def apply(intervals: Array[Interval]): IntervalSet =
+    new IntervalSet(fromSorted(intervals.sortBy(_.start), 0, intervals.length - 1).get)
+
+  def fromSorted(intervals: Array[Interval], start: Int, end: Int): Option[IntervalTreeNode] = {
+    if (start > end)
+      None
+    else {
+      val mid = (start + end) / 2
+      val i = intervals(mid)
+      val lft = fromSorted(intervals, start, mid - 1)
+      val rt = fromSorted(intervals, mid + 1, end)
+      Some(IntervalTreeNode(i, lft, rt,
+        max(i.end, max(lft.map(_.maximum).getOrElse(-1), rt.map(_.maximum).getOrElse(-1)))))
     }
   }
 
-  override def equals(that: Any) = that match {
-    case ilist: IntervalList => m == ilist.m
-    case _ => false
+  def gen: Gen[IntervalSet] = {
+    Gen.buildableOf[Array[Interval], Interval](Interval.gen(0, 100))
+      .filter(_.nonEmpty)
+      .map(intervals => IntervalSet(intervals))
+  }
+
+  def genQueries(t: IntervalSet, n: Int = 100): Gen[Array[Int]] = {
+    val intervals = t.intervals
+    val maximum = intervals.map(_.end).max
+    val minimum = intervals.map(_.start).min
+    val range = (maximum - minimum) / 10
+    Gen.parameterized[Array[Int]] { p =>
+      val rng = p.rng
+      Gen.const((0 until n)
+        .map(x => rng.nextInt(minimum - range, maximum + range))
+        .toArray)
+    }
   }
 }
--- src/test/scala/org/broadinstitute/hail/variant/IntervalListSuite.scala
+++ src/test/scala/org/broadinstitute/hail/variant/IntervalListSuite.scala
@@ -2,14 +2,15 @@ package org.broadinstitute.hail.variant
 
 import org.testng.annotations.Test
 import org.broadinstitute.hail.SparkSuite
+import org.broadinstitute.hail.check.Prop
 
 class IntervalListSuite extends SparkSuite {
 
   @Test def test() {
-    val ilist = IntervalList(List(
-      Interval("1", 10, 20),
-      Interval("1", 30, 40),
-      Interval("2", 40, 50)))
+    val ilist = GenomicIntervalSet(Set(
+      GenomicInterval("1", 10, 20),
+      GenomicInterval("1", 30, 40),
+      GenomicInterval("2", 40, 50)))
 
     assert(!ilist.contains("1", 5))
     assert(ilist.contains("1", 10))
@@ -23,14 +24,82 @@ class IntervalListSuite extends SparkSuite {
 
     assert(!ilist.contains("3", 0))
 
-    val ex1 = IntervalList.read("src/test/resources/example1.interval_list", hadoopConf)
+    val ex1 = GenomicIntervalSet.read("src/test/resources/example1.interval_list", hadoopConf)
 
     val f = tmpDir.createTempFile("example", extension = ".interval_list")
     ex1.write(f, hadoopConf)
-    val ex1wr = IntervalList.read(f, hadoopConf)
+    val ex1wr = GenomicIntervalSet.read(f, hadoopConf)
     assert(ex1wr == ex1)
 
-    val ex2 = IntervalList.read("src/test/resources/example2.interval_list", hadoopConf)
+    val ex2 = GenomicIntervalSet.read("src/test/resources/example2.interval_list", hadoopConf)
     assert(ex1 == ex2)
+  }
+
+  @Test def testNew() {
+    val a = Set(
+      GenomicInterval("1", 10, 20),
+      GenomicInterval("1", 30, 40),
+      GenomicInterval("2", 40, 50))
+
+    val t = GenomicIntervalSet(a)
+
+    assert(t.contains("1", 15))
+    assert(t.contains("1", 30))
+    assert(t.contains("1", 40))
+    assert(t.contains("1", 20))
+    assert(!t.contains("1", 25))
+    assert(!t.contains("1", 9))
+    assert(!t.contains("5", 20))
+
+    // Test queries
+    val a2 = Set(
+      GenomicInterval("1", 10, 20),
+      GenomicInterval("1", 30, 40),
+      GenomicInterval("1", 30, 50),
+      GenomicInterval("1", 29, 31),
+      GenomicInterval("1", 29, 30),
+      GenomicInterval("2", 41, 50),
+      GenomicInterval("2", 43, 50),
+      GenomicInterval("2", 45, 70),
+      GenomicInterval("2", 42, 43),
+      GenomicInterval("2", 1, 10))
+
+    val t2 = GenomicIntervalSet(a2)
+
+    assert(t2.query("1", 30).toSet == Set(
+      GenomicInterval("1", 30, 40), GenomicInterval("1", 30, 50),
+      GenomicInterval("1", 29, 31), GenomicInterval("1", 29, 30)))
+
+  }
+
+  @Test def random() {
+
+    val g = for (t <- IntervalSet.gen;
+                 queries <- IntervalSet.genQueries(t)) yield (t, queries)
+
+    val p = Prop.forAll(g) { case (t, queries) =>
+      val intervals = t.intervals
+      queries.forall { i =>
+        val inSet = intervals.exists(_.contains(i))
+        val inTree = t.contains(i)
+        if (inSet != inTree)
+          println(
+            s"""contains results not the same for position [ $i ]
+                |  from set:  $inSet
+                |  from tree: $inTree
+             """.stripMargin)
+
+        val setQueries = intervals.filter(_.contains(i))
+        val treeQueries = t.query(i)
+        if (treeQueries != setQueries)
+          println(
+            s"""query results not the same for query position [ $i ]
+                |  from set:  $setQueries
+                |  from tree: $treeQueries
+             """.stripMargin)
+        (inSet == inTree) && (treeQueries == setQueries)
+      }
+    }
+    p.check()
   }
 }
