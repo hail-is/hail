2ad6ed4b2fe055693934b916592a50ea8de859b9
Cotton Seed <cseed@broadinstitute.org>
2016-09-14 18:05:09 -0400

    Fix sortedParquetRead on Dataproc.
    Reorder partitions based on ParquetInputSplit path.

--- src/main/scala/org/broadinstitute/hail/utils/richUtils/RichSQLContext.scala
+++ src/main/scala/org/broadinstitute/hail/utils/richUtils/RichSQLContext.scala
@@ -1,11 +1,15 @@
 package org.broadinstitute.hail.utils.richUtils
 
-import org.apache.hadoop.fs.PathIOException
-import org.apache.spark.sql.{DataFrame, SQLContext}
+import java.lang.reflect.Method
+
+import org.apache.hadoop.fs.{Path, PathIOException}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, SQLContext, SparkExport}
+import org.apache.spark.sql.functions._
 import org.broadinstitute.hail.utils._
 
 class RichSQLContext(val sqlContext: SQLContext) extends AnyVal {
-  def sortedParquetRead(dirname: String): DataFrame = {
+  def readParquetSorted(dirname: String, selection: Option[Array[String]] = None): RDD[Row] = {
     val partRegex = ".*/?part-r-(\\d+)-.*\\.parquet.*".r
     def getPartNumber(fname: String): Int = {
       fname match {
@@ -14,10 +18,42 @@ class RichSQLContext(val sqlContext: SQLContext) extends AnyVal {
       }
     }
 
-    val parquetFiles = sqlContext.sparkContext.hadoopConfiguration.globAll(Array(s"""$dirname/*.parquet"""))
-      .sortBy(getPartNumber)
+    val parquetFiles = sqlContext.sparkContext.hadoopConfiguration.globAll(Array(dirname + "/*.parquet"))
+    if (parquetFiles.isEmpty)
+      return sqlContext.sparkContext.emptyRDD[Row]
 
-    sqlContext.read.parquet(parquetFiles: _*)
+    var df = sqlContext.read.parquet(dirname + "/part-r-*")
+    selection.foreach { cols =>
+      df = df.select(cols.map(col): _*)
+    }
+
+    val rdd = df.rdd
+
+    val oldIndices = rdd.partitions
+      .map { p =>
+        val parquetInputSplit = SparkExport.sqlNewHadoopPartitionRawSplit(p)
+
+        /*
+         * Use reflection to invoke getPath instead of importing ParquetInputSplit and casting since the parquet
+         * package (parquet vs org.apache.parquet) moved and depends on the distribution we are running against.
+         */
+        def f(c: Class[_], method: String): Method = {
+          try {
+            c.getDeclaredMethod(method)
+          } catch {
+            case _: Exception =>
+              assert(c != classOf[java.lang.Object])
+              f(c.getSuperclass, method)
+          }
+        }
+        val m = f(parquetInputSplit.getClass, "getPath")
+        val path = m.invoke(parquetInputSplit).asInstanceOf[Path]
+        getPartNumber(path.getName)
+      }
+      .zipWithIndex
+      .sortBy(_._1)
+      .map(_._2)
+
+    rdd.reorderPartitions(oldIndices)
   }
 }
-
