7f37ce8aacb91537dcea3b45d1abc18c704684ab
Daniel King <daniel.zidan.king@gmail.com>
2016-09-30 14:07:46 -0400

    concatenate IBD output (#873)

--- src/main/scala/org/broadinstitute/hail/driver/IBDCommand.scala
+++ src/main/scala/org/broadinstitute/hail/driver/IBDCommand.scala
@@ -78,7 +78,7 @@ object IBDCommand extends Command {
       .map { case ((i, j), ibd) =>
         s"$i\t$j\t${ ibd.ibd.Z0 }\t${ ibd.ibd.Z1 }\t${ ibd.ibd.Z2 }\t${ ibd.ibd.PI_HAT }"
       }
-      .saveAsTextFile(options.outputFile)
+      .writeTable(options.outputFile, Some("SAMPLE_ID_1\tSAMPLE_ID_2\tZ0\tZ1\tZ2\tPI_HAT"))
 
     state
   }
