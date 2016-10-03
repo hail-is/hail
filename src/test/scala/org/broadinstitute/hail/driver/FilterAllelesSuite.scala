b74707430564364263fc30e3555812a1870a8875
lfrancioli <lfran@broadinstitute.org>
2016-10-03 14:40:30 -0400

    Added subset and filterAlteredGenotypes options to filteralleles (#886)

--- src/test/scala/org/broadinstitute/hail/driver/FilterAllelesSuite.scala
+++ src/test/scala/org/broadinstitute/hail/driver/FilterAllelesSuite.scala
@@ -16,7 +16,7 @@ class FilterAllelesSuite extends SparkSuite {
   @Test def filterAllAlleles(): Unit = {
     Prop.forAll(VSMSubgen.random.gen(sc)) { vds =>
       val s = FilterAlleles.run(State(sc, sqlContext, vds),
-        Array("--keep", "-c", "false", "-a", noop))
+        Array("--keep", "--subset", "-c", "false", "-a", noop))
       s.vds.nVariants == 0
     }.check()
   }
@@ -24,12 +24,12 @@ class FilterAllelesSuite extends SparkSuite {
   @Test def filterNoAlleles(): Unit = {
     Prop.forAll(VSMSubgen.random.gen(sc)) { vds =>
       val s = FilterAlleles.run(State(sc, sqlContext, vds),
-        Array("--keep", "-c", "true", "-a", noop))
+        Array("--keep", "--subset", "-c", "true", "-a", noop))
       s.vds.nVariants == vds.nVariants
     }.check()
   }
 
-  @Test def filterSecondOfTwoAlleles(): Unit = {
+  @Test def filterSecondOfTwoAllelesDowncode(): Unit = {
     val variant1 = new Variant("contig",0,"ref",IndexedSeq("alt1","alt2").map(AltAllele("ref",_)))
     val va1 = null
     val genotype11 = Genotype(Option(1), Option(Array(25,5,30)), Option(100), Option(5), Option(Array(10,0,10,10,5,7)))
@@ -47,7 +47,7 @@ class FilterAllelesSuite extends SparkSuite {
       sc.parallelize(Seq(row1)).toOrderedRDD)
 
     val s = FilterAlleles.run(State(sc, sqlContext, vds),
-      Array("--remove", "-c", "aIndex == 2", "-a", noop))
+      Array("--remove", "--downcode", "-c", "aIndex == 2", "-a", noop))
 
     val newVariant1 = variant1.copy(altAlleles = variant1.altAlleles.take(1))
     val newVa1 = va1
@@ -59,7 +59,7 @@ class FilterAllelesSuite extends SparkSuite {
     s.vds.rdd.collect().toSeq shouldEqual Seq(newRow1)
   }
 
-  @Test def filterFirstOfTwoAlleles(): Unit = {
+  @Test def filterFirstOfTwoAllelesDowncode(): Unit = {
     val variant1 = new Variant("contig", 0, "ref", IndexedSeq("alt1", "alt2").map(AltAllele("ref", _)))
     val va1 = null
     val genotype11 = Genotype(Option(1), Option(Array(25, 5, 30)), Option(100), Option(5), Option(Array(10, 0, 10, 10, 5, 7)))
@@ -77,13 +77,103 @@ class FilterAllelesSuite extends SparkSuite {
       sc.parallelize(Seq(row1)).toOrderedRDD)
 
     val s = FilterAlleles.run(State(sc, sqlContext, vds),
-      Array("--remove", "-c", "aIndex == 1", "-a", noop))
+      Array("--remove", "--downcode", "-c", "aIndex == 1", "-a", noop))
 
     val newVariant1 = variant1.copy(altAlleles = variant1.altAlleles.drop(1))
     val newVa1 = va1
     val newGenotype11 = genotype11.copy(gt = Option(0), ad = Option(Array(30, 30)), px = Option(Array(0, 5, 7)))
     val newGenotype12 = genotype12.copy(gt = Option(0), ad = Option(Array(60, 0)), px = Option(Array(0, 5, 10)))
     val newGenotype13 = genotype13.copy(gt = Option(1), ad = Option(Array(35, 10)), gq = Option(7), px = Option(Array(10, 0, 7)))
+    val newGenotypes1 = Seq(newGenotype11, newGenotype12, newGenotype13)
+    val newRow1 = (newVariant1, (newVa1, newGenotypes1))
+    s.vds.rdd.collect().toSeq shouldEqual Seq(newRow1)
+  }
+
+  @Test def filterFirstOfTwoAllelesSubset(): Unit = {
+    val variant1 = new Variant("contig", 0, "ref", IndexedSeq("alt1", "alt2").map(AltAllele("ref", _)))
+    val va1 = null
+    val genotype11 = Genotype(Option(1), Option(Array(25, 5, 30)), Option(100), Option(5), Option(Array(10, 0, 10, 10, 5, 7)))
+    val genotype12 = Genotype(Option(2), Option(Array(25, 35, 0)), Option(100), Option(5), Option(Array(10, 10, 0, 7, 5, 10)))
+    val genotype13 = Genotype(Option(3), Option(Array(25, 10, 10)), Option(100), Option(5), Option(Array(10, 10, 10, 0, 5, 7)))
+    val genotypes1 = Seq(genotype11, genotype12, genotype13)
+    val row1: (Variant, (Annotation, Iterable[Genotype])) = (variant1, (va1, genotypes1))
+
+    val vds = VariantSampleMatrix(VariantMetadata(Array("1", "2", "3"),
+      IndexedSeq[Annotation](null, null, null),
+      null,
+      TChar,
+      TStruct.empty,
+      TChar),
+      sc.parallelize(Seq(row1)).toOrderedRDD)
+
+    val s = FilterAlleles.run(State(sc, sqlContext, vds),
+      Array("--remove","--subset", "-c", "aIndex == 1", "-a", noop))
+
+    val newVariant1 = variant1.copy(altAlleles = variant1.altAlleles.drop(1))
+    val newVa1 = va1
+    val newGenotype11 = genotype11.copy(gt = Option(2), ad = Option(Array(25, 30)), gq = Option(3), px = Option(Array(3, 3, 0)))
+    val newGenotype12 = genotype12.copy(gt = Option(1), ad = Option(Array(25, 0)), gq = Option(3), px = Option(Array(3, 0, 3)))
+    val newGenotype13 = genotype13.copy(gt = Option(1), ad = Option(Array(25, 10)), gq = Option(7), px = Option(Array(10, 0, 7)))
+    val newGenotypes1 = Seq(newGenotype11, newGenotype12, newGenotype13)
+    val newRow1 = (newVariant1, (newVa1, newGenotypes1))
+    s.vds.rdd.collect().toSeq shouldEqual Seq(newRow1)
+  }
+
+  @Test def filterSecondOfTwoAllelesSubset(): Unit = {
+    val variant1 = new Variant("contig",0,"ref",IndexedSeq("alt1","alt2").map(AltAllele("ref",_)))
+    val va1 = null
+    val genotype11 = Genotype(Option(1), Option(Array(25,5,30)), Option(100), Option(5), Option(Array(10,0,10,10,5,7)))
+    val genotype12 = Genotype(Option(2), Option(Array(25,35,0)), Option(100), Option(5), Option(Array(10,10,0,7,5,10)))
+    val genotype13 = Genotype(Option(3), Option(Array(25,10,10)), Option(100), Option(5), Option(Array(10,10,10,0,5,7)))
+    val genotypes1 = Seq(genotype11, genotype12, genotype13)
+    val row1: (Variant, (Annotation, Iterable[Genotype])) = (variant1, (va1, genotypes1))
+
+    val vds = VariantSampleMatrix(VariantMetadata(Array("1","2","3"),
+      IndexedSeq[Annotation](null,null,null),
+      null,
+      TChar,
+      TStruct.empty,
+      TChar),
+      sc.parallelize(Seq(row1)).toOrderedRDD)
+
+    val s = FilterAlleles.run(State(sc, sqlContext, vds),
+      Array("--remove","--subset", "-c", "aIndex == 2", "-a", noop))
+
+    val newVariant1 = variant1.copy(altAlleles = variant1.altAlleles.take(1))
+    val newVa1 = va1
+    val newGenotype11 = genotype11.copy(gt = Option(1), ad = Option(Array(25,5)), gq = Option(10), px = Option(Array(10,0,10)))
+    val newGenotype12 = genotype12.copy(gt = Option(2), ad = Option(Array(25,35)), gq = Option(10), px = Option(Array(10,10,0)))
+    val newGenotype13 = genotype13.copy(gt = Option(0), ad = Option(Array(25,10)), gq = Option(0), px = Option(Array(0,0,0)))
+    val newGenotypes1 = Seq(newGenotype11, newGenotype12, newGenotype13)
+    val newRow1 = (newVariant1, (newVa1, newGenotypes1))
+    s.vds.rdd.collect().toSeq shouldEqual Seq(newRow1)
+  }
+
+  @Test def filterSecondOfTwoAllelesFilterAlteredGenotypes(): Unit = {
+    val variant1 = new Variant("contig",0,"ref",IndexedSeq("alt1","alt2").map(AltAllele("ref",_)))
+    val va1 = null
+    val genotype11 = Genotype(Option(1), Option(Array(25,5,30)), Option(100), Option(5), Option(Array(10,0,10,10,5,7)))
+    val genotype12 = Genotype(Option(2), Option(Array(25,35,0)), Option(100), Option(5), Option(Array(10,10,0,7,5,10)))
+    val genotype13 = Genotype(Option(3), Option(Array(25,10,10)), Option(100), Option(5), Option(Array(10,10,10,0,5,7)))
+    val genotypes1 = Seq(genotype11, genotype12, genotype13)
+    val row1: (Variant, (Annotation, Iterable[Genotype])) = (variant1, (va1, genotypes1))
+
+    val vds = VariantSampleMatrix(VariantMetadata(Array("1","2","3"),
+      IndexedSeq[Annotation](null,null,null),
+      null,
+      TChar,
+      TStruct.empty,
+      TChar),
+      sc.parallelize(Seq(row1)).toOrderedRDD)
+
+    val s = FilterAlleles.run(State(sc, sqlContext, vds),
+      Array("--remove","--subset", "--filterAlteredGenotypes", "-c", "aIndex == 2", "-a", noop))
+
+    val newVariant1 = variant1.copy(altAlleles = variant1.altAlleles.take(1))
+    val newVa1 = va1
+    val newGenotype11 = genotype11.copy(gt = Option(1), ad = Option(Array(25,5)), gq = Option(10), px = Option(Array(10,0,10)))
+    val newGenotype12 = genotype12.copy(gt = Option(2), ad = Option(Array(25,35)), gq = Option(10), px = Option(Array(10,10,0)))
+    val newGenotype13 = genotype13.copy(gt = None, ad = Option(Array(25,10)), gq = Option(0), px = Option(Array(0,0,0)))
     val newGenotypes1 = Seq(newGenotype11, newGenotype12, newGenotype13)
     val newRow1 = (newVariant1, (newVa1, newGenotypes1))
     s.vds.rdd.collect().toSeq shouldEqual Seq(newRow1)
@@ -104,7 +194,7 @@ class FilterAllelesSuite extends SparkSuite {
       sc.parallelize(Seq(row1)).toOrderedRDD)
 
     val s = FilterAlleles.run(State(sc, sqlContext, vds),
-      Array("--remove", "-c", "aIndex == 2", "-a", "va = aIndices"))
+      Array("--remove", "--downcode", "-c", "aIndex == 2", "-a", "va = aIndices"))
 
     val newVariant1 = variant1.copy(altAlleles = variant1.altAlleles.take(1))
     val newVa1: IndexedSeq[Int] = Array(0,1)
