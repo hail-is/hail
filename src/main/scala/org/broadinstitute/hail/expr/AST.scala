e6ab9ec2116ecdda82b259244b1ebf45b80ae277
Daniel King <daniel.zidan.king@gmail.com>
2016-10-19 13:04:01 -0700

    resolves issue #933 (#934)
    * resolves issue #933
    
    Provide a hint to the user when a field or
    method is not found.
    
    * Update AST.scala

--- src/main/scala/org/broadinstitute/hail/expr/AST.scala
+++ src/main/scala/org/broadinstitute/hail/expr/AST.scala
@@ -309,7 +309,9 @@ case class Select(posn: Position, lhs: AST, rhs: String) extends AST(posn, lhs) 
       AST.evalCompose[IndexedSeq[_]](ec, lhs)(_.tail)
 
     case (t, name) => FunctionRegistry.lookupField(ec)(t, name)(lhs)
-      .getOrElse(fatal(s"No such method `$name' on object of type `$t'"))
+      .getOrElse(fatal(s"""`$t' has neither a field nor a method named `$name'
+                          |  Hint: sum, min, max, etc. have no parentheses when called on an Array:
+                          |    counts.sum""".stripMargin))
   }
 }
 
