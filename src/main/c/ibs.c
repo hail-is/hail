7881bbe2d638593920bcb733440fab2d554bb9b1
Daniel King <dking@broadinstitute.org>
2016-11-09 17:05:54 -0500

    fixes

--- src/main/c/ibs.c
+++ src/main/c/ibs.c
@@ -4,21 +4,32 @@
 #include <popcntintrin.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <inttypes.h>
+
+#define EXPORT __attribute__((visibility("default")))
+
+#define fmt256 "%016llx  %016llx  %016llx  %016llx"
+// XXX: only call with identifiers
+#define splat256(x) _mm256_extract_epi64((x), 0), _mm256_extract_epi64((x), 1), _mm256_extract_epi64((x), 2), _mm256_extract_epi64((x), 3)
 
 __m256i allones = { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF };
 __m256i rightAllele = { 0x5555555555555555, 0x5555555555555555, 0x5555555555555555, 0x5555555555555555 };
+__m256i allNA = { 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA };
 
 void ibs256(uint64_t* result, __m256i x, __m256i y) {
   __m256i nxor = _mm256_xor_si256(_mm256_xor_si256(x, y), allones);
+
   // if both bits are one, then the genotype is missing
-  __m256i xna = _mm256_and_si256(_mm256_srli_si256(x, 1), x);
-  __m256i yna = _mm256_and_si256(_mm256_srli_si256(y, 1), y);
+  __m256i xna_tmp = _mm256_xor_si256(_mm256_xor_si256(allNA, x), allones);
+  __m256i xna = _mm256_and_si256(_mm256_srli_epi64(xna_tmp, 1), xna_tmp);
+  __m256i yna_tmp = _mm256_xor_si256(_mm256_xor_si256(allNA, y), allones);
+  __m256i yna = _mm256_and_si256(_mm256_srli_epi64(yna_tmp, 1), yna_tmp);
   // if either sample is missing a genotype, we ignore that genotype pair
-  __m256i na = _mm256_or_si256(xna, yna);
+  __m256i na = _mm256_and_si256(_mm256_or_si256(xna, yna), rightAllele);
   // 1. shift the left alleles over the right ones
   // 2. and the alleles
   // 3. mask to the right ones
-  __m256i ibs2 = _mm256_and_si256(_mm256_and_si256(_mm256_and_si256(_mm256_srli_si256(nxor, 1), nxor), rightAllele), na);
+  __m256i ibs2 = _mm256_andnot_si256(na, _mm256_and_si256(_mm256_and_si256(_mm256_srli_epi64(nxor, 1), nxor), rightAllele));
   // 4. popcnt
   uint64_t ibs2sum = _mm_popcnt_u64(_mm256_extract_epi64(ibs2, 0));
   ibs2sum += _mm_popcnt_u64(_mm256_extract_epi64(ibs2, 1));
@@ -28,7 +39,7 @@ void ibs256(uint64_t* result, __m256i x, __m256i y) {
   // 1. shift the left alleles over the right ones
   // 2. or the alleles
   // 3. mask to the right ones
-  __m256i ibs1 = _mm256_and_si256(_mm256_and_si256(_mm256_xor_si256(_mm256_srli_si256(nxor, 1), nxor), rightAllele), na);
+  __m256i ibs1 = _mm256_andnot_si256(na, _mm256_and_si256(_mm256_xor_si256(_mm256_srli_epi64(nxor, 1), nxor), rightAllele));
   // 4. popcnt
   uint64_t ibs1sum = _mm_popcnt_u64(_mm256_extract_epi64(ibs1, 0));
   ibs1sum += _mm_popcnt_u64(_mm256_extract_epi64(ibs1, 1));
@@ -39,7 +50,7 @@ void ibs256(uint64_t* result, __m256i x, __m256i y) {
   // 2. or the alleles
   // 3. negate the alleles
   // 4. mask to the right ones
-  __m256i ibs0 = _mm256_and_si256(_mm256_andnot_si256(rightAllele, _mm256_or_si256(_mm256_srli_si256(nxor, 1), nxor)), na);
+  __m256i ibs0 = _mm256_andnot_si256(na, _mm256_andnot_si256(_mm256_or_si256(_mm256_srli_epi64(nxor, 1), nxor), rightAllele));
   // 5. popcnt
   uint64_t ibs0sum = _mm_popcnt_u64(_mm256_extract_epi64(ibs0, 0));
   ibs0sum += _mm_popcnt_u64(_mm256_extract_epi64(ibs0, 1));
@@ -61,7 +72,12 @@ uint8_t bytepopcnt(uint8_t x) {
 
 void ibsVec(uint64_t* result, uint64_t length, uint64_t* x, uint64_t* y) {
   uint64_t i;
+  /* printf("x 0x%" PRIXPTR "\n", (uintptr_t)x); */
+  /* printf("y 0x%" PRIXPTR "\n", (uintptr_t)y); */
   for (i = 0; i < (length - 3); i += 4) {
+    /* printf("i %llu\n", i); */
+    /* printf("x %llx  %llx  %llx  %llx\n", x[i], x[i+1], x[i+2], x[i+3]); */
+    /* printf("y %llx  %llx  %llx  %llx\n", y[i], y[i+1], y[i+2], y[i+3]); */
     __m256i x256 = _mm256_set_epi64x(x[i], x[i+1], x[i+2], x[i+3]);
     __m256i y256 = _mm256_set_epi64x(y[i], y[i+1], y[i+2], y[i+3]);
     ibs256(result, x256, y256);
@@ -81,39 +97,211 @@ void ibsVec(uint64_t* result, uint64_t length, uint64_t* x, uint64_t* y) {
 }
 
 // samples in rows, genotypes in columns
+EXPORT
 void ibsMat(uint64_t* result, uint64_t nSamples, uint64_t nGenotypePacks, uint64_t* genotypes1, uint64_t* genotypes2) {
+  /* printf("genotypes1 0x%" PRIXPTR "\n", (uintptr_t)genotypes1); */
+  /* printf("genotypes2 0x%" PRIXPTR "\n", (uintptr_t)genotypes2); */
+  /* printf("sizeof(uint64_t) %lu\n", sizeof(uint64_t)); */
   for (uint64_t si = 0; si != nSamples; ++si) {
+    /* printf("si %llu\n", si); */
     for (uint64_t sj = 0; sj != nSamples; ++sj) {
-      ibsVec(result + si*nSamples*3*sizeof(uint64_t) + sj*3*sizeof(uint64_t),
+      /* printf("**** si,sj %llu, %llu\n", si, sj); */
+      ibsVec(result + si*nSamples*3 + sj*3,
              nGenotypePacks,
-             genotypes1 + si*nGenotypePacks*sizeof(uint64_t),
-             genotypes2 + sj*nGenotypePacks*sizeof(uint64_t));
+             genotypes1 + si*nGenotypePacks,
+             genotypes2 + sj*nGenotypePacks);
     }
   }
 }
 
+#define expect(name, x) if (!(x)) { ++failures; printf(name ": expected " #x " to be true, but was false\n\n"); } else { ++successes; }
+#define expect_equal(name, fmt, x, y) if ((x) != (y)) { ++failures; printf(name ": expected " #x " to equal " #y ", but actually got " fmt " and " fmt"\n\n", x, y); } else { ++successes; }
+
 int main(int argc, char** argv) {
-  if (argc != 2) {
+  if (argc != 1) {
+    printf("Expected zero arguments.\n");
     return -1;
   }
-  int itr = atoi(argv[1]);
-  uint64_t arr[3] = { 0, 0, 0 };
 
-  for (int i = 0; i < itr; ++i ) {
-    uint64_t x0 = 0; //(((uint64_t)rand()) << 32) | rand();
-    uint64_t x1 = 0;//(((uint64_t)rand()) << 32) | rand();
-    uint64_t x2 = 0; //(((uint64_t)rand()) << 32) | rand();
-    uint64_t x3 = 0;//(((uint64_t)rand()) << 32) | rand();
-    uint64_t y0 = 0;//(((uint64_t)rand()) << 32) | rand();
-    uint64_t y1 = 0;//(((uint64_t)rand()) << 32) | rand();
-    uint64_t y2 = 0;//(((uint64_t)rand()) << 32) | rand();
-    uint64_t y3 = 0;//(((uint64_t)rand()) << 32) | rand();
-    __m256i x = _mm256_set_epi64x(x0, x1, x2, x3);
-    __m256i y = _mm256_set_epi64x(y0, y1, y2, y3);
-    ibs256(arr, x, y);
+  uint64_t failures = 0;
+  uint64_t successes = 0;
+
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i allNA1 = { 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA };
+    __m256i allNA2 = { 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA };
+    ibs256(result, allNA1, allNA2);
+
+    expect_equal("allNA", "%llu", result[0], 0ULL);
+    expect_equal("allNA", "%llu", result[1], 0ULL);
+    expect_equal("allNA", "%llu", result[2], 0ULL);
+  }
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i allHomRef1 = { 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000 };
+    __m256i allHomRef2 = { 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000 };
+    ibs256(result, allHomRef1, allHomRef2);
+
+    expect_equal("allHomRef", "%llu", result[0], 0ULL);
+    expect_equal("allHomRef", "%llu", result[1], 0ULL);
+    expect_equal("allHomRef", "%llu", result[2], 128ULL);
+  }
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i allHet1 = { 0x5555555555555555, 0x5555555555555555, 0x5555555555555555, 0x5555555555555555 };
+    __m256i allHet2 = { 0x5555555555555555, 0x5555555555555555, 0x5555555555555555, 0x5555555555555555 };
+    ibs256(result, allHet1, allHet2);
+
+    expect_equal("allHet", "%llu", result[0], 0ULL);
+    expect_equal("allHet", "%llu", result[1], 0ULL);
+    expect_equal("allHet", "%llu", result[2], 128ULL);
+  }
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i allHomAlt1 = { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF };
+    __m256i allHomAlt2 = { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF };
+    ibs256(result, allHomAlt1, allHomAlt2);
+
+    expect_equal("allHomAlt", "%llu", result[0], 0ULL);
+    expect_equal("allHomAlt", "%llu", result[1], 0ULL);
+    expect_equal("allHomAlt", "%llu", result[2], 128ULL);
+  }
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i allHomAlt = { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF };
+    __m256i allHomRef = { 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000 };
+    ibs256(result, allHomAlt, allHomRef);
+
+    expect_equal("homAlt v homRef", "%llu", result[0], 128ULL);
+    expect_equal("homAlt v homRef", "%llu", result[1], 0ULL);
+    expect_equal("homAlt v homRef", "%llu", result[2], 0ULL);
+  }
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i allHet = { 0x5555555555555555, 0x5555555555555555, 0x5555555555555555, 0x5555555555555555 };
+    __m256i allHomRef = { 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000 };
+    ibs256(result, allHet, allHomRef);
+
+    expect_equal("het v homRef", "%llu", result[0], 0ULL);
+    expect_equal("het v homRef", "%llu", result[1], 128ULL);
+    expect_equal("het v homRef", "%llu", result[2], 0ULL);
+  }
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i allHet = { 0x5555555555555555, 0x5555555555555555, 0x5555555555555555, 0x5555555555555555 };
+    __m256i allHomAlt = { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF };
+    ibs256(result, allHet, allHomAlt);
+
+    expect_equal("het v homAlt", "%llu", result[0], 0ULL);
+    expect_equal("het v homAlt", "%llu", result[1], 128ULL);
+    expect_equal("het v homAlt", "%llu", result[2], 0ULL);
+  }
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i allHet = { 0x5555555555555555, 0x5555555555555555, 0x5555555555555555, 0x5555555555555555 };
+    __m256i allHomAltOneNA = { 0xBFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF };
+    ibs256(result, allHet, allHomAltOneNA);
+
+    expect_equal("het v homAltOneNA", "%llu", result[0], 0ULL);
+    expect_equal("het v homAltOneNA", "%llu", result[1], 127ULL);
+    expect_equal("het v homAltOneNA", "%llu", result[2], 0ULL);
+  }
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i allHet = { 0x5555555555555555, 0x5555555555555555, 0x5555555555555555, 0x5555555555555555 };
+    __m256i allHomAltTwoNA = { 0xAFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF };
+    ibs256(result, allHet, allHomAltTwoNA);
+
+    expect_equal("het v homAltTwoNA", "%llu", result[0], 0ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[1], 126ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[2], 0ULL);
+  }
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i ref_het_alt_het = { 0x1DAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA };
+    __m256i het_ref_het_alt = { 0x47AAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA };
+    ibs256(result, ref_het_alt_het, het_ref_het_alt);
+
+    expect_equal("het v homAltTwoNA", "%llu", result[0], 0ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[1], 4ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[2], 0ULL);
+  }
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i ref_het_alt_het = { 0xAAAAAAAAAAAAAAAA, 0x1DAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA };
+    __m256i het_ref_het_alt = { 0xAAAAAAAAAAAAAAAA, 0x47AAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA };
+    ibs256(result, ref_het_alt_het, het_ref_het_alt);
+
+    expect_equal("het v homAltTwoNA", "%llu", result[0], 0ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[1], 4ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[2], 0ULL);
+  }
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i ref_het_alt_het = { 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0x1DAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA };
+    __m256i het_ref_het_alt = { 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0x47AAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA };
+    ibs256(result, ref_het_alt_het, het_ref_het_alt);
+
+    expect_equal("het v homAltTwoNA", "%llu", result[0], 0ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[1], 4ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[2], 0ULL);
+  }
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i ref_het_alt_het = { 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0x1DAAAAAAAAAAAAAA };
+    __m256i het_ref_het_alt = { 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0x47AAAAAAAAAAAAAA };
+    ibs256(result, ref_het_alt_het, het_ref_het_alt);
+
+    expect_equal("het v homAltTwoNA", "%llu", result[0], 0ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[1], 4ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[2], 0ULL);
+  }
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i ref_het_alt_het = { 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAA1D };
+    __m256i het_ref_het_alt = { 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAA47 };
+    ibs256(result, ref_het_alt_het, het_ref_het_alt);
+
+    expect_equal("het v homAltTwoNA", "%llu", result[0], 0ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[1], 4ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[2], 0ULL);
+  }
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i ref_het_alt_het = { 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAA1D, 0xAAAAAAAAAAAAAAAA };
+    __m256i het_ref_het_alt = { 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAA47, 0xAAAAAAAAAAAAAAAA };
+    ibs256(result, ref_het_alt_het, het_ref_het_alt);
+
+    expect_equal("het v homAltTwoNA", "%llu", result[0], 0ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[1], 4ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[2], 0ULL);
+  }
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i ref_het_alt_het = { 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAA1D, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA };
+    __m256i het_ref_het_alt = { 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAA47, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA };
+    ibs256(result, ref_het_alt_het, het_ref_het_alt);
+
+    expect_equal("het v homAltTwoNA", "%llu", result[0], 0ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[1], 4ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[2], 0ULL);
+  }
+  {
+    uint64_t result[3] = { 0, 0, 0 };
+    __m256i ref_het_alt_het = { 0xAAAAAAAAAAAAAA1D, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA };
+    __m256i het_ref_het_alt = { 0xAAAAAAAAAAAAAA47, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA };
+    ibs256(result, ref_het_alt_het, het_ref_het_alt);
+
+    expect_equal("het v homAltTwoNA", "%llu", result[0], 0ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[1], 4ULL);
+    expect_equal("het v homAltTwoNA", "%llu", result[2], 0ULL);
   }
 
-  printf("%llu", arr[0]);
+  if (failures != 0) {
+    printf("%llu test(s) failed.\n", failures);
+  } else {
+    printf("%llu test(s) succeeded.\n", successes);
+  }
 
   return 0;
 }
--- src/main/scala/org/broadinstitute/hail/methods/IBSFFI.scala
+++ src/main/scala/org/broadinstitute/hail/methods/IBSFFI.scala
@@ -19,21 +19,21 @@ object IBSFFI {
     val length = packedLength + (if (nGenotypes % 64 != 0) 1 else 0)
 
     def pack(gs: Array[Byte]): Array[Long] = {
-      val sampleOrientedGenotypes = new Array[Long](nSamples * packedLength)
+      val sampleOrientedGenotypes = new Array[Long](nSamples * length)
       var si = 0
       while (si != nSamples) {
         var vBlocki = 0
         while (vBlocki != packedLength) {
           val k = si + vBlocki*64*nSamples
           sampleOrientedGenotypes(si * length + vBlocki) =
-              gs(k)                 << 62 | gs(k + 1 * nSamples)  << 60 | gs(k + 2 * nSamples)  << 58 | gs(k + 3 * nSamples)  << 56 |
-              gs(k + 4 * nSamples)  << 54 | gs(k + 5 * nSamples)  << 52 | gs(k + 6 * nSamples)  << 50 | gs(k + 7 * nSamples)  << 48 |
-              gs(k + 8 * nSamples)  << 46 | gs(k + 9 * nSamples)  << 44 | gs(k + 10 * nSamples) << 42 | gs(k + 11 * nSamples) << 40 |
-              gs(k + 12 * nSamples) << 38 | gs(k + 13 * nSamples) << 36 | gs(k + 14 * nSamples) << 34 | gs(k + 15 * nSamples) << 32 |
-              gs(k + 16 * nSamples) << 30 | gs(k + 17 * nSamples) << 28 | gs(k + 18 * nSamples) << 26 | gs(k + 19 * nSamples) << 24 |
-              gs(k + 20 * nSamples) << 22 | gs(k + 21 * nSamples) << 20 | gs(k + 22 * nSamples) << 18 | gs(k + 23 * nSamples) << 16 |
-              gs(k + 24 * nSamples) << 14 | gs(k + 25 * nSamples) << 12 | gs(k + 26 * nSamples) << 10 | gs(k + 27 * nSamples) << 8  |
-              gs(k + 28 * nSamples) << 6  | gs(k + 29 * nSamples) << 4  | gs(k + 30 * nSamples) << 2  | gs(k + 31 * nSamples)
+              gs(k).toLong                 << 62 | gs(k + 1 * nSamples).toLong  << 60 | gs(k + 2 * nSamples).toLong  << 58 | gs(k + 3 * nSamples).toLong  << 56 |
+              gs(k + 4 * nSamples).toLong  << 54 | gs(k + 5 * nSamples).toLong  << 52 | gs(k + 6 * nSamples).toLong  << 50 | gs(k + 7 * nSamples).toLong  << 48 |
+              gs(k + 8 * nSamples).toLong  << 46 | gs(k + 9 * nSamples).toLong  << 44 | gs(k + 10 * nSamples).toLong << 42 | gs(k + 11 * nSamples).toLong << 40 |
+              gs(k + 12 * nSamples).toLong << 38 | gs(k + 13 * nSamples).toLong << 36 | gs(k + 14 * nSamples).toLong << 34 | gs(k + 15 * nSamples).toLong << 32 |
+              gs(k + 16 * nSamples).toLong << 30 | gs(k + 17 * nSamples).toLong << 28 | gs(k + 18 * nSamples).toLong << 26 | gs(k + 19 * nSamples).toLong << 24 |
+              gs(k + 20 * nSamples).toLong << 22 | gs(k + 21 * nSamples).toLong << 20 | gs(k + 22 * nSamples).toLong << 18 | gs(k + 23 * nSamples).toLong << 16 |
+              gs(k + 24 * nSamples).toLong << 14 | gs(k + 25 * nSamples).toLong << 12 | gs(k + 26 * nSamples).toLong << 10 | gs(k + 27 * nSamples).toLong << 8  |
+              gs(k + 28 * nSamples).toLong << 6  | gs(k + 29 * nSamples).toLong << 4  | gs(k + 30 * nSamples).toLong << 2  | gs(k + 31 * nSamples).toLong
 
           vBlocki += 1
         }
@@ -48,8 +48,11 @@ object IBSFFI {
         }
         si += 1
       }
+//      println(s"packing ${gs.map(" %04X " format _).mkString}\n\n %%% to\n\n${sampleOrientedGenotypes.map(" %04X " format _).mkString}")
       sampleOrientedGenotypes
     }
+
+    println(s"nSamples $nSamples, length $length,")
 
     val ibs = new Array[Long](nSamples * nGenotypes * 3)
     lib.ibsMat(ibs, nSamples, length, pack(gs1), pack(gs2))
