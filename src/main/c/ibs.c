eea3a4262c4c2479d61116d601f797f164d74de2
Daniel King <dking@broadinstitute.org>
2016-11-14 16:42:35 -0500

    cleanup and add ifdef for AVX

--- src/main/c/ibs.c
+++ src/main/c/ibs.c
@@ -8,55 +8,35 @@
 
 #define EXPORT __attribute__((visibility("default")))
 
-#define fmt256 "%016llx  %016llx  %016llx  %016llx"
-// XXX: only call with identifiers
-#define splat256(x) _mm256_extract_epi64((x), 0), _mm256_extract_epi64((x), 1), _mm256_extract_epi64((x), 2), _mm256_extract_epi64((x), 3)
-
-#define echo(x) printf(#x " = %016llx\n", x)
-
 __m256i allones = { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF };
-__m256i rightAllele = { 0x5555555555555555, 0x5555555555555555, 0x5555555555555555, 0x5555555555555555 };
 __m256i allNA = { 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA };
 __m256i leftAllele = { 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA };
 
+
+#ifdef __AVX__
 void ibs256(uint64_t* result, __m256i x, __m256i y) {
   __m256i nxor = _mm256_xor_si256(_mm256_xor_si256(x, y), allones);
   __m256i nxorSR1 = _mm256_srli_epi64(nxor, 1);
 
-  // if both bits are one, then the genotype is missing
   __m256i xna_tmp = _mm256_xor_si256(allNA, x);
   __m256i xna = _mm256_or_si256(_mm256_srli_epi64(xna_tmp, 1), xna_tmp);
   __m256i yna_tmp = _mm256_xor_si256(allNA, y);
   __m256i yna = _mm256_or_si256(_mm256_srli_epi64(yna_tmp, 1), yna_tmp);
-  // if either sample is missing a genotype, we ignore that genotype pair
-  // also set all the left alleles to 1
   __m256i na = _mm256_or_si256(_mm256_xor_si256(_mm256_and_si256(xna, yna), allones), leftAllele);
-  // 1. shift the left alleles over the right ones
-  // 2. and the alleles
-  // 3. mask to the right ones
+
   __m256i ibs2 = _mm256_andnot_si256(na, _mm256_and_si256(nxorSR1, nxor));
-  // 4. popcnt
   uint64_t ibs2sum = _mm_popcnt_u64(_mm256_extract_epi64(ibs2, 0));
   ibs2sum += _mm_popcnt_u64(_mm256_extract_epi64(ibs2, 1));
   ibs2sum += _mm_popcnt_u64(_mm256_extract_epi64(ibs2, 2));
   ibs2sum += _mm_popcnt_u64(_mm256_extract_epi64(ibs2, 3));
 
-  // 1. shift the left alleles over the right ones
-  // 2. or the alleles
-  // 3. mask to the right ones
   __m256i ibs1 = _mm256_andnot_si256(na, _mm256_xor_si256(nxorSR1, nxor));
-  // 4. popcnt
   uint64_t ibs1sum = _mm_popcnt_u64(_mm256_extract_epi64(ibs1, 0));
   ibs1sum += _mm_popcnt_u64(_mm256_extract_epi64(ibs1, 1));
   ibs1sum += _mm_popcnt_u64(_mm256_extract_epi64(ibs1, 2));
   ibs1sum += _mm_popcnt_u64(_mm256_extract_epi64(ibs1, 3));
 
-  // 1. shift the left alleles over the right ones
-  // 2. or the alleles
-  // 3. negate the alleles
-  // 4. mask to the right ones
   __m256i ibs0 = _mm256_andnot_si256(na, _mm256_xor_si256(_mm256_or_si256(nxorSR1, nxor), allones));
-  // 5. popcnt
   uint64_t ibs0sum = _mm_popcnt_u64(_mm256_extract_epi64(ibs0, 0));
   ibs0sum += _mm_popcnt_u64(_mm256_extract_epi64(ibs0, 1));
   ibs0sum += _mm_popcnt_u64(_mm256_extract_epi64(ibs0, 2));
@@ -66,20 +46,18 @@ void ibs256(uint64_t* result, __m256i x, __m256i y) {
   result[1] += ibs1sum;
   result[2] += ibs2sum;
 }
+#endif // __AVX__
 
 void ibsVec(uint64_t* result, uint64_t length, uint64_t* x, uint64_t* y) {
-  uint64_t i;
-  /* printf("x 0x%" PRIXPTR "\n", (uintptr_t)x); */
-  /* printf("y 0x%" PRIXPTR "\n", (uintptr_t)y); */
-  for (i = 0; i < (length - 3); i += 4) {
-    /* printf("i %llu\n", i); */
-    /* printf("x %llx  %llx  %llx  %llx\n", x[i], x[i+1], x[i+2], x[i+3]); */
-    /* printf("y %llx  %llx  %llx  %llx\n", y[i], y[i+1], y[i+2], y[i+3]); */
+  uint64_t i = 0;
+  #ifdef __AVX__
+  for (; i < (length - 3); i += 4) {
     __m256i x256 = _mm256_load_si256((__m256i*)x);
     __m256i y256 = _mm256_load_si256((__m256i*)x);
     ibs256(result, x256, y256);
   }
-  while (i < length) {
+  #endif // __AVX__
+  for (; i < length; ++i) {
     uint64_t rightAllele64 = 0x5555555555555555;
     uint64_t allNA64 = 0xAAAAAAAAAAAAAAAA;
     uint64_t xb = x[i];
@@ -93,7 +71,6 @@ void ibsVec(uint64_t* result, uint64_t length, uint64_t* x, uint64_t* y) {
     result[2] += _mm_popcnt_u64(((nxor >> 1) & nxor) & rightAllele64 & ~na);
     result[1] += _mm_popcnt_u64(((nxor >> 1) ^ nxor) & rightAllele64 & ~na);
     result[0] += _mm_popcnt_u64(~((nxor >> 1) | nxor) & rightAllele64 & ~na);
-    ++i;
   }
 }
 
