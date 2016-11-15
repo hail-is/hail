741b02a30f1b27583dca738f7fb903d9313b1cad
Daniel King <dking@broadinstitute.org>
2016-11-14 19:34:25 -0500

    remove compile.sh

--- src/main/c/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-
-clang -O3 -march=native -g -dynamiclib -std=gnu99 ibs.c -current_version 1.0.1 -compatibility_version 1.0.1 -fvisibility=hidden -o ibs.dylib
-
-cp ibs.dylib ../../../build/resources/main/darwin/libibs.dylib
-cp ibs.dylib ../../../build/resources/main/libibs.dylib
