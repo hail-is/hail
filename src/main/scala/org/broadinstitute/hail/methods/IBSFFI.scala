7346429706ef59d0f06fcd61a71c27038eea1ca3
Daniel King <dking@broadinstitute.org>
2016-11-17 11:18:11 -0500

    naming

--- src/main/scala/org/broadinstitute/hail/methods/IBSFFI.scala
+++ src/main/scala/org/broadinstitute/hail/methods/IBSFFI.scala
@@ -8,22 +8,24 @@ case class IBS (N0: Long, N1: Long, N2: Long) { }
 object IBSFFI {
 
   @native
-  def ibsMat(result: Array[Long], nSamples: Long, nGenotypePacks: Long, genotypes1: Array[Long], genotypes2: Array[Long])
+  def ibsMat(result: Array[Long], nSamples: Long, nPacks: Long, genotypes1: Array[Long], genotypes2: Array[Long])
 
   Native.register("ibs");
 
-  def pack(nSamples: Int, nGenotypes: Int, gs: Array[Byte]): Array[Long] = {
-    val packedLength = nGenotypes / 32
-    val length = packedLength + (if (nGenotypes % 32 != 0) 1 else 0)
+  val genotypesPerPack = 32
 
-    val sampleOrientedGenotypes = new Array[Long](nSamples * length)
+  def pack(nSamples: Int, nGenotypes: Int, gs: Array[Byte]): Array[Long] = {
+    val nFullPacks = nGenotypes / genotypesPerPack
+    val nPacks = nFullPacks + (if (nGenotypes % genotypesPerPack != 0) 1 else 0)
+
+    val sampleOrientedGenotypes = new Array[Long](nSamples * nPacks)
     var si = 0
     while (si != nSamples) {
-      var vBlocki = 0
-      while (vBlocki != packedLength) {
-        val k = si + vBlocki*32*nSamples
-        sampleOrientedGenotypes(si * length + vBlocki) =
-          gs(k).toLong                 << 62 | gs(k + 1 * nSamples).toLong  << 60 | gs(k + 2 * nSamples).toLong  << 58 | gs(k + 3 * nSamples).toLong  << 56 |
+      var pack = 0
+      while (pack != nFullPacks) {
+        val k = si + pack*genotypesPerPack*nSamples
+        sampleOrientedGenotypes(si * nPacks + pack) =
+            gs(k).toLong                 << 62 | gs(k + 1 * nSamples).toLong  << 60 | gs(k + 2 * nSamples).toLong  << 58 | gs(k + 3 * nSamples).toLong  << 56 |
             gs(k + 4 * nSamples).toLong  << 54 | gs(k + 5 * nSamples).toLong  << 52 | gs(k + 6 * nSamples).toLong  << 50 | gs(k + 7 * nSamples).toLong  << 48 |
             gs(k + 8 * nSamples).toLong  << 46 | gs(k + 9 * nSamples).toLong  << 44 | gs(k + 10 * nSamples).toLong << 42 | gs(k + 11 * nSamples).toLong << 40 |
             gs(k + 12 * nSamples).toLong << 38 | gs(k + 13 * nSamples).toLong << 36 | gs(k + 14 * nSamples).toLong << 34 | gs(k + 15 * nSamples).toLong << 32 |
@@ -32,15 +34,15 @@ object IBSFFI {
             gs(k + 24 * nSamples).toLong << 14 | gs(k + 25 * nSamples).toLong << 12 | gs(k + 26 * nSamples).toLong << 10 | gs(k + 27 * nSamples).toLong << 8  |
             gs(k + 28 * nSamples).toLong << 6  | gs(k + 29 * nSamples).toLong << 4  | gs(k + 30 * nSamples).toLong << 2  | gs(k + 31 * nSamples).toLong
 
-        vBlocki += 1
+        pack += 1
       }
-      var vStragglersi = vBlocki*32
+      var genotypeStraggler = pack*genotypesPerPack
       var shift = 62
-      while (vStragglersi != length*32) {
-        val k = si + vStragglersi*nSamples
-        val gt = if (vStragglersi < nGenotypes) gs(k).toLong else 2L
-        sampleOrientedGenotypes(si * length + packedLength) |= gt << shift
-        vStragglersi += 1
+      while (genotypeStraggler != nPacks*genotypesPerPack) {
+        val k = si + genotypeStraggler*nSamples
+        val gt = if (genotypeStraggler < nGenotypes) gs(k).toLong else 2L
+        sampleOrientedGenotypes(si * nPacks + nFullPacks) |= gt << shift
+        genotypeStraggler += 1
         shift -= 2
       }
       si += 1
@@ -49,10 +51,10 @@ object IBSFFI {
   }
 
   def ibs(nSamples: Int, nGenotypes: Int, gs1: Array[Long], gs2: Array[Long]): Array[Long] = {
-    val packedLength = nGenotypes / 32
-    val length = packedLength + (if (nGenotypes % 32 != 0) 1 else 0)
+    val nFullPacks = nGenotypes / genotypesPerPack
+    val nPacks = nFullPacks + (if (nGenotypes % genotypesPerPack != 0) 1 else 0)
     val ibs = new Array[Long](nSamples * nSamples * 3)
-    ibsMat(ibs, nSamples, length, gs1, gs2)
+    ibsMat(ibs, nSamples, nPacks, gs1, gs2)
     ibs
   }
 
