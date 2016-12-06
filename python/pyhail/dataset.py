376dfff82c271c01bc3de87bc47ce168ea0382dd
Daniel King <dking@broadinstitute.org>
2016-12-06 09:34:04 -0500

    migrate exportvcf docs to python

--- python/pyhail/dataset.py
+++ python/pyhail/dataset.py
@@ -518,6 +518,67 @@ class VariantDataset(object):
 
         :param bool parallel: If True, export .vcf in parallel.
 
+        ``exportvcf`` writes out the internal VDS in a vcf file to the `VCF 4.2
+        spec <https://samtools.github.io/hts-specs/VCFv4.2.pdf>`_.
+
+        Importing a VCF to a Hail VDS then exporting it as a VCF will not
+        produce exactly the same VCF.  The differences, however, are limited to
+        the header and INFO field. Hail's VCF header will contain:
+
+        * FORMAT lines, with the format "GT:AD:DP:GQ:PL"
+        * FILTER lines, if present in original imported VCF
+        * INFO lines (see exported annotations below)
+
+        Hail's VCF header will not contain:
+
+        * contig lines
+        * lines added by external tools (bcftools, GATK, etc.)
+
+        In order to determine what to export in the INFO field of a VCF, Hail
+        looks in the variant annotation schema under ``va.info``.  The
+        workflow::
+
+            (hc.import_vcf("some/file.vcf.bgz")
+               .export_vcf("some/other/file.vcf.bgz"))
+
+        will write out the same info field that was read in.  This means that
+        problems can emerge when a workflow becomes more complicated.  If
+        samples or genotypes are filtered after importing a VCF, the value
+        stored in ``va.info.AC`` value may no longer reflect the number of
+        called alternate alleles in the variant dataset.  If this state is
+        exported to VCF, downstream tools may produce false results.
+
+        The solution to this problem is to use ``annotatevariants`` to create new
+        annotations in ``va.info`` (or overwrite existing annotations).  For
+        example, in order to produce an accurate "AC" field, one should run
+        ``variantqc`` and copy the ``va.qc.AC`` field::
+
+            vds.annotate_variants_expr('va.info.AC = va.qc.AC')
+
+        In the context of a larger workflow::
+
+            (hc.importvcf("file.vcf.bgz")
+               .filtergenotypes('g.gq > 20')
+               .sample_qc()
+               .filter_samples('sa.qc.callRate < 0.99', keep=false)
+               .variant_qc()
+               .annotate_variants_expr('va.info.AC = va.qc.AC')
+               .exportvcf("fileQC.vcf.bgz"))
+
+        For more information about what types of functionality is available to
+        copy and create annotations, `see the documentation
+        here <reference.html#Annotations>`_
+
+        **Hail's VCF will not contain any added sample annotations, or variant annotations not in** ``va.info`` **.**
+
+        Writing an uncompressed VCF::
+
+            hc.read("/path/to/file.vds").exportvcf("/path/to/file.vcf")
+
+        Writing a block-compressed VCF::
+
+            hc.read("/path/to/file.vds").exportvcf("/path/to/file.vcf.bgz")
+
         """
 
         pargs = ['exportvcf', '--output', output]
