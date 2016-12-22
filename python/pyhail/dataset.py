47bbba489929dee789ebe3bb26693f6c625e4384
jbloom22 <jbloom@broadinstitute.org>
2016-12-22 14:13:54 -0500

    corrected annotate_samples_list

--- python/pyhail/dataset.py
+++ python/pyhail/dataset.py
@@ -257,7 +257,7 @@ class VariantDataset(object):
 
         We have a file with a list of samples::
 
-          $ cat ~/batch1.txt
+          $ cat ~/batch.txt
           PT-1234
           PT-1235
           PT-1236
@@ -268,9 +268,9 @@ class VariantDataset(object):
         The command
 
         >>> vds1 = (hc.read('example.vds')
-        >>>  .annotate_samples_list('batch1.txt','sa.batch1')
+        >>>  .annotate_samples_list('batch.txt','sa.batch1')
 
-        will read the file `samples.txt` and annotate samples with `true` if in the list and `false` otherwise, resulting in the following schema::
+        will read the file `batch.txt` and annotate samples with `true` if in the list and `false` otherwise, resulting in the following schema::
 
           Sample annotations:
           sa: Struct {
