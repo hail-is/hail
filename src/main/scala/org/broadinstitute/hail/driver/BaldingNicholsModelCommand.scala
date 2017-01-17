e7032d54ac2e46a92dd8b2bcf2a2f021022737b5
John Compitello <johnc@broadinstitute.org>
2017-01-17 12:39:22 -0500

    Removed old Balding-Nichols implementation, added Scaladoc, added numPartitions argument

--- src/main/scala/org/broadinstitute/hail/driver/BaldingNicholsModelCommand.scala
+++ src/main/scala/org/broadinstitute/hail/driver/BaldingNicholsModelCommand.scala
@@ -3,23 +3,11 @@ package org.broadinstitute.hail.driver
 import breeze.linalg.DenseVector
 import org.apache.spark.SparkContext
 import org.broadinstitute.hail.utils.{fatal, plural}
-import org.broadinstitute.hail.stats.{BaldingNicholsModel, BaldingNicholsModelDist}
+import org.broadinstitute.hail.stats.BaldingNicholsModel
 import org.broadinstitute.hail.variant.VariantDataset
 
 object BaldingNicholsModelCommand {
 
-  /*def name = "balding_nichols"
-  def description = "Generate a variant dataset using the Balding-Nichols model"
-  class Options extends BaseOptions
-  def newOptions = new Options
-  def supportsMultiallelic = true
-  def requiresVDS = false
-  def run(state: State, options: Options): State = fatal("balding_nichols is only supported via Python")
-  */
-
-//  def balding_nichols(): Unit = {
-//    print("yay")
-//  }
   def balding_nichols(sc: SparkContext,
     nPops: Int,
     nSamples: Int,
@@ -27,7 +15,8 @@ object BaldingNicholsModelCommand {
     popDist: Array[Double],
     FstOfPop: Array[Double],
     seed: Int,
-    nPartitions: Int): VariantDataset = {
+    nPartitions: Int,
+    root: String): VariantDataset = {
     if (nPops <= 0)
       fatal(s"Number of populations must be positive, got ${ nPops }")
 
@@ -58,12 +47,14 @@ object BaldingNicholsModelCommand {
       fatal(s"Number of partitions must be positive, got $nPartitions")
 
 
-    BaldingNicholsModelDist(sc,
+    BaldingNicholsModel(sc,
         nPops,
         nSamples,
         nVariants,
+        nPartitions,
         DenseVector(popDist),
         DenseVector(FstOfPop),
+        root,
         seed)
   }
 }
