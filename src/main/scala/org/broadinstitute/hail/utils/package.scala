05986c1b9081bbbd933704be20c74ddfad00bfd6
cseed <cotton@alum.mit.edu>
2017-01-17 11:44:39 -0500

    Get working with Spark 2.1. (#1261)
    In Spark 2.1, write.parquet writes part-NNNNN instead of part-r-NNNN
    files.

--- src/main/scala/org/broadinstitute/hail/utils/package.scala
+++ src/main/scala/org/broadinstitute/hail/utils/package.scala
@@ -234,10 +234,10 @@ package object utils extends Logging
   }
 
   def getParquetPartNumber(fname: String): Int = {
-    val parquetRegex = ".*/?part-r-(\\d+)-.*\\.parquet.*".r
+    val parquetRegex = ".*/?part-(r-)?(\\d+)-.*\\.parquet.*".r
 
     fname match {
-      case parquetRegex(i) => i.toInt
+      case parquetRegex(_, i) => i.toInt
       case _ => throw new PathIOException(s"invalid parquet file `$fname'")
     }
   }
