04afc1168ae2e5e81ba713bbf0475063ab651440
Tim Poterba <tpoterba@broadinstitute.org>
2017-01-16 19:39:00 -0500

    Fix bug in annotate samples vds with --root option (#1257)
    * Fix bug in annotate samples vds with --root option
    
    * Performance improvement

--- src/main/scala/org/broadinstitute/hail/driver/AnnotateSamplesVDS.scala
+++ src/main/scala/org/broadinstitute/hail/driver/AnnotateSamplesVDS.scala
@@ -41,10 +41,11 @@ object AnnotateSamplesVDS extends Command with JoinAnnotator {
         "sa" -> (0, vds.saSignature),
         "vds" -> (1, other.saSignature)))
       buildInserter(code, vds.saSignature, ec, Annotation.SAMPLE_HEAD)
-    } else vds.insertSA(other.vaSignature, Parser.parseAnnotationRoot(root, Annotation.SAMPLE_HEAD))
+    } else vds.insertSA(other.saSignature, Parser.parseAnnotationRoot(root, Annotation.SAMPLE_HEAD))
 
+    val m = other.sampleIdsAndAnnotations.toMap
     vds
-      .annotateSamples(other.sampleIdsAndAnnotations.toMap.get(_), finalType, inserter)
+      .annotateSamples(m.get _, finalType, inserter)
   }
 
   def run(state: State, options: Options): State = {
