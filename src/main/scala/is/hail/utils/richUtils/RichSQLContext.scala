34b429433948f2b35d3b6cc3d127171187fdb07e
Cotton Seed <cotton@alum.mit.edu>
2017-01-18 14:32:13 -0500

    Fix slowdown problem 1.

--- src/main/scala/is/hail/utils/richUtils/RichSQLContext.scala
+++ src/main/scala/is/hail/utils/richUtils/RichSQLContext.scala
@@ -11,7 +11,7 @@ class RichSQLContext(val sqlContext: SQLContext) extends AnyVal {
     if (parquetFiles.isEmpty)
       return sqlContext.sparkContext.emptyRDD[Row]
 
-    var df = sqlContext.read.parquet(parquetFiles: _*)
+    var df = sqlContext.read.parquet(dirname + "/part-*")
     selection.foreach { cols =>
       df = df.select(cols.map(col): _*)
     }
