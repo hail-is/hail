a077d2a414df4efff2578fa5e1da2d135b8ec95e
Jackie Goldstein <jigold@broadinstitute.org>
2017-01-23 10:55:13 -0500

    started adding annotate variants kt

--- src/test/scala/is/hail/variant/vsm/VSMSuite.scala
+++ src/test/scala/is/hail/variant/vsm/VSMSuite.scala
@@ -9,6 +9,7 @@ import is.hail.check.Prop._
 import is.hail.driver._
 import is.hail.expr._
 import is.hail.io.vcf.LoadVCF
+import is.hail.keytable.KeyTable
 import is.hail.variant._
 import is.hail.utils._
 import is.hail.sparkextras.OrderedRDD
@@ -387,5 +388,15 @@ class VSMSuite extends SparkSuite {
 
     s2 = Read.run(s2, Array(path))
     assert(s.vds.same(s2.vds))
+  }
+
+  @Test def testAnnotateVariantsKeyTable() {
+    var s = State(sc, sqlContext)
+    s = ImportVCF.run(s, Array("src/test/resources/sample.vcf"))
+
+    val kt = s.vds.variantsKT()
+    val kt2 = KeyTable.importTextTable(sc, Array("src/test/resources/sampleAnnotations.tsv"), "Sample", 2, TextTableConfiguration())
+    s.vds.annotateVariantsKT(kt, null, "va.foo = table.va")
+    ExportVCF.run(s, Array("-o", "foo"))
   }
 }
