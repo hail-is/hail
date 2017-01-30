66191a3aaad4ff7b6b39a3da7401de903005f431
jbloom22 <jbloom@broadinstitute.org>
2017-01-29 22:01:51 -0500

    Defined countNonNegative in IntIterator to avoid Function1 creation

--- src/main/scala/is/hail/utils/IntIterator.scala
+++ src/main/scala/is/hail/utils/IntIterator.scala
@@ -4,10 +4,10 @@ abstract class IntIterator extends Iterator[Int] {
   def nextInt(): Int
   override def next(): Int = nextInt()
 
-  override def count(p: Int => Boolean): Int = {
+  def countNonNegative(): Int = {
     var cnt = 0
     while (hasNext) {
-      if (p(nextInt())) cnt += 1
+      if (nextInt() >= 0) cnt += 1
     }
     cnt
   }
