62813274805930a3ae2b85f08e8d42573283224b
Jackie Goldstein <jigold@broadinstitute.org>
2017-01-31 21:53:18 -0500

    fixed rich iterable genotype

--- src/main/scala/is/hail/variant/package.scala
+++ src/main/scala/is/hail/variant/package.scala
@@ -1,19 +1,31 @@
 package is.hail
 
+import is.hail.utils.IntIterator
+
 import scala.language.implicitConversions
 
 package object variant {
   type VariantDataset = VariantSampleMatrix[Genotype]
 
-  class RichIterableGenotype(val it: Iterable[Genotype]) extends AnyVal {
+  class RichIterableGenotype(val git: Iterable[Genotype]) extends AnyVal {
     def toGenotypeStream(v: Variant, isDosage: Boolean, compress: Boolean): GenotypeStream =
-      it match {
+      git match {
         case gs: GenotypeStream => gs
         case _ =>
           val b: GenotypeStreamBuilder = new GenotypeStreamBuilder(v.nAlleles, isDosage = isDosage, compress = compress)
-          b ++= it
+          b ++= git
           b.result()
       }
+
+    def hardCallIterator: IntIterator = git match {
+      case gs: GenotypeStream => gs.gsHardCallIterator
+      case _ =>
+        new IntIterator {
+          val it: Iterator[Genotype] = git.iterator
+          override def hasNext: Boolean = it.hasNext
+          override def nextInt(): Int = it.next().unboxedGT
+        }
+    }
   }
 
   implicit def toRichIterableGenotype(it: Iterable[Genotype]): RichIterableGenotype = new RichIterableGenotype(it)
