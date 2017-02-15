61f37e4ff3d3e7466bce77474efcff568d495736
Jackie Goldstein <jigold@broadinstitute.org>
2017-02-15 17:25:24 -0500

    addressed comments

--- python/hail/docs/scripts/setupTutorial.sh
+++ python/hail/docs/scripts/setupTutorial.sh
@@ -13,11 +13,11 @@ if [ -d 1kg.vds/ ]; then
     rm -r 1kg.vds
 fi
 
-if [ -z $tutorialFiles ]; then
+if [ -z "$tutorialFiles" ]; then
     wget https://storage.googleapis.com/hail-tutorial/Hail_Tutorial_Data-v2.tgz && tar -xvzf Hail_Tutorial_Data-v2.tgz --strip 1
 else
-    for f in ${tutorialFiles}/*; do
-        if [ ! -e $(basename $f) ]; then
+    for f in "${tutorialFiles}/*"; do
+        if [ ! -e $(basename "$f") ]; then
             ln -s $f ./
         fi
     done
