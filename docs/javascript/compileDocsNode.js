d160fbb5d596d93007c7b20c7d368b5314e8150d
jbloom22 <jbloom@broadinstitute.org>
2017-02-16 19:32:38 -0500

    updated image, added fix for www going from markdown to html

--- docs/javascript/compileDocsNode.js
+++ docs/javascript/compileDocsNode.js
@@ -164,7 +164,8 @@ function buildIndex(htmlTemplate, selector, pandocInput, outputFileName) {
                  $('a[href*="badge"]').remove();
                  $('a[href*="buildTypeStatusDiv"]').remove();
                  $('a[href="https://hail.is"]').replaceWith("Hail");
-                 $("head").append('<script>$(document).ready(function () {$("#hail-navbar").load("navbar.html", function () {$(".nav li").removeClass("active"); $("#home").addClass("active");});});</script>')
+                 $("head").append('<script>$(document).ready(function () {$("#hail-navbar").load("navbar.html", function () {$(".nav li").removeClass("active"); $("#home").addClass("active");});});</script>');
+                 $('img[src="www/hail_spark_summit.png"]').replaceWith('<img src="hail_spark_summit.png"></img>');
 
                  var document = jsdom.jsdom($('html').html());
 
