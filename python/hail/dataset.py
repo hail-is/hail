49e008eb481d5f05dad34e10fb836517055eb32b
John Compitello <johnc@broadinstitute.org>
2017-02-21 13:46:36 -0500

    Finished documenting impute_vds

--- python/hail/dataset.py
+++ python/hail/dataset.py
@@ -2000,28 +2000,29 @@ class VariantDataset(object):
         """Impute sex of samples by calculating inbreeding coefficient on the
         X chromosome.
 
+        **Examples**
+
+        >>> imputed_sex_vds = vds.impute_sex()
+
         **Notes**
 
         We have used the same implementation as `PLINK v1.7 <http://pngu.mgh.harvard.edu/~purcell/plink/summary.shtml#sexcheck>`_.
 
         1. X chromosome variants are selected from the VDS: ``v.contig == "X" || v.contig == "23"``
         2. Variants with a minor allele frequency less than the threshold given by ``maf-threshold`` are removed
-        3. Variants in the pseudoautosomal region `(X:60001-2699520) || (X:154931044-155260560)` are included if the ``include_par``
-        optional parameter is set to true.
+        3. Variants in the pseudoautosomal region `(X:60001-2699520) || (X:154931044-155260560)` are included if the ``include_par`` optional parameter is set to true.
         4. The minor allele frequency (maf) per variant is calculated.
-        5. For each variant and sample with a non-missing genotype call, :math:`E`, the expected number of homozygotes (from population MAF),
-        is computed as :math:`1.0 - (2.0*maf*(1.0-maf))`.
-        6. For each variant and sample with a non-missing genotype call, :math:`O`, the observed number of homozygotes, is computed as
-        `0 = heterozygote; 1 = homozygote`
+        5. For each variant and sample with a non-missing genotype call, :math:`E`, the expected number of homozygotes (from population MAF), is computed as :math:`1.0 - (2.0*maf*(1.0-maf))`.
+        6. For each variant and sample with a non-missing genotype call, :math:`O`, the observed number of homozygotes, is computed as `0 = heterozygote; 1 = homozygote`
         7. For each variant and sample with a non-missing genotype call, :math:`N` is incremented by 1
         8. For each sample, :math:`E`, :math:`O`, and :math:`N` are combined across variants
         9. :math:`F` is calculated by :math:`(O - E) / (N - E)`
-        10. A sex is assigned to each sample with the following criteria: `F < 0.2 => Female; F > 0.8 => Male`. Use ``female-threshold``
-        and ``male-threshold`` to change this behavior.
+        10. A sex is assigned to each sample with the following criteria: `F < 0.2 => Female; F > 0.8 => Male`. Use ``female-threshold`` and ``male-threshold`` to change this behavior.
 
         **Annotations**
 
         The below annotations can be accessed with ``sa.imputesex``.
+
         - **isFemale** (*Boolean*) -- True if the imputed sex is female, false if male, missing if undetermined
         - **Fstat** (*Double*) -- Inbreeding coefficient
         - **nTotal** (*Long*) -- Total number of variants considered
