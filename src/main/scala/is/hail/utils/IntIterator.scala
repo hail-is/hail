6fcdb1a5508b21a89f4c9807b65c84c18959b20a
jbloom22 <jbloom@broadinstitute.org>
2017-02-23 19:25:27 -0500

    using self =>

--- src/main/scala/is/hail/utils/IntIterator.scala
+++ src/main/scala/is/hail/utils/IntIterator.scala
@@ -6,24 +6,27 @@ import is.hail.stats.{LinRegBuilder, SparseGtBuilder, SparseGtVectorAndStats}
 import scala.collection.mutable
 
 abstract class IntIterator {
+  self =>
+
   def nextInt(): Int
+
   def hasNext: Boolean
 
   // requires that hasNext is called exactly once between each call to nextInt
   // requires that `this` is at least as long as `that` (excess `this` is filtered)
   def unsafeFilter(that: Iterator[Boolean]): IntIterator = new IntIterator {
-      def nextInt(): Int = IntIterator.this.nextInt()
+    def nextInt(): Int = self.nextInt()
 
-      def hasNext: Boolean = {
-        while (that.hasNext && IntIterator.this.hasNext) {
-          if (that.next())
-            return true
-          else
-            IntIterator.this.nextInt()
-        }
-        false
+    def hasNext: Boolean = {
+      while (that.hasNext && self.hasNext) {
+        if (that.next())
+          return true
+        else
+          self.nextInt()
       }
+      false
     }
+  }
 
   def toArray: Array[Int] = {
     val b = new mutable.ArrayBuilder.ofInt
