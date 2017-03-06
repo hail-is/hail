73e6ede02c961d98304c4e90060ac406933ac10b
John Compitello <johnc@broadinstitute.org>
2017-03-06 15:53:49 -0500

    WIP getting closer

--- src/main/scala/is/hail/methods/Nirvana.scala
+++ src/main/scala/is/hail/methods/Nirvana.scala
@@ -14,7 +14,7 @@ import scala.collection.JavaConverters._
 
 
 object Nirvana {
-  val nirvanaSignature = TStruct(
+  val nirvanaWholeSignature = TStruct(
     "header" -> TStruct(
       "annotator" -> TString,
       "creationTime" -> TString,
@@ -175,6 +175,150 @@ object Nirvana {
     ))
   )
 
+  val nirvanaSignature = TStruct(
+    "chromosome" -> TString,
+    "refAllele" -> TString,
+    "position" -> TInt,
+    "altAlleles" -> TArray(TString),
+    "cytogeneticBand" -> TString,
+    "quality" -> TInt,
+    "filters" -> TArray(TString),
+    "jointSomaticNormalQuality" -> TInt,
+    "copyNumber" -> TInt,
+    "strandBias" -> TDouble,
+    "recalibratedQuality" -> TDouble,
+    "samples" -> TArray(TStruct(
+      "variantFreq" -> TDouble,
+      "totalDepth" -> TInt,
+      "alleleDepths" -> TArray(TInt),
+      "genotype" -> TString,
+      "genotypeQuality" -> TInt,
+      "failedFilter" -> TBoolean,
+      "isEmpty" -> TBoolean,
+      "copyNumber" -> TInt,
+      "lossOfHeterozygosity" -> TBoolean
+    )),
+    "variants" -> TArray(TStruct(
+      "ancestralAllele" -> TString,
+      "altAllele" -> TString,
+      "refAllele" -> TString,
+      "chromosome" -> TString,
+      "begin" -> TInt,
+      "end" -> TInt,
+      "phylopScore" -> TDouble,
+      "dbsnp" -> TArray(TString),
+      "globalMinorAllele" -> TString,
+      "gmaf" -> TDouble,
+      "isReferenceMinorAllele" -> TBoolean,
+      "variantType" -> TString,
+      "vid" -> TString,
+      "oneKgAll" -> TDouble,
+      "oneKgAllAc" -> TInt,
+      "oneKgAllAn" -> TInt,
+      "oneKgAfr" -> TDouble,
+      "oneKgAfrAc" -> TInt,
+      "oneKgAfrAn" -> TInt,
+      "oneKgAmr" -> TDouble,
+      "oneKgAmrAc" -> TInt,
+      "oneKgAmrAn" -> TInt,
+      "oneKgEas" -> TDouble,
+      "oneKgEasAc" -> TInt,
+      "oneKgEasAn" -> TInt,
+      "oneKgEur" -> TDouble,
+      "oneKgEurAc" -> TInt,
+      "oneKgEurAn" -> TInt,
+      "oneKgSas" -> TDouble,
+      "oneKgSasAc" -> TInt,
+      "oneKgSasAn" -> TInt,
+      "evsCoverage" -> TInt,
+      "evsSamples" -> TInt,
+      "evsAll" -> TDouble,
+      "evsAfr" -> TDouble,
+      "evsEur" -> TDouble,
+      "exacCoverage" -> TInt,
+      "exacAll" -> TDouble,
+      "exacAllAc" -> TInt,
+      "exacAllAn" -> TInt,
+      "exacAfr" -> TDouble,
+      "exacAfrAc" -> TInt,
+      "exacAfrAn" -> TInt,
+      "exacAmr" -> TDouble,
+      "exacAmrAc" -> TInt,
+      "exacAmrAn" -> TInt,
+      "exacEas" -> TDouble,
+      "exacEasAc" -> TInt,
+      "exacEasAn" -> TInt,
+      "exacFin" -> TDouble,
+      "exacFinAc" -> TInt,
+      "exacFinAn" -> TInt,
+      "exacNfe" -> TDouble,
+      "exacNfeAc" -> TInt,
+      "exacNfeAn" -> TInt,
+      "exacOth" -> TDouble,
+      "exacOthAc" -> TInt,
+      "exacOthAn" -> TInt,
+      "exacSas" -> TDouble,
+      "exacSasAc" -> TInt,
+      "exacSasAn" -> TInt
+    )),
+    "regulatoryRegions" -> TArray(TStruct(
+      "id" -> TString,
+      "consequence" -> TArray(TString)
+    )),
+    "clinVar" -> TArray(TStruct(
+      "id" -> TString,
+      "reviewStatus" -> TString,
+      "isAlleleSpecific" -> TBoolean,
+      "alleleOrigin" -> TString,
+      "refAllele" -> TString,
+      "altAllele" -> TString,
+      "phenotype" -> TString,
+      "geneReviewsId" -> TString,
+      "medGenId" -> TString,
+      "omimId" -> TString,
+      "orphanetId" -> TString,
+      "significance" -> TString,
+      "snoMetCtId" -> TString,
+      "lastEvaluatedDate" -> TString,
+      "pubMedIds" -> TArray(TString)
+    )),
+    "cosmic" -> TArray(TStruct(
+      "id" -> TString,
+      "isAlleleSpecific" -> TBoolean,
+      "refAllele" -> TString,
+      "altAllele" -> TString,
+      "gene" -> TString,
+      "studies" -> TArray(TStruct(
+        "id" -> TInt,
+        "histology" -> TString,
+        "primarySite" -> TString
+      ))
+    )),
+    "transcripts" -> TStruct(
+      "ensembl" -> TArray(TStruct(
+        "transcript" -> TString,
+        "aminoAcids" -> TString,
+        "bioType" -> TString,
+        "cDnaPos" -> TString,
+        "codons" -> TString,
+        "cdsPos" -> TString,
+        "exons" -> TString,
+        "introns" -> TString,
+        "geneId" -> TString,
+        "hgnc" -> TString,
+        "consequence" -> TArray(TString),
+        "hgvsc" -> TString,
+        "hgvsp" -> TString,
+        "isCanonical" -> TBoolean,
+        "polyPhenScore" -> TDouble,
+        "polyPhenPrediction" -> TString,
+        "proteinId" -> TString,
+        "proteinPos" -> TString,
+        "siftScore" -> TDouble,
+        "siftPrediction" -> TString
+      ))
+    ))
+
   def printContext(w: (String) => Unit) {
     w("##fileformat=VCFv4.1")
     w("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT")
@@ -248,10 +392,10 @@ object Nirvana {
       "-o", "-"
     )
 
-    val contigQuery = nirvanaSignature.query("positions")//, "chromosome")
-    val startQuery = nirvanaSignature.query("positions")//, "variants", "begin")
-    val refQuery = nirvanaSignature.query("positions")//, "refAllele")
-    val altsQuery = nirvanaSignature.query("positions")//, "altAlleles")
+    val contigQuery = nirvanaSignature.query("chromosome")//, "chromosome")
+    val startQuery = nirvanaSignature.query("variants")//, "variants", "begin")
+    val refQuery = nirvanaSignature.query("refAllele")//, "refAllele")
+    val altsQuery = nirvanaSignature.query("altAlleles")//, "altAlleles")
 
 
     val localBlockSize = blockSize
@@ -271,9 +415,11 @@ object Nirvana {
               printContext,
               printElement,
               _ => ())
-
-            val kt = jt.map { s =>
+            //Drop header line for now, reconsider later.
+            val kt = jt.drop(1).map { s =>
+              println(s)
                 val a = JSONAnnotationImpex.importAnnotation(JsonMethods.parse(s), nirvanaSignature)
+              println(s"a = $a")
                 val v = variantFromInput(contigQuery(a).asInstanceOf[String], startQuery(a).asInstanceOf[Int],
                   refQuery(a).asInstanceOf[String], altsQuery(a).asInstanceOf[Array[String]])
                 (v, a)
