007127c517aa7eed8b589cbdce615540175351da
jbloom22 <jbloom@broadinstitute.org>
2017-03-14 20:52:12 -0400

    changed hardyWeinburg aggregator doc as well

--- src/main/scala/is/hail/expr/FunctionRegistry.scala
+++ src/main/scala/is/hail/expr/FunctionRegistry.scala
@@ -1610,7 +1610,7 @@ object FunctionRegistry {
 
     **Notes**
 
-    See this `document <../LeveneHaldane.pdf>`_ for more information on how HWE p-values are computed.
+    Hail computes the exact p-value with mid-p-value correction, i.e. the probability of a less-likely outcome plus one-half the probability of an equally-likely outcome. See this `document <../LeveneHaldane.pdf>`_ for details on the Levene-Haldane distribution and references.
     """
   )(aggregableHr(genotypeHr),
     new HailRep[Any] {
