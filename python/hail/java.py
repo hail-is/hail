b2c4f1d062f5010d9656381c82ab956052c1d35a
tpoterba <tpoterba@broadinstitute.org>
2017-03-30 16:06:31 -0400

    Add version to error messages

--- python/hail/java.py
+++ python/hail/java.py
@@ -108,11 +108,15 @@ def handle_py4j(func, *args, **kwargs):
         tpl = Env.jutils().handleForPython(e.java_exception)
         deepest, full = tpl._1(), tpl._2()
 
-        raise FatalError('%s\n\nJava stack trace:\n%s\n\nERROR SUMMARY: %s' % (deepest, full, deepest))
+        raise FatalError('%s\n\nJava stack trace:\n%s\n'
+                         'Hail version: %s\n'
+                         'Error summary: %s' % (deepest, full, Env.hc().version, deepest))
     except py4j.protocol.Py4JError as e:
         if e.args[0].startswith('An error occurred while calling'):
             msg = 'An error occurred while calling into JVM, probably due to invalid parameter types.'
-            raise FatalError('%s\n\nJava stack trace:\n%s\n\nERROR SUMMARY: %s' % (msg, e.message, msg))
+            raise FatalError('%s\n\nJava stack trace:\n%s\n'
+                             'Hail version: %s\n'
+                             'Error summary: %s' % (msg, e.message, Env.hc().version, msg))
         else:
             raise e
     return r
