322daacc7aa31ef987e6545de144ac96a8b28cd9
Daniel King <dking@broadinstitute.org>
2017-03-31 11:19:35 -0400

    pull URL out into variable

--- python/hail/docs/scripts/setupTutorial.sh
+++ python/hail/docs/scripts/setupTutorial.sh
@@ -14,17 +14,19 @@ if [ -d 1kg.vds/ ]; then
 fi
 
 if [ -z "$tutorialFiles" ]; then
+    URL="https://storage.googleapis.com/hail-tutorial/Hail_Tutorial_Data-v2.tgz"
+
     if command -v wget >/dev/null 2>&1
     then
-        wget https://storage.googleapis.com/hail-tutorial/Hail_Tutorial_Data-v2.tgz
+        wget $URL
     elif command -v curl >/dev/null 2>&1
     then
-        curl -LO https://storage.googleapis.com/hail-tutorial/Hail_Tutorial_Data-v2.tgz
+        curl -LO $URL
     else
         echo "building the Hail documentation requires either wget or curl" 1>&2
         exit 1
     fi
-    
+
     tar -xvzf Hail_Tutorial_Data-v2.tgz --strip 1
 else
     for f in "${tutorialFiles}"/*; do
