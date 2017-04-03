a67ab8d333cd41c49d88ebb3015eed3bff00b86a
Daniel King <daniel.zidan.king@gmail.com>
2017-04-02 22:56:23 -0400

    add curl as an alternative to wget (#1535)
    * add curl as an alternative to wget
    
    * pull URL out into variable

--- python/hail/docs/scripts/setupTutorial.sh
+++ python/hail/docs/scripts/setupTutorial.sh
@@ -14,7 +14,20 @@ if [ -d 1kg.vds/ ]; then
 fi
 
 if [ -z "$tutorialFiles" ]; then
-    wget https://storage.googleapis.com/hail-tutorial/Hail_Tutorial_Data-v2.tgz && tar -xvzf Hail_Tutorial_Data-v2.tgz --strip 1
+    URL="https://storage.googleapis.com/hail-tutorial/Hail_Tutorial_Data-v2.tgz"
+
+    if command -v wget >/dev/null 2>&1
+    then
+        wget $URL
+    elif command -v curl >/dev/null 2>&1
+    then
+        curl -LO $URL
+    else
+        echo "building the Hail documentation requires either wget or curl" 1>&2
+        exit 1
+    fi
+
+    tar -xvzf Hail_Tutorial_Data-v2.tgz --strip 1
 else
     for f in "${tutorialFiles}"/*; do
         if [ ! -e $(basename "$f") ]; then
