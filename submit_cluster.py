d52734901ed5d80540c796a28477b9034ae19c57
Liam Abbott <liam.e.abbott@gmail.com>
2017-04-04 11:59:44 -0400

    update

--- submit_cluster.py
+++ submit_cluster.py
@@ -23,7 +23,7 @@ hail_zip = 'pyhail-hail-is-master-{}.zip'.format(hail_hash)
 cmd = ' '.join([
     'gcloud dataproc jobs submit pyspark',
     args.script,
-    '--cluster={}'.format(args.cluster),
+    '--cluster={}'.format(args.name),
     '--files={}'.format('gs://hail-common/' + hail_jar),
     '--py-files={}'.format('gs://hail-common/' + hail_zip),
     '--properties="spark.driver.extraClassPath=./{0},spark.executor.extraClassPath=./{0}"'.format(hail_jar)
