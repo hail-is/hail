4d4f3603c6ba0e41423323bb707c592ba9e08e5a
Daniel King <dking@broadinstitute.org>
2017-04-06 11:32:24 -0400

    kinda works

---
+++ src/test/scala/is/hail/methods/PCRelateSuite.scala
@@ -0,0 +1,23 @@
+package is.hail.methods
+
+import is.hail.SparkSuite
+import org.testng.annotations.Test
+import is.hail.check._
+import is.hail.check.Prop._
+import is.hail.variant.VariantDataset
+import is.hail.variant.VSMSubgen
+import is.hail.stats._
+
+class PCRelateSuite extends SparkSuite {
+  @Test def noErrors() {
+    val vds: VariantDataset = BaldingNicholsModel(hc, 2, 5, 10, None, None, 0, None, BetaDist(0.5,0.5)).splitMulti()
+    val pcs = SamplePCA.justScores(vds, 1)
+    println(PCRelate.pcRelate(vds, pcs).phiHat.bm.blocks.take(1).toSeq)
+  }
+
+  // @Test def foo() {
+  //   val vds: VariantDataset = BaldingNicholsModel(hc, 5, 1000, 10000, None, None, 0, None, UniformDist(0.0,1.0)).splitMulti()
+  //   val pcs = PCRelate.MMatrix.from(vds.sparkContext, SamplePCA.justScores(vds, 10))
+  //   println(PCRelate.pcRelate(vds, pcs).phiHat.m.bm.toLocalMatrix().toArray)
+  // }
+}
