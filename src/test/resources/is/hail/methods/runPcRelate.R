4244d3d83b1be6a7ce975ce10c7443a8749c20c5
Daniel King <dking@broadinstitute.org>
2017-04-07 16:12:17 -0400

    add R script

---
+++ src/test/resources/is/hail/methods/runPcRelate.R
@@ -0,0 +1,28 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+library(GENESIS)
+library(SNPRelate)
+library(GWASTools)
+
+fname = args[1]
+
+snpgdsBED2GDS( bed.fn = paste(fname,".bed",sep="")
+             , bim.fn = paste(fname,".bim",sep="")
+             , fam.fn = paste(fname,".fam",sep="")
+             , out.gdsfn = paste(fname,".gds",sep="")
+             )
+
+gdsfile <- system.file("extdata", paste(fname,".gds",sep=""), package="GENESIS")
+HapMap_geno <- GdsGenotypeReader(filename = paste(fname,".gds",sep=""))
+HapMap_genoData <- GenotypeData(HapMap_geno)
+
+mypcair <- pcair(genoData = HapMap_genoData)
+
+print(mypcair$vectors)
+mypcrelate <- pcrelate( genoData = HapMap_genoData
+                      , pcMat = mypcair$vectors[,1:2]
+                      ## , training.set = mypcair$unrels
+                      )
+foo <- pcrelateReadKinship(pcrelObj = mypcrelate)
+
+write.table(foo, paste(fname,".out",sep=""), row.names=FALSE, quote=FALSE)
