6238185d82db8b44288f1286ba126be8dc2768ff
cseed <cotton@alum.mit.edu>
2017-04-13 15:16:04 -0400

    Added Type.genStruct. (#1661)
    * Added Type.genStruct.
    
    * Adjust sizing in genStruct.

--- src/main/scala/is/hail/expr/Type.scala
+++ src/main/scala/is/hail/expr/Type.scala
@@ -29,21 +29,23 @@ object Type {
         genArb.resize(size - 1).map(TArray),
         genArb.resize(size - 1).map(TSet),
         Gen.zip(genArb.resize(size - 1), genArb.resize(size - 1)).map { case (k, v) => TDict(k, v) },
-        Gen.buildableOf[Array, (String, Type, Map[String, String])](
-          Gen.zip(Gen.identifier,
-            genArb,
-            Gen.option(
-              Gen.buildableOf2[Map, String, String](
-                Gen.zip(arbitrary[String].filter(s => !s.isEmpty), arbitrary[String])))
-              .map(o => o.getOrElse(Map.empty[String, String]))))
-          .filter(fields => fields.map(_._1).areDistinct())
-          .map(fields => TStruct(fields
-            .iterator
-            .zipWithIndex
-            .map { case ((k, t, m), i) => Field(k, t, i, m) }
-            .toIndexedSeq)))
+        genStruct.resize(size))
   }
 
+  def genStruct: Gen[TStruct] =
+    Gen.buildableOf[Array, (String, Type, Map[String, String])](
+      Gen.zip(Gen.identifier,
+        genArb,
+        Gen.option(
+          Gen.buildableOf2[Map, String, String](
+            Gen.zip(arbitrary[String].filter(s => !s.isEmpty), arbitrary[String])))
+          .map(o => o.getOrElse(Map.empty[String, String]))))
+      .filter(fields => fields.map(_._1).areDistinct())
+      .map(fields => TStruct(fields
+        .iterator
+        .zipWithIndex
+        .map { case ((k, t, m), i) => Field(k, t, i, m) }
+        .toIndexedSeq))
 
   def genArb: Gen[Type] = Gen.sized(genSized)
 
