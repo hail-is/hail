9d156ad23174ce892c4089773af851360bd5a9ad
tpoterba <tpoterba@broadinstitute.org>
2017-04-13 15:30:20 -0400

    add copy to the __all__

--- python/hail/__init__.py
+++ python/hail/__init__.py
@@ -6,7 +6,7 @@ from hail.dataset import VariantDataset
 from hail.expr import Type
 from hail.keytable import KeyTable
 from hail.kinshipMatrix import KinshipMatrix
-from hail.utils import TextTableConfig, hdfs_read, hdfs_write
+from hail.utils import TextTableConfig, hdfs_read, hdfs_write, hdfs_copy
 
 __all__ = ['HailContext',
            'VariantDataset',
@@ -17,4 +17,5 @@ __all__ = ['HailContext',
            'representation',
            'hdfs_read',
            'hdfs_write',
+           'hdfs_copy',
            'KinshipMatrix']
