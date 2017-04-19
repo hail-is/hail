f86e5e2bdd48d399d260c34aab6f92fd25e44575
John Compitello <johnc@broadinstitute.org>
2017-04-19 13:26:48 -0400

    Grm to kinship matrix (#1681)
    * Changed GRM to use toBlockMatrixDense, refactored implicits for RichIndexedRowMatrix
    
    * Refactored GRM to return KinshipMatrix
    
    * Created relevant PLINK export methods on KinshipMatrix
    
    * Addressed Tim's comments
    
    * Fixed failing test
    
    * Fixed final failing test, added print out when GRM is called
    
    * Addressed more Tim comments
    
    * Made KinshipMatrix a case-class
    
    * Last style fix
    
    * Auto-indented

--- python/hail/kinshipMatrix.py
+++ python/hail/kinshipMatrix.py
@@ -1,8 +1,12 @@
 from pyspark.mllib.linalg.distributed import IndexedRowMatrix
+from hail.java import *
 
 class KinshipMatrix:
     """
     Represents a symmetric matrix encoding the relatedness of each pair of samples in the accompanying sample list.
+    
+    The output formats are consistent with `PLINK formats <https://www.cog-genomics.org/plink2/formats>`_ as created by the `make-rel and make-grm commands <https://www.cog-genomics.org/plink2/distance#make_rel>`_ and used by `GCTA <http://cnsgenomics.com/software/gcta/estimate_grm.html>`_.
+
     """
     def __init__(self, jkm):
         self._jkm = jkm
@@ -32,3 +36,37 @@ class KinshipMatrix:
         :param string output: The file path to output the matrix to. 
         """
         self._jkm.exportTSV(output)
+
+    def export_rel(self, output):
+        """
+        Export kinship matrix as .rel file. See `PLINK formats <https://www.cog-genomics.org/plink2/formats>`_.
+        
+        :param output: The file path to output to. 
+        """
+        self._jkm.exportRel(output)
+
+    def export_gcta_grm(self, output):
+        """
+        Export kinship matrix as .grm file. See `PLINK formats <https://www.cog-genomics.org/plink2/formats>`_.
+        
+        :param output: The file path to output to.
+        """
+        self._jkm.exportGctaGrm(output)
+
+    def export_gcta_grm_bin(self, output, opt_n_file=None):
+        """
+        Export kinship matrix as .grm.bin file or as .grm.N.bin file, depending on whether an N file is specified. See `PLINK formats <https://www.cog-genomics.org/plink2/formats>`_.
+        
+        :param output: The file path to output to. 
+        
+        :param opt_n_file: The file path to the N file. 
+        """
+        self._jkm.exportGctaGrmBin(output, joption(opt_n_file))
+
+    def export_id_file(self, output):
+        """
+        Export samples as .id file. See `PLINK formats <https://www.cog-genomics.org/plink2/formats>`_.
+        
+        :param output: The file to output to.
+        """
+        self._jkm.exportIdFile(output)
--- src/main/scala/is/hail/utils/richUtils/RichIndexedRowMatrix.scala
+++ src/main/scala/is/hail/utils/richUtils/RichIndexedRowMatrix.scala
@@ -7,6 +7,8 @@ import org.apache.spark.rdd.RDD
 
 import scala.reflect.classTag
 
+import is.hail.utils._
+
 /**
   * Adds toBlockMatrixDense method to IndexedRowMatrix
   *
@@ -14,71 +16,72 @@ import scala.reflect.classTag
   * since it seems broadly useful.
   */
 
-object RichIndexedRowMatrix {
-
-  val intClass = classTag[Int].runtimeClass
-  val gpObjectClass = Class.forName("org.apache.spark.mllib.linalg.distributed.GridPartitioner$")
-  val gpApply = gpObjectClass.getMethod("apply", intClass, intClass, intClass)
-
-  def sneakyGridPartitioner(nRowBlocks: Int, nColBlocks: Int, suggestedNumPartitions: Int): Partitioner = try {
-    gpApply.setAccessible(true)
-    gpApply.invoke(gpObjectClass.getField("MODULE$").get(null), nRowBlocks: java.lang.Integer,
-      nColBlocks: java.lang.Integer, suggestedNumPartitions: java.lang.Integer).asInstanceOf[Partitioner]
-  } finally {
-    gpApply.setAccessible(false)
+class RichIndexedRowMatrix(indexedRowMatrix: IndexedRowMatrix) {
+  def toBlockMatrixDense(): BlockMatrix = {
+    toBlockMatrixDense(1024, 1024)
   }
 
-  implicit class RichIndexedRowMatrix(indexedRowMatrix: IndexedRowMatrix) {
-    def toBlockMatrixDense(): BlockMatrix = {
-      toBlockMatrixDense(1024, 1024)
-    }
+  /**
+    * Gets a GridParitioner instance from spark mllib using reflection, since the constructor is private.
+    */
+  private def sneakyGridPartitioner(nRowBlocks: Int, nColBlocks: Int, suggestedNumPartitions: Int): Partitioner = {
+    val intClass = classTag[Int].runtimeClass
+    val gpObjectClass = Class.forName("org.apache.spark.mllib.linalg.distributed.GridPartitioner$")
+    val gpApply = gpObjectClass.getMethod("apply", intClass, intClass, intClass)
 
-    def toBlockMatrixDense(rowsPerBlock: Int, colsPerBlock: Int): BlockMatrix = {
-      require(rowsPerBlock > 0,
-        s"rowsPerBlock needs to be greater than 0. rowsPerBlock: $rowsPerBlock")
-      require(colsPerBlock > 0,
-        s"colsPerBlock needs to be greater than 0. colsPerBlock: $colsPerBlock")
-
-      val m = indexedRowMatrix.numRows()
-      val n = indexedRowMatrix.numCols()
-      val lastRowBlockIndex = m / rowsPerBlock
-      val lastColBlockIndex = n / colsPerBlock
-      val lastRowBlockSize = (m % rowsPerBlock).toInt
-      val lastColBlockSize = (n % colsPerBlock).toInt
-      val numRowBlocks = (m / rowsPerBlock + (if (m % rowsPerBlock == 0) 0 else 1)).toInt
-      val numColBlocks = (n / colsPerBlock + (if (n % colsPerBlock == 0) 0 else 1)).toInt
-
-
-      val blocks: RDD[((Int, Int), Matrix)] = indexedRowMatrix.rows.flatMap({ ir =>
-        val blockRow = ir.index / rowsPerBlock
-        val rowInBlock = ir.index % rowsPerBlock
-
-        ir.vector.toArray
-          .grouped(colsPerBlock)
-          .zipWithIndex
-          .map({ case (values, blockColumn) =>
-            ((blockRow.toInt, blockColumn), (rowInBlock.toInt, values))
-          })
-      }).groupByKey(sneakyGridPartitioner(numRowBlocks, numColBlocks, indexedRowMatrix.rows.getNumPartitions)).map({
-        case ((blockRow, blockColumn), itr) =>
-          val actualNumRows: Int = if (blockRow == lastRowBlockIndex) lastRowBlockSize else rowsPerBlock
-          val actualNumColumns: Int = if (blockColumn == lastColBlockIndex) lastColBlockSize else colsPerBlock
-
-          val arraySize = actualNumRows * actualNumColumns
-          val matrixAsArray = new Array[Double](arraySize)
-          itr.foreach({ case (rowWithinBlock, values) =>
-            var i = 0
-            while (i < values.length) {
-              matrixAsArray.update(i * actualNumRows + rowWithinBlock, values(i))
-              i += 1
-            }
-          })
-          ((blockRow, blockColumn), new DenseMatrix(actualNumRows, actualNumColumns, matrixAsArray))
-      })
-      new BlockMatrix(blocks, rowsPerBlock, colsPerBlock)
+    try {
+      gpApply.setAccessible(true)
+      gpApply.invoke(gpObjectClass.getField("MODULE$").get(null), nRowBlocks: java.lang.Integer,
+        nColBlocks: java.lang.Integer, suggestedNumPartitions: java.lang.Integer).asInstanceOf[Partitioner]
+    } finally {
+      gpApply.setAccessible(false)
     }
   }
 
+  def toBlockMatrixDense(rowsPerBlock: Int, colsPerBlock: Int): BlockMatrix = {
+    require(rowsPerBlock > 0,
+      s"rowsPerBlock needs to be greater than 0. rowsPerBlock: $rowsPerBlock")
+    require(colsPerBlock > 0,
+      s"colsPerBlock needs to be greater than 0. colsPerBlock: $colsPerBlock")
+
+    val m = indexedRowMatrix.numRows()
+    val n = indexedRowMatrix.numCols()
+    val lastRowBlockIndex = m / rowsPerBlock
+    val lastColBlockIndex = n / colsPerBlock
+    val lastRowBlockSize = (m % rowsPerBlock).toInt
+    val lastColBlockSize = (n % colsPerBlock).toInt
+    val numRowBlocks = (m / rowsPerBlock + (if (m % rowsPerBlock == 0) 0 else 1)).toInt
+    val numColBlocks = (n / colsPerBlock + (if (n % colsPerBlock == 0) 0 else 1)).toInt
+
+
+    val blocks: RDD[((Int, Int), Matrix)] = indexedRowMatrix.rows.flatMap{ ir =>
+      val blockRow = ir.index / rowsPerBlock
+      val rowInBlock = ir.index % rowsPerBlock
+
+      ir.vector.toArray
+        .grouped(colsPerBlock)
+        .zipWithIndex
+        .map{ case (values, blockColumn) =>
+          ((blockRow.toInt, blockColumn), (rowInBlock.toInt, values))
+        }
+    }.groupByKey(sneakyGridPartitioner(numRowBlocks, numColBlocks, indexedRowMatrix.rows.getNumPartitions)).mapValuesWithKey{
+      case ((blockRow, blockColumn), itr) =>
+        val actualNumRows: Int = if (blockRow == lastRowBlockIndex) lastRowBlockSize else rowsPerBlock
+        val actualNumColumns: Int = if (blockColumn == lastColBlockIndex) lastColBlockSize else colsPerBlock
+
+        val arraySize = actualNumRows * actualNumColumns
+        val matrixAsArray = new Array[Double](arraySize)
+        itr.foreach{ case (rowWithinBlock, values) =>
+          var i = 0
+          while (i < values.length) {
+            matrixAsArray.update(i * actualNumRows + rowWithinBlock, values(i))
+            i += 1
+          }
+        }
+        new DenseMatrix(actualNumRows, actualNumColumns, matrixAsArray)
+    }
+    new BlockMatrix(blocks, rowsPerBlock, colsPerBlock)
+  }
 }
 
 
