ae8e366470f1736d2390c7ef922fb44492b825aa
Jackie Goldstein <jigold@broadinstitute.org>
2017-04-25 13:53:56 -0400

    add minimal version

--- python/hail/docs/conf.py
+++ python/hail/docs/conf.py
@@ -29,7 +29,7 @@ sys.path.insert(0, os.path.abspath('./_ext'))
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.4'
+needs_sphinx = '1.5.4'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
