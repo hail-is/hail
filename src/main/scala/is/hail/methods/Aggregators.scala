90744d546b0114dc21b0e3f88c6f44c145a98d91
Daniel King <dking@broadinstitute.org>
2017-05-04 14:07:52 -0400

    fix TakeByAggregator merge bug
    Instead of using the computed sort-keys we were recomputing
    the keys when merging. At merge-time, the environment is not
    appropriately configured so the results are non-sense.

--- src/main/scala/is/hail/methods/Aggregators.scala
+++ src/main/scala/is/hail/methods/Aggregators.scala
@@ -610,8 +610,10 @@ class TakeByAggregator[T](var f: (Any) => Any, var n: Int)(implicit var tord: Or
 
   def result = _state.toArray[(Any, Any)].map(_._1).reverse: IndexedSeq[Any]
 
-  def seqOp(x: Any) = {
-    val p = (x, f(x))
+  def seqOp(x: Any) = seqOp(x, f(x))
+
+  private def seqOp(x: Any, sortKey: Any) = {
+    val p = (x, sortKey)
     if (_state.length < n)
       _state += p
     else {
@@ -623,7 +625,7 @@ class TakeByAggregator[T](var f: (Any) => Any, var n: Int)(implicit var tord: Or
   }
 
   def combOp(agg2: this.type) {
-    agg2._state.foreach { case (x, p) => seqOp(x) }
+    agg2._state.foreach { case (x, p) => seqOp(x, p) }
   }
 
   def copy() = new TakeByAggregator(f, n)
