bca117448d69ab8d91cec834223e4a4c019652e1
Jackie Goldstein <jigold@broadinstitute.org>
2017-05-09 11:22:23 -0400

    addressed comments

--- src/main/scala/is/hail/variant/GenomeReference.scala
+++ src/main/scala/is/hail/variant/GenomeReference.scala
@@ -1,28 +1,24 @@
 package is.hail.variant
 
-import is.hail.expr.JSONAnnotationImpex
+import is.hail.expr.{JSONAnnotationImpex, TInterval}
 import is.hail.utils._
-import org.apache.spark.sql.Row
 import org.json4s._
 import org.json4s.jackson.JsonMethods
 
 import scala.reflect.ClassTag
 
-case class GenomeReference(name: String, contigs: Array[Contig]) extends Serializable {
-  val xContigs = contigs.filter(_.isX)
-  val yContigs = contigs.filter(_.isY)
-  val mtContigs = contigs.filter(_.isMT)
-  val autosomalContigs = contigs.filterNot(c => c.isX && c.isY && c.isMT)
+case class GenomeReference(name: String, contigs: Array[Contig], xContigs: Array[String],
+  yContigs: Array[String], mtContigs: Array[String], par: Interval[Locus]*) extends Serializable {
 
-  def inX(contig: String): Boolean = xContigs.exists(_.name == contig)
+  def inX(contig: String): Boolean = xContigs.contains(contig)
 
-  def inY(contig: String): Boolean = yContigs.exists(_.name == contig)
+  def inY(contig: String): Boolean = yContigs.contains(contig)
 
-  def isMitochondrial(contig: String): Boolean = mtContigs.exists(_.name == contig)
+  def isMitochondrial(contig: String): Boolean = mtContigs.contains(contig)
 
-  def inXPar(locus: Locus): Boolean = xContigs.exists(_.par.exists(_.contains(locus)))
+  def inXPar(locus: Locus): Boolean = inX(locus.contig) && par.exists(_.contains(locus))
 
-  def inYPar(locus: Locus): Boolean = yContigs.exists(_.par.exists(_.contains(locus)))
+  def inYPar(locus: Locus): Boolean = inY(locus.contig) && par.exists(_.contains(locus))
 }
 
 object GenomeReference {
@@ -35,31 +31,43 @@ object GenomeReference {
       if (resourceStream == null) {
         throw new RuntimeException(s"Could not read genome reference file `$file'.")
       }
+
       val json = JsonMethods.parse(resourceStream)
 
       val fields = json.asInstanceOf[JObject].obj.toMap
 
-      def getAndCastJSON[T <: JValue](fields: Map[String, JValue], fname: String)(implicit tct: ClassTag[T]): T =
+      def getAndCastJSON[T <: JValue](fname: String)(implicit tct: ClassTag[T]): T =
         fields.get(fname) match {
           case Some(t: T) => t
           case Some(other) =>
             fatal(
               s"""corrupt json: invalid metadata
-               |  Expected `${ tct.runtimeClass.getName }' in field `$fname', but got `${ other.getClass.getName }'""".stripMargin)
+               |  Expected `${ tct.runtimeClass.getName }' in field `$fname', but got `${ other.getClass.getName }'.""".stripMargin)
           case None =>
             fatal(
               s"""corrupt json: invalid metadata
-               |  Missing field `$fname'""".stripMargin)
+               |  Missing field `$fname'.""".stripMargin)
         }
 
-      val name = getAndCastJSON[JString](fields, "name").s
+      val referenceName = getAndCastJSON[JString]("name").s
 
-      val contigs = getAndCastJSON[JArray](fields, "contigs")
+      val contigs = getAndCastJSON[JArray]("contigs")
         .arr
-        .map { jv => Contig.fromRow(JSONAnnotationImpex.importAnnotation(jv, Contig.t).asInstanceOf[Row]) }
+        .map {
+          case JObject(List(("name", JString(name)), ("length", JInt(length)))) => Contig(name, length.toInt)
+          case other => fatal(s"Contig schema did not match {'name': String, 'length': Int}. Found $other.")
+        }.toArray
+
+      val xContigs = getAndCastJSON[JArray]("x_contigs").arr.map { case JString(s) => s }.toArray
+      val yContigs = getAndCastJSON[JArray]("y_contigs").arr.map { case JString(s) => s }.toArray
+      val mtContigs = getAndCastJSON[JArray]("mt_contigs").arr.map { case JString(s) => s }.toArray
+
+      val par = getAndCastJSON[JArray]("par")
+        .arr
+        .map { jv => JSONAnnotationImpex.importAnnotation(jv, TInterval).asInstanceOf[Interval[Locus]] }
         .toArray
 
-      GenomeReference(name, contigs)
+      GenomeReference(referenceName, contigs, xContigs, yContigs, mtContigs, par: _*)
 
     } catch {
       case npe: NullPointerException =>
