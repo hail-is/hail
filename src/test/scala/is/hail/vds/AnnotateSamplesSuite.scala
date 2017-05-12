31148c9e192e1be2e64a4297056d6643eccd0a14
Tim Poterba <tpoterba@gmail.com>
2017-05-12 17:44:17 -0400

    Removed unnecessary methods (last VDS methods that take files) (#1796)
    * Removed unnecessary methods:
    
      - aggregate intervals (easy to implement with key tables)
      - annotate samples list (easy to implement with annotate_samples_table)
      - annotate global list and annotate global table (easy to implement with key table and annotate_global (renamed from annotate_global_py)
    
    Also cleaned up VSM interface (removed annotateSamplesList)
    
    * Fix tests
    
    * Fix tests

--- src/test/scala/is/hail/vds/AnnotateSamplesSuite.scala
+++ src/test/scala/is/hail/vds/AnnotateSamplesSuite.scala
@@ -5,18 +5,6 @@ import org.apache.spark.sql.Row
 import org.testng.annotations.Test
 
 class AnnotateSamplesSuite extends SparkSuite {
-
-  @Test def testVDS() {
-    val vds = hc.importVCF("src/test/resources/sample2.vcf")
-
-    val selfAnnotated = vds.annotateSamplesVDS(vds, root = Some("sa.other"))
-
-    val (_, q) = selfAnnotated.querySA("sa.other")
-    assert(vds.sampleIdsAndAnnotations == selfAnnotated.sampleIdsAndAnnotations.map { case (id, anno) =>
-      (id, q(anno))
-    })
-  }
-
   @Test def testKeyTable() {
     val kt = hc.importTable("src/test/resources/sampleAnnotations.tsv", impute = true).keyBy("Sample")
 
