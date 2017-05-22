7d5c266f9c70583db0585bb5fd6983017c6b8119
John Compitello <johnc@broadinstitute.org>
2017-05-22 18:26:41 -0400

    Fixed docs. Addresses #1787 (#1867)
    * Fixed docs. Addresses #1787
    
    * Added None
    
    * Addressed comments

--- python/hail/kinshipMatrix.py
+++ python/hail/kinshipMatrix.py
@@ -52,7 +52,7 @@ class KinshipMatrix:
         """
         Export kinship matrix to tab-delimited text file with sample list as header.
         
-        :param string output: The file path to output the matrix to. 
+        :param str output: File path for output. 
         """
         self._jkm.exportTSV(output)
 
@@ -61,7 +61,7 @@ class KinshipMatrix:
         """
         Export kinship matrix as .rel file. See `PLINK formats <https://www.cog-genomics.org/plink2/formats>`_.
         
-        :param output: The file path to output to. 
+        :param str output: File path for output. 
         """
         self._jkm.exportRel(output)
 
@@ -70,7 +70,7 @@ class KinshipMatrix:
         """
         Export kinship matrix as .grm file. See `PLINK formats <https://www.cog-genomics.org/plink2/formats>`_.
         
-        :param output: The file path to output to.
+        :param str output: File path for output.
         """
         self._jkm.exportGctaGrm(output)
 
@@ -80,9 +80,10 @@ class KinshipMatrix:
         """
         Export kinship matrix as .grm.bin file or as .grm.N.bin file, depending on whether an N file is specified. See `PLINK formats <https://www.cog-genomics.org/plink2/formats>`_.
         
-        :param output: The file path to output to. 
+        :param str output: File path for output. 
         
         :param opt_n_file: The file path to the N file. 
+        :type opt_n_file: str or None
         """
         self._jkm.exportGctaGrmBin(output, joption(opt_n_file))
 
@@ -91,6 +92,6 @@ class KinshipMatrix:
         """
         Export samples as .id file. See `PLINK formats <https://www.cog-genomics.org/plink2/formats>`_.
         
-        :param output: The file to output to.
+        :param str output: File path for output.
         """
         self._jkm.exportIdFile(output)
