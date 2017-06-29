72c65b429219e8d07b89b5581a1cc7d799551150
tpoterba <tpoterba@broadinstitute.org>
2017-06-29 10:27:53 -0400

    Fixed Bgen 1.1 bugs, made sampleqc faster

--- src/main/scala/is/hail/methods/SampleQC.scala
+++ src/main/scala/is/hail/methods/SampleQC.scala
@@ -3,7 +3,7 @@ package is.hail.methods
 import is.hail.annotations.Annotation
 import is.hail.expr.{TStruct, _}
 import is.hail.utils._
-import is.hail.variant.{Genotype, Variant, VariantDataset}
+import is.hail.variant.{AltAlleleType, Genotype, Variant, VariantDataset}
 import org.apache.spark.util.StatCounter
 
 import scala.collection.mutable
@@ -69,55 +69,52 @@ class SampleQCCombiner(val keepStar: Boolean) extends Serializable {
 
   // FIXME per-genotype
 
-  def merge(v: Variant, ACs: Array[Int], g: Genotype): SampleQCCombiner = {
+  def merge(v: Variant, acs: Array[Int], g: Genotype): SampleQCCombiner = {
 
-    g.gt match {
+    val gt = g.unboxedGT
+    if (gt == 0) {
+      nHomRef += 1
+    } else if (gt > 0) {
+      val gtPair = Genotype.gtPair(gt)
 
-      case Some(0) =>
-        nHomRef += 1
+      def mergeAllele(ai: Int) {
+        if (ai > 0 && (keepStar || !v.altAlleles(ai - 1).isStar)) {
 
-      case Some(gt) =>
-        val nonRefAlleleIndices = Genotype.gtPair(gt).alleleIndices.filter(i => i > 0 && (keepStar || !v.altAlleles(i - 1).isStar))
+          val altAllele = v.altAlleles(ai - 1)
+          altAllele.altAlleleType match {
+            case AltAlleleType.SNP =>
+              nSNP += 1
+              if (altAllele.isTransition)
+                nTi += 1
+              else
+                nTv += 1
+            case AltAlleleType.Insertion =>
+              nIns += 1
+            case AltAlleleType.Deletion =>
+              nDel += 1
+            case _ =>
+          }
 
-        if (!nonRefAlleleIndices.isEmpty) {
-          nonRefAlleleIndices.foreach({
-            ai =>
-              val altAllele = v.altAlleles(ai - 1)
-              if (altAllele.isSNP) {
-                nSNP += 1
-                if (altAllele.isTransition)
-                  nTi += 1
-                else {
-                  assert(altAllele.isTransversion)
-                  nTv += 1
-                }
-              } else if (altAllele.isInsertion)
-                nIns += 1
-              else if (altAllele.isDeletion)
-                nDel += 1
-
-              if (ACs(ai - 1) == 1)
-                nSingleton += 1
-          })
-
-          if (nonRefAlleleIndices.length == 1 || nonRefAlleleIndices(0) != nonRefAlleleIndices(1))
-            nHet += 1
-          else
-            nHomVar += 1
+          if (acs(ai) == 1)
+            nSingleton += 1
         }
-      case None =>
-        nNotCalled += 1
+      }
 
+      mergeAllele(gtPair.j)
+      mergeAllele(gtPair.k)
+
+      if (gtPair.j == gtPair.k)
+        nHet += 1
+      else
+        nHomVar += 1
     }
 
-    if (g.isCalled) {
-      g.dp.foreach { v =>
-        dpSC.merge(v)
-      }
-      g.gq.foreach { v =>
-        gqSC.merge(v)
-      }
-    }
+    if (g.unboxedDP >= 0)
+      dpSC.merge(g.unboxedDP)
+
+    if (g.unboxedGQ >= 0)
+      gqSC.merge(g.unboxedGQ)
+
     this
   }
 
@@ -138,68 +135,6 @@ class SampleQCCombiner(val keepStar: Boolean) extends Serializable {
     gqSC.merge(that.gqSC)
 
     this
-  }
-
-  def emitSC(sb: mutable.StringBuilder, sc: StatCounter) {
-    sb.tsvAppend(someIf(sc.count > 0, sc.mean))
-    sb += '\t'
-    sb.tsvAppend(someIf(sc.count > 0, sc.stdev))
-  }
-
-  def emit(sb: mutable.StringBuilder) {
-
-    val nCalled = nHomRef + nHet + nHomVar
-    val callRate = divOption(nHomRef + nHet + nHomVar, nHomRef + nHet + nHomVar + nNotCalled)
-
-    sb.tsvAppend(callRate)
-    sb += '\t'
-    sb.append(nCalled)
-    sb += '\t'
-    sb.append(nNotCalled)
-    sb += '\t'
-    sb.append(nHomRef)
-    sb += '\t'
-    sb.append(nHet)
-    sb += '\t'
-    sb.append(nHomVar)
-    sb += '\t'
-
-    sb.append(nSNP)
-    sb += '\t'
-    sb.append(nIns)
-    sb += '\t'
-    sb.append(nDel)
-    sb += '\t'
-
-    sb.append(nSingleton)
-    sb += '\t'
-
-    sb.append(nTi)
-    sb += '\t'
-    sb.append(nTv)
-    sb += '\t'
-
-
-    emitSC(sb, dpSC)
-    sb += '\t'
-
-    emitSC(sb, gqSC)
-    sb += '\t'
-
-    // nNonRef
-    sb.append(nHet + nHomVar)
-    sb += '\t'
-
-    // nTiTvf
-    sb.tsvAppend(divOption(nTi, nTv))
-    sb += '\t'
-
-    // rHetHomVar
-    sb.tsvAppend(divOption(nHet, nHomVar))
-    sb += '\t'
-
-    // rInsertionDeletion
-    sb.tsvAppend(divOption(nIns, nDel))
   }
 
   def asAnnotation: Annotation =
@@ -235,16 +170,21 @@ object SampleQC {
           .rdd
           .treeAggregate(Array.fill[SampleQCCombiner](vds.nSamples)(new SampleQCCombiner(keepStar)))({ case (acc, (v, (va, gs))) =>
 
-            val ACs = gs.foldLeft(Array.fill(v.nAltAlleles)(0))({
-              case (acc, g) =>
-                g.gt
-                  .filter(_ > 0)
-                  .foreach(call => Genotype.gtPair(call).alleleIndices.filter(_ > 0).foreach(x => acc(x - 1) += 1)
-                  )
-                acc
-            })
-            for ((g, i) <- gs.iterator.zipWithIndex)
-              acc(i).merge(v, ACs, g)
+            val acs = Array.fill(v.nAlleles)(0)
+            gs.foreach { g =>
+              if (g.unboxedGT >= 0) {
+                val gtPair = Genotype.gtPair(g.unboxedGT)
+                acs(gtPair.j) += 1
+                acs(gtPair.k) += 1
+              }
+            }
+
+            var i = 0
+            gs.foreach { g =>
+              acc(i).merge(v, acs, g)
+              i += 1
+            }
+
             acc
           }, { case (comb1, comb2) =>
             for (i <- comb1.indices)
