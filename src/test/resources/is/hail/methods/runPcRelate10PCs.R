22a5522bb38fe02376a39c51bd0b1529551811f8
Daniel King <dking@broadinstitute.org>
2017-06-30 14:57:49 -0400

    forgot to commit runPcRelate10PCs.R

---
+++ src/test/resources/is/hail/methods/runPcRelate10PCs.R
@@ -0,0 +1,31 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+library(GENESIS)
+library(SNPRelate)
+library(GWASTools)
+
+fname = args[1]
+
+snpgdsBED2GDS( bed.fn = paste(fname,".bed",sep="")
+             , bim.fn = paste(fname,".bim",sep="")
+             , fam.fn = paste(fname,".fam",sep="")
+             , out.gdsfn = paste(fname,".gds",sep="")
+             )
+
+gdsfile <- system.file("extdata", paste(fname,".gds",sep=""), package="GENESIS")
+HapMap_geno <- GdsGenotypeReader(filename = paste(fname,".gds",sep=""))
+HapMap_genoData <- GenotypeData(HapMap_geno)
+
+mypcair <- pcair(genoData = HapMap_genoData)
+
+options(digits=18)
+print(mypcair$vectors[,1:10])
+mypcrelate <- pcrelate( genoData = HapMap_genoData
+                      , pcMat = mypcair$vectors[,1:10]
+                      , correct = FALSE
+                      , MAF = 0.0
+                      )
+foo <- pcrelateReadKinship( pcrelObj = mypcrelate
+                          )
+
+write.table(foo, paste(fname,".out",sep=""), row.names=FALSE, quote=FALSE)
