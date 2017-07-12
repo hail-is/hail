c073c31bf0430ebc24c9c787f5e1f6dc2a9a1801
Daniel King <daniel.zidan.king@gmail.com>
2017-07-11 21:17:36 -0400

    Update BlockMatrixIsDistributedMatrix.scala

--- src/main/scala/is/hail/distributedmatrix/BlockMatrixIsDistributedMatrix.scala
+++ src/main/scala/is/hail/distributedmatrix/BlockMatrixIsDistributedMatrix.scala
@@ -67,7 +67,7 @@ object BlockMatrixIsDistributedMatrix extends DistributedMatrix[BlockMatrix] {
       .collect()
       .sortBy(_._1)
       .map(_._2)
-      .reduceLeft[Array[Double]](_ ++ _._2)
+      .reduceLeft(_ ++ _)
   }
 
   def multiply(l: M, r: M): M = l.multiply(r)
