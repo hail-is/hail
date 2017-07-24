b129a4e87206441f5f6970f923d9583b92ee4058
jbloom22 <jbloom@broadinstitute.org>
2017-07-24 13:17:31 +0300

    moved linreg(multi) fit and schema into linreg model

--- src/main/scala/is/hail/stats/LinearRegressionModel.scala
+++ src/main/scala/is/hail/stats/LinearRegressionModel.scala
@@ -1,20 +1,58 @@
 package is.hail.stats
 
-import breeze.linalg.{Matrix, Vector}
+import breeze.linalg._
+import breeze.numerics.sqrt
 import is.hail.annotations.Annotation
+import is.hail.expr.{TArray, TDouble, TStruct}
 import net.sourceforge.jdistlib.T
 
+case class LinearRegressionStats(b: Double, se: Double, t: Double, p: Double) {
+  def toAnnotation: Annotation = Annotation(b, se, t, p)
+}
+
+case class LinearRegressionMultiPhenoStats(b: Array[Double], se: Array[Double], t: Array[Double], p: Array[Double]) {
+  def toAnnotation: Annotation = Annotation(b: IndexedSeq[Double], se: IndexedSeq[Double], t: IndexedSeq[Double], p: IndexedSeq[Double])
+}
+
 object LinearRegressionModel {
-    def fit(x: Vector[Double], y: Vector[Double], yyp: Double, qt: Matrix[Double], qty: Vector[Double], d: Int): Annotation = {
-      val qtx = qt * x
-      val xxp = (x dot x) - (qtx dot qtx)
-      val xyp = (x dot y) - (qtx dot qty)
+  def schema = TStruct(
+    ("beta", TDouble),
+    ("se", TDouble),
+    ("tstat", TDouble),
+    ("pval", TDouble))
 
-      val b = xyp / xxp
-      val se = math.sqrt((yyp / xxp - b * b) / d)
-      val t = b / se
-      val p = 2 * T.cumulative(-math.abs(t), d, true, false)
+  def fit(x: Vector[Double], y: Vector[Double], yyp: Double, qt: Matrix[Double], qty: Vector[Double], d: Int): LinearRegressionStats = {
+    val qtx = qt * x
+    val xxp = (x dot x) - (qtx dot qtx)
+    val xyp = (x dot y) - (qtx dot qty)
 
-      Annotation(b, se, t, p)
-    }
+    val b = xyp / xxp
+    val se = math.sqrt((yyp / xxp - b * b) / d)
+    val t = b / se
+    val p = 2 * T.cumulative(-math.abs(t), d, true, false)
+
+    LinearRegressionStats(b, se, t, p)
+  }
+
+  def schemaMultiPheno = TStruct(
+    ("beta", TArray(TDouble)),
+    ("se", TArray(TDouble)),
+    ("tstat", TArray(TDouble)),
+    ("pval", TArray(TDouble)))
+
+  def fitMultiPheno(x: Vector[Double], y: DenseMatrix[Double], yyp: DenseVector[Double], qt: Matrix[Double],
+    qty: DenseMatrix[Double], d: Int): LinearRegressionMultiPhenoStats = {
+    
+    val qtx = qt * x
+    val xxpRec = 1 / ((x dot x) - (qtx dot qtx))
+    val xyp: Vector[Double] = (y.t * x) - (qty.t * qtx)
+
+    val dRec = 1.0 / d
+    val b = xxpRec * xyp
+    val se = sqrt(dRec * (xxpRec * yyp - (b :* b)))
+    val t = b :/ se
+    val p = t.map(stat => 2 * T.cumulative(-math.abs(stat), d, true, false))
+    
+    LinearRegressionMultiPhenoStats(b.toArray, se.toArray, t.toArray, p.toArray)
+  }
 }
