0320a618e8adacbb017e6d6acc9c2d5c477d733e
Tim Poterba <tpoterba@gmail.com>
2017-07-27 19:52:27 -0400

    Updated the build to match the website changes: (#2034)
    * Updated the build to match the website changes:
    
     - fixed structure of docs + links (hail/* -> docs/stable/*)
     - Added Hail version + supported spark versions + git hash as gradle variables
     - Used these versions in Sphinx.
     - Changed path of distribution links in getting started to point at current hash.
    
    * Fix sphinx version comments
    
    * Fix up build

--- generate-build-info.sh
+++ generate-build-info.sh
@@ -9,8 +9,9 @@ echo_build_properties() {
   echo date=$(date -u +%Y-%m-%dT%H:%M:%SZ)
   echo url=$(git config --get remote.origin.url)
   echo sparkVersion=$1
+  echo hailVersion=$2
 }
 
 mkdir -p src/main/resources/
 
-echo_build_properties $1 > "src/main/resources/build-info.properties"
+echo_build_properties $1 $2 > "src/main/resources/build-info.properties"
