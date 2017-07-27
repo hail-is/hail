47e9887410ef343026d471f06abe0165a1938cff
tpoterba <tpoterba@broadinstitute.org>
2017-07-27 15:01:14 -0400

    Fixed gradle dependencies

--- generate-dist-links.sh
+++ generate-dist-links.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-mkdir -p python/hail/docs
-rm -f python/hail/docs/distLinks.rst
+TARGET=build/tmp/python/hail/docs/distLinks.rst
+rm -f $TARGET
 
 HAIL_VERSION=$1
 HASH=$(git rev-parse --short HEAD)
 
 
-echo "Hail uploads distributions to Google Storage as part of our continuous integration suite." >> python/hail/docs/distLinks.rst
+echo "Hail uploads distributions to Google Storage as part of our continuous integration suite." >> $TARGET
 
-echo "You can download a pre-built distribution from the below links. Make sure you download the distribution that matches your Spark version!" >> python/hail/docs/distLinks.rst
+echo "You can download a pre-built distribution from the below links. Make sure you download the distribution that matches your Spark version!" >> $TARGET
 
-echo "" >> python/hail/docs/distLinks.rst
+echo "" >> $TARGET
 
 shift
 while test ${#} -gt 0
 do
-  echo "- \`Current distribution for Spark $1 <https://storage.googleapis.com/hail-common/distributions/Hail-$HAIL_VERSION-$HASH-Spark-$1.zip>\`_" >> python/hail/docs/distLinks.rst
+  echo "- \`Current distribution for Spark $1 <https://storage.googleapis.com/hail-common/distributions/Hail-$HAIL_VERSION-$HASH-Spark-$1.zip>\`_" >> $TARGET
   shift
 done
