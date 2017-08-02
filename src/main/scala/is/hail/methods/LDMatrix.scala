1defea90d1e8017836bc88941228ee043f2e209a
jbloom22 <jbloom@broadinstitute.org>
2017-08-02 09:56:20 -0400

    addressed comments, removed caching

--- src/main/scala/is/hail/methods/LDMatrix.scala
+++ src/main/scala/is/hail/methods/LDMatrix.scala
@@ -2,15 +2,12 @@ package is.hail.methods
 
 import is.hail.distributedmatrix.{BlockMatrixIsDistributedMatrix, DistributedMatrix}
 import is.hail.utils._
-import is.hail.stats.{RegressionUtils}
+import is.hail.stats.RegressionUtils
 import is.hail.variant.{Variant, VariantDataset}
 import org.apache.spark.mllib.linalg.{DenseMatrix, Matrix, Vectors}
 import org.apache.spark.mllib.linalg.distributed.{BlockMatrix, IndexedRow, IndexedRowMatrix}
-import org.apache.spark.storage.StorageLevel
-
 
 object LDMatrix {
-
   /**
     * Computes the LD matrix for the given VDS.
     * @param vds VDS on which to compute Pearson correlation between pairs of variants.
@@ -20,28 +17,27 @@ object LDMatrix {
     val nSamples = vds.nSamples
     val nVariants = vds.countVariants()
 
-    val normalizedHardCalls = vds.rdd.map { case (v, (va, gs)) => (v, RegressionUtils.normalizedHardCalls(gs, nSamples))}
-    val filteredNormalizedHardCalls = normalizedHardCalls.filter{ case (v, opt) => !opt.isEmpty}
-
+    val filteredNormalizedHardCalls = vds.rdd.flatMap { 
+      case (v, (va, gs)) => RegressionUtils.normalizedHardCalls(gs, nSamples).map(x => (v, x))
+    }
+    
     val variantsKept = filteredNormalizedHardCalls.map(_._1).collect()
     assert(variantsKept.isSorted, "ld_matrix: Array of variants is not sorted. This is a bug")
 
-    val normalizedIndexedRows = filteredNormalizedHardCalls.map(_._2.get).zipWithIndex()
+    val normalizedIndexedRows = filteredNormalizedHardCalls.map(_._2).zipWithIndex()
       .map{ case (values, idx) => IndexedRow(idx, Vectors.dense(values))}
-    val normalizedIRM = new IndexedRowMatrix(normalizedIndexedRows)
-    val normalizedBlockMatrix = normalizedIRM.toBlockMatrixDense()
+    val normalizedBlockMatrix = new IndexedRowMatrix(normalizedIndexedRows).toBlockMatrixDense()
 
     val nVariantsKept = variantsKept.length
     val nVariantsDropped = nVariants - nVariantsKept
 
-    info(s"Computing LD Matrix with ${variantsKept.length} variants using $nSamples samples. $nVariantsDropped variants were dropped.")
+    info(s"Computing LD matrix with ${variantsKept.length} variants using $nSamples samples. $nVariantsDropped variants were dropped.")
 
     val localBound = 5000 * 5000
     val nEntries: Long = nVariantsKept * nVariantsKept
+    val nSamplesInverse = 1.0 / nSamples
 
     val computeLocally = optComputeLocally.getOrElse(nEntries <= localBound)
-
-    val nSamplesInverse = 1.0 / nSamples
 
     var indexedRowMatrix: IndexedRowMatrix = null
 
@@ -51,13 +47,10 @@ object LDMatrix {
       indexedRowMatrix =
         BlockMatrixIsDistributedMatrix.from(vds.sparkContext, product, normalizedBlockMatrix.rowsPerBlock,
           normalizedBlockMatrix.colsPerBlock).toIndexedRowMatrix()
-    }
-    else {
+    } else {
       import is.hail.distributedmatrix.DistributedMatrix.implicits._
       val dm = DistributedMatrix[BlockMatrix]
       import dm.ops._
-      normalizedBlockMatrix.persist(StorageLevel.MEMORY_AND_DISK)
-      normalizedBlockMatrix.blocks.count()
       indexedRowMatrix = (normalizedBlockMatrix * normalizedBlockMatrix.t)
         .toIndexedRowMatrix()
     }
@@ -76,7 +69,7 @@ object LDMatrix {
   * @param nSamples Number of samples used to compute this matrix.
   */
 case class LDMatrix(matrix: IndexedRowMatrix, variants: Array[Variant], nSamples: Int) {
-  def toLocalMatrix(): Matrix = {
+  def toLocalMatrix: Matrix = {
     matrix.toBlockMatrixDense().toLocalMatrix()
   }
 }
