3844143df7cc0e10f161ef38e2dbca7c01d7ac7c
Liam Abbott <liam.e.abbott@gmail.com>
2017-08-09 12:40:52 -0400

    incremented to v1.1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cloudtools',
-	  version='1.1.1',
+	  version='1.1.2',
 	  description='Collection of utilities for working on the Google Cloud Platform.',
 	  url='https://github.com/Nealelab/cloudtools',
 	  author='Liam Abbott',
