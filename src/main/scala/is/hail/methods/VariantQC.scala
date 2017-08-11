2bb3665287d9251eb6b7b0312d80f2e3153ee6c3
tpoterba <tpoterba@broadinstitute.org>
2017-08-11 10:48:08 -0400

    Addressed variantqc comment

--- src/main/scala/is/hail/methods/VariantQC.scala
+++ src/main/scala/is/hail/methods/VariantQC.scala
@@ -10,6 +10,76 @@ import org.apache.spark.util.StatCounter
 
 import scala.reflect.classTag
 
+class VariantQCCombiner {
+  var nNotCalled: Int = 0
+  var nHomRef: Int = 0
+  var nHet: Int = 0
+  var nHomVar: Int = 0
+
+  val dpSC = new StatCounter()
+
+  val gqSC: StatCounter = new StatCounter()
+
+  def mergeGT(gt: Int) {
+    (gt: @unchecked) match {
+      case 0 => nHomRef += 1
+      case 1 => nHet += 1
+      case 2 => nHomVar += 1
+    }
+  }
+
+  def skipGT() {
+    nNotCalled += 1
+  }
+
+  def mergeDP(dp: Int) {
+    dpSC.merge(dp)
+  }
+
+  def mergeGQ(gq: Int) {
+    gqSC.merge(gq)
+  }
+
+  def result(): Annotation = {
+    val af = {
+      val refAlleles = nHomRef * 2 + nHet
+      val altAlleles = nHomVar * 2 + nHet
+      divNull(altAlleles, refAlleles + altAlleles)
+    }
+
+    val nCalled = nHomRef + nHet + nHomVar
+    val callrate = divOption(nCalled, nCalled + nNotCalled)
+    val ac = nHet + 2 * nHomVar
+
+    val n = nHomRef + nHet + nHomVar
+    val nAB = nHet
+    val nA = nAB + 2 * nHomRef.min(nHomVar)
+
+    val LH = LeveneHaldane(n, nA)
+    val rExpectedHetFreq = divNull(LH.getNumericalMean, n)
+    val hweP = LH.exactMidP(nAB)
+
+    Annotation(
+      divNull(nCalled, nCalled + nNotCalled),
+      ac,
+      af,
+      nCalled,
+      nNotCalled,
+      nHomRef,
+      nHet,
+      nHomVar,
+      nullIfNot(dpSC.count > 0, dpSC.mean),
+      nullIfNot(dpSC.count > 0, dpSC.stdev),
+      nullIfNot(gqSC.count > 0, gqSC.mean),
+      nullIfNot(gqSC.count > 0, gqSC.stdev),
+      nHet + nHomVar,
+      divNull(nHet, nHomRef + nHet + nHomVar),
+      divNull(nHet, nHomVar),
+      rExpectedHetFreq,
+      hweP)
+  }
+}
+
 object VariantQC {
   val signature = TStruct(
     "callRate" -> TFloat64,
@@ -31,7 +101,6 @@ object VariantQC {
     "pHWE" -> TFloat64)
 
   def apply(vds: GenericDataset, root: String): GenericDataset = {
-    val extract = Genotype.buildGenotypeExtractor(vds.genotypeSignature)
     val (newVAS, insertQC) = vds.vaSignature.insert(VariantQC.signature,
       Parser.parseAnnotationRoot(root, Annotation.VARIANT_HEAD))
     val nSamples = vds.nSamples
@@ -41,73 +110,24 @@ object VariantQC {
       it.map { r =>
         view.setRegion(r.region, r.offset)
 
-        var nNotCalled: Int = 0
-        var nHomRef: Int = 0
-        var nHet: Int = 0
-        var nHomVar: Int = 0
-
-        val dpSC = new StatCounter()
-
-        val gqSC: StatCounter = new StatCounter()
-
+        val comb = new VariantQCCombiner
         var i = 0
         while (i < nSamples) {
           view.setGenotype(i)
-          if (view.hasGT) {
-            val gt = view.getGT
-            (gt: @unchecked) match {
-              case 0 => nHomRef += 1
-              case 1 => nHet += 1
-              case 2 => nHomVar += 1
-            }
-          } else nNotCalled += 1
+          if (view.hasGT)
+            comb.mergeGT(view.getGT)
+          else
+            comb.skipGT()
 
           if (view.hasDP)
-            dpSC.merge(view.getDP)
+            comb.mergeDP(view.getDP)
           if (view.hasGQ)
-            gqSC.merge(view.getGQ)
+            comb.mergeGQ(view.getGQ)
 
           i += 1
         }
-        val af = {
-          val refAlleles = nHomRef * 2 + nHet
-          val altAlleles = nHomVar * 2 + nHet
-          divNull(altAlleles, refAlleles + altAlleles)
-        }
 
-
-        val nCalled = nHomRef + nHet + nHomVar
-        val callrate = divOption(nCalled, nCalled + nNotCalled)
-        val ac = nHet + 2 * nHomVar
-
-        val n = nHomRef + nHet + nHomVar
-        val nAB = nHet
-        val nA = nAB + 2 * nHomRef.min(nHomVar)
-
-        val LH = LeveneHaldane(n, nA)
-        val rExpectedHetFreq = divNull(LH.getNumericalMean, n)
-        val hweP = LH.exactMidP(nAB)
-
-        val qcStruct = Annotation(
-          divNull(nCalled, nCalled + nNotCalled),
-          ac,
-          af,
-          nCalled,
-          nNotCalled,
-          nHomRef,
-          nHet,
-          nHomVar,
-          nullIfNot(dpSC.count > 0, dpSC.mean),
-          nullIfNot(dpSC.count > 0, dpSC.stdev),
-          nullIfNot(gqSC.count > 0, gqSC.mean),
-          nullIfNot(gqSC.count > 0, gqSC.stdev),
-          nHet + nHomVar,
-          divNull(nHet, nHomRef + nHet + nHomVar),
-          divNull(nHet, nHomVar),
-          rExpectedHetFreq,
-          hweP)
-
-        (r.get(0): Annotation) -> (insertQC(r.get(1), qcStruct) -> r.getAs[Iterable[Annotation]](2))
+        (r.get(0): Annotation) -> (insertQC(r.get(1), comb.result()) -> r.getAs[Iterable[Annotation]](2))
       }
     }
     val ord = OrderedRDD.apply(rdd, vds.rdd.orderedPartitioner)(vds.rdd.kOk, classTag[(Annotation, Iterable[Annotation])])
