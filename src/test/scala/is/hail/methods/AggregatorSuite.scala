9a375bfc4cd15c5959a59fd9a5e37700e4ecb880
Jackie Goldstein <jigold@broadinstitute.org>
2017-08-14 13:45:04 -0400

    fixed test

--- src/test/scala/is/hail/methods/AggregatorSuite.scala
+++ src/test/scala/is/hail/methods/AggregatorSuite.scala
@@ -247,13 +247,13 @@ class AggregatorSuite extends SparkSuite {
       vds.annotateVariantsExpr("va = gs.map(g => 5)"))
     TestUtils.interceptFatal("unrealizable type.*Aggregable\\[Genotype\\]")(
       vds.annotateVariantsExpr("va = gs.filter(g => true)"))
-    TestUtils.interceptFatal("unrealizable type.*Aggregable\\[Variant\\]")(
+    TestUtils.interceptFatal("unrealizable type.*Aggregable\\[Variant.*\\]")(
       vds.queryVariants("variants")
     )
     TestUtils.interceptFatal("unrealizable type.*Aggregable\\[String\\]")(
       vds.queryVariants("variants.map(v => v.contig)")
     )
-    TestUtils.interceptFatal("unrealizable type.*Aggregable\\[Variant\\]")(
+    TestUtils.interceptFatal("unrealizable type.*Aggregable\\[Variant.*\\]")(
       vds.queryVariants("variants.filter(v => false)")
     )
     TestUtils.interceptFatal("unrealizable type.*Aggregable\\[String\\]")(
