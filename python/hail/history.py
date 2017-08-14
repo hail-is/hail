fba787bf20a64fc93ec44319e698a5abe0327842
Jackie Goldstein <jigold@broadinstitute.org>
2017-08-14 10:35:11 -0400

    addressed comments

--- python/hail/history.py
+++ python/hail/history.py
@@ -5,43 +5,38 @@ import inspect
 import itertools
 from collections import OrderedDict
 from decorator import decorator
-from hail.utils import hadoop_write, wrap_to_list
+from hail.utils import hadoop_write
 from hail.java import Env
 
 
-def parse_args(f, args, kwargs):
+def parse_args(f, args, kwargs, is_method=True):
     argspec = inspect.getargspec(f)
-    kwargs_not_default = OrderedDict({})
 
-    if argspec.defaults:
-        n_postnl_args = len(argspec.args) - len(argspec.defaults)
-        kwargs_not_default.update(OrderedDict({k:v for k, (v, d) in zip(argspec.args[n_postnl_args:], zip(args[n_postnl_args - 1:], argspec.defaults)) if v != d}))
-        postnl_args = args[:n_postnl_args - 1]
-    else:
-        postnl_args = args
+    arg_names = argspec.args[1:] if is_method else argspec.args
+    defaults = list(argspec.defaults) if argspec.defaults else []
+    n_postnl_args = len(arg_names) - len(defaults)
+    defaults = n_postnl_args * [None] + defaults
 
-    kwargs_not_default.update(OrderedDict({k:v for k,v in kwargs.iteritems()}))
-
-    return postnl_args, kwargs_not_default
+    parsed_args = OrderedDict({k: v for k, (v, d) in zip(arg_names, zip(args, defaults)) if v != d})
+    parsed_args.update(OrderedDict({k:v for k,v in kwargs.iteritems()}))
+    return parsed_args
 
 
 @decorator
 def record_init(func, obj, *args, **kwargs):
-    postnl_args, kwargs_not_default = parse_args(func, args, kwargs)
-    h = History.from_init(type(obj).__name__, postnl_args, kwargs_not_default)
+    parsed_args = parse_args(func, args, kwargs)
+    h = History.from_init(type(obj).__name__, parsed_args)
     func(obj, *args, **kwargs)
-    obj._set_history(h)
+    obj._history = h
 
 
 @decorator
 def record_method(func, obj, *args, **kwargs):
-    postnl_args, kwargs_not_default = parse_args(func, args, kwargs)
+    parsed_args = parse_args(func, args, kwargs)
 
     def set_history(item, index=None, key_name=None):
         if isinstance(item, HistoryMixin):
-            item._set_history(obj
-                              ._get_history()
-                              .add_method(func.__name__, postnl_args, kwargs_not_default, index=index, key_name=key_name))
+            item._history = (obj._history.add_method(func.__name__, parsed_args, index=index, key_name=key_name))
 
     result = func(obj, *args, **kwargs)
     if isinstance(result, dict):
@@ -57,15 +52,15 @@ def record_method(func, obj, *args, **kwargs):
 
 @decorator
 def record_classmethod(func, cls, *args, **kwargs):
-    postnl_args, kwargs_not_default = parse_args(func, args, kwargs)
+    parsed_args = parse_args(func, args, kwargs)
     result = func(cls, *args, **kwargs)
-    result._set_history(History.from_classmethod(cls.__name__, func.__name__, postnl_args, kwargs_not_default))
+    result._history = History.from_classmethod(cls.__name__, func.__name__, parsed_args)
     return result
 
 
 def write_history(path_arg_name, is_dir=False):
     def _write(f, obj, *args, **kwargs):
-        postnl_args, kwargs_not_default = parse_args(f, args, kwargs)
+        parsed_args = parse_args(f, args, kwargs)
         argnames = inspect.getcallargs(f, obj, *args, **kwargs)
         result = f(obj, *args, **kwargs)
 
@@ -76,64 +71,64 @@ def write_history(path_arg_name, is_dir=False):
             output_path += ".history.txt"
 
         (obj._history
-         .add_method(f.__name__, postnl_args, kwargs_not_default)
+         .add_method(f.__name__, parsed_args)
          .write(output_path))
 
         return result
     return decorator(_write)
 
 
-def format_args(arg, stmts):
+def format_args(arg, statements):
     if isinstance(arg, list):
-        return [format_args(a, stmts) for a in arg]
+        return [format_args(a, statements) for a in arg]
     elif isinstance(arg, tuple):
-        return tuple([format_args(a, stmts) for a in arg])
+        return tuple([format_args(a, statements) for a in arg])
     elif isinstance(arg, dict):
-        return {format_args(k, stmts): format_args(v, stmts) for k, v in arg.iteritems()}
+        return {format_args(k, statements): format_args(v, statements) for k, v in arg.iteritems()}
     else:
         if isinstance(arg, HistoryMixin):
-            h = arg._get_history()
-            stmts += remove_dup_stmts(stmts, h.stmts)
+            h = arg._history
+            statements += remove_dup_statements(statements, h.statements)
             return h
         else:
             return arg
 
 
-def remove_dup_stmts(stmts1, stmts2):
-    return [s2 for s1, s2 in itertools.izip_longest(stmts1, stmts2) if s2 and s1 != s2]
+def remove_dup_statements(statements1, statements2):
+    return [s2 for s1, s2 in itertools.izip_longest(statements1, statements2) if s2 and s1 != s2]
 
 
 class History(object):
-    def __init__(self, expr="", stmts=[]):
+    def __init__(self, expr="", statements=[]):
         self.expr = expr
-        self.stmts = stmts
+        self.statements = statements
 
     def set_varid(self, id):
-        return History(id, self.stmts + ['{} = ({})'.format(id, self.expr)])
+        return History(id, self.statements + ['{} = ({})'.format(id, self.expr)])
 
-    def add_method(self, f_name, args, kwargs, index=None, key_name=None):
-        stmts = list(self.stmts) # make a copy
-        f_args = [repr(format_args(a, stmts)) for a in args] + ["{}={}".format(k, repr(format_args(v, stmts))) for k, v in kwargs.iteritems()]
-        expr = self.expr + ("\n    ." + f_name + "(" + ", ".join(f_args) + ")")
+    def add_method(self, f_name, kwargs, index=None, key_name=None):
+        statements = self.statements[:]
+        f_args = ["{}={}".format(k, repr(format_args(v, statements))) for k, v in kwargs.iteritems()]
+        expr = "{expr}\n.{f_name}({f_args})".format(expr=self.expr, f_name=f_name, f_args=", ".join(f_args))
         if index:
             expr += "[{}]".format(index)
         elif key_name:
             expr += "[{}]".format(repr(key_name))
-        return History(expr, stmts)
+        return History(expr, statements)
 
     @staticmethod
-    def from_classmethod(cls_name, f_name, args, kwargs):
-        stmts = []
-        f_args = [repr(format_args(a, stmts)) for a in args] + ["{}={}".format(k, repr(format_args(v, stmts))) for k, v in kwargs.iteritems()]
-        expr = cls_name + "." + f_name + "(" + ", ".join(f_args) + ")"
-        return History(expr, stmts)
+    def from_classmethod(cls_name, f_name, kwargs):
+        statements = []
+        f_args = ["{}={}".format(k, repr(format_args(v, statements))) for k, v in kwargs.iteritems()]
+        expr = "{cls_name}.{f_name}({args})".format(cls_name=cls_name, f_name=f_name, args=", ".join(f_args))
+        return History(expr, statements)
 
     @staticmethod
-    def from_init(cls_name, args, kwargs):
-        stmts = []
-        f_args = [repr(format_args(a, stmts)) for a in args] + ["{}={}".format(k, repr(format_args(v, stmts))) for k, v in kwargs.iteritems()]
-        expr = cls_name + "(" + ", ".join(f_args) + ")"
-        return History(expr, stmts)
+    def from_init(cls_name, kwargs):
+        statements = []
+        f_args = ["{}={}".format(k, repr(format_args(v, statements))) for k, v in kwargs.iteritems()]
+        expr = "{cls_name}({args})".format(cls_name=cls_name, args=", ".join(f_args))
+        return History(expr, statements)
 
     def write(self, file):
         with hadoop_write(file) as f:
@@ -145,13 +140,13 @@ class History(object):
     def __str__(self):
         now = datetime.datetime.now()
         history = "# {}\n# version: {}\n\n".format(now.isoformat(), Env._hc.version)
-        for stmt in self.stmts:
+        for stmt in self.statements:
             history += (stmt + "\n\n")
         history += ("(" + self.expr + ")")
         try:
             import autopep8
             return autopep8.fix_code(history)
-        except:
+        except ImportError:
             return history
 
 
@@ -159,12 +154,6 @@ class HistoryMixin(object):
     def __init__(self):
         self._history = None
 
-    def _set_history(self, history):
-        self._history = history
-
-    def _get_history(self):
-        return self._history
-
     def with_id(self, id):
-        self._set_history(self._history.set_varid(id))
+        self._history = self._history.set_varid(id)
         return self
