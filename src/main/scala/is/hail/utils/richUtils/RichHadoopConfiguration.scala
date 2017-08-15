5b8cbecbccea5e75f3fed9e63dbe9b3bf03e5de2
Daniel King <daniel.zidan.king@gmail.com>
2017-08-15 14:02:28 -0400

    fix latent bug in readObjectFile (#2102)
    * fix latent bug in readObjectFile
    
    * also fix writeobjectfile

--- src/main/scala/is/hail/utils/richUtils/RichHadoopConfiguration.scala
+++ src/main/scala/is/hail/utils/richUtils/RichHadoopConfiguration.scala
@@ -212,22 +212,36 @@ class RichHadoopConfiguration(val hConf: hadoop.conf.Configuration) extends AnyV
   }
 
   def writeObjectFile[T](filename: String)(f: (ObjectOutputStream) => T): T = {
-    val oos = new ObjectOutputStream(create(filename))
+    val os = create(filename)
     try {
-      f(oos)
+      // NB: ObjectOutputStream constructor can throw an exception without closing is
+      val oos = new ObjectOutputStream(create(filename))
+      try {
+        f(oos)
+      } finally {
+        oos.close()
+      }
     } finally {
-      oos.close()
+      // Closable.close() is idempotent
+      os.close()
     }
   }
 
   def fileStatus(filename: String): FileStatus = fileSystem(filename).getFileStatus(new hadoop.fs.Path(filename))
 
   def readObjectFile[T](filename: String)(f: (ObjectInputStream) => T): T = {
-    val ois = new ObjectInputStream(open(filename))
+    val is = open(filename)
     try {
-      f(ois)
+      // NB: ObjectInputStream constructor can throw an exception without closing is
+      val ois = new ObjectInputStream(is)
+      try {
+        f(ois)
+      } finally {
+        ois.close()
+      }
     } finally {
-      ois.close()
+      // Closable.close() is idempotent
+      is.close()
     }
   }
 
