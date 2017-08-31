3163f1cd7c5808eb4b66cb810ef3aefca62c5306
Daniel King <daniel.zidan.king@gmail.com>
2017-08-31 12:25:34 -0400

    Gotta pass --zone to all the gcloud commands (#2169)
    * Gotta pass --zone to all the gcloud commands
    
    * Update test-gcp.sh
    
    * Update test-gcp.sh
    
    * Update test-gcp.sh
    
    * no zone on delete commands

--- scripts/test-gcp.sh
+++ scripts/test-gcp.sh
@@ -15,6 +15,7 @@ DATAPROC_VERSION=$1
 ID=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-z0-9' | head -c 12)
 CLUSTER=cluster-ci-$ID
 MASTER=$CLUSTER-m
+ZONE=us-central1-b
 
 # pick up gcloud, gsutil
 PATH=$PATH:/usr/local/google-cloud-sdk/bin
@@ -25,7 +26,7 @@ function cleanup {
 trap cleanup EXIT SIGINT
 
 gcloud --project broad-ctsa dataproc clusters create $CLUSTER \
-    --zone us-central1-b \
+    --zone $ZONE \
     --master-machine-type n1-standard-2 \
     --master-boot-disk-size 100 \
     --num-workers 2 \
@@ -36,17 +37,19 @@ gcloud --project broad-ctsa dataproc clusters create $CLUSTER \
 
 # copy up necessary files
 gcloud --project broad-ctsa compute scp \
+       --zone $ZONE \
        ./build/libs/hail-all-spark-test.jar \
        ./testng.xml \
        $MASTER:~
 
-gcloud --project broad-ctsa compute ssh $MASTER -- 'mkdir -p src/test'
+gcloud --project broad-ctsa compute ssh --zone ${ZONE} $MASTER -- 'mkdir -p src/test'
 gcloud --project broad-ctsa compute scp --recurse \
+       --zone ${ZONE} \
        ./src/test/resources \
        $MASTER:~/src/test
 
 set +e
-cat <<'EOF' | gcloud --project broad-ctsa compute ssh $MASTER -- bash
+cat <<'EOF' | gcloud --project broad-ctsa compute ssh --zone ${ZONE} $MASTER -- bash
 set -ex
 
 hdfs dfs -mkdir -p src/test
@@ -63,6 +66,6 @@ EOF
 TEST_EXIT_CODE=$?
 set -e
 
-gcloud --project broad-ctsa compute scp --recurse $MASTER:test-output test-output
+gcloud --project broad-ctsa compute scp --zone ${ZONE} --recurse $MASTER:test-output test-output
 
 exit ${TEST_EXIT_CODE}
