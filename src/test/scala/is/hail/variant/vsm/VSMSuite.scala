c9e0d4d053192edda9fe41f5054a73af33770a33
jbloom22 <jbloom@broadinstitute.org>
2017-09-05 19:51:07 -0400

    added 0.1-commithash.vds, interceptFatal for invalid metadata

--- src/test/scala/is/hail/variant/vsm/VSMSuite.scala
+++ src/test/scala/is/hail/variant/vsm/VSMSuite.scala
@@ -340,6 +340,12 @@ class VSMSuite extends SparkSuite {
 
     vds.write(out, overwrite = true)
   }
+  
+  @Test def testInvalidMetadata() {
+    TestUtils.interceptFatal("""invalid metadata""") {
+      hc.readVDS("src/test/resources/0.1-commithash.vds").count()
+    }
+  }
 
   @Test def testAnnotateVariantsKeyTable() {
     forAll(VariantSampleMatrix.gen(hc, VSMSubgen.random)) { vds =>
