b06d75a1712daf5f855e1a5d42e107df4ece629b
Tim Poterba <tpoterba@gmail.com>
2017-09-05 17:14:31 -0400

    Add docs to KeyTable.collect (#2194)
    Fixes #2192

--- python/hail/keytable.py
+++ python/hail/keytable.py
@@ -864,7 +864,21 @@ class KeyTable(object):
 
     @handle_py4j
     def collect(self):
-        """Collect key table as a Python object."""
+        """Collect table to a local list.
+
+        **Examples**
+
+        >>> id_to_sex = {row.ID : row.SEX for row in kt1.collect()}
+
+        **Notes**
+
+        This method should be used on very small tables and as a last resort.
+        It is very slow to convert distributed Java objects to Python
+        (especially serially), and the resulting list may be too large
+        to fit in memory on one machine.
+
+        :rtype: list of :py:class:`.hail.representation.Struct`
+        """
 
         return TArray(self.schema)._convert_to_py(self._jkt.collect())
 
