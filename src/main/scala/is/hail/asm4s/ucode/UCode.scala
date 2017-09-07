bf3885172e6d1b4a9aa91f818b0b129dd74322c9
Daniel King <dking@broadinstitute.org>
2017-09-07 14:47:05 -0400

    wip

--- src/main/scala/is/hail/asm4s/ucode/UCode.scala
+++ src/main/scala/is/hail/asm4s/ucode/UCode.scala
@@ -12,29 +12,10 @@ import scala.language.existentials
 
 trait UCode {
   def emit(il: Growable[AbstractInsnNode]): Unit
-  def toConditional: Option[UCodeConditional] =
-    None
-  def coerceConditional: UCodeConditional =
-    toConditional.getOrElse { throw new RuntimeException(s"${this} cannot be used as a conditional") }
 }
 
 abstract class InsnUCode(insn: AbstractInsnNode) extends UCode {
   def emit(il: Growable[AbstractInsnNode]) { il += insn }
-}
-
-trait UCodeConditional extends UCode {
-  override def emit(il: Growable[AbstractInsnNode]) {
-    val lafter = new LabelNode
-    val (ltrue, lfalse) = emitConditional(il)
-    il += lfalse
-    il += new LdcInsnNode(0)
-    il += new JumpInsnNode(GOTO, lafter)
-    il += ltrue
-    il += new LdcInsnNode(1)
-    il += lafter
-  }
-
-  def emitConditional(il: Growable[AbstractInsnNode]): (LabelNode, LabelNode)
 }
 
 case class Null() extends InsnUCode(new InsnNode(ACONST_NULL))
@@ -42,35 +23,19 @@ case class I32(x: Int) extends InsnUCode(new LdcInsnNode(x))
 case class I64(x: Long) extends InsnUCode(new LdcInsnNode(x))
 case class F32(x: Float) extends InsnUCode(new LdcInsnNode(x))
 case class F64(x: Double) extends InsnUCode(new LdcInsnNode(x))
-case class True() extends UCodeConditional {
-  override def toConditional = Some(this)
-  override def emitConditional(il: Growable[AbstractInsnNode]): (LabelNode, LabelNode) = {
-    val ltrue = new LabelNode
-    val lfalse = new LabelNode
-    il += new JumpInsnNode(GOTO, ltrue)
-    (ltrue, lfalse)
-  }
-}
-case class False() extends UCodeConditional {
-  override def toConditional = Some(this)
-  override def emitConditional(il: Growable[AbstractInsnNode]): (LabelNode, LabelNode) = {
-    val ltrue = new LabelNode
-    val lfalse = new LabelNode
-    il += new JumpInsnNode(GOTO, lfalse)
-    (ltrue, lfalse)
-  }
-}
+case class True() extends InsnUCode(new LdcInsnNode(1))
+case class False() extends InsnUCode(new LdcInsnNode(0))
 case class _If(cond: UCode, cnsq: UCode, altr: UCode) extends UCode {
   def emit(il: Growable[AbstractInsnNode]) {
-    val c = cond.coerceConditional
+    val c = cond
+    val lfalse = new LabelNode
     val lafter = new LabelNode
-    val (ltrue, lfalse) = c.emitConditional(il)
+    c.emit(il)
+    il += new JumpInsnNode(IFEQ, lfalse)
+    cnsq.emit(il)
+    il += new JumpInsnNode(GOTO, lafter)
     il += lfalse
     altr.emit(il)
-    il += new JumpInsnNode(GOTO, lafter)
-    il += ltrue
-    cnsq.emit(il)
-    // fall through
     il += lafter
   }
 }
