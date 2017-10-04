b870f1ac186d21f7ca60a4899df7ab82753f60db
Daniel King <daniel.zidan.king@gmail.com>
2017-10-04 12:18:43 -0400

    Simplify RichRDDByteArray (#2265)
    * Simplify RichRDDByteArray
    
    * fix binary writing
    
    * remove unused vals

--- src/main/scala/is/hail/utils/richUtils/RichRDDByteArray.scala
+++ src/main/scala/is/hail/utils/richUtils/RichRDDByteArray.scala
@@ -1,6 +1,7 @@
 package is.hail.utils.richUtils
 
-import is.hail.io.hadoop.{ByteArrayOutputFormat, BytesOnlyWritable}
+import is.hail.io.hadoop.ByteArrayOutputFormat
+import is.hail.io.hadoop.BytesOnlyWritable
 import is.hail.utils._
 import org.apache.hadoop.io.{BytesWritable, NullWritable}
 import org.apache.spark.rdd.RDD
@@ -9,8 +10,6 @@ import scala.reflect.ClassTag
 
 class RichRDDByteArray(val r: RDD[Array[Byte]]) extends AnyVal {
   def saveFromByteArrays(filename: String, tmpDir: String, header: Option[Array[Byte]] = None, deleteTmpFiles: Boolean = true) {
-    val nullWritableClassTag = implicitly[ClassTag[NullWritable]]
-    val bytesClassTag = implicitly[ClassTag[BytesOnlyWritable]]
     val hConf = r.sparkContext.hadoopConfiguration
 
     val tmpFileName = hConf.getTemporaryFile(tmpDir)
@@ -18,13 +17,12 @@ class RichRDDByteArray(val r: RDD[Array[Byte]]) extends AnyVal {
     val rMapped = r.mapPartitions { iter =>
       val bw = new BytesOnlyWritable()
       iter.map { bb =>
-        bw.set(new BytesWritable(bb))
+        bw.set(bb)
         (NullWritable.get(), bw)
       }
     }
 
-    RDD.rddToPairRDDFunctions(rMapped)(nullWritableClassTag, bytesClassTag, null)
-      .saveAsHadoopFile[ByteArrayOutputFormat](tmpFileName)
+    rMapped.saveAsHadoopFile[ByteArrayOutputFormat](tmpFileName)
 
     header.foreach { str =>
       hConf.writeDataFile(tmpFileName + "/header") { s =>
