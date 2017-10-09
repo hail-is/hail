ace8667d17e68b96b583d379b1a62bc95e8db8e4
Daniel King <daniel.zidan.king@gmail.com>
2017-10-09 13:22:25 -0400

    PC Relate: Add a desired output flag [PCRI-2] (#2249)
    * add desire flag
    
    * fix
    
    * address comments
    
    * remove nbsp

--- src/main/scala/is/hail/variant/VariantDataset.scala
+++ src/main/scala/is/hail/variant/VariantDataset.scala
@@ -668,19 +668,20 @@ class VariantDatasetFunctions(private val vds: VariantSampleMatrix[Genotype]) ex
 
   /**
     *
-    * @param k         the number of principal components to use to distinguish
-    *                  ancestries
-    * @param maf       the minimum individual-specific allele frequency for an
-    *                  allele used to measure relatedness
-    * @param blockSize the side length of the blocks of the block-distributed
-    *                  matrices; this should be set such that atleast three of
-    *                  these matrices fit in memory (in addition to all other
-    *                  objects necessary for Spark and Hail).
+    * @param k          the number of principal components to use to distinguish
+    *                   ancestries
+    * @param maf        the minimum individual-specific allele frequency for an
+    *                   allele used to measure relatedness
+    * @param blockSize  the side length of the blocks of the block-distributed
+    *                   matrices; this should be set such that atleast three of
+    *                   these matrices fit in memory (in addition to all other
+    *                   objects necessary for Spark and Hail).
+    * @param statistics which subset of the four statistics to compute
     */
-  def pcRelate(k: Int, maf: Double, blockSize: Int, minKinship: Double = PCRelate.defaultMinKinship): KeyTable = {
+  def pcRelate(k: Int, maf: Double, blockSize: Int, minKinship: Double = PCRelate.defaultMinKinship, statistics: PCRelate.StatisticSubset = PCRelate.defaultStatisticSubset): KeyTable = {
     requireSplit("PCRelate")
     val pcs = SamplePCA.justScores(vds, k)
-    PCRelate.toKeyTable(vds, pcs, maf, blockSize, minKinship)
+    PCRelate.toKeyTable(vds, pcs, maf, blockSize, minKinship, statistics)
   }
 
   def sampleQC(root: String = "sa.qc", keepStar: Boolean = false): VariantDataset = SampleQC(vds, root, keepStar)
