60206b09c6e9c484f87cda48fc6d3488e231c446
Daniel King <dking@broadinstitute.org>
2017-10-10 21:05:04 -0400

    make test comment not confusing

--- src/test/scala/is/hail/distributedmatrix/GridPartitionerSuite.scala
+++ src/test/scala/is/hail/distributedmatrix/GridPartitionerSuite.scala
@@ -60,31 +60,29 @@ class GridPartitionerSuite extends TestNGSuite {
 
   @Test
   def transposedRectangleMoreRows() {
-    // nb: physical layout is transposed and column major, so:
-    //   0 2 4
-    //   1 3 5
     assertLayout(GridPartitioner(6, 4, 2, transposed=true),
+      // 0 1
+      // 2 3
+      // 4 5
       (0, 0) -> 0,
-      (1, 0) -> 2,
-      (2, 0) -> 4,
       (0, 1) -> 1,
+      (1, 0) -> 2,
       (1, 1) -> 3,
+      (2, 0) -> 4,
       (2, 1) -> 5
     )
   }
 
   @Test
   def transposedRectangleMoreCols() {
-    // nb: physical layout is transposed and column major, so:
-    //   0 3
-    //   1 4
-    //   2 5
     assertLayout(GridPartitioner(4, 6, 2, transposed=true),
+      // 0 1 2
+      // 3 4 5
       (0, 0) -> 0,
-      (1, 0) -> 3,
       (0, 1) -> 1,
-      (1, 1) -> 4,
       (0, 2) -> 2,
+      (1, 0) -> 3,
+      (1, 1) -> 4,
       (1, 2) -> 5
     )
   }
