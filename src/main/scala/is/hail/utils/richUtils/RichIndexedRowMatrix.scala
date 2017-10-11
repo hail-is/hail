ab8d41e646dea0412697683a21f2d06df9968c59
Daniel King <dking@broadinstitute.org>
2017-10-11 17:57:05 -0400

    wip
    wip2
    
    wip3

--- src/main/scala/is/hail/utils/richUtils/RichIndexedRowMatrix.scala
+++ src/main/scala/is/hail/utils/richUtils/RichIndexedRowMatrix.scala
@@ -12,37 +12,78 @@ class RichIndexedRowMatrix(indexedRowMatrix: IndexedRowMatrix) {
 
     val rows = indexedRowMatrix.numRows()
     val cols = indexedRowMatrix.numCols()
+    val partitioner = GridPartitioner(rows, cols, blockSize)
+    val colPartitions = partitioner.colPartitions
     // NB: if excessRows == 0, we never reach the truncatedBlockRow
     val truncatedBlockRow = rows / blockSize
     val truncatedBlockCol = cols / blockSize
     val excessRows = (rows % blockSize).toInt
     val excessCols = (cols % blockSize).toInt
 
-    val blocks = indexedRowMatrix.rows.flatMap { ir =>
-      val i = ir.index / blockSize
-      val ii = ir.index % blockSize
-
-      ir.vector.toArray
-        .grouped(blockSize)
-        .zipWithIndex
-        .map { case (values, blockColumn) =>
-          ((i.toInt, blockColumn), (ii.toInt, values))
-        }
-    }.groupByKey(GridPartitioner(rows, cols, blockSize)).mapValuesWithKey {
-      case ((i, j), it) =>
+    def seqOp(block: Array[Double], row: (Int, Int, Int, Array[Double])): Array[Double] = row match {
+      case (i, j, ii, a) =>
         val rowsInBlock: Int = if (i == truncatedBlockRow) excessRows else blockSize
         val colsInBlock: Int = if (j == truncatedBlockCol) excessCols else blockSize
 
-        val a = new Array[Double](rowsInBlock * colsInBlock)
-        it.foreach{ case (ii, values) =>
-          var jj = 0
-          while (jj < values.length) {
-            a(jj * rowsInBlock + ii) = values(jj)
-            jj += 1
+        var jj = 0
+        while (jj < a.length) {
+          block(jj * rowsInBlock + ii) = a(jj)
+          jj += 1
+        }
+        a
+    }
+
+    def combOp(l: Array[Double], r: Array[Double]): Array[Double] = {
+      var k = 0
+      while (k < l.length) {
+        if (r(k) != 0)
+          l(k) = r(k)
+        k += 1
+      }
+      l
+    }
+
+    val blocks = indexedRowMatrix.rows.flatMap { ir =>
+      val i = ir.index / blockSize
+      val ii = ir.index % blockSize
+      val a = ir.vector.toArray
+      val grouped = new Array[((Int, Int), (Int, Int, Int, Array[Double]))](a.length / blockSize)
+
+      var j = 0
+      while (j < colPartitions) {
+        val group = new Array[Double](blockSize)
+        grouped(j) = ((i.toInt, j.toInt), (i.toInt, j.toInt, ii.toInt, group))
+        if (j != truncatedBlockCol)
+          System.arraycopy(a, j*blockSize, group, 0, blockSize)
+        else
+          System.arraycopy(a, j*blockSize, group, 0, excessCols)
+        j += 1
+      }
+
+      grouped.iterator
+    }.aggregateByKey(new Array[Double](blockSize * blockSize), partitioner)(seqOp, combOp)
+      .mapValuesWithKey { case ((i, j), a) =>
+        if (j == truncatedBlockCol) {
+          if (i == truncatedBlockRow) {
+            val a2 = new Array[Double](excessRows * excessCols)
+            System.arraycopy(a, 0, a2, 0, a2.length)
+            new BDM[Double](excessRows, excessCols, a2)
+          } else {
+            val a2 = new Array[Double](blockSize * excessCols)
+            System.arraycopy(a, 0, a2, 0, a2.length)
+            new BDM[Double](blockSize, excessCols, a2)
+          }
+        } else {
+          if (i == truncatedBlockRow) {
+            val a2 = new Array[Double](excessRows * blockSize)
+            System.arraycopy(a, 0, a2, 0, a2.length)
+            new BDM[Double](excessRows, blockSize, a2)
+          } else {
+            new BDM[Double](blockSize, blockSize, a)
           }
         }
-        new BDM[Double](rowsInBlock, colsInBlock, a)
     }
+
     new BlockMatrix(blocks, blockSize, rows, cols)
   }
 }
