ea8a093e378b4c3bd739422f4b299ff2f3f5e3ba
Daniel King <dking@broadinstitute.org>
2017-10-24 17:46:08 -0400

    implement roundUpAlignment

--- src/main/scala/is/hail/annotations/UnsafeUtils.scala
+++ src/main/scala/is/hail/annotations/UnsafeUtils.scala
@@ -1,11 +1,18 @@
 package is.hail.annotations
 
+import is.hail.asm4s._
 import is.hail.expr.Type
 
 object UnsafeUtils {
   def arrayElementSize(t: Type): Long = roundUpAlignment(t.byteSize, t.alignment)
 
   def roundUpAlignment(offset: Long, alignment: Long): Long = {
+    assert(alignment > 0)
+    assert((alignment & (alignment - 1)) == 0) // power of 2
+    (offset + (alignment - 1)) & ~(alignment - 1)
+  }
+
+  def roundUpAlignment(offset: Code[Long], alignment: Long): Code[Long] = {
     assert(alignment > 0)
     assert((alignment & (alignment - 1)) == 0) // power of 2
     (offset + (alignment - 1)) & ~(alignment - 1)
