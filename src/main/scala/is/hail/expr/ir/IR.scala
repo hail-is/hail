0b9831688fb004e631df811857b5c73a26e77d73
Daniel King <dking@broadinstitute.org>
2017-11-06 12:35:03 -0500

    remove sealed keyword

--- src/main/scala/is/hail/expr/ir/IR.scala
+++ src/main/scala/is/hail/expr/ir/IR.scala
@@ -11,53 +11,53 @@ sealed trait IR extends BaseIR {
   override def copy(newChildren: IndexedSeq[BaseIR]): BaseIR =
     Copy(this, newChildren)
 }
-sealed case class I32(x: Int) extends IR { val typ = TInt32 }
-sealed case class I64(x: Long) extends IR { val typ = TInt64 }
-sealed case class F32(x: Float) extends IR { val typ = TFloat32 }
-sealed case class F64(x: Double) extends IR { val typ = TFloat64 }
-sealed case class True() extends IR { val typ = TBoolean }
-sealed case class False() extends IR { val typ = TBoolean }
+case class I32(x: Int) extends IR { val typ = TInt32 }
+case class I64(x: Long) extends IR { val typ = TInt64 }
+case class F32(x: Float) extends IR { val typ = TFloat32 }
+case class F64(x: Double) extends IR { val typ = TFloat64 }
+case class True() extends IR { val typ = TBoolean }
+case class False() extends IR { val typ = TBoolean }
 
-sealed case class NA(typ: Type) extends IR
-sealed case class MapNA(name: String, value: IR, body: IR, var typ: Type = null) extends IR
-sealed case class IsNA(value: IR) extends IR { val typ = TBoolean }
+case class NA(typ: Type) extends IR
+case class MapNA(name: String, value: IR, body: IR, var typ: Type = null) extends IR
+case class IsNA(value: IR) extends IR { val typ = TBoolean }
 
-sealed case class If(cond: IR, cnsq: IR, altr: IR, var typ: Type = null) extends IR
+case class If(cond: IR, cnsq: IR, altr: IR, var typ: Type = null) extends IR
 
-sealed case class Let(name: String, value: IR, body: IR, var typ: Type = null) extends IR
-sealed case class Ref(name: String, var typ: Type = null) extends IR
+case class Let(name: String, value: IR, body: IR, var typ: Type = null) extends IR
+case class Ref(name: String, var typ: Type = null) extends IR
 
-sealed case class ApplyPrimitive(op: String, args: Array[IR], var typ: Type = null) extends IR {
+case class ApplyPrimitive(op: String, args: Array[IR], var typ: Type = null) extends IR {
   override def toString(): String = s"ApplyPrimitive($op, ${args: IndexedSeq[IR]}, $typ)"
 }
-sealed case class LazyApplyPrimitive(op: String, args: Array[IR], var typ: Type = null) extends IR {
+case class LazyApplyPrimitive(op: String, args: Array[IR], var typ: Type = null) extends IR {
   override def toString(): String = s"LazyApplyPrimitive($op, ${args: IndexedSeq[IR]}, $typ)"
 }
 
-sealed case class Lambda(names: Array[(String, Type)], body: IR, var typ: Type = null) extends IR {
+case class Lambda(names: Array[(String, Type)], body: IR, var typ: Type = null) extends IR {
   override def toString(): String = s"Lambda(${names: IndexedSeq[(String, Type)]}, $body, $typ)"
 }
 
-sealed case class MakeArray(args: Array[IR], var typ: TArray = null) extends IR {
+case class MakeArray(args: Array[IR], var typ: TArray = null) extends IR {
   override def toString(): String = s"MakeArray(${args: IndexedSeq[IR]}, $typ)"
 }
-sealed case class MakeArrayN(len: IR, elementType: Type) extends IR { def typ: TArray = TArray(elementType) }
-sealed case class ArrayRef(a: IR, i: IR, var typ: Type = null) extends IR
-sealed case class ArrayMissingnessRef(a: IR, i: IR) extends IR { val typ: Type = TBoolean }
-sealed case class ArrayLen(a: IR) extends IR { val typ = TInt32 }
-sealed case class ArrayMap(a: IR, lam: IR, var elementTyp: Type = null) extends IR { def typ: TArray = TArray(elementTyp) }
-sealed case class ArrayFold(a: IR, zero: IR, lam: IR, var typ: Type = null) extends IR
+case class MakeArrayN(len: IR, elementType: Type) extends IR { def typ: TArray = TArray(elementType) }
+case class ArrayRef(a: IR, i: IR, var typ: Type = null) extends IR
+case class ArrayMissingnessRef(a: IR, i: IR) extends IR { val typ: Type = TBoolean }
+case class ArrayLen(a: IR) extends IR { val typ = TInt32 }
+case class ArrayMap(a: IR, lam: IR, var elementTyp: Type = null) extends IR { def typ: TArray = TArray(elementTyp) }
+case class ArrayFold(a: IR, zero: IR, lam: IR, var typ: Type = null) extends IR
 
-sealed case class MakeStruct(fields: Array[(String, Type, IR)]) extends IR {
+case class MakeStruct(fields: Array[(String, Type, IR)]) extends IR {
   val typ: TStruct = TStruct(fields.map(x => x._1 -> x._2):_*)
   override def toString(): String =
     s"MakeStruct(${fields: IndexedSeq[(String, Type, IR)]})"
 }
-sealed case class GetField(o: IR, name: String, var typ: Type = null) extends IR
-sealed case class GetFieldMissingness(o: IR, name: String) extends IR { val typ: Type = TBoolean }
+case class GetField(o: IR, name: String, var typ: Type = null) extends IR
+case class GetFieldMissingness(o: IR, name: String) extends IR { val typ: Type = TBoolean }
 
-sealed case class In(i: Int, val typ: Type) extends IR
-sealed case class InMissingness(i: Int) extends IR { val typ: Type = TBoolean }
-sealed case class Out(v: IR) extends IR { val typ = TVoid }
+case class In(i: Int, val typ: Type) extends IR
+case class InMissingness(i: Int) extends IR { val typ: Type = TBoolean }
+case class Out(v: IR) extends IR { val typ = TVoid }
 // FIXME: should be type any
-sealed case class Die(message: String) extends IR { val typ = TVoid }
+case class Die(message: String) extends IR { val typ = TVoid }
