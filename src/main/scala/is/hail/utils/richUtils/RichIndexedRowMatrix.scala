e24fe3974452784482b01d39085632303f223eba
Daniel King <daniel.zidan.king@gmail.com>
2017-11-06 12:27:26 -0500

    Better RichIndexedRowMatrix.toHailBlockMatrix (#2297) for Master (#2364)
    * Better RichIndexedRowMatrix.toHailBlockMatrix (#2297)
    
    * wip
    
    wip2
    
    wip3
    
    * fix
    
    * fix again
    
    * dan is an idiot
    
    * small zero value
    
    * fix
    
    * spaces around *
    
    * no branches in loops
    
    * fix two errors
    
    * improve further
    
    * address comments
    
    Increase diversity of blockSizes to hit all whitebox cases
    
    normalize usage of toInt (remove unnecessary uses, collapse
    repeated uses)
    
    avoid repeated evaluation of fullColPartitions
    
    * fix
    
    Forgot to add the three new columns
    
    * remove extra row from test data
    
    * add test for number of blocks
    
    * Resolve empty blocks issue
    
    We just add a shim RDD that checks for empty partitions and
    replaces them with iterators containing empty matrices
    
    * fix test again
    
    I messed this up the first time. 3 + 10 * 1 + 2 = 15
    
    * address comments
    
    * formatting

--- src/main/scala/is/hail/utils/richUtils/RichIndexedRowMatrix.scala
+++ src/main/scala/is/hail/utils/richUtils/RichIndexedRowMatrix.scala
@@ -1,50 +1,117 @@
 package is.hail.utils.richUtils
 
-import org.apache.spark.mllib.linalg.distributed.IndexedRowMatrix
+import org.apache.spark._
+import org.apache.spark.rdd.RDD
 import breeze.linalg.{DenseMatrix => BDM}
 import is.hail.distributedmatrix._
 import is.hail.utils._
+import org.apache.spark.mllib.linalg.distributed.IndexedRowMatrix
+
+object RichIndexedRowMatrix {
+  private def seqOp(truncatedBlockRow: Int, truncatedBlockCol: Int, excessRows: Int, excessCols: Int, blockSize: Int)
+    (block: Array[Double], row: (Int, Int, Int, Array[Double])): Array[Double] = row match {
+    case (i, j, ii, a) =>
+      val rowsInBlock: Int = if (i == truncatedBlockRow) excessRows else blockSize
+      val colsInBlock: Int = if (j == truncatedBlockCol) excessCols else blockSize
+      val block2 = if (block == null) new Array[Double](rowsInBlock * colsInBlock) else block
+
+      var jj = 0
+      while (jj < a.length) {
+        block2(jj * rowsInBlock + ii) = a(jj)
+        jj += 1
+      }
+      block2
+  }
+
+  private def combOp(l: Array[Double], r: Array[Double]): Array[Double] = {
+    if (l == null)
+      r
+    else if (r == null)
+      l
+    else {
+      var k = 0
+      while (k < l.length) {
+        l(k) += r(k)
+        k += 1
+      }
+      l
+    }
+  }
+}
 
 class RichIndexedRowMatrix(indexedRowMatrix: IndexedRowMatrix) {
+  import RichIndexedRowMatrix._
+
   def toHailBlockMatrix(blockSize: Int = BlockMatrix.defaultBlockSize): BlockMatrix = {
     require(blockSize > 0,
       s"blockSize needs to be greater than 0. blockSize: $blockSize")
 
     val rows = indexedRowMatrix.numRows()
     val cols = indexedRowMatrix.numCols()
+    val partitioner = GridPartitioner(blockSize, rows, cols)
+    val colPartitions = partitioner.colPartitions
     // NB: if excessRows == 0, we never reach the truncatedBlockRow
-    val truncatedBlockRow = rows / blockSize
-    val truncatedBlockCol = cols / blockSize
+    val truncatedBlockRow = (rows / blockSize).toInt
+    val truncatedBlockCol = (cols / blockSize).toInt
     val excessRows = (rows % blockSize).toInt
     val excessCols = (cols % blockSize).toInt
+    val fullColPartitions = if (excessCols == 0) colPartitions else colPartitions - 1
 
     val blocks = indexedRowMatrix.rows.flatMap { ir =>
-      val i = ir.index / blockSize
-      val ii = ir.index % blockSize
+      val i = (ir.index / blockSize).toInt
+      val ii = (ir.index % blockSize).toInt
+      val a = ir.vector.toArray
+      val grouped = new Array[((Int, Int), (Int, Int, Int, Array[Double]))](colPartitions)
 
-      ir.vector.toArray
-        .grouped(blockSize)
-        .zipWithIndex
-        .map { case (values, blockColumn) =>
-          ((i.toInt, blockColumn), (ii.toInt, values))
-        }
-    }.groupByKey(GridPartitioner(blockSize, rows, cols)).mapValuesWithKey {
-      case ((i, j), it) =>
+      var j = 0
+      while (j < fullColPartitions) {
+        val group = new Array[Double](blockSize)
+        grouped(j) = ((i, j), (i, j, ii, group))
+        System.arraycopy(a, j * blockSize, group, 0, blockSize)
+        j += 1
+      }
+      if (excessCols > 0) {
+        val group = new Array[Double](excessCols)
+        grouped(j) = ((i, j), (i, j, ii, group))
+        System.arraycopy(a, j * blockSize, group, 0, excessCols)
+      }
+
+      grouped.iterator
+    }.aggregateByKey(null: Array[Double], partitioner)(
+      seqOp(truncatedBlockRow, truncatedBlockCol, excessRows, excessCols, blockSize), combOp)
+      .mapValuesWithKey { case ((i, j), a) =>
         val rowsInBlock: Int = if (i == truncatedBlockRow) excessRows else blockSize
         val colsInBlock: Int = if (j == truncatedBlockCol) excessCols else blockSize
-
-        val a = new Array[Double](rowsInBlock * colsInBlock)
-        it.foreach{ case (ii, values) =>
-          var jj = 0
-          while (jj < values.length) {
-            a(jj * rowsInBlock + ii) = values(jj)
-            jj += 1
-          }
-        }
         new BDM[Double](rowsInBlock, colsInBlock, a)
     }
-    new BlockMatrix(blocks, blockSize, rows, cols)
+
+    new BlockMatrix(new EmptyPartitionIsAZeroMatrixRDD(blocks), blockSize, rows, cols)
   }
+
 }
 
+private class EmptyPartitionIsAZeroMatrixRDD(blocks: RDD[((Int, Int), BDM[Double])])
+    extends RDD[((Int, Int), BDM[Double])](blocks.sparkContext, Seq[Dependency[_]](new OneToOneDependency(blocks))) {
+  @transient val gp = (blocks.partitioner: @unchecked) match {
+    case Some(p: GridPartitioner) => p
+  }
 
+  def compute(split: Partition, context: TaskContext): Iterator[((Int, Int), BDM[Double])] = {
+    val p = split.asInstanceOf[BlockPartition]
+    val it = blocks.iterator(split, context)
+    if (it.hasNext)
+      it
+    else
+      Iterator.single(p.coords -> BDM.zeros[Double](p.blockSize, p.blockSize))
+  }
+
+  protected def getPartitions: Array[Partition] =
+    blocks.partitions.map { p =>
+      new BlockPartition(p.index, gp.blockSize, gp.blockCoordinates(p.index))
+    }.toArray
+
+  @transient override val partitioner: Option[Partitioner] =
+    Some(gp)
+}
+
+private class BlockPartition(val index: Int, val blockSize: Int, val coords: (Int, Int)) extends Partition {}
