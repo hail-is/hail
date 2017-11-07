469ada517ea764d6de8e08d1f6603e6f0b91a8ac
Daniel King <dking@broadinstitute.org>
2017-11-07 15:58:47 -0500

    propagate root-cause exceptions

--- src/main/scala/is/hail/utils/ErrorHandling.scala
+++ src/main/scala/is/hail/utils/ErrorHandling.scala
@@ -1,19 +1,25 @@
 package is.hail.utils
 
-class HailException(val msg: String, val logMsg: Option[String] = None) extends RuntimeException(msg)
+class HailException(val msg: String, val logMsg: Option[String] = None, cause: Throwable = null) extends RuntimeException(msg, cause)
 
 trait ErrorHandling {
   def fatal(msg: String): Nothing = throw new HailException(msg)
 
-  def fatal(msg: String, t: Truncatable): Nothing = {
+  def fatal(msg: String, t: Truncatable): Nothing =
+    fatal(msg, t, null: Throwable)
+
+  def fatal(msg: String, t: Truncatable, e: Throwable): Nothing = {
     val (screen, logged) = t.strings
-    throw new HailException(format(msg, screen), Some(format(msg, logged)))
+    throw new HailException(format(msg, screen), Some(format(msg, logged)), e)
   }
 
   def fatal(msg: String, t1: Truncatable, t2: Truncatable): Nothing = {
+    fatal(msg, t1, t2, null)
+
+  def fatal(msg: String, t1: Truncatable, t2: Truncatable, e: Throwable): Nothing = {
     val (screen1, logged1) = t1.strings
     val (screen2, logged2) = t2.strings
-    throw new HailException(format(msg, screen1, screen2), Some(format(msg, logged1, logged2)))
+    throw new HailException(format(msg, screen1, screen2), Some(format(msg, logged1, logged2)), e)
   }
 
   def deepestMessage(e: Throwable): String = {
