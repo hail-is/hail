4c1d768dd2d983027065625eca385c81df3b7bf0
Daniel King <dking@broadinstitute.org>
2017-11-07 16:34:16 -0500

    fix

--- src/main/scala/is/hail/expr/FunctionRegistry.scala
+++ src/main/scala/is/hail/expr/FunctionRegistry.scala
@@ -2460,8 +2460,8 @@ object FunctionRegistry {
             Code(stx, sty, b.update(i, z), i.store(i + 1))
           ),
           CompilationHelp.arrayToWrappedArray(b)).asInstanceOf[Code[IndexedSeq[S]]],
-        Code._throw(Code.newInstance[is.hail.utils.HailException, String, Option[String]](
-          s"""Cannot apply operation $name to arrays of unequal length.""".stripMargin, Code.invokeStatic[scala.Option[String], scala.Option[String]]("empty"))))),
+        Code._throw(Code.newInstance[is.hail.utils.HailException, String, Option[String], Throwable](
+          s"""Cannot apply operation $name to arrays of unequal length.""".stripMargin, Code.invokeStatic[scala.Option[String], scala.Option[String], Throwable]("empty", Code._null[Throwable]))))),
       null)
   }
 
