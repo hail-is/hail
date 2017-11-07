70d33961d5af9b3bcabc5dd37476ef3cfb5bc1e1
Daniel King <daniel.zidan.king@gmail.com>
2017-11-07 17:46:03 -0500

    codify isElementDefined (#2394)
    * codify isElementDefined
    
    * (append|store)Annotation (#2396)
    
    * add (append|store)Annotation
    
    This encapsulates the type dispatch necessary to load and store
    annotations.
    
    * improve type annotation on appendAnnotation
    
    * Annotation -> Primitive
    
    * IR Compiler (#2395)
    
    * wip
    
    add TUnit
    
    add some terms
    
    fix up IRSuite
    
    weird shit
    
    wip compiler
    
    wip
    
    adapt SRVB to my uses
    
    wip
    
    remove superfluous stuff
    
    first passing test
    
    wip
    
    * wip
    
    * wip
    
    * remove na stuff
    
    * Revert "remove na stuff"
    
    This reverts commit 62c5e2c632c18c30aea22000ed427f194596e35d.
    
    * fix up
    
    * wip
    
    * wip
    
    * wip
    
    * add missing bits
    
    * implement MapNA
    
    * fix up missingbits
    
    * require a type on the input
    
    * add @unchecked to several casts
    
    * missing a cast in implementation of Compile(MapNA)
    
    * pass missingness as Boolean argument
    
    * add several new tests
    
    * add || to Primitives
    
    * type of In is required
    
    * fix MissingBits
    
    Booleans are represented in JVM as Ints, not Longs
    
    * add Env
    
    * remove some shit
    
    * return fresh bindings
    
    * one approach to missingness desugaring
    
    * wip
    
    * wip
    
    * works maybe?
    
    * ugh
    
    * new compile
    
    * old stuff
    
    * more old stuff
    
    * Revert "more old stuff"
    
    This reverts commit 8d89e634748fd5bd6545291d8a5fe4e22f201260.
    
    * Revert "old stuff"
    
    This reverts commit 533e562a23e10518636537e32e19a0c565a6fa31.
    
    * more old stuff removed
    
    * work
    
    * remove allocaiton
    
    * remove duplicate srvb.addAnnotation
    
    * remove debugging crap
    
    * rename Compile2 to Compile
    
    * fix for BaseIR
    
    * use load/store annotations
    
    * avoid referencing TVoid as Unit
    
    * dummyValue -> defaultValue
    
    * expression -> compile
    
    * bad reference to Compile2
    
    * remove excess spacing and remove IR.Seq
    
    * remove IR.seq
    
    * fixup MakeStruct and add Copy and Compile
    
    * remove lazyPrimitives
    
    * hookup copy and children
    
    * remove sealed keyword
    
    * variety of fixups
    
    * adjust for addAnnotation -> addPrimitive
    
    * remove LazyApplyPrimitive
    
    * remove lambda
    
    * represent casts in IR explicitly
    
    * teach numeric codes to unary_-
    
    * bin-/un-ary ops
    
    * bunch of changes
    
    * fixes

---
+++ src/main/scala/is/hail/expr/ir/Infer.scala
@@ -0,0 +1,107 @@
+package is.hail.expr.ir
+
+import is.hail.utils._
+import is.hail.annotations.MemoryBuffer
+import is.hail.asm4s._
+import is.hail.expr.{TInt32, TInt64, TArray, TContainer, TStruct, TFloat32, TFloat64, TBoolean, Type, TVoid, TFunction, TNumeric}
+import is.hail.annotations.StagedRegionValueBuilder
+
+object Infer {
+  def apply(ir: IR) { apply(ir, new Env[Type]()) }
+
+  def apply(ir: IR, env: Env[Type]) {
+    def infer(ir: IR, env: Env[Type] = env) { apply(ir, env) }
+    ir match {
+      case I32(x) =>
+      case I64(x) =>
+      case F32(x) =>
+      case F64(x) =>
+      case True() =>
+      case False() =>
+
+      case Cast(v, typ) =>
+        infer(v)
+        assert(Casts.valid(v.typ, typ))
+
+      case NA(t) =>
+      case x@MapNA(name, value, body, _) =>
+        infer(value)
+        infer(body, env = env.bind(name, value.typ))
+        x.typ = body.typ
+      case IsNA(v) =>
+        infer(v)
+
+      case x@If(cond, cnsq, altr, _) =>
+        infer(cond)
+        infer(cnsq)
+        infer(altr)
+        assert(cond.typ == TBoolean)
+        assert(cnsq.typ == altr.typ, s"${cnsq.typ}, ${altr.typ}")
+        x.typ = cnsq.typ
+
+      case x@Let(name, value, body, _) =>
+        infer(value)
+        infer(body, env = env.bind(name, value.typ))
+        println("let body: " + body.typ)
+        x.typ = body.typ
+      case x@Ref(_, _) =>
+        x.typ = env.lookup(x)
+      case x@ApplyBinaryPrimOp(op, l, r, _) =>
+        infer(l)
+        infer(r)
+        x.typ = BinaryOp.inferReturnType(op, l.typ, r.typ)
+      case x@ApplyUnaryPrimOp(op, v, _) =>
+        infer(v)
+        x.typ = UnaryOp.inferReturnType(op, v.typ)
+      case x@MakeArray(args, _) =>
+        args.map(infer(_))
+        val t = args.head.typ
+        args.map(_.typ).zipWithIndex.tail.foreach { case (x, i) => assert(x == t, s"at position $i type mismatch: $t $x") }
+        x.typ = TArray(t)
+      case MakeArrayN(len, _) =>
+        infer(len)
+        assert(len.typ == TInt32)
+      case x@ArrayRef(a, i, _) =>
+        infer(a)
+        infer(i)
+        assert(i.typ == TInt32)
+        x.typ = a.typ.asInstanceOf[TArray].elementType
+      case ArrayMissingnessRef(a, i) =>
+        infer(a)
+        infer(i)
+        assert(i.typ == TInt32)
+      case ArrayLen(a) =>
+        infer(a)
+        assert(a.typ.isInstanceOf[TArray])
+      case x@ArrayMap(a, name, body, _) =>
+        infer(a)
+        val tarray = a.typ.asInstanceOf[TArray]
+        infer(body, env = env.bind(name, tarray.elementType))
+        x.elementTyp = body.typ
+      case x@ArrayFold(a, zero, accumName, valueName, body, _) =>
+        infer(a)
+        val tarray = a.typ.asInstanceOf[TArray]
+        infer(zero)
+        infer(body, env.bind(accumName -> zero.typ, valueName -> tarray.elementType))
+        assert(body.typ == zero.typ)
+        x.typ = zero.typ
+      case MakeStruct(fields) =>
+        fields.map { case (_, typ, v) =>
+          infer(v)
+          assert(typ == v.typ)
+        }
+      case x@GetField(o, name, _) =>
+        infer(o)
+        val t = o.typ.asInstanceOf[TStruct]
+        assert(t.index(name).nonEmpty)
+        x.typ = t.field(name).typ
+      case GetFieldMissingness(o, name) =>
+        infer(o)
+        val t = o.typ.asInstanceOf[TStruct]
+        assert(t.index(name).nonEmpty)
+      case In(i, typ) =>
+        assert(typ != null)
+      case InMissingness(i) =>
+      case Die(msg) =>
+    } }
+}
