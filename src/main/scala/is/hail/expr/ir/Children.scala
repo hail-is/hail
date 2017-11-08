aceb4874329bece213f4d314fc66e281e7a38f4b
Daniel King <daniel.zidan.king@gmail.com>
2017-11-08 09:23:12 -0500

    import postfixOps to remove warning (#2386)
    * import postfixOps to remove warning
    
    * remove rogue code
    
    * implement ior (#2391)
    
    * implement ior
    
    * implicit conversion from Code[Boolean] to CodeInt (#2397)
    
    * implicit conversion from Code[Boolean] to CodeInt
    
    * add toI and toZ in place of implicit conversion
    
    * Create StagedBitSet (#2387)
    
    * move StagedBitSet to own file
    
    * add tests
    
    * remove redundant := syntax from LocalRef
    
    * stop using named locals
    
    * use new toI.toL pattern
    
    * add some tests with large numbers of bits
    
    * fix two tests
    
    * implement StagedRegionValueBuilder.addAnnotation (#2388)
    
    * teach SRVB to addAnnotation
    
    * teach SRVB how to addBoolean
    
    * use new .toI.toB pattern
    
    * addAnnotation -> addPrimitive
    
    * correct tests
    
    * fix test name
    
    * move getStatic and putStatic to Code (#2389)
    
    * move getStatic and putStatic to Code
    
    This weakens the requirements for using them, obviating the
    need for a FunctionBuilder
    
    * add 3,4,5-ary fb constructors (#2392)
    
    * add 3,4,5-ary fb constructors
    
    * allow custom naming of locals (#2393)
    
    * allow custom naming of locals
    
    * codify isElementDefined (#2394)
    
    * codify isElementDefined
    
    * (append|store)Annotation (#2396)
    
    * add (append|store)Annotation
    
    This encapsulates the type dispatch necessary to load and store
    annotations.
    
    * improve type annotation on appendAnnotation
    
    * Annotation -> Primitive
    
    * IR Compiler (#2395)
    
    * wip
    
    add TUnit
    
    add some terms
    
    fix up IRSuite
    
    weird shit
    
    wip compiler
    
    wip
    
    adapt SRVB to my uses
    
    wip
    
    remove superfluous stuff
    
    first passing test
    
    wip
    
    * wip
    
    * wip
    
    * remove na stuff
    
    * Revert "remove na stuff"
    
    This reverts commit 62c5e2c632c18c30aea22000ed427f194596e35d.
    
    * fix up
    
    * wip
    
    * wip
    
    * wip
    
    * add missing bits
    
    * implement MapNA
    
    * fix up missingbits
    
    * require a type on the input
    
    * add @unchecked to several casts
    
    * missing a cast in implementation of Compile(MapNA)
    
    * pass missingness as Boolean argument
    
    * add several new tests
    
    * add || to Primitives
    
    * type of In is required
    
    * fix MissingBits
    
    Booleans are represented in JVM as Ints, not Longs
    
    * add Env
    
    * remove some shit
    
    * return fresh bindings
    
    * one approach to missingness desugaring
    
    * wip
    
    * wip
    
    * works maybe?
    
    * ugh
    
    * new compile
    
    * old stuff
    
    * more old stuff
    
    * Revert "more old stuff"
    
    This reverts commit 8d89e634748fd5bd6545291d8a5fe4e22f201260.
    
    * Revert "old stuff"
    
    This reverts commit 533e562a23e10518636537e32e19a0c565a6fa31.
    
    * more old stuff removed
    
    * work
    
    * remove allocaiton
    
    * remove duplicate srvb.addAnnotation
    
    * remove debugging crap
    
    * rename Compile2 to Compile
    
    * fix for BaseIR
    
    * use load/store annotations
    
    * avoid referencing TVoid as Unit
    
    * dummyValue -> defaultValue
    
    * expression -> compile
    
    * bad reference to Compile2
    
    * remove excess spacing and remove IR.Seq
    
    * remove IR.seq
    
    * fixup MakeStruct and add Copy and Compile
    
    * remove lazyPrimitives
    
    * hookup copy and children
    
    * remove sealed keyword
    
    * variety of fixups
    
    * adjust for addAnnotation -> addPrimitive
    
    * remove LazyApplyPrimitive
    
    * remove lambda
    
    * represent casts in IR explicitly
    
    * teach numeric codes to unary_-
    
    * bin-/un-ary ops
    
    * bunch of changes
    
    * fixes
    
    * Changed uses loadAnnotation => loadPrimitive.

---
+++ src/main/scala/is/hail/expr/ir/Children.scala
@@ -0,0 +1,56 @@
+package is.hail.expr.ir
+
+import is.hail.expr.BaseIR
+
+object Children {
+  private def none: IndexedSeq[BaseIR] = Array.empty[BaseIR]
+  def apply(x: IR): IndexedSeq[BaseIR] = x match {
+    case I32(x) => none
+    case I64(x) => none
+    case F32(x) => none
+    case F64(x) => none
+    case True() => none
+    case False() => none
+    case NA(typ) => none
+    case MapNA(name, value, body, typ) =>
+      Array(value, body)
+    case IsNA(value) =>
+      Array(value)
+    case If(cond, cnsq, altr, typ) =>
+      Array(cond, cnsq, altr)
+    case Let(name, value, body, typ) =>
+      Array(value, body)
+    case Ref(name, typ) =>
+      none
+    case ApplyBinaryPrimOp(op, l, r, typ) =>
+      Array(l, r)
+    case ApplyUnaryPrimOp(op, x, typ) =>
+      Array(x)
+    case MakeArray(args, typ) =>
+      args
+    case MakeArrayN(len, elementType) =>
+      Array(len)
+    case ArrayRef(a, i, typ) =>
+      Array(a, i)
+    case ArrayMissingnessRef(a, i) =>
+      Array(a, i)
+    case ArrayLen(a) =>
+      Array(a)
+    case ArrayMap(a, name, body, elementTyp) =>
+      Array(a, body)
+    case ArrayFold(a, zero, accumName, valueName, body, typ) =>
+      Array(a, zero, body)
+    case MakeStruct(fields) =>
+      fields.map(_._3)
+    case GetField(o, name, typ) =>
+      Array(o)
+    case GetFieldMissingness(o, name) =>
+      Array(o)
+    case In(i, typ) =>
+      none
+    case InMissingness(i) =>
+      none
+    case Die(message) =>
+      none
+  }
+}
---
+++ src/main/scala/is/hail/expr/ir/Compile.scala
@@ -0,0 +1,324 @@
+package is.hail.expr.ir
+
+import is.hail.annotations.{MemoryBuffer, StagedRegionValueBuilder}
+import is.hail.asm4s._
+import is.hail.expr
+import is.hail.expr.{TArray, TBoolean, TContainer, TFloat32, TFloat64, TInt32, TInt64, TStruct}
+import is.hail.utils._
+import org.objectweb.asm.Opcodes._
+import org.objectweb.asm.tree._
+
+import scala.language.existentials
+
+object Compile {
+  private def defaultValue(t: expr.Type): Code[_] = t match {
+    case TBoolean => false
+    case TInt32 => 0
+    case TInt64 => 0L
+    case TFloat32 => 0.0f
+    case TFloat64 => 0.0
+    case _ => 0L // reference types
+  }
+  private def typeToTypeInfo(t: expr.Type): TypeInfo[_] = t match {
+    case TInt32 => typeInfo[Int]
+    case TInt64 => typeInfo[Long]
+    case TFloat32 => typeInfo[Float]
+    case TFloat64 => typeInfo[Double]
+    case TBoolean => typeInfo[Boolean]
+    case _ => typeInfo[Long] // reference types
+  }
+
+  type E = Env[(TypeInfo[_], Code[Boolean], Code[_])]
+
+  def apply(ir: IR, fb: FunctionBuilder[_]) {
+    apply(ir, fb, new Env())
+  }
+
+  def apply(ir: IR, fb: FunctionBuilder[_], env: E) {
+    val (dov, mv, vv) = compile(ir, fb, env, new StagedBitSet(fb))
+    typeToTypeInfo(ir.typ) match { case ti: TypeInfo[t] =>
+      fb.emit(Code(dov, mv.mux(
+        Code._throw(Code.newInstance[RuntimeException, String]("cannot return empty")),
+        coerce[t](vv))))
+    }
+  }
+
+  private def present(x: Code[_]): (Code[Unit], Code[Boolean], Code[_]) =
+    (Code._empty, const(false), x)
+  private def tcoerce[T <: expr.Type](x: expr.Type): T = x.asInstanceOf[T]
+
+  // the return value is interpreted as: (precompute, missingness, value)
+  // rules:
+  //  1. evaluate each returned Code[_] at most once
+  //  2. evaluate precompute *on all static code-paths* leading to missingness or value
+  //  3. gaurd the the evaluation of value by missingness
+  //
+  // JVM gotcha:
+  //  a variable must be initialized on all static code-paths to its use (ergo defaultValue)
+  def compile(ir: IR, fb: FunctionBuilder[_], env: E, mb: StagedBitSet): (Code[Unit], Code[Boolean], Code[_]) = {
+    val region = fb.getArg[MemoryBuffer](1).load()
+    def compile(ir: IR, fb: FunctionBuilder[_] = fb, env: E = env, mb: StagedBitSet = mb): (Code[Unit], Code[Boolean], Code[_]) =
+      Compile.compile(ir, fb, env, mb)
+    ir match {
+      case I32(x) =>
+        present(const(x))
+      case I64(x) =>
+        present(const(x))
+      case F32(x) =>
+        present(const(x))
+      case F64(x) =>
+        present(const(x))
+      case True() =>
+        present(const(true))
+      case False() =>
+        present(const(false))
+
+      case Cast(v, typ) =>
+        val (dov, mv, vv) = compile(v)
+        val cast = Casts.get(v.typ, typ)
+        (dov, mv, cast(vv))
+
+      case NA(typ) =>
+        (Code._empty, const(true), defaultValue(typ))
+      case IsNA(v) =>
+        val (dov, mv, _) = compile(v)
+        (dov, const(false), mv)
+      case MapNA(name, value, body, typ) =>
+        val vti = typeToTypeInfo(value.typ)
+        val bti = typeToTypeInfo(typ)
+        val mx = mb.newBit()
+        val x = fb.newLocal(name)(vti).asInstanceOf[LocalRef[Any]]
+        val mout = mb.newBit()
+        val out = fb.newLocal(name)(bti).asInstanceOf[LocalRef[Any]]
+        val (dovalue, mvalue, vvalue) = compile(value)
+        val bodyenv = env.bind(name -> (vti, mx, x))
+        val (dobody, mbody, vbody) = compile(body, env = bodyenv)
+        val setup = Code(
+          dovalue,
+          mx := mvalue,
+          mx.mux(
+            Code(mout := true, out := defaultValue(typ)),
+            Code(x := vvalue, dobody, mout := mbody, out := vbody)))
+
+        (setup, mout, out)
+
+      case expr.ir.If(cond, cnsq, altr, typ) =>
+        val (docond, mcond, vcond) = compile(cond)
+        val xvcond = mb.newBit()
+        val out = fb.newLocal()(typeToTypeInfo(typ)).asInstanceOf[LocalRef[Any]]
+        val mout = mb.newBit()
+        val (docnsq, mcnsq, vcnsq) = compile(cnsq)
+        val (doaltr, maltr, valtr) = compile(altr)
+        val setup = Code(
+          docond,
+          mcond.mux(
+            Code(mout := true, out := defaultValue(typ)),
+            Code(
+              xvcond := coerce[Boolean](vcond),
+              coerce[Boolean](xvcond).mux(
+                Code(docnsq, mout := mcnsq, out := vcnsq),
+                Code(doaltr, mout := maltr, out := valtr)))))
+
+        (setup, mout, out)
+
+      case expr.ir.Let(name, value, body, typ) =>
+        val vti = typeToTypeInfo(value.typ)
+        val mx = mb.newBit()
+        val x = fb.newLocal(name)(vti).asInstanceOf[LocalRef[Any]]
+        val (dovalue, mvalue, vvalue) = compile(value)
+        val bodyenv = env.bind(name -> (vti, mx, x))
+        val (dobody, mbody, vbody) = compile(body, env = bodyenv)
+        val setup = Code(
+          dovalue,
+          mx := mvalue,
+          x := vvalue,
+          dobody)
+
+        (setup, mbody, vbody)
+      case Ref(name, typ) =>
+        val ti = typeToTypeInfo(typ)
+        val (t, m, v) = env.lookup(name)
+        assert(t == ti, s"$name type annotation, $typ, doesn't match typeinfo: $ti")
+        (Code._empty, m, v)
+
+      case ApplyBinaryPrimOp(op, l, r, typ) =>
+        val (dol, ml, vl) = compile(l)
+        val (dor, mr, vr) = compile(r)
+        (Code(dol, dor),
+          ml || mr,
+          BinaryOp.compile(op, l.typ, r.typ, vl, vr))
+      case ApplyUnaryPrimOp(op, x, typ) =>
+        val (dox, mx, vx) = compile(x)
+        (dox, mx, UnaryOp.compile(op, x.typ, vx))
+
+      case MakeArray(args, typ) =>
+        val srvb = new StagedRegionValueBuilder(fb, typ)
+        val addElement = srvb.addPrimitive(typ.elementType)
+        val mvargs = args.map(compile(_))
+        present(Code(
+          srvb.start(args.length, init = true),
+          Code(mvargs.map { case (dov, m, v) =>
+            Code(dov, m.mux(srvb.setMissing(), addElement(v)), srvb.advance())
+          }: _*),
+          srvb.offset))
+      case x@MakeArrayN(len, elementType) =>
+        val srvb = new StagedRegionValueBuilder(fb, x.typ)
+        val (dolen, mlen, vlen) = compile(len)
+        (dolen,
+          mlen,
+          Code(srvb.start(coerce[Int](vlen), init = true),
+            srvb.offset))
+      case ArrayRef(a, i, typ) =>
+        val ti = typeToTypeInfo(typ)
+        val tarray = TArray(typ)
+        val ati = typeToTypeInfo(tarray).asInstanceOf[TypeInfo[Long]]
+        val (doa, ma, va) = compile(a)
+        val (doi, mi, vi) = compile(i)
+        val xma = mb.newBit()
+        val xa = fb.newLocal()(ati)
+        val xi = fb.newLocal[Int]
+        val xmi = mb.newBit()
+        val xmv = mb.newBit()
+        val setup = Code(
+          doa,
+          xma := ma,
+          xa := coerce[Long](xma.mux(defaultValue(tarray), va)),
+          doi,
+          xmi := mi,
+          xi := coerce[Int](xmi.mux(defaultValue(TInt32), vi)),
+          xmv := xma || xmi || !tarray.isElementDefined(region, xa, xi))
+
+        (setup, xmv, region.loadPrimitive(typ)(tarray.loadElement(region, xa, xi)))
+      case ArrayMissingnessRef(a, i) =>
+        val tarray = tcoerce[TArray](a.typ)
+        val ati = typeToTypeInfo(tarray).asInstanceOf[TypeInfo[Long]]
+        val (doa, ma, va) = compile(a)
+        val (doi, mi, vi) = compile(i)
+        present(Code(
+          doa,
+          doi,
+          ma || mi || !tarray.isElementDefined(region, coerce[Long](va), coerce[Int](vi))))
+      case ArrayLen(a) =>
+        val (doa, ma, va) = compile(a)
+        (doa, ma, TContainer.loadLength(region, coerce[Long](va)))
+      case x@ArrayMap(a, name, body, elementTyp) =>
+        val tin = a.typ.asInstanceOf[TArray]
+        val tout = x.typ.asInstanceOf[TArray]
+        val srvb = new StagedRegionValueBuilder(fb, tout)
+        val addElement = srvb.addPrimitive(tout.elementType)
+        val eti = typeToTypeInfo(elementTyp).asInstanceOf[TypeInfo[Any]]
+        val xa = fb.newLocal[Long]("am_a")
+        val xmv = mb.newBit()
+        val xvv = fb.newLocal(name)(eti)
+        val i = fb.newLocal[Int]("am_i")
+        val len = fb.newLocal[Int]("am_len")
+        val out = fb.newLocal[Long]("am_out")
+        val bodyenv = env.bind(name -> (eti, xmv, xvv))
+        val lmissing = new LabelNode()
+        val lnonmissing = new LabelNode()
+        val ltop = new LabelNode()
+        val lnext = new LabelNode()
+        val lend = new LabelNode()
+        val (doa, ma, va) = compile(a)
+        val (dobody, mbody, vbody) = compile(body, env = bodyenv)
+
+        (doa, ma, Code(
+          xa := coerce[Long](va),
+          len := TContainer.loadLength(region, xa),
+          i := 0,
+          srvb.start(len, init = true),
+          Code.whileLoop(i < len,
+            xmv := !tin.isElementDefined(region, xa, i),
+            xvv := xmv.mux(
+              defaultValue(elementTyp),
+              region.loadPrimitive(tin.elementType)(tin.loadElement(region, xa, i))),
+            dobody,
+            mbody.mux(srvb.setMissing(), addElement(vbody)),
+            srvb.advance(),
+            i := i + 1),
+          srvb.offset))
+      case ArrayFold(a, zero, name1, name2, body, typ) =>
+        val tarray = a.typ.asInstanceOf[TArray]
+        val tti = typeToTypeInfo(typ)
+        val eti = typeToTypeInfo(tarray.elementType)
+        val xma = mb.newBit()
+        val xa = fb.newLocal[Long]("af_array")
+        val xmv = mb.newBit()
+        val xvv = fb.newLocal(name2)(eti).asInstanceOf[LocalRef[Any]]
+        val xmout = mb.newBit()
+        val xvout = fb.newLocal(name1)(tti).asInstanceOf[LocalRef[Any]]
+        val i = fb.newLocal[Int]("af_i")
+        val len = fb.newLocal[Int]("af_len")
+        val bodyenv = env.bind(
+          name1 -> (tti, xmout, xvout.load()),
+          name2 -> (eti, xmv, xvv.load()))
+        val lmissing = new LabelNode()
+        val lnonmissing = new LabelNode()
+        val ltop = new LabelNode()
+        val lnext = new LabelNode()
+        val lend = new LabelNode()
+        val (doa, ma, va) = compile(a)
+        val (dozero, mzero, vzero) = compile(zero)
+        val (dobody, mbody, vbody) = compile(body, env = bodyenv)
+        val setup = Code(
+          doa,
+          ma.mux(
+            Code(xmout := true, xvout := defaultValue(typ)),
+            Code(
+              xa := coerce[Long](va),
+              len := TContainer.loadLength(region, xa),
+              i := 0,
+              dozero,
+              xmout := mzero,
+              xvout := xmout.mux(defaultValue(typ), vzero),
+              Code.whileLoop(i < len,
+                xmv := !tarray.isElementDefined(region, xa, i),
+                xvv := xmv.mux(
+                  defaultValue(tarray.elementType),
+                  region.loadPrimitive(tarray.elementType)(tarray.loadElement(region, xa, i))),
+                dobody,
+                xmout := mbody,
+                xvout := xmout.mux(defaultValue(typ), vbody),
+                i := i + 1))))
+        (setup, xmout, xvout)
+
+      case MakeStruct(fields) =>
+        val t = TStruct(fields.map { case (name, t, _) => (name, t) }: _*)
+        val initializers = fields.map { case (_, t, v) => (t, compile(v)) }
+        val srvb = new StagedRegionValueBuilder(fb, t)
+        present(Code(
+          srvb.start(false),
+          Code(initializers.map { case (t, (dov, mv, vv)) =>
+            Code(
+              dov,
+              mv.mux(srvb.setMissing(), srvb.addPrimitive(t)(vv)),
+              srvb.advance()) }: _*),
+          srvb.offset))
+      case GetField(o, name, _) =>
+        val t = o.typ.asInstanceOf[TStruct]
+        val fieldIdx = t.fieldIdx(name)
+        val (doo, mo, vo) = compile(o)
+        val xmo = mb.newBit()
+        val xo = fb.newLocal[Long]
+        val setup = Code(
+          doo,
+          xmo := mo,
+          xo := coerce[Long](xmo.mux(defaultValue(t), vo)))
+        (setup,
+          xmo || !t.isFieldDefined(region, xo, fieldIdx),
+          region.loadPrimitive(t)(t.fieldOffset(xo, fieldIdx)))
+      case GetFieldMissingness(o, name) =>
+        val t = o.typ.asInstanceOf[TStruct]
+        val fieldIdx = t.fieldIdx(name)
+        val (doo, mo, vo) = compile(o)
+        present(Code(doo, mo || !t.isFieldDefined(region, coerce[Long](vo), fieldIdx)))
+
+      case In(i, typ) =>
+        (Code._empty, fb.getArg[Boolean](i*2 + 3), fb.getArg(i*2 + 2)(typeToTypeInfo(typ)))
+      case InMissingness(i) =>
+        present(fb.getArg[Boolean](i*2 + 3))
+      case Die(m) =>
+        present(Code._throw(Code.newInstance[RuntimeException, String](m)))
+    }
+  }
+}
