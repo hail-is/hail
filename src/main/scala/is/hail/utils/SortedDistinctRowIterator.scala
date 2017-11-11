693bc5cd9bc73ad63e7252dd03daa78aee97b835
Daniel King <dking@broadinstitute.org>
2017-11-11 13:58:52 -0500

    simplify SortedDistinctRowIterator

--- src/main/scala/is/hail/utils/SortedDistinctRowIterator.scala
+++ src/main/scala/is/hail/utils/SortedDistinctRowIterator.scala
@@ -12,34 +12,13 @@ object SortedDistinctRowIterator {
 class SortedDistinctRowIterator(ort: OrderedRDD2Type, it: Iterator[RegionValue]) extends Iterator[RegionValue] {
   private val bit = it.buffered
   private val wrv: WritableRegionValue = WritableRegionValue(ort.rowType)
-  private var haveNext = false
-  private var stale = false
 
-  if (bit.hasNext) {
-    haveNext = true
-    wrv.set(bit.next())
-  }
-
-  private def advance() {
-    while (bit.hasNext && ort.kInRowOrd.compare(wrv.value, bit.head) == 0)
-      bit.next()
-    haveNext = bit.hasNext
-    if (haveNext)
-      wrv.set(bit.next())
-    stale = false
-  }
-
-  override def hasNext: Boolean = {
-    if (stale)
-      advance()
-    haveNext
-  }
+  override def hasNext: Boolean = bit.hasNext
 
   override def next(): RegionValue = {
-    if (stale)
-      advance()
-    stale = true
-    assert(haveNext)
+    wrv.set(bit.next())
+    while (bit.hasNext && ort.kInRowOrd.compare(wrv.value, bit.head) == 0)
+      bit.next()
     wrv.value
   }
 }
