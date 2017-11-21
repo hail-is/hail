b06d6a416e9f7d974c58c9f1675d8a1b32f5f96c
Daniel King <daniel.zidan.king@gmail.com>
2017-11-21 16:34:10 -0500

    An approach to views (#2451)
    * add variant view
    
    * fix asserts
    
    * use TVariant instead of TStruct
    
    * add AltAlleleView and ArrayView
    
    * move alt allele to own file
    
    * rename AltAlleleView, separate ConcreteAltAllele from AltAllele
    
    * make RegionValueAltAllele an AltAllele
    
    * create concrete variant view
    
    wip
    
    still need to create an interface that mimics IndexedSeq[AltAllele]
    but that doesnt allocate if no copying operations are performed
    
    * add ConcreteVariant.copy
    
    * fix tests
    
    * fixed maybe
    
    * fix
    
    * fix
    
    * taking out the garbage
    
    * missing files
    
    * fixes
    
    * fix names
    
    * fix
    
    * clean up
    
    * some fixup
    
    * add comment
    
    * simplify
    
    * some clean up
    
    * slim down
    
    * fixes
    
    * fix JSON import
    
    * also fix Variant importing
    
    * fix reading ld matrix metadata
    
    * lazy load altAlleles
    
    * more fixes
    
    * fix
    
    * address tpoterbas comments
    
    * delete ArrayView.apply
    
    * revert
    
    * revert
    
    * revert
    
    * remove irrelevant stuff

---
+++ src/main/scala/is/hail/variant/RegionValueVariant.scala
@@ -0,0 +1,68 @@
+package is.hail.variant
+
+import is.hail.utils._
+
+import is.hail.annotations._
+import is.hail.expr._
+
+class RegionValueVariant(tv: TVariant) extends IVariant with View {
+  private val t: TStruct = tv.representation.asInstanceOf[TStruct]
+  private var region: MemoryBuffer = _
+  private var offset: Long = _
+  private val contigIdx: Int = t.fieldIdx("contig")
+  private val startIdx: Int = t.fieldIdx("start")
+  private val refIdx: Int = t.fieldIdx("ref")
+  private val altAllelesIdx: Int = t.fieldIdx("altAlleles")
+  private val tAltAlleles: TArray = t.fieldType(altAllelesIdx).asInstanceOf[TArray]
+  private val tAltAllele: TAltAllele = tAltAlleles.elementType.asInstanceOf[TAltAllele]
+  private val altAllelesView = new ArrayView(tAltAlleles, new RegionValueAltAllele(tAltAllele))
+
+  assert(t.fieldType(contigIdx).required)
+  assert(t.fieldType(startIdx).required)
+  assert(t.fieldType(refIdx).required)
+  assert(tAltAlleles.required)
+  assert(tAltAlleles.elementType.required)
+
+  def setRegion(region: MemoryBuffer, offset: Long) {
+    this.region = region
+    this.offset = offset
+    cachedContig = null
+    cachedRef = null
+  }
+
+  def getOffset(): Long = offset
+
+  private var cachedContig: String = null
+  private var cachedRef: String = null
+  private var cachedAltAlleles: IndexedSeq[AltAllele] = null
+
+  override def contig(): String = {
+    if (cachedContig == null)
+      cachedContig = TString.loadString(region, t.loadField(region, offset, contigIdx))
+    cachedContig
+  }
+
+  override def start(): Int = {
+    region.loadInt(t.loadField(region, offset, startIdx))
+  }
+
+  override def ref(): String = {
+    if (cachedRef == null)
+      cachedRef = TString.loadString(region, t.loadField(region, offset, refIdx))
+    cachedRef
+  }
+
+  override def altAlleles(): IndexedSeq[AltAllele] = {
+    if (cachedAltAlleles == null) {
+      altAllelesView.setRegion(region, t.loadField(region, offset, altAllelesIdx))
+      val a = new Array[AltAllele](altAllelesView.length)
+      var i = 0
+      while (i < altAllelesView.length) {
+        altAllelesView.set(i)
+        a(i) = altAllelesView.elementView.reify()
+      }
+      cachedAltAlleles = a
+    }
+    cachedAltAlleles
+  }
+}
