98ed6b9ea24106e477b5c90b74d5e3db1cf5a041
Patrick Schultz <schultzp@mit.edu>
2017-11-21 15:15:27 -0500

    Comment test cases generation in Mathematica (#2463)

--- src/test/scala/is/hail/utils/HashMethodsSuite.scala
+++ src/test/scala/is/hail/utils/HashMethodsSuite.scala
@@ -7,6 +7,10 @@ class HashMethodsSuite extends SparkSuite {
   @Test def testMultGF() {
     import PolyHash._
 
+    // multGF should agree with the Mathematica function
+    // f[a_, b_] := PolynomialRemainder[g[a, b], x^32 + x^7 + x^3 + x^2 + 1, x, Modulus -> 2] /. x -> 2
+    // where
+    // g[a_, b_] := Expand[FromDigits[IntegerDigits[a, 2], x] * FromDigits[IntegerDigits[b, 2], x], Modulus -> 2]
     val x1 = 3705006673L.toInt
     val y1 = 2551778209L.toInt
     assert(multGF(x1, y1) == 2272553040L.toInt)
