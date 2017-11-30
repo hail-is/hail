38ce416a90e623a24a6a3502b6690f860d257721
Daniel King <daniel.zidan.king@gmail.com>
2017-11-30 13:41:52 -0500

    Update keytable.py

--- python/hail/keytable.py
+++ python/hail/keytable.py
@@ -1414,14 +1414,14 @@ class KeyTable(object):
 
         >>> related_pairs = vds.pc_relate(2, 0.001).filter("kin > 0.125")
         >>> related_samples = related_pairs.query('i.flatMap(i => [i,j]).collectAsSet()')
-        >>> related_samples_to_keep = (related_pairs
+        >>> related_nodes_to_keep = (related_pairs
         ...   .key_by("i").join(vds.samples_table()).annotate('iAndCase = { id: i, isCase: sa.isCase }')
         ...   .select(['j', 'iAndCase'])
         ...   .key_by("j").join(vds.samples_table()).annotate('jAndCase = { id: j, isCase: sa.isCase }')
         ...   .select(['iAndCase', 'jAndCase'])
         ...   .maximal_independent_set("iAndCase", "jAndCase",
         ...     'if (l.isCase && !r.isCase) -1 else if (!l.isCase && r.isCase) 1 else 0'))
-        >>> related_samples_to_remove = related_samples - {x.id for x in related_samples_to_keep}
+        >>> related_samples_to_remove = related_samples - {x.id for x in related_nodes_to_keep}
         >>> vds.filter_samples_list(list(related_samples_to_remove))
 
         **Notes**
