dacde4c35380f098247533c834b618b4721e0282
Jackie Goldstein <jigold@broadinstitute.org>
2017-12-15 16:36:57 -0500

    add test

--- src/test/scala/is/hail/utils/RichRDDSuite.scala
+++ src/test/scala/is/hail/utils/RichRDDSuite.scala
@@ -59,4 +59,31 @@ class RichRDDSuite extends SparkSuite {
     assert(readBytes(parallelWriteHeader + "/part-00000") sameElements data(0))
     assert(readBytes(parallelWriteHeader + "/part-00001") sameElements data(1))
   }
+
+  @Test def parallelWrite() {
+    def read(file: String): Array[String] = hc.hadoopConf.readLines(file)(_.map(_.value).toArray)
+
+    val header = "my header is awesome!"
+    val data = Array("the cat jumped over the moon.", "all creatures great and small")
+    val r = sc.parallelize(data, numSlices = 2)
+    assert(r.getNumPartitions == 2)
+
+    val concatenated = tmpDir.createTempFile("concatenated")
+    r.writeTable(concatenated, tmpDir.createTempFile("concatenated"), Some(header), exportType = ExportType.CONCATENATED)
+
+    assert(read(concatenated) sameElements (header +: data))
+
+    val shardHeaders = tmpDir.createTempFile("shardHeader")
+    r.writeTable(shardHeaders, tmpDir.createTempFile("shardHeader"), Some(header), exportType = ExportType.PARALLEL_HEADER_IN_SHARD)
+
+    assert(read(shardHeaders + "/part-00000") sameElements header +: Array(data(0)))
+    assert(read(shardHeaders + "/part-00001") sameElements header +: Array(data(1)))
+
+    val separateHeader = tmpDir.createTempFile("separateHeader")
+    r.writeTable(separateHeader, tmpDir.createTempFile("separateHeader"), Some(header), exportType = ExportType.PARALLEL_SEPARATE_HEADER)
+
+    assert(read(separateHeader + "/header") sameElements Array(header))
+    assert(read(separateHeader + "/part-00000") sameElements Array(data(0)))
+    assert(read(separateHeader + "/part-00001") sameElements Array(data(1)))
+  }
 }
