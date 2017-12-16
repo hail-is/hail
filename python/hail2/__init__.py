5cbf32044c021dce3157430f449698668b14a54a
tpoterba <tpoterba@broadinstitute.org>
2017-12-16 08:09:21 -0500

    Added more generic methods to Table.
    Did NOT yet add order_by, which will take more work.

--- python/hail2/__init__.py
+++ python/hail2/__init__.py
@@ -8,6 +8,10 @@ from hail.genetics import LDMatrix, KinshipMatrix, Variant, Locus, AltAllele, In
     GenomeReference
 from hail.utils import Struct, hadoop_write, hadoop_read, hadoop_copy
 from hail.utils import hadoop_read, hadoop_write, hadoop_copy
+<<<<<<< 0f68e12456f79f3e4b2383883ed4b8df075970e5
+=======
+from hail.methods import trio_matrix, ld_matrix, linreg, sample_qc
+>>>>>>> Added more generic methods to Table.
 
 __all__ = ['HailContext',
            'Table',
