3a1bb10f79d24dc1ecf925da06594855aa421682
Daniel King <dking@broadinstitute.org>
2018-01-09 15:34:23 -0500

    fix

--- python/hail/methods/statgen.py
+++ python/hail/methods/statgen.py
@@ -430,6 +430,7 @@ def split_multi_hts(ds, keep_star=False, left_aligned=False):
       A   C   0/1:8,8:16:45:45,0,99
       A   T   0/1:10,6:16:50:50,0,99
 
+
     VCF Info Fields
     ---------------
 
