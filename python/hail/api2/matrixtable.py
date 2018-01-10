eb1f86f1d7dc4454ed47bf3bbc9b524d79f93a50
tpoterba <tpoterba@broadinstitute.org>
2018-01-10 18:35:43 -0500

    fix typo

--- python/hail/api2/matrixtable.py
+++ python/hail/api2/matrixtable.py
@@ -2241,7 +2241,7 @@ class MatrixTable(object):
 
         >>> dataset_result = dataset_to_union_1.union_rows(dataset_to_union_2)
         >>> dataset_result = all_datasets[0].union_rows(*all_datasets[1:])
-        >>> dataset_result = MatrixTable.union_rows(*all_datsets)
+        >>> dataset_result = MatrixTable.union_rows(*all_datasets)
 
         Notes
         -----
