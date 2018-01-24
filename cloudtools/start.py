fb14d2904229020a76af79c5834f75702ac5d1c6
tpoterba <tpoterba@broadinstitute.org>
2018-01-24 09:20:59 -0500

    Made the os.mkdir statements safer and free from race conditions

--- cloudtools/start.py
+++ cloudtools/start.py
@@ -1,6 +1,6 @@
 from subprocess import call, check_output
 
-COMPATIBILITY_VERSION = 4
+COMPATIBILITY_VERSION = 5
 
 # master machine type to memory map, used for setting spark.driver.memory property
 machine_mem = {
