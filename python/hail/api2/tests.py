5dde6f57ca28136f97b186a479fc4826c7561445
cseed <cotton@alum.mit.edu>
2018-01-28 22:27:17 -0500

    added MT._filter_partitions (#2835)
    * added MT._filter_partitions
    
    * formatting

--- python/hail/api2/tests.py
+++ python/hail/api2/tests.py
@@ -561,6 +561,16 @@ class MatrixTests(unittest.TestCase):
              (df.GT == df.entry_struct.GT)) &
              (df.AD == df.entry_struct.AD))))
 
+    def test_filter_partitions(self):
+        ds = self.get_vds(min_partitions=8)
+        self.assertEqual(ds.num_partitions(), 8)
+        self.assertEqual(ds._filter_partitions([0, 1, 4]).num_partitions(), 3)
+        self.assertEqual(ds._filter_partitions([4, 5, 7], keep=False).num_partitions(), 5)
+        self.assertTrue(
+            ds._same(MatrixTable.union_rows(
+                ds._filter_partitions([0, 3, 7]),
+                ds._filter_partitions([0, 3, 7], keep=False))))
+
 
 class FunctionsTests(unittest.TestCase):
     def test(self):
