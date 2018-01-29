c2a0f59921a3ee7b23c7109d043bb750e8dbe6cf
Tim Poterba <tpoterba@gmail.com>
2018-01-29 13:15:51 -0500

    Add allele methods to function registry and functions.py (#2832)
    * Add allele methods to function registry and functions.py
    
    * Address comments

--- src/main/scala/is/hail/variant/AltAllele.scala
+++ src/main/scala/is/hail/variant/AltAllele.scala
@@ -4,9 +4,12 @@ import is.hail.annotations._
 import is.hail.check._
 import is.hail.expr.types._
 import is.hail.utils._
+import is.hail.variant.AltAlleleType.AltAlleleType
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.types._
 import org.json4s._
+
+import scala.util.matching.Regex
 
 object AltAlleleType extends Enumeration {
   type AltAlleleType = Value
@@ -41,11 +44,110 @@ object AltAllele {
 
   def gen: Gen[AltAllele] =
     for (ref <- genDNAString;
-      alt <- genDNAString if alt != ref)
+    alt <- genDNAString if alt != ref)
       yield AltAllele(ref, alt)
 
   implicit def altAlleleOrder: Ordering[AltAllele] = new Ordering[AltAllele] {
     def compare(x: AltAllele, y: AltAllele): Int = x.compare(y)
+  }
+}
+
+object AltAlleleMethods {
+  val alleleRegex: Regex = "^([ACGT]+)|\\*$".r
+
+  def validate(allele: String) {
+    if (!alleleRegex.matches(allele))
+      fatal(s"""invalid allele "$allele"""")
+  }
+
+  def altAlleleType(ref: String, alt: String): AltAlleleType = {
+    validate(ref)
+    validate(alt)
+    if (ref.length == alt.length) {
+      if (ref.length == 1)
+        if (ref == "*" || alt == "*")
+          AltAlleleType.Star
+        else
+          AltAlleleType.SNP
+      else {
+        val mismatches = hamming(ref, alt)
+        if (mismatches == 1)
+          AltAlleleType.SNP
+        else
+          AltAlleleType.MNP
+      }
+    } else {
+      if (ref == "*" || alt == "*")
+        AltAlleleType.Star
+      else if (ref.length < alt.length) {
+        if (ref(0) == alt(0) && alt.endsWith(ref.substring(1)))
+          AltAlleleType.Insertion
+        else
+          AltAlleleType.Complex
+      } else {
+        if (ref(0) == alt(0) && ref.endsWith(alt.substring(1)))
+          AltAlleleType.Deletion
+        else
+          AltAlleleType.Complex
+      }
+    }
+  }
+
+  def isSNP(ref: String, alt: String): Boolean = altAlleleType(ref, alt) == AltAlleleType.SNP
+
+  def isMNP(ref: String, alt: String): Boolean = altAlleleType(ref, alt) == AltAlleleType.MNP
+
+  def isInsertion(ref: String, alt: String): Boolean = altAlleleType(ref, alt) == AltAlleleType.Insertion
+
+  def isDeletion(ref: String, alt: String): Boolean = altAlleleType(ref, alt) == AltAlleleType.Deletion
+
+  def isIndel(ref: String, alt: String): Boolean = {
+    val aat = altAlleleType(ref, alt)
+    aat == AltAlleleType.Insertion || aat == AltAlleleType.Deletion
+  }
+
+  def isStar(ref: String, alt: String): Boolean = altAlleleType(ref, alt) == AltAlleleType.Star
+
+  def isComplex(ref: String, alt: String): Boolean = altAlleleType(ref, alt) == AltAlleleType.Complex
+
+  def isTransition(ref: String, alt: String): Boolean = {
+    val aat = altAlleleType(ref, alt)
+    aat == AltAlleleType.SNP && isTransitionSNP(ref, alt)
+  }
+
+  def isTransversion(ref: String, alt: String): Boolean = {
+    val aat = altAlleleType(ref, alt)
+    aat == AltAlleleType.SNP && !isTransitionSNP(ref, alt)
+  }
+
+  private def isTransitionSNP(ref: String, alt: String): Boolean = {
+    val pos = snpPos(ref, alt)
+    val refChar = ref(pos)
+    val altChar = alt(pos)
+    (refChar == 'A' && altChar == 'G') || (refChar == 'G' && altChar == 'A') ||
+      (refChar == 'C' && altChar == 'T') || (refChar == 'T' && altChar == 'C')
+  }
+
+  def hamming(a1: String, a2: String): Int = {
+    if (a1.length != a2.length)
+      fatal(s"function 'hamming' requires strings to have equal length, " +
+        s"found lengths ${a1.length} and ${a2.length}: '$a1', '$a2'")
+    var n = 0
+    var i = 0
+    while (i < a1.length) {
+      if (a1(i) != a2(i))
+        n += 1
+      i += 1
+    }
+    n
+  }
+
+  def snpPos(ref: String, alt: String): Int = {
+    var i = 0
+    while (ref(i) == alt(i)) {
+      i += 1
+    }
+    i
   }
 }
 
