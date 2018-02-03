04b34fb0c3cf60446a5420305f62d862f42f51cc
jbloom22 <jbloom@broadinstitute.org>
2018-02-03 18:54:55 -0500

    toward numpy

--- src/main/scala/is/hail/distributedmatrix/LocalMatrix.scala
+++ src/main/scala/is/hail/distributedmatrix/LocalMatrix.scala
@@ -7,12 +7,14 @@ import is.hail.utils.richUtils.RichDenseMatrixDouble
 import is.hail.utils._
 import org.apache.commons.math3.random.MersenneTwister
 
+
+
 object LocalMatrix {
   type M = LocalMatrix  
   
   def apply(m: BDM[Double]): M = new LocalMatrix(m)
-
-  def apply(nRows: Int, nCols: Int): M =
+  
+  def zeros(nRows: Int, nCols: Int): M =
     LocalMatrix(new BDM[Double](nRows, nCols))
   
   def apply(nRows: Int, nCols: Int, data: Array[Double]): M =
@@ -31,6 +33,22 @@ object LocalMatrix {
     LocalMatrix(BDM.rand(nRows, nCols, rand))
   }
   
+  def addOuter(row: Array[Double], col: Array[Double]): BDM[Double] = {
+    val nRows = col.length
+    val nCols = row.length
+    assert(nRows > 0 && nCols > 0 && (nRows * nCols.toLong < Int.MaxValue))
+    
+    val a = new Array[Double](nRows * nCols)
+      var j = 0
+      while (j < nCols) {
+        var i = 0
+        while (i < nRows) {
+          a(j * nRows + i) = col(i) + row(j)
+        }
+      }
+      new BDM[Double](nRows, nCols, a)
+  }
+  
   object ops {
 
     implicit class Shim(l: M) {
@@ -40,19 +58,16 @@ object LocalMatrix {
       def diag: Array[Double] =
         l.diagonal()
 
-      def *(r: M): M =
-        l.multiply(r)
-
-      def :+(r: M): M =
+      def +(r: M): M =
         l.add(r)
 
-      def :-(r: M): M =
+      def -(r: M): M =
         l.subtract(r)
 
-      def :*(r: M): M =
+      def *(r: M): M =
         l.pointwiseMultiply(r)
 
-      def :/(r: M): M =
+      def /(r: M): M =
         l.pointwiseDivide(r)
 
       def +(r: Double): M =
@@ -66,18 +81,6 @@ object LocalMatrix {
 
       def /(r: Double): M =
         l.scalarDivide(r)
-
-      def :+(v: Array[Double]): M =
-        l.vectorAddToEveryColumn(v)
-
-      def :*(v: Array[Double]): M =
-        l.vectorPointwiseMultiplyEveryColumn(v)
-
-      def --+(v: Array[Double]): M =
-        l.vectorAddToEveryRow(v)
-
-      def --*(v: Array[Double]): M =
-        l.vectorPointwiseMultiplyEveryRow(v)
     }
 
     implicit class ScalarShim(l: Double) {
@@ -107,29 +110,109 @@ class LocalMatrix(val m: BDM[Double]) {
 
   def data: Array[Double] = m.data
 
+  def dataColMajor: Array[Double] = if (!isTranspose) m.data else m.toArray
+
   def toArray: Array[Double] = m.toArray
-  
+
+  def copy() = LocalMatrix(m.copy)
+
   def isTranspose: Boolean = m.isTranspose
 
-  def write(hc: HailContext, path: String) { m.write(hc: HailContext, path) }
+  def write(hc: HailContext, path: String) {
+    m.write(hc: HailContext, path)
+  }
 
   def toBlockMatrix(hc: HailContext, blockSize: Int = BlockMatrix.defaultBlockSize): BlockMatrix =
     BlockMatrix.from(hc.sc, m, blockSize)
-  
+
   def transpose() = LocalMatrix(m.t)
 
   def diagonal(): Array[Double] = diag(m).toArray
 
-  def add(that: LocalMatrix) = LocalMatrix(m + that.m)
+  def shape: (Int, Int) = (nRows, nCols)
 
-  def subtract(that: LocalMatrix) = LocalMatrix(m - that.m)
+  def apply(i: Int, j: Int) = m(i, j)
 
-  def multiply(that: LocalMatrix) = LocalMatrix(m * that.m)
+  // 0 if scalar, 1 if single column, 2 if single row, 3 otherwise
+  def typ: Int =
+    if (nRows > 1) {
+      if (nCols > 1) 3 else 1
+    } else {
+      if (nCols > 1) 2 else 0
+    }
 
-  def pointwiseMultiply(that: LocalMatrix) = LocalMatrix(m :* that.m)
+  def negate() = LocalMatrix(-m)
+  
+  def reciprocate() = LocalMatrix(1.0 :/ m)
+ 
+  def dot(that: LocalMatrix) = LocalMatrix(m * that.m)
 
-  def pointwiseDivide(that: LocalMatrix) = LocalMatrix(m :/ that.m)
+  def add(that: LocalMatrix) = LocalMatrix(
+    if (typ == that.typ)
+      m + that.m
+    else if (that.typ == 0)
+      m + that(0, 0)
+    else if (typ == 0)
+      that.m + m(0, 0)
+    else
+      (typ, that.typ) match {
+        case (3, 1) => m(*, ::) :+ BDV(that.data)
+        case (3, 2) => m(::, *) :+ BDV(that.data)
+        case (1, 3) => that.m(*, ::) :+ BDV(data)
+        case (2, 3) => that.m(::, *) :+ BDV(data)
+        case (1, 2) => LocalMatrix.addOuter(that.data, data)
+        case (2, 1) => LocalMatrix.addOuter(data, that.data)
+      }
+  )
+  
+  def subtract(that: LocalMatrix) = LocalMatrix(
+    if (typ == that.typ)
+      m - that.m
+    else if (that.typ == 0)
+      m - that(0, 0)
+    else if (typ == 0)
+      m(0, 0) - that.m
+    else
+      (typ, that.typ) match {
+        case (3, 1) => m(*, ::) :- BDV(that.data)
+        case (3, 2) => m(::, *) :- BDV(that.data)
+        case _ => add(that.negate()).m // FIXME: room for improvement
+      }
+  )
 
+  def pointwiseMultiply(that: LocalMatrix) = LocalMatrix(
+    if (typ == that.typ)
+      m :* that.m
+    else if (that.typ == 0)
+      m * that(0, 0)
+    else if (typ == 0)
+      that.m * m(0, 0)
+    else
+      (typ, that.typ) match {
+        case (3, 1) => m(*, ::) :* BDV(that.data)
+        case (3, 2) => m(::, *) :* BDV(that.data)
+        case (1, 3) => that.m(*, ::) :* BDV(data)
+        case (2, 3) => that.m(::, *) :* BDV(data)
+        case (1, 2) => m * that.m
+        case (2, 1) => that.m * m
+      }
+  )
+  
+  def pointwiseDivide(that: LocalMatrix) = LocalMatrix(
+    if (typ == that.typ)
+      m :/ that.m
+    else if (that.typ == 0)
+      m :/ that(0, 0)
+    else if (typ == 0)
+      m(0, 0) :/ that.m
+    else
+      (typ, that.typ) match {
+        case (3, 1) => m(*, ::) :/ BDV(that.data)
+        case (3, 2) => m(::, *) :/ BDV(that.data)
+        case _ => pointwiseMultiply(that.reciprocate()).m // FIXME: room for improvement
+      }
+  )
+  
   def scalarAdd(i: Double) = LocalMatrix(m + i)
 
   def scalarSubtract(i: Double) = LocalMatrix(m - i)
@@ -141,12 +224,4 @@ class LocalMatrix(val m: BDM[Double]) {
   def scalarDivide(i: Double) = LocalMatrix(m :/ i)  
   
   def scalarDivideLeft(i: Double) = LocalMatrix(i :/ m)
-
-  def vectorAddToEveryRow(v: Array[Double]) = LocalMatrix(m(*, ::) + BDV(v))  
-  
-  def vectorAddToEveryColumn(v: Array[Double]) = LocalMatrix(m(::, *) + BDV(v))
-
-  def vectorPointwiseMultiplyEveryRow(v: Array[Double]) = LocalMatrix(m(*, ::) :* BDV(v))  
-  
-  def vectorPointwiseMultiplyEveryColumn(v: Array[Double]) = LocalMatrix(m(::, *) :* BDV(v))
 }
