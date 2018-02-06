e3867d5a2ca70150c0524072168a1fd9f87d471c
Tim Poterba <tpoterba@gmail.com>
2018-02-06 12:47:04 -0500

    Added case and switch to expr (#2838)
    * Added case and switch to expr
    
    * Add sphinx conf
    
    * Fix whitespace
    
    * Fix docs

--- python/hail/docs/conf.py
+++ python/hail/docs/conf.py
@@ -54,11 +54,12 @@ nbsphinx_allow_errors = False
 if not tags.has('checktutorial'):
     nbsphinx_execute = 'never'
 
-autosummary_generate = ['api.rst', 'genetics/index.rst', 'methods/index.rst', 'utils/index.rst', 'linalg/index.rst', 'stats/index.rst']
+autosummary_generate = ['api.rst', 'genetics/index.rst', 'methods/index.rst', 'utils/index.rst', 'linalg/index.rst', 'stats/index.rst', 'expr/utils.rst']
 # autoclass_content = "both"
 autodoc_default_flags = ['members', 'undoc-members']
 
 napoleon_use_rtype = False
+napoleon_use_param = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates', '_templates/_autosummary']
