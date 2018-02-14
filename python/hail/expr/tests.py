aae4f308f90c199f3e950e0f106b1f0ec23e06fe
Tim Poterba <tpoterba@gmail.com>
2018-02-13 20:42:42 -0500

    Change the interface of aggregate (#2892)
    * Change the interface of aggregate
    
    * Address comments

--- python/hail/expr/tests.py
+++ python/hail/expr/tests.py
@@ -115,21 +115,21 @@ class Tests(unittest.TestCase):
 
     def test_aggregators(self):
         table = Table.range(10)
-        r = table.aggregate(x=agg.count(),
-                            y=agg.count_where(table.idx % 2 == 0),
-                            z=agg.count(agg.filter(lambda x: x % 2 == 0, table.idx)),
-                            arr_sum=agg.array_sum([1, 2, hl.null(TInt32())]))
+        r = table.aggregate(Struct(x=agg.count(),
+                                   y=agg.count_where(table.idx % 2 == 0),
+                                   z=agg.count(agg.filter(lambda x: x % 2 == 0, table.idx)),
+                                   arr_sum=agg.array_sum([1, 2, hl.null(TInt32())])))
 
         self.assertEqual(r.x, 10)
         self.assertEqual(r.y, 5)
         self.assertEqual(r.z, 5)
         self.assertEqual(r.arr_sum, [10, 20, 0])
 
-        r = table.aggregate(fraction_odd=agg.fraction(table.idx % 2 == 0),
-                            lessthan6=agg.fraction(table.idx < 6),
-                            gt6=agg.fraction(table.idx > 6),
-                            assert1=agg.fraction(table.idx > 6) < 0.50,
-                            assert2=agg.fraction(table.idx < 6) >= 0.50)
+        r = table.aggregate(Struct(fraction_odd=agg.fraction(table.idx % 2 == 0),
+                                   lessthan6=agg.fraction(table.idx < 6),
+                                   gt6=agg.fraction(table.idx > 6),
+                                   assert1=agg.fraction(table.idx > 6) < 0.50,
+                                   assert2=agg.fraction(table.idx < 6) >= 0.50))
         self.assertEqual(r.fraction_odd, 0.50)
         self.assertEqual(r.lessthan6, 0.60)
         self.assertEqual(r.gt6, 0.30)
