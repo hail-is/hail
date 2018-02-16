03d598fc35a5a8980d2b5d91e7fd02cd7efdb357
wang <wang@broadinstitute.org>
2018-02-16 14:09:40 -0500

    fix

--- src/main/scala/is/hail/io/LoadMatrix.scala
+++ src/main/scala/is/hail/io/LoadMatrix.scala
@@ -34,7 +34,7 @@ class LoadMatrixParser(rvb: RegionValueBuilder, fieldTypes: Array[Type], entryTy
              |    expected $nFields fields but only $ii found.
              |    File: $file
              |    Line:
-             |        ${ line.truncate(76) }""".stripMargin
+             |        ${ line.truncate }""".stripMargin
         )
       }
     }
@@ -48,7 +48,7 @@ class LoadMatrixParser(rvb: RegionValueBuilder, fieldTypes: Array[Type], entryTy
              |    expected $nCols entries but only $ii entries found.
              |    File: $file
              |    Line:
-             |        ${ line.truncate(76) }""".stripMargin
+             |        ${ line.truncate }""".stripMargin
         )
       }
       rvb.startStruct()
