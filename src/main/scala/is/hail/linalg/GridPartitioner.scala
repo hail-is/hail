76d9aa434250ba25d3f7da3e43632a1762c8091a
jbloom22 <jbloom@broadinstitute.org>
2018-02-22 12:34:18 -0500

    more improvements

--- src/main/scala/is/hail/linalg/GridPartitioner.scala
+++ src/main/scala/is/hail/linalg/GridPartitioner.scala
@@ -3,43 +3,27 @@ package is.hail.linalg
 import org.apache.spark.Partitioner
 
 case class GridPartitioner(blockSize: Int, nRows: Long, nCols: Long) extends Partitioner {
-  require(nRows > 0)
-  require(nCols > 0)
+  require(nRows > 0 && nRows <= Int.MaxValue.toLong * blockSize)
+  require(nCols > 0 && nCols <= Int.MaxValue.toLong * blockSize)
   
   def blockIndex(index: Long): Int = (index / blockSize).toInt
-  def blockOffset(index: Long): Int = (index % blockSize).toInt  
 
-  require((nRows - 1) / blockSize + 1 <= Int.MaxValue)
-  val nBlockRows: Int = blockIndex(nRows - 1) + 1
+  def blockOffset(index: Long): Int = (index % blockSize).toInt
 
-  require((nCols - 1) / blockSize + 1 <= Int.MaxValue)    
-  val nBlockCols: Int = blockIndex(nCols - 1) + 1
-
-  require(nBlockRows.toLong * nBlockCols <= Int.MaxValue)
-  override val numPartitions: Int = nBlockRows * nBlockCols
+  val nBlockRows: Int = (nRows / blockSize).toInt
+  val nBlockCols: Int = (nCols / blockSize).toInt
   
-  def blockRowNRows(i: Int): Int =
-    if (i < nBlockRows - 1)
-      blockSize
-    else
-      blockOffset(nRows - 1) + 1
-
-  def blockColNCols(j: Int): Int =
-    if (j < nBlockCols - 1)
-      blockSize
-    else
-      blockOffset(nRows - 1) + 1
-
-  def blockDims(pi: Int): (Int, Int) = (blockRowNRows(blockBlockRow(pi)), blockColNCols(blockBlockCol(pi)))
-
-  override def getPartition(key: Any): Int = key match {
-    case (i: Int, j: Int) => coordinatesBlock(i, j)
-  }
+  val lastBlockRowNRows: Int = blockOffset(nRows - 1) + 1
+  val lastBlockColNCols: Int = blockOffset(nCols - 1) + 1  
+  
+  def blockRowNRows(i: Int): Int = if (i < nBlockRows - 1) blockSize else lastBlockRowNRows
+  def blockColNCols(j: Int): Int = if (j < nBlockCols - 1) blockSize else lastBlockColNCols
 
   def blockBlockRow(pi: Int): Int = pi % nBlockRows
-
   def blockBlockCol(pi: Int): Int = pi / nBlockRows
 
+  def blockDims(pi: Int): (Int, Int) = (blockRowNRows(blockBlockRow(pi)), blockColNCols(blockBlockCol(pi)))
+  
   def blockCoordinates(pi: Int): (Int, Int) = (blockBlockRow(pi), blockBlockCol(pi))
 
   def coordinatesBlock(i: Int, j: Int): Int = {
@@ -48,6 +32,11 @@ case class GridPartitioner(blockSize: Int, nRows: Long, nCols: Long) extends Par
     i + j * nBlockRows
   }
 
-  def transpose: GridPartitioner =
-    GridPartitioner(this.blockSize, this.nCols, this.nRows)
+  override val numPartitions: Int = nBlockRows * nBlockCols
+  
+  override def getPartition(key: Any): Int = key match {
+    case (i: Int, j: Int) => coordinatesBlock(i, j)
+  }
+  
+  def transpose: GridPartitioner = GridPartitioner(this.blockSize, this.nCols, this.nRows)
 }
