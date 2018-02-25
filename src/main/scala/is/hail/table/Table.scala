6262d9627dbe36b72429b937316d8ab7515d179c
Cotton Seed <cotton@alum.mit.edu>
2018-02-25 14:07:36 -0500

    use new Expression.show, collect, etc. in tutorial
    Use Table.aggregate in Expression.collect since it is much faster
    (doesn't convert row and unneeded keys to python)

--- src/main/scala/is/hail/table/Table.scala
+++ src/main/scala/is/hail/table/Table.scala
@@ -1051,7 +1051,7 @@ class Table(val hc: HailContext, val ir: TableIR) {
 
   def explode(columnNames: java.util.ArrayList[String]): Table = explode(columnNames.asScala.toArray)
 
-  def collect(): IndexedSeq[Row] = rdd.collect()
+  def collect(): Array[Row] = rdd.collect()
 
   def write(path: String, overwrite: Boolean = false, codecSpecJSONStr: String = null) {
     val codecSpec =
@@ -1234,7 +1234,7 @@ class Table(val hc: HailContext, val ir: TableIR) {
     else {
       val takeResult = take(n + 1)
       val hasMoreData = takeResult.length > n
-      (takeResult.take(n): IndexedSeq[Row]) -> hasMoreData
+      takeResult.take(n) -> hasMoreData
     }
 
     def convertType(t: Type, name: String, ab: ArrayBuilder[(String, String, Boolean)]) {
