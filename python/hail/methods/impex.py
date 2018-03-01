b49406d00bb6fdfe373318ea51bc08f39c891a67
wang <wang@broadinstitute.org>
2018-03-01 16:57:03 -0500

    address comments

--- python/hail/methods/impex.py
+++ python/hail/methods/impex.py
@@ -1043,7 +1043,8 @@ def import_matrix_table(paths, row_fields={}, row_key=[], entry_type=tint32, mis
         'Barcode': str
     ----------------------------------------
 
-    If the header information is missing for the row fields, like in the following:
+    In this example, the header information is missing for the row fields, but
+    the column IDs are still present:
 
     .. code-block:: text
 
@@ -1081,14 +1082,14 @@ def import_matrix_table(paths, row_fields={}, row_key=[], entry_type=tint32, mis
 
     The resulting matrix table has the following structure:
 
-        - The **row fields** are named as specified in the column header. If they
+        - The row fields are named as specified in the column header. If they
         are missing from the header or ``no_header=True``, row field names are
         set to the strings `f0`, `f1`, ... (0-indexed) in column order. The types
         of all row fields must be specified in the `row_fields` argument.
-        - The **row key** is taken from the `row_key` argument, and must be a
+        - The row key is taken from the `row_key` argument, and must be a
         subset of row fields. If left empty, the row key will be a new row field
-        `row_idx` of type :obj:`int`, whose values 0, 1, ... are an index on the
-        original rows of the matrix.
+        `row_idx` of type :obj:`int`, whose values 0, 1, ... index the original
+        rows of the matrix.
         - There is one column field, **col_id**, which is a key field of type
         :obj:str or :obj:int. By default, its values are the strings given by
         the corresponding column names in the header line. If ``no_header=True``,
@@ -1101,7 +1102,8 @@ def import_matrix_table(paths, row_fields={}, row_key=[], entry_type=tint32, mis
     All columns to be imported as row fields must be at the start of the row.
 
     Unlike import_table, no type imputation is done so types must be specified
-    for all columns that should be imported as row fields.
+    for all columns that should be imported as row fields. (The other columns are
+    imported as entries in the matrix.)
 
     The header information for row fields is allowed to be missing, if the
     column IDs are present, but the header must then consist only of tab-delimited
