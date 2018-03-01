457554773a472219a060e6201b38bbda0a149d8d
cseed <cotton@alum.mit.edu>
2018-03-01 00:03:51 -0500

    removed utils.Summary. (#3029)
    had already removed summarize, just missed this.

--- python/hail/utils/__init__.py
+++ python/hail/utils/__init__.py
@@ -1,11 +1,9 @@
-from .summary import Summary
 from .misc import wrap_to_list, get_env_or_default, get_URI, new_temp_file, storage_level, range_matrix_table, range_table
 from .hadoop_utils import hadoop_copy, hadoop_read, hadoop_write, hadoop_read_binary
 from .struct import Struct
 from .java import error, warn, info, FatalError
 
-__all__ = ['Summary',
-           'hadoop_read',
+__all__ = ['hadoop_read',
            'hadoop_read_binary',
            'hadoop_write',
            'hadoop_copy',
