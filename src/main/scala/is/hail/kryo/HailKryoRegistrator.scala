6ee7a7158bf72488957ba0db6c9e4640b4567185
Cotton Seed <cotton@alum.mit.edu>
2018-03-02 10:15:49 -0500

    workaround kryo exception

--- src/main/scala/is/hail/kryo/HailKryoRegistrator.scala
+++ src/main/scala/is/hail/kryo/HailKryoRegistrator.scala
@@ -5,6 +5,7 @@ import com.esotericsoftware.kryo.Kryo
 import com.esotericsoftware.kryo.serializers.JavaSerializer
 import is.hail.annotations.{Region, UnsafeIndexedSeq, UnsafeRow}
 import is.hail.utils.{Interval, SerializableHadoopConfiguration}
+import is.hail.expr.types.{TTuple, TStruct}
 
 class HailKryoRegistrator extends KryoRegistrator {
   override def registerClasses(kryo: Kryo) {
@@ -12,5 +13,9 @@ class HailKryoRegistrator extends KryoRegistrator {
     kryo.register(classOf[UnsafeRow])
     kryo.register(classOf[UnsafeIndexedSeq])
     kryo.register(classOf[Region])
+
+    // work around mysterious KryoException caused by ArrayIndexOutOfBoundsException
+    kryo.register(classOf[TTuple], new JavaSerializer())
+    kryo.register(classOf[TStruct], new JavaSerializer())
   }
 }
