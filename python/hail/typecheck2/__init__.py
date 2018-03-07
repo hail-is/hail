bbd6c33c804363dfd8bd12bf99b49a69200cb352
tpoterba <tpoterba@broadinstitute.org>
2018-03-07 12:32:58 -0500

    Typecheck 2: the Types Strike Back

---
+++ python/hail/typecheck2/__init__.py
@@ -0,0 +1,8 @@
+from .check import typecheck, register_conversion
+__all__ = ['typecheck',
+           'register_conversion']
+
+__doc__ = """
+Heavily inspired by typeguard: 
+    https://github.com/agronholm/typeguard/
+"""
