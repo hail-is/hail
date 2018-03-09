1735f24a9637e5fb2f6ad94f05eea5ec3ae9e3d0
Tim Poterba <tpoterba@gmail.com>
2018-03-08 20:45:55 -0500

    Fix VEP/Nirvana (#3100)

--- src/main/scala/is/hail/methods/VEP.scala
+++ src/main/scala/is/hail/methods/VEP.scala
@@ -217,6 +217,7 @@ object VEP {
 
   def annotate(vsm: MatrixTable, config: String, root: String = "va.vep", csq: Boolean,
     blockSize: Int): MatrixTable = {
+    assert(vsm.rowKey == IndexedSeq("locus", "alleles"))
 
     val parsedRoot = Parser.parseAnnotationRoot(root, Annotation.VARIANT_HEAD)
 
@@ -396,7 +397,7 @@ object VEP {
     val vepType: Type = if (csq) TArray(TString()) else vepSignature
 
     val vepORVDType = new OrderedRVDType(
-      Array("locus"), Array("locus", "alleles"),
+      vsm.rowPartitionKey.toArray, vsm.rowKey.toArray,
       TStruct(
         "locus" -> vsm.rowKeyTypes(0),
         "alleles" -> vsm.rowKeyTypes(1),
