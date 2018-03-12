3630e5eaa9c1dc6a3e30da430e271422dc8f3b43
Jackie Goldstein <jigold@broadinstitute.org>
2018-03-12 11:50:08 -0400

    addressed comments

--- python/hail/genetics/reference_genome.py
+++ python/hail/genetics/reference_genome.py
@@ -318,12 +318,7 @@ class ReferenceGenome(object):
     def from_fasta_file(cls, name, fasta_file, index_file,
                         x_contigs=[], y_contigs=[], mt_contigs=[], par=[]):
         """Create reference genome from a FASTA file.
-
-        Notes
-        -----
-        The resulting reference genome will have default values
-        for `x_contigs`, `y_contigs`, `mt_contigs`, and `par`.
-
+\
         Parameters
         ----------
         name: :obj:`str`
