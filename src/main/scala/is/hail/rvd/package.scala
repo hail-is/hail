9fc44111c329ab16eb0a4ef710403771b208ed97
Daniel King <dking@broadinstitute.org>
2018-03-26 18:35:44 -0400

    make RVDContextIsPointed serializable

--- src/main/scala/is/hail/rvd/package.scala
+++ src/main/scala/is/hail/rvd/package.scala
@@ -3,7 +3,8 @@ package is.hail
 import is.hail.utils._
 
 package object rvd {
-  implicit object RVDContextIsPointed extends Pointed[RVDContext] {
+  implicit object RVDContextIsPointed
+      extends Pointed[RVDContext] with Serializable {
     def point: RVDContext = RVDContext.default
   }
 }
