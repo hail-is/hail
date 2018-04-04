77ada36ef126d6ffc059b8f475c89de7193e303a
Daniel King <dking@broadinstitute.org>
2018-04-03 20:45:56 -0400

    omfg

--- src/main/scala/is/hail/utils/SetupIterator.scala
+++ src/main/scala/is/hail/utils/SetupIterator.scala
@@ -1,12 +1,12 @@
 package is.hail.utils
 
 class SetupIterator[T](it: Iterator[T], setup: () => Unit) extends Iterator[T] {
-  private[this] var needsSetup = false
+  private[this] var needsSetup = true
 
   private[this] def maybeSetup(): Unit = {
     if (needsSetup) {
       setup()
-      needsSetup = true
+      needsSetup = false
     }
   }
 
