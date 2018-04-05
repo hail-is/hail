ef3333fff89bacda40c998e62941ae25d319f0ef
Daniel King <daniel.zidan.king@gmail.com>
2018-04-05 15:59:00 -0400

    Context rdd coalesce and shuffle (#3282)
    * shuffle compiles and seems to work OK
    
    * rebase fixes
    
    * fix use of weaken
    
    * remove uses of explicit context constructor
    
    * coalesce does not end a lifetime if shuffle=False

---
+++ src/main/scala/is/hail/sparkextras/ContextPairRDDFunctions.scala
@@ -0,0 +1,18 @@
+package is.hail.sparkextras
+
+import org.apache.spark._
+import org.apache.spark.rdd._
+
+import scala.reflect.ClassTag
+
+class ContextPairRDDFunctions[C <: AutoCloseable, K: ClassTag, V: ClassTag](
+  crdd: ContextRDD[C, (K, V)]
+) {
+  def shuffle(p: Partitioner, o: Ordering[K]): ContextRDD[C, (K, V)] =
+    // NB: the run marks the end of a context lifetime, the next context
+    // lifetime starts after the shuffle (and potentially on a different machine
+    // than the one that owned the previous lifetime)
+    ContextRDD.weaken(
+      new ShuffledRDD[K, V, V](crdd.run, p).setKeyOrdering(o),
+      crdd.mkc)
+}
