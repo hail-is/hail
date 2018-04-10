d6ea029371f931fcd4c00c52c02f13240e471c4b
Daniel King <dking@broadinstitute.org>
2018-04-10 17:23:02 -0400

    introduce resettablecontext
    also add rvb to rvdcontext and give it children

--- src/main/scala/is/hail/rvd/RVDContext.scala
+++ src/main/scala/is/hail/rvd/RVDContext.scala
@@ -1,6 +1,9 @@
 package is.hail.rvd
 
-import is.hail.annotations.Region
+import is.hail.annotations.{Region, RegionValueBuilder}
+import is.hail.sparkextras.ResettableContext
+
+import scala.collection.mutable
 
 object RVDContext {
   def default: RVDContext = new RVDContext(Region())
@@ -10,11 +13,48 @@ object RVDContext {
 
 // NB: must be *Auto*Closeable because calling close twice is undefined behavior
 // (see AutoCloseable javadoc)
-class RVDContext(r: Region) extends AutoCloseable {
+class RVDContext(r: Region) extends ResettableContext {
+  private[this] val children: mutable.ArrayBuffer[AutoCloseable] = new mutable.ArrayBuffer()
+
+  private[this] def own(child: AutoCloseable): Unit = children += child
+
+  own(r)
+
+  def freshContext: RVDContext = {
+    val ctx = RVDContext.default
+    own(ctx)
+    ctx
+  }
+
   def region: Region = r // lifetime: element
 
   def partitionRegion: Region = r // lifetime: partition
 
+  private[this] val theRvb = new RegionValueBuilder(r)
+  def rvb = theRvb
+
+  def reset(): Unit = {
+    r.clear()
+  }
+
   // frees the memory associated with this context
-  def close(): Unit = ()
+  def close(): Unit = {
+    var e: Exception = null
+    var i = 0
+    while (i < children.size) {
+      try {
+        children(i).close()
+      } catch {
+        case e2: Exception =>
+          if (e == null)
+            e = e2
+          else
+            e.addSuppressed(e2)
+      }
+      i += 1
+    }
+
+    if (e != null)
+      throw e
+  }
 }
