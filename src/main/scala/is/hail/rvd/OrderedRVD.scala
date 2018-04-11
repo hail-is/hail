93eae01f781b3913f9ca273e777410d7363b83f2
patrick-schultz <pschultz@broadinstitute.org>
2018-04-11 15:37:35 -0400

    Ordered tables
    Make key_by order the table
    Make annotateRowsTable not shuffle if table already ordered

--- src/main/scala/is/hail/rvd/OrderedRVD.scala
+++ src/main/scala/is/hail/rvd/OrderedRVD.scala
@@ -92,8 +92,6 @@ class OrderedRVD(
     require(ordType.rowType == typ.rowType)
     require(ordType.kType isPrefixOf typ.kType)
     require(newPartitioner.pkType isIsomorphicTo ordType.pkType)
-    // Should remove this requirement in the future
-    require(typ.pkType isPrefixOf ordType.pkType)
 
     new OrderedRVD(
       typ = ordType,
