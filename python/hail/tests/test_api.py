39d794f238233af3bf0f98b8adcc19cc7423dc9a
Amanda Wang <wang@broadinstitute.org>
2018-04-12 18:24:03 -0400

    fix unsafe ordering on interval (#3355)
    * fix unsafeOrdering
    
    * add test

--- python/hail/tests/test_api.py
+++ python/hail/tests/test_api.py
@@ -1061,6 +1061,23 @@ class MatrixTests(unittest.TestCase):
         self.assertEqual(ht.order_by(hl.asc('idx')).idx.collect(), list(range(10)))
         self.assertEqual(ht.order_by(hl.desc('idx')).idx.collect(), list(range(10))[::-1])
 
+    def test_order_by_intervals(self):
+        intervals = {0: hl.Interval(0, 3, includes_start=True, includes_end=False),
+                     1: hl.Interval(0, 4, includes_start=True, includes_end=True),
+                     2: hl.Interval(1, 4, includes_start=True, includes_end=False),
+                     3: hl.Interval(0, 4, includes_start=False, includes_end=False),
+                     4: hl.Interval(0, 4, includes_start=True, includes_end=False)}
+        ht = hl.utils.range_table(5)
+
+        ht = ht.annotate_globals(ilist = intervals)
+        ht = ht.annotate(interval = ht['ilist'][ht['idx']])
+        ht = ht.order_by(ht['interval'])
+
+        ordered = ht['interval'].collect()
+        expected = [intervals[i] for i in [0, 4, 1, 3, 2]]
+
+        self.assertEqual(ordered, expected)
+
     def test_range_matrix_table(self):
         mt = hl.utils.range_matrix_table(13, 7, n_partitions = 5)
         self.assertEqual(mt.globals.dtype, hl.tstruct())
