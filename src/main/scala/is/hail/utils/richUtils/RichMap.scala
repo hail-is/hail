b0eb7b564fcee2f3751099fff78935e34590b3f1
tpoterba <tpoterba@broadinstitute.org>
2018-04-16 10:12:37 -0400

    Delete a bunch of unused utility code

--- src/main/scala/is/hail/utils/richUtils/RichMap.scala
+++ src/main/scala/is/hail/utils/richUtils/RichMap.scala
@@ -1,8 +1,6 @@
 package is.hail.utils.richUtils
 
 class RichMap[K, V](val m: Map[K, V]) extends AnyVal {
-  def mapValuesWithKeys[T](f: (K, V) => T): Map[K, T] = m map { case (k, v) => (k, f(k, v)) }
-
   def force = m.map(identity) // needed to make serializable: https://issues.scala-lang.org/browse/SI-7005
 
   def outerJoin[V2](other: Map[K, V2]): Map[K, (Option[V], Option[V2])] = {
