9d2dc8bbdb0481d7f88802adad782d900deed606
jbloom22 <jbloom@broadinstitute.org>
2018-04-16 23:50:05 -0400

    more docs, low-rank vs full-rank

--- python/hail/methods/linear_mixed_model.py
+++ python/hail/methods/linear_mixed_model.py
@@ -3,14 +3,81 @@ from hail.typecheck import *
 
 
 class LinearMixedModel(object):
-    r"""
+    r"""Class representing a linear mixed model.
+
     .. include:: ../_templates/experimental.rst
+
+    This class represents a linear model with two variance components:
 
     .. math::
 
-        y \sim \mathrm{N}\left(X \beta, \sigma^2 K + \tau^2 I\right)
+        y \sim \mathrm{N}\left(C \beta, \sigma^2 K + \tau^2 I\right)
 
-    The following attributes are set at initialization.
+    where :math:`\mathrm{N}` is an math:`n`-dimensional normal distribution and
+
+    - :math:`y` is a known vector of :math:`n` observations.
+    - :math:`C` is a known :math:`n \times k` design matrix for math:`k` fixed effects.
+    - :math:`K` is a known :math:`n \times n` positive semi-definite kernel.
+    - :math:`I` is the :math:`n \times n` identity matrix.
+    - :math:`\beta` is a :math:`k`-parameter vector of fixed effects.
+    - :math:`\sigma^2` is a the variance parameter on :math:`K`.
+    - :math:`\tau^2` is a the variance parameter on :math:`I`.
+
+    In other words, the residual covariance of observations :math:`y_i` and
+    :math:`y_j` is :math:`\sigma^2 K_{ij} + \tau^2` if math:`i=j` and
+    :math:`\sigma^2 K_{ij}` otherwise.
+
+    This model is equivalent to a
+    `mixed model <https://en.wikipedia.org/wiki/Mixed_model>`__
+    of the form
+
+    .. math::
+
+        y = C \beta + Z \nu + \epsilon
+
+    via :math:`K = ZZ^T`, where
+
+    - :math:`Z` is a known :math:`n \times m` design matrix for :math:`m` random effects.
+    - :math:`\nu` is a :math:`m`-vector of random effects drawn from :math:`\mathrm{N}\left(0, \sigma^2 I\right)`.
+    - :math:`\epsilon` is a :math:`n`-vector of random errors drawn from :math:`\mathrm{N}\left(0, \tau^2 I\right)`.
+
+    However, this class does not construct :math:`K` as the linear kernel
+    of such a mixed model. Rather, it takes as input
+    the images :math:`Py` and :math:`PC` of :math:`y` and :math:`C`, respectively,
+    under the decorrelation transformation :math:`P` given by eigenvectors of
+    :math:`K`, as well as the corresponding eigenvalues.
+
+    More precisely, the model is equivalent to the
+    linear model with diagonal covariance
+
+    .. math::
+
+        Py \sim \mathrm{N}\left((PC) \beta, \sigma_g^2 (\gamma S + I)\right)
+
+    where
+
+    - :math:`K` has eigendecomposition :math:`USU^T`.
+    - :math:`P: \mathbb{R}^n \rightarrow \mathbb{R}^r` is the isometry :math:`U^T`.
+    - :math:`\gamma` is the ratio of variance parameters :math:`\frac{\sigma^2}{\tau^2}`.
+
+    Hence, the triple :math:`(Py, PC, S)` is sufficient to fit the original model,
+    and we term this approach the `full-rank strategy`.
+
+    The class also provides an efficient `low-rank strategy` to fit the original
+    model with :math:`K` replaced by its rank-:math:`r` approximation. In this case,
+    the quintuple :math:`(P_r y, P_r C, S_r, y, C)` is sufficient , where
+
+    - :math:`P_r: \mathbb{R}^n \rightarrow \mathbb{R}^r` is the projection
+      corresponding to the top :math:`r` eigenvectors.
+    - :math:`S_r` is the corresponding vector of non-zero eigenvalues.
+
+    Note that when :math:`K` actually has rank :math:`r`, this 
+    low-rank strategy is not an approximation.
+    This situation arises, for example, when :math:`K` is the linear
+    kernel of a mixed model with fewer random effects than observations.
+
+    The following attributes are set at initialization for the low-rank strategy.
+    For the full-rank strategy, `y` and `c` are not set and :math:`r` must equal :math:`n`.
 
     .. list-table::
       :header-rows: 1
@@ -27,21 +94,21 @@ class LinearMixedModel(object):
       * - `r`
         - int
         - Number of mixed effects
+      * - `py`
+        - numpy.ndarray
+        - Projected vector :math:`Py` with shape :math:`(r)`
+      * - `pc`
+        - numpy.ndarray
+        - Projected design matrix :math:`PC` with shape :math:`(r, k)`
+      * - `s`
+        - numpy.ndarray
+        - Eigenvalues vector of projection with shape :math:`(r)`
       * - `y`
         - numpy.ndarray
         - Response vector with shape :math:`(n)`
       * - `c`
         - numpy.ndarray
-        - Covariate matrix of fixed effects with shape :math:`(n, k)`
-      * - `py`
-        - numpy.ndarray
-        - Projected response vector :math:`Py` with shape :math:`(r)`
-      * - `pc`
-        - numpy.ndarray
-        - Projected covariate matrix :math:`PC` with shape :math:`(r, k)`
-      * - `s`
-        - numpy.ndarray
-        - Eigenvalues vector of projection with shape :math:`(r)`
+        - Design matrix with shape :math:`(n, k)`
 
     :meth:`fit` uses `restricted maximum likelihood
     <https://en.wikipedia.org/wiki/Restricted_maximum_likelihood>`__ (REML)
@@ -76,14 +143,14 @@ class LinearMixedModel(object):
         - float
         - log of REML
 
-    Estimation proceed by minimizing the function :meth:`compute_neg_log_reml`
+    Estimation proceeds by minimizing the function :meth:`compute_neg_log_reml`
     with respect to the parameter :math:`\log{\gamma}` governing the (log)
     ratio of the variance parameters :math:`\sigma^2` and :math:`\tau^2`. For
     any fixed ratio, the REML estimate and value have closed form solutions.
 
     The above linear model is equivalent to its augmentation by an
-    additional fixed effect :math:`x` under the null hypothesis that the
-    coefficient of :math:`x` is zero. Once :meth:`fit` has been used to set :math:`\log{\gamma}` for the null model,
+    additional covariate :math:`x` under the null hypothesis that the corresponding
+    fixed effect is zero. Once :meth:`fit` has been used to set :math:`\log{\gamma}` for the null model,
     the methods :meth:`fit_local_rows` and :meth:`fit_rows`
     may be used to test this null hypothesis for each of a collection of rows
     using the likelihood ratio test with both the null and alternative models
@@ -91,21 +158,24 @@ class LinearMixedModel(object):
     The test statistic :math:`\chi^2` is given by :math:`n` times the log ratio of
     the squared residuals and follows a chi-squared distribution with one degree of freedom.
 
+    For these methods, the vector :math:`Px` is sufficient for the full-rank strategy, whereas the low-rank strategy
+    depends on both :math:`P_r x` and :math:`x`.
+
     Parameters
     ----------
-    y: :class:`numpy.ndarray`
-        Response vector.
-    c: :class:`numpy.ndarray`
-        Covariate matrix
     py: :class:`numpy.ndarray`
         Projected response vector.
     pc: :class:`numpy.ndarray`
-        Projected covariate matrix.
+        Projected design matrix.
     s: :class:`numpy.ndarray`
         Eigenvalues vector.
+    y: :class:`numpy.ndarray`, optional.
+        Response vector, included for low-rank strategy.
+    c: :class:`numpy.ndarray`, optional.
+        Design matrix, included for low-rank strategy.
     """
     @typecheck_method(y=np.ndarray, c=np.ndarray, py=np.ndarray, pc=np.ndarray, s=np.ndarray)
-    def __init__(self, y, c, py, pc, s):
+    def __init__(self, py, pc, s, y=None, c=None):
         assert y.ndim == 1 and c.ndim == 2 and py.ndim == 1 and pc.ndim == 2 and s.ndim == 1
 
         self.n, self.k = c.shape
@@ -338,19 +408,19 @@ class LinearMixedModel(object):
             - Type
             - Value
           * - `idx`
-            - Int64
+            - int64
             - :math:`\beta`
           * - `beta`
-            - Float64
+            - float64
             - :math:`\beta`
           * - `sigma_sq`
-            - Float64
+            - float64
             - :math:`\sigma^2`
           * - `chi_sq`
-            - Float64
+            - float64
             - :math:`\chi^2`
           * - `p_value`
-            - Float64
+            - float64
             - p-value
 
         Parameters
@@ -405,7 +475,7 @@ class LinearMixedModel(object):
         from hail.linalg import _jarray_from_ndarray, _breeze_from_ndarray
 
         if not self._fitted:
-            raise Exception("Ratio of variance parameters is undefined. Run 'fit' first")
+            raise Exception("Ratio of variance parameters is undefined. Run 'fit' first.")
 
         self._scala_model = Env.hail().stats.LinearMixedModel.apply(
             Env.hc()._jhc,
