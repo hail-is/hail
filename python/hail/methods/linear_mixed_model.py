61486490177db4ab10c5388c8348367ac506e7b9
jbloom22 <jbloom@broadinstitute.org>
2018-04-18 12:49:21 -0400

    more docs

--- python/hail/methods/linear_mixed_model.py
+++ python/hail/methods/linear_mixed_model.py
@@ -8,25 +8,25 @@ class LinearMixedModel(object):
 
     .. include:: ../_templates/experimental.rst
 
-    This class represents a linear model with two variance components:
+    :class:`LinearMixedModel` represents a linear model of the form
 
     .. math::
 
-        y \sim \mathrm{N}\left(C \beta, \, \sigma^2 K + \tau^2 I\right)
+        y \sim \mathrm{N}(C \beta, \, \sigma^2 K + \tau^2 I)
 
-    where :math:`\mathrm{N}` is an :math:`n`-dimensional normal distribution and
+    where
 
+    - :math:`\mathrm{N}` is a :math:`n`-dimensional normal distribution.
     - :math:`y` is a known vector of :math:`n` observations.
     - :math:`C` is a known :math:`n \times k` design matrix for :math:`k` fixed effects.
     - :math:`K` is a known :math:`n \times n` positive semi-definite kernel.
     - :math:`I` is the :math:`n \times n` identity matrix.
     - :math:`\beta` is a :math:`k`-parameter vector of fixed effects.
-    - :math:`\sigma^2` is a the variance parameter on :math:`K`.
-    - :math:`\tau^2` is a the variance parameter on :math:`I`.
+    - :math:`\sigma^2` is the variance parameter on :math:`K`.
+    - :math:`\tau^2` is the variance parameter on :math:`I`.
 
-    In other words, the residual covariance of observations :math:`y_i` and
-    :math:`y_j` is :math:`\sigma^2 K_{ij} + \tau^2` if :math:`i=j` and
-    :math:`\sigma^2 K_{ij}` otherwise.
+    In particular, the residuals for the :math:`i^\mathit{th}` and :math:`j^\mathit{th}`
+    observations have covariance :math:`\sigma^2 K_{ij}` for :math:`i \neq j`.
 
     This model is equivalent to a
     `mixed model <https://en.wikipedia.org/wiki/Mixed_model>`__
@@ -36,54 +36,62 @@ class LinearMixedModel(object):
 
         y = C \beta + Z \nu + \epsilon
 
-    via :math:`K = ZZ^T`, where
+    by setting :math:`K = ZZ^T` where
 
     - :math:`Z` is a known :math:`n \times r` design matrix for :math:`r` random effects.
-    - :math:`\nu` is a :math:`r`-vector of random effects drawn from :math:`\mathrm{N}\left(0, \sigma^2 I\right)`.
-    - :math:`\epsilon` is a :math:`n`-vector of random errors drawn from :math:`\mathrm{N}\left(0, \tau^2 I\right)`.
+    - :math:`\nu` is a :math:`r`-vector of random effects drawn from :math:`\mathrm{N}(0, \sigma^2 I)`.
+    - :math:`\epsilon` is a :math:`n`-vector of random errors drawn from :math:`\mathrm{N}(0, \tau^2 I)`.
 
-    However, this class does not itself realize :math:`K` as the linear kernel
-    with respect to random effects. Rather, it takes as input
-    the images :math:`Py` and :math:`PC` of :math:`y` and :math:`C`, respectively,
-    under the decorrelation transformation :math:`P` whose rows are eigenvectors of
-    :math:`K`, as well as the corresponding eigenvalues.
-
-    More precisely, the model is equivalent to the
-    linear model with diagonal covariance
+    However, :class:`LinearMixedModel` does not itself realize :math:`K` as a linear kernel
+    with respect to random effects, nor does it take :math:`K` explicitly as input. Rather,
+    via the eigendecomposion :math:`K = P^T S P`, the the class leverages a third, decorrelated
+    form of the model
 
     .. math::
 
-        Py \sim \mathrm{N}\left((PC) \beta, \, \sigma_g^2 (\gamma S + I)\right)
+        Py \sim \mathrm{N}(PC \beta, \, \sigma_g^2 (\gamma S + I))
 
     where
 
-    - :math:`K` has eigendecomposition :math:`P^T S P`.
-    - :math:`\gamma` is the ratio of variance parameters, :math:`\frac{\sigma^2}{\tau^2}`.
+    - :math:`P: \mathbb{R}^n \rightarrow \mathbb{R}^n` is an orthonormal transformation
+      that decorrelates the fixed effects. The rows of :math:`P` are an eigenbasis for :math:`K`.
+    - :math:`S` is the :math:`n \times n` diagonal matrix of corresponding eigenvalues.
+    - :math:`\gamma = \frac{\sigma^2}{\tau^2}` is the ratio of variance parameters.
 
-    Hence, the triple :math:`(Py, PC, S)` is sufficient to fit the original model.
-    This triple corresponds to the default "full-rank" initialization of the class.
+    Hence, the triple :math:`(Py, PC, S)` determines the probability
+    of the observations for any choice of model parameters, and is
+    therefore sufficient for inference.
+    This triple, with S encoded as a vector, is the default
+    ("full-rank") initialization of the class.
 
-    The class also provides an efficient strategy to fit the
-    model with :math:`K` replaced by its rank-:math:`r` approximation
+    :class:`LinearMixedModel` also provides an efficient strategy to fit the
+    model above with :math:`K` replaced by its rank-:math:`r` approximation
     :math:`K_r = P_r^T S_r P_r` where
 
-    - :math:`P_r: \mathbb{R}^n \rightarrow \mathbb{R}^r` is the projection
-      whose rows are the top :math:`r` eigenvectors of :math:`K`.
-    - :math:`S_r` is the corresponding diagonal matrix of non-zero eigenvalues.
+    - :math:`P_r: \mathbb{R}^n \rightarrow \mathbb{R}^r` has orthonormal rows
+      consisting of the top :math:`r`  eigenvectors of :math:`K`.
+    - :math:`S_r` is the `r \times r` diagonal matrix of corresponding
+      non-zero eigenvalues.
 
-    The quintuple :math:`(P_r y, P_r C, S_r, y, C)` is sufficient to
-    fit the low-rank model and corresponds to the low-rank
-    initialization of the class.
+    For this low-rank model, the quintuple :math:`(P_r y, P_r C, S_r, y, C)`
+    is similarly sufficient for inference and corresponds to the "low-rank"
+    initialization of the class. Morally, :math:`y` and :math:`C` are
+    required for low-rank inference because the diagonal :math:`\gamma S + I`
+    is always full-rank.
 
-    Note that when :math:`K` actually has rank :math:`r`, then :math:`K` equals :math:`K_r`
+    If :math:`K` actually has rank :math:`r`, then :math:`K = K_r`
     and the low-rank and full-rank models are equivalent.
-    Hence low-rank inference provides a more efficient, equally exact algorithm for
-    fitting the full-rank model. This situation arises, for example, when
-    :math:`K` is the linear kernel of a mixed model with fewer random
-    effects than observations.
+    Hence low-rank inference provides a more efficient, equally-exact
+    algorithm for fitting the full-rank model.
+    This situation arises, for example, when :math:`K` is the linear kernel
+    of a mixed model with fewer random effects than observations.
 
-    The following attributes are set for low-rank initialization.
-    For full-rank initialization, :math:`n` must equal :math:`r`
+    Even when :math:`K` has full rank, using a lower-rank approximation may
+    be an effective from of regularization, in addition to boosting
+    computational efficiency.
+
+    For low-rank initialization, all of the following class attributes are 
+    set. For full-rank initialization, :math:`n` must equal :math:`r`
     and `y` and `c` are not set.
 
     .. list-table::
@@ -103,7 +111,7 @@ class LinearMixedModel(object):
         - Number of mixed effects
       * - `py`
         - numpy.ndarray
-        - Decorrelated response vector :math:`Py` with shape :math:`(r)`
+        - Projected response vector :math:`Py` with shape :math:`(r)`
       * - `pc`
         - numpy.ndarray
         - Decorrelated design matrix :math:`PC` with shape :math:`(r, k)`
@@ -150,34 +158,42 @@ class LinearMixedModel(object):
     Estimation proceeds by minimizing the function :meth:`compute_neg_log_reml`
     with respect to the parameter :math:`\log{\gamma}` governing the (log)
     ratio of the variance parameters :math:`\sigma^2` and :math:`\tau^2`. For
-    any fixed ratio, the REML estimate and value have closed form solutions.
+    any fixed ratio, the REML estimate and log likelihood have closed form solutions.
 
-    The above linear model is equivalent to its augmentation by an
-    additional covariate :math:`x` under the null hypothesis that the corresponding
-    fixed effect is zero. Once :meth:`fit` has been used to set :math:`\log{\gamma}` for the null model,
+    The model is also equivalent to its augmentation
+
+    .. math::
+
+        y \sim \mathrm{N}\left(x_\star\beta_\star + C \beta, \, \sigma^2 K + \tau^2 I\right)
+
+    by an additional covariate of interest :math:`x_\star` under the
+    null hypothesis that the corresponding fixed effect :math:`\beta_\star` is zero.
+
+    After running :meth:`fit` to fit the null model,
     the methods :meth:`fit_alternative` and :meth:`fit_alternative_numpy`
-    may be used to test this null hypothesis for each of a collection of
-    augmentation covariates
-    using the likelihood ratio test with both the null and alternative models
-    constrained by :math:`\log{\gamma}`.
-    The test statistic :math:`\chi^2` is given by :math:`n` times the log ratio of
+    may be used to test the null hypothesis :math:`\beta_\star = 0` versus
+    the alternative hypothesis :math:`\beta_\star \neq 0` for
+    each :math:`n`-vector :math:`x_\star` in a collection of augmentations.
+    Testing uses the likelihood ratio test with both the null and alternative models
+    constrained by the REML estimate of :math:`\log{\gamma}` under the null hypothesis.
+    The test statistic :math:`\chi^2` equals :math:`n` times the log ratio of
     the squared residuals and follows a chi-squared distribution with one degree of freedom.
-    For these methods, the decorrelated vector :math:`Px` is sufficient for full-rank inference,
-    whereas the low-rank inference depends on both :math:`P_r x` and :math:`x`.
+    For these methods, the vector :math:`Px` is sufficient for full-rank inference,
+    whereas the low-rank inference requires both :math:`P_r x` and :math:`x`.
 
     Parameters
     ----------
     py: :class:`numpy.ndarray`
-        Decorrelated response vector with shape :math:`(r)`.
+        Projected response vector with shape :math:`(r)`.
     pc: :class:`numpy.ndarray`
         Decorrelated design matrix with shape :math:`(r, k)`.
     s: :class:`numpy.ndarray`
         Eigenvalues vector with shape :math:`(r)`.
-    y: :class:`numpy.ndarray`, optional.
+    y: :class:`numpy.ndarray`, optional
         Response vector with shape :math:`(n)`.
         Required for low-rank inference.
-    c: :class:`numpy.ndarray`, optional.
-        Design matrix shape :math:`(n, k)`.
+    c: :class:`numpy.ndarray`, optional
+        Design matrix with shape :math:`(n, k)`.
         Required for low-rank inference.
     """
     @typecheck_method(y=np.ndarray, c=np.ndarray, py=np.ndarray, pc=np.ndarray, s=np.ndarray)
@@ -248,8 +264,8 @@ class LinearMixedModel(object):
         r"""Compute negative log REML constrained to a fixed value
         of :math:`\log{\gamma}`.
 
-        This function computes the triple :math:`(\beta, \sigma_sq, \tau^2)` with
-        :math:`\gamma = \frac{\sigma_sq}{\tau^2}` at which the restricted likelihood
+        This function computes the triple :math:`(\beta, \sigma^2, \tau^2)` with
+        :math:`\gamma = \frac{\sigma^2}{\tau^2}` at which the restricted likelihood
         is maximized and returns the negative of the log of the
         restricted likelihood at these parameters, shifted by a constant whose value is
         independent of the input.
@@ -267,14 +283,14 @@ class LinearMixedModel(object):
         log_gamma: :obj:`float`
             Value of :math:`\log{\gamma}`.
         return_parameters:
-            If ``True``, also return :math:`\beta`, :math:`\sigma_sq`,
+            If ``True``, also return :math:`\beta`, :math:`\sigma^2`,
             and :math:`\tau^2`.
 
         Returns
         -------
         :obj:`float` or (:obj:`float`, :class:`numpy.ndarray`, :obj:`float`, :obj:`float`)
-            If `return_parameters` is ``False``, returns negative REML.
-            Otherwise, returns negative REML, :math:`\beta`, :math:`\sigma^2`,
+            If `return_parameters` is ``False``, returns (shifted) negative log REML.
+            Otherwise, returns (shifted) negative log REML, :math:`\beta`, :math:`\sigma^2`,
             and :math:`\tau^2`.
         """
         from scipy.linalg import solve, LinAlgError
@@ -314,25 +330,19 @@ class LinearMixedModel(object):
     def fit(self, log_gamma=None, bounds=(-8.0, 8.0), tol=1e-8, maxiter=500):
         r"""Find the triple :math:`(\beta, \sigma^2, \tau^2)` maximizing REML.
 
+        This method sets the attributes `beta`, `sigma_sq`, `tau_sq`, `gamma`,
+        `log_gamma`, and `h_sq` as described in the top-level class documentation.
+
         If `log_gamma` is provided, :meth:`fit` finds the REML solution
         with :math:`\log{\gamma}` constrained to this value.
-
-        Otherwise, :meth:`fit` first uses the ``bounded`` method of
-        `scipy.optimize.minimize_scalar
-        <https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize_scalar.html>`__
-        to find the value of `:math:`\log{\gamma}` that minimizes
-        :meth:`compute_neg_log_reml`. The attribute
-        `optimize_result` is then set to the returned value of type
-        `scipy.optimize.OptimizeResult
+        Otherwise, :meth:`fit` searches for the value of :math:`\log{\gamma}`
+        that minimizes :meth:`compute_neg_log_reml`, and also sets the attribute
+        `optimize_result` of type `scipy.optimize.OptimizeResult
         <https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.OptimizeResult.html>`__.
-
-        Either way, :meth:`fit` computes and sets the attributes `beta`, `sigma_sq`,
-        `tau_sq`, `gamma`, `log_gamma`, and `h_sq` as described in the top-level
-        class documentation.
 
         Parameters
         ----------
-        log_gamma: :obj:`float`, optional.
+        log_gamma: :obj:`float`, optional
             If provided, the solution is constrained to have this value of
             :math:`\log{\gamma}`.
         bounds: :obj:`float`, :obj:`float`
@@ -378,7 +388,7 @@ class LinearMixedModel(object):
 
     @typecheck_method(path_pxt=str, path_xt=nullable(str), partition_size=int)
     def fit_alternative(self, path_pxt, path_xt=None, partition_size=1024):
-        r"""Fit alternative model for augmented design matrices in parallel.
+        r"""Fit and test alternative model for each augmented design matrix in parallel.
 
         The resulting Table has the following fields:
 
@@ -390,10 +400,10 @@ class LinearMixedModel(object):
             - Value
           * - `idx`
             - int64
-            - :math:`\beta`
+            - Index of augmented design matrix.
           * - `beta`
             - float64
-            - :math:`\beta`
+            - :math:`\beta_\star`
           * - `sigma_sq`
             - float64
             - :math:`\sigma^2`
@@ -407,12 +417,16 @@ class LinearMixedModel(object):
         Parameters
         ----------
         path_pxt: :obj:`str`
-            Path to decorrelated block matrix with transpose of shape :math:`(r,m)`.
-        path_xt: :obj:`str`, optional.
-            Path to block matrix with transpose of shape :math:`(n,m)`.
+            Path to the transpose of the projected block matrix :math:`PX`.
+            The transposed matrix has shape :math:`(m, r)`.
+            Each row is a projected augmentation :math:`P x_\star`.
+        path_xt: :obj:`str`, optional
+            Path to the transpose of block matrix :math:`X`.
+            The transposed matrix has shape :math:`(m , n)`.
+            Each row is an augmentation :math:`x_\star`.
             Required for low-rank inference.
         partition_size: :obj:`int`
-            Number of alternative per partition.
+            Number of rows per partition.
 
         Returns
         -------
@@ -431,7 +445,7 @@ class LinearMixedModel(object):
 
     @typecheck_method(px=np.ndarray, x=nullable(np.ndarray))
     def fit_alternative_numpy(self, px, x=None):
-        r"""Fit alternative model for augmented design matrices.
+        r"""Fit and test alternative model for each augmented design matrix.
 
         The resulting pandas DataFrame has the following fields.
 
@@ -443,7 +457,7 @@ class LinearMixedModel(object):
             - Value
           * - `beta`
             - float
-            - :math:`\beta`
+            - :math:`\beta_\star`
           * - `sigma_sq`
             - float
             - :math:`\sigma^2`
@@ -457,11 +471,11 @@ class LinearMixedModel(object):
         Parameters
         ----------
         px: :class:`numpy.ndarray`
-            Matrix with shape :math:`(r,m)`.
-            Columns are decorrelated augmentations.
-        x: :class:`numpy.ndarray`, optional.
-            Matrix with shape :math:`(n,m)`.
-            Columns are augmentations.
+            Matrix :math:`PX` with shape :math:`(r, m)`.
+            Each column is a projected augmentation :math:`P x_\star`.
+        x: :class:`numpy.ndarray`, optional
+            Matrix :math:`X` with shape :math:`(n, m)`.
+            Each column is an augmentation :math:`x_\star`.
             Required for low-rank inference.
 
         Returns
