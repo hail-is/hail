a4bcc36aa8af101bf595f5e229e2e0b7f0a76209
Tim Poterba <tpoterba@gmail.com>
2018-04-19 17:13:36 -0400

    Fix infinite loop from testing outside directory (#3408)

--- python/hail/tests/utils.py
+++ python/hail/tests/utils.py
@@ -19,8 +19,12 @@ def resource(filename):
     global _test_dir
     if _test_dir is None:
         path = '.'
+        i = 0
         while not os.path.exists(os.path.join(path, 'LICENSE')):
             path = os.path.join(path, '..')
+            i += 1
+            if i > 100:
+                raise EnvironmentError("Hail tests must be run from inside the Hail git repository")
         _test_dir = os.path.join(path, 'src', 'test', 'resources')
 
     return os.path.join(_test_dir, filename)
@@ -30,8 +34,12 @@ def doctest_resource(filename):
     global _doctest_dir
     if _doctest_dir is None:
         path = '.'
+        i = 0
         while not os.path.exists(os.path.join(path, 'LICENSE')):
             path = os.path.join(path, '..')
+            i += 1
+            if i > 100:
+                raise EnvironmentError("Hail tests must be run from inside the Hail git repository")
         _doctest_dir = os.path.join(path, 'python', 'hail', 'docs', 'data')
 
     return os.path.join(_doctest_dir, filename)
