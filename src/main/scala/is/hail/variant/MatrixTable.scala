19b28cb2f4098557c39912a4a9e6e206f9381ab0
cseed <cotton@alum.mit.edu>
2018-04-20 09:49:05 -0400

    use FilterColsIR in dropCols (#3410)

--- src/main/scala/is/hail/variant/MatrixTable.scala
+++ src/main/scala/is/hail/variant/MatrixTable.scala
@@ -1165,7 +1165,7 @@ class MatrixTable(val hc: HailContext, val ast: MatrixIR) {
   def deleteVA(path: List[String]): (Type, Deleter) = rowType.delete(path)
 
   def dropCols(): MatrixTable =
-    copyAST(ast = FilterCols(ast, Const(null, false, TBoolean())))
+    copyAST(ast = FilterColsIR(ast, ir.False()))
 
   def dropRows(): MatrixTable = copy2(rvd = OrderedRVD.empty(sparkContext, matrixType.orvdType))
 
