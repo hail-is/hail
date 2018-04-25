10545cf183a6bed469121044ab6bb1d228bec8b0
Daniel King <dking@broadinstitute.org>
2018-04-25 17:18:43 -0400

    formatting

--- src/main/scala/is/hail/sparkextras/GeneralRDD.scala
+++ src/main/scala/is/hail/sparkextras/GeneralRDD.scala
@@ -5,19 +5,25 @@ import org.apache.spark.rdd.RDD
 
 import scala.reflect.ClassTag
 
-case class GeneralRDDPartition[T](index: Int, partitionInputs: Array[(Int, Partition)]) extends Partition
+case class GeneralRDDPartition(
+  partitionInputs: Array[(Int, Partition)],
+  index: Int
+) extends Partition
 
-class GeneralRDD[T](@transient var sc: SparkContext,
-  var rdds: Array[RDD[T]],
-  var inputs: Array[Array[(Int, Int)]])(implicit tct: ClassTag[T]) extends RDD[Array[Iterator[T]]](sc, Nil) {
+class GeneralRDD[T: ClassTag](
+  sc: SparkContext,
+  rdds: Array[RDD[T]],
+  inputs: Array[Array[(Int, Int)]]
+) extends RDD[Array[Iterator[T]]](sc, Nil) {
 
   override def getPartitions: Array[Partition] = {
     // Do not call getPartitions here!
-    val parentPartitions = rdds.zipWithIndex.map { case (rdd, i) => (i, rdd.partitions) }.toMap
-    inputs.zipWithIndex.map { case (input, i) =>
-      val partitionInputs = input.map { case (rddIndex, partitionIndex) => (rddIndex, parentPartitions(rddIndex)(partitionIndex)) }
-      new GeneralRDDPartition[T](i, partitionInputs)
-    }
+    val parentPartitions = rdds.map(_.partitions)
+
+    inputs.map { _.map { case (rddIndex, partitionIndex) =>
+      (rddIndex, parentPartitions(rddIndex)(partitionIndex)) }
+    } .zipWithIndex
+      .map { case (x, i) => new GeneralRDDPartition(x, i) }
   }
 
   override def compute(
@@ -25,24 +31,22 @@ class GeneralRDD[T](@transient var sc: SparkContext,
     context: TaskContext
   ): Iterator[Array[Iterator[T]]] = {
     // Do not call partitions or getPartitions here!
-    val gp = split.asInstanceOf[GeneralRDDPartition[T]]
-    Iterator.single(gp.partitionInputs.map { case (rddIndex, partition) =>
-      val rdd = rdds(rddIndex)
-      rdd.iterator(partition, context)
-    })
+    val gp = split.asInstanceOf[GeneralRDDPartition]
+    Iterator.single(
+      gp.partitionInputs.map { case (rddIndex, partition) =>
+        rdds(rddIndex).iterator(partition, context) } )
   }
 
   override def getDependencies: Seq[Dependency[_]] = {
     inputs
       .zipWithIndex
-      .flatMap { case (input, i) => input.map { case (rddIndex, partitionIndex) => (rddIndex, partitionIndex, i) } }
+      .flatMap { case (input, i) =>
+        input.map { case (rddIndex, partitionIndex) => (rddIndex, partitionIndex, i) } }
       .groupBy(_._1)
       .map { case (rddIndex, x) =>
         new NarrowDependency[T](rdds(rddIndex)) {
           override def getParents(partitionId: Int): Seq[Int] =
-            x.filter {
-              partitionId == _._3
-            }.map(_._2)
+            x.filter(partitionId == _._3).map(_._2)
         }
       }.toSeq
   }
