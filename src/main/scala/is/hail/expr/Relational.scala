6f7f5b7a81d12a0c5ccefa7adff92c1af3ddcecf
maccum <35241112+maccum@users.noreply.github.com>
2018-04-26 16:15:51 -0400

    move MatrixTable entriesTable() to IR (#3440)
    * move MatrixTable entriesTable() to IR
    
    * address comments
    
    * fix

--- src/main/scala/is/hail/expr/Relational.scala
+++ src/main/scala/is/hail/expr/Relational.scala
@@ -53,6 +53,8 @@ case class MatrixValue(
 
   def rowsTableValue: TableValue = TableValue(typ.rowsTableType, globals, rowsRVD())
 
+  def entriesTableValue: TableValue = TableValue(typ.entriesTableType, globals, entriesRVD())
+  
   private def writeCols(path: String, codecSpec: CodecSpec) {
     val hc = HailContext.get
     val hadoopConf = hc.hadoopConf
@@ -198,6 +200,53 @@ case class MatrixValue(
 
     new UnpartitionedRVD(signature, hc.sc.parallelize(colValues.value.toArray.map(_.asInstanceOf[Row]))
       .mapPartitions(_.toRegionValueIterator(signature)))
+  }
+
+  def entriesRVD(): RVD = {
+    val resultStruct = typ.entriesTableType.rowType
+    val fullRowType = typ.rvRowType
+    val localEntriesIndex = typ.entriesIdx
+    val localEntriesType = typ.entryArrayType
+    val localColType = typ.colType
+    val localEntryType = typ.entryType
+    val localNCols = nCols
+
+    val localColValues = colValues.broadcast.value
+
+    rvd.mapPartitions(resultStruct) { it =>
+
+      val rv2b = new RegionValueBuilder()
+      val rv2 = RegionValue()
+
+      it.flatMap { rv =>
+        val rvEnd = rv.region.size
+        rv2b.set(rv.region)
+        val gsOffset = fullRowType.loadField(rv, localEntriesIndex)
+        (0 until localNCols).iterator
+          .filter { i =>
+            localEntriesType.isElementDefined(rv.region, gsOffset, i)
+          }
+          .map { i =>
+            rv.region.clear(rvEnd)
+            rv2b.clear()
+            rv2b.start(resultStruct)
+            rv2b.startStruct()
+
+            var j = 0
+            while (j < fullRowType.size) {
+              if (j != localEntriesIndex)
+                rv2b.addField(fullRowType, rv, j)
+              j += 1
+            }
+
+            rv2b.addInlineRow(localColType, localColValues(i).asInstanceOf[Row])
+            rv2b.addAllFields(localEntryType, rv.region, localEntriesType.elementOffsetInRegion(rv.region, gsOffset, i))
+            rv2b.endStruct()
+            rv2.set(rv.region, rv2b.end())
+            rv2
+          }
+      }
+    }
   }
 }
 
@@ -1703,6 +1752,24 @@ case class MatrixColsTable(child: MatrixIR) extends TableIR {
     val ctv = mv.colsTableValue
     assert(ctv.typ == typ)
     ctv
+  }
+}
+
+case class MatrixEntriesTable(child: MatrixIR) extends TableIR {
+  val children: IndexedSeq[BaseIR] = Array(child)
+
+  def copy(newChildren: IndexedSeq[BaseIR]): MatrixEntriesTable = {
+    assert(newChildren.length == 1)
+    MatrixEntriesTable(newChildren(0).asInstanceOf[MatrixIR])
+  }
+
+  val typ: TableType = child.typ.entriesTableType
+
+  def execute(hc: HailContext): TableValue = {
+    val mv = child.execute(hc)
+    val etv = mv.entriesTableValue
+    assert(etv.typ == typ)
+    etv
   }
 }
 
