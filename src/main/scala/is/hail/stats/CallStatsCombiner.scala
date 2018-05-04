395917870b78a5ef8d173f52ad0b1e8a57239e01
jigold <jigold@users.noreply.github.com>
2018-05-04 10:21:16 -0400

    Removed GC from call stats in Scala (#3496)
    * Removed GC from call stats in Scala
    
    * fix test

--- src/main/scala/is/hail/stats/CallStatsCombiner.scala
+++ src/main/scala/is/hail/stats/CallStatsCombiner.scala
@@ -9,45 +9,37 @@ object CallStats {
   def schema = TStruct(
     "AC" -> TArray(TInt32()),
     "AF" -> TArray(TFloat64()),
-    "AN" -> TInt32(),
-    "GC" -> TArray(TInt32()))
+    "AN" -> TInt32())
 }
 
-case class CallStats(alleleCount: IndexedSeq[Int], alleleFrequency: Option[IndexedSeq[Double]], alleleNumber: Int,
-  genotypeCount: IndexedSeq[Int]) {
-  require(alleleNumber == genotypeCount.sum * 2, s"AN was not equal to 2 * GC sum: $this")
+case class CallStats(alleleCount: IndexedSeq[Int], alleleFrequency: Option[IndexedSeq[Double]], alleleNumber: Int) {
   require(alleleFrequency.forall(f => D_==(f.sum, 1d)), s"AF did not sum to 1: $this")
 
-  def asAnnotation: Annotation = Annotation(alleleCount, alleleFrequency.orNull, alleleNumber, genotypeCount)
+  def asAnnotation: Annotation = Annotation(alleleCount, alleleFrequency.orNull, alleleNumber)
 }
 
 class CallStatsCombiner(val alleles: IndexedSeq[String]) extends Serializable {
   val alleleCount = new Array[Int](alleles.length)
-  val genotypeCount = new Array[Int](VariantMethods.nGenotypes(alleles.length))
 
   def merge(c: Call): CallStatsCombiner = {
     val p = Call.allelePair(c)
     alleleCount(p.j) += 1
     alleleCount(p.k) += 1
-    genotypeCount(Call.unphasedDiploidGtIndex(c)) += 1
     this
   }
 
   def merge(that: CallStatsCombiner): CallStatsCombiner = {
     alleleCount.indices.foreach { i => alleleCount(i) += that.alleleCount(i) }
-    genotypeCount.indices.foreach { i => genotypeCount(i) += that.genotypeCount(i) }
     this
   }
 
   def result(): CallStats = {
-    assert(alleleCount.sum == 2 * genotypeCount.sum)
-
     val alleleNumber = alleleCount.sum
     val alleleFrequency =
       if (alleleNumber == 0)
         None
       else
         Some(alleleCount.map(_.toDouble / alleleNumber): IndexedSeq[Double])
-    CallStats(alleleCount, alleleFrequency, alleleNumber, genotypeCount)
+    CallStats(alleleCount, alleleFrequency, alleleNumber)
   }
 }
