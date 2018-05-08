8bd12e8b6e2c1574ba8d0ac40b75e8f9c11dcf0d
wang <wang@broadinstitute.org>
2018-05-08 18:24:36 -0400

    add test

--- src/test/scala/is/hail/expr/ir/FunctionSuite.scala
+++ src/test/scala/is/hail/expr/ir/FunctionSuite.scala
@@ -27,6 +27,7 @@ class ScalaTestCompanion {
 
 object TestRegisterFunctions extends RegistryFunctions {
   def registerAll() {
+    registerIR("testIR", TInt32())(ApplyBinaryPrimOp(Add(), _, I32(1)))
     registerJavaStaticFunction("compare", TInt32(), TInt32(), TInt32())(classOf[java.lang.Integer], "compare")
     registerScalaFunction("foobar1", TInt32())(ScalaTestObject.getClass, "testFunction")
     registerScalaFunction("foobar2", TInt32())(ScalaTestCompanion.getClass, "testFunction")
@@ -86,6 +87,14 @@ class FunctionSuite {
     val f = toF[Int](ir)
     val actual = f(region)
     assert(actual == 1)
+  }
+
+  @Test
+  def testIR() {
+    val ir = lookup("testIR", TInt32())(In(0, TInt32()))
+    val f = toF[Int, Int](ir)
+    val actual = f(region, 5, false)
+    assert(actual == 6)
   }
 
   @Test
