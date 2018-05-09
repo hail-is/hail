9c5df416e9519c3e907856d707cde76ad239f3ac
Daniel King <dking@broadinstitute.org>
2018-05-09 16:29:45 -0400

    fix

--- src/test/scala/is/hail/expr/ir/FunctionSuite.scala
+++ src/test/scala/is/hail/expr/ir/FunctionSuite.scala
@@ -52,7 +52,8 @@ class FunctionSuite extends SparkSuite {
   def emitFromFB[F >: Null : TypeInfo](fb: FunctionBuilder[F]) =
     new EmitFunctionBuilder[F](fb.parameterTypeInfo, fb.returnTypeInfo, fb.packageName)
 
-  def fromHailString(hql: String): IR = Parser.parseToAST(hql, ec).toIR().get
+  def fromHailString(hql: String): IR =
+    Parser.parseToAST(hql, ec).toIROptNoWarning().get
 
   def toF[R: TypeInfo](ir: IR): AsmFunction1[Region, R] = {
     val fb = emitFromFB(FunctionBuilder.functionBuilder[Region, R])
