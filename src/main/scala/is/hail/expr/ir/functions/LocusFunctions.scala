78b80fec7d1d30aafb0175a923d8c459c35e0402
wang <wang@broadinstitute.org>
2018-05-09 12:34:33 -0400

    pull out locus-getting

--- src/main/scala/is/hail/expr/ir/functions/LocusFunctions.scala
+++ src/main/scala/is/hail/expr/ir/functions/LocusFunctions.scala
@@ -4,15 +4,20 @@ import is.hail.asm4s._
 import is.hail.expr.types._
 import is.hail.expr.types
 import is.hail.asm4s
+import is.hail.expr.ir.EmitMethodBuilder
 import is.hail.variant.Locus
 
 object LocusFunctions extends RegistryFunctions {
 
+  def getLocus(mb: EmitMethodBuilder, locus: Code[Long]): Code[Locus] = {
+    val tlocus = types.coerce[TLocus](tv("T").t)
+    asm4s.coerce[Locus](wrapArg(mb, tlocus)(locus))
+  }
+
   def registerAll() {
     registerCode("contig", tv("T", _.isInstanceOf[TLocus]), TString()) {
       case (mb, locus: Code[Long]) =>
-        val tlocus = types.coerce[TLocus](tv("T").t)
-        val locusObject = asm4s.coerce[Locus](wrapArg(mb, tlocus)(locus))
+        val locusObject = getLocus(mb, locus)
         unwrapReturn(mb, TString())(locusObject.invoke[String]("contig"))
     }
   }
