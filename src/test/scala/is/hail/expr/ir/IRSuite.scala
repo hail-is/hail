ab4c57042023578dd70a9e6ea9872ae1e246752f
wang <wang@broadinstitute.org>
2018-05-14 18:17:30 -0400

    remove nonstrictEQ

--- src/test/scala/is/hail/expr/ir/IRSuite.scala
+++ src/test/scala/is/hail/expr/ir/IRSuite.scala
@@ -7,11 +7,6 @@ import org.testng.annotations.Test
 import org.scalatest.testng.TestNGSuite
 
 class IRSuite extends TestNGSuite {
-  @Test def testNonstrictEQ() {
-    assertEvalsTo(nonstrictEQ(NA(TInt32()), NA(TInt32())), true)
-    assertEvalsTo(nonstrictEQ(I32(5), I32(5)), true)
-    assertEvalsTo(nonstrictEQ(NA(TInt32()), I32(5)), false)
-  }
 
   @Test def testArrayFilter() {
     val naa = NA(TArray(TInt32()))
