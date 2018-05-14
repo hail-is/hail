fb895828aa04b97c3208faec6fe38bd962be738a
Daniel King <dking@broadinstitute.org>
2018-05-14 13:07:35 -0400

    fix

--- src/main/scala/is/hail/rvd/OrderedRVD.scala
+++ src/main/scala/is/hail/rvd/OrderedRVD.scala
@@ -545,7 +545,7 @@ object OrderedRVD {
 
     val pkis = keys.cmapPartitionsWithIndex { (i, ctx, it) =>
       val out = if (it.hasNext)
-        Iterator(OrderedRVPartitionInfo(localType, samplesPerPartition, i, it, partitionSeed(i), ctx))
+        Iterator(OrderedRVPartitionInfo(localType, samplesPerPartition, i, it, partitionSeed(i)))
       else
         Iterator()
       out
