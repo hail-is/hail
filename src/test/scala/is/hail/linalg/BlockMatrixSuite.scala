8f8443333a8195d7b211a4ae3ae55efc86805607
jbloom22 <jbloom@broadinstitute.org>
2018-05-17 02:19:28 -0400

    fixed test

--- src/test/scala/is/hail/linalg/BlockMatrixSuite.scala
+++ src/test/scala/is/hail/linalg/BlockMatrixSuite.scala
@@ -1007,8 +1007,8 @@ class BlockMatrixSuite extends SparkSuite {
     
     TestUtils.interceptFatal(notSupported) { bm0.rowVectorDiv(v0) }
     TestUtils.interceptFatal(notSupported) { bm0.colVectorDiv(v0) }
-    TestUtils.interceptFatal("cannot multiply block matrix by scalar") { bm0 * Double.NaN }
-    TestUtils.interceptFatal("cannot divide block matrix by scalar 0.0") { bm0 / 0 }
+    TestUtils.interceptFatal("multiplication by scalar NaN") { bm0 * Double.NaN }
+    TestUtils.interceptFatal("division by scalar 0.0") { bm0 / 0 }
     
     // exponent to negative power not supported
     TestUtils.interceptFatal(notSupported) { bm0.pow(-1)}
