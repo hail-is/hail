a65fbe415166f01c47232b40393de3dff9906d76
jigold <jigold@users.noreply.github.com>
2018-05-21 11:04:14 -0400

    IR functions (#3608)
    * IR functions
    
    min(set<tnum>):tnum
    max(set<tnum>):tnum
    mean(set<tnum>):tnum
    median(set<tnum>):tnum
    argmax(array<T>):int32
    argmin(array<T>):int32
    uniqueMinIndex(array<T>):int32
    uniqueMaxIndex(array<T>):int32
    abs(tnum):tnum
    max(T,T):T
    min(T,T):T
    max(array<tnum>):tnum
    min(array<tnum>):tnum
    mean(array<tnum>):tnum
    
    * address comments
    
    * fixed compile errors
    
    * fix tests

--- src/main/scala/is/hail/expr/ir/functions/SetFunctions.scala
+++ src/main/scala/is/hail/expr/ir/functions/SetFunctions.scala
@@ -1,7 +1,7 @@
 package is.hail.expr.ir.functions
 
 import is.hail.expr.ir._
-import is.hail.expr.types.{TArray, TBoolean, TSet}
+import is.hail.expr.types._
 import is.hail.utils.FastSeq
 
 object SetFunctions extends RegistryFunctions {
@@ -68,6 +68,67 @@ object SetFunctions extends RegistryFunctions {
         // FIXME short circuit
         ApplySpecial("&&",
           FastSeq(Ref(a, TBoolean()), SetContains(w, Ref(x, t)))))
+    }
+
+    registerIR("min", TSet(tnum("T"))) { s =>
+      val t = s.typ.asInstanceOf[TSet].elementType
+      val a = genUID()
+      Let(a, ToArray(s), If(
+        ApplyComparisonOp(GT(TInt32()), ArrayLen(Ref(a, TArray(t))), I32(0)),
+        ArrayRef(Ref(a, TArray(t)), I32(0)),
+        NA(t)))
+    }
+
+    registerIR("max", TSet(tnum("T"))) { s =>
+      val t = s.typ.asInstanceOf[TSet].elementType
+      val a = genUID()
+      val size = genUID()
+      val last = genUID()
+
+      Let(a, ToArray(s),
+        Let(size, ArrayLen(Ref(a, TArray(t))),
+          If(ApplyComparisonOp(EQ(TInt32()), Ref(size, TInt32()), I32(0)),
+            NA(t),
+            Let(last, ArrayRef(Ref(a, TArray(t)), ApplyBinaryPrimOp(Subtract(), Ref(size, TInt32()), I32(1))),
+              If(IsNA(Ref(last, t)),
+                If(ApplyComparisonOp(EQ(TInt32()), Ref(size, TInt32()), I32(1)),
+                  NA(t),
+                  ArrayRef(Ref(a, TArray(t)), ApplyBinaryPrimOp(Subtract(), Ref(size, TInt32()), I32(2)))),
+                Ref(last, t))))))
+    }
+
+    registerIR("mean", TSet(tnum("T"))) { s => ArrayFunctions.mean(ToArray(s)) }
+
+    registerIR("median", TSet(tnum("T"))) { s =>
+      val t = s.typ.asInstanceOf[TSet].elementType
+      val a = genUID()
+      val size = genUID()
+      val lastIdx = genUID()
+      val midIdx = genUID()
+      val midIdx2 = genUID()
+
+      Let(a, ToArray(s),
+        Let(size, ArrayLen(Ref(a, TArray(t))),
+          If(ApplyComparisonOp(EQ(TInt32()), Ref(size, TInt32()), I32(0)),
+            NA(t),
+            If(ApplyComparisonOp(EQ(TInt32()), Ref(size, TInt32()), I32(1)),
+              ArrayRef(Ref(a, TArray(t)), I32(0)),
+              Let(lastIdx, ApplyBinaryPrimOp(Subtract(), Ref(size, TInt32()), I32(1)),
+                Let(lastIdx, If(
+                  IsNA(ArrayRef(Ref(a, TArray(t)), Ref(lastIdx, TInt32()))),
+                  ApplyBinaryPrimOp(Subtract(), Ref(lastIdx, TInt32()), I32(1)),
+                  Ref(lastIdx, TInt32())),
+                  Let(midIdx, ApplyBinaryPrimOp(RoundToNegInfDivide(), Ref(lastIdx, TInt32()), I32(2)),
+                    If(ApplyComparisonOp(EQ(TInt32()), Apply("%", FastSeq(Ref(lastIdx, TInt32()), I32(2))), I32(0)),
+                      ArrayRef(Ref(a, TArray(t)), Ref(midIdx, TInt32())), // odd number of non-missing elements
+                      Let(midIdx2, ApplyBinaryPrimOp(Add(), Ref(midIdx, TInt32()), I32(1)), // even number of non-missing elements
+                        ApplyBinaryPrimOp(
+                          RoundToNegInfDivide(),
+                          ApplyBinaryPrimOp(
+                            Add(),
+                            ArrayRef(Ref(a, TArray(t)), Ref(midIdx, TInt32())),
+                            ArrayRef(Ref(a, TArray(t)), Ref(midIdx2, TInt32()))),
+                          Cast(I32(2), t)))))))))))
     }
   }
 }
