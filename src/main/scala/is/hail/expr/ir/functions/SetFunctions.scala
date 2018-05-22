66be574d1fa3b354556223034f43304e52e49c61
Cotton Seed <cotton@alum.mit.edu>
2018-05-22 00:22:53 -0400

    back out some changes

--- src/main/scala/is/hail/expr/ir/functions/SetFunctions.scala
+++ src/main/scala/is/hail/expr/ir/functions/SetFunctions.scala
@@ -34,7 +34,7 @@ object SetFunctions extends RegistryFunctions {
           Ref(x, TArray(t))))
     }
 
-    registerIR("union", TSet(tv("T")), TSet(tv("T"))) { case (s1, s2) =>
+    registerIR("union", TSet(tv("T")), TSet(tv("T"))) { (s1, s2) =>
       val t = -s1.typ.asInstanceOf[TSet].elementType
       val x = genUID()
       ToSet(
@@ -44,7 +44,7 @@ object SetFunctions extends RegistryFunctions {
           Ref(x, TArray(t))))
     }
 
-    registerIR("intersection", TSet(tv("T")), TSet(tv("T"))) { case (s1, s2) =>
+    registerIR("intersection", TSet(tv("T")), TSet(tv("T"))) { (s1, s2) =>
       val t = -s1.typ.asInstanceOf[TSet].elementType
       val x = genUID()
       ToSet(
@@ -52,7 +52,7 @@ object SetFunctions extends RegistryFunctions {
           SetContains(s2, Ref(x, t))))
     }
 
-    registerIR("difference", TSet(tv("T")), TSet(tv("T"))) { case (s1, s2) =>
+    registerIR("difference", TSet(tv("T")), TSet(tv("T"))) { (s1, s2) =>
       val t = -s1.typ.asInstanceOf[TSet].elementType
       val x = genUID()
       ToSet(
@@ -60,7 +60,7 @@ object SetFunctions extends RegistryFunctions {
           ApplyUnaryPrimOp(Bang(), SetContains(s2, Ref(x, t)))))
     }
 
-    registerIR("isSubset", TSet(tv("T")), TSet(tv("T"))) { case (s, w) =>
+    registerIR("isSubset", TSet(tv("T")), TSet(tv("T"))) { (s, w) =>
       val t = -s.typ.asInstanceOf[TSet].elementType
       val a = genUID()
       val x = genUID()
@@ -68,6 +68,67 @@ object SetFunctions extends RegistryFunctions {
         // FIXME short circuit
         ApplySpecial("&&",
           FastSeq(Ref(a, TBoolean()), SetContains(w, Ref(x, t)))))
+    }
+
+    registerIR("min", TSet(tnum("T"))) { s =>
+      val t = s.typ.asInstanceOf[TSet].elementType
+      val a = genUID()
+      Let(a, ToArray(s), If(
+        ApplyComparisonOp(GT(TInt32()), ArrayLen(Ref(a, TArray(t))), I32(0)),
+        ArrayRef(Ref(a, TArray(t)), I32(0)),
+        NA(t)))
+    }
+
+    registerIR("max", TSet(tnum("T"))) { s =>
+      val t = s.typ.asInstanceOf[TSet].elementType
+      val a = genUID()
+      val size = genUID()
+      val last = genUID()
+
+      Let(a, ToArray(s),
+        Let(size, ArrayLen(Ref(a, TArray(t))),
+          If(ApplyComparisonOp(EQ(TInt32()), Ref(size, TInt32()), I32(0)),
+            NA(t),
+            Let(last, ArrayRef(Ref(a, TArray(t)), ApplyBinaryPrimOp(Subtract(), Ref(size, TInt32()), I32(1))),
+              If(IsNA(Ref(last, t)),
+                If(ApplyComparisonOp(EQ(TInt32()), Ref(size, TInt32()), I32(1)),
+                  NA(t),
+                  ArrayRef(Ref(a, TArray(t)), ApplyBinaryPrimOp(Subtract(), Ref(size, TInt32()), I32(2)))),
+                Ref(last, t))))))
+    }
+
+    registerIR("mean", TSet(tnum("T"))) { s => ArrayFunctions.mean(ToArray(s)) }
+
+    registerIR("median", TSet(tnum("T"))) { s =>
+      val t = s.typ.asInstanceOf[TSet].elementType
+      val a = genUID()
+      val size = genUID()
+      val lastIdx = genUID()
+      val midIdx = genUID()
+      val midIdx2 = genUID()
+
+      Let(a, ToArray(s),
+        Let(size, ArrayLen(Ref(a, TArray(t))),
+          If(ApplyComparisonOp(EQ(TInt32()), Ref(size, TInt32()), I32(0)),
+            NA(t),
+            If(ApplyComparisonOp(EQ(TInt32()), Ref(size, TInt32()), I32(1)),
+              ArrayRef(Ref(a, TArray(t)), I32(0)),
+              Let(lastIdx, ApplyBinaryPrimOp(Subtract(), Ref(size, TInt32()), I32(1)),
+                Let(lastIdx, If(
+                  IsNA(ArrayRef(Ref(a, TArray(t)), Ref(lastIdx, TInt32()))),
+                  ApplyBinaryPrimOp(Subtract(), Ref(lastIdx, TInt32()), I32(1)),
+                  Ref(lastIdx, TInt32())),
+                  Let(midIdx, ApplyBinaryPrimOp(RoundToNegInfDivide(), Ref(lastIdx, TInt32()), I32(2)),
+                    If(ApplyComparisonOp(EQ(TInt32()), Apply("%", FastSeq(Ref(lastIdx, TInt32()), I32(2))), I32(0)),
+                      ArrayRef(Ref(a, TArray(t)), Ref(midIdx, TInt32())), // odd number of non-missing elements
+                      Let(midIdx2, ApplyBinaryPrimOp(Add(), Ref(midIdx, TInt32()), I32(1)), // even number of non-missing elements
+                        ApplyBinaryPrimOp(
+                          RoundToNegInfDivide(),
+                          ApplyBinaryPrimOp(
+                            Add(),
+                            ArrayRef(Ref(a, TArray(t)), Ref(midIdx, TInt32())),
+                            ArrayRef(Ref(a, TArray(t)), Ref(midIdx2, TInt32()))),
+                          Cast(I32(2), t)))))))))))
     }
   }
 }
