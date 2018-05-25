17c1c73d6eb868342dd2d199b9d1190732311440
Jackie Goldstein <jigold@broadinstitute.org>
2018-05-25 00:20:56 -0400

    fix directory cp

--- python/hail/docs/makeDocs.sh
+++ python/hail/docs/makeDocs.sh
@@ -20,7 +20,7 @@ xsltproc --html -o build/www/jobs.html www/jobs.xslt build/tmp/jobs.html
 pandoc -s www/about.md -f markdown -t html --mathjax --highlight-style=pygments --columns 10000 -o build/tmp/about.html
 xsltproc --html -o build/www/about.html www/about.xslt build/tmp/about.html
 
-cp -r python/ build/tmp/
+cp -R python build/tmp
 
 (cd build/tmp/python/hail/docs && make clean html)
 
