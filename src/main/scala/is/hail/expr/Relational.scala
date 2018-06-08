1caaf64a27f2e774f09019ca19a7d427ea352ce4
Daniel King <dking@broadinstitute.org>
2018-06-08 13:13:34 -0400

    remove stray println

--- src/main/scala/is/hail/expr/Relational.scala
+++ src/main/scala/is/hail/expr/Relational.scala
@@ -2065,7 +2065,6 @@ case class TableJoin(left: TableIR, right: TableIR, joinType: String) extends Ta
         rv
       }
     }
-    println(s"TableJoin: ${leftTV.rvd} ${rightTV.rvd} ${leftTV.rvd.crdd.rdd.toDebugString} ${rightTV.rvd.crdd.rdd.toDebugString}")
     val leftORVD = leftTV.rvd match {
       case ordered: OrderedRVD => ordered
       case unordered =>
