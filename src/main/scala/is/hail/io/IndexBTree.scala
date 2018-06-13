f3f19e17638d17b620012eb1280235fdec5a3417
Daniel King <daniel.zidan.king@gmail.com>
2018-06-13 05:53:41 -0400

    Fix index btree bug when # elements == branchingFactor (#3750)
    * move functions from object to class
    
    * abstract over branchingFactor
    
    * rework, parameterize, tests pass
    
    * fix bug in conversion
    
    * introduce failing test case
    
    * fix test: array equality is wrong
    
    * fix padding elements
    
    * also test read and write
    
    * reformat file

--- src/main/scala/is/hail/io/IndexBTree.scala
+++ src/main/scala/is/hail/io/IndexBTree.scala
@@ -7,50 +7,64 @@ import org.apache.hadoop.fs._
 import scala.collection.mutable
 
 object IndexBTree {
+  private[io] def calcDepth(arr: Array[Long], branchingFactor: Int) =
+    //max necessary for array of length 1 becomes depth=0
+    math.max(1, (math.log10(arr.length) / math.log10(branchingFactor)).ceil.toInt)
 
-  def calcDepth(arr: Array[Long]) = math.max(1,(math.log10(arr.length) / math.log10(1024)).ceil.toInt) //max necessary for array of length 1 becomes depth=0
-
-  def write(arr: Array[Long], fileName: String, hConf: Configuration) {
+  private[io] def btreeBytes(
+    arr: Array[Long],
+    branchingFactor: Int = 1024
+  ): Array[Byte] = {
     require(arr.length > 0)
 
-    hConf.writeDataFile(fileName) { w =>
-      val depth = calcDepth(arr)
+    val depth = calcDepth(arr, branchingFactor)
 
-      // Write layers above last layer if needed -- padding of -1 included
-      val layers = mutable.ArrayBuffer[IndexedSeq[Long]]()
-      for (i <- 0 until depth - 1) {
-        val multiplier = math.pow(1024, depth - 1 - i).toInt
-        layers.append((0 until math.pow(1024, i + 1).toInt).map { j =>
-          if (j * multiplier < arr.length)
-            arr(j * multiplier)
-          else
-            -1L
-        })
-      }
-
-      // Write last layer
-      layers.append(arr)
-
-      // Pad last layer so last block is 1024 elements (1024*8 bytes)
-      val paddingRequired = 1024 - (arr.length % 1024)
-      layers.append((0 until paddingRequired).map{_ => -1L})
-
-      val bytes = layers.flatten.flatMap(l => Array[Byte](
-        (l >>> 56).toByte,
-        (l >>> 48).toByte,
-        (l >>> 40).toByte,
-        (l >>> 32).toByte,
-        (l >>> 24).toByte,
-        (l >>> 16).toByte,
-        (l >>> 8).toByte,
-        (l >>> 0).toByte)).toArray
-
-      w.write(bytes)
+    // Write layers above last layer if needed -- padding of -1 included
+    val layers = mutable.ArrayBuffer[IndexedSeq[Long]]()
+    for (i <- 0 until depth - 1) {
+      val multiplier = math.pow(branchingFactor, depth - 1 - i).toInt
+      layers.append((0 until math.pow(branchingFactor, i + 1).toInt).map { j =>
+        if (j * multiplier < arr.length)
+          arr(j * multiplier)
+        else
+          -1L
+      })
     }
+
+    // Write last layer
+    layers.append(arr)
+
+    // Pad last layer so last block is branchingFactor elements (branchingFactor*8 bytes)
+    val danglingElements = (arr.length % branchingFactor)
+    val paddingRequired =
+      if (danglingElements == 0) 0
+      else branchingFactor - danglingElements
+    layers.append((0 until paddingRequired).map { _ => -1L })
+
+    val bytes = layers.flatten.flatMap(l => Array[Byte](
+      (l >>> 56).toByte,
+      (l >>> 48).toByte,
+      (l >>> 40).toByte,
+      (l >>> 32).toByte,
+      (l >>> 24).toByte,
+      (l >>> 16).toByte,
+      (l >>> 8).toByte,
+      (l >>> 0).toByte)).toArray
+
+    bytes
+  }
+
+  def write(
+    arr: Array[Long],
+    fileName: String,
+    hConf: Configuration,
+    branchingFactor: Int = 1024
+  ): Unit = hConf.writeDataFile(fileName) { w =>
+    w.write(btreeBytes(arr, branchingFactor))
   }
 }
 
-class IndexBTree(indexFileName: String, hConf: Configuration) {
+class IndexBTree(indexFileName: String, hConf: Configuration, branchingFactor: Int = 1024) {
   val maxDepth = calcDepth()
   private val fs = try {
     hConf.fileSystem(indexFileName).open(new Path(indexFileName))
@@ -63,18 +77,18 @@ class IndexBTree(indexFileName: String, hConf: Configuration) {
   def calcDepth(): Int = {
     val numBtreeElements = hConf.getFileSize(indexFileName) / 8
     var depth = 1
-    while (numBtreeElements > math.pow(1024,depth).toInt) {
+    while (numBtreeElements > math.pow(branchingFactor, depth).toInt) {
       depth += 1
     }
     depth
   }
 
   private def getOffset(depth: Int): Long = {
-    (1 until depth).map(math.pow(1024,_).toLong * 8).sum
+    (1 until depth).map(math.pow(branchingFactor, _).toLong * 8).sum
   }
 
   private def getOffset(depth: Int, blockIndex: Long): Long = {
-    getOffset(depth) + blockIndex * 8 * 1024
+    getOffset(depth) + blockIndex * 8 * branchingFactor
   }
 
   private def traverseTree(query: Long, startIndex: Long, currentDepth: Int): Long = {
@@ -87,7 +101,7 @@ class IndexBTree(indexFileName: String, hConf: Configuration) {
           prevPos
         else if (currentDepth == maxDepth && query <= currValue || currValue == -1L)
           currValue
-        else if (prevPos >= (startIndex + 1024 * 8))
+        else if (prevPos >= (startIndex + branchingFactor * 8))
           fatal("did not find query in block")
         else
           read(currValue, prevPos + 8)
@@ -114,7 +128,7 @@ class IndexBTree(indexFileName: String, hConf: Configuration) {
   }
 
   def queryIndex(query: Long): Option[Long] = {
-    require( query >= 0 )
+    require(query >= 0)
 
     val result = traverseTree(query, 0L, 1)
 
