871da5172ff49823cf8e7d22686324b984bb8e8c
jbloom22 <jbloom@broadinstitute.org>
2018-06-13 21:16:59 -0400

    test_aggregate_by_with_joins failing

--- python/hail/tests/test_api.py
+++ python/hail/tests/test_api.py
@@ -636,10 +636,30 @@ class TableTests(unittest.TestCase):
 
 class GroupedTableTests(unittest.TestCase):
     def test_aggregate_by(self):
-        mt = hl.utils.range_table(4)
-        mt = mt.annotate(foo=0, group=mt.idx < 2, bar='hello').annotate_globals(glob=5)
-        grouped = mt.group_by(mt.group)
-        result = grouped.aggregate(sum=hl.agg.sum(mt.idx + mt.glob) + mt.glob - 15, max=hl.agg.max(mt.idx))
+        ht = hl.utils.range_table(4)
+        ht = ht.annotate(foo=0, group=ht.idx < 2, bar='hello').annotate_globals(glob=5)
+        grouped = ht.group_by(ht.group)
+        result = grouped.aggregate(sum=hl.agg.sum(ht.idx + ht.glob) + ht.glob - 15, max=hl.agg.max(ht.idx))
+
+        expected = (
+            hl.Table.parallelize(
+                [{'group': True, 'sum': 1, 'max': 1},
+                 {'group': False, 'sum': 5, 'max': 3}],
+                hl.tstruct(group=hl.tbool, sum=hl.tint64, max=hl.tint32)
+            ).annotate_globals(glob=5).key_by('group')
+        )
+
+        self.assertTrue(result._same(expected))
+    
+    def test_aggregate_by_with_joins(self):
+        ht = hl.utils.range_table(4)
+        ht2 = hl.utils.range_table(4)
+        ht2 = ht2.annotate(idx2=ht2.idx)
+
+        ht = ht.annotate(group=ht2[ht.idx].idx2).annotate_globals(glob=5)
+        grouped = ht.group_by(k = ht.group < 2)
+        result = grouped.aggregate(sum=hl.agg.sum(ht2[ht.idx].idx2 + ht.glob) + ht.glob - 15,
+                                   max=hl.agg.max(ht2[ht.idx].idx2))
 
         expected = (
             hl.Table.parallelize(
