897938986fe57b42a1a04b70c9d319aa60013585
Amanda Wang <amwang@alum.mit.edu>
2018-06-15 14:40:46 -0400

    add dosage, plDosage to IR Functions (#3759)

--- src/main/scala/is/hail/expr/ir/functions/GenotypeFunctions.scala
+++ src/main/scala/is/hail/expr/ir/functions/GenotypeFunctions.scala
@@ -5,6 +5,7 @@ import is.hail.asm4s._
 import is.hail.expr.ir.EmitMethodBuilder
 import is.hail.expr.types._
 import is.hail.utils._
+import is.hail.variant.Genotype
 
 object GenotypeFunctions extends RegistryFunctions {
 
@@ -36,6 +37,39 @@ object GenotypeFunctions extends RegistryFunctions {
         ),
         m2 - m
       )
+    }
+
+    registerCode("dosage", TArray(tv("N", _ isOfType TFloat64())), TFloat64()) { (mb, gpOff: Code[Long]) =>
+      def getRegion(mb: EmitMethodBuilder): Code[Region] = mb.getArg[Region](1)
+      val tarray = TArray(tv("N").t)
+      val gp = mb.newLocal[Long]
+      val region = getRegion(mb)
+      val len = tarray.loadLength(region, gp)
+
+      Code(
+        gp := gpOff,
+        len.cne(3).mux(
+          Code._fatal(const("length of gp array must be 3, got ").concat(len.toS)),
+          region.loadDouble(tarray.elementOffset(gp, 3, 1)) +
+            region.loadDouble(tarray.elementOffset(gp, 3, 2)) * 2.0))
+    }
+
+
+    registerCode("plDosage", TArray(tv("N", _ isOfType TInt32())), TFloat64()) { (mb, plOff: Code[Long]) =>
+      def getRegion(mb: EmitMethodBuilder): Code[Region] = mb.getArg[Region](1)
+      val tarray = TArray(tv("N").t)
+      val pl = mb.newLocal[Long]
+      val region = getRegion(mb)
+      val len = tarray.loadLength(region, pl)
+
+      Code(
+        pl := plOff,
+        len.cne(3).mux(
+          Code._fatal(const("length of pl array must be 3, got ").concat(len.toS)),
+          Code.invokeScalaObject[Int, Int, Int, Double](Genotype.getClass, "plToDosage",
+            region.loadInt(tarray.elementOffset(pl, 3, 0)),
+            region.loadInt(tarray.elementOffset(pl, 3, 1)),
+            region.loadInt(tarray.elementOffset(pl, 3, 2)))))
     }
   }
 }
