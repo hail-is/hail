0e460ddade364047d0b6a146dc03f2d11d416091
Jack Goldsmith <jackgoldsmith4@gmail.com>
2018-07-03 09:51:05 -0400

    Hail fields as arguments in plot functions (#3863)
    * initial v2
    
    * add some scatter changes and qq v2
    
    * scatter fixes
    
    * update tutorial-01
    
    * histogram v2 work in progress
    
    * update histogram()
    
    * fix
    
    * work in progress
    
    * hist progress
    
    * update tutorial histograms
    
    * update docs
    
    * address changes
    
    * address changes
    
    * fix docstring
    
    * address changes

--- python/hail/plot/plots.py
+++ python/hail/plot/plots.py
@@ -1,18 +1,27 @@
 import numpy as np
 from math import log, isnan
-from hail.typecheck import *
+from hail.expr.expressions import *
+from hail.expr.expr_ast import *
+from hail.expr import aggregators
+from hail.expr.expressions import Expression
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource, CategoricalColorMapper
 from bokeh.palettes import Category10
 
 
-def histogram(data, legend=None, title=None):
+@typecheck(data=oneof(hail.utils.struct.Struct, expr_float64), range=nullable(sized_tupleof(numeric, numeric)),
+           bins=int, legend=nullable(str), title=nullable(str))
+def histogram(data, range=None, bins=50, legend=None, title=None):
     """Create a histogram.
 
     Parameters
     ----------
-    data : :class:`.Struct`
+    data : :class:`.Struct` or :class:`.Float64Expression`
         Sequence of data to plot.
+    range : Tuple[float]
+        Range of x values in the histogram.
+    bins : int
+        Number of bins in the histogram.
     legend : str
         Label of data on the x-axis.
     title : str
@@ -22,6 +31,18 @@ def histogram(data, legend=None, title=None):
     -------
     :class:`bokeh.plotting.figure.Figure`
     """
+    if isinstance(data, Expression):
+        if data._indices.source is not None:
+            agg_f = data._aggregation_method()
+            if range is not None:
+                start = range[0]
+                end = range[1]
+            else:
+                start, end = agg_f((aggregators.min(data), aggregators.max(data)))
+            data = agg_f(aggregators.hist(data, start, end, bins))
+        else:
+            return ValueError('Invalid input')
+
     p = figure(title=title, x_axis_label=legend, y_axis_label='Frequency', background_fill_color='#EEEEEE')
     p.quad(
         bottom=0, top=data.bin_freq,
@@ -30,16 +51,19 @@ def histogram(data, legend=None, title=None):
     return p
 
 
+@typecheck(x=oneof(sequenceof(numeric), expr_float64), y=oneof(sequenceof(numeric), expr_float64),
+           label=oneof(nullable(str), expr_str), title=nullable(str),
+           xlabel=nullable(str), ylabel=nullable(str), size=int)
 def scatter(x, y, label=None, title=None, xlabel=None, ylabel=None, size=4):
     """Create a scatterplot.
 
     Parameters
     ----------
-    x : List[float]
+    x : List[float] or :class:`.Float64Expression`
         List of x-values to be plotted.
-    y : List[float]
+    y : List[float] or :class:`.Float64Expression`
         List of y-values to be plotted.
-    label : List[str]
+    label : List[str] or :class:`.StringExpression`
         List of labels for x and y values, used to assign each point a label (e.g. population)
     title : str
         Title of the scatterplot.
@@ -54,6 +78,19 @@ def scatter(x, y, label=None, title=None, xlabel=None, ylabel=None, size=4):
     -------
     :class:`bokeh.plotting.figure.Figure`
     """
+    if isinstance(x, Expression) and isinstance(y, Expression):
+        if isinstance(label, Expression):
+            res = hail.tuple([x, y, label]).collect()
+            x = [point[0] for point in res]
+            y = [point[1] for point in res]
+            label = [point[2] for point in res]
+        else:
+            res = hail.tuple([x, y]).collect()
+            x = [point[0] for point in res]
+            y = [point[1] for point in res]
+    elif isinstance(x, Expression) or isinstance(y, Expression):
+        raise TypeError('Invalid input: x and y must both be either Expressions or Python Lists.')
+
     p = figure(title=title, x_axis_label=xlabel, y_axis_label=ylabel, background_fill_color='#EEEEEE')
     if label is not None:
         source = ColumnDataSource(dict(x=x, y=y, label=label))
@@ -66,18 +103,25 @@ def scatter(x, y, label=None, title=None, xlabel=None, ylabel=None, size=4):
     return p
 
 
+@typecheck(pvals=oneof(sequenceof(numeric), expr_float64))
 def qq(pvals):
     """Create a Quantile-Quantile plot. (https://en.wikipedia.org/wiki/Q-Q_plot)
 
     Parameters
     ----------
-    pvals : List[float]
+    pvals : List[float] or :class:`.Float64Expression`
         P-values to be plotted.
 
     Returns
     -------
     :class:`bokeh.plotting.figure.Figure`
     """
+    if isinstance(pvals, Expression):
+        if pvals._indices.source is not None:
+            pvals = pvals.collect()
+        else:
+            return ValueError('Invalid input')
+
     spvals = sorted(filter(lambda x: x and not(isnan(x)), pvals))
     exp = [-log(float(i) / len(spvals), 10) for i in np.arange(1, len(spvals) + 1, 1)]
     obs = [-log(p, 10) for p in spvals]
