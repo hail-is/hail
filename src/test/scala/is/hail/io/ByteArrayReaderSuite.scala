dc08279c387823bdbd916ab3fdb66ea3a917e7e5
Daniel King <dking@broadinstitute.org>
2018-07-10 17:33:52 -0400

    more tests

--- src/test/scala/is/hail/io/ByteArrayReaderSuite.scala
+++ src/test/scala/is/hail/io/ByteArrayReaderSuite.scala
@@ -15,6 +15,13 @@ class ByteArrayReaderSuite extends TestNGSuite {
   def readLongReadsALong2() {
     val a = Array(0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8)
       .map(_.toByte)
+    assert(new ByteArrayReader(a).readLong() == 0xf8f7f6f5f4f3f2f1L)
+  }
+
+  @Test
+  def readLongReadsALong3() {
+    val a = Array(0xf8, 0xf7, 0xf6, 0xf5, 0xf4, 0xf3, 0xf2, 0xf1)
+      .map(_.toByte)
     assert(new ByteArrayReader(a).readLong() == 0xf1f2f3f4f5f6f7f8L)
   }
 }
