efc913929519086cbe6f08f9aa4c573afda7cabe
wang <wang@broadinstitute.org>
2018-07-18 17:12:27 -0400

    fix import in plot module

--- python/hail/plot/plots.py
+++ python/hail/plot/plots.py
@@ -8,6 +8,8 @@ from itertools import cycle
 from hail.expr import aggregators
 from hail.expr.expressions import *
 from hail.expr.expressions import Expression
+from hail.typecheck import *
+import hail
 
 palette = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
