63d253f4b53a5d21b4d433b320659067a3a66c91
Daniel King <dking@broadinstitute.org>
2018-07-18 15:50:29 -0400

    approved check seems to work

--- ci.py
+++ ci.py
@@ -1,5 +1,11 @@
 from flask import Flask, request, jsonify
+import batch
+import requests
+
 app = Flask(__name__)
+
+with open('oauth-token', 'r') as f:
+  oauth_token = f.read()
 
 prs = {}
 
@@ -16,10 +22,49 @@ def github():
 def status():
     return jsonify(prs), 200
 
-@app.route('/<pr_number>/retest')
-def status(pr_number):
-    if prs[pr_number]
-    return '', 200
+@app.route('/pr/<pr_number>/retest')
+def retest(pr_number):
+    return pr_number, 200
+
+@app.route('/pr/<pr_number>/review_status')
+def review_status(pr_number):
+    r = requests.get(
+        'https://api.github.com/repos/hail-is/hail/pulls/'+pr_number+'/reviews',
+        headers={ 'access_token' : oauth_token }
+    )
+    if r.status_code != 200:
+        return jsonify({
+            'message': 'github error',
+            'github_json': r.json()
+        }), r.status_code
+
+    reviews = r.json()
+    latest_state_by_login = {}
+    for review in reviews:
+        login = review['user']['login']
+        state = review['state']
+        # reviews is chronological, so later ones are newer statuses
+        latest_state_by_login[login] = state
+    at_least_one_approved = False
+    for login, state in latest_state_by_login.items():
+        if (state == 'CHANGES_REQUESTED'):
+            return jsonify({
+                'state': 'CHANGES_REQUESTED',
+                'reviews': latest_state_by_login
+            }), 200
+        elif (state == 'APPROVED'):
+            at_least_one_approved = True
+
+    if at_least_one_approved:
+        return jsonify({
+            'state': 'APPROVED',
+            'reviews': latest_state_by_login
+        }), 200
+    else:
+        return jsonify({
+            'state': 'PENDING',
+            'reviews': latest_state_by_login
+        }), 200
 
 if __name__ == '__main__':
     app.run()
