f16101b75047893d7e4d01a1bcbc9fabe1756b3f
Daniel King <dking@broadinstitute.org>
2018-07-18 13:50:45 -0400

    Create setup.py

---
+++ batch/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'batch',
+    version = '0.0.1',
+    url = 'https://github.com/hail-is/batch.git',
+    author = 'Hail Team',
+    author_email = 'hail@broadinstitute.org',
+    description = 'Job manager for k8s',
+    packages = find_packages()
+)
