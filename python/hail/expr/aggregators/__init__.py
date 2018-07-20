234d58f02414c12ed974d44e4a664cd56da3e5d4
wang <wang@broadinstitute.org>
2018-07-20 13:16:15 -0400

    clean up aggregator namespace

---
+++ python/hail/expr/aggregators/__init__.py
@@ -0,0 +1,35 @@
+from .aggregators import collect, collect_as_set, count, count_where, counter, \
+    any, all, take, min, max, sum, array_sum, mean, stats, product, fraction, \
+    hardy_weinberg, explode, filter, inbreeding, call_stats, info_score, hist, \
+    linreg
+
+# because `aggregators` is designed to be imported as `agg` instead of with the
+# `from module import *` notation, the presence of `__all__` doesn't hide the
+# other functions imported into its namespace.
+
+__all__ = [
+    'collect',
+    'collect_as_set',
+    'count',
+    'count_where',
+    'counter',
+    'any',
+    'all',
+    'take',
+    'min',
+    'max',
+    'sum',
+    'array_sum',
+    'mean',
+    'stats',
+    'product',
+    'fraction',
+    'hardy_weinberg',
+    'explode',
+    'filter',
+    'inbreeding',
+    'call_stats',
+    'info_score',
+    'hist',
+    'linreg'
+]
