c0167fe52c758c35a481df695caccbc9fbe6f4bb
Richard Cownie <rcownie@broadinstitute.org>
2018-07-23 13:58:10 -0400

    small changes ported from nregion3

--- src/main/scala/is/hail/nativecode/NativeCode.java
+++ src/main/scala/is/hail/nativecode/NativeCode.java
@@ -4,32 +4,71 @@ import java.io.*;
 import java.util.*;
 import java.net.URL;
 import com.sun.jna.*;
+import org.apache.log4j.*;
 
-class NativeCode {
+public class NativeCode {
   static {
     try {
-      File file = File.createTempFile("libhail", ".lib");
+      String libHail = libToLocalFile("libhail");
+      if (isLinux()) {
+        // libboot.so has native methods to call dlopen/dlclose
+        String libBoot = libToLocalFile("libboot");
+        System.load(libBoot);
+        // We need libhail.so to be loaded with RTLD_GLOBAL so that its symbols
+        // are visible to dynamic-generated code in other DLLs ...
+        long handle = dlopenGlobal(libHail);
+        // ... but we also need System.load to let the JVM see it
+        System.load(libHail);
+      } else {
+        // MacOS System.load uses RTLD_GLOBAL, so it just works
+        System.load(libHail);
+      }
+    } catch (Exception e) {
+      handleException(e);
+    }
+  }
+  
+  private static void handleException(Exception e) {
+    String msg = "NativeCode.init caught exception: " + e.toString();
+    LogManager.getLogger("Hail").error(msg);
+    LogManager.getRootLogger().error(msg);
+  }
+  
+  private static Boolean isLinux() {
+    String osName = System.getProperty("os.name").toLowerCase();
+    if ((osName.length() >= 3) && osName.substring(0, 3).equals("mac")) {
+      return false;
+    }
+    return true;
+  }
+  
+  private static String libToLocalFile(String libName) {
+    String path = "";
+    try {
+      File file = File.createTempFile(libName, ".so");
       ClassLoader loader = NativeCode.class.getClassLoader();
       InputStream s = null;
-      String osName = System.getProperty("os.name");
-      if (osName.equals("Linux") || osName.equals("linux")) {
-        s = loader.getResourceAsStream("linux-x86-64/libhail.so");
+      if (isLinux()) {
+        s = loader.getResourceAsStream("linux-x86-64/" + libName + ".so");
       } else {
-        s = loader.getResourceAsStream("darwin/libhail.dylib");
+        s = loader.getResourceAsStream("darwin/" + libName + ".dylib");
       }
       java.nio.file.Files.copy(s, file.getAbsoluteFile().toPath(),
         java.nio.file.StandardCopyOption.REPLACE_EXISTING
       );
-      String path = file.getAbsoluteFile().toPath().toString();
-      // Unlike JNA, this loads it with RTLD_GLOBAL to make all symbols
-      // visible to DLL's loaded in future
-      System.load(path);
+      path = file.getAbsoluteFile().toPath().toString();
     } catch (Exception e) {
-      System.err.println("ERROR: NativeCode.init caught exception");
+      handleException(e);
+      path = libName + "_resource_not_found";
     }
+    return path;
   }
+  
+  private native static long dlopenGlobal(String path);
+  
+  private native static long dlclose(long handle);
 
-  final static String getIncludeDir() {
+  public final static String getIncludeDir() {
     String name = ClassLoader.getSystemResource("include/hail/hail.h").toString();
     int len = name.length();
     if (len >= 12) {
@@ -38,8 +77,8 @@ class NativeCode {
     if (name.substring(0, 5).equals("file:")) {
       name = name.substring(5, name.length());
     }
-    return(name);
+    return name;
   }
 
-  final static void forceLoad() { }
+  public final static void forceLoad() { }
 }
