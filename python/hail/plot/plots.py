f44bde18e3108a76cee35abac1f675e2ef2939e9
jackgoldsmith4 <jackgoldsmith4@gmail.com>
2018-07-23 09:27:41 -0400

    palette to Category10 colors

--- python/hail/plot/plots.py
+++ python/hail/plot/plots.py
@@ -10,7 +10,7 @@ from hail.expr.expr_ast import *
 from hail.expr.expressions import *
 from hail.expr.expressions import Expression
 
-palette = ['#d62728', '#1f77b4', '#ff7f0e', '#2ca02c', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+palette = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
 
 @typecheck(data=oneof(hail.utils.struct.Struct, expr_float64), range=nullable(sized_tupleof(numeric, numeric)),
