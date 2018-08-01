510589e0ad4d9daed8458fd3ecfc871060c2877a
laurent <lfran@broadinstitute.org>
2018-08-01 16:06:27 -0400

    Added phase_by_transmission import to experimental.__init__.py

--- python/hail/experimental/__init__.py
+++ python/hail/experimental/__init__.py
@@ -1,5 +1,9 @@
 from .ldscore import ld_score
 from .plots import hail_metadata
+from .phase_by_transmission import *
 
 __all__ = ['ld_score',
-           'hail_metadata']
+           'hail_metadata',
+           'phase_trio_matrix_by_transmission',
+           'phase_by_transmission',
+           'explode_trio_matrix']
