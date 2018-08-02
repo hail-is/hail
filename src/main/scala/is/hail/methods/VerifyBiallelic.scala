2c76ee7067c85428d5beaef0e6651f96209b6af8
tpoterba <tpoterba@broadinstitute.org>
2018-08-02 17:27:58 -0400

    IR-ify VerifyBiallelic

--- src/main/scala/is/hail/methods/VerifyBiallelic.scala
+++ src/main/scala/is/hail/methods/VerifyBiallelic.scala
@@ -1,31 +1,15 @@
 package is.hail.methods
 
-import is.hail.annotations.UnsafeRow
-import is.hail.expr.types.TArray
-import is.hail.variant.{Locus, MatrixTable}
-import is.hail.utils._
+import is.hail.expr.ir._
+import is.hail.expr.types.TInt32
+import is.hail.variant.MatrixTable
 
 object VerifyBiallelic {
   def apply(vsm: MatrixTable, method: String): MatrixTable = {
-    val fullRowType = vsm.rvRowType
-    val locusField = vsm.rvRowType.fieldByName("locus")
-    val allelesField = vsm.rvRowType.fieldByName("alleles")
-    val allelesType = allelesField.typ.asInstanceOf[TArray]
-    vsm.copy2(
-      rvd = vsm.rvd.mapPreservesPartitioning(vsm.rvd.typ) { rv =>
-        if (!fullRowType.isFieldDefined(rv, allelesField.index))
-          fatal(s"in $method: found missing locus")
-        if (!fullRowType.isFieldDefined(rv, locusField.index))
-          fatal(s"in $method: found missing alleles")
-        val allelesOffset = fullRowType.loadField(rv, allelesField.index)
-        val allelesLength = allelesType.loadLength(rv.region, allelesOffset)
-        if (allelesLength != 2) {
-          val ur = new UnsafeRow(fullRowType, rv)
-          val locus = ur.getAs[Locus](locusField.index)
-          val alleles = ur.getAs[IndexedSeq[String]](allelesField.index)
-          fatal(s"in $method: found non-biallelic variant: locus $locus, alleles ${ alleles.mkString(", ") }")
-        }
-        rv
-      })
+    new MatrixTable(vsm.hc, MatrixMapRows(vsm.ast,
+      If(ApplyComparisonOp(NEQ(TInt32()), ArrayLen(GetField(Ref("va", vsm.rvRowType), "alleles")), I32(2)),
+        // TODO: message should include key when we can put IR in Die
+        Die(s"'$method' expects biallelic variants ('alleles' field has length 2)", vsm.rvRowType),
+        Ref("va", vsm.rvRowType)), None))
   }
 }
