8cdf75c0c5f8249c27c83c92db19ab53166347a6
konradjk <konradjkarczewski@gmail.com>
2018-08-02 12:01:35 -0400

    typo

--- src/main/scala/is/hail/experimental/ExperimentalFunctions.scala
+++ src/main/scala/is/hail/experimental/ExperimentalFunctions.scala
@@ -8,7 +8,7 @@ object ExperimentalFunctions extends RegistryFunctions {
   def registerAll() {
     val experimentalPackageClass = Class.forName("is.hail.experimental.package$")
 
-    registerScalaFunction("filtering_allele_frequency", TInt32(), TInt32(), TFloat64(), TFloat64())(experimentalClass, "calcFilterAlleleFreq")
+    registerScalaFunction("filtering_allele_frequency", TInt32(), TInt32(), TFloat64(), TFloat64())(experimentalPackageClass, "calcFilterAlleleFreq")
 
   }
 
