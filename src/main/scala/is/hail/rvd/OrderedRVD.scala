993ca1ebf41404cbfc921c85af6ccabbe5aeb392
wang <wang@broadinstitute.org>
2018-08-02 18:08:58 -0400

    fix rebase

--- src/main/scala/is/hail/rvd/OrderedRVD.scala
+++ src/main/scala/is/hail/rvd/OrderedRVD.scala
@@ -1048,11 +1048,6 @@ object OrderedRVD {
 
   def union(rvds: Seq[OrderedRVD]): OrderedRVD = {
     require(rvds.length > 1)
-    val first = rvds.head
-    OrderedRVD.coerce(
-      first.typ,
-      RVD.union(rvds),
-      None,
-      None)
+    rvds.reduce(_.orderedMerge(_))
   }
 }
