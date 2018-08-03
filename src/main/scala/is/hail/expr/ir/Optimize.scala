92cdac36af8de978e0bef85d00bc5d693ced6077
Tim Poterba <tpoterba@gmail.com>
2018-08-02 21:25:36 -0400

    Make Optimize less noisy when coming from Compile. (#3999)
    * Make Optimize less noisy when coming from Compile.
    
    This makes the logs MUCH more readable.
    
    * Fix

--- src/main/scala/is/hail/expr/ir/Optimize.scala
+++ src/main/scala/is/hail/expr/ir/Optimize.scala
@@ -3,24 +3,30 @@ package is.hail.expr.ir
 import is.hail.utils._
 
 object Optimize {
-  private def optimize(ir0: BaseIR): BaseIR = {
-    log.info("optimize: before:\n" + Pretty(ir0))
+  private def optimize(ir0: BaseIR, noisy: Boolean): BaseIR = {
+    if (noisy)
+      log.info("optimize: before:\n" + Pretty(ir0))
 
     var ir = ir0
     ir = FoldConstants(ir)
     ir = Simplify(ir)
     ir = PruneDeadFields(ir)
 
-    assert(ir.typ == ir0.typ, s"optimization changed type!\n  before: ${ir0.typ}\n  after:  ${ir.typ}")
+    if (ir.typ != ir0.typ)
+      fatal(s"optimization changed type!\n  before: ${ir0.typ}\n  after:  ${ir.typ}")
 
-    log.info("optimize: after:\n" + Pretty(ir))
+    if (noisy)
+      log.info("optimize: after:\n" + Pretty(ir))
 
     ir
   }
 
-  def apply(ir: TableIR): TableIR = optimize(ir).asInstanceOf[TableIR]
+  def apply(ir: TableIR, noisy: Boolean): TableIR = optimize(ir, noisy).asInstanceOf[TableIR]
+  def apply(ir: TableIR): TableIR = optimize(ir, noisy = true).asInstanceOf[TableIR]
 
-  def apply(ir: MatrixIR): MatrixIR = optimize(ir).asInstanceOf[MatrixIR]
+  def apply(ir: MatrixIR, noisy: Boolean): MatrixIR = optimize(ir, noisy).asInstanceOf[MatrixIR]
+  def apply(ir: MatrixIR): MatrixIR = optimize(ir, noisy = true).asInstanceOf[MatrixIR]
 
-  def apply(ir: IR): IR = optimize(ir).asInstanceOf[IR]
+  def apply(ir: IR, noisy: Boolean): IR = optimize(ir, noisy).asInstanceOf[IR]
+  def apply(ir: IR): IR = optimize(ir, noisy = true).asInstanceOf[IR]
 }
