7eac5d0a28b0e6d1d7ced0b786251a854a732d89
Tim Poterba <tpoterba@gmail.com>
2018-08-14 10:25:13 -0400

    Revert functools to decorator (#4139)
    * Revert functools to decorator
    
    Had to refactor some things to deal with subtle importing graphs.
    
    I also left the hl.scan stuff using functools, because I'm not sure that
    we can fix that as easily.
    
    fixes #4112
    
    * fix group_by docs
    
    * fix test
    
    * fix test

--- python/hail/matrixtable.py
+++ python/hail/matrixtable.py
@@ -301,8 +301,7 @@ class GroupedMatrixTable(ExprContainer):
         elif self._row_keys is not None:
             if self._partition_key is None:
                 self._partition_key = self._row_keys.keys()
-            keyed_mt = base._select_rows_processed(hl.struct(**group_exprs),
-                                                   pk_size=len(self._partition_key))
+            keyed_mt = base._select_rows_processed(hl.struct(**group_exprs))
             mt = MatrixTable(keyed_mt._jvds.aggregateRowsByKey(str(hl.struct(**named_exprs)._ir)))
         else:
             raise ValueError("GroupedMatrixTable cannot be aggregated if no groupings are specified.")
