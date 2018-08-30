3ae9a3a584644604acadf1d1d69c11dab536f854
Cotton Seed <cotton@alum.mit.edu>
2018-08-29 20:31:59 -0400

    added missing file

---
+++ python/hail/ir/renderer.py
@@ -0,0 +1,19 @@
+
+class Renderer(object):
+    def __init__(self, stop_at_jir):
+        self.stop_at_jir = stop_at_jir
+        self.count = 0
+        self.jirs = {}
+
+    def add_jir(self, jir):
+        jir_id = f'm{self.count}'
+        self.count = self.count + 1
+        self.jirs[jir_id] = jir
+        return jir_id
+
+    def __call__(self, x):
+        if self.stop_at_jir and hasattr(x, '_jir'):
+            jir_id = context.add_jir(self._jir)
+            return f'(JavaMatrix {jir_id})'
+        else:
+            return x.render(self)
