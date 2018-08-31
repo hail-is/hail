c331269c2ef78be3c15da81bed0de3fabb0d82a7
Tim Poterba <tpoterba@gmail.com>
2018-08-31 11:31:01 -0400

    update version to 1.14 (#75)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cloudtools',
-	  version='1.1.13',
+	  version='1.1.14',
 	  description='Collection of utilities for working on the Google Cloud Platform.',
 	  url='https://github.com/Nealelab/cloudtools',
 	  author='Liam Abbott',
