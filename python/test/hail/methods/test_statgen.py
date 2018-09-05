5dfbe2ec29f860247e33bfe0cf152a51dbe434f1
Daniel King <daniel.zidan.king@gmail.com>
2018-09-05 16:16:01 -0400

    no n_partitions in balding nichols model (#4256)
    * no n_partitions in balding nichols model
    
    * for jon
    
    * maybe fix tests

--- python/test/hail/methods/test_statgen.py
+++ python/test/hail/methods/test_statgen.py
@@ -988,11 +988,11 @@ class Tests(unittest.TestCase):
         self.assertEqual(ds.n_partitions(), 3)
 
         glob = ds.globals
-        self.assertEqual(glob.n_populations.value, 2)
-        self.assertEqual(glob.n_samples.value, 20)
-        self.assertEqual(glob.n_variants.value, 25)
-        self.assertEqual(glob.pop_dist.value, [1, 2])
-        self.assertEqual(glob.fst.value, [.02, .06])
+        self.assertEqual(glob.bn.n_populations.value, 2)
+        self.assertEqual(glob.bn.n_samples.value, 20)
+        self.assertEqual(glob.bn.n_variants.value, 25)
+        self.assertEqual(glob.bn.pop_dist.value, [1, 2])
+        self.assertEqual(glob.bn.fst.value, [.02, .06])
 
     def test_balding_nichols_model_same_results(self):
         hl.set_global_seed(1)
