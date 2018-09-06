f45c8e3f45132bb546cbedabb43c07492750d2cd
Daniel King <daniel.zidan.king@gmail.com>
2018-09-06 10:21:48 -0400

    maybe fix hail-ci-build.sh

--- hail-ci-build.sh
+++ hail-ci-build.sh
@@ -10,39 +10,16 @@ gcloud auth activate-service-account \
     hail-ci-0-1@broad-ctsa.iam.gserviceaccount.com \
     --key-file=/secrets/hail-ci-0-1.key
 
-shutdown_cluster() {
+on_exit() {
+    trap - INT TERM
     set +e
-    time cluster stop --async ${CLUSTER_NAME}
-    exit
-}
-trap shutdown_cluster INT TERM
-
-set +e
-
-GRADLE_OPTS=-Xmx2048m ./gradlew testAll makeDocs archiveZip --gradle-user-home /gradle-cache && \
-    time gsutil cp \
-         build/libs/hail-all-spark.jar \
-         gs://hail-ci-0-1/temp/$SOURCE_SHA/$TARGET_SHA/hail.jar && \
-    time gsutil cp \
-         build/distributions/hail-python.zip \
-         gs://hail-ci-0-1/temp/$SOURCE_SHA/$TARGET_SHA/hail.zip && \
-    time cluster start ${CLUSTER_NAME} \
-         --version devel \
-         --spark 2.2.0 \
-         --bucket=hail-ci-0-1-dataproc-staging-bucket \
-         --jar gs://hail-ci-0-1/temp/$SOURCE_SHA/$TARGET_SHA/hail.jar \
-         --zip gs://hail-ci-0-1/temp/$SOURCE_SHA/$TARGET_SHA/hail.zip && \
-    time cluster submit ${CLUSTER_NAME} \
-         cluster-sanity-check.py
-EXIT_CODE=$?
-shutdown_cluster
-rm -rf artifacts
-mkdir -p artifacts
-cp build/libs/hail-all-spark.jar artifacts/hail-all-spark.jar
-cp build/distributions/hail-python.zip artifacts/hail-python.zip
-cp -R build/www artifacts/www
-cp -R build/reports/tests artifacts/test-report
-cat <<EOF > artifacts/index.html
+    rm -rf artifacts
+    mkdir -p artifacts
+    cp build/libs/hail-all-spark.jar artifacts/hail-all-spark.jar
+    cp build/distributions/hail-python.zip artifacts/hail-python.zip
+    cp -R build/www artifacts/www
+    cp -R build/reports/tests artifacts/test-report
+    cat <<EOF > artifacts/index.html
 <html>
 <body>
 <h1>$(git rev-parse HEAD)</h1>
@@ -55,4 +32,31 @@ cat <<EOF > artifacts/index.html
 </body>
 </html>
 EOF
-exit $EXIT_CODE
+    time cluster stop --async ${CLUSTER_NAME}
+}
+trap on_exit EXIT
+
+# some non-bash shells (in particular: dash and sh) do not trigger EXIT if the
+# interpretation halts due to INT or TERM. Explicitly calling exit when INT or
+# TERM is received ensures the EXIT handler is called.
+trap "exit 42" INT TERM
+
+GRADLE_OPTS=-Xmx2048m ./gradlew testAll makeDocs archiveZip --gradle-user-home /gradle-cache
+
+time gsutil cp \
+     build/libs/hail-all-spark.jar \
+     gs://hail-ci-0-1/temp/$SOURCE_SHA/$TARGET_SHA/hail.jar
+
+time gsutil cp \
+     build/distributions/hail-python.zip \
+     gs://hail-ci-0-1/temp/$SOURCE_SHA/$TARGET_SHA/hail.zip
+
+time cluster start ${CLUSTER_NAME} \
+     --version devel \
+     --spark 2.2.0 \
+     --bucket=hail-ci-0-1-dataproc-staging-bucket \
+     --jar gs://hail-ci-0-1/temp/$SOURCE_SHA/$TARGET_SHA/hail.jar \
+     --zip gs://hail-ci-0-1/temp/$SOURCE_SHA/$TARGET_SHA/hail.zip
+
+time cluster submit ${CLUSTER_NAME} \
+     cluster-sanity-check.py
