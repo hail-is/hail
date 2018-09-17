6826a55af4c31fd60bd8e7c0c312c32c2e9c9a29
Daniel King <daniel.zidan.king@gmail.com>
2018-09-17 10:57:58 -0400

    Fix SHA check (#51)
    * Update hail-ci-deploy.sh
    
    * Update deployment.yaml.in
    
    * Update hail-ci-deploy.sh
    
    * Update hail-ci-deploy.sh

--- hail-ci-deploy.sh
+++ hail-ci-deploy.sh
@@ -10,7 +10,7 @@ gcloud -q auth configure-docker
 SHA=$(git rev-parse --short=12 HEAD)
 
 # get sha label of batch deployment
-DEPLOYED_SHA=$(kubectl get --selector=app=batch deployments -o jsonpath="{.items[*].metadata.labels['hail\.is/sha']}")
+DEPLOYED_SHA=$(kubectl get --selector=app=batch deployments -o "jsonpath={.items[*].metadata.labels.hail\.is/sha}")
 
 if [[ "$SHA" == "$DEPLOYED_SHA" ]]; then
     exit 0
