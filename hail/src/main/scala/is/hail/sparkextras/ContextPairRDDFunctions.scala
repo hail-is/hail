1ec2c209f22836dc72a5836a035e3bcd62d90207
patrick-schultz <pschultz@broadinstitute.org>
2018-09-18 14:18:49 -0400

    delete orphaned shuffle method

--- hail/src/main/scala/is/hail/sparkextras/ContextPairRDDFunctions.scala
+++ hail/src/main/scala/is/hail/sparkextras/ContextPairRDDFunctions.scala
@@ -8,14 +8,6 @@ import scala.reflect.ClassTag
 class ContextPairRDDFunctions[C <: AutoCloseable, K: ClassTag, V: ClassTag](
   crdd: ContextRDD[C, (K, V)]
 ) {
-  def shuffle(p: Partitioner, o: Ordering[K]): ContextRDD[C, (K, V)] =
-    // NB: the run marks the end of a context lifetime, the next context
-    // lifetime starts after the shuffle (and potentially on a different machine
-    // than the one that owned the previous lifetime)
-    ContextRDD.weaken(
-      new ShuffledRDD[K, V, V](crdd.run, p).setKeyOrdering(o),
-      crdd.mkc)
-
   // FIXME: this needs to take RDDs with values that are not in region-value-land.
   def partitionBy(p: Partitioner): ContextRDD[C, (K, V)] =
     if (crdd.partitioner.contains(p)) crdd
