78421187ca91d175b7209353eab0f8259cb850dd
Cotton Seed <cotton@alum.mit.edu>
2018-09-21 01:55:52 -0400

    fix cron k8s incompatibility

--- site.sh
+++ site.sh
@@ -3,6 +3,10 @@
 bash /poll-devel.sh
 bash /poll-0.1.sh
 
+# otherwise cron jobs won't run
+# see: https://stackoverflow.com/questions/34962020/cron-and-crontab-files-not-executed-in-docker
+touch /etc/crontab /etc/cron.*/*
+
 service rsyslog start
 service cron start
 
