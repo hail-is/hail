88c2186a514063bf9b36cce316a907d122037310
Cotton Seed <cotton@alum.mit.edu>
2018-09-22 18:07:35 -0400

    prep to merge into monorepo
    clean up unused files, experiments (jupyter, spark, etc.)

--- batch/launch-tests.py
+++
@@ -1,5 +0,0 @@
-import batch.client
-c = batch.client.BatchClient('http://localhost:5000')
-j = c.create_job('gcr.io/broad-ctsa/batch-test', ports=[5869])
-status = j.wait()
-print(status)
--- hail-ci-deploy.sh
+++ hail-ci-deploy.sh
@@ -1,29 +1,4 @@
+#!/bin/bash
 set -ex
 
-cd batch
-
-gcloud -q auth activate-service-account \
-  --key-file=/secrets/gcr-push-service-account-key.json
-
-gcloud -q auth configure-docker
-
-SHA=$(git rev-parse --short=12 HEAD)
-
-# get sha label of batch deployment
-DEPLOYED_SHA=$(kubectl get --selector=app=batch deployments -o "jsonpath={.items[*].metadata.labels.hail\.is/sha}")
-
-if [[ "$SHA" == "$DEPLOYED_SHA" ]]; then
-    exit 0
-fi
-
-# requires docker
-make push-batch
-
-sed -e "s,@sha@,$SHA," \
-    -e "s,@image@,$(cat batch-image)," \
-    < deployment.yaml.in > deployment.yaml
-
-kubectl apply -f deployment.yaml
-
-# ci can't recover from batch restart yet
-kubectl delete pods -l app=hail-ci
+(cd batch && /bin/bash hail-ci-deploy.sh)
