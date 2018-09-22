d9212449c21a55ad02834de7d7a51db201e3045e
Cotton Seed <cotton@alum.mit.edu>
2018-09-22 18:40:27 -0400

    fix

--- hail-ci-build.sh
+++ hail-ci-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-HAIL_NEEDS_REDEPLOY=$(python needs-redeploy.py target/$TARGET_BRANCH)
-if [[ $HAIL_NEEDS_REDEPLOY != no ]]; then
+HAIL_CHANGED=$(python project-changed.py target/$TARGET_BRANCH hail)
+if [[ $HAIL_CHANGED != no ]]; then
     (cd hail && /bin/bash hail-ci-build.sh)
 fi
