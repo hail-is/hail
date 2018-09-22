b65b7919090cc5c6d23a534f04a900e78e461cea
Cotton Seed <cotton@alum.mit.edu>
2018-09-22 18:35:21 -0400

    only test hail if project changed

--- hail-ci-build.sh
+++ hail-ci-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ex
 
-(cd hail && /bin/bash hail-ci-build.sh)
-
+HAIL_NEEDS_REDEPLOY=$(python needs-redeploy.py target/$TARGET_BRANCH)
+if [[ $HAIL_NEEDS_REDEPLOY != no ]]; then
+    (cd hail && /bin/bash hail-ci-build.sh)
+fi
