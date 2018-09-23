e4414a6c74f7d4a2461b5a0af08b90a6805087a6
Cotton Seed <cotton@alum.mit.edu>
2018-09-22 21:42:00 -0400

    hopefully fix python version conflict
    base image debian stretch 9.5
    install python3 via apt
    install miniconda in /opt/conda to pick up python2
    invoke python3 explicit for project-changed.py

--- hail-ci-build.sh
+++ hail-ci-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-HAIL_CHANGED=$(python project-changed.py target/$TARGET_BRANCH hail)
+HAIL_CHANGED=$(python3 project-changed.py target/$TARGET_BRANCH hail)
 if [[ $HAIL_CHANGED != no ]]; then
     (cd hail && /bin/bash hail-ci-build.sh)
 fi
