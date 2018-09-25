4d4e2bed5cb02880bdb0c6ef5b86102a38d7abf8
Cotton Seed <cotton@alum.mit.edu>
2018-09-25 17:43:04 -0400

    fixed indentation

--- hail-ci-build.sh
+++ hail-ci-build.sh
@@ -7,7 +7,7 @@ for project in $PROJECTS; do
     if [[ -e $project/hail-ci-build.sh ]]; then
         CHANGED=$(python3 project-changed.py target/$TARGET_BRANCH $project)
         if [[ $CHANGED != no ]]; then
-	    (cd $project && /bin/bash hail-ci-build.sh)
-	fi
+            (cd $project && /bin/bash hail-ci-build.sh)
+        fi
     fi
 done
