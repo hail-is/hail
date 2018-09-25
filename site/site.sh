81178114d1a6f9d8bf11bd11b4e89519666a32c2
Daniel King <daniel.zidan.king@gmail.com>
2018-09-25 12:29:04 -0400

    Update site.sh

--- site/site.sh
+++ site/site.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 bash /poll-devel.sh
 bash /poll-0.1.sh
 
@@ -9,5 +11,9 @@ touch /etc/crontab /etc/cron.*/*
 
 service rsyslog start
 service cron start
+
+# link nginx logs to docker log collectors
+ln -sf /dev/stdout /var/log/nginx/access.log
+ln -sf /dev/stderr /var/log/nginx/error.log
 
 nginx -g "daemon off;"
