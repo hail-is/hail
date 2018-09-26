be31136f9669a0858dc9be81806d9afa503fcf89
Tim Poterba <tpoterba@gmail.com>
2018-09-26 18:07:30 -0400

    Change font to Lucida

--- hail/python/hail/docs/makeDocs.sh
+++ hail/python/hail/docs/makeDocs.sh
@@ -13,15 +13,13 @@ mkdir -p build/www/ build/tmp/python/ build/tmp/docs build/www/docs
 # copy website content
 cp www/*.{js,css,css.map,html,png} build/www #  www/annotationdb/* does not exist
 
-curl https://gist.githubusercontent.com/killercup/5917178/raw/40840de5352083adb2693dc742e9f75dbb18650f/pandoc.css > build/pandoc.css
-
-pandoc -s ../README.md -f markdown -t html --mathjax --highlight-style=pygments --columns 10000 -o build/tmp/README.html --css build/pandoc.css
+pandoc -s ../README.md -f markdown -t html --mathjax --highlight-style=pygments --columns 10000 -o build/tmp/README.html
 xsltproc --html -o build/www/index.html www/readme-to-index.xslt build/tmp/README.html
 
-pandoc -s www/jobs.md -f markdown -t html --mathjax --highlight-style=pygments --columns 10000 -o build/tmp/jobs.html --css build/pandoc.css
+pandoc -s www/jobs.md -f markdown -t html --mathjax --highlight-style=pygments --columns 10000 -o build/tmp/jobs.html
 xsltproc --html -o build/www/jobs.html www/jobs.xslt build/tmp/jobs.html
 
-pandoc -s www/about.md -f markdown -t html --mathjax --highlight-style=pygments --columns 10000 -o build/tmp/about.html --css build/pandoc.css
+pandoc -s www/about.md -f markdown -t html --mathjax --highlight-style=pygments --columns 10000 -o build/tmp/about.html
 xsltproc --html -o build/www/about.html www/about.xslt build/tmp/about.html
 
 cp -R python build/tmp
