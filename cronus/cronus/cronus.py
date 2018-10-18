58d63c4b0ab13f64d65289e200403c0bdb098ca9
Daniel King <daniel.zidan.king@gmail.com>
2018-10-18 16:08:13 -0700

    newlines

--- cronus/cronus/cronus.py
+++ cronus/cronus/cronus.py
@@ -13,12 +13,15 @@ fmt = logging.Formatter(
    # NB: no space after levelname because WARNING is so long
    '%(levelname)s\t| %(asctime)s \t| %(filename)s \t| %(funcName)s:%(lineno)d | '
    '%(message)s')
+
 fh = logging.FileHandler('cronus.log')
 fh.setLevel(logging.INFO)
 fh.setFormatter(fmt)
+
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 ch.setFormatter(fmt)
+
 log = logging.getLogger('cronus')
 log.setLevel(logging.INFO)
 logging.basicConfig(
