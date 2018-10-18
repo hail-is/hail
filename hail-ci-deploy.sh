5a67787100977e0ee675d523d1bad5999564dc97
cseed <cotton@alum.mit.edu>
2018-10-17 21:16:34 -0400

    fix 0.1 deploy (ci is gone) (#4575)

--- hail-ci-deploy.sh
+++ hail-ci-deploy.sh
@@ -1,7 +1,6 @@
 set -ex
 
 SPARK_VERSION=2.0.2
-BRANCH_SOURCE_NAME=stable
 BRANCH_TARGET_NAME=0.1
 HASH_TARGET=gs://hail-common/builds/${BRANCH_TARGET_NAME}/latest-hash-spark-${SPARK_VERSION}.txt
 SHA=$(git rev-parse --short=12 HEAD)
@@ -45,40 +44,6 @@ tar cvf hail-${BRANCH_TARGET_NAME}-docs-${SHA}.tar.gz -C build/www docs
 gsutil cp hail-${BRANCH_TARGET_NAME}-docs-${SHA}.tar.gz ${DOCS}
 gsutil acl set public-read ${DOCS}
 
-# update website
-## since we're non-interactive, we explicitly state the fingerprint for ci.hail.is
-mkdir -p ~/.ssh
-printf 'ci.hail.is ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3tuH5V3ubO7PqQ3gD2G7yFJ4bkwgSFNBqmaLmiuiCF86UpE4Lo4yQryt9VYssoLqsdStIOR0P/Bo3S4Nuj8cHCzAbft3/u25oa8lQKAazoiA0I82d7JXYurV/NvjH7O1MMuPohwjlBp+d4damUA3TO2oIHbYqzmArrvTs/k6DxUonWRRxZa0zW+edv78y6IdLXuSVyN5FPa+jWBMJar9CsvbsWUWtcJ8vHHldg0DJ7TFVecouy4U3hmQxi90OCGSk4N9vi+XC+EjoNeCmGt5/VGAnKCUZntOZluBqKKZ0/TWlC6HJgBWYQllnjAE1tFs9Xrrx+5ADB9quMtYVqk0R\n' \
-       >> ~/.ssh/known_hosts
-
-USER=web-updater
-IDENTITY_FILE=/secrets/ci.hail.is-web-updater-rsa-key
-
-rsync -rlv \
-      -e "ssh -i ${IDENTITY_FILE}" \
-      --exclude docs \
-      --exclude misc \
-      --exclude tools \
-      build/www/ \
-      ${USER}@ci.hail.is:/var/www/html/ \
-      --delete
-
-DEST=/var/www/html/docs/archive/${BRANCH_TARGET_NAME}/$SHA
-
-ssh -i ${IDENTITY_FILE} \
-    ${USER}@ci.hail.is \
-    mkdir -p $DEST
-
-scp -i ${IDENTITY_FILE} \
-    -r build/www/docs/${BRANCH_SOURCE_NAME}/* \
-    ${USER}@ci.hail.is:$DEST
-
-ssh -i ${IDENTITY_FILE} \
-    ${USER}@ci.hail.is \
-    "rm -rf /var/www/html/docs/${BRANCH_TARGET_NAME} && \
-     ln -s $DEST /var/www/html/docs/${BRANCH_TARGET_NAME} && \
-     chgrp www-data $DEST /var/www/html/docs/${BRANCH_TARGET_NAME}"
-     
 echo ${SHA} > latest-hash-spark-${SPARK_VERSION}.txt
 gsutil cp ./latest-hash-spark-${SPARK_VERSION}.txt ${HASH_TARGET}
 gsutil acl set public-read ${HASH_TARGET}
