7ea3ae982922a9dfe22cba2fc35828ceaa5ebae6
Cotton Seed <cotton@alum.mit.edu>
2018-10-19 11:31:47 -0400

    deploy artifacts as 0.2, not devel

--- hail/hail-ci-deploy.sh
+++ hail/hail-ci-deploy.sh
@@ -2,7 +2,7 @@
 set -ex
 
 SPARK_VERSION=2.2.0
-BRANCH=devel
+BRANCH=0.2
 CLOUDTOOLS_VERSION=2
 HASH_TARGET=gs://hail-common/builds/${BRANCH}/latest-hash/cloudtools-${CLOUDTOOLS_VERSION}-spark-${SPARK_VERSION}.txt
 SHA=$(git rev-parse --short=12 HEAD)
