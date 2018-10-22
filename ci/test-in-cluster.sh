37f0cbe458905bbae78af1deb05851894a0d04af
Cotton Seed <cotton@alum.mit.edu>
2018-10-22 16:34:51 -0400

    clarify namespace on hostnames

--- ci/test-in-cluster.sh
+++ ci/test-in-cluster.sh
@@ -27,6 +27,6 @@ cp /secrets/hail-ci-0-1.key gcloud-token
 
 export IN_CLUSTER=true
 export SELF_HOSTNAME=https://ci.hail.is/$SERVICE_NAME
-export BATCH_SERVER_URL=http://batch
+export BATCH_SERVER_URL=http://batch.default
 
 ./test-locally.sh
