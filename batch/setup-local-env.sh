ca290399a28863bfef104887430ca27a203893f6
Daniel King <daniel.zidan.king@gmail.com>
2018-10-22 11:55:23 -0400

    update

--- batch/setup-local-env.sh
+++ batch/setup-local-env.sh
@@ -3,7 +3,13 @@
 
 set -ex
 
+# dependencies
+docker version || brew cask install docker # FIXME: non OS X environments
+conda -V || (cd /tmp && curl -LO https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh && bash /tmp/Miniconda3-latest-MacOSX-x86_64.sh)
+kubectl version || brew install kubernetes-cli # FIXME: non OS X environments
+
 conda env create -f environment.yml || conda env update -f environment.yml
+. activate hail-batch
 make build
 SVC_ACCT_NAME=$(whoami)-gke
 KEY_FILE=~/.hail/gke/svc-acct/${SVC_ACCT_NAME}.json
