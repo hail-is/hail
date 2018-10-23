1a01a19c0eea91164fdb4c080399937e04e39e2b
Cotton Seed <cotton@alum.mit.edu>
2018-10-23 18:19:13 -0400

    get sha in default namespace

--- ci/get-deployed-sha.sh
+++ ci/get-deployed-sha.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-kubectl get --selector=app=hail-ci deployments -o "jsonpath={.items[*].metadata.labels.hail\.is/sha}"
+kubectl -n default get --selector=app=hail-ci deployments -o "jsonpath={.items[*].metadata.labels.hail\.is/sha}"
