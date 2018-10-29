97a1b05c12b59624805ca4bd912bc41f41693272
Christopher Vittal <christopher.vittal@gmail.com>
2018-10-29 17:12:04 -0400

    Restructure

--- hail/src/main/scala/is/hail/io/tabix/TabixReader.scala
+++ hail/src/main/scala/is/hail/io/tabix/TabixReader.scala
@@ -1,7 +1,7 @@
 package is.hail.io.tabix
 
 import is.hail.HailContext
-import is.hail.utils.{ArrayBuilder, fatal}
+import is.hail.utils._
 
 import htsjdk.tribble.util.{ParsingUtils, TabixUtils}
 import org.apache.hadoop.io.compress.SplitCompressionInputStream
@@ -49,72 +49,68 @@ class TabixReader(val filePath: String, private val idxFilePath: Option[String])
   private val hc = HailContext.get
   private val sc = hc.sc
   private val hConf = sc.hadoopConfiguration
-  private lazy val indexStream: SplitCompressionInputStream = null
 
-  lazy val index: Tabix = {
-    val is = indexStream
-    if (is == null) {
-      null
-    } else {
-      var buf = new Array[Byte](4)
-      is.read(buf, 0, 4)
-      assert(Magic sameElements buf, "magic number failed validation")
-      val seqs = new Array[String](readInt(is))
-      val format = readInt(is)
-      val colSeq = readInt(is)
-      val colBeg = readInt(is)
-      val colEnd = readInt(is)
-      val meta = readInt(is)
-      readInt(is) // unused
+  def readIndex(is: InputStream): Tabix = {
+    var buf = new Array[Byte](4)
+    is.read(buf, 0, 4)
+    assert(Magic sameElements buf, "magic number failed validation")
+    val seqs = new Array[String](readInt(is))
+    val format = readInt(is)
+    val colSeq = readInt(is)
+    val colBeg = readInt(is)
+    val colEnd = readInt(is)
+    val meta = readInt(is)
+    readInt(is) // unused, need to consume
 
-      // read the sequence dictionary
-      buf = new Array[Byte](readInt(is)) // # sequences
-      var (i, j, k) = (0, 0, 0)
-      is.read(buf)
-      while (i < buf.length) {
-        if (buf(i) == 0) {
-          val contig = new String(buf.slice(j, i))
-          // TODO map????
-          seqs(k) = contig
-          k += 1
-          j = i + 1
-        }
-        i += 1
+    // read the sequence dictionary
+    buf = new Array[Byte](readInt(is)) // # sequences
+    var (i, j, k) = (0, 0, 0)
+    is.read(buf)
+    while (i < buf.length) {
+      if (buf(i) == 0) {
+        val contig = new String(buf.slice(j, i))
+        // TODO map????
+        seqs(k) = contig
+        k += 1
+        j = i + 1
       }
-
-      // read the index
-      val indices = new ArrayBuilder[(HashMap[Int, Array[TbiPair]], Array[Long])](seqs.length)
-      i = 0
-      while (i < seqs.length) {
-        // binning index
-        val nBin = readInt(is);
-        val binIdx = new HashMap[Int, Array[TbiPair]]()
-        j = 0
-        while (j < nBin) {
-          val bin = readInt(is);
-          val chunks = new Array[TbiPair](readInt(is))
-          k = 0
-          while (k < chunks.length) {
-            chunks(k) = readLong(is) -> readLong(is)
-            k += 1
-          }
-          binIdx += bin -> chunks
-          j += 1
-        }
-        // linear index
-        val linIdx = new Array[Long](readInt(is))
-        k = 0
-        while (k < linIdx.length) {
-          linIdx(k) = readLong(is)
-          k += 1
-        }
-        indices += binIdx -> linIdx
-        i += 1
-      }
-      is.close()
-      new Tabix(format, colSeq, colBeg, meta, seqs, indices.result())
+      i += 1
     }
+
+    // read the index
+    val indices = new ArrayBuilder[(HashMap[Int, Array[TbiPair]], Array[Long])](seqs.length)
+    i = 0
+    while (i < seqs.length) {
+      // binning index
+      val nBin = readInt(is);
+      val binIdx = new HashMap[Int, Array[TbiPair]]()
+      j = 0
+      while (j < nBin) {
+        val bin = readInt(is);
+        val chunks = new Array[TbiPair](readInt(is))
+        k = 0
+        while (k < chunks.length) {
+          chunks(k) = readLong(is) -> readLong(is)
+          k += 1
+        }
+        binIdx += bin -> chunks
+        j += 1
+      }
+      // linear index
+      val linIdx = new Array[Long](readInt(is))
+      k = 0
+      while (k < linIdx.length) {
+        linIdx(k) = readLong(is)
+        k += 1
+      }
+      indices += binIdx -> linIdx
+      i += 1
+    }
+    is.close()
+    new Tabix(format, colSeq, colBeg, meta, seqs, indices.result())
   }
+
+  lazy val index: Tabix = hConf.readFile(indexPath)(readIndex(_))
 }
 
 class Tabix(
