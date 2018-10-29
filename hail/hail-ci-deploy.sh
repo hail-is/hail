d6640ac6eee78abded0c60a494ad7f9a2751f56f
Tim Poterba <tpoterba@gmail.com>
2018-10-29 14:48:04 -0400

    bump cloudtools version (#4672)

--- hail/hail-ci-deploy.sh
+++ hail/hail-ci-deploy.sh
@@ -3,7 +3,7 @@ set -ex
 
 SPARK_VERSION=2.2.0
 BRANCH=0.2
-CLOUDTOOLS_VERSION=2
+CLOUDTOOLS_VERSION=3
 HASH_TARGET=gs://hail-common/builds/${BRANCH}/latest-hash/cloudtools-${CLOUDTOOLS_VERSION}-spark-${SPARK_VERSION}.txt
 SHA=$(git rev-parse --short=12 HEAD)
 
