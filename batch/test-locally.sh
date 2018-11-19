82c350efd455f0de5e0c807a8a24919dcad14434
Daniel King <daniel.zidan.king@gmail.com>
2018-11-19 11:08:01 -0500

    [batch][dag1] Teach batch to test itself locally (#4786)
    * fix test locally
    
    * add maximum tries
    
    * fix batch to always kill background task
    
    * better approach
    
    * wait 30 seconds
    
    * fix

---
+++ batch/test-locally.sh
@@ -0,0 +1,20 @@
+set -ex
+
+cleanup() {
+    set +e
+    trap "" INT TERM
+    kill -9 $server_pid
+}
+trap cleanup EXIT
+trap "exit 24" INT TERM
+
+BATCH_USE_KUBE_CONFIG=1 python batch/server.py &
+server_pid=$!
+
+until curl -fL 127.0.0.1:5000/jobs >/dev/null 2>&1
+do
+    ((tries++)) && [ $tries -lt 30 ]
+    sleep 1
+done
+
+POD_IP='127.0.0.1' BATCH_URL='http://127.0.0.1:5000' python -m unittest -v test/test_batch.py
