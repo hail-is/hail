17d8e4c2feca230cb6bf3bfb9e11458b4ba78201
Daniel King <daniel.zidan.king@gmail.com>
2018-11-26 15:09:00 -0500

    move constants into globals (#4796)

--- batch/batch/server/globals.py
+++ batch/batch/server/globals.py
@@ -1,3 +1,17 @@
+import os
+import uuid
+from .log import log
+
+KUBERNETES_TIMEOUT_IN_SECONDS = float(os.environ.get('KUBERNETES_TIMEOUT_IN_SECONDS', 5.0))
+REFRESH_INTERVAL_IN_SECONDS = int(os.environ.get('REFRESH_INTERVAL_IN_SECONDS', 5 * 60))
+POD_NAMESPACE = os.environ.get('POD_NAMESPACE', 'batch-pods')
+INSTANCE_ID = uuid.uuid4().hex
+
+log.info(f'KUBERNETES_TIMEOUT_IN_SECONDS {KUBERNETES_TIMEOUT_IN_SECONDS}')
+log.info(f'REFRESH_INTERVAL_IN_SECONDS {REFRESH_INTERVAL_IN_SECONDS}')
+log.info(f'POD_NAMESPACE {POD_NAMESPACE}')
+log.info(f'INSTANCE_ID = {INSTANCE_ID}')
+
 pod_name_job = {}
 job_id_job = {}
 batch_id_batch = {}
