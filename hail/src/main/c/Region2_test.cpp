91f88de81e9224ffbac39a60a3e823f3446c57cf
wang <wang@broadinstitute.org>
2018-11-26 16:57:13 -0500

    add benchmark test

--- hail/src/main/c/Region2_test.cpp
+++ hail/src/main/c/Region2_test.cpp
@@ -1,5 +1,8 @@
 #include "catch.hpp"
 #include "hail/Region2.h"
+#include <vector>
+#include <ctime>
+#include <cstring>
 
 namespace hail {
 
@@ -49,6 +52,30 @@ TEST_CASE("region pools allocate and manage regions/blocks") {
       REQUIRE(pool.num_free_regions() == 2);
     }
   }
+}
+
+TEST_CASE("get_region() speed for allocating 10,000 regions in different batch sizes", "[!benchmark]") {
+  RegionPool pool;
+  REQUIRE(pool.num_free_regions() == 0);
+  REQUIRE(pool.num_free_blocks() == 0);
+
+  std::vector<std::shared_ptr<Region2>> regions {};
+  REQUIRE(regions.size() == 0);
+
+  std::vector<int> batches { 5, 25, 100, 1000, 10000 };
+
+  for(int size : batches) {
+    SECTION(std::string("SIZE=") + std::to_string(size)) {
+      for (int i = 0; i < 10000; i += size) {
+        for (int j = 0; j < size; ++j) {
+          regions.push_back(pool.get_region());
+        }
+        regions.clear();
+      }
+    }
+  }
+  REQUIRE(regions.size() == 0);
+
 }
 
 }
