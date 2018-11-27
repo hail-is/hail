6a61c3a2827f7dd684e16257afcf293f6b3cdbc0
Christopher Vittal <cvittal@broadinstitute.org>
2018-11-26 23:01:37 -0500

    Add readRepeatedly to RichInputStream
    The new functions will continue reading until either enough bytes are
    read into the buffer, or read returns 0 or 1. In either case, the number
    of bytes read into the buffer is returned.

--- hail/src/main/scala/is/hail/utils/richUtils/RichInputStream.scala
+++ hail/src/main/scala/is/hail/utils/richUtils/RichInputStream.scala
@@ -4,16 +4,22 @@ import java.io.InputStream
 import is.hail.utils._
 
 class RichInputStream(val in: InputStream) extends AnyVal {
-  def readFully(to: Array[Byte], toOff: Int, n: Int) {
-    assert(toOff + n <= to.length)
+  def readFully(to: Array[Byte], toOff: Int, n: Int) =
+    if (readRepeatedly(to, toOff, n) < n) fatal("Premature end of file")
 
+  def readRepeatedly(to: Array[Byte], toOff: Int, n: Int): Int = {
+    assert(toOff + n <= to.length)
     var read = 0
-    while (read < n) {
+    var endOfStream = false
+    while (read < n && !endOfStream) {
       val r = in.read(to, toOff + read, n - read)
-      assert(r != 0)
-      if (r == -1)
-        fatal("Premature end of file")
-      read += r
+      if (r <= 0)
+        endOfStream = true
+      else
+        read += r
     }
+    read
   }
+
+  def readRepeatedly(to: Array[Byte]): Int = readRepeatedly(to, 0, to.length)
 }
