7afb0fd989b15dc1fa02ef2a30e8870fa8dca5cf
Jackie Goldstein <jigold@broadinstitute.org>
2018-11-28 17:48:55 -0500

    wip

--- hail/python/test/hail/expr/test_types.py
+++ hail/python/test/hail/expr/test_types.py
@@ -58,10 +58,11 @@ class Tests(unittest.TestCase):
                 else:
                     self.assertNotEqual(ts[i], ts2[j])
 
-    def test_type_parser_roundtrip(self):
+    def test_type_jvm_roundtrip(self):
         ts = self.types_to_test()
         for t in ts:
             rev_str = t._parsable_string()
+            jtyp =
             self.assertEqual(t, dtype(rev_str))
 
     def test_pretty_roundtrip(self):
