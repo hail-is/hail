4973f222f4bd372d640b0051964137a2b6077ffc
Daniel King <daniel.zidan.king@gmail.com>
2018-12-06 20:11:45 -0500

    Update connect.py (#126)

--- cloudtools/connect.py
+++ cloudtools/connect.py
@@ -60,5 +60,6 @@ def main(args):
             'http://localhost:{}'.format(connect_port),
             '--proxy-server=socks5://localhost:{}'.format(args.port),
             '--host-resolver-rules=MAP * 0.0.0.0 , EXCLUDE localhost',
+            '--proxy-bypass-list=<-loopback>', # https://chromium.googlesource.com/chromium/src/+/da790f920bbc169a6805a4fb83b4c2ab09532d91
             '--user-data-dir=/tmp/'
         ], stdout=f, stderr=f)
