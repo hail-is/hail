c652acf579818de9a3a3234209fcc166620869eb
Tim Poterba <tpoterba@gmail.com>
2018-12-06 20:34:24 -0500

    [Release] Create changelog and update with 0.2.5

--- hail/python/hail/docs/makeDocs.sh
+++ hail/python/hail/docs/makeDocs.sh
@@ -2,6 +2,14 @@
 
 set -ex
 
+cleanup() {
+    trap "" INT TERM
+    set +e
+    rm -f python/hail/docs/change_log.rst
+}
+trap cleanup EXIT
+trap 'exit 1' INT TERM
+
 # make directories
 mkdir -p build/www/ build/tmp/python/ build/tmp/docs build/www/docs
 
@@ -17,8 +25,13 @@ xsltproc --html -o build/www/jobs.html www/jobs.xslt build/tmp/jobs.html
 pandoc -s www/about.md -f markdown -t html --mathjax --highlight-style=pygments --columns 10000 -o build/tmp/about.html
 xsltproc --html -o build/www/about.html www/about.xslt build/tmp/about.html
 
+# sed for creating GitHub links
+pandoc <(cat python/hail/docs/change_log.md | sed -E "s/\(hail\#([0-9]+)\)/(\[#\1](https:\/\/github.com\/hail-is\/hail\/pull\/\1))/g") -o python/hail/docs/change_log.rst
+
 cp -R python build/tmp
 
 (cd build/tmp/python/hail/docs && make BUILDDIR=_build clean html)
 
-mv build/tmp/python/hail/docs/_build/html build/www/docs/$HAIL_SHORT_VERSION
+DEST="build/www/docs/${HAIL_SHORT_VERSION}/"
+rm -rf ${DEST}
+mv build/tmp/python/hail/docs/_build/html $DEST
