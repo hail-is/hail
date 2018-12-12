4806b7d2d673247077a1b5adaada1dc69f359355
wang <wang@broadinstitute.org>
2018-12-12 14:14:16 -0500

    fix

--- hail/src/main/resources/include/hail/RegionPool.h
+++ hail/src/main/resources/include/hail/RegionPool.h
@@ -15,26 +15,16 @@ class RegionPool {
 
   private:
     std::vector<std::unique_ptr<Region>> regions_{};
-    std::vector<Region *> free_regions_{};
-    std::vector<std::unique_ptr<char[]>> free_blocks_{};
     Region::SharedPtr new_region();
 
   public:
+    std::vector<Region *> free_regions_{};
+    std::vector<std::unique_ptr<char[]>> free_blocks_{};
     RegionPool() = default;
     RegionPool(RegionPool &p) = delete;
     RegionPool(RegionPool &&p) = delete;
     std::unique_ptr<char[]> get_block();
     Region::SharedPtr get_region();
-    std::vector<std::unique_ptr<char[]>> && free_blocks(std::vector<std::unique_ptr<char[]>> &&block) {
-      std::move(std::begin(block), std::end(block), std::back_inserter(free_blocks_));
-      block.clear();
-      return std::move(block);
-    }
-
-    void free_region(Region::SharedPtr region) {
-      free_regions_.push_back(region.get());
-      region = nullptr;
-    }
 
     //tracking methods:
     size_t num_regions() { return regions_.size(); }
