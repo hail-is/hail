878f5045f2dc4efe2d8fe7ef133e8e9c3b6cc326
Tim Poterba <tpoterba@gmail.com>
2018-12-18 13:53:49 -0500

    Add benchmark methods for annotate_rows and annotate_cols with many aggs (#4986)

--- hail/python/benchmark/matrix_table_benchmarks.py
+++ hail/python/benchmark/matrix_table_benchmarks.py
@@ -29,3 +29,53 @@ def matrix_table_rows_force_count():
 def matrix_table_rows_is_transition():
     ht = hl.read_matrix_table(resource('profile.mt')).rows().key_by()
     ht.select(is_snp = hl.is_snp(ht.alleles[0], ht.alleles[1]))._force_count()
+
+def many_aggs(mt):
+    aggs = [
+        hl.agg.count_where(mt.GT.is_hom_ref()),
+        hl.agg.count_where(mt.GT.is_het()),
+        hl.agg.count_where(mt.GT.is_hom_var()),
+        hl.agg.count_where(mt.GT.is_non_ref()),
+        hl.agg.count_where(mt.GT.n_alt_alleles() == 2),
+        hl.agg.count_where(mt.GT.phased),
+        hl.agg.count_where(mt.GT.is_haploid()),
+        hl.agg.count_where(mt.GT.is_diploid()),
+        hl.agg.count_where(mt.GT.ploidy == 2),
+        hl.agg.fraction(mt.AD[0] > 0),
+        hl.agg.fraction(mt.AD[0] < 0),
+        hl.agg.fraction(mt.AD.length() < 0),
+        hl.agg.fraction(mt.AD.length() > 0),
+        hl.agg.fraction(mt.PL[0] > 0),
+        hl.agg.fraction(mt.PL[0] < 0),
+        hl.agg.fraction(mt.PL.length() < 0),
+        hl.agg.fraction(mt.PL.length() > 0),
+        hl.agg.fraction(mt.GQ < 0),
+        hl.agg.fraction(mt.GQ > 0),
+        hl.agg.fraction(mt.GQ % 2 == 0),
+        hl.agg.fraction(mt.GQ % 2 != 0),
+        hl.agg.fraction(mt.GQ / 5 < 10),
+        hl.agg.fraction(mt.GQ / 5 <= 10),
+        hl.agg.fraction(mt.GQ / 5 > 10),
+        hl.agg.fraction(mt.GQ / 5 >= 10),
+        hl.agg.fraction(mt.DP < 0),
+        hl.agg.fraction(mt.DP > 0),
+        hl.agg.fraction(mt.DP % 2 == 0),
+        hl.agg.fraction(mt.DP % 2 != 0),
+        hl.agg.fraction(mt.DP / 5 < 10),
+        hl.agg.fraction(mt.DP / 5 <= 10),
+        hl.agg.fraction(mt.DP / 5 > 10),
+        hl.agg.fraction(mt.DP / 5 >= 10),
+    ]
+    return {f'x{i}': expr for i, expr in enumerate(aggs)}
+
+@benchmark
+def matrix_table_many_aggs_row_wise():
+    mt = hl.read_matrix_table(resource('profile.mt'))
+    mt = mt.annotate_rows(**many_aggs(mt))
+    mt.rows()._force_count()
+
+@benchmark
+def matrix_table_many_aggs_col_wise():
+    mt = hl.read_matrix_table(resource('profile.mt'))
+    mt = mt.annotate_cols(**many_aggs(mt))
+    mt.cols()._force_count()
