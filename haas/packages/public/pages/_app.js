98e13c7ef09ab17bfef74f45aa26202e7940f269
Alex Kotlar <akotlar@bu.edu>
2018-12-19 23:56:49 -0500

    replace svg with font

--- haas/packages/public/pages/_app.js
+++ haas/packages/public/pages/_app.js
@@ -142,15 +142,9 @@ class Hail extends App {
               auth={Auth}
             />
           </div>
-          <span className="icon-button" onClick={this.toggleTheme}>
-            <svg width="24" height="24" viewBox="0 0 24 24">
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path
-                fill={this.state.darkTheme ? 'white' : 'black'}
-                d="M20 15.31L23.31 12 20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69zM12 18V6c3.31 0 6 2.69 6 6s-2.69 6-6 6z"
-              />
-            </svg>
-          </span>
+          <i className="material-icons icon-button" onClick={this.toggleTheme}>
+            brightness_medium
+          </i>
         </div>
       </Container>
     );
