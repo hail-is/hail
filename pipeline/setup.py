0d4d1f3975bc0febc476542accb35abbdf970606
Jackie Goldstein <jigold@broadinstitute.org>
2019-01-09 15:52:54 -0500

    change directory structure

---
+++ pipeline/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pipeline',
+    version = '0.0.1',
+    url = 'https://github.com/hail-is/pipeline.git',
+    author = 'Hail Team',
+    author_email = 'hail@broadinstitute.org',
+    description = 'Pipeline builder',
+    packages = find_packages(),
+    install_requires=[],
+)
