f3f294bd4fdbdab1513014cb24bef95c92aacf12
Jackie Goldstein <jigold@broadinstitute.org>
2019-01-10 15:41:56 -0500

    added docker support

--- pipeline/pipeline/backend.py
+++ pipeline/pipeline/backend.py
@@ -3,7 +3,7 @@ import os
 import subprocess as sp
 
 from .resource import Resource, ResourceGroup
-from .utils import get_sha
+from .utils import get_sha, escape_string
 
 
 class Backend(object):
@@ -25,13 +25,13 @@ class LocalBackend(Backend):
         self._tmp_dir = tmp_dir
 
     def run(self, pipeline, dry_run, verbose, bg, delete_on_exit):
-        wd = self.tmp_dir()
+        tmpdir = self.tmp_dir()
 
         script = ['#!/bin/bash',
                   'set -e' + 'x' if verbose else '',
                   '\n',
                   '# change cd to tmp directory',
-                  f"cd {wd}",
+                  f"cd {tmpdir}",
                   '\n']
 
         def define_resource(r):
@@ -40,23 +40,35 @@ class LocalBackend(Backend):
 
             if isinstance(r, Resource):
                 assert r._value is not None
-                init = f"{r._uid}={r._value}"
+                init = f"{r._uid}={escape_string(r._value)}"
             else:
                 assert isinstance(r, ResourceGroup)
-                init = f"{r._uid}={r._root}"
+                init = f"{r._uid}={escape_string(r._root)}"
             return init
 
         for task in pipeline._tasks:
             script.append(f"# {task._uid} {task._label if task._label else ''}")
-            script += [define_resource(r) for _, r in task._resources.items()]
-            script += task._command + ["\n"]
+
+            resource_defs = [define_resource(r) for _, r in task._resources.items()]
+            if task._settings.docker:
+                defs = '; '.join(resource_defs) + '; ' if resource_defs else ''
+                cmd = "&& ".join(task._command)
+                image = task._settings.docker
+                script += [f"docker run "
+                            f"-v {tmpdir}:{tmpdir} "
+                            f"-w {tmpdir} "
+                            f"{image} /bin/bash "
+                            f"-c {escape_string(defs + cmd)}",
+                           '\n']
+            else:
+                script += resource_defs
+                script += task._command + ['\n']
 
         script = "\n".join(script)
 
         if dry_run:
             print(script)
-
-        if not dry_run:
+        else:
             try:
                 sp.check_output(script, shell=True) # FIXME: implement non-blocking (bg = True)
             except sp.CalledProcessError as e:
@@ -64,7 +76,7 @@ class LocalBackend(Backend):
                 raise e
             finally:
                 if delete_on_exit:
-                    sp.run(f'rm -r {wd}', shell=True)
+                    sp.run(f'rm -r {tmpdir}', shell=True)
 
     def tmp_dir(self):
         def _get_random_name():
--- pipeline/pipeline/utils.py
+++ pipeline/pipeline/utils.py
@@ -1,5 +1,9 @@
-import random, string
+import random, string, shlex
 
 
 def get_sha(k):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=k))
+
+
+def escape_string(s):
+    return shlex.quote(s)
