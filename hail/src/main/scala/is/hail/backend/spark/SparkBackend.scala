93371444a420bb4391ebf3db9d6e2058321f1818
wang <wang@broadinstitute.org>
2019-01-10 16:32:03 -0500

    wip

--- hail/src/main/scala/is/hail/backend/spark/SparkBackend.scala
+++ hail/src/main/scala/is/hail/backend/spark/SparkBackend.scala
@@ -1,7 +1,7 @@
 package is.hail.backend.spark
 
 import is.hail.expr.JSONAnnotationImpex
-import is.hail.expr.ir.IR
+import is.hail.expr.ir._
 import org.json4s.jackson.JsonMethods
 
 object SparkBackend {
@@ -12,7 +12,27 @@ object SparkBackend {
       JSONAnnotationImpex.exportAnnotation(value, t))
   }
 
-  def execute(ir: IR): Any = {
-    ???
+  def execute(ir0: IR): Any = {
+    var ir = ir0
+
+    println("SparkBackend.execute got", Pretty(ir))
+
+    ir = ir.unwrap
+    ir = Optimize(ir, noisy = true, canGenerateLiterals = true)
+    ir = LiftLiterals(ir).asInstanceOf[IR]
+    ir = LowerMatrixIR(ir)
+    ir = Optimize(ir, noisy = true, canGenerateLiterals = false)
+
+    println("LocalBackend.execute to lower", Pretty(ir))
+
+    ir = LowerTableIR.lower(ir)
+
+    println("LocalBackend.execute lowered", Pretty(ir))
+
+    ir = Optimize(ir, noisy = true, canGenerateLiterals = false)
+
+    println("LocalBackend.execute", Pretty(ir))
+
+    Interpret[Any](ir)
   }
 }
