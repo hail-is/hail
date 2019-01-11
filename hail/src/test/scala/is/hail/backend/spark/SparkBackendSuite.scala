56393cf9dfc7a12dfc3dea705125213763face5e
wang <wang@broadinstitute.org>
2019-01-11 15:32:47 -0500

    change test case

--- hail/src/test/scala/is/hail/backend/spark/SparkBackendSuite.scala
+++ hail/src/test/scala/is/hail/backend/spark/SparkBackendSuite.scala
@@ -7,8 +7,8 @@ import org.testng.annotations.Test
 class SparkBackendSuite extends SparkSuite {
 
   @Test def testRangeCount() {
-    val node = ir.TableCount(ir.TableRange(10, 2))
-    assert(SparkBackend.execute(hc.sc, node, optimize = false) == 10)
+    val node = ir.ApplyBinaryPrimOp(ir.Add(), ir.TableCount(ir.TableRange(10, 2)), ir.TableCount(ir.TableRange(15, 5)))
+    assert(SparkBackend.execute(hc.sc, node, optimize = false) == 25)
   }
 
 }
