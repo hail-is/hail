476a5a3096b796e07e6aabb45d015d2442d8190f
Alex Kotlar <akotlar@bu.edu>
2019-01-19 16:47:03 -0500

    an example of polling in the background for data that we're ok with being somewhat stale

--- web/packages/web-client/pages/scorecard.tsx
+++ web/packages/web-client/pages/scorecard.tsx
@@ -1,24 +1,91 @@
 import { PureComponent } from 'react';
 import fetch from 'isomorphic-unfetch';
 
-class Scorecard extends PureComponent {
-  static async getInitialProps() {
-    const data = await fetch('https://scorecard.hail.is/json').then(d =>
-      d.json()
-    );
+const URL = 'https://scorecard.hail.is/json';
+declare type scorecardJson = {
+  data: object;
+};
 
-    return { pageProps: { data } };
+interface Props {
+  pageProps: scorecardJson;
+}
+
+// TODO: think about triggering this in _app.js
+// An example of how we can cache events that can be slightly stale
+// at some memory cost
+// With the benefit that if refresh is smaller than the
+// time between page clicks, non-stale state will be served in << 16ms on click
+let cache: object;
+
+let timeout: NodeJS.Timeout;
+const startPolling = (ms: number = 1 * 60 * 1000) => {
+  if (timeout) {
+    clearTimeout(timeout);
   }
 
-  async componentDidMount() {
-    // const data = await fetch('https://scorecard.hail.is/json').then(d =>
-    //   d.json()
-    // );
-    //console.info('fetched');
+  timeout = setInterval(() => {
+    fetch(URL)
+      .then(d => d.json())
+      .then(data => {
+        cache = data;
+      });
+  }, ms);
+};
+
+class Scorecard extends PureComponent<Props, scorecardJson> {
+  // Data that is fetched during the server rendering phase does not need
+  // to be re-fetched during the client rendering phase
+  // The data is automatically available under this.props.pageProps
+  static async getInitialProps() {
+    if (typeof window === 'undefined') {
+      let ssr = {};
+      ssr = await fetch(URL).then(d => d.json());
+      return { pageProps: { data: ssr } };
+    }
+
+    return { pageProps: null };
+  }
+
+  constructor(props: any) {
+    super(props);
+
+    // Initial page load (from refresh/ssr phase)
+    // will have pageProps; so lets re-use these
+    // After that, lets use our cached/polled version
+    if (this.props.pageProps !== null) {
+      this.state = { data: this.props.pageProps.data };
+
+      // We will set the initial cache state, and then poll to update that this.state
+      // Every time this page is visited after initial load, cache will be used
+      if (typeof window !== 'undefined') {
+        cache = this.props.pageProps.data;
+        startPolling();
+      }
+    } else {
+      this.state = { data: cache };
+    }
   }
 
   render() {
-    return <div>Scorecard</div>;
+    if (!this.state.data) {
+      return <div>No data</div>;
+    }
+
+    return <div>Have data</div>;
+    // <span id="scorecard">
+    //   <table>
+    //     <thead>
+    //       <tr>User</tr>
+    //       <tr>Review</tr>
+    //       <tr>Change</tr>
+    //       <tr>Issues</tr>
+    //     </thead>
+    //     <tbody>
+    //       <trow>
+    //       <th>User</th>
+    //     </tbody>
+    //   </table>
+    // </span>
   }
 }
 
