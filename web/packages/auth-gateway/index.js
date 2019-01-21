b5d5c8b519389ebed6931219942637d311f5ab18
Alex Kotlar <akotlar@bu.edu>
2019-01-20 20:37:44 -0500

    working server with authentication at any endpoint; custom from underlying libraries, no express middleware used; turns out they add little. TODO: when needed lets add client credentials exchange.

--- web/packages/auth-gateway/index.js
+++ web/packages/auth-gateway/index.js
@@ -1,4 +1,53 @@
+require('dotenv').config();
+
 const polka = require('polka');
+const jwksRsa = require('jwks-rsa');
+const jwt = require('jsonwebtoken');
+
+const { AUTH0_DOMAIN, AUTH0_AUDIENCE } = process.env;
+
+// const issuer = `https://${AUTH0_DOMAIN}/`;
+// const a
+// Authentication middleware. When used, the
+// Access Token must exist and be verified against
+// the Auth0 JSON Web Key Set
+const jwksFn = jwksRsa({
+  cache: true,
+  rateLimit: true,
+  jwksRequestsPerMinute: 5,
+  jwksUri: `https://${AUTH0_DOMAIN}/.well-known/jwks.json`
+});
+
+// TODO: Validate audience, issuer
+
+// Ref: https://github.com/auth0/node-jwks-rsa/blob/master/src/integrations/express.js
+function getSecretKey(header, cb) {
+  jwksFn.getSigningKey(header.kid, (err, key) => {
+    if (err) {
+      cb(err, null);
+      return;
+    }
+
+    cb(null, key.publicKey || key.rsaPublicKey);
+  });
+}
+
+const verifyToken = token => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(
+      token,
+      getSecretKey,
+      { algorithms: ['RS256'] },
+      (err, dToken) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(dToken);
+      }
+    );
+  });
+};
 
 const PORT = 8000;
 // Polka is *mostly* express compatible, but let's not use unnecessary libraries
@@ -31,14 +80,50 @@ const getAuthToken = req => {
   return null;
 };
 
-polka()
-  .get('/notebook', (req, res) => {
-    const token = getAuthToken(req);
+async function attachTokenMiddleware(req, res, next) {
+  const token = getAuthToken(req);
 
-    if (token === null) {
-      res.writeHead(401);
-      res.end();
-    }
+  if (!token) {
+    next();
+    return;
+  }
+
+  try {
+    req.user = await verifyToken(token);
+    next();
+  } catch (_) {
+    //TODO: Decide whether to return info to user
+    const err = new Error('Unauthorized');
+    err.code = 401;
+    next(err);
+  }
+}
+
+// x-requested-with for csrf, and for xmlhttp requests
+// https://stackoverflow.com/questions/17478731/whats-the-point-of-the-x-requested-with-header
+const corsMiddleware = (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Authorization, Content-Type, Content-Length, X-Requested-With'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+
+  next();
+};
+
+polka()
+  .use(corsMiddleware)
+  .get('/notebook', attachTokenMiddleware, (req, res) => {
+    // Kotlar: If we're here the user is authenticated
+    console.info('token', req.user);
+    res.end();
   })
   .listen(PORT, err => {
     if (err) {
