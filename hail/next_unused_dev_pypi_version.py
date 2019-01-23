156a936fc2597732eef223e6d1eb20a7d15d3702
Daniel King <daniel.zidan.king@gmail.com>
2019-01-23 11:54:57 -0500

    creds and fix next_unused

--- hail/next_unused_dev_pypi_version.py
+++ hail/next_unused_dev_pypi_version.py
@@ -1,11 +1,9 @@
 import re
 import sys
 import json
-import urllib.request as urllib
 import distutils.version as duv
 
-url = "https://pypi.python.org/pypi/%s/json" % (sys.argv[1],)
-data = json.load(urllib.urlopen(urllib.Request(url)))
+data = json.load(sys.stdin)
 versions = list(data["releases"])
 
 latest = max(versions, key=duv.LooseVersion)
