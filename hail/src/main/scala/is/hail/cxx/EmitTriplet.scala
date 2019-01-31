312ff61cce84e528903f11ae923df7de4a8946e6
wang <wang@broadinstitute.org>
2019-01-31 16:50:08 -0500

    some notes

--- hail/src/main/scala/is/hail/cxx/EmitTriplet.scala
+++ hail/src/main/scala/is/hail/cxx/EmitTriplet.scala
@@ -25,6 +25,27 @@ class EmitTriplet private(val pType: PType, val setup: Code, val m: Code, val v:
   }
 }
 
+/*
+ * A new EmitRegion should get created at the start of every stream (e.g.
+ * ArrayRange, for the body of ArrayFlatMap). The rest of the stream is emitted,
+ * and if the generated code used to process every element of the stream uses
+ * the provided region in the EmitRegion, then the code generation for the node
+ * that creates the EmitRegion is responsible for initializing it and acquiring
+ * a new region for every element, as well as for adding references from the base
+ * region (outside the stream) to the necessary regions within the stream.
+ *
+ * This would be better if:
+ *  - The (stream) region wasn't added to the base region if unneeded,
+ *    regardless of whether the computation used the region (e.g. computation
+ *    produces an integer, but creates intermediary structs/arrays)
+ *  - Instead of needing to instantiate a stream region for and then reference
+ *    it in the base region, we should just have those things compute directly
+ *    into the base region.
+ *  - In addition to that, we can create scratch regions e.g. for filter
+ *    conditions, so that values that don't appear in the final result can be
+ *    released as long as they are no longer useful.
+ */
+
 class EmitRegion(val region: Variable, init: Code) {
   assert(region.typ == "RegionPtr")
 
