3a706d91652adbc1b8dbd58b1c32e8ec54457086
Daniel King <daniel.zidan.king@gmail.com>
2019-01-31 15:52:35 -0500

    Update test-in-cluster.sh

--- ci/test-in-cluster.sh
+++ ci/test-in-cluster.sh
@@ -33,7 +33,7 @@ export BATCH_SERVER_URL=http://127.0.0.1:5001
 pushd ../batch
 . ../loadconda
 conda activate hail-batch
-POD_NAMESPACE='test' python -c 'import batch.server; batch.server.serve(5001)' & batch_pid=$!
+python -c 'import batch.server; batch.server.serve(5001)' & batch_pid=$!
 conda deactivate
 popd
 
