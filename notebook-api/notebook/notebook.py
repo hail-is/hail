d25156a8339276c230f2c2cfcd79be611bcf2218
Alex Kotlar <akotlar@bu.edu>
2019-02-02 10:56:04 -0500

    no more tail recursion in path finding during json marshalling; Python doesn't optimize for tail calls, so new stack each call... optimize away unnecessary call to read service status after post, since svc already created; TODO: probably inspect the svc object instead of just setting to Running during post, and if wrong this state should be quickly corrected from ws communication...need more testing, understanding of failure modes
    also remove secrets bits; we no longer have any need, since no client-side sessions used in rest setting

--- notebook-api/notebook/notebook.py
+++ notebook-api/notebook/notebook.py
@@ -6,10 +6,9 @@ import gevent
 from gevent import monkey
 monkey.patch_all()
 
-import base64
 import requests
 import ujson
-from flask import Flask, session, redirect, render_template, request
+from flask import Flask, request
 from flask_sockets import Sockets
 import flask
 import kubernetes as kube
@@ -57,18 +56,11 @@ def read_string(f):
         return f.read().strip()
 
 
-# TODO: hash instead?
-def UNSAFE_user_id_transform(user_id): return user_id.replace('|', '--_--')
-
-
 AUTH_GATEWAY = os.environ.get("AUTH_GATEWAY", "http://auth-gateway")
 HAIL_IMAGE = os.environ.get("HAIL_IMAGE", "hail-jupyter")
 
 KUBERNETES_TIMEOUT_IN_SECONDS = float(
     os.environ.get('KUBERNETES_TIMEOUT_IN_SECONDS', 5.0))
-app.secret_key = read_string('/notebook-secrets/secret-key')
-PASSWORD = read_string('/notebook-secrets/password')
-ADMIN_PASSWORD = read_string('/notebook-secrets/admin-password')
 INSTANCE_ID = uuid.uuid4().hex
 
 # used for /verify; will likely go away once 2nd (notebook) verify step moved to nginx
@@ -86,6 +78,14 @@ except FileNotFoundError as e:
     raise ValueError(
         "working directory must contain a file called `notebook-worker-images' "
         "containing the name of the docker image to use for worker pods.") from e
+
+
+#################### Kube resource maangement #########################
+
+
+# A basic transformation to make user_ids safe for Kube
+# TODO: use hash
+def UNSAFE_user_id_transform(user_id): return user_id.replace('|', '--_--')
 
 
 def start_pod(jupyter_token, image, labels={}):
@@ -145,38 +145,43 @@ def start_pod(jupyter_token, image, labels={}):
         _request_timeout=KUBERNETES_TIMEOUT_IN_SECONDS,
     )
 
-    # Avoid extra queries to get pod_name (or reciprocol, svc_name)
-    # svc.metadata.labels['pod_name'] = pod.metadata.name
-    # k8s.patch_namespaced_service(svc.metadata.name, 'default', {
-    #     'metadata': {
-    #         'labels': {
-    #             'pod_name': pod.metadata.name
-    #         }
-    #     }
-    # })
     return svc, pod
 
 
-def forbidden():
-    return 'Forbidden', 403
+def del_pod(pod_name):
+    k8s.delete_namespaced_pod(
+        pod_name,
+        'default',
+        kube.client.V1DeleteOptions(),
+        _request_timeout=KUBERNETES_TIMEOUT_IN_SECONDS)
 
-# Have consumer pass a graph to awalk
 
-# TODO: could support array values as well, i.e path.to.0
+def del_svc(svc_name):
+    k8s.delete_namespaced_service(
+        svc_name,
+        'default',
+        kube.client.V1DeleteOptions(),
+        _request_timeout=KUBERNETES_TIMEOUT_IN_SECONDS)
+
+
+###################### General resource marshalling functions ###################
 
 
 def get_path(data, path: str):
-    idx = path.find('.')
-    if idx == -1:
-        if isinstance(data, dict):
-            return data[path]
-        return getattr(data, path)
+    # Not using tail recursion because python doesn't optimize such calls
+    while True:
+        idx = path.find('.')
 
-    return get_path(getattr(data, path[0:idx]), path[idx + 1:])
+        if idx == -1:
+            if isinstance(data, dict):
+                return data[path]
+            return getattr(data, path)
 
-# Could make this somewhat more elegenat by checking whether resources is list or object
-# if object, return json object, else json array; this may come at 1 additional functional call per resource
-# or an uglier inline block
+        data = getattr(data, path[0:idx])
+
+        path = path[idx + 1:]
+
+
 # Given a kubernetes object, and some collection of field paths, walk path tree and fetch values
 # @param resources<List> : kubernetes v1 object
 # @param paths<List<List[3]>> : fields and transformations: [key, path_in_kube_object, lambda_for_found_val]
@@ -206,28 +211,43 @@ def marshall_json(resources: [], paths=[], flatten=False):
     return ujson.dumps(resp)
 
 
-# Services and pods are seperate resources, and operations between them are not atomic
-# Must support deletion of one indep of presence other
+#################### Pod resource marshalling path functions ###################
 
 
-def del_pod(pod_name):
-    k8s.delete_namespaced_pod(
-        pod_name,
-        'default',
-        kube.client.V1DeleteOptions(),
-        _request_timeout=KUBERNETES_TIMEOUT_IN_SECONDS)
+def read_svc_status(svc_name):
+    try:
+        # TODO: inspect exception for non-404
+        svc = k8s.read_namespaced_service(svc_name, 'default')
+        return 'Running'
+    except:
+        return 'Deleted'
 
 
-def del_svc(svc_name):
-    k8s.delete_namespaced_service(
-        svc_name,
-        'default',
-        kube.client.V1DeleteOptions(),
-        _request_timeout=KUBERNETES_TIMEOUT_IN_SECONDS)
+common_pod_paths = [
+    ['name', 'metadata.labels.name'],
+    ['pod_name', 'metadata.name'],
+    ['svc_name', 'metadata.labels.svc_name'],
+    ['pod_status', 'status.phase'],
+    ['creation_date', 'metadata.creation_timestamp',
+        lambda x: x.strftime('%D')],
+    ['token', 'metadata.labels.jupyter_token']
+]
+
+pod_paths = [
+    *common_pod_paths,
+    ['svc_status', 'metadata.labels.svc_name', lambda x: read_svc_status(x)],
+]
+
+# TODO: This may not work in all cases; we may need to pass the svc object
+# and check it; it doesn't seem to have useful status information
+pod_paths_post = [
+    *common_pod_paths,
+    ['svc_status', 'metadata.labels.svc_name', lambda _: 'Running'],
+]
 
 
-##### New notebook methods meant for consumption from agnostic client#####
-###### Methods that do not call the auth-gateway /verify method must not be directly public #######
+########################## WS and HTTP Routes ##################################
+
 
 # TODO: learn how to properly handle webscocket close in gevent + wsgi
 # or just move to aiohttp and stop dealing with greenlets and websockets with
@@ -239,6 +259,10 @@ def del_svc(svc_name):
 # no need to go to public web to hit http endpoint
 # and real insight into pod status
 # Weakness is currently not checking whether svc is accessible; easily can add
+
+
+def forbidden():
+    return 'Forbidden', 40
 
 
 @sockets.route('/api/ws')
@@ -306,36 +330,6 @@ def verify(svc_name):
     return '', 200
 
 
-# read_svc_status checks that the pod's backing service exists
-# It returns a string status, which must match the corresponding Pod status
-# Ex: If Kubernetes names running pod status (phase) "Running", and svc is running
-# we should return "Running" here
-# TODO: Inspect exceptions to distinguish between 404 and others
-
-def read_svc_status(svc_name):
-    try:
-        svc = k8s.read_namespaced_service(svc_name, 'default')
-        return 'Running'
-    except:
-        return 'Deleted'
-
-
-# TODO: support lambda expressions
-# TODO: had default key as path name, but this adds branches/if statements
-# in tight loop, dont think the space savings worth it
-
-pod_paths = [
-    ['name', 'metadata.labels.name'],
-    ['pod_name', 'metadata.name'],
-    ['svc_name', 'metadata.labels.svc_name'],
-    ['pod_status', 'status.phase'],
-    ['svc_status', 'metadata.labels.svc_name', lambda x: read_svc_status(x)],
-    ['creation_date', 'metadata.creation_timestamp',
-        lambda x: x.strftime('%D')],
-    ['token', 'metadata.labels.jupyter_token']
-]
-
-
 @app.route('/api', methods=['GET'])
 def get_notebooks():
     user_id = request.headers.get('User')
@@ -343,12 +337,6 @@ def get_notebooks():
     if not user_id:
         return forbidden()
 
-    # svcs = k8s.list_namespaced_service(
-    #     namespace='default',
-    #     watch=False,
-    #     label_selector='user_id=' + UNSAFE_user_id_transform(user_id),
-    #     _request_timeout=KUBERNETES_TIMEOUT_IN_SECONDS).items
-    # k8s.read_namespaced_service(svc_name, 'default')
     pods = k8s.list_namespaced_pod(
         namespace='default', watch=False,
         label_selector=f"user_id={UNSAFE_user_id_transform(user_id)}").items
@@ -422,7 +410,8 @@ def new_notebook():
                 'user_id': UNSAFE_user_id_transform(user_id)}
     )
 
-    return marshall_json([pod], pod_paths, True), 200
+    # We could also construct pod_paths_post here, and close over svc
+    return marshall_json([pod], pod_paths_post, True), 200
 
 
 if __name__ == '__main__':
