6995178ef500fb221e421f503126fd54538d46e8
Cotton Seed <cotton@alum.mit.edu>
2019-02-04 23:46:10 -0500

    tweaks

--- hail/python/hail/plot/__init__.py
+++ hail/python/hail/plot/__init__.py
@@ -1,4 +1,4 @@
-from .plots import histogram, cumulative_histogram, scatter, qq, manhattan
+from .plots import output_notebook, show, histogram, cumulative_histogram, scatter, qq, manhattan
 
 __all__ = ['output_notebook',
            'show',
