45350f878569c4a23c87c7ada23ea923747c030b
Alex Kotlar <akotlar@bu.edu>
2019-02-06 16:30:52 -0500

    since scorecard won't be merged, revert

--- web/packages/web-client/pages/scorecard/user.tsx
+++ web/packages/web-client/pages/scorecard/user.tsx
@@ -12,7 +12,7 @@ const WEB_URL: string = config.WEB_URL;
 import '../../styles/pages/scorecard/user.scss';
 
 declare type response = {
-  user_data: {
+  data: {
     CHANGES_REQUESTED: PR[];
     NEEDS_REVIEW: PR[];
     FAILING: any[];
@@ -101,7 +101,7 @@ class User extends PureComponent<pageProps> {
 
     return {
       pageProps: {
-        user_data: res.user_data,
+        data: res.data,
         updated: res.updated,
         name: query.name
       }
@@ -114,7 +114,7 @@ class User extends PureComponent<pageProps> {
 
   render() {
     const {
-      user_data: { CHANGES_REQUESTED, NEEDS_REVIEW, FAILING, ISSUES },
+      data: { CHANGES_REQUESTED, NEEDS_REVIEW, FAILING, ISSUES },
       updated,
       name
     } = this.props.pageProps;
