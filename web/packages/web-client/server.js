0102d7f8f104e270f446dda8c91e0d061ad3b6e1
Alex Kotlar <akotlar@bu.edu>
2019-02-07 17:35:20 -0500

    disable cache, not granular enough

--- web/packages/web-client/server.js
+++ web/packages/web-client/server.js
@@ -10,41 +10,41 @@ const handle = app.getRequestHandler();
 
 const favicon = fs.readFileSync('./static/favicon.ico');
 
-const cache = {};
+// const cache = {};
 
-const send = (res, html) => {
-  res.writeHeader(200, 'text/html');
-  res.write(html);
-  res.end();
-};
+// const send = (res, html) => {
+//   res.writeHeader(200, 'text/html');
+//   res.write(html);
+//   res.end();
+// };
 
-const pagesToCache = {
-  '/': isDev ? 0 : -1, // forever
-  '/scorecard': isDev ? 0 : 3 * 60 * 1000, // 3 min
-  '/scorecard/user': isDev ? 0 : 3 * 60 * 1000
-};
+// const pagesToCache = {
+//   //Breaks dark mode: '/': isDev ? 0 : -1, // forever
+//   // '/scorecard': isDev ? 0 : 3 * 60 * 1000, // 3 min
+//   // '/scorecard/user': isDev ? 0 : 3 * 60 * 1000
+// };
 
-const render = (cacheTime = 5 * 60 * 1000, req, res, pagePath, query) => {
-  if (
-    !!cache[req.url] &&
-    (cache[req.url].expires === -1 || cache[req.url].expires >= Date.now())
-  ) {
-    send(res, cache[req.url].html);
-    return;
-  }
+// const render = (cacheTime = 5 * 60 * 1000, req, res, pagePath, query) => {
+//   if (
+//     !!cache[req.url] &&
+//     (cache[req.url].expires === -1 || cache[req.url].expires >= Date.now())
+//   ) {
+//     send(res, cache[req.url].html);
+//     return;
+//   }
 
-  app
-    .renderToHTML(req, res, pagePath, query)
-    .then(html => {
-      cache[req.url] = {
-        html,
-        expires: cacheTime === -1 ? cacheTime : cacheTime + Date.now()
-      };
+//   app
+//     .renderToHTML(req, res, pagePath, query)
+//     .then(html => {
+//       cache[req.url] = {
+//         html,
+//         expires: cacheTime === -1 ? cacheTime : cacheTime + Date.now()
+//       };
 
-      send(res, html);
-    })
-    .catch(err => console.error(err));
-};
+//       send(res, html);
+//     })
+//     .catch(err => console.error(err));
+// };
 
 app.prepare().then(() => {
   createServer((req, res) => {
@@ -55,15 +55,17 @@ app.prepare().then(() => {
       res.writeHeader(200, 'image/png');
       res.write(favicon);
       res.end();
-    } else if (pagesToCache[parsedUrl.pathname]) {
-      render(
-        pagesToCache[parsedUrl.pathname],
-        req,
-        res,
-        parsedUrl.pathname,
-        parsedUrl.query
-      );
-    } else {
+    }
+    // else if (pagesToCache[parsedUrl.pathname]) {
+    //   render(
+    //     pagesToCache[parsedUrl.pathname],
+    //     req,
+    //     res,
+    //     parsedUrl.pathname,
+    //     parsedUrl.query
+    //   );
+    // }
+    else {
       handle(req, res, parsedUrl);
     }
   }).listen(process.env.PORT || 3000, err => {
