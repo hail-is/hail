5d158f08d9ab1fabc84a56d19192078622cf61c2
konradjk <konradjkarczewski@gmail.com>
2019-02-12 20:42:30 -0500

    addressed comments

--- hail/python/hail/experimental/expressions.py
+++ hail/python/hail/experimental/expressions.py
@@ -7,53 +7,53 @@ from hail.typecheck import *
 def write_expression(expr, path, overwrite=False):
     """Write an Expression.
 
-       In the same vein as Python's pickle, write out an expression
-       that does not have a source (such as one that comes from
-       Table.aggregate with _localize=False).
+   In the same vein as Python's pickle, write out an expression
+   that does not have a source (such as one that comes from
+   Table.aggregate with _localize=False).
 
-       Example
-       -------
-       >>> ht = hl.utils.range_table(100).annotate(x=hl.rand_norm())
-       >>> mean_norm = ht.aggregate(hl.agg.mean(ht.x), _localize=False)
-       >>> mean_norm
-       >>> hl.eval(mean_norm)
-       >>> hl.experimental.write_expression(mean_norm, 'output/expression.he')
+   Example
+   -------
+   >>> ht = hl.utils.range_table(100).annotate(x=hl.rand_norm())
+   >>> mean_norm = ht.aggregate(hl.agg.mean(ht.x), _localize=False)
+   >>> mean_norm
+   >>> hl.eval(mean_norm)
+   >>> hl.experimental.write_expression(mean_norm, 'output/expression.he')
 
-       Parameters
-       ----------
+   Parameters
+   ----------
 
-       expr : :class:`Expression`
-           Expression to write.
-       path : :obj:`str`
-           Path to which to write expression.
-           Suggested extension: .he (hail expression).
-       overwrite : :obj:`bool`
-           If ``True``, overwrite an existing file at the destination.
+   expr : :class:`Expression`
+       Expression to write.
+   path : :obj:`str`
+       Path to which to write expression.
+       Suggested extension: .he (hail expression).
+   overwrite : :obj:`bool`
+       If ``True``, overwrite an existing file at the destination.
 
-       Returns
-       -------
-       None
+   Returns
+   -------
+   None
     """
-    hl.utils.range_table(1).annotate_globals(expr=expr).write(path, overwrite=overwrite)
+    hl.utils.range_table(1).filter(False).annotate_globals(expr=expr).write(path, overwrite=overwrite)
 
 
 @typecheck(path=str)
 def read_expression(path):
     """Read an :class:`Expression` written with :meth:`.experimental.write_expression`.
 
-       Example
-       -------
-       >>> expression = hl.experimental.read_expression('output/expression.he')
-       >>> hl.eval(expression)
+   Example
+   -------
+   >>> expression = hl.experimental.read_expression('output/expression.he')
+   >>> hl.eval(expression)
 
-       Parameters
-       ----------
+   Parameters
+   ----------
 
-       path : :obj:`str`
-           File to read.
+   path : :obj:`str`
+       File to read.
 
-       Returns
-       -------
-       :class:`Expression`
+   Returns
+   -------
+   :class:`Expression`
     """
     return hl.read_table(path).index_globals().expr
