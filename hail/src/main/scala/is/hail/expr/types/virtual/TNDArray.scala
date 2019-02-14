e1d93fca82eaaab2d19bedb5a10a4618f630ffb0
jbloom22 <jbloom@broadinstitute.org>
2019-02-14 14:23:56 -0500

    lift to Type and PType

--- hail/src/main/scala/is/hail/expr/types/virtual/TNDArray.scala
+++ hail/src/main/scala/is/hail/expr/types/virtual/TNDArray.scala
@@ -6,15 +6,7 @@ import org.apache.spark.sql.Row
 
 import scala.reflect.{ClassTag, classTag}
 
-final case class TNDArray(elementType: Type, override val required: Boolean = false) extends ComplexType {
-  val representation: Type = TStruct(
-    "flags" -> TInt64Required,           // encodes data layout
-    "shape" -> TArray(TInt64Required),   // length is ndim
-    "offset" ->  TInt64Required,         // offset into data
-    "strides" -> TArray(TInt64Required), // stride in each dim, length is 1
-    "data" -> TArray(elementType)
-  )
-
+final case class TNDArray(elementType: Type, override val required: Boolean = false) extends Type {
   lazy val physicalType: PNDArray = PNDArray(elementType.physicalType, required)
 
   override def pyString(sb: StringBuilder): Unit = {
@@ -27,16 +19,7 @@ final case class TNDArray(elementType: Type, override val required: Boolean = fa
 
   override def scalaClassTag: ClassTag[Row] = classTag[Row]
 
-  def _typeCheck(a: Any): Boolean = {
-    a.isInstanceOf[Row] && {
-      val r = a.asInstanceOf[Row]
-      val s = representation.asInstanceOf[TStruct]
-      r.length == s.size && s.types.zipWithIndex.forall {
-        case (t, i) => t._typeCheck(r.get(i))
-      }
-    }
-  }
+  def _typeCheck(a: Any): Boolean = throw new UnsupportedOperationException
 
-  val ordering: ExtendedOrdering = TBaseStruct.getOrdering(
-    representation.asInstanceOf[TStruct].types)
+  val ordering: ExtendedOrdering = null
 }
