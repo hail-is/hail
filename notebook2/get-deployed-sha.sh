434080f34944e0b14d388e35cdb5d75cb6a15d83
Alex Kotlar <akotlar@bu.edu>
2019-02-20 21:44:39 -0500

    get deploy using pwd

--- notebook2/get-deployed-sha.sh
+++ notebook2/get-deployed-sha.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-kubectl -n default get --selector=app=notebook deployments -o "jsonpath={.items[*].metadata.labels.hail\.is/sha}"
+kubectl -n default get --selector=app=${PWD##*/} deployments -o "jsonpath={.items[*].metadata.labels.hail\.is/sha}"
