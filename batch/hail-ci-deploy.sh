f1e9cfbe361967b84375989095301c7bab6f8b36
Daniel King <daniel.zidan.king@gmail.com>
2019-02-28 14:55:37 -0500

    use makefileg

--- batch/hail-ci-deploy.sh
+++ batch/hail-ci-deploy.sh
@@ -9,16 +9,4 @@ gcloud -q auth activate-service-account \
 
 gcloud -q auth configure-docker
 
-kubectl delete persistvolumeclaim --all --namespace batch-pods
-kubectl delete namespace test
-# when we have k8s 1.12 https://github.com/kubernetes/kubernetes/pull/64034:
-# kubectl wait --for=delete namespace/test
-: $((delay = 1))
-while kubectl get namespace test
-do
-    : $((delay = delay + delay))
-    sleep ${delay}
-done
-kubectl create -f test-namespace.yaml
-
 make deploy
