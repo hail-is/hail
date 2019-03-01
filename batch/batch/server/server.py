89d445e4f33f71a4902b5b0ee59f73c9537d2231
Daniel King <daniel.zidan.king@gmail.com>
2019-03-01 10:31:34 -0500

    works

--- batch/batch/server/server.py
+++ batch/batch/server/server.py
@@ -58,13 +58,16 @@ def make_logger():
 log = make_logger()
 
 KUBERNETES_TIMEOUT_IN_SECONDS = float(os.environ.get('KUBERNETES_TIMEOUT_IN_SECONDS', 5.0))
-
 REFRESH_INTERVAL_IN_SECONDS = int(os.environ.get('REFRESH_INTERVAL_IN_SECONDS', 5 * 60))
-
 POD_NAMESPACE = os.environ.get('POD_NAMESPACE', 'batch-pods')
+POD_VOLUME_SIZE = os.environ.get('POD_VOLUME_SIZE', '10Mi')
 
 log.info(f'KUBERNETES_TIMEOUT_IN_SECONDS {KUBERNETES_TIMEOUT_IN_SECONDS}')
 log.info(f'REFRESH_INTERVAL_IN_SECONDS {REFRESH_INTERVAL_IN_SECONDS}')
+log.info(f'POD_NAMESPACE {POD_NAMESPACE}')
+log.info(f'POD_VOLUME_SIZE {POD_VOLUME_SIZE}')
+
+STORAGE_CLASS_NAME = 'batch'
 
 if 'BATCH_USE_KUBE_CONFIG' in os.environ:
     kube.config.load_kube_config()
@@ -110,13 +113,51 @@ class Job:
         self._task_idx += 1
         if self._task_idx < len(self._tasks):
             self._current_task = self._tasks[self._task_idx]
+        else:
+            self._delete_pvc()
 
     def _has_next_task(self):
         return self._task_idx < len(self._tasks)
 
+    def pvc(self):
+        if self._pvc is None:
+            self._pvc = v1.create_namespaced_persistent_volume_claim(
+                POD_NAMESPACE,
+                kube.client.V1PersistentVolumeClaim(
+                    metadata=kube.client.V1ObjectMeta(
+                        generate_name=f'job-{self.id}-',
+                        labels={'app': 'batch-job',
+                                'hail.is/batch-instance': instance_id}),
+                    spec=kube.client.V1PersistentVolumeClaimSpec(
+                        access_modes=['ReadWriteOnce'],
+                        volume_mode='Filesystem',
+                        resources=kube.client.V1ResourceRequirements(
+                            requests={'storage': POD_VOLUME_SIZE}),
+                        storage_class_name=STORAGE_CLASS_NAME)),
+                _request_timeout=KUBERNETES_TIMEOUT_IN_SECONDS)
+            log.info(f'created pvc name: {self._pvc.metadata.name} for job {self.id}')
+        return self._pvc
+
     def _create_pod(self):
         assert not self._pod_name
         assert self._current_task is not None
+
+        if len(self._tasks) > 1:
+            current_pod_spec = self._current_task.pod_template.spec
+            if current_pod_spec.volumes is None:
+                current_pod_spec.volumes = []
+            current_pod_spec.volumes.append(
+                kube.client.V1Volume(
+                    persistent_volume_claim=kube.client.V1PersistentVolumeClaimVolumeSource(
+                        claim_name=self.pvc().metadata.name),
+                    name=self.pvc().metadata.name))
+            for container in current_pod_spec.containers:
+                if container.volume_mounts is None:
+                    container.volume_mounts = []
+                container.volume_mounts.append(
+                    kube.client.V1VolumeMount(
+                        mount_path='/volume',
+                        name=self.pvc().metadata.name))
 
         pod = v1.create_namespaced_pod(
             POD_NAMESPACE,
@@ -132,8 +173,25 @@ class Job:
                                                                    self.id,
                                                                    self._current_task.name))
 
-    def _delete_pod(self):
-        if self._pod_name:
+    def _delete_pvc(self):
+        if self._pvc is not None:
+            try:
+                v1.delete_namespaced_persistent_volume_claim(
+                    self._pvc.metadata.name,
+                    POD_NAMESPACE,
+                    kube.client.V1DeleteOptions(),
+                    _request_timeout=KUBERNETES_TIMEOUT_IN_SECONDS)
+            except kube.client.rest.ApiException as err:
+                if err.status == 404:
+                    log.info(f'persistent volume claim {self._pvc.metadata.name} in '
+                             f'{self._pvc.metadata.namespace} is already deleted')
+                else:
+                    log.warning(f'persistent volume claim {self._pvc.metadata.name} in '
+                                f'{self._pvc.metadata.namespace} could not be deleted')
+
+    def _delete_k8s_resources(self):
+        self._delete_pvc()
+        if self._pod_name is not None:
             try:
                 v1.delete_namespaced_pod(
                     self._pod_name,
@@ -178,6 +236,7 @@ class Job:
         self.incomplete_parent_ids = set(self.parent_ids)
         self.scratch_folder = scratch_folder
 
+        self._pvc = None
         self._pod_name = None
         self.exit_code = None
         self._state = 'Created'
@@ -251,7 +310,7 @@ class Job:
     def cancel(self):
         if self.is_complete():
             return
-        self._delete_pod()
+        self._delete_k8s_resources()
         self.set_state('Cancelled')
 
     def delete(self):
@@ -261,7 +320,7 @@ class Job:
             batch = batch_id_batch[self.batch_id]
             batch.remove(self)
 
-        self._delete_pod()
+        self._delete_k8s_resources()
         self.set_state('Cancelled')
 
     def is_complete(self):
