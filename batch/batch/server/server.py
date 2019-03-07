89f97f1ad9733e22acb39e97da50a16c4987f230
Jackie Goldstein <jigold@broadinstitute.org>
2019-03-07 15:36:51 -0500

    [batch] Change server to use aiohttp

--- batch/batch/server/server.py
+++ batch/batch/server/server.py
@@ -7,12 +7,15 @@ import uuid
 from collections import Counter
 import logging
 import threading
-from flask import Flask, request, jsonify, abort, render_template
 import kubernetes as kube
 import cerberus
 import requests
+import asyncio
+from aiohttp import web
+import aiohttp_jinja2
+import jinja2
 
-from .globals import max_id, pod_name_job, job_id_job, _log_path, _read_file, batch_id_batch
+from .globals import max_id, _log_path, _read_file, pod_name_job, job_id_job, batch_id_batch
 from .globals import next_id, get_recent_events, add_event
 
 from .. import schemas
@@ -74,6 +77,24 @@ v1 = kube.client.CoreV1Api()
 
 instance_id = uuid.uuid4().hex
 log.info(f'instance_id = {instance_id}')
+
+app = web.Application()
+routes = web.RouteTableDef()
+aiohttp_jinja2.setup(app,
+                     loader=jinja2.PackageLoader('batch', 'templates'))
+
+
+def abort(code, reason=None):
+    if code == 400:
+        raise web.HTTPBadRequest(reason=reason)
+    elif code == 404:
+        raise web.HTTPNotFound(reason=reason)
+    else:
+        raise NotImplementedError
+
+
+def jsonify(data):
+    return web.json_response(data)
 
 
 class JobTask:  # pylint: disable=R0903
@@ -338,14 +359,9 @@ class Job:
         return result
 
 
-app = Flask('batch')
-
-log.info(f'app.root_path = {app.root_path}')
-
-
-@app.route('/jobs/create', methods=['POST'])
-def create_job():  # pylint: disable=R0912
-    parameters = request.json
+@routes.post('/jobs/create')
+async def create_job(request):  # pylint: disable=R0912
+    parameters = await request.json()
 
     schema = {
         # will be validated when creating pod
@@ -406,24 +422,27 @@ def create_job():  # pylint: disable=R0912
         scratch_folder,
         input_files,
         output_files)
+
     return jsonify(job.to_json())
 
 
-@app.route('/jobs', methods=['GET'])
-def get_job_list():
+@routes.get('/jobs')
+async def get_job_list(request):
     return jsonify([job.to_json() for _, job in job_id_job.items()])
 
 
-@app.route('/jobs/<int:job_id>', methods=['GET'])
-def get_job(job_id):
+@routes.get('/jobs/{job_id}')
+async def get_job(request):
+    job_id = int(request.match_info['job_id'])
     job = job_id_job.get(job_id)
     if not job:
         abort(404)
     return jsonify(job.to_json())
 
 
-@app.route('/jobs/<int:job_id>/log', methods=['GET'])
-def get_job_log(job_id):  # pylint: disable=R1710
+@routes.get('/jobs/{job_id}/log')
+async def get_job_log(request):  # pylint: disable=R1710
+    job_id = int(request.match_info['job_id'])
     if job_id > max_id():
         abort(404)
 
@@ -443,8 +462,9 @@ def get_job_log(job_id):  # pylint: disable=R1710
     abort(404)
 
 
-@app.route('/jobs/<int:job_id>/delete', methods=['DELETE'])
-def delete_job(job_id):
+@routes.delete('/jobs/{job_id}/delete')
+async def delete_job(request):
+    job_id = int(request.match_info['job_id'])
     job = job_id_job.get(job_id)
     if not job:
         abort(404)
@@ -452,8 +472,9 @@ def delete_job(job_id):
     return jsonify({})
 
 
-@app.route('/jobs/<int:job_id>/cancel', methods=['POST'])
-def cancel_job(job_id):
+@routes.post('/jobs/{job_id}/cancel')
+async def cancel_job(request):
+    job_id = int(request.match_info['job_id'])
     job = job_id_job.get(job_id)
     if not job:
         abort(404)
@@ -523,9 +544,9 @@ class Batch:
         }
 
 
-@app.route('/batches/create', methods=['POST'])
-def create_batch():
-    parameters = request.json
+@routes.post('/batches/create')
+async def create_batch(request):
+    parameters = await request.json()
 
     schema = {
         'attributes': {
@@ -544,16 +565,18 @@ def create_batch():
     return jsonify(batch.to_json())
 
 
-@app.route('/batches/<int:batch_id>', methods=['GET'])
-def get_batch(batch_id):
+@routes.get('/batches/{batch_id}')
+async def get_batch(request):
+    batch_id = int(request.match_info['batch_id'])
     batch = batch_id_batch.get(batch_id)
     if not batch:
         abort(404)
     return jsonify(batch.to_json())
 
 
-@app.route('/batches/<int:batch_id>/delete', methods=['DELETE'])
-def delete_batch(batch_id):
+@routes.delete('/batches/{batch_id}/delete')
+async def delete_batch(request):
+    batch_id = int(request.match_info['batch_id'])
     batch = batch_id_batch.get(batch_id)
     if not batch:
         abort(404)
@@ -561,8 +584,9 @@ def delete_batch(batch_id):
     return jsonify({})
 
 
-@app.route('/batches/<int:batch_id>/close', methods=['POST'])
-def close_batch(batch_id):
+@routes.post('/batches/{batch_id}/close')
+async def close_batch(request):
+    batch_id = int(request.match_info['batch_id'])
     batch = batch_id_batch.get(batch_id)
     if not batch:
         abort(404)
@@ -583,13 +607,14 @@ def update_job_with_pod(job, pod):
         job.mark_unscheduled()
 
 
-@app.route('/pod_changed', methods=['POST'])
-def pod_changed():
-    parameters = request.json
+@routes.post('/pod_changed')
+async def pod_changed(request):
+    parameters = await request.json()
 
     pod_name = parameters['pod_name']
 
     job = pod_name_job.get(pod_name)
+
     if job and not job.is_complete():
         try:
             pod = v1.read_namespaced_pod(
@@ -604,11 +629,11 @@ def pod_changed():
 
         update_job_with_pod(job, pod)
 
-    return '', 204
+    return web.Response(status=204)
 
 
-@app.route('/refresh_k8s_state', methods=['POST'])
-def refresh_k8s_state():
+@routes.post('/refresh_k8s_state')
+async def refresh_k8s_state(request):
     log.info('started k8s state refresh')
 
     pods = v1.list_namespaced_pod(
@@ -631,13 +656,14 @@ def refresh_k8s_state():
 
     log.info('k8s state refresh complete')
 
-    return '', 204
+    return web.Response(status=204)
 
 
-@app.route('/recent', methods=['GET'])
-def recent():
+@routes.get('/recent')
+@aiohttp_jinja2.template('recent.html')
+async def recent(request):
     recent_events = get_recent_events()
-    return render_template('recent.html', recent=list(reversed(recent_events)))
+    return {'recent': list(reversed(recent_events))}
 
 
 def run_forever(target, *args, **kwargs):
@@ -663,10 +689,12 @@ def run_once(target, *args, **kwargs):
         log.info(f'run_forever: {target.__name__} returned')
     except Exception:  # pylint: disable=W0703
         log.error(f'run_forever: {target.__name__} caught_exception: ', exc_info=sys.exc_info())
+        #  FIXME: Should this raise an exception? Server tests log this, but do not fail
 
 
-def flask_event_loop(port):
-    app.run(threaded=False, host='0.0.0.0', port=port)
+def aiohttp_event_loop(port):
+    app.add_routes(routes)
+    web.run_app(app, host='0.0.0.0', port=port)
 
 
 def kube_event_loop(port):
@@ -719,6 +747,6 @@ def serve(port=5000):
     # see: https://stackoverflow.com/questions/31264826/start-a-flask-application-in-separate-thread
     # flask_thread = threading.Thread(target=flask_event_loop)
     # flask_thread.start()
-    run_forever(flask_event_loop, port)
+    run_forever(aiohttp_event_loop, port)
 
     kube_thread.join()
