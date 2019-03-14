521d0e141e4ee56a6b004272590de45683424ddc
Cotton Seed <cotton@alum.mit.edu>
2019-03-14 12:50:12 -0400

    remove print

--- hail/src/main/scala/is/hail/expr/ir/Compile.scala
+++ hail/src/main/scala/is/hail/expr/ir/Compile.scala
@@ -22,7 +22,6 @@ object Compile {
     body: IR,
     nSpecialArgs: Int
   ): (PType, Int => F) = {
-    println(Pretty(body))
     val normalizeNames = new NormalizeNames
     val normalizedBody = normalizeNames(body,
       Env(args.map { case (n, _, _) => n -> n }: _*))
