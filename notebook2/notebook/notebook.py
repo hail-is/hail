8b9f3dd10bbb84fdeb2b2099425ab1386ad2ab2c
Alex Kotlar <akotlar@bu.edu>
2019-03-15 21:17:26 -0400

    cleanup merge

--- notebook2/notebook/notebook.py
+++ notebook2/notebook/notebook.py
@@ -462,10 +462,9 @@ def auth0_callback():
 
     email = userinfo['email']
 
-    # if AUTHORIZED_USERS.get(email) is None:
-    #     return redirect(external_url_for(f"error?err=Unauthorized"))
+    if AUTHORIZED_USERS.get(email) is None:
+        return redirect(external_url_for(f"error?err=Unauthorized"))
 
-    print("userinfo", userinfo)
     session['user'] = {
         'id': userinfo['sub'],
         'name': userinfo['name'],
