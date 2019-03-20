ac3c7aae07a1a6fef36e08c4dfbb682f3f1c13e5
Daniel King <daniel.zidan.king@gmail.com>
2019-03-20 10:59:51 -0400

    address comments

--- hailjwt/hailjwt/hailjwt.py
+++ hailjwt/hailjwt/hailjwt.py
@@ -4,12 +4,19 @@ import jwt
 class JWTClient:
     __ALGORITHM = 'HS256'
 
+    def _verify_key_preqrequisites(self, secret_key):
+        assert isinstance(secret_key, str)
+        n_bytes = len(secret_key.encode('utf-8'))
+        if n_bytes < 256:
+            raise ValueError(
+                f'found secret key with {n_bytes} bytes, but secret key must '
+                f'have at least 256 bytes')
+
     def __init__(self, secret_key):
+        self._verify_key_preqrequisites(secret_key)
         self.secret_key = secret_key
 
     def decode(self, token):
-        if token is None:
-            return None
         return jwt.decode(
             token, self.secret_key, algorithms=[JWTClient.__ALGORITHM])
 
--- hailjwt/test/test_jwt.py
+++ hailjwt/test/test_jwt.py
@@ -1,11 +1,31 @@
-import uuid
+import jwt
+import re
+import secrets
+
 import hailjwt as hj
 
 
 def test_round_trip():
-    c = hj.JWTClient(uuid.uuid4().hex)
+    c = hj.JWTClient(secrets.randbits(256))
     json = {'hello': 'world'}
     assert c.decode(c.encode(json)) == json
+
+
+def test_fewer_than_256_bits_is_error():
+    try:
+        hj.JWTClient(secrets.randbits(255 * 8))
+        assert False
+    except ValueError as err:
+        assert re.search('found secret key with 255 bytes', err.message)
+
+
+def test_bad_input_is_error():
+    try:
+        c = hj.JWTClient(secrets.randbits(256 * 8))
+        c.decode('garbage')
+        assert False
+    except jwt.exceptions.DecodeError:
+        pass
 
 
 def test_get_domain():
