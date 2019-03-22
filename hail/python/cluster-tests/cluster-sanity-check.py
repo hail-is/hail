ce298c49ece1f2e5fb252f84f5f1de523f40e8bb
Daniel King <daniel.zidan.king@gmail.com>
2019-03-22 15:51:28 -0400

    add bgen cluster read test

--- hail/python/cluster-tests/cluster-sanity-check.py
+++ hail/python/cluster-tests/cluster-sanity-check.py
@@ -3,3 +3,6 @@ import hail as hl
 mt = hl.import_vcf('gs://hail-1kg/1kg_coreexome.vcf.bgz')
 mt = mt.annotate_rows(x = 5)
 mt._force_count_rows()
+
+mt = hl.import_bgen('gs://hail-ci/example.8bits.bgen')
+mt._force_count_rows()
