d0fe82e66195a5afdc2ab152800f22a162c74e9f
Tim Poterba <tpoterba@gmail.com>
2019-03-22 16:56:59 -0400

    fix

--- hail/python/cluster-tests/cluster-sanity-check.py
+++ hail/python/cluster-tests/cluster-sanity-check.py
@@ -4,5 +4,5 @@ mt = hl.import_vcf('gs://hail-1kg/1kg_coreexome.vcf.bgz')
 mt = mt.annotate_rows(x = 5)
 mt._force_count_rows()
 
-mt = hl.import_bgen('gs://hail-ci/example.8bits.bgen')
+mt = hl.import_bgen('gs://hail-ci/example.8bits.bgen', entry_fields=['GT'])
 mt._force_count_rows()
