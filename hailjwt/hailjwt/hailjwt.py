0029c23daf146d599e66624ef9f8a26ea6d29d8b
Daniel King <daniel.zidan.king@gmail.com>
2019-03-25 16:25:33 -0400

    [jwt] create jwt subproject (#5641)
    * hailjwt
    
    * updtes
    
    * add hailjwt project
    
    * add tests
    
    * add hail ci build
    
    * no need for install
    
    * remove unnecessary prefix
    
    * address comments
    
    * address comments
    
    * address comments
    
    * python3
    
    * fix test
    
    * fixes

---
+++ hailjwt/hailjwt/hailjwt.py
@@ -0,0 +1,39 @@
+import jwt
+
+
+class JWTClient:
+    __ALGORITHM = 'HS256'
+
+    @staticmethod
+    def generate_key():
+        import secrets
+        return secrets.token_bytes(64)
+
+    @staticmethod
+    def _verify_key_preqrequisites(secret_key):
+        if isinstance(secret_key, str):
+            key_bytes = secret_key.encode('utf-8')
+        else:
+            assert isinstance(secret_key, bytes), type(secret_key)
+            key_bytes = secret_key
+        if len(key_bytes) < 32:
+            raise ValueError(
+                f'found secret key with {len(key_bytes)} bytes, but secret key '
+                f'must have at least 32 bytes (i.e. 256 bits)')
+
+    def __init__(self, secret_key):
+        JWTClient._verify_key_preqrequisites(secret_key)
+        self.secret_key = secret_key
+
+    def decode(self, token):
+        return jwt.decode(
+            token, self.secret_key, algorithms=[JWTClient.__ALGORITHM])
+
+    def encode(self, payload):
+        return jwt.encode(
+            payload, self.secret_key, algorithm=JWTClient.__ALGORITHM)
+
+
+def get_domain(host):
+    parts = host.split('.')
+    return f"{parts[-2]}.{parts[-1]}"
