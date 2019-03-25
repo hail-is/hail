6db4c5c310b32ea421d5bbc2980301f89b73043e
Jackie Goldstein <jigold@broadinstitute.org>
2019-03-25 13:30:43 -0400

    add missing file

---
+++ cloud-sql.mk
@@ -0,0 +1,20 @@
+.PHONY: install-cloud-sql-proxy local-cloud-sql-config run-cloud-sql-proxy
+
+CLOUD_SQL_PORT ?= 3307
+
+install-cloud-sql-proxy:
+	test -f cloud_sql_proxy || \
+		curl -o cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64 && \
+		chmod +x cloud_sql_proxy
+
+local-cloud-sql-config:
+	mkdir -p batch-secrets
+	kubectl get secret batch-test-cloud-sql-config -n batch-pods -o "jsonpath={.data.batch-test-cloud-sql-config\.json}" | base64 --decode > batch-secrets/batch-test-cloud-sql-config.json
+	jq '.host = "127.0.0.1" | .port = $(CLOUD_SQL_PORT)' batch-secrets/batch-test-cloud-sql-config.json > batch-secrets/tmp.batch-test-cloud-sql-config.json
+	mv batch-secrets/tmp.batch-test-cloud-sql-config.json batch-secrets/batch-test-cloud-sql-config.json
+
+run-cloud-sql-proxy: local-cloud-sql-config install-cloud-sql-proxy
+	$(eval config := $(shell pwd)/batch-secrets/batch-test-cloud-sql-config.json)
+	$(eval connection_name := $(shell jq -r '.connection_name' $(config)))
+	$(eval port := $(shell jq -r '.port' $(config)))
+	./cloud_sql_proxy -instances=$(connection_name)=tcp:$(port) &
