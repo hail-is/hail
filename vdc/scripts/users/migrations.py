84a19600c6db7990008b3a713e01c50497b4a8d1
Alex Kotlar <akotlar@bu.edu>
2019-04-02 13:44:29 -0400

    update migration to use pymysql

--- vdc/scripts/users/migrations.py
+++ vdc/scripts/users/migrations.py
@@ -1,42 +1,36 @@
 from table import Table
-import mysql.connector
+import pymysql
 
 
 class MigrateTable(Table):
     def __init__(self):
         secrets = Table.getSecrets()
 
-        if not secrets:
-            raise "Couldn't read secrets"
-
-        self.cnx = mysql.connector.connect(
-            host=secrets['host'],
-            user=secrets['user'],
-            password=secrets['password']
-        )
+        self.cnx = pymysql.connect(host=secrets['host'],
+                                   user=secrets['user'],
+                                   password=secrets['password'],
+                                   cursorclass=pymysql.cursors.DictCursor)
 
     def up(self):
-        cursor = self.cnx.cursor()
+        with self.cnx.cursor() as cursor:
+            cursor.execute("CREATE DATABASE IF NOT EXISTS users;")
+            cursor.execute("USE users;")
+            cursor.execute(
+                """
+                    CREATE TABLE IF NOT EXISTS user_data (
+                        id INT NOT NULL AUTO_INCREMENT,
+                        user_id VARCHAR(255) NOT NULL,
+                        gsa_email VARCHAR(255) NOT NULL,
+                        ksa_name VARCHAR(255) NOT NULL,
+                        bucket_name VARCHAR(255) NOT NULL,
+                        gsa_key_secret_name VARCHAR(255) NOT NULL,
+                        PRIMARY KEY (id),
+                        UNIQUE INDEX auth0_id (user_id)
+                    ) ENGINE=INNODB;
+                """
+            )
 
-        cursor.execute("CREATE DATABASE IF NOT EXISTS users;")
-        cursor.execute("USE users;")
-        cursor.execute(
-            """
-                CREATE TABLE IF NOT EXISTS user_data (
-                    id INT NOT NULL AUTO_INCREMENT,
-                    user_id VARCHAR(255) NOT NULL,
-                    gsa_email VARCHAR(255) NOT NULL,
-                    ksa_name VARCHAR(255) NOT NULL,
-                    bucket_name VARCHAR(255) NOT NULL,
-                    gsa_key_secret_name VARCHAR(255) NOT NULL,
-                    PRIMARY KEY (id),
-                    UNIQUE INDEX auth0_id (user_id)
-                ) ENGINE=INNODB;
-            """
-        )
-
-        self.cnx.commit()
-        cursor.close()
+            self.cnx.commit()
 
 
 if __name__ == "__main__":
