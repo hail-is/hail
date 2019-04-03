61b99b4b6b1d7d51a2938d9f6350ae94b5204a57
Christopher Vittal <christopher.vittal@gmail.com>
2019-04-03 15:30:59 -0400

    Increase cloudtools version

--- hail/hail-ci-deploy.sh
+++ hail/hail-ci-deploy.sh
@@ -5,7 +5,7 @@ export CXXFLAGS="${CXXFLAGS} -Werror"
 
 SPARK_VERSION=2.4.0
 BRANCH=0.2
-CLOUDTOOLS_VERSION=3
+CLOUDTOOLS_VERSION=4
 HASH_TARGET=gs://hail-common/builds/${BRANCH}/latest-hash/cloudtools-${CLOUDTOOLS_VERSION}-spark-${SPARK_VERSION}.txt
 SHA=$(git rev-parse --short=12 HEAD)
 
