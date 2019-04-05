ae2f3bd0f7b184b9ffc128de254c010c16e59b0f
Tim Poterba <tpoterba@gmail.com>
2019-04-05 10:24:31 -0400

    Fix Mentions (#5740)
    * Fix Mentions
    
    * fix

--- hail/src/main/scala/is/hail/expr/ir/Mentions.scala
+++ hail/src/main/scala/is/hail/expr/ir/Mentions.scala
@@ -1,27 +1,5 @@
 package is.hail.expr.ir
 
 object Mentions {
-  def apply(x: IR, v: String): Boolean = CountMentions(x, v) > 0
-}
-
-object CountMentions {
-  def apply(x: IR, v: String): Int = {
-    x match {
-      case Ref(n, _) =>
-        if (v == n)
-          1
-        else
-          0
-      case _ =>
-        Children(x).iterator.zipWithIndex.map {
-          case (c: IR, i) =>
-            if (Binds(x, v, i))
-              0
-            else
-              CountMentions(c, v)
-          case _ => 0
-        }
-          .sum
-    }
-  }
+  def apply(x: IR, v: String): Boolean = FreeVariables(x).exists(_.name == v)
 }
