5f41d7939184f4c533524638f7ca582eb78a5f83
Jackie Goldstein <jigold@broadinstitute.org>
2019-04-05 17:00:50 -0400

    addressed comment?

--- batch/test/test_database.py
+++ batch/test/test_database.py
@@ -6,18 +6,22 @@ from batch.server.database import Database
 
 class Test(unittest.TestCase):
     def setUp(self):
-        self.db = Database.create_synchronous(os.environ.get('CLOUD_SQL_CONFIG_PATH',
-                                                             '/batch-secrets/batch-production-cloud-sql-config.json'))
+        self.db = Database.create_synchronous(os.environ.get('CLOUD_SQL_CONFIG_PATH'))
 
     def test_create_drop(self):
         schema = {'id': 'BIGINT'}
         key = ['id']
 
-        t1_name = self.db.create_temp_table_sync("t1", schema, key)
-        t2_name = self.db.create_temp_table_sync("t2", schema, key)
-        assert t1_name.startswith("t1")
-        assert t2_name.startswith("t2")
+        try:
+            t1_name = self.db.create_temp_table_sync("t1", schema, key)
+            t2_name = self.db.create_temp_table_sync("t2", schema, key)
+            assert t1_name.startswith("t1")
+            assert t2_name.startswith("t2")
 
-        self.db.drop_table_sync(t1_name, t2_name)
-        assert not self.db.has_table_sync(t1_name)
-        assert not self.db.has_table_sync(t2_name)
+            self.db.drop_table_sync(t1_name, t2_name)
+            assert not self.db.has_table_sync(t1_name)
+            assert not self.db.has_table_sync(t2_name)
+        finally:
+            self.db.drop_table_sync(t1_name)
+            self.db.drop_table_sync(t2_name)
+
