6cad04c5bccebfa90fab0ec3bcbc7ff60d760328
jigold <jigold@users.noreply.github.com>
2019-04-05 19:22:17 -0400

    [batch] sql temporary tables (#5781)
    * [batch] create temporary tables for testing
    
    * fix flake8 errors
    
    * address comment
    
    * fix pylint
    
    * fix
    
    * addressed comment?
    
    * address comment?

---
+++ batch/test/test_database.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+
+from batch.server.database import Database
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.db = Database.create_synchronous(os.environ.get('CLOUD_SQL_CONFIG_PATH'))
+
+    def test_create_drop(self):
+        schema = {'id': 'BIGINT'}
+        key = ['id']
+
+        try:
+            t1_name = self.db.create_temp_table_sync("t1", schema, key)
+            t2_name = self.db.create_temp_table_sync("t2", schema, key)
+            assert t1_name.startswith("t1")
+            assert t2_name.startswith("t2")
+        finally:
+            self.db.drop_table_sync(t1_name, t2_name)
+            assert not self.db.has_table_sync(t1_name)
+            assert not self.db.has_table_sync(t2_name)
+
