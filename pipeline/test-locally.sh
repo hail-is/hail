eedb0f7e71911d7e64861c2edfd483e9abff62dc
Jackie Goldstein <jigold@broadinstitute.org>
2019-04-08 12:56:51 -0400

    fix pipeline test-locally

--- pipeline/test-locally.sh
+++ pipeline/test-locally.sh
@@ -18,9 +18,11 @@ trap "exit 24" INT TERM
 if [[ $CLOUD_SQL_PROXY -eq 1 ]]; then
     export CLOUD_SQL_CONFIG_PATH=`pwd`/batch-secrets/batch-test-cloud-sql-config.json
     connection_name=$(jq -r '.connection_name' $CLOUD_SQL_CONFIG_PATH)
+    host=$(jq -r '.host' $CLOUD_SQL_CONFIG_PATH)
     port=$(jq -r '.port' $CLOUD_SQL_CONFIG_PATH)
     ./cloud_sql_proxy -instances=$connection_name=tcp:$port &
     proxy_pid=$!
+    ../until-with-fuel 30 curl -fL $host:$port
 else
     export CLOUD_SQL_CONFIG_PATH=/batch-secrets/batch-test-cloud-sql-config.json
 fi
