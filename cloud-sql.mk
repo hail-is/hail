ef60507a903870fa0b561fe83cdeb153cd1da0e5
Jackie Goldstein <jigold@broadinstitute.org>
2019-04-08 12:52:32 -0400

    address comments

--- cloud-sql.mk
+++ cloud-sql.mk
@@ -7,24 +7,32 @@ install-cloud-sql-proxy:
 		curl -o cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64 && \
 		chmod +x cloud_sql_proxy
 
-batch-secrets/batch-test-cloud-sql-config:
+batch-secrets/batch-test-cloud-sql-config.json:
 	mkdir -p batch-secrets && kubectl get secret \
 	    batch-test-cloud-sql-config -n batch-pods \
 	    -o "jsonpath={.data.batch-test-cloud-sql-config\.json}" \
 	  | base64 --decode \
 	  | jq '.host = "127.0.0.1" | .port = $(CLOUD_SQL_PORT)' \
-	    > batch-secrets/batch-test-cloud-sql-config.json
+	    > $@
 
-batch-secrets/batch-production-cloud-sql-config:
+batch-secrets/batch-production-cloud-sql-config.json:
 	mkdir -p batch-secrets && kubectl get secret \
 	    batch-production-cloud-sql-config -n default \
 	    -o "jsonpath={.data.batch-production-cloud-sql-config\.json}" \
 	  | base64 --decode \
 	  | jq '.host = "127.0.0.1" | .port = $(CLOUD_SQL_PORT)' \
-	    > batch-secrets/batch-production-cloud-sql-config.json
+	    > $@
 
-run-cloud-sql-proxy: batch-secrets/batch-test-cloud-sql-config install-cloud-sql-proxy
+run-cloud-sql-proxy-batch-test: batch-secrets/batch-test-cloud-sql-config.json
+run-cloud-sql-proxy-batch-test: install-cloud-sql-proxy
 	$(eval config := $(shell pwd)/batch-secrets/batch-test-cloud-sql-config.json)
+	$(eval connection_name := $(shell jq -r '.connection_name' $(config)))
+	$(eval port := $(shell jq -r '.port' $(config)))
+	./cloud_sql_proxy -instances=$(connection_name)=tcp:$(port) &
+
+run-cloud-sql-proxy-batch-production: batch-secrets/batch-production-cloud-sql-config.json
+run-cloud-sql-proxy-batch-production: install-cloud-sql-proxy
+	$(eval config := $(shell pwd)/batch-secrets/batch-production-cloud-sql-config.json)
 	$(eval connection_name := $(shell jq -r '.connection_name' $(config)))
 	$(eval port := $(shell jq -r '.port' $(config)))
 	./cloud_sql_proxy -instances=$(connection_name)=tcp:$(port) &
