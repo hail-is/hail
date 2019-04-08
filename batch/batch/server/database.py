d7db28288895acebfcce73f2feb3e3d2fd477db8
jigold <jigold@users.noreply.github.com>
2019-04-08 11:46:51 -0400

    [batch] Expose and test SQL table operations (#5784)
    * [batch] Add support for sql table objects
    
    * fix pylint errors
    
    * Revert "fix pylint errors"
    
    This reverts commit 1322972b851ddc1bcf971ab5b19910e8c2d7ed3b.
    
    * fix pylint errors
    
    * fix pylint error
    
    * address comments
    
    * fix backticks
    
    * get rid of echo
    
    * retest

--- batch/batch/server/database.py
+++ batch/batch/server/database.py
@@ -63,7 +63,7 @@ class Database:
 
         async with self.pool.acquire() as conn:
             async with conn.cursor() as cursor:
-                schema = ", ".join([f"`{n}` {t}" for n, t in schema.items()])
+                schema = ", ".join([f'`{n.replace("`", "``")}` {t}' for n, t in schema.items()])
                 key_names = ", ".join([f'`{name.replace("`", "``")}`' for name in keys])
                 keys = f", PRIMARY KEY( {key_names} )" if keys else ''
                 exists = 'IF NOT EXISTS' if can_exist else ''
@@ -73,16 +73,87 @@ class Database:
     def create_table_sync(self, name, schema, keys):
         return run_synchronous(self.create_table(name, schema, keys))
 
-    async def create_temp_table(self, root_name, schema, keys):
+    async def create_temporary_table(self, root_name, schema, keys):
         for i in range(5):
             try:
                 suffix = uuid.uuid4().hex[:8]
                 name = f'{root_name}-{suffix}'
-                await self.create_table(name, schema, keys, can_exist=False)
-                return name
+                return await Table(self, name, schema, keys, can_exist=False)
             except pymysql.err.InternalError:
                 pass
         raise Exception("Too many attempts to get temp table.")
 
-    def create_temp_table_sync(self, root_name, schema, keys):
-        return run_synchronous(self.create_temp_table(root_name, schema, keys))
+    def create_temporary_table_sync(self, root_name, schema, keys):
+        return run_synchronous(self.create_temporary_table(root_name, schema, keys))
+
+
+def make_where_statement(items):
+    template = []
+    values = []
+    for k, v in items.items():
+        if isinstance(v, list):
+            if len(v) == 0:
+                template.append("FALSE")
+            else:
+                template.append(f'`{k.replace("`", "``")}` IN %s')
+                values.append(v)
+        else:
+            template.append(f'`{k.replace("`", "``")}` = %s')
+            values.append(v)
+
+    template = " AND ".join(template)
+    return template, values
+
+
+@asyncinit
+class Table:  # pylint: disable=R0903
+    async def __init__(self, db, name, schema, keys, can_exist=False):
+        self.name = name
+        self._db = db
+        await self._db.create_table(name, schema, keys, can_exist)
+
+    async def new_record(self, items):
+        names = ", ".join([f'`{name.replace("`", "``")}`' for name in items.keys()])
+        values_template = ", ".join(["%s" for _ in items.values()])
+        async with self._db.pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                sql = f"INSERT INTO `{self.name}` ({names}) VALUES ({values_template})"
+                await cursor.execute(sql, tuple(items.values()))
+                id = cursor.lastrowid  # This returns 0 unless an autoincrement field is in the table
+        return id
+
+    async def update_record(self, where_items, set_items):
+        async with self._db.pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                if len(set_items) != 0:
+                    where_template, where_values = make_where_statement(where_items)
+                    set_template = ", ".join([f'`{k.replace("`", "``")}` = %s' for k, v in set_items.items()])
+                    set_values = set_items.values()
+                    sql = f"UPDATE `{self.name}` SET {set_template} WHERE {where_template}"
+                    await cursor.execute(sql, (*set_values, *where_values))
+
+    async def get_record(self, where_items, select_fields=None):
+        assert select_fields is None or len(select_fields) != 0
+        async with self._db.pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                where_template, where_values = make_where_statement(where_items)
+                select_fields = ",".join(select_fields) if select_fields is not None else "*"
+                sql = f"SELECT {select_fields} FROM `{self.name}` WHERE {where_template}"
+                await cursor.execute(sql, where_values)
+                result = await cursor.fetchall()
+        return result
+
+    async def has_record(self, where_items):
+        async with self._db.pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                where_template, where_values = make_where_statement(where_items)
+                sql = f"SELECT COUNT(1) FROM `{self.name}` WHERE {where_template}"
+                count = await cursor.execute(sql, tuple(where_values))
+        return count >= 1
+
+    async def delete_record(self, where_items):
+        async with self._db.pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                where_template, where_values = make_where_statement(where_items)
+                sql = f"DELETE FROM `{self.name}` WHERE {where_template}"
+                await cursor.execute(sql, where_values)
