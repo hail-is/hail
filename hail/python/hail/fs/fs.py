ac042604fe455e57144c2d342f522829abfa4166
Alex Kotlar <akotlar@bu.edu>
2019-04-15 10:35:03 -0400

    add human-readable filesize, make ls more efficient

--- hail/python/hail/fs/fs.py
+++ hail/python/hail/fs/fs.py
@@ -7,6 +7,7 @@ from typing import Dict, List
 import sys
 import os
 import gcsfs
+from hurry.filesize import size
 
 
 class FS(abc.ABC):
@@ -151,21 +152,22 @@ class GoogleCloudStorageFS(FS):
         return self._stat_is_dir(stats)
 
     def stat(self, path: str) -> Dict:
-        stats = self.client.info(path)
+        return self._process_obj(self.client.info(path))
 
+    def _process_obj(self, stats: Dict) -> Dict:
         return {
             'is_dir': self._stat_is_dir(stats),
             'size_bytes': stats['size'],
-            'size': stats['size'],
+            'size': size(stats['size']),
             'path': stats['path'],
             'owner': stats['bucket'],
             'modification_time': stats.get('updated')
         }
 
-    def _stat_is_dir(self, stats: Dict):
+    def _stat_is_dir(self, stats: Dict) -> bool:
         return stats['storageClass'] == 'DIRECTORY' or stats['name'].endswith('/')
 
     def ls(self, path: str) -> List[Dict]:
-        files = self.client.ls(path)
+        files = self.client.ls(path, detail=True)
 
-        return [self.stat(file) for file in files]
+        return [self._process_obj(file) for file in files]
