f6f957771f674c31833a61a892b3d12d7f8d09e5
Jackie Goldstein <jigold@broadinstitute.org>
2019-04-16 13:05:01 -0400

    fix interface to not require secret key

--- hailjwt/hailjwt/hailjwt.py
+++ hailjwt/hailjwt/hailjwt.py
@@ -21,18 +21,21 @@ class JWTClient:
                 f'found secret key with {len(key_bytes)} bytes, but secret key '
                 f'must have at least 32 bytes (i.e. 256 bits)')
 
-    def __init__(self, secret_key):
-        JWTClient._verify_key_preqrequisites(secret_key)
+    def __init__(self, secret_key=None):
+        if secret_key is not None:
+            JWTClient._verify_key_preqrequisites(secret_key)
         self.secret_key = secret_key
 
     def unsafe_decode(self, token):
         return jwt.decode(token, verify=False)
         
     def decode(self, token):
+        assert self.secret_key is not None
         return jwt.decode(
             token, self.secret_key, algorithms=[JWTClient.__ALGORITHM])
 
     def encode(self, payload):
+        assert self.secret_key is not None        
         return jwt.encode(
             payload, self.secret_key, algorithm=JWTClient.__ALGORITHM)
 
