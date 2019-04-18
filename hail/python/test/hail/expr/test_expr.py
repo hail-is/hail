7dd9706dd67a92ca03cc33719d210ca1037e729c
Daniel King <daniel.zidan.king@gmail.com>
2019-04-18 18:52:02 -0400

    fixes

--- hail/python/test/hail/expr/test_expr.py
+++ hail/python/test/hail/expr/test_expr.py
@@ -2838,18 +2838,18 @@ class Tests(unittest.TestCase):
         self.assertRaises(ValueError, lambda: cube.transpose((1, 1)))
 
     def test_collection_getitem(self):
-        collection_types = [hl.array, hl.set]
-        for typ in collection_types:
-            x = typ([hl.struct(a='foo', b=3), hl.struct(a='bar', b=4)])
+        collection_types = [(hl.array, list), (hl.set, set)]
+        for (htyp, ptyp) in collection_hl.arrayes:
+            x = htyp([hl.struct(a='foo', b=3), hl.struct(a='bar', b=4)])
             assert hl.eval(x.a) == ['foo', 'bar']
 
-            a = typ([hl.struct(b=[hl.struct(inner=1),
-                                  hl.struct(inner=2)]),
-                     hl.struct(b=[hl.struct(inner=3)])])
-            assert hl.eval(a.b) == [[hl.struct(inner=1), hl.struct(inner=2)],
-                                    [hl.struct(inner=3)]]
-            assert hl.eval(hl.flatten(a.b).inner) == [1, 2, 3]
-            assert hl.eval(a.b.inner) == [[1, 2], [3]]
-            assert hl.eval(a["b"].inner) == [[1, 2], [3]]
-            assert hl.eval(a["b"]["inner"]) == [[1, 2], [3]]
-            assert hl.eval(a.b["inner"]) == [[1, 2], [3]]
+            a = htyp([hl.struct(b=htyp([hl.struct(inner=1),
+                                      hl.struct(inner=2)])),
+                      hl.struct(b=htyp([hl.struct(inner=3)]))])
+            assert hl.eval(a.b) == ptyp([ptyp([hl.struct(inner=1), hl.struct(inner=2)]),
+                                         ptyp([hl.struct(inner=3)])])
+            assert hl.eval(hl.flatten(a.b).inner) == ptyp([1, 2, 3])
+            assert hl.eval(a.b.inner) == ptyp([ptyp([1, 2]), ptyp([3])])
+            assert hl.eval(a["b"].inner) == ptyp([ptyp([1, 2]), ptyp([3])])
+            assert hl.eval(a["b"]["inner"]) == ptyp([ptyp([1, 2]), ptyp([3])])
+            assert hl.eval(a.b["inner"]) == ptyp([ptyp([1, 2]), ptyp([3])])
