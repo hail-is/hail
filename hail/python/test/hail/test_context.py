be56879edee2658253911e636e051c59e85cc05b
Daniel King <daniel.zidan.king@gmail.com>
2019-04-19 10:48:32 -0400

    tests

--- hail/python/test/hail/test_context.py
+++ hail/python/test/hail/test_context.py
@@ -9,3 +9,7 @@ tearDownModule = stopTestHailContext
 class Tests(unittest.TestCase):
     def test_init_hail_context_twice(self):
         hl.init(hl.spark_context(), idempotent=True) # Should be no error
+
+    def test_top_level_functions_are_do_not_error(self):
+        hl.current_backend()
+        hl.debug_info()
