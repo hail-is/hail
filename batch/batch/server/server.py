4416fc2b8ec2b206f347790439fee1ab78853d79
Daniel King <daniel.zidan.king@gmail.com>
2019-04-29 17:28:52 -0400

    [batch] fix duration calculation (#5987)
    * [batch] fix duration calculation
    
    * add log

--- batch/batch/server/server.py
+++ batch/batch/server/server.py
@@ -504,7 +504,12 @@ class Job:
         else:
             terminated = pod.status.container_statuses[0].state.terminated
             self.exit_code = terminated.exit_code
-            self.duration = (terminated.finished_at - terminated.started_at).total_seconds()
+            if terminated.finished_at is not None and terminated.started_at is not None:
+                self.duration = terminated.finished_at - terminated.started_at).total_seconds()
+            else:
+                log.warn(f'job {self.id} has pod {pod.metadata.name} which is '
+                         f'terminated but has no timing information. {pod}')
+                self.duration = None
             await db.jobs.update_record(self.id,
                                         exit_code=self.exit_code,
                                         duration=self.duration,
