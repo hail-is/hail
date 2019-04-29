acc2ead7d224ca84c6781c3394b39414aced3388
laurent <lfran@broadinstitute.org>
2019-04-29 10:59:18 -0400

    added `join_plot` to plots init

--- hail/python/hail/plot/__init__.py
+++ hail/python/hail/plot/__init__.py
@@ -1,10 +1,11 @@
-from .plots import output_notebook, show, histogram, cumulative_histogram, histogram2d, scatter, qq, manhattan, pdf, cdf
+from .plots import output_notebook, show, histogram, cumulative_histogram, histogram2d, scatter, joint_plot, qq, manhattan, pdf, cdf
 
 __all__ = ['output_notebook',
            'show',
            'histogram',
            'cumulative_histogram',
            'scatter',
+           'joint_plot'
            'histogram2d',
            'qq',
            'manhattan',
