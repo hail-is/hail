e7e997b2c3099adc1ff0e1ad40b956fe60a79f60
liameabbott <liam.e.abbott@gmail.com>
2019-04-30 19:26:28 +0000

    in progress

--- extract/extract.LDSC_baselineLD_v2.2_bed_files.sh
+++ extract/extract.LDSC_baselineLD_v2.2_bed_files.sh
@@ -5,6 +5,6 @@ mkdir /tmp/baselineLD_v2.2
 tar -C /tmp/baselineLD_v2.2/ -xzvf /tmp/baselineLD_v2.2_bedfiles.tgz
 for file in /tmp/baselineLD_v2.2/*.bed; do
     echo $file;
-    bgzip -c $file | gsutil cp - gs://hail-datasets-raw-data/LDSC/baselineLD_v2.2/$(basename ${file}).bgz;
+    bgzip -c $file | gsutil cp - gs://hail-datasets-raw-data/LDSC/baselineLD_v2.2/bed_files/$(basename ${file}).bgz;
 done
 rm -r /tmp/baselineLD_v2.2*
--- extract/extract.LDSC_baseline_v1.1_bed_files.sh
+++ extract/extract.LDSC_baseline_v1.1_bed_files.sh
@@ -4,6 +4,6 @@ wget -O /tmp/baseline_v1.1_bedfiles.tgz https://data.broadinstitute.org/alkesgro
 tar -C /tmp/ -xzvf /tmp/baseline_v1.1_bedfiles.tgz
 for file in /tmp/baseline_v1.1/*.bed; do
     echo $file;
-    bgzip -c $file | gsutil cp - gs://hail-datasets-raw-data/LDSC/baseline_v1.1/$(basename ${file}).bgz;
+    bgzip -c $file | gsutil cp - gs://hail-datasets-raw-data/LDSC/baseline_v1.1/bed_files/$(basename ${file}).bgz;
 done
 rm -r /tmp/baseline_v1.1*
