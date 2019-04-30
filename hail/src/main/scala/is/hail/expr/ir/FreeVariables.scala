6ffedd72f03fc2833e0e9e6db9ca7fdc9feabec7
Tim Poterba <tpoterba@gmail.com>
2019-04-30 09:09:53 -0400

    fix stuff

--- hail/src/main/scala/is/hail/expr/ir/FreeVariables.scala
+++ hail/src/main/scala/is/hail/expr/ir/FreeVariables.scala
@@ -8,37 +8,37 @@ object FreeVariables {
 
     def compute(ir1: IR, baseEnv: BindingEnv[Unit]): BindingEnv[Unit] = {
       ir1 match {
-        case x@Ref(name, _) =>
+        case Ref(name, _) =>
           baseEnv.bindEval(name, ())
-        case TableAggregate(_, _) =>
-        case MatrixAggregate(_, _) =>
+        case TableAggregate(_, _) => baseEnv
+        case MatrixAggregate(_, _) => baseEnv
         case ArrayAggScan(a, name, query) =>
           val aE = compute(a, baseEnv)
           val qE = compute(query, baseEnv.copy(scan = Some(Env.empty)))
-          aE.merge(qE.copy(eval = qE.eval.bindIterable(qE.scan.get.m - name)))
+          aE.merge(qE.copy(eval = qE.eval.bindIterable(qE.scan.get.m - name), scan = None))
         case ArrayAgg(a, name, query) =>
           val aE = compute(a, baseEnv)
           val qE = compute(query, baseEnv.copy(agg = Some(Env.empty)))
-          aE.merge(qE.copy(eval = qE.eval.bindIterable(qE.agg.get.m - name)))
+          aE.merge(qE.copy(eval = qE.eval.bindIterable(qE.agg.get.m - name), agg = None))
         case _ =>
           ir1.children
             .iterator
             .zipWithIndex
             .map {
               case (child: IR, i) =>
-                val base = ChildEnvWithoutBindings(ir1, i, base)
-                val sub = compute(child, ChildEnvWithoutBindings(ir1, i, base))
-                  .subtract(NewBindings(ir1, i, base))
+                val childEnv = ChildEnvWithoutBindings(ir1, i, baseEnv)
+                val sub = compute(child, ChildEnvWithoutBindings(ir1, i, childEnv))
+                  .subtract(NewBindings(ir1, i, childEnv))
                 if (UsesAggEnv(ir1, i))
                   sub.copy(agg = Some(sub.eval))
                 else if (UsesScanEnv(ir1, i))
-                  sub.copy(scan= Some(sub.eval))
+                  sub.copy(scan = Some(sub.eval))
                 else
                   sub
               case _ =>
                 baseEnv
             }
-          .fold(baseEnv)(_.merge(_))
+            .fold(baseEnv)(_.merge(_))
       }
     }
 
