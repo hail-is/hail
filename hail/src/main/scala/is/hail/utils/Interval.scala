aff0a36bcc373cc079afe0d7603fc5beea21c170
Tim Poterba <tpoterba@gmail.com>
2019-05-08 17:42:29 -0400

    [hail] Add interval reduction and intersection functions (#6073)
    * [infrastructure] Add interval reduction and intersection functions
    
    * address comments

--- hail/src/main/scala/is/hail/utils/Interval.scala
+++ hail/src/main/scala/is/hail/utils/Interval.scala
@@ -226,4 +226,50 @@ object Interval {
       }
     }
   }
+
+  def union(xs: Array[Interval], ord: IntervalEndpointOrdering): Array[Interval] = {
+
+    val sorted = xs.sortBy(_.left: Any)(ord.toOrdering)
+
+    val ab = new ArrayBuilder[Interval]()
+    var i = 0
+    while (i < sorted.length) {
+      var interval = sorted(i)
+      i += 1
+      while (i < sorted.length && ord.gteq(interval.right, sorted(i).left)) {
+        interval = Interval(interval.left, ordMax(interval.right, sorted(i).right, ord))
+        i += 1
+      }
+
+      ab += interval
+    }
+    ab.result()
+  }
+
+  // assumes that both `x1` and `x2` are both sorted, non-overlapping interval sequences.
+  def intersection(x1: Array[Interval], x2: Array[Interval], ord: IntervalEndpointOrdering): Array[Interval] = {
+
+    var i = 0
+    var j = 0
+    val ab = new ArrayBuilder[Interval]()
+
+    while (!(i >= x1.length || j >= x2.length)) {
+      val l = x1(i)
+      val r = x2(j)
+
+      if (ord.gteq(l.left, r.right))
+        j += 1
+      else if (ord.gteq(r.left, l.right))
+        i += 1
+      else {
+        val overlap = Interval(ordMax(l.left, r.left, ord), ordMin(l.right, r.right, ord))
+        ab += overlap
+        if (ord.lt(l.right, r.right))
+          i += 1
+        else
+          j += 1
+      }
+    }
+    ab.result()
+  }
 }
