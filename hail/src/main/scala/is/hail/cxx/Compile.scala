31954dadd8a642d16cbd2b6c9ac307566351fdda
Alex Kotlar <akotlar@bu.edu>
2019-05-08 14:41:47 -0400

    compile cleanup

--- hail/src/main/scala/is/hail/cxx/Compile.scala
+++ hail/src/main/scala/is/hail/cxx/Compile.scala
@@ -9,6 +9,7 @@ import is.hail.expr.ir.BindingEnv
 import is.hail.expr.types.physical._
 import is.hail.expr.types.virtual.TVoid
 import is.hail.io.CodecSpec
+import is.hail.io.fs.HadoopFS
 import is.hail.nativecode.{NativeModule, NativeStatus, ObjectArray}
 import is.hail.utils.fatal
 
@@ -140,9 +141,7 @@ object Compile {
     mod.close()
     st.close()
 
-    // TODO: WHY NOT JUST Hailcontext.fs?
-    // new HadoopFS(HailContext.get.sc.hadoopConfiguration)
-    val fs = HailContext.sFS
+    val fs = new HadoopFS(HailContext.get.sc.hadoopConfiguration)
 
     { (region: Long) =>
       val st2 = new NativeStatus()
@@ -170,9 +169,7 @@ object Compile {
     mod.close()
     st.close()
 
-    // TODO: WHY NOT JUST Hailcontext.fs?
-    // new HadoopFS(HailContext.get.sc.hadoopConfiguration)
-    val fs = HailContext.sFS
+    val fs = new HadoopFS(HailContext.get.sc.hadoopConfiguration)
 
     { (region: Long, v2: Long) =>
       val st2 = new NativeStatus()
