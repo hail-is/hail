7623d7b92d889ea293cc48136eed3f4b1e6eae23
Cotton Seed <hail-ci-leader@example.com>
2019-05-07 21:01:54 -0400

    add back non-foldable nodes

--- hail/src/main/scala/is/hail/expr/ir/FoldConstants.scala
+++ hail/src/main/scala/is/hail/expr/ir/FoldConstants.scala
@@ -5,6 +5,16 @@ import is.hail.utils.HailException
 object FoldConstants {
   def apply(ir: BaseIR, canGenerateLiterals: Boolean = true): BaseIR =
     RewriteBottomUp(ir, {
+      case _: Ref |
+           _: In |
+           _: ApplySeeded |
+           _: ApplyAggOp |
+           _: ApplyScanOp |
+           _: SeqOp |
+           _: Begin |
+           _: InitOp |
+           _: ArrayRange |
+           _: Die => None
       case ir: IR if !IsConstant(ir) &&
         Interpretable(ir) &&
         ir.children.forall {
