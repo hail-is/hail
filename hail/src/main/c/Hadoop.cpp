22d94b57f18dfd55f39ffa2888a33e3e1b991882
Alex Kotlar <akotlar@bu.edu>
2019-05-08 17:14:48 -0400

    call FS instead of Hadoop

--- hail/src/main/c/Hadoop.cpp
+++ hail/src/main/c/Hadoop.cpp
@@ -1,16 +1,16 @@
-#include "hail/Hadoop.h"
+#include "hail/FS.h"
 #include "hail/Upcalls.h"
 #include <jni.h>
 
 namespace hail {
 
-HadoopConfig::HadoopConfig(jobject jhadoop_config) : jhadoop_config_(up_.env()->NewGlobalRef(jhadoop_config)) { }
+FS::FS(jobject jfs) : jfs_(up_.env()->NewGlobalRef(jfs)) { }
 
-HadoopConfig::~HadoopConfig() { up_.env()->DeleteGlobalRef(jhadoop_config_); }
+FS::~FS() { up_.env()->DeleteGlobalRef(jfs_); }
 
-std::shared_ptr<OutputStream> HadoopConfig::unsafe_writer(std::string path) {
+std::shared_ptr<OutputStream> FS::unsafe_writer(std::string path) {
   jstring jpath = up_.env()->NewStringUTF(path.c_str());
-  jobject joutput_stream = up_.env()->CallObjectMethod(jhadoop_config_, unsafe_writer_method_id_, jpath);
+  jobject joutput_stream = up_.env()->CallObjectMethod(jfs_, unsafe_writer_method_id_, jpath);
   up_.env()->DeleteLocalRef(jpath);
 
   return std::make_shared<OutputStream>(up_, joutput_stream);
