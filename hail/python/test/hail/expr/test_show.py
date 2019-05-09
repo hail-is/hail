4895201cad10a6bd48c5e660245b7fc5c00cad7c
Daniel King <daniel.zidan.king@gmail.com>
2019-05-09 14:42:28 -0400

    add some show specific tests

---
+++ hail/python/test/hail/expr/test_show.py
@@ -0,0 +1,21 @@
+import hail as hl
+
+setUpModule = startTestHailContext
+tearDownModule = stopTestHailContext
+
+class Tests(unittest.TestCase):
+    def test():
+        mt = hl.balding_nichols_model(3, 10, 10)
+        t = hl.utils.range_table(10)
+        mt.GT.show()
+        mt.locus.show()
+        mt.af.show()
+        mt.pop.show()
+        mt.sample_idx.show()
+        mt.bn.show()
+        mt.bn.fst.show()
+        mt.GT.n_alt_alleles().show()
+        (mt.GT.n_alt_alleles() * mt.GT.n_alt_alleles()).show()
+        (mt.af * mt.GT.n_alt_alleles()).show()
+        t.af.show()
+        (t.af * 3).show()
