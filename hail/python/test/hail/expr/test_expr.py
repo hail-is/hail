d1943eec928ed2893c15ab97fe352462f6e39a19
Daniel King <daniel.zidan.king@gmail.com>
2019-05-09 14:15:18 -0400

    [hail][feature] Super array expression (#5913)
    * [hail] collection expression acts like a table
    
    * boom
    
    * remove reuqirements changes
    
    * fixes
    
    * fix
    
    * fix
    
    * fml
    
    * fixes
    
    * fix
    
    * fix
    
    * fix
    
    * no getitem, no column format
    
    * expressions!
    
    * fix array expression getitem
    
    * elif style
    
    * elif style
    
    * fix
    
    * fix
    
    * fix two bugs

--- hail/python/test/hail/expr/test_expr.py
+++ hail/python/test/hail/expr/test_expr.py
@@ -2836,3 +2836,25 @@ class Tests(unittest.TestCase):
 
         self.assertRaises(ValueError, lambda: v.transpose((1,)))
         self.assertRaises(ValueError, lambda: cube.transpose((1, 1)))
+
+    def test_collection_getitem(self):
+        collection_types = [(hl.array, list), (hl.set, frozenset)]
+        for (htyp, pytyp) in collection_types:
+            x = htyp([hl.struct(a='foo', b=3), hl.struct(a='bar', b=4)])
+            assert hl.eval(x.a) == pytyp(['foo', 'bar'])
+
+        a = hl.array([hl.struct(b=[hl.struct(inner=1),
+                                   hl.struct(inner=2)]),
+                      hl.struct(b=[hl.struct(inner=3)])])
+        assert hl.eval(a.b) == [[hl.Struct(inner=1), hl.Struct(inner=2)],
+                                [hl.Struct(inner=3)]]
+        assert hl.eval(hl.flatten(a.b).inner) == [1, 2, 3]
+        assert hl.eval(a.b.inner) == [[1, 2], [3]]
+        assert hl.eval(a["b"].inner) == [[1, 2], [3]]
+        assert hl.eval(a["b"]["inner"]) == [[1, 2], [3]]
+        assert hl.eval(a.b["inner"]) == [[1, 2], [3]]
+
+        self.assertRaises(AttributeError, lambda: hl.array([1,2,3]).a)
+        self.assertRaises(AttributeError, lambda: hl.array([1,2,3])["a"])
+        self.assertRaises(AttributeError, lambda: hl.array([[1],[2],[3]])["a"])
+        self.assertRaises(AttributeError, lambda: hl.array([{1},{2},{3}])["a"])
