2fd7a857dad74be5e0b5d69e8c2a991fc2542ad1
Tim Poterba <tpoterba@gmail.com>
2019-05-16 17:32:08 -0400

    [hail][feature] Pass through args on `import_locus_intervals` (#6115)
    * [hail][feature] Pass through args on `import_locus_intervals`
    
    * bump
    
    * fix
    
    * use filter instead of partitions

--- hail/python/test/hail/methods/test_impex.py
+++ hail/python/test/hail/methods/test_impex.py
@@ -1210,6 +1210,14 @@ class LocusIntervalTests(unittest.TestCase):
         t = hl.import_bed(bed_file, reference_genome=None, skip_invalid_intervals=True)
         self.assertTrue(t.count() == 4)
 
+    def test_pass_through_args(self):
+        interval_file = resource('example3.interval_list')
+        t = hl.import_locus_intervals(interval_file,
+                                      reference_genome='GRCh37',
+                                      skip_invalid_intervals=True,
+                                      filter=r'target_\d\d')
+        assert t.count() == 9
+
 
 class ImportMatrixTableTests(unittest.TestCase):
     @skip_unless_spark_backend()
