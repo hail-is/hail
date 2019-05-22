0a7d431edc0f09d1e02321a00f5a0041b3cd0830
Daniel King <daniel.zidan.king@gmail.com>
2019-05-22 10:08:27 -0400

    Update Parser.scala

--- hail/src/main/scala/is/hail/expr/ir/Parser.scala
+++ hail/src/main/scala/is/hail/expr/ir/Parser.scala
@@ -142,8 +142,17 @@ object IRParser {
   def error(t: Token, msg: String): Nothing = ParserUtils.error(t.pos, msg)
 
   def deserialize[T](str: String)(implicit formats: Formats, mf: Manifest[T]): T = {
-    Serialization.read[T](str)
-  }
+    try {
+	      Serialization.read[T](str)
+	    } catch {
+	      case e: MappingException => 
+          if (e.cause != null) {
+            throw e.cause
+          } else {
+            throw e
+          }
+	    }
+	  }
 
   def consumeToken(it: TokenIterator): Token = {
     if (!it.hasNext)
