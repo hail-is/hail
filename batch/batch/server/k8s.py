b433da4210af916bd5a7b6440ac81d5add3c799a
Daniel King <daniel.zidan.king@gmail.com>
2019-05-23 17:17:22 -0400

    missing file

---
+++ batch/batch/server/k8s.py
@@ -0,0 +1,67 @@
+from .globals import blocking_to_async
+
+
+class K8s:
+    def __init__(self, blocking_pool, timeout, namespace, k8s_api):
+        self.blocking_pool = blocking_pool
+        self.timeout = timeout
+        self.namespace = namespace
+        self.__delete_pod = self._wrap_k8s_delete(k8s_api.delete_namespaced_pod)
+        self.__delete_pvc = self._wrap_k8s_delete(k8s_api.delete_namespaced_persistent_volume_claim)
+        self.__create_pod = self._wrap_k8s(k8s_api.create_namespaced_pod)
+        self.__create_pvc = self._wrap_k8s(k8s_api.create_namespaced_persistent_volume_claim)
+        self.__read_pod_log = self._wrap_k8s(k8s_api.read_namespaced_pod_log)
+        self.__list_pods = self._wrap_k8s(k8s_api.list_namespaced_pod)
+        self.__list_pvcs = self._wrap_k8s(k8s_api.list_namespaced_persistent_volume_claim)
+
+    def _wrap_k8s(fun):
+        async def wrapped(*args, **kwargs):
+            try:
+                if '_request_timeout' not in kwargs:
+                    kwargs['_request_timeout'] = self.timeout
+                if 'namespace' not in kwargs:
+                    kwargs['namespace'] = self.namespace
+                return (await blocking_to_async(self.blocking_pool,
+                                                f,
+                                                *args,
+                                                **kwargs),
+                        None)
+            except kube.client.rest.ApiException as err:
+                return (None, err)
+        wrapped.__name__ = fun.__name__
+        return wrapped
+
+    def _wrap_k8s_delete(fun):
+        k8s_fun = self._wrap_k8s(fun)
+
+        def wrapped(*args, **kwargs):
+            _, err = k8s_fun(args, kwargs)
+            if err is None or err.status is 404:
+                log.debug(f'ignore already deleted {fun.__name__}(*{args}, **{kwargs})')
+                return None
+            return err
+        wrapped.__name__ = fun.__name__
+        return wrapped
+
+    def delete_pod(name):
+        assert name is not None
+        return self.__delete_pod(name=name)
+
+    def delete_pvc(name):
+        assert name is not None
+        return self.__delete_pvc(name=name)
+
+    def create_pod(*args, **kwargs):
+        return self.__create_pod(*args, **kwargs)
+
+    def create_pvc(*args, **kwargs):
+        return self.__create_pvc(*args, **kwargs)
+
+    def read_pod_log(*args, **kwargs):
+        return self.__read_pod_log(*args, **kwargs)
+
+    def list_pods(*args, **kwargs):
+        return self.__list_pods(*args, **kwargs)
+
+    def list_pvcs(*args, **kwargs):
+        return self.__list_pvcs(*args, **kwargs)
