8127be525421fe1a5271f140c386cbf621d67030
Daniel King <daniel.zidan.king@gmail.com>
2019-05-28 15:57:09 -0400

    maybe appeas pylint

--- batch/test.sh
+++ batch/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
 python3 -m flake8 /batch/batch
-python3 -m pylint --rcfile /pylintrc /batch/batch --score=n
+(cd /batch/ && python3 -m pylint --rcfile /pylintrc batch --score=n)
 python3 -m pytest -vv -s /test/
