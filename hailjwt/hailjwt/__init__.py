2e9ba11599b0b58b77bd8d9cc4d686a2ef121f6d
cseed <cotton@alum.mit.edu>
2019-06-05 17:13:21 -0400

    Monitoring (#6242)
    * added monitoring
    
    cleanup
    
    fix
    
    fixed external access to prometheus
    
    * bump
    
    * bump

--- hailjwt/hailjwt/__init__.py
+++ hailjwt/hailjwt/__init__.py
@@ -1,7 +1,8 @@
-from .hailjwt import JWTClient, get_domain, authenticated_users_only
+from .hailjwt import JWTClient, get_domain, authenticated_users_only, authenticated_developers_only
 
 __all__ = [
     'JWTClient',
     'get_domain',
-    'authenticated_users_only'
+    'authenticated_users_only',
+    'authenticated_developers_only'
 ]
