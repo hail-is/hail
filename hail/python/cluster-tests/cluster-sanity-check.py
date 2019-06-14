5e8bd278b90fd990b24964adca3c1f2e6e87591e
Tim Poterba <tpoterba@gmail.com>
2019-06-14 16:00:20 -0400

    [test] move test hail-ci files to hail-common; make public (#6358)

--- hail/python/cluster-tests/cluster-sanity-check.py
+++ hail/python/cluster-tests/cluster-sanity-check.py
@@ -5,7 +5,7 @@ mt = hl.import_vcf('gs://hail-1kg/1kg_coreexome.vcf.bgz', min_partitions=16)
 mt = mt.annotate_rows(x = 5)
 mt._force_count_rows()
 
-mt = hl.import_bgen('gs://hail-ci/example.8bits.bgen', entry_fields=['GT'])
+mt = hl.import_bgen('gs://hail-common/test-resources/example.8bits.bgen', entry_fields=['GT'])
 mt._force_count_rows()
 
 bm = BlockMatrix.random(10, 11, block_size=32)
