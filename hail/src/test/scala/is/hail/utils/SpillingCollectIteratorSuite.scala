b3023a5508ad5223602bdb85ee917252c1bf759f
Daniel King <dking@broadinstitute.org>
2019-06-19 17:50:03 -0400

    hasNext takes no argument list

--- hail/src/test/scala/is/hail/utils/SpillingCollectIteratorSuite.scala
+++ hail/src/test/scala/is/hail/utils/SpillingCollectIteratorSuite.scala
@@ -7,9 +7,9 @@ class SpillingCollectIteratorSuite extends SparkSuite {
   @Test def addOneElement() {
     val array = (0 to 100000).toArray
     val sci = SpillingCollectIterator(sc.parallelize(array, 10000), 100)
-    assert(sci.hasNext())
+    assert(sci.hasNext)
     assert(sci.next() == 0)
-    assert(sci.hasNext())
+    assert(sci.hasNext)
     assert(sci.next() == 1)
     assert(sci.toArray == (2 to 100000))
   }
