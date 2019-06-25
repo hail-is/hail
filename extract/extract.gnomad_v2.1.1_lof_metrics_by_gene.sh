2e183d46df257912ed4bb79dfccfa9057eaf32e6
Brandon Franco <bfranco@broadinstitute.org>
2019-06-25 09:46:45 -0400

    Fixed extract gnomad_v2.1.1_lof_metrics

--- extract/extract.gnomad_v2.1.1_lof_metrics_by_gene.sh
+++ extract/extract.gnomad_v2.1.1_lof_metrics_by_gene.sh
@@ -1,7 +1,9 @@
 
 #!/bin/bash
+set -ex
 
-wget ftp://console.cloud.google.com/storage/browser/gnomad-public/release/2.1.1/constraint/gnomad.v2.1.1.lof_metrics.by_gene.txt.bgz | 
-zcat | 
-bgzip -c > tofile
+wget -c -O - https://storage.googleapis.com/gnomad-public/release/2.1.1/constraint/gnomad.v2.1.1.lof_metrics.by_gene.txt.bgz | \
+zcat | \
+gzip -c > tofile
+
 
