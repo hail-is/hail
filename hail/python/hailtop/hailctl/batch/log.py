1f1df4cb47373c5e609d74455fea98ec6af978e2
John Compitello <j301600@gmail.com>
2019-06-27 14:18:57 -0400

    Logs command works now

--- hail/python/hailtop/hailctl/batch/log.py
+++ hail/python/hailtop/hailctl/batch/log.py
@@ -5,9 +5,21 @@ def init_parser(parser):
     parser.add_argument('job_id', type=int)
 
 def main(args, pass_through_args, client):
-    maybe_job= get_job_if_exists(client, args.batch_id, args.job_id)
+    maybe_job = get_job_if_exists(client, args.batch_id, args.job_id)
     if maybe_job is None:
         print("Job with id {} on batch {} not found".format(args.job_id, args.batch_id))
         return
 
-    print(maybe_job.log())
+    logs = maybe_job.log()
+
+    if 'input' in logs:
+        print("Input Logs:")
+        print(logs['input'])
+
+    if 'main' in logs:
+        print("Main Logs:")
+        print(logs['main'])
+
+    if 'output' in logs:
+        print("Output Logs:")
+        print(logs['output'])
