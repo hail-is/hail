3b021e9d9f2963a27eb48fc94dab0ed593b9ffd8
John Compitello <j301600@gmail.com>
2019-06-27 20:33:30 -0400

    hailctl batch (#6503)
    * Beginnings of hailctl batch
    
    * Hitting a wall about tokens
    
    * hailctl WIP
    
    * httpS was needed
    
    * Basic listing
    
    * list batches works well, just need attributes to finish it up
    
    * Describe works, outputting JSON
    
    * Get WIP
    
    * Deleted lingering file that was unused
    
    * First version of get is written
    
    * Delete works, unclosed client error hidden
    
    * Pretty printing json
    
    * Cancel and delete are now working
    
    * Filtering by job attributes works
    
    * Logs command works, output is not pretty
    
    * Logs command works now
    
    * Implemented wait
    
    * Addressed Tim's comments / required tabulate

---
+++ hail/python/hailtop/hailctl/batch/delete.py
@@ -0,0 +1,15 @@
+from .batch_cli_utils import get_batch_if_exists
+
+def init_parser(parser):
+    parser.add_argument('batch_id', type=int, help="ID number of batch to be deleted")
+
+def main(args, pass_through_args, client):
+    maybe_batch = get_batch_if_exists(client, args.batch_id)
+    if maybe_batch is None:
+        print(f"Batch with batch_id {args.batch_id} not found")
+        exit(1)
+
+    batch = maybe_batch
+
+    batch.delete()
+    print(f"Batch with batch_id {args.batch_id} was deleted successfully")
