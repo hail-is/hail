f67c535c11931dfb7fac0600bde4c2383a4b52d8
Tim Poterba <tpoterba@gmail.com>
2019-07-03 09:22:25 -0400

    [benchmark] Parameterize output formats, files, verbosity.
    Default format is now a table.

--- hail/python/hailtop/hailctl/dev/benchmark/cli.py
+++ hail/python/hailtop/hailctl/dev/benchmark/cli.py
@@ -1,6 +1,7 @@
 import argparse
-
-from . import initialize, run_all, run_pattern, run_single
+import sys
+import json
+from .utils import initialize, run_all, run_pattern, run_list, RunConfig
 
 
 def main(args_):
@@ -23,14 +24,56 @@ def main(args_):
     parser.add_argument("--log", "-l",
                         type=str,
                         help='Log file path')
+    parser.add_argument("--verbose", "-v",
+                        action="store_true",
+                        help="Print testing information to stderr in real time.")
+    parser.add_argument("--output", "-o",
+                        type=str,
+                        help="Output file path.")
+    parser.add_argument("--format", "-f",
+                        type=str,
+                        help="Output format.",
+                        default='table',
+                        choices=['table', 'text'])
 
     args = parser.parse_args(args_)
 
-    initialize(args.cores, args.log, args.n_iter)
+    initialize(args)
+
+    finalizers = []
+    if args.output:
+        out_file = open(args.output, 'w')
+        finalizers.append(lambda: out_file.close())
+    else:
+        out_file = None
+    writer = lambda s: print(s, end='', file=out_file)
+
+    run_data = {'cores': args.cores}
+    if args.format == 'table':
+        writer(f'#{json.dumps(run_data, separators=(",", ":"))}\n')
+        writer('Name\tMean\tMedian\tStDev\n')
+
+        def handler(stats):
+            writer(f'{stats["name"]}\t{stats["mean"]:.3f}\t{stats["median"]:.3f}\t{stats["stdev"]:.3f}\n')
+    else:
+        assert args.format == 'text'
+        writer(f'Run data:\n')
+        for k, v in run_data.items():
+            writer(f'    {k}: {v}\n')
+
+        def handler(stats):
+            writer(f'{stats["name"]}:\n')
+            for t in stats["times"]:
+                writer(f'    {t:.2f}s\n')
+            writer(f'    Mean {stats["mean"]:.2f}, Median {stats["median"]:.2f}\n')
+
+    config = RunConfig(args.n_iter, handler, args.verbose)
     if args.tests:
-        for test in args.tests.split(','):
-            run_single(test)
+        run_list(args.tests, config)
     if args.pattern:
-        run_pattern(args.pattern)
+        run_pattern(args, config)
     if not args.pattern and not args.tests:
-        run_all()
+        run_all(config)
+
+    for f in finalizers:
+        f()
