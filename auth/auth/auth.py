94605e0df82bea1056713988b857a7453979f737
Cotton Seed <cotton@alum.mit.edu>
2019-07-09 16:37:33 -0400

    wip

--- auth/auth/auth.py
+++ auth/auth/auth.py
@@ -2,12 +2,14 @@ import re
 import logging
 import asyncio
 import datetime
+from cryptography import fernet
 import aiohttp
 from aiohttp import web
+import aiohttp_session
+import aiohttp_session.cookie_storage
 import aiomysql
 import uvloop
 import base64
-import secrets
 
 import google.auth.transport.requests
 import google.oauth2.id_token
@@ -19,7 +21,6 @@ log = logging.getLogger('auth')
 
 uvloop.install()
 
-app = web.Application()
 routes = web.RouteTableDef()
 
 CLIENT_SECRET = '/auth-oauth2-client-secret/client_secret.json'
@@ -31,8 +32,10 @@ SCOPES = [
 
 
 def get_flow(state=None):
-  return google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
     CLIENT_SECRET, scopes=SCOPES, state=state)
+  flow.redirect_uri = 'https://auth.hail.is/oauth2callback'
+  return flow
 
 
 @routes.get('/')
@@ -44,44 +47,40 @@ async def get_index(request):
 async def login(request):
     flow = get_flow()
 
-    flow.redirect_uri = 'https://auth.hail.is/oauth2callback'
-
     authorization_url, state = flow.authorization_url(
         access_type='offline',
         include_granted_scopes='true')
 
-    resp = aiohttp.web.HTTPFound(authorization_url)
-    resp.set_cookie('state', state,
-                    # 5m
-                    max_age=300,
-                    secure=True,
-                    httponly=True)
-    # FIXME put next, state in the session: https://github.com/aio-libs/aiohttp-session
+    session = await aiohttp_session.get_session(request)
+    session['state'] = state
     next = request.query.get('next')
     if next:
-        resp.set_cookie('next', next,
-                        max_age=300,
-                        secure=True,
-                        httponly=True)
-    return resp
+        session['next'] = next
+    elif 'next' in session:
+        del session['next']
+
+    return aiohttp.web.HTTPFound(authorization_url)
 
 
 @routes.post('/logout')
 async def logout():
-    return {}
+    session = await aiohttp_session.get_session(request)
+    if 'session_id' in session:
+        del session['session_id']
+    return aiohttp.web.HTTPFound('https://auth.hail.is/login')
 
 
 @routes.get('/oauth2callback')
 async def callback(request):
-    authorization_response = str(request.url)
-    authorization_response = re.sub('^http://', 'https://', authorization_response)
+    session = await aiohttp_session.get_session(request)
 
-    state = request.cookies['state']
+    # FIXME check
+    state = session['state']
 
     flow = get_flow(state=state)
-    flow.redirect_uri = 'https://auth.hail.is/oauth2callback'
-    flow.fetch_token(authorization_response=authorization_response)
+    flow.fetch_token(authorization_response='https://auth.hail.is/oauth2callback')
 
+    # FIXME check
     decoded = google.oauth2.id_token.verify_oauth2_token(
         flow.credentials.id_token, google.auth.transport.requests.Request())
 
@@ -90,21 +89,26 @@ async def callback(request):
 
     session_id = secrets.token_bytes(32)
 
-    next = request.cookies.get('next', 'https://notebook2.hail.is')
-    resp = aiohttp.web.HTTPFound(next)
-    # clear state
-    resp.set_cookie('state', '', max_age=0, secure=True, httponly=True)
-    resp.set_cookie('next', '', max_age=0, secure=True, httponly=True)
-    resp.set_cookie('session', base64.b64encode(session_id).decode('ascii'),
-                    # 30d
-                    max_age=2592000,
-                    secure=True, httponly=True)
-    return resp
+    del session['state']
+    session['session_id'] = session_id
+    next = session.get('next', 'https://notebook2.hail.is')
+    if 'next' in session:
+        del session['next']
+    return aiohttp.web.HTTPFound(next)
 
 
-app.add_routes(routes)
+@routes.post('/log')
+async def logout():
+    session = await aiohttp_session.get_session(request)
+    log.info(f'session: {session}')
+    return web.Response(status=200)
 
 
 def run():
     configure_logging()
+    app = web.Application()
+    fernet_key = fernet.Fernet.generate_key()
+    secret_key = base64.urlsafe_b64decode(fernet_key)
+    aiohttp_session.setup(app, aiohttp_session.cookie_storage.EncryptedCookieStorage(secret_key, max_age=30 * 24 * 60 * 60))
+    app.add_routes(routes)
     web.run_app(app, host='0.0.0.0', port=5000)
