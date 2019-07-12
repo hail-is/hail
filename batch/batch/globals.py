7658ae393402a733b99cf8dff36260b99cb7bdff
Cotton Seed <hail-ci-leader@example.com>
2019-07-11 22:46:44 -0400

    fix valid state transitions

--- batch/batch/globals.py
+++ batch/batch/globals.py
@@ -4,9 +4,9 @@ states = {'Pending', 'Ready', 'Running', 'Cancelled', 'Error', 'Failed', 'Succes
 complete_states = ('Cancelled', 'Error', 'Failed', 'Success')
 
 valid_state_transitions = {
-    'Pending': {'Ready', 'Cancelled'},
-    'Ready': {'Pending', 'Running', 'Error', 'Cancelled'},
-    'Running': set(complete_states),
+    'Pending': {'Ready'},
+    'Ready': {'Running', 'Error', 'Cancelled'},
+    'Running': {'Ready', 'Cancelled', 'Error', 'Failed', 'Success'},
     'Cancelled': set(),
     'Error': set(),
     'Failed': set(),
