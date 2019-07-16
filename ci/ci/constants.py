7d0aa24843fd061e138ded017e3e6f7ea44e7357
John Compitello <j301600@gmail.com>
2019-07-16 10:37:03 -0400

    Sending the profile along so that it can be changed during development

--- ci/ci/constants.py
+++ ci/ci/constants.py
@@ -23,23 +23,4 @@ AUTHORIZED_USERS = {
     'GreatBrando',
     'johnc1231',
     'iitalics'
-}   
-
-profiles = {
-    "batch_test": [
-        'default_ns',
-        'deploy_batch_sa',
-        'batch_pods_ns',
-        'base_image',
-        'create_accounts',
-        'batch_image',
-        'batch_database',
-        'create_batch_tables_image',
-        'create_batch_tables',
-        'create_batch_tables2',
-        'deploy_batch',
-        'deploy_batch_pods',
-        'test_batch_image',
-        'test_batch'
-    ]
 }
