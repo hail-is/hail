f1708bf7f1529e03130410823a78a7e6faec2030
Brandon Franco <bboyfranco@att.net>
2019-07-17 21:09:26 -0400

    update db.py to be compatible with genecode and gnomad datasets

--- hail/python/hail/experimental/db.py
+++ hail/python/hail/experimental/db.py
@@ -13,15 +13,16 @@ class DB:
             r = requests.get("https://www.googleapis.com/storage/v1/b/hail-common/o/annotationdb%2f1%2fannotation_db.json?alt=media")
             j = r.json()
             
-            DB._annotation_dataset_urls = {(x["name"], x["reference_genome"]): (x["path"], x["gene_key"]) for x in j}
+            DB._annotation_dataset_urls = {(x["name"], x.get("reference_genome")): x["path"] for x in j}
+            DB._geneDict_dataset_urls = { x["name"]: (x["gene_key"]) for x in j}
         
-        return DB._annotation_dataset_urls
+        return DB._annotation_dataset_urls, DB._geneDict_dataset_urls
     
     def annotate_rows_db(self,mt,*names):
-            """
+        """
             Examples
             --------
-            Annotates rows based on keyword implementation of annotation name. 
+            Annotates rows based on keyword implementation of annotation name.
             The user can type in multiple annotation names when attaching to their datasets.
             
             >>> import hail as hl
@@ -29,29 +30,35 @@ class DB:
             >>> db = hl.annotation_database(config)
             
             # adds the vep, CADD, and gnomAD annotations to mt
-            >>> mt = db.annotate_rows_db(mt, 'vep', 'CADD', 'gnomAD')  
+            >>> mt = db.annotate_rows_db(mt, 'vep', 'CADD', 'gnomAD')
             ...
             
             Parameters
             ----------
-            names: Keyword argument of the annotation. 
+            names: Keyword argument of the annotation.
             Can include multiple annotations at one time.
             
             Returns
             -------
             :class:`.MatrixTable`
             """
-        d = DB.annotation_dataset_urls()
+        d, geneDict = DB.annotation_dataset_urls()
         reference_genome = mt.row_key.locus.dtype.reference_genome.name
         for name in names:
-            url, gene_key = d[(name,reference_genome)]
+            gene_key = geneDict[(name)]
             if gene_key is True:
-                gene_url, _ = d[('gencode', reference_genome)]
-                t = hl.read_table(gene_url)
-                mt = mt.annotate_rows(gene_name=t[mt.row_key].gene_name)
-                mt = mt.annotate_rows(**{name:t[mt.gene_name]})
+                gene_url = d[('gencode', reference_genome)]
+                t  = hl.read_table(gene_url)
+                mt = mt.annotate_rows(gene_name=t[mt.locus].gene_name)
+                url = d[(name, None)]
+                t2 = hl.read_table(url)
+                mt = mt.annotate_rows(**{name:t2[mt.gene_name]})
                 mt = mt.drop('gene_name')
             else:
+                url = d[(name,reference_genome)]
                 t = hl.read_table(url)
-                mt = mt.annotate_rows(**{name:t[mt.row_key]})
+                if len(t.key) > 1:
+                    mt = mt.annotate_rows(**{name:t[mt.row_key]})
+                else:
+                    mt = mt.annotate_rows(**{name:t[mt.locus]})
         return mt
