d8bcb3952607e73348ac0da42fd6f15f55cedc7e
Milo Turner <iitalics@gmail.com>
2019-07-22 13:55:26 -0400

    create BlockMatrixToTableFunction class

--- hail/src/main/scala/is/hail/expr/ir/functions/RelationalFunctions.scala
+++ hail/src/main/scala/is/hail/expr/ir/functions/RelationalFunctions.scala
@@ -33,6 +33,12 @@ abstract class MatrixToTableFunction {
   def lower(): Option[TableToTableFunction] = None
 }
 
+abstract class BlockMatrixToTableFunction {
+  def typ(bmType: BlockMatrixType, auxType: Type): TableType
+
+  def execute(ctx: ExecuteContext, bm: BlockMatrix, aux: Any): TableValue
+}
+
 case class WrappedMatrixToTableFunction(
   function: MatrixToTableFunction,
   colsFieldName: String,
@@ -146,6 +152,8 @@ object RelationalFunctions {
   def lookupMatrixToTable(config: String): MatrixToTableFunction = extractTo[MatrixToTableFunction](config)
 
   def lookupTableToTable(config: String): TableToTableFunction = extractTo[TableToTableFunction](config)
+
+  def lookupBlockMatrixToTable(config: String): BlockMatrixToTableFunction = extractTo[BlockMatrixToTableFunction](config)
 
   def lookupTableToValue(config: String): TableToValueFunction = extractTo[TableToValueFunction](config)
 
