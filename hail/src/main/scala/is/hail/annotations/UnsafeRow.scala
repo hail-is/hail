7da2392440e003edfd92de32a4e3a161fbf062ff
Tim Poterba <tpoterba@gmail.com>
2019-07-23 06:45:15 -0400

    s/fundamentalType/representation

--- hail/src/main/scala/is/hail/annotations/UnsafeRow.scala
+++ hail/src/main/scala/is/hail/annotations/UnsafeRow.scala
@@ -54,7 +54,7 @@ object UnsafeRow {
     new String(readBinary(region, boff, t.fundamentalType))
 
   def readLocus(region: Region, offset: Long, t: PLocus): Locus = {
-    val ft = t.fundamentalType.asInstanceOf[PStruct]
+    val ft = t.representation.asInstanceOf[PStruct]
     Locus(
       readString(region, ft.loadField(region, offset, 0), ft.types(0).asInstanceOf[PString]),
       region.loadInt(ft.loadField(region, offset, 1)))
