8c9b82ff07fab82003c8ff245446e467b75bb0d4
Cotton Seed <hail-ci-leader@example.com>
2019-07-25 21:40:45 -0400

    wip

--- hack/task-shutdown.sh
+++ hack/task-shutdown.sh
@@ -4,4 +4,4 @@ set -ex
 MASTER=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/master")
 TOKEN=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/token")
 
-curl -X POST -d "{\"token\":\"$TOKEN\"}" http://$MASTER/shutdown
+curl -X POST -d "{\"token\":\"$TOKEN\"}" http://$MASTER:5000/shutdown
