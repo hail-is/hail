1faf046895cbf6bb099c06429bff2b6dc6b6b776
Jackie Goldstein <jigold@broadinstitute.org>
2019-08-02 15:53:00 -0400

    remove docker dependency

--- batch2/batch/agent.py
+++ batch2/batch/agent.py
@@ -1,12 +1,9 @@
-import docker
 import uvloop
 from aiohttp import web
 
 from .utils import jsonify
 
 uvloop.install()
-
-dc = docker.from_env()
 
 app = web.Application()
 routes = web.RouteTableDef()
@@ -15,12 +12,6 @@ routes = web.RouteTableDef()
 @routes.get('/healthcheck')
 async def get_healthcheck(request):  # pylint: disable=W0613
     return jsonify({})
-
-
-@routes.get('/helloworld')
-async def run_helloworld(request):  # pylint: disable=W0613
-    container = dc.containers.run('hello-world', detach=True)
-    return jsonify({'name': container.name})
 
 
 app.add_routes(routes)
