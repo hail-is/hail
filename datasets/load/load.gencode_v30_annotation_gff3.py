59af015c5bbfa605d1e684d5639e60ef933ac61e
Brandon Franco <bboyfranco@att.net>
2019-08-02 15:52:00 -0400

    update load.gencode_v30_annotation_gff3.py
    Made changes to gencode_v30_annotation_gff3.py

--- datasets/load/load.gencode_v30_annotation_gff3.py
+++ datasets/load/load.gencode_v30_annotation_gff3.py
@@ -3,21 +3,16 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-v', required=True, help='Dataset version.')
 parser.add_argument('-b', required=True, choices=['GRCh37', 'GRCh38'], help='gencode build.')
 args = parser.parse_args()
 
-version = args.v
 build = args.b
 
-if version == 'v19':
+t = hl.import_table('/Users/bfranco/downloads/gencode.v31.annotation.gff3',no_header=True, impute=True, comment=('#'))
 
-else:
-    t = hl.import_table('/Users/bfranco/downloads/gencode.v31.annotation.gff3',no_header=True, impute=True, comment=('#'))
+t2 = t.annotate(GFF_Columns = t.f8.split(";").map(lambda x: x.split("=")))
 
-    t2 = t.annotate(GFF_Columns = t.f8.split(";").map(lambda x: x.split("=")))
-
-    t2 = t2.filter(t2.f2 == "gene")
+t2 = t2.filter(t2.f2 == "gene")
 
 if build == 'GRCh37':
     t2 = t2.annotate(f0 = t2.f0.replace("chr", ""))
@@ -36,4 +31,4 @@ t2 = t2.annotate(type=t2.f2, gene_score=t2.f5, gene_strand=t2.f6, gene_phase=t2.
 
 t2 = t2.drop(t2.GFF_Columns, t2.f8, t2.f0, t2.f1, t2.f2, t2.f3, t2.f4, t2.f5, t2.f6, t2.f7)
 
-t2.write(f'gs://hail-common/datasets/1/gencode.{version}.annotation.{build}.ht', overwrite=True)
+t2.write(f'gs://hail-common/datasets/1/gencode.v31.annotation.{build}.ht', overwrite=True)
