b1b12667eaf8d247f993c4631db5e5a8faf29a75
Daniel King <dking@broadinstitute.org>
2019-08-07 19:53:25 -0400

    [notebook] asyncio notebook leader

--- notebook/notebook/notebook.py
+++ notebook/notebook/notebook.py
@@ -1,50 +1,54 @@
-import gevent
-# must happen before anything else
-from gevent import monkey; monkey.patch_all()
-from flask import Flask, session, redirect, render_template, request
-from flask_sockets import Sockets
-import flask
-import kubernetes as kube
+import aiohttp
+import aiohttp_jinja2
+import aiohttp_session
+import aiohttp_session.cookie_storage
+import asyncio
+import base64
+import jinja2
+import kubernetes_asyncio as kube
 import logging
 import os
 import re
-import requests
-import time
 import uuid
+import uvloop
 
-fmt = logging.Formatter(
-   # NB: no space after levelname because WARNING is so long
-   '%(levelname)s\t| %(asctime)s \t| %(filename)s \t| %(funcName)s:%(lineno)d | '
-   '%(message)s')
+from cryptography import fernet
+from pythonjsonlogger import jsonlogger
 
-fh = logging.FileHandler('notebook.log')
-fh.setLevel(logging.INFO)
-fh.setFormatter(fmt)
 
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-ch.setFormatter(fmt)
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+        log_record['funcNameAndLine'] = "{}:{}".format(record.funcName, record.lineno)
 
+
+def configure_logging():
+    fmt = CustomJsonFormatter('(levelname) (asctime) (filename) (funcNameAndLine) (message)')
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(fmt)
+
+    logging.basicConfig(handlers=[stream_handler], level=logging.INFO)
+
+
+configure_logging()
 log = logging.getLogger('notebook')
-log.setLevel(logging.INFO)
-logging.basicConfig(
-    handlers=[fh, ch],
-    level=logging.INFO)
 
-if 'BATCH_USE_KUBE_CONFIG' in os.environ:
-    kube.config.load_kube_config()
-else:
-    kube.config.load_incluster_config()
 k8s = kube.client.CoreV1Api()
+app = aiohttp.web.Application(client_max_size=None)
+routes = aiohttp.web.RouteTableDef()
 
-app = Flask(__name__)
-sockets = Sockets(app)
+uvloop.install()
+
 
 def read_string(f):
     with open(f, 'r') as f:
         return f.read().strip()
 
-KUBERNETES_TIMEOUT_IN_SECONDS = float(os.environ.get('KUBERNETES_TIMEOUT_IN_SECONDS', 5.0))
+
+KUBERNETES_TIMEOUT_IN_SECONDS = float(
+    os.environ.get('KUBERNETES_TIMEOUT_IN_SECONDS', 5.0))
 app.secret_key = read_string('/notebook-secrets/secret-key')
 PASSWORD = read_string('/notebook-secrets/password')
 ADMIN_PASSWORD = read_string('/notebook-secrets/admin-password')
@@ -64,7 +68,7 @@ except FileNotFoundError as e:
         "containing the name of the docker image to use for worker pods.") from e
 
 
-def start_pod(jupyter_token, image):
+async def start_pod(jupyter_token, image):
     pod_id = uuid.uuid4().hex
     service_spec = kube.client.V1ServiceSpec(
         selector={
@@ -80,7 +84,7 @@ def start_pod(jupyter_token, image):
                 'hail.is/notebook-instance': INSTANCE_ID,
                 'uuid': pod_id}),
         spec=service_spec)
-    svc = k8s.create_namespaced_service(
+    svc = await k8s.create_namespaced_service(
         'default',
         service_template,
         _request_timeout=KUBERNETES_TIMEOUT_IN_SECONDS
@@ -114,78 +118,82 @@ def start_pod(jupyter_token, image):
                 'uuid': pod_id,
             }),
         spec=pod_spec)
-    pod = k8s.create_namespaced_pod(
+    pod = await k8s.create_namespaced_pod(
         'default',
         pod_template,
         _request_timeout=KUBERNETES_TIMEOUT_IN_SECONDS)
     return svc, pod
 
 
-def external_url_for(path):
-    # NOTE: nginx strips https and sets X-Forwarded-Proto: https, but
-    # it is not used by request.url or url_for, so rewrite the url and
-    # set _scheme='https' explicitly.
-    protocol = request.headers.get('X-Forwarded-Proto', None)
-    url = flask.url_for('root', _scheme=protocol, _external='true')
-    return url + path
+@routes.get('/healthcheck')
+async def healthcheck():
+    return aiohttp.web.Response()
 
 
-@app.route('/healthcheck')
-def healthcheck():
-    return '', 200
-
-
-@app.route('/')
-def root():
+@routes.get('/', name='root')
+@aiohttp_jinja2.template('index.html')
+async def root(request):
+    session = await aiohttp_session.get_session(request)
     if 'svc_name' not in session:
         log.info(f'no svc_name found in session {session.keys()}')
-        return render_template('index.html',
-                               form_action_url=external_url_for('new'),
-                               images=list(WORKER_IMAGES),
-                               default='hands-on-with-hail')
+        return {'form_action_url': request.app.router['new'].url_for(),
+                'images': list(WORKER_IMAGES),
+                'default': 'gew2019'}
     svc_name = session['svc_name']
     jupyter_token = session['jupyter_token']
-    log.info('redirecting to ' + external_url_for(f'instance/{svc_name}/?token={jupyter_token}'))
-    return redirect(external_url_for(f'instance/{svc_name}/?token={jupyter_token}'))
+    url = (request.app.router['root'].url_for() +
+           f'instance/{svc_name}/?token={jupyter_token}')
+    log.info('redirecting to ' + url)
+    raise aiohttp.web.HTTPFound(url)
 
 
-@app.route('/new', methods=['GET'])
-def new_get():
+@routes.get('/new', name='new')
+async def new_get(request):
+    session = await aiohttp_session.get_session(request)
     pod_name = session.get('pod_name')
     svc_name = session.get('svc_name')
     if pod_name:
-        delete_worker_pod(pod_name, svc_name)
+        await delete_worker_pod(pod_name, svc_name)
     session.clear()
-    return redirect(external_url_for('/'))
+    raise aiohttp.web.HTTPFound(
+        request.app.router['root'].url_for())
 
-@app.route('/new', methods=['POST'])
-def new_post():
+
+@routes.post('/new')
+async def new_post(request):
+    session = await aiohttp_session.get_session(request)
     log.info('new received')
     password = request.form['password']
     image = request.form['image']
     if password != PASSWORD or image not in WORKER_IMAGES:
-        return '403 Forbidden', 403
-    jupyter_token = uuid.uuid4().hex  # FIXME: probably should be cryptographically secure
-    svc, pod = start_pod(jupyter_token, WORKER_IMAGES[image])
+        raise aiohttp.web.HTTPForbidden()
+    jupyter_token = base64.urlsafe_b64decode(fernet.Fernet.generate_key())
+    svc, pod = await start_pod(jupyter_token, WORKER_IMAGES[image])
     session['svc_name'] = svc.metadata.name
     session['pod_name'] = pod.metadata.name
     session['jupyter_token'] = jupyter_token
-    return redirect(external_url_for(f'wait'))
+    raise aiohttp.web.HTTPFound(
+        request.app.router['wait'].url_for())
 
-@app.route('/wait', methods=['GET'])
-def wait_webpage():
-    return render_template('wait.html')
 
-@app.route('/auth/<requested_svc_name>')
-def auth(requested_svc_name):
+@routes.get('/wait', name='wait')
+@aiohttp_jinja2.template('wait.html')
+async def wait_webpage(request):
+    return {}
+
+
+@routes.get('/auth/{requested_svc_name}')
+async def auth(request):
+    session = await aiohttp_session.get_session(request)
+    requested_svc_name = request.match_info['requested_svc_name']
     approved_svc_name = session.get('svc_name')
     if approved_svc_name and approved_svc_name == requested_svc_name:
-        return '', 200
-    return '', 403
+        return aiohttp.web.Response()
+    raise aiohttp.web.HTTPForbidden()
 
 
-def get_all_workers():
-    workers = k8s.list_namespaced_pod(
+async def get_all_workers():
+    workers = await k8s.list_namespaced_pod(
         namespace='default',
         watch=False,
         label_selector='app=notebook-worker',
@@ -193,7 +201,7 @@ def get_all_workers():
     workers_and_svcs = []
     for w in workers.items:
         uuid = w.metadata.labels['uuid']
-        svcs = k8s.list_namespaced_service(
+        svcs = await k8s.list_namespaced_service(
             namespace='default',
             watch=False,
             label_selector='uuid=' + uuid,
@@ -207,45 +215,54 @@ def get_all_workers():
     return workers_and_svcs
 
 
-@app.route('/workers')
-def workers():
+@routes.get('/workers', name='workers')
+@aiohttp_jinja2.template('workers.html')
+async def workers(request):
+    session = await aiohttp_session.get_session(request)
     if not session.get('admin'):
-        return redirect(external_url_for('admin-login'))
-    workers_and_svcs = get_all_workers()
-    return render_template('workers.html',
-                           workers=workers_and_svcs,
-                           workers_url=external_url_for('workers'),
-                           leader_instance=INSTANCE_ID)
+        raise aiohttp.web.HTTPFound(
+            request.app.router['admin-login'].url_for())
+    workers_and_svcs = await get_all_workers()
+    return {'workers': workers_and_svcs,
+            'workers_url': request.app.router['workers'].url_for(),
+            'leader_instance': INSTANCE_ID}
 
 
-@app.route('/workers/<pod_name>/<svc_name>/delete')
-def workers_delete(pod_name, svc_name):
+@routes.get('/workers/{pod_name}/{svc_name}/delete')
+async def workers_delete(request):
+    session = await aiohttp_session.get_session(request)
+    pod_name = request.match_info['pod_name']
+    svc_name = request.match_info['svc_name']
     if not session.get('admin'):
-        return redirect(external_url_for('admin-login'))
-    delete_worker_pod(pod_name, svc_name)
-    return redirect(external_url_for('workers'))
+        raise aiohttp.web.HTTPFound(
+            request.app.router['admin-login'].url_for())
+    await delete_worker_pod(pod_name, svc_name)
+    raise aiohttp.web.HTTPFound(request.app.router['workers'].url_for())
 
 
-@app.route('/workers/delete-all-workers', methods=['POST'])
-def delete_all_workers():
+@routes.post('/workers/delete-all-workers')
+async def delete_all_workers(request):
+    session = await aiohttp_session.get_session(request)
     if not session.get('admin'):
-        return redirect(external_url_for('admin-login'))
-    workers_and_svcs = get_all_workers()
-    for pod, svc in workers_and_svcs:
+        raise aiohttp.web.HTTPFound(
+            request.app.router['admin-login'].url_for())
+    workers_and_svcs = await get_all_workers()
+    await asyncio.gather(*[
         delete_worker_pod(pod.metadata.name, svc.metadata.name)
-    return redirect(external_url_for('workers'))
+        for pod, svc in workers_and_svcs])
+    raise aiohttp.web.HTTPFound(request.app.router['workers'].url_for())
 
 
-def delete_worker_pod(pod_name, svc_name):
+async def delete_worker_pod(pod_name, svc_name):
     try:
-        k8s.delete_namespaced_pod(
+        await k8s.delete_namespaced_pod(
             pod_name,
             'default',
             _request_timeout=KUBERNETES_TIMEOUT_IN_SECONDS)
     except kube.client.rest.ApiException as e:
         log.info(f'pod {pod_name} or associated service already deleted {e}')
     try:
-        k8s.delete_namespaced_service(
+        await k8s.delete_namespaced_service(
             svc_name,
             'default',
             _request_timeout=KUBERNETES_TIMEOUT_IN_SECONDS)
@@ -253,38 +270,46 @@ def delete_worker_pod(pod_name, svc_name):
         log.info(f'service {svc_name} (for pod {pod_name}) already deleted {e}')
 
 
-@app.route('/admin-login', methods=['GET'])
-def admin_login():
-    return render_template('admin-login.html',
-                           form_action_url=external_url_for('admin-login'))
+@routes.get('/admin-login', name='admin-login')
+@aiohttp_jinja2.template('admin-login.html')
+async def admin_login(request):
+    return {'form_action_url': request.app.router['workers'].url_for()}
 
 
-@app.route('/admin-login', methods=['POST'])
-def admin_login_post():
-    if request.form['password'] != ADMIN_PASSWORD:
-        return '403 Forbidden', 403
+@routes.post('/admin-login')
+async def admin_login_post(request):
+    session = await aiohttp_session.get_session(request)
+    form = await request.post()
+    if form['password'] != ADMIN_PASSWORD:
+        raise aiohttp.web.HTTPForbidden()
     session['admin'] = True
-    return redirect(external_url_for('workers'))
+    raise aiohttp.web.HTTPFound(request.app.router['workers'].url_for())
 
 
-@app.route('/worker-image')
-def worker_image():
-    return '\n'.join(WORKER_IMAGES.values()), 200
+@routes.get('/worker-image')
+async def worker_image(request):
+    del request
+    return aiohttp.web.Response(text='\n'.join(WORKER_IMAGES.values()))
 
 
-@sockets.route('/wait')
-def wait_websocket(ws):
+@routes.get('/wait')
+async def wait_websocket(request):
+    session = await aiohttp_session.get_session(request)
+    ws = aiohttp.web.WebSocketResponse()
+    await ws.prepare(request)
     pod_name = session['pod_name']
     svc_name = session['svc_name']
     jupyter_token = session['jupyter_token']
     log.info(f'received wait websocket for {svc_name} {pod_name}')
-    # wait for instance ready
     while True:
         try:
-            response = requests.head(f'https://notebook.hail.is/instance-ready/{svc_name}/',
-                                     timeout=1)
+            response = app['client_session'].head(
+                f'https://notebook.hail.is/instance-ready/{svc_name}/',
+                timeout=1)
             if response.status_code < 500:
-                log.info(f'HEAD on jupyter succeeded for {svc_name} {pod_name} response: {response}')
+                log.info(
+                    f'HEAD on jupyter succeeded for {svc_name} {pod_name} '
+                    f'response: {response}')
                 # if someone responds with a 2xx, 3xx, or 4xx, the notebook
                 # server is alive and functioning properly (in particular, our
                 # HEAD request will return 405 METHOD NOT ALLOWED)
@@ -292,15 +317,39 @@ def wait_websocket(ws):
             # somewhat unusual, means the gateway had an error before we
             # timed out, usually means the gateway itself is broken
             log.info(f'HEAD on jupyter failed for {svc_name} {pod_name} response: {response}')
-        except requests.exceptions.Timeout as e:
-            log.info(f'GET on jupyter failed for {svc_name} {pod_name}')
-        gevent.sleep(1)
-    ws.send(external_url_for(f'instance/{svc_name}/?token={jupyter_token}'))
+        except Exception as e:
+            log.info(f'GET on jupyter failed for {svc_name} {pod_name} {e}')
+        await asyncio.sleep(1)
+    await ws.send_str(
+        request.app.router['root'].url_for() +
+        f'instance/{svc_name}/?token={jupyter_token}')
+    await ws.close()
     log.info(f'notification sent to user for {svc_name} {pod_name}')
+    return ws
 
 
 if __name__ == '__main__':
-    from gevent import pywsgi
-    from geventwebsocket.handler import WebSocketHandler
-    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler, log=log)
-    server.serve_forever()
+    log.info(f'starting')
+    aiohttp_jinja2.setup(
+        app,
+        loader=jinja2.FileSystemLoader(os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            'templates')))
+    log.info(f'jinja2 up')
+    app.add_routes(routes)
+    log.info(f'routes registered')
+    kube.config.load_incluster_config()
+    log.info(f'k8s config loaded')
+    app['client_session'] = aiohttp.ClientSession()
+    log.info(f'aiohttp client session created')
+    app.on_cleanup.append(app['client_session'].close)
+    log.info(f'close registered')
+    fernet_key = fernet.Fernet.generate_key()
+    log.info(f'key generated')
+    secret_key = base64.urlsafe_b64decode(fernet_key)
+    log.info(f'secret key genereated')
+    aiohttp_session.setup(
+        app,
+        aiohttp_session.cookie_storage.EncryptedCookieStorage(secret_key))
+    log.info(f'session setup')
+    aiohttp.web.run_app(app, host='0.0.0.0', port=5000)
