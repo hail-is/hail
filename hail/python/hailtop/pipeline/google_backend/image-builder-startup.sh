b5d568d10b457b117387b8b33fc85ea2c9474a33
Cotton Seed <hail-ci-leader@example.com>
2019-08-09 18:35:34 -0400

    add missing dependency to image build

--- hail/python/hailtop/pipeline/google_backend/image-builder-startup.sh
+++ hail/python/hailtop/pipeline/google_backend/image-builder-startup.sh
@@ -4,6 +4,6 @@ set -ex
 apt -y update
 apt -y dist-upgrade
 apt -y install emacs-nox docker.io python3-pip
-python3 -m pip install python-json-logger gcsfs==0.2.1 hurry.filesize==0.9 aiohttp sortedcontainers google-api-python-client google-cloud uvloop
+python3 -m pip install python-json-logger gcsfs==0.2.1 hurry.filesize==0.9 aiohttp sortedcontainers google-api-python-client google-cloud google-cloud-logging uvloop
 
 shutdown -h now
