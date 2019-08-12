20f2fa1d1e7b075ce6d012cbbe5ebd8e7a375c94
Cotton Seed <hail-ci-leader@example.com>
2019-08-12 16:17:47 -0400

    fix

--- hail/python/hailtop/pipeline/google_backend/worker-startup.sh
+++ hail/python/hailtop/pipeline/google_backend/worker-startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
 
-gsutil -m cp gs://hail-common/dev/pipeline/run-worker.sh gs://hail-common/dev/pipeline/worker.py /
+gsutil -m cp gs://hail-common/dev2/pipeline/run-worker.sh gs://hail-common/dev2/pipeline/worker.py /
 
 nohup /bin/bash run-worker.sh >run-worker.log 2>&1 &
