37e5f1e8fba4e39a04d639714c7447140b13348b
John Compitello <j301600@gmail.com>
2019-08-21 14:16:11 -0400

    Copied over the useful bits from ComplexPType

--- hail/src/main/scala/is/hail/expr/types/physical/PNDArray.scala
+++ hail/src/main/scala/is/hail/expr/types/physical/PNDArray.scala
@@ -1,6 +1,6 @@
 package is.hail.expr.types.physical
 
-import is.hail.annotations.CodeOrdering
+import is.hail.annotations.{CodeOrdering, UnsafeOrdering}
 import is.hail.expr.Nat
 import is.hail.expr.ir.EmitMethodBuilder
 import is.hail.expr.types.virtual.TNDArray
@@ -12,9 +12,7 @@ final case class PNDArray(elementType: PType, nDims: Int, override val required:
   override def _toPretty = s"NDArray[$elementType,$nDims]"
 
   override def codeOrdering(mb: EmitMethodBuilder, other: PType): CodeOrdering = throw new UnsupportedOperationException
-
-  //override def fundamentalType: PType = representation.fundamentalType
-
+  
   val representation: PStruct = {
     PStruct(required,
       ("flags", PInt32Required),
@@ -23,4 +21,12 @@ final case class PNDArray(elementType: PType, nDims: Int, override val required:
       ("strides", PTuple(true, Array.tabulate(nDims)(_ => PInt64Required):_*)),
       ("data", PArray(elementType, required = true)))
   }
+
+  override def byteSize: Long = representation.byteSize
+
+  override def alignment: Long = representation.alignment
+
+  override def unsafeOrdering(): UnsafeOrdering = representation.unsafeOrdering()
+
+  override def fundamentalType: PType = representation.fundamentalType
 }
