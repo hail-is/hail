47fafe7c0f21834a6bdbb5ef9bdd9f602d65c028
cseed <cotton@alum.mit.edu>
2019-08-23 10:12:46 -0400

    fix hailctl dev deploy client (#6926)

--- hail/python/hailtop/hailctl/dev/deploy/cli.py
+++ hail/python/hailtop/hailctl/dev/deploy/cli.py
@@ -1,28 +1,27 @@
 import asyncio
 import aiohttp
-from asyncinit import asyncinit
 
 from hailtop.gear.auth.hailjwt import find_token
 
 
 def init_parser(parser):
     parser.add_argument("--branch", "-b", type=str,
-                        help="Fully-qualified branch, e.g., hail-is/hail:feature.")
+                        help="Fully-qualified branch, e.g., hail-is/hail:feature.", required=True)
     parser.add_argument("--steps", "-s", type=str,
-                        help="Comma-separated list of steps to run.")
+                        help="Comma-separated list of steps to run.", required=True)
 
 
-@asyncinit
 class CIClient:
-    async def __init__(self):
+    def __init__(self):
+        self._session = None
+
+    async def __aenter__(self):
         token = find_token()
         self._session = aiohttp.ClientSession(
             raise_for_status=True,
             timeout=aiohttp.ClientTimeout(total=60),
             headers={"Authorization": f"Bearer {token}"})
-
-    async def __aenter__(self):
-        pass
+        return self
 
     async def __aexit__(self, exc_type, exc, tb):
         await self.close()
@@ -38,8 +37,8 @@ class CIClient:
             'steps': steps
         }
         async with self._session.post(
-                'https://ci.hail.is/api/v1alpha/dev_test_branch/', json=data) as resp:
-            resp_data = resp.json()
+                'https://ci.hail.is/api/v1alpha/dev_deploy_branch/', json=data) as resp:
+            resp_data = await resp.json()
             return resp_data['batch_id']
 
 
@@ -48,7 +47,7 @@ async def submit(args):
     steps = [s.strip() for s in steps]
     steps = [s for s in steps if s]
     async with CIClient() as ci_client:
-        batch_id = ci_client.dev_deploy_branch(args.branch, steps)
+        batch_id = await ci_client.dev_deploy_branch(args.branch, steps)
         print(f'Created deploy batch, see https://ci.hail.is/batches/{batch_id}')
 
 
