hail-is/hail

commit 7595f21c6b2e629e030dc3919912e4bac368fcba

Add --beta argument to force gcloud beta (#6879)

hail/python/hailtop/hailctl/dataproc/start.py
@@ -177,7 +177,7 @@ def main(args, pass_through_args):
     # command to start cluster
     cmd = conf.get_command(args.name)
 
-    if args.max_idle or args.max_age:
+    if args.beta:
         cmd.insert(1, 'beta')
     if args.max_idle:
         cmd.append('--max-idle={}'.format(args.max_idle))

hail/python/hailtop/hailctl/dataproc/cli.py
@@ -16,6 +16,12 @@ def parser():
     main_parser = argparse.ArgumentParser(
         prog='hailctl dataproc',
         description='Manage and monitor Hail deployments.')
+
+    main_parser.add_argument(
+        '--beta',
+        action='store_true',
+        help='Force use of `beta` in gcloud commands')
+
     subparsers = main_parser.add_subparsers()
 
     start_parser = subparsers.add_parser(
@@ -38,17 +44,14 @@ def parser():
         'stop',
         help='Shut down a Dataproc cluster.',
         description='Shut down a Dataproc cluster.')
-
     list_parser = subparsers.add_parser(
         'list',
         help='List active Dataproc clusters.',
         description='List active Dataproc clusters.')
-
     modify_parser = subparsers.add_parser(
         'modify',
         help='Modify active Dataproc clusters.',
         description='Modify active Dataproc clusters.')
-
     describe_parser = subparsers.add_parser(
         'describe',
         help='Gather information about a hail file (including the schema)',
@@ -81,8 +84,9 @@ def parser():
 
 
 def main(args):
+    p = parser()
     if not args:
-        parser().print_help()
+        p.print_help()
         sys.exit(0)
     jmp = {
         'start': start,
@@ -95,5 +99,8 @@ def main(args):
         'describe': describe,
     }
 
-    args, pass_through_args = parser().parse_known_args(args=args)
+    args, pass_through_args = p.parse_known_args(args=args)
+    if "module" not in args:
+        p.error('positional argument required')
+
     jmp[args.module].main(args, pass_through_args)

hail/python/hailtop/hailctl/dataproc/modify.py
@@ -35,18 +35,18 @@ def main(args, pass_through_args):
         modify_args.append('--max-idle={}'.format(args.max_idle))
 
     if modify_args:
-        cmd = [
-                  'gcloud',
-                  'dataproc',
-                  'clusters',
-                  'update',
-                  args.name] + modify_args
+        cmd = ['gcloud',
+               'dataproc',
+               'clusters',
+               'update',
+               args.name] + modify_args
 
-        if args.max_idle or args.graceful_decommission_timeout:
+        if args.beta:
             cmd.insert(1, 'beta')
 
+        cmd.extend(pass_through_args)
+
         # print underlying gcloud command
-        print('gcloud update config command:')
         print(' '.join(cmd[:5]) + ' \\\n    ' + ' \\\n    '.join(cmd[5:]))
 
         # Update cluster

hail/python/hailtop/hailctl/dataproc/stop.py
@@ -2,13 +2,14 @@ from subprocess import check_call
 
 def init_parser(parser):
     parser.add_argument('name', type=str, help='Cluster name.')
-    parser.add_argument('--async', action='store_true', help="Do not wait for cluster deletion.")
+    parser.add_argument('--async', action='store_true', dest='asink',
+                        help="Do not wait for cluster deletion.")
 
 def main(args, pass_through_args):
     print("Stopping cluster '{}'...".format(args.name))
 
     cmd = ['gcloud', 'dataproc', 'clusters', 'delete', '--quiet', args.name]
-    if vars(args)['async']:
+    if args.asink:
         cmd.append('--async')
 
     check_call(cmd)

commit 2d54b8f23cf45415d3b10035fd248fe8042e0c26

[hailctl] Fix start with `max_idle` or `max_age`
Fixes bug introduced in #6879

hail/python/hailtop/hailctl/dataproc/start.py
@@ -177,7 +177,7 @@ def main(args, pass_through_args):
     # command to start cluster
     cmd = conf.get_command(args.name)
 
-    if args.beta:
+    if args.beta or args.max_idle or args.max_age:
         cmd.insert(1, 'beta')
     if args.max_idle:
         cmd.append('--max-idle={}'.format(args.max_idle))