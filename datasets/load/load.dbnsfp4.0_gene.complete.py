06d7f0317faa51496a6de7ce58b3d1f7d57d36fe
Brandon Franco <bboyfranco@att.net>
2019-08-27 18:27:43 -0500

    Update load.dbnsfp4.0_gene.complete.py
    Made splits for ";"

--- datasets/load/load.dbnsfp4.0_gene.complete.py
+++ datasets/load/load.dbnsfp4.0_gene.complete.py
@@ -2,6 +2,14 @@ import hail as hl
 
 t = hl.import_table('gs://hail-common/datasets-raw/dbnsfp/dbNSFP4.0_gene.complete.bgz',force=True, impute=True, missing=".")
 
+t = t.annotate(Gene_old_names = t.Gene_old_names.split(";"))
+t = t.annotate(Gene_other_names = t.Gene_other_names.split(";"))
+t = t.annotate(CCDS_id = t.CCDS_id.split(";"))
+t = t.annotate(Refseq_id = t.Refseq_id.split(";"))
+t = t.annotate(GO_biological_process = t.GO_biological_process.split(";"))
+t = t.annotate(GO_cellular_component = t.GO_cellular_component.split(";"))
+t = t.annotate(GO_molecular_function = t.GO_molecular_function.split(";"))
+
 t = t.key_by(t["Gene_name"])
 
 t.write('gs://hail-common/datasets/1/dbnsfp/dbNSFP4.0_gene.complete.bgz.ht', overwrite=True)
