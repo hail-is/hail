71923d17944a38b45ef0d41c420c801100b388e9
Cotton Seed <hail-ci-leader@example.com>
2019-09-03 18:31:50 -0400

    fix paths

--- notebook2/notebook/notebook.py
+++ notebook2/notebook/notebook.py
@@ -45,6 +45,8 @@ log.info(f'INSTANCE_ID {INSTANCE_ID}')
 
 
 async def start_pod(k8s, userdata):
+    notebook2_base_url = deploy_config.base_url('notebook2')
+
     jupyter_token = uuid.uuid4().hex
     pod_id = uuid.uuid4().hex
 
@@ -62,7 +64,7 @@ async def start_pod(k8s, userdata):
                     'jupyter',
                     'notebook',
                     f'--NotebookApp.token={jupyter_token}',
-                    f'--NotebookApp.base_url=/instance/{pod_id}/',
+                    f'--NotebookApp.base_url={notebook2_base_url}/instance/{pod_id}/',
                     f'--GoogleStorageContentManager.default_path="{bucket}"',
                     "--ip", "0.0.0.0", "--no-browser", "--allow-root"
                 ],
@@ -76,7 +78,7 @@ async def start_pod(k8s, userdata):
                 readiness_probe=kube.client.V1Probe(
                     period_seconds=5,
                     http_get=kube.client.V1HTTPGetAction(
-                        path=f'/instance/{pod_id}/login',
+                        path=f'{notebook2_base_url}/instance/{pod_id}/login',
                         port=POD_PORT)),
                 volume_mounts=[
                     kube.client.V1VolumeMount(
@@ -317,7 +319,8 @@ async def wait_websocket(request, userdata):
     notebook = session['notebook']
 
     pod_uuid = notebook['pod_uuid']
-    url = deploy_config.external_url('notebook2', f'/instance-ready/{pod_uuid}')
+    notebook2_base_url = deploy_config.base_url('notebook2')
+    url = deploy_config.external_url('notebook2', f'{notebook2_base_url}/instance-ready/{pod_uuid}')
 
     ws = web.WebSocketResponse()
     await ws.prepare(request)
