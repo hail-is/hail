4727d5b39d2ee8cce0fc9413ad5d314f75e73b60
Cotton Seed <hail-ci-leader@example.com>
2019-09-06 17:24:19 -0400

    prefix scheduler, batch2

--- batch2/batch/batch.py
+++ batch2/batch/batch.py
@@ -1,5 +1,7 @@
 from aiohttp import web
 
+from hailtop.gear import get_deploy_config
+
 from .utils import jsonify
 
 
@@ -13,4 +15,5 @@ async def get_healthcheck(request):  # pylint: disable=W0613
 
 
 app.add_routes(routes)
-web.run_app(app, host='0.0.0.0', port=5000)
+deploy_config = get_deploy_config()
+web.run_app(deploy_config.prefix_application(app, 'batch2'), host='0.0.0.0', port=5000)
