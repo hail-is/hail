d61c8936ff88a46a959363a50f928ab389b1d587
Jackie Goldstein <jigold@broadinstitute.org>
2019-09-09 16:21:10 -0400

    addressed comments

--- hail/python/hailtop/gear/deploy_config.py
+++ hail/python/hailtop/gear/deploy_config.py
@@ -42,6 +42,8 @@ class DeployConfig:
         if self._location == 'k8s':
             return f'{service}.{ns}'
         if self._location == 'gce':
+            if ns == 'default':
+                return f'{service}.hail'
             return 'internal.hail'
         assert self._location == 'external'
         if ns == 'default':
