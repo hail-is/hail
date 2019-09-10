a485cc81e6b541ea96f0d289a4ae5276d5313715
Tim Poterba <tpoterba@gmail.com>
2019-09-10 05:31:03 -0400

    [build] Quiet down tar commands

--- hail/benchmark/benchmark_in_pipeline.py
+++ hail/benchmark/benchmark_in_pipeline.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 
     make_resources = p.new_task('create_resources').cpu(4)
     make_resources.command('hailctl dev benchmark create-resources --data-dir benchmark-resources')
-    make_resources.command("time tar -czvf benchmark-resources.tar.gz benchmark-resources --exclude='*.crc'")
+    make_resources.command("time tar -czf benchmark-resources.tar.gz benchmark-resources --exclude='*.crc'")
     make_resources.command('ls -lh benchmark-resources.tar.gz')
     make_resources.command(f'mv benchmark-resources.tar.gz {make_resources.ofile}')
 
@@ -38,7 +38,7 @@ if __name__ == '__main__':
         for replicate in range(N_REPLICATES):
             t = p.new_task(name=f'{name}_{replicate}')
             t.command(f'mv {make_resources.ofile} benchmark-resources.tar.gz')
-            t.command('time tar -xvf benchmark-resources.tar.gz')
+            t.command('time tar -xf benchmark-resources.tar.gz')
             t.command(f'hailctl dev benchmark run '
                       f'-v -o {t.ofile} -n {N_ITERS} --data-dir benchmark-resources -t {name}')
             all_output.append(t.ofile)
