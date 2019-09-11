d33d7312268c0193580d1b339a6fa1c3020762db
Cotton Seed <hail-ci-leader@example.com>
2019-09-11 11:02:47 -0400

    delint

--- hail/python/hailtop/hailctl/auth/auth_list.py
+++ hail/python/hailtop/hailctl/auth/auth_list.py
@@ -1,7 +1,8 @@
 from hailtop.config import get_deploy_config
 from hailtop.auth import get_tokens
 
-def init_parser(parser):
+
+def init_parser(parser):  # pylint: disable=unused-argument
     pass
 
 
@@ -9,7 +10,7 @@ def main(args, pass_through_args):  # pylint: disable=unused-argument
     deploy_config = get_deploy_config()
     auth_ns = deploy_config.service_ns('auth')
     tokens = get_tokens()
-    for ns, token in tokens.items():
+    for ns in tokens:
         if ns == auth_ns:
             s = '*'
         else:
