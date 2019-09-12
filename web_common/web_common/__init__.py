8ae87c44748173cf34b7a44066b1d7bdbff0598d
Cotton Seed <hail-ci-leader@example.com>
2019-09-12 17:36:28 -0400

    fix imports

--- web_common/web_common/__init__.py
+++ web_common/web_common/__init__.py
@@ -1,4 +1,4 @@
-from .web_common import setup_aiohttp_jinja2, base_context
+from .web_common import sass_compile, setup_aiohttp_jinja2, setup_common_static_routes, base_context
 
 __all__ = [
     'sass_compile',
