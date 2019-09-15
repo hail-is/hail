0b1a3756b852f8546051de7821261f91ac414ec5
Cotton Seed <hail-ci-leader@example.com>
2019-09-15 14:35:51 -0400

    retry gsutil in worker-startup.sh

--- hail/python/hailtop/pipeline/google_backend/worker-startup.sh
+++ hail/python/hailtop/pipeline/google_backend/worker-startup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -ex
 
-gsutil -m cp gs://hail-common/dev2/pipeline/run-worker.sh gs://hail-common/dev2/pipeline/worker.py /
+for i in $(seq 1 30); do
+    gsutil -m cp gs://hail-common/dev2/pipeline/run-worker.sh gs://hail-common/dev2/pipeline/worker.py /
+    if [[ $? = 0 ]]; then
+        break;
+    fi
+    sleep 1
+done
 
 nohup /bin/bash run-worker.sh >run-worker.log 2>&1 &
