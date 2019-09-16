9250fcde9d3ca794c7687e1c8cbf7a7d52591de2
Cotton Seed <hail-ci-leader@example.com>
2019-09-15 21:57:56 -0400

    gcloud auth docker

--- hail/python/hailtop/pipeline/google_backend/worker-startup.sh
+++ hail/python/hailtop/pipeline/google_backend/worker-startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -x
 
 for i in $(seq 1 30); do
     gsutil -m cp gs://hail-common/dev2/pipeline/run-worker.sh gs://hail-common/dev2/pipeline/worker.py /
@@ -8,5 +8,14 @@ for i in $(seq 1 30); do
     fi
     sleep 5
 done
+
+VERSION=1.5.0
+OS=linux
+ARCH=amd64
+curl -fsSL "https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz" \
+  | tar xz --to-stdout ./docker-credential-gcr \
+  | sudo tee /usr/bin/docker-credential-gcr > /dev/null && sudo chmod +x /usr/bin/docker-credential-gcr
+
+gcloud auth docker-configure
 
 nohup /bin/bash run-worker.sh >run-worker.log 2>&1 &
