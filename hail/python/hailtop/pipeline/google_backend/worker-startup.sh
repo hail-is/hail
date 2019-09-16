3dded6bcfebd4d4b337245b7eb71934eac27bacc
Cotton Seed <hail-ci-leader@example.com>
2019-09-15 23:38:03 -0400

    fix

--- hail/python/hailtop/pipeline/google_backend/worker-startup.sh
+++ hail/python/hailtop/pipeline/google_backend/worker-startup.sh
@@ -12,11 +12,11 @@ done
 VERSION=1.5.0
 OS=linux
 ARCH=amd64
+
 curl -fsSL "https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz" \
   | tar xz --to-stdout ./docker-credential-gcr \
-  | sudo tee /usr/bin/docker-credential-gcr > /dev/null && sudo chmod +x /usr/bin/docker-credential-gcr
+  > /usr/bin/docker-credential-gcr && chmod +x /usr/bin/docker-credential-gcr
 
 docker-credential-gcr configure-docker
-gcloud -q auth configure-docker
 
 nohup /bin/bash run-worker.sh >run-worker.log 2>&1 &
