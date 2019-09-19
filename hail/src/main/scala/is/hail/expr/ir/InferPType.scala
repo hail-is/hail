cc4ff0d26e5a4869b6c5503afca9b2af68796573
Alex V. Kotlar <akotlar@bu.edu>
2019-09-19 13:22:23 -0400

    Update InferPType.scala

--- hail/src/main/scala/is/hail/expr/ir/InferPType.scala
+++ hail/src/main/scala/is/hail/expr/ir/InferPType.scala
@@ -189,9 +189,9 @@ object InferPType {
         val elt = coerce[PBaseStruct](coerce[PIterable](a.pType2).elementType)
         // Dict key/value types don't depend on PIterable's requiredeness because we have an interface guarantee that
         // null PIterables are filtered out before dict construction
-        val typeRequired = elt.types(0).required
+        val keyRequired = elt.types(0).required
         val valRequired =  elt.types(1).required
-        PDict(elt.types(0).setRequired(typeRequired), elt.types(1).setRequired(valRequired), a.pType2.required)
+        PDict(elt.types(0).setRequired(keyRequired), elt.types(1).setRequired(valRequired), a.pType2.required)
       }
       case ToArray(a) => {
         InferPType(a, env)
