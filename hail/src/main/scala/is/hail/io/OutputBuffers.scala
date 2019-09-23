909cdfa3dcc0340249b59f3eb40b158193152e1b
Daniel King <dking@broadinstitute.org>
2019-09-23 09:37:58 -0400

    revert

--- hail/src/main/scala/is/hail/io/OutputBuffers.scala
+++ hail/src/main/scala/is/hail/io/OutputBuffers.scala
@@ -156,18 +156,12 @@ final class LEB128OutputBuffer(out: OutputBuffer) extends OutputBuffer {
   def writeDoubles(from: Array[Double], fromOff: Int, n: Int): Unit = out.writeDoubles(from, fromOff, n)
 }
 
-final class BlockingOutputBuffer(
-  private val buf: Array[Byte],
-  out: OutputBlockBuffer
-) extends OutputBuffer {
-  def this(blockSize: Int, out: OutputBlockBuffer) {
-    this(new Array[Byte](blockSize), out)
-  }
-
+final class BlockingOutputBuffer(blockSize: Int, out: OutputBlockBuffer) extends OutputBuffer {
+  private val buf: Array[Byte] = new Array[Byte](blockSize)
   private var off: Int = 0
 
   def indexOffset(): Long = {
-    if (off == buf.size)
+    if (off == blockSize)
       writeBlock()
     (out.getPos() << 16) | off
   }
