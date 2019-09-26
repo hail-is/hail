3b2afc683c6ebb85cb48d511a577fd3667a10313
Cotton Seed <hail-ci-leader@example.com>
2019-09-26 00:54:08 -0400

    make session cookie secure, httponly

--- gear/gear/session.py
+++ gear/gear/session.py
@@ -10,6 +10,8 @@ def setup_aiohttp_session(app):
         aiohttp_session.setup(app, aiohttp_session.cookie_storage.EncryptedCookieStorage(
             f.read(),
             cookie_name=deploy_config.auth_session_cookie_name(),
+            secure=True,
+            httponly=True,
             domain=os.environ['HAIL_DOMAIN'],
             # 2592000s = 30d
             max_age=2592000))
