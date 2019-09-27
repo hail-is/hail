c5b683a5279216d27e2b5ec988f268e4fe4cef83
Daniel King <dking@broadinstitute.org>
2019-09-26 17:47:04 -0700

    fix

--- hail/python/hailtop/hailctl/dev/benchmark/run/shuffle_benchmarks.py
+++ hail/python/hailtop/hailctl/dev/benchmark/run/shuffle_benchmarks.py
@@ -6,7 +6,7 @@ from .utils import benchmark, resource
 
 
 @benchmark
-def shuffle__key_rows_by_mt():
+def shuffle_key_rows_by_mt():
     mt = hl.read_matrix_table(resource('profile.mt'))
     mt = mt.annotate_rows(reversed_position_locus=hl.struct(
         contig=mt.locus.contig,
@@ -16,14 +16,14 @@ def shuffle__key_rows_by_mt():
 
 
 @benchmark
-def shuffle__order_by_10m_int():
+def shuffle_order_by_10m_int():
     t = hl.utils.range_table(10_000_000, n_partitions=100)
     t = t.order_by(-t.idx)
     t._force_count()
 
 
 @benchmark
-def shuffle__key_rows_by_4096_byte_rows():
+def shuffle_key_rows_by_4096_byte_rows():
     mt = hl.utils.range_matrix_table(100_000, (1 << 12) // 4)
     mt = mt.annotate_entries(entry=mt.row_idx * mt.col_idx)
     mt = mt.key_rows_by(backward_rows_idx=-mt.row_idx)
@@ -31,7 +31,7 @@ def shuffle__key_rows_by_4096_byte_rows():
 
 
 @benchmark
-def shuffle__key_rows_by_65k_byte_rows():
+def shuffle_key_rows_by_65k_byte_rows():
     mt = hl.utils.range_matrix_table(10_000, (1 << 16) // 4)
     mt = mt.annotate_entries(entry=mt.row_idx * mt.col_idx)
     mt = mt.key_rows_by(backward_rows_idx=-mt.row_idx)
