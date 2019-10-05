936bf89ab5d3ec34c558e28ed0e8ece88d76f241
Christopher Vittal <christopher.vittal@gmail.com>
2019-10-05 00:04:36 -0400

    [bugfix][hail] Check for missingness in VCF float/double arrays

--- hail/python/test/hail/methods/test_impex.py
+++ hail/python/test/hail/methods/test_impex.py
@@ -386,6 +386,13 @@ class VCFTests(unittest.TestCase):
         mt = hl.import_vcf(vcf_path)
         assert mt._force_count_rows() == 1
 
+    def test_missing_float_entries(self):
+        vcf = hl.import_vcf(resource('noglgp.vcf'), array_elements_required=False,
+                            reference_genome='GRCh38')
+        gl_gp = vcf.aggregate_entries(hl.agg.collect(hl.struct(GL=vcf.GL, GP=vcf.GP)))
+        assert gl_gp == [hl.Struct(GL=[None, None, None], GP=[0.22, 0.5, 0.27]),
+                         hl.Struct(GL=[None, None, None], GP=[None, None, None])]
+
 
 class PLINKTests(unittest.TestCase):
     def test_import_fam(self):
