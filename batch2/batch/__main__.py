699070ef9444a12dd9cec0da10f1238c94e62596
cseed <cotton@alum.mit.edu>
2019-10-08 00:03:15 +0100

    make app, v1, db not global (#7210)

--- batch2/batch/__main__.py
+++ batch2/batch/__main__.py
@@ -1,12 +1,11 @@
-from aiohttp import web
-
-from hailtop.config import get_deploy_config
 from gear import configure_logging
-
-from .batch import app
-
+# configure logging before importing anything else
 configure_logging()
 
-deploy_config = get_deploy_config()
 
-web.run_app(deploy_config.prefix_application(app, 'batch2'), host='0.0.0.0', port=5000)
+def main():
+    from .batch import run
+    run()
+
+
+main()
