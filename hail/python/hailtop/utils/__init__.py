73c144142ea69af9ea27326414cb958a31405b8a
Cotton Seed <cotton@alum.mit.edu>
2019-10-07 22:51:09 +0100

    move shared async subprocess code to hailtop.utils

--- hail/python/hailtop/utils/__init__.py
+++ hail/python/hailtop/utils/__init__.py
@@ -1,4 +1,5 @@
 from .utils import unzip, async_to_blocking, blocking_to_async, AsyncWorkerPool
+from .process import CalledProcessError, check_shell, check_shell_output
 
 __all__ = [
     'unzip',
