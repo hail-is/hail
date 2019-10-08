4277de1e64ad83dfe061b61c8ec8b60e53cd83fa
Milo Turner <iitalics@gmail.com>
2019-10-08 13:06:03 -0400

    stream impl of ArrayMap, ArrayFilter
    add Skip
    
    other streams
    
    add tests
    
    delete all but map/filter
    
    delete tests

--- hail/src/main/scala/is/hail/expr/ir/Streamify.scala
+++ hail/src/main/scala/is/hail/expr/ir/Streamify.scala
@@ -6,6 +6,8 @@ object Streamify {
   def apply(ir: IR): IR = ir match {
     case NA(t) => NA(t)
     case ArrayRange(x, y, z) => StreamRange(x, y, z)
+    case ArrayMap(a, n, b) => ArrayMap(apply(a), n, b)
+    case ArrayFilter(a, n, b) => ArrayFilter(apply(a), n, b)
     case Let(n, v, b) => Let(n, v, apply(b))
     case ir => ToStream(ir)
   }
