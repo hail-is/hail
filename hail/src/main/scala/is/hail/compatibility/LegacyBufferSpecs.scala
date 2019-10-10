178d7d6eda67418d130212d92e143cb4372d2c5f
Daniel King <dking@broadinstitute.org>
2019-10-10 18:55:12 -0400

    no deprecated

--- hail/src/main/scala/is/hail/compatibility/LegacyBufferSpecs.scala
+++ hail/src/main/scala/is/hail/compatibility/LegacyBufferSpecs.scala
@@ -3,7 +3,6 @@ package is.hail.compatibility
 import is.hail.io.compress.LZ4
 import is.hail.io._
 
-@deprecated("LZ4HCBlockBufferSpec is a drop-in replacement for this class", "Hail 0.2.24")
 final case class LZ4BlockBufferSpec(blockSize: Int, child: BlockBufferSpec)
     extends LZ4BlockBufferSpecCommon {
   def lz4 = LZ4.hc
