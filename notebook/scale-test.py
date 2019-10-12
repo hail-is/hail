0f165a572e205474fc5228a29b074abe436b4a5e
Tim Poterba <tpoterba@gmail.com>
2019-10-11 21:04:35 -0400

    [misc] add quantiles to scale test

--- notebook/scale-test.py
+++ notebook/scale-test.py
@@ -130,6 +130,7 @@ async def main():
 
     print(f'successes: {len(times)} / {n} = {len(times) / n}')
     print(f'mean time: {sum(times) / n}')
+    print(f'quantiles 50%/95%/99%:: {np.quantile(times, [.5, .95, .99])}')
     print(f'max time: {max(times)}')
     print(f'histogram:\n{np.histogram(times, density=True)}')
 
