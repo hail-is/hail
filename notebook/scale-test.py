94bbc08ff12f2380e4da21ef3fe27e8dc4a9d410
Tim Poterba <tpoterba@gmail.com>
2019-10-12 09:19:34 -0400

    Update scale-test.py

--- notebook/scale-test.py
+++ notebook/scale-test.py
@@ -130,7 +130,7 @@ async def main():
 
     print(f'successes: {len(times)} / {n} = {len(times) / n}')
     print(f'mean time: {sum(times) / n}')
-    print(f'quantiles min/50%/95%/99%/max:: {np.quantile(times, [0.0, .5, .95, .99, 1.0])}')
+    print(f'quantiles min/50%/95%/99%/max: {np.quantile(times, [0.0, .5, .95, .99, 1.0])}')
     print(f'histogram:\n{np.histogram(times, density=True)}')
 
 
