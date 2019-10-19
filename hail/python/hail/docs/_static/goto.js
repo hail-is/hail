c54ad5cd01bf8c703e30d61db84f08b7a93f5dcf
Alex Kotlar <akotlar@bu.edu>
2019-10-19 00:01:41 -0500

    fixes

--- hail/python/hail/docs/_static/goto.js
+++ hail/python/hail/docs/_static/goto.js
@@ -5,13 +5,13 @@ $(document).ready(function () {
         var hash = window.location.hash
 
         var hashName = hash.substring(1, hash.length);
-        var elem = $(document.getElementById(hashName));
+        var elem = document.getElementById(hashName);
 
-        if (elem === null) {
+        if (!elem) {
             return;
         }
 
-        window.scrollTo(0, parseInt(elem.offset().top, 10) - navHeight);
+        window.scrollTo(0, parseInt($(elem).offset().top, 10) - navHeight);
     }
 
     $('.wy-nav-content').on('click', 'a', function (e) {
@@ -21,14 +21,14 @@ $(document).ready(function () {
             return;
         }
 
-        var elem = $(document.getElementById(hashName[1]));
+        var elem = document.getElementById(hashName[1]);
 
-        if (elem === null) {
+        if (!elem) {
             return;
         }
 
         e.preventDefault();
 
-        window.scrollTo(0, parseInt(elem.offset().top, 10) - navHeight);
+        window.scrollTo(0, parseInt($(elem).offset().top, 10) - navHeight);
     })
 });
