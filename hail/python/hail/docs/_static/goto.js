88314bfaa0cfb629271ff44ec87e7d48e971a25f
Alex Kotlar <akotlar@bu.edu>
2019-10-18 20:22:22 -0500

    full fix

---
+++ hail/python/hail/docs/_static/goto.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    var navHeight = $('nav').height();
+
+    if (window.location.hash) {
+        var hash = window.location.hash
+
+        var hashName = hash.substring(1, hash.length);
+        var elem = $(document.getElementById(hashName));
+
+        //if element has this id then scroll to it
+        if (elem === null) {
+            console.info("NULL");
+            return;
+        }
+
+        window.scrollTo(0, parseInt(elem.offset().top, 10) - navHeight);
+    }
+
+    $(document).on('click', 'a', function (e) {
+        e.preventDefault();
+        var hashName = this.href.split('#')[1];
+
+        var elem = $(document.getElementById(hashName));
+
+        //if element has this id then scroll to it
+        if (elem === null) {
+            return;
+        }
+
+        window.scrollTo(0, parseInt(elem.offset().top, 10) - navHeight);
+    })
+});
