ed6c1e175d14f45fa8a7c7980f10e3eb121f3f0c
Cotton Seed <cotton@alum.mit.edu>
2019-10-22 01:00:17 +0100

    fix

--- batch2/batch/batch.py
+++ batch2/batch/batch.py
@@ -265,7 +265,11 @@ class Job:
             result['duration'] = {k: None for k in tasks}
             result['message'] = {
                 # self.status['container_statuses'][k]['container_status']['message']
-                k: getopt(getopt(getopt(self.status['container_statuses'], k), 'container_status'), 'message')
+                # the execution of the pod container might have
+                # failed, or the docker container might have completed
+                # (wait returned) but had status error
+                k: (getopt(getopt(self.status['container_statuses'], k), 'message') or
+                    getopt(getopt(getopt(self.status['container_statuses'], k), 'container_status'), 'message'))
                 for k in tasks
             }
         if self.attributes:
