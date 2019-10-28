fb1308ff2c1e85d2a84e80189b7774f0a4759a2f
Cotton Seed <cotton@alum.mit.edu>
2019-10-28 21:24:33 +0000

    cleanup

--- batch2/delete-batch-tables.sql
+++ batch2/delete-batch-tables.sql
@@ -1,6 +1,11 @@
-DROP TABLE IF EXISTS `jobs`;
 DROP TABLE IF EXISTS `batch-attributes`;
 DROP TABLE IF EXISTS `jobs-parents`;
+DROP TABLE IF EXISTS `jobs`;
 DROP TABLE IF EXISTS `batch`;
-DROP TABLE IF EXISTS `pods`;
 DROP TABLE IF EXISTS `instances`;
+DROP PROCEDURE IF EXISTS activate_instance;
+DROP PROCEDURE IF EXISTS deactivate_instance;
+DROP PROCEDURE IF EXISTS mark_instance_deleted;
+DROP PROCEDURE IF EXISTS schedule_job;
+DROP PROCEDURE IF EXISTS unschedule_job;
+DROP PROCEDURE IF EXISTS mark_job_complete;
