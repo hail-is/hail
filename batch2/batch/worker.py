6a23fc0deb6150819975b412e747ea340c311c45
Cotton Seed <cotton@alum.mit.edu>
2019-10-30 10:37:41 +0000

    debugging

--- batch2/batch/worker.py
+++ batch2/batch/worker.py
@@ -34,7 +34,7 @@ log = logging.getLogger('batch2-agent')
 
 docker = aiodocker.Docker()
 
-MAX_IDLE_TIME_SECS = 60
+MAX_IDLE_TIME_SECS = 10 * 60
 
 
 async def docker_call_retry(f, *args, **kwargs):
@@ -421,7 +421,7 @@ class Job:
 
             log.info(f'{self}: cleaning up')
             try:
-                shutil.rmtree(self.scratch, ignore_errors=True)
+                # shutil.rmtree(self.scratch, ignore_errors=True)
                 if io:
                     await docker_call_retry(io.delete)
             except Exception:
