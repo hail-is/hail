9bbbdca88ab7b04f0460ec5c9f68e95576dd97dd
Arcturus Wang <wang@broadinstitute.org>
2019-10-30 14:05:56 -0400

    [ci] allow custom endpoint for wait-for.py instead of always using /healthcheck (#7401)
    * [ci] allow custom endpoint for  instead of always using /healthcheck
    
    * service_parser

--- ci/ci/build.py
+++ ci/ci/build.py
@@ -682,6 +682,7 @@ set -e
                     assert w['for'] == 'alive', w['for']
                     port = w.get('port', 80)
                     resource_type = w.get('resource_type', 'deployment').lower()
+                    endpoint = w.get('endpoint', '/healthcheck')
                     timeout = w.get('timeout', 60)
                     if resource_type == 'statefulset':
                         wait_cmd = f'kubectl -n {self.namespace} wait --timeout=1h --for=condition=ready pods --selector=app={name}'
@@ -693,7 +694,7 @@ set -e
 set +e
 kubectl -n {self.namespace} rollout status --timeout=1h {resource_type} {name} && \
   {wait_cmd} && \
-  python3 wait-for.py {timeout} {self.namespace} Service -p {port} {name}
+  python3 wait-for.py {timeout} {self.namespace} Service -p {port} {name} --endpoint {endpoint}
 EC=$?
 kubectl -n {self.namespace} logs --tail=999999 -l app={name} | {pretty_print_log}
 set -e
