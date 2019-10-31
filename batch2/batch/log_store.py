6049c780b80f101ce60a975720f6235aa1caccc9
Cotton Seed <cotton@alum.mit.edu>
2019-10-31 13:52:25 +0000

    fix

--- batch2/batch/log_store.py
+++ batch2/batch/log_store.py
@@ -17,10 +17,10 @@ class LogStore:
         return f'{self.batch_log_dir(batch_id)}/{job_id}/{task}/log'
 
     async def read_log_file(self, batch_id, job_id, task):
-        return await self.gcs.read_gs_file(self.log_path(self.log_root, batch_id, job_id, task))
+        return await self.gcs.read_gs_file(self.log_path(batch_id, job_id, task))
 
     async def write_log_file(self, batch_id, job_id, task, data):
-        return await self.gcs.write_gs_file(self.log_path(self.log_root, batch_id, job_id, task), data)
+        return await self.gcs.write_gs_file(self.log_path(batch_id, job_id, task), data)
 
     async def delete_batch_logs(self, batch_id):
         await self.gcs.delete_gs_files(
