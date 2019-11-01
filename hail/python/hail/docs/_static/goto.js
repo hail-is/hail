2727bb832cf95ee2860305efc6ad9d41ab88cb32
Alex Kotlar <akotlar@bu.edu>
2019-11-01 10:59:38 -0400

    working katex implementation

--- hail/python/hail/docs/_static/goto.js
+++ hail/python/hail/docs/_static/goto.js
@@ -1,42 +1,42 @@
 //https://caniuse.com/#feat=history
 if ((window.history && window.history.pushState && window.scrollTo)) {
     $(document).ready(function () {
-        MathJax.Hub.Register.StartupHook("End", function () {
-            var navHeight = $('nav').height();
-            var hash = window.location.hash ? decodeURIComponent(window.location.hash.replace('#', '')) : null;
+        var navHeight = $('nav').height();
+        var hash = window.location.hash ? decodeURIComponent(window.location.hash.replace('#', '')) : null;
 
-            if (hash) {
-                var elem = document.getElementById(hash);
+        if (hash) {
+            var elem = document.getElementById(hash);
 
-                if (!elem) {
-                    return;
-                }
-
-                setTimeout(() => {
-                    window.scrollTo(0, parseInt($(elem).offset().top, 10) - navHeight);
-                }, 1)
+            if (!elem) {
+                return;
             }
 
-            $(document).on('click', 'a', function (e) {
-                var currentHref = location.href.split("#")[0];
-                var hrefParts = this.href.split('#');
+            setTimeout(() => {
+                window.scrollTo(0, parseInt($(elem).offset().top, 10) - navHeight);
+            }, 0)
+        }
 
-                if (hrefParts.length == 1 || hrefParts[0] !== currentHref) {
-                    return;
-                }
+        $(document).on('click', 'a', function (e) {
+            var currentHref = location.href.split("#")[0];
+            var hrefParts = this.href.split('#');
 
-                var hash = decodeURIComponent(hrefParts[1]);
-                var elem = document.getElementById(hash);
+            if (hrefParts.length == 1 || hrefParts[0] !== currentHref) {
+                return;
+            }
 
-                if (!elem) {
-                    console.warn(`Couldn't find element with id ${hash}`)
-                    return;
-                }
+            var hash = decodeURIComponent(hrefParts[1]);
+            var elem = document.getElementById(hash);
 
-                e.preventDefault();
+            if (!elem) {
+                console.warn(`Couldn't find element with id ${hash}`)
+                return;
+            }
+
+            e.preventDefault();
+            setTimeout(() => {
                 window.scrollTo(0, parseInt($(elem).offset().top, 10) - navHeight);
                 history.pushState({}, null, `#${hash}`);
-            });
+            }, 0);
         });
     });
 } else {
