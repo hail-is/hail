e78a09c77816b01710215762a1e5ef7a48e97b5e
John Compitello <j301600@gmail.com>
2019-11-01 16:21:09 -0400

    Made a testing file to try and call LAPACK

---
+++ hail/src/main/scala/is/hail/lapack/LapackTest.scala
@@ -0,0 +1,11 @@
+package is.hail.lapack
+
+import com.sun.jna.Library
+
+object LapackTest {
+  final def main(args: Array[String]): Unit = {
+    println("Trying to call LAPACK")
+
+    System.exit(0)
+  }
+}
