28f8747248ea5cb165bf240a134d5fe7c736bb5e
Cotton Seed <cotton@alum.mit.edu>
2019-11-04 13:31:06 +0000

    debugging

--- batch2/batch/worker.py
+++ batch2/batch/worker.py
@@ -669,6 +669,7 @@ class Worker:
                     'Authorization': f'Bearer {os.environ["ACTIVATION_TOKEN"]}'
                 })
             resp_json = await resp.json()
+            log.info(f'resp_json {resp_json}')
             self.headers = {
                 'X-Hail-Instance-Name': NAME,
                 'Authorization': f'Bearer {resp_json["token"]}'
@@ -676,6 +677,7 @@ class Worker:
 
             credentials = google.oauth2.service_account.Credentials.from_service_account_info(
                 resp_json['key'])
+            log.info(f'credentials {credentials}')
             self.log_store = LogStore(BUCKET_NAME, INSTANCE_ID, self.pool, credentials)
 
 
