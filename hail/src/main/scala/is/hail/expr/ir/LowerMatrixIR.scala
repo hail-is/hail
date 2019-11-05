33d359dd493b04eb445e3ae074c7bb3dadc0ebc9
Tim Poterba <tpoterba@broadinstitute.org>
2019-11-05 17:21:50 -0500

    [hail] Redesign `InterpretNonCompilable` to be a lowering pass (#7423)
    * [hail] Remove `LiftNonCompilable` compiler pass
    
    Should be unnecessary now.
    
    Stacked on #7421
    
    * [hail] Redesign `InterpretNonCompilable` to be a lowering pass
    
    * fix
    
    * simpler algorithm
    
    * create `InterpretableButNotCompilable` analysis
    
    * use rewriteBottomUp
    
    * fix interpret lowering
    
    * combine EvaluateRelationalLets and InterpretNonCompilable
    
    * fix lowerer
    
    * fix multi write in compilable

--- hail/src/main/scala/is/hail/expr/ir/LowerMatrixIR.scala
+++ hail/src/main/scala/is/hail/expr/ir/LowerMatrixIR.scala
@@ -125,7 +125,7 @@ object LowerMatrixIR {
 
       case MatrixFilterRows(child, pred) =>
         lower(child, ab)
-          .filter(subst(pred, matrixSubstEnv(child)))
+          .filter(subst(lower(pred, ab), matrixSubstEnv(child)))
 
       case MatrixFilterCols(child, pred) =>
         lower(child, ab)
@@ -133,7 +133,7 @@ object LowerMatrixIR {
             irRange(0, 'global (colsField).len)
               .filter('i ~>
                 (let(sa = 'global (colsField)('i))
-                  in subst(pred, matrixGlobalSubstEnv(child))))))
+                  in subst(lower(pred, ab), matrixGlobalSubstEnv(child))))))
           .mapRows('row.insertFields(entriesField -> 'global ('newColIdx).map('i ~> 'row (entriesField)('i))))
           .mapGlobals('global
             .insertFields(colsField ->
@@ -171,7 +171,7 @@ object LowerMatrixIR {
       case MatrixMapGlobals(child, newGlobals) =>
         lower(child, ab)
           .mapGlobals(
-            subst(newGlobals, BindingEnv(Env[IRProxy](
+            subst(lower(newGlobals, ab), BindingEnv(Env[IRProxy](
               "global" -> 'global.selectFields(child.typ.globalType.fieldNames: _*))))
               .insertFields(colsField -> 'global (colsField)))
 
@@ -270,7 +270,7 @@ object LowerMatrixIR {
 
         val lc = lower(child, ab)
         lc.mapRows(
-          liftScans(Subst(newRow, matrixSubstEnvIR(child, lc)))
+          liftScans(Subst(lower(newRow, ab), matrixSubstEnvIR(child, lc)))
             .insertFields(entriesField -> 'row(entriesField)))
 
       case MatrixMapCols(child, newCol, _) =>
@@ -367,7 +367,7 @@ object LowerMatrixIR {
         val scanBuilder = new ArrayBuilder[(String, IR)]
         val aggBuilder = new ArrayBuilder[(String, IR)]
 
-        val b0 = lift(Subst(newCol, matrixSubstEnvIR(child,loweredChild)), scanBuilder, aggBuilder)
+        val b0 = lift(Subst(lower(newCol, ab), matrixSubstEnvIR(child, loweredChild)), scanBuilder, aggBuilder)
         val aggs = aggBuilder.result()
         val scans = scanBuilder.result()
 
@@ -441,7 +441,7 @@ object LowerMatrixIR {
             'i ~>
               let(g = 'row (entriesField)('i)) {
                 irIf(let(sa = 'global (colsField)('i))
-                  in !subst(pred, matrixSubstEnv(child))) {
+                  in !subst(lower(pred, ab), matrixSubstEnv(child))) {
                   NA(child.typ.entryType)
                 } {
                   'g
@@ -479,7 +479,7 @@ object LowerMatrixIR {
             'i ~>
               let(g = 'row (entriesField)('i),
                 sa = 'global (colsField)('i)) {
-                subst(newEntries, BindingEnv(Env(
+                subst(lower(newEntries, ab), BindingEnv(Env(
                   "global" -> 'global.selectFields(child.typ.globalType.fieldNames: _*),
                   "va" -> 'row.selectFields(child.typ.rowType.fieldNames: _*))))
               }
@@ -550,8 +550,8 @@ object LowerMatrixIR {
       case MatrixAggregateRowsByKey(child, entryExpr, rowExpr) =>
 
         val substEnv = matrixSubstEnv(child)
-        val eeSub = subst(entryExpr, substEnv)
-        val reSub = subst(rowExpr, substEnv)
+        val eeSub = subst(lower(entryExpr, ab), substEnv)
+        val reSub = subst(lower(rowExpr, ab), substEnv)
         lower(child, ab)
           .aggregateByKey(
             reSub.insertFields(entriesField -> irRange(0, 'global (colsField).len)
@@ -607,9 +607,9 @@ object LowerMatrixIR {
         val aggElementIdx = Symbol(genUID())
 
         val substEnv = matrixSubstEnv(child)
-        val ceSub = subst(colExpr, substEnv)
+        val ceSub = subst(lower(colExpr, ab), substEnv)
         val vaBinding = 'row.selectFields(child.typ.rowType.fieldNames: _*)
-        val eeSub = subst(entryExpr, substEnv.bindEval("va", vaBinding).bindAgg("va", vaBinding))
+        val eeSub = subst(lower(entryExpr, ab), substEnv.bindEval("va", vaBinding).bindAgg("va", vaBinding))
 
         lower(child, ab)
           .mapGlobals('global.insertFields(keyMap ->
