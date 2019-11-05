6188b94681792f62603009b05d1c79cccd808d11
Tim Poterba <tpoterba@gmail.com>
2019-11-05 09:21:54 -0500

    use rewriteBottomUp

--- hail/src/main/scala/is/hail/expr/ir/InterpretNonCompilable.scala
+++ hail/src/main/scala/is/hail/expr/ir/InterpretNonCompilable.scala
@@ -8,29 +8,16 @@ import scala.collection.mutable
 object InterpretNonCompilable {
 
   def apply(ctx: ExecuteContext, ir: BaseIR): BaseIR = {
-
-    def rewrite(ir: BaseIR): BaseIR = {
-      val children = ir.children
-      val rewritten = children.map(rewrite)
-      val refEq = children.indices.forall(i => children(i).eq(rewritten(i)))
-      val rwIR = if (refEq)
-        ir
-      else
-        ir.copy(rewritten)
-
-      rwIR match {
-        case x: IR if InterpretableButNotCompilable(x) =>
-          val preTime = System.nanoTime()
-          log.info(s"interpreting non compilable node: $x")
-          val r = Interpret.alreadyLowered(ctx, x)
-          log.info(s"took ${ formatTime(System.nanoTime() - preTime) }")
-          if (x.typ == TVoid) {
-            Begin(FastIndexedSeq())
-          } else Literal.coerce(x.typ, r)
-        case _ => rwIR
-      }
-    }
-
-    rewrite(ir)
+    RewriteBottomUp(ir, {
+      case x: IR if InterpretableButNotCompilable(x) =>
+        val preTime = System.nanoTime()
+        log.info(s"interpreting non compilable node: $x")
+        val r = Interpret.alreadyLowered(ctx, x)
+        log.info(s"took ${ formatTime(System.nanoTime() - preTime) }")
+        Some(if (x.typ == TVoid) {
+          Begin(FastIndexedSeq())
+        } else Literal.coerce(x.typ, r))
+      case _ => None
+    })
   }
 }
