8a2e061b5ee05c6ee5f32eb501f0e6295a24e514
John Compitello <j301600@gmail.com>
2019-11-06 09:24:35 -0500

    Calling LAPACK successfully, but surprising results

--- hail/src/main/scala/is/hail/lapack/LapackTest.scala
+++ hail/src/main/scala/is/hail/lapack/LapackTest.scala
@@ -1,6 +1,7 @@
 package is.hail.lapack
 
 import com.sun.jna.{Native, Library, Pointer, Memory}
+import com.sun.jna.ptr._
 import is.hail.annotations.{Memory => HailMemory}
 
 object LAPACKLibrary {
@@ -12,11 +13,16 @@ object BLASLibrary {
 }
 
 trait BLASLibrary extends Library {
-  def dgemm(TRANSA: Char, TRANSB: Char, M: Int, N: Int, K: Int, ALPHA: Double, A: Pointer, LDA: Int, B: Pointer, LDB: Int, BETA: Double, C: Pointer, LDC: Int)
+  //def dgemm(TRANSA: Char, TRANSB: Char, M: Int, N: Int, K: Int, ALPHA: Double, A: Pointer, LDA: Int, B: Pointer, LDB: Int, BETA: Double, C: Pointer, LDC: Int)
+  def dgemm(TRANSA: IntByReference, TRANSB: IntByReference, M: IntByReference, N: IntByReference, K: IntByReference,
+    ALPHA: DoubleByReference, A: Pointer, LDA: IntByReference, B: Pointer, LDB: IntByReference, BETA: DoubleByReference,
+    C: Pointer, LDC: IntByReference)
 }
 
 trait LAPACKLibrary extends Library {
   def dsyevd(JOBZ: Char, UPLO: Char, N: Int, A: Pointer, LDA: Int, W: Pointer, WORK: Pointer, LWORK: Int, IWORK: Pointer, LIWORK: Int, INFO: Int)
+  //def disnan(DIN: Double)
+  def dlapy2(X: DoubleByReference, Y: DoubleByReference): Double
 }
 
 object LapackTest {
@@ -27,11 +33,46 @@ object LapackTest {
     println(BLASLibrary.instance)
 
 
-    val aMem = HailMemory.malloc(8 * 16) //16 8 byte doubles
-    val bMem = HailMemory.malloc(8 * 16)
-    val cMem = HailMemory.malloc(8 * 16)
+    val aMem = new Memory(8 * 16)//HailMemory.malloc(8 * 16) //16 8 byte doubles
+    val bMem = new Memory (8 * 16) //HailMemory.malloc(8 * 16)
+    val cMem = new Memory(8 * 16) //HailMemory.malloc(8 * 16)
 
-    BLASLibrary.instance.dgemm('n', 'n', 4, 4, 4, 1.0, new Pointer(aMem), 8, new Pointer(bMem), 8, 1.0, new Pointer(cMem), 8)
+    println(aMem.getDouble(0L))
+
+    (0 until 16).foreach{idx =>
+      aMem.setDouble(idx.toLong, 1.0)
+      bMem.setDouble(idx.toLong, 1.0)
+      cMem.setDouble(idx.toLong, 0.0)
+    }
+
+    println(aMem.getDouble(0L))
+    println(cMem.getDouble(0L))
+
+    val LDA = new IntByReference(4)
+    val LDB = new IntByReference(4)
+    val LDC = new IntByReference(4)
+
+    BLASLibrary.instance.dgemm(new IntByReference('n'.toInt), new IntByReference('n'),
+      new IntByReference(4), new IntByReference(4),
+      new IntByReference(4), new DoubleByReference(1.0), aMem,
+      LDA, bMem, LDB, new DoubleByReference(1.0),
+      cMem, LDC)
+
+    println(aMem.getDouble(0L))
+    println(cMem.getDouble(0L))
+
+    //println(LAPACKLibrary.instance.disnan(4.3))
+//    var x = 3.0
+//    var y = 4.0
+//    var z = LAPACKLibrary.instance.dlapy2(new DoubleByReference(x), new DoubleByReference(y))
+//
+//    println(z)
+    //println(res)
+    /**
+      * Code.invokeStatic[BLAS, BLAS]("getInstance").dgemm("n", "n", getLeftShapeAtIdx(0).toI, getRightShapeAtIdx(1).toI,
+      * getLeftShapeAtIdx(1).toI, 1.0, a, getLeftShapeAtIdx(0).toI,
+      * b, getRightShapeAtIdx(0).toI, 0, c, getLeftShapeAtIdx(0).toI),
+      */
 
     System.exit(0)
   }
