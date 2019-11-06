c3e87c82d1b88c4a94e867cbf43a9a065c747df0
John Compitello <j301600@gmail.com>
2019-11-06 17:00:46 -0500

    Fully wired, gets to Emit

--- hail/src/main/scala/is/hail/expr/ir/Copy.scala
+++ hail/src/main/scala/is/hail/expr/ir/Copy.scala
@@ -94,6 +94,9 @@ object Copy {
       case NDArrayMatMul(_, _) =>
         assert(newChildren.length == 2)
         NDArrayMatMul(newChildren(0).asInstanceOf[IR], newChildren(1).asInstanceOf[IR])
+      case NDArrayQR(_, mode) =>
+        assert(newChildren.length == 1)
+        NDArrayQR(newChildren(0).asInstanceOf[IR], mode)
       case NDArrayWrite(_, _) =>
         assert(newChildren.length == 2)
         NDArrayWrite(newChildren(0).asInstanceOf[IR], newChildren(1).asInstanceOf[IR])
