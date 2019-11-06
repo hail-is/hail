48e0f248ce9c70f59429751bb2567c36cf15b78b
John Compitello <j301600@gmail.com>
2019-11-06 13:42:37 -0500

    Fixed example print out

--- hail/python/hail/nd/nd.py
+++ hail/python/hail/nd/nd.py
@@ -20,13 +20,13 @@ def arange(start, stop=None, step=1) -> NDArrayNumericExpression:
     --------
 
     >>> hl.eval(hl._nd.arange(10))
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=int32)
 
     >>> hl.eval(hl._nd.arange(3, 10))
-    [3, 4, 5, 6, 7, 8, 9]
+    array([3, 4, 5, 6, 7, 8, 9], dtype=int32)
 
     >>> hl.eval(hl._nd.arange(0, 10, step=3))
-    [0, 3, 6, 9]
+    array([0, 3, 6, 9], dtype=int32)
 
     Notes
     -----
