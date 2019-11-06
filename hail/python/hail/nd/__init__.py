d8ea072403f217618a4aa7d3d00661c7d64df106
John Compitello <j301600@gmail.com>
2019-11-06 15:48:18 -0500

    Have to figure out how to construct the expression

--- hail/python/hail/nd/__init__.py
+++ hail/python/hail/nd/__init__.py
@@ -1,3 +1,3 @@
-from .nd import array, arange, full, zeros, ones
+from .nd import array, arange, full, zeros, ones, qr
 
-__all__ = ["array", "arange", "full", "zeros", "ones"]
+__all__ = ["array", "arange", "full", "zeros", "ones", "qr"]
