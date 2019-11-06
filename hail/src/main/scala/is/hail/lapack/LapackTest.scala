9ee0ca1a06bc7e884e44a12b60befe03d972f535
John Compitello <j301600@gmail.com>
2019-11-06 09:55:25 -0500

    Needed to multiply by 8 for memory offsets

--- hail/src/main/scala/is/hail/lapack/LapackTest.scala
+++ hail/src/main/scala/is/hail/lapack/LapackTest.scala
@@ -37,42 +37,51 @@ object LapackTest {
     val bMem = new Memory (8 * 16) //HailMemory.malloc(8 * 16)
     val cMem = new Memory(8 * 16) //HailMemory.malloc(8 * 16)
 
-    println(aMem.getDouble(0L))
+//    println(aMem.getDouble(0L))
+//    aMem.setDouble(0L, 100.0)
+//    println(aMem.getDouble(0L))
 
     (0 until 16).foreach{idx =>
-      aMem.setDouble(idx.toLong, 1.0)
-      bMem.setDouble(idx.toLong, 1.0)
-      cMem.setDouble(idx.toLong, 0.0)
+      val adjusted = idx.toLong * 8
+      aMem.setDouble(adjusted, 1.0)
+      println("Set A in loop")
+      bMem.setDouble(adjusted, 1.0)
+      //cMem.setDouble(idx.toLong, 0.0)
     }
 
-    println(aMem.getDouble(0L))
-    println(cMem.getDouble(0L))
 
+    println(aMem.getDouble(0L))
+//    println(cMem.getDouble(0L))
+//
+    val TRANSA = new IntByReference('n'.toInt)
+    val TRANSB = new IntByReference('n'.toInt)
+    val N = new IntByReference(4)
+    val M = new IntByReference(4)
+    val K = new IntByReference(4)
     val LDA = new IntByReference(4)
     val LDB = new IntByReference(4)
     val LDC = new IntByReference(4)
-
-    BLASLibrary.instance.dgemm(new IntByReference('n'.toInt), new IntByReference('n'),
-      new IntByReference(4), new IntByReference(4),
-      new IntByReference(4), new DoubleByReference(1.0), aMem,
+//
+    BLASLibrary.instance.dgemm(TRANSA, TRANSB,
+      M, N, K, new DoubleByReference(1.0), aMem,
       LDA, bMem, LDB, new DoubleByReference(1.0),
       cMem, LDC)
-
-    println(aMem.getDouble(0L))
-    println(cMem.getDouble(0L))
-
-    //println(LAPACKLibrary.instance.disnan(4.3))
-//    var x = 3.0
-//    var y = 4.0
-//    var z = LAPACKLibrary.instance.dlapy2(new DoubleByReference(x), new DoubleByReference(y))
 //
-//    println(z)
-    //println(res)
-    /**
-      * Code.invokeStatic[BLAS, BLAS]("getInstance").dgemm("n", "n", getLeftShapeAtIdx(0).toI, getRightShapeAtIdx(1).toI,
-      * getLeftShapeAtIdx(1).toI, 1.0, a, getLeftShapeAtIdx(0).toI,
-      * b, getRightShapeAtIdx(0).toI, 0, c, getLeftShapeAtIdx(0).toI),
-      */
+//    println(aMem.getDouble(0L))
+//    println(cMem.getDouble(0L))
+//
+//    //println(LAPACKLibrary.instance.disnan(4.3))
+////    var x = 3.0
+////    var y = 4.0
+////    var z = LAPACKLibrary.instance.dlapy2(new DoubleByReference(x), new DoubleByReference(y))
+////
+////    println(z)
+//    //println(res)
+//    /**
+//      * Code.invokeStatic[BLAS, BLAS]("getInstance").dgemm("n", "n", getLeftShapeAtIdx(0).toI, getRightShapeAtIdx(1).toI,
+//      * getLeftShapeAtIdx(1).toI, 1.0, a, getLeftShapeAtIdx(0).toI,
+//      * b, getRightShapeAtIdx(0).toI, 0, c, getLeftShapeAtIdx(0).toI),
+//      */
 
     System.exit(0)
   }
