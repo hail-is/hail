143bc290c463bd099b09e466037c7b9a92e2939a
John Compitello <j301600@gmail.com>
2019-11-07 14:38:06 -0500

    Moved LAPACKLibrary into linalg, started setting up CodeLAPACK

---
+++ hail/src/main/scala/is/hail/linalg/LAPACKLibrary.scala
@@ -0,0 +1,14 @@
+package is.hail.linalg
+
+import com.sun.jna.{Library, Native, Pointer}
+import com.sun.jna.ptr.DoubleByReference
+
+object LAPACKLibrary {
+  val instance = Native.loadLibrary("lapack", classOf[LAPACKLibrary]).asInstanceOf[LAPACKLibrary]
+}
+
+trait LAPACKLibrary extends Library {
+  def dsyevd(JOBZ: Char, UPLO: Char, N: Int, A: Pointer, LDA: Int, W: Pointer, WORK: Pointer, LWORK: Int, IWORK: Pointer, LIWORK: Int, INFO: Int)
+  //def disnan(DIN: Double)
+  def dlapy2(X: DoubleByReference, Y: DoubleByReference): Double
+}
