43cf177d4ed358111a8d3d42bef980ff1f0253c6
John Compitello <j301600@gmail.com>
2019-11-07 14:56:06 -0500

    Added 8 argument invoke, CodeLapack calls dgeqrf

--- hail/src/main/scala/is/hail/asm4s/Code.scala
+++ hail/src/main/scala/is/hail/asm4s/Code.scala
@@ -712,7 +712,9 @@ class CodeArray[T](val lhs: Code[Array[T]])(implicit tti: TypeInfo[T]) {
 
 class CodeLapack(val cl: Code[LAPACKLibrary]) {
   //LDA: Length of one row in row major order, or length of one column in column major order
-  //def dgeqrf(M: ???, N: ???, A: Code[Long], LDA: ???, TAU: ???, WORK: Code[Long], LWORK: ???, INFO: ???)
+  def dgeqrf(M: Code[Long], N: Code[Long], A: Code[Long], LDA: Code[Long], TAU: Code[Long], WORK: Code[Long], LWORK: Code[Long], INFO: Code[Long]): Code[Unit] = {
+    cl.invoke[Long, Long, Long, Long, Long, Long, Long, Long, Unit]("dgeqrf", M, N, A, LDA, TAU, WORK, LWORK, INFO)
+  }
 }
 
 object Invokeable {
@@ -945,6 +947,14 @@ class CodeObject[T <: AnyRef : ClassTag](val lhs: Code[T]) {
   def invoke[A1, A2, A3, A4, A5, S](method: String, a1: Code[A1], a2: Code[A2], a3: Code[A3], a4: Code[A4], a5: Code[A5])
     (implicit a1ct: ClassTag[A1], a2ct: ClassTag[A2], a3ct: ClassTag[A3], a4ct: ClassTag[A4], a5ct: ClassTag[A5], sct: ClassTag[S]): Code[S] =
     invoke[S](method, Array[Class[_]](a1ct.runtimeClass, a2ct.runtimeClass, a3ct.runtimeClass, a4ct.runtimeClass, a5ct.runtimeClass), Array[Code[_]](a1, a2, a3, a4, a5))
+
+  def invoke[A1, A2, A3, A4, A5, A6, A7, A8, S](method: String, a1: Code[A1], a2: Code[A2], a3: Code[A3], a4: Code[A4],
+    a5: Code[A5], a6: Code[A6], a7: Code[A7], a8: Code[A8])
+    (implicit a1ct: ClassTag[A1], a2ct: ClassTag[A2], a3ct: ClassTag[A3], a4ct: ClassTag[A4], a5ct: ClassTag[A5],
+    a6ct: ClassTag[A6], a7ct: ClassTag[A7], a8ct: ClassTag[A8], sct: ClassTag[S]): Code[S] = {
+    invoke[S](method, Array[Class[_]](a1ct.runtimeClass, a2ct.runtimeClass, a3ct.runtimeClass, a4ct.runtimeClass, a5ct.runtimeClass,
+      a6ct.runtimeClass, a7ct.runtimeClass, a8ct.runtimeClass), Array[Code[_]](a1, a2, a3, a4, a5, a6, a7, a8))
+  }
 }
 
 class CodeNullable[T >: Null : TypeInfo](val lhs: Code[T]) {
