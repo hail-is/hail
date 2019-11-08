3d90a1be598a9d51c679e3f4bd6e82c080d57674
Jackie Goldstein <jigold@broadinstitute.org>
2019-11-08 10:40:50 -0500

    fixes

--- batch2/test/test_batch.py
+++ batch2/test/test_batch.py
@@ -34,7 +34,7 @@ def poll_until(p, max_polls=None):
 
 class Test(unittest.TestCase):
     def setUp(self):
-        self.client = BatchClient(_service='batch2')
+        self.client = BatchClient()
 
     def tearDown(self):
         self.client.close()
@@ -327,7 +327,7 @@ class Test(unittest.TestCase):
 
     def test_bad_token(self):
         token = base64.urlsafe_b64encode(secrets.token_bytes(32)).decode('ascii')
-        bc = BatchClient(_token=token, _service='batch2')
+        bc = BatchClient(_token=token)
         try:
             b = bc.create_batch()
             j = b.create_job('ubuntu:18.04', ['false'])
