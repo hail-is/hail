2dff6e311989330594c1532345da85635ebde270
Tim Poterba <tpoterba@broadinstitute.org>
2019-11-21 16:53:14 -0500

    [hail] Delete Scala `MatrixTable` class (#7578)
    * [hail] Delete Scala `MatrixTable` class
    
    * Move some MatrixIRSuite tests to Python
    * delete covered MatrixIRSuite test
    
    * womp

--- hail/src/test/scala/is/hail/testUtils/package.scala
+++
@@ -1,8 +0,0 @@
-package is.hail
-
-import is.hail.variant.MatrixTable
-import scala.language.implicitConversions
-
-package object testUtils {
-  implicit def toRichMatrixTable(mt: MatrixTable): RichMatrixTable = new RichMatrixTable(mt)
-}
