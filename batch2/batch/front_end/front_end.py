5192f729ae26da2eb433a5649fd51f7beac9369a
Jackie Goldstein <jigold@broadinstitute.org>
2019-11-21 12:11:23 -0500

    delint

--- batch2/batch/front_end/front_end.py
+++ batch2/batch/front_end/front_end.py
@@ -293,7 +293,7 @@ WHERE user = %s AND id = %s AND NOT deleted;
                     f'are unsatisfiable: cpu={resources["cpu"]}, memory={resources["memory"]}')
                 if cores_mcpu == 0:
                     raise web.HTTPBadRequest(reason=f'resource requests for job {id} '
-                                             f'are unsatisfiable: cpu must be greater than 0')
+                    f'are unsatisfiable: cpu must be greater than 0')
 
                 secrets = spec.get('secrets')
                 if not secrets:
