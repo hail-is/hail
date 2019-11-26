e54f2a2b9bf398c661f90538b09554dd25b2ade5
Daniel King <dking@broadinstitute.org>
2019-11-26 18:18:23 -0500

    fix

--- gear/gear/auth_utils.py
+++ gear/gear/auth_utils.py
@@ -7,11 +7,11 @@ async def insert_user(dbpool, spec):
         async with conn.cursor() as cursor:
             await cursor.execute(
                 '''
-INSERT INTO user_data (username, user_id, gsa_email, bucket_name, gsa_key_secret_name, jwt_secret_name,
+INSERT INTO user_data (username, user_id, developer, gsa_email, bucket_name, gsa_key_secret_name, jwt_secret_name,
     service_account)
-VALUES (%s, %s, %s, %s, %s, %s, %s)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 ''',
-                (spec['username'], spec['user_id'], spec['gsa_email'], spec['bucket_name'], spec['gsa_key_secret_name'], spec['jwt_secret_name'],
+                (spec['username'], spec['user_id'], spec.get('developer', 0), spec['gsa_email'], spec['bucket_name'], spec['gsa_key_secret_name'], spec['jwt_secret_name'],
                  spec.get('service_account')))
 
         async with conn.cursor() as cursor:
