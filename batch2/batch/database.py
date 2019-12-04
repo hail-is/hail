6ca0af6d59ab38252681949d84e4190071ed6239
Jackie Goldstein <jigold@broadinstitute.org>
2019-12-04 14:12:58 -0500

    fix?

--- batch2/batch/database.py
+++ batch2/batch/database.py
@@ -61,7 +61,7 @@ class LeasedDatabase:
 
     async def execute_and_fetchall(self, sql, args=None):
         await self.lease.wait()
-        return await self.db.execute_and_fetchall(sql, args)
+        return self.db.execute_and_fetchall(sql, args)
 
     async def execute_insertone(self, sql, args=None):
         await self.lease.wait()
