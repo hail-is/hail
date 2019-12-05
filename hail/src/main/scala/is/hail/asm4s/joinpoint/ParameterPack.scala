9f2e22ec4f479ff81364dad9aebff449374baf4c
Milo <iitalics@gmail.com>
2019-12-05 15:06:27 -0500

    [hail] [streams] more complicated streams (#7229)
    * some more ParameterPack utilities
    
    * create EmitStream interface; implements ArrayRange + ToStream + NA
    
    standalone emitstream missing impl
    
    make NA case trivially easy
    
    simplify range impl
    
    * EmitStream test suite
    
    fix the Let test
    
    * stream impl of ArrayMap, ArrayFilter
    
    add Skip
    
    other streams
    
    add tests
    
    delete all but map/filter
    
    delete tests
    
    * stream impl of ArrayMake
    
    simplify makeArray implementation
    
    * EmitStream tests for map, filter
    
    * EmitStream tests for make
    
    * remove Empty in favor of "empty state" that gives an empty stream
    
    delete Empty; rename dummyState => emptyState
    
    get rid of Empty in toArrayIterator
    
     dummyState to emptyState in missing/range
    
    get rid of Empty in test suite
    
    delete Empty in map, filter
    
    rename dummyState in make
    
    * sequence.length fix
    
    * stream impl of ArrayFlatMap
    
    leftjoin test
    
    clean up compose impl
    
    less code dup in EmitStream.compose by doing Skip(..dummyState..)
    
    flatmap new emptyState version
    
    * TypedTriplet utility for ParameterPacking values associated w PType
    
    * stream impl of ArrayLeftJoinDistinct
    
    leftjoin new emptyState version
    
    * EmitStream tests for flatMap
    
    get rid of Empty in compose
    
    get rid of empty in compose (2)
    
    * EmitStream tests for leftJoinDistinct
    
    get rid of Empty in leftjoin
    
    * stream impl of Array(Agg)Scan
    
    add all but map/filter
    
    add tests
    
    more complex let test
    
    ...more
    
    get rid of Empty in scan
    
    * EmitStream tests for scan, aggScan
    
    * more type annotations
    
    * use HailSuite instead of TestNGSuite
    
    * rename ParameterPack."localMemoize" => "let"
    
    * make TypedTriplet constructor private
    
    * guardParam (sort of like contramap) to simplify emitStream
    
    * remove Skip()
    
    * avoid calling some continuations more than once
    
    avoid calling the continuation too many times in EmitStream.sequence
    
    avoid calling the Missing continuation in guardParam
    
    * omit 'canGenerateLiterals' since it was deleted in prev commit
    
    * fix ArrayAggScan again by making ExecuteContext's

--- hail/src/main/scala/is/hail/asm4s/joinpoint/ParameterPack.scala
+++ hail/src/main/scala/is/hail/asm4s/joinpoint/ParameterPack.scala
@@ -1,6 +1,9 @@
 package is.hail.asm4s.joinpoint
 
 import is.hail.asm4s._
+import is.hail.expr.ir
+import is.hail.expr.ir.EmitTriplet
+import is.hail.expr.types.physical.PType
 
 object ParameterPack {
   implicit val unit: ParameterPack[Unit] = new ParameterPack[Unit] {
@@ -42,6 +45,12 @@ object ParameterPack {
       ParameterStore(Code(cs.store, bs.store, as.store), (as.load, bs.load, cs.load))
     }
   }
+
+  def let[A: ParameterPack, X](mb: MethodBuilder, a0: A)(k: A => Code[X]): Code[X] = {
+    val ap = implicitly[ParameterPack[A]]
+    val as = ap.newLocals(mb)
+    Code(ap.push(a0), as.store, k(as.load))
+  }
 }
 
 object ParameterStore {
@@ -56,4 +65,44 @@ trait ParameterPack[A] {
 case class ParameterStore[A](
   store: Code[Unit],
   load: A
-)
+) {
+  def :=(v: A)(implicit p: ParameterPack[A]): Code[Unit] =
+    Code(p.push(v), store)
+
+  def :=(cc: JoinPoint.CallCC[A]): Code[Unit] =
+    Code(cc.code, store)
+}
+
+object TypedTriplet {
+  def apply(t: PType, et: EmitTriplet): TypedTriplet[t.type] =
+    TypedTriplet(et.setup, et.m, et.v)
+
+  def missing(t: PType): TypedTriplet[t.type] =
+    TypedTriplet(t, EmitTriplet(Code._empty, true, ir.defaultValue(t)))
+
+  class Pack[P] private[joinpoint](t: PType) extends ParameterPack[TypedTriplet[P]] {
+    def push(trip: TypedTriplet[P]): Code[Unit] = Code(
+      trip.setup,
+      trip.m.mux(
+        Code(coerce[Unit](ir.defaultValue(t)), coerce[Unit](const(true))),
+        Code(coerce[Unit](trip.v), coerce[Unit](const(false)))))
+
+    def newLocals(mb: MethodBuilder): ParameterStore[TypedTriplet[P]] = {
+      val m = mb.newLocal[Boolean]("m")
+      val v = mb.newLocal("v")(ir.typeToTypeInfo(t))
+      ParameterStore(Code(m.storeInsn, v.storeInsn), TypedTriplet(Code._empty, m, v))
+    }
+
+    def newFields(fb: FunctionBuilder[_], name: String): (Settable[Boolean], Settable[_]) =
+      (fb.newField[Boolean](name + "_missing"), fb.newField(name)(ir.typeToTypeInfo(t)))
+  }
+
+  def pack(t: PType): Pack[t.type] = new Pack(t)
+}
+
+case class TypedTriplet[P] private(setup: Code[Unit], m: Code[Boolean], v: Code[_]) {
+  def untyped: EmitTriplet = EmitTriplet(setup, m, v)
+
+  def storeTo(dm: Settable[Boolean], dv: Settable[_]): Code[Unit] =
+    Code(setup, dm := m, (!dm).orEmpty(dv.storeAny(v)))
+}
