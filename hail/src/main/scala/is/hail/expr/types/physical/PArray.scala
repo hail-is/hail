1ceecf7cda3c8eef5e0134cb315047da15f9088c
Alex Kotlar <akotlar@bu.edu>
2019-12-05 05:44:47 -0500

    wip

--- hail/src/main/scala/is/hail/expr/types/physical/PArray.scala
+++ hail/src/main/scala/is/hail/expr/types/physical/PArray.scala
@@ -8,36 +8,8 @@ import is.hail.expr.types.virtual.TArray
 import is.hail.utils._
 import scala.reflect.{ClassTag, _}
 
-final case class PArray(elementType: PType, override val required: Boolean = false) extends PContainer with PStreamable {
-  lazy val virtualType: TArray = TArray(elementType.virtualType, required)
-
-  val elementByteSize: Long = UnsafeUtils.arrayElementSize(elementType)
-
-  val contentsAlignment: Long = elementType.alignment.max(4)
-
-  override def pyString(sb: StringBuilder): Unit = {
-    sb.append("array<")
-    elementType.pyString(sb)
-    sb.append('>')
-  }
-  override val fundamentalType: PArray = {
-    if (elementType == elementType.fundamentalType)
-      this
-    else
-      this.copy(elementType = elementType.fundamentalType)
-  }
-
-  def _asIdent = s"array_of_${elementType.asIdent}"
-  def _toPretty = s"Array[$elementType]"
-
-  override def _pretty(sb: StringBuilder, indent: Int, compact: Boolean = false) {
-    sb.append("Array[")
-    elementType.pretty(sb, indent, compact)
-    sb.append("]")
-  }
-
-  def codeOrdering(mb: EmitMethodBuilder, other: PType): CodeOrdering = {
-    assert(this isOfType other)
-    CodeOrdering.iterableOrdering(this, other.asInstanceOf[PArray], mb)
+abstract class PArray extends PContainer with PStreamable {
+  def toConcrete(t: PArray) = t match {
+    case x: PCanonicalArray => x
   }
 }
