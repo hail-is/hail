fb69421f4a10357406f4382baa914473b11f69a9
Cotton Seed <cotton@alum.mit.edu>
2019-12-12 00:48:16 +0000

    fixes

--- batch/batch/front_end/front_end.py
+++ batch/batch/front_end/front_end.py
@@ -862,13 +862,12 @@ WHERE billing_projects.name = %s;
 ''',
         (billing_project, user, billing_project))
     if not row:
-        set_message(session, 'No such billing project {billing_project}.', 'error')
+        set_message(session, f'No such billing project {billing_project}.', 'error')
         raise web.HTTPFound(deploy_config.url('batch', f'/billing_projects'))
-    assert len(row) == 1
     assert row['billing_project'] == billing_project
 
     if row['user'] is None:
-        set_message(session, 'User {user} is not member of billing project {billing_project}.', 'info')
+        set_message(session, f'User {user} is not member of billing project {billing_project}.', 'info')
         return web.HTTPFound(deploy_config.url('batch', f'/billing_projects'))
 
     await db.just_execute(
@@ -878,7 +877,7 @@ WHERE billing_project = %s AND user = %s;
 ''',
         (billing_project, user))
 
-    set_message(session, 'Removed user {user} from billing project {billing_project}.', 'info')
+    set_message(session, f'Removed user {user} from billing project {billing_project}.', 'info')
     return web.HTTPFound(deploy_config.url('batch', f'/billing_projects'))
 
 
@@ -906,11 +905,11 @@ WHERE billing_projects.name = %s;
 ''',
         (billing_project, user, billing_project))
     if row is None:
-        set_message(session, 'No such billing project {billing_project}.', 'error')
+        set_message(session, f'No such billing project {billing_project}.', 'error')
         raise web.HTTPFound(deploy_config.url('batch', f'/billing_projects'))
 
     if row['user'] is not None:
-        set_message(session, 'User {user} is already member of billing project {billing_project}.', 'info')
+        set_message(session, f'User {user} is already member of billing project {billing_project}.', 'info')
         return web.HTTPFound(deploy_config.url('batch', f'/billing_projects'))
 
     await db.execute_insertone(
@@ -920,7 +919,7 @@ VALUES (%s, %s);
 ''',
         (billing_project, user))
 
-    set_message(session, 'Added user {user} to billing project {billing_project}.', 'info')
+    set_message(session, f'Added user {user} to billing project {billing_project}.', 'info')
     return web.HTTPFound(deploy_config.url('batch', f'/billing_projects'))
 
 
@@ -942,7 +941,7 @@ WHERE name = %s;
 ''',
         (billing_project))
     if row is not None:
-        set_message(session, ' Billing project {billing_project} already exists.', 'error')
+        set_message(session, f'Billing project {billing_project} already exists.', 'error')
         raise web.HTTPFound(deploy_config.url('batch', f'/billing_projects'))
 
     await db.execute_insertone(
@@ -952,7 +951,7 @@ VALUES (%s);
 ''',
         (billing_project))
 
-    set_message(session, 'Added billing project {billing_project}.', 'info')
+    set_message(session, f'Added billing project {billing_project}.', 'info')
     return web.HTTPFound(deploy_config.url('batch', f'/billing_projects'))
 
 
