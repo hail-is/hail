af061f01fbf3035f32452a1ca5b2a5cd89f3f544
Tim Poterba <tpoterba@broadinstitute.org>
2019-12-16 09:02:58 -0500

    [hail] fix semantic version ordering (#7732)
    * [hail] fix semantic version ordering
    
    * [hail] fix semantic version ordering

---
+++ hail/src/test/scala/is/hail/utils/SemanticVersionSuite.scala
@@ -0,0 +1,28 @@
+package is.hail.utils
+
+import org.scalatest.testng.TestNGSuite
+import org.testng.annotations.Test
+
+class SemanticVersionSuite extends TestNGSuite {
+  @Test def testOrdering() {
+    val versions = Array(
+      SemanticVersion(1, 1, 0),
+      SemanticVersion(1, 1, 1),
+      SemanticVersion(1, 2, 2),
+      SemanticVersion(1, 2, 3),
+      SemanticVersion(1, 3, 0),
+      SemanticVersion(1, 3, 1),
+      SemanticVersion(2, 0, 0),
+      SemanticVersion(2, 0, 1))
+
+    versions.zipWithIndex.foreach { case (v, i) =>
+      (0 until i).foreach { j =>
+        assert(v > versions(j))
+      }
+
+      (i + 1 until versions.length).foreach { j =>
+        assert(v < versions(j))
+      }
+    }
+  }
+}
