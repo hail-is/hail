b051ff88840b26416344535f704a8e61ecd7b4b6
John Compitello <johnc@broadinstitute.org>
2019-12-16 10:29:12 -0500

    Benchmark NDArray Addition and Multiplication (#7724)
    * Wrote a benchmark for matrix multiply
    
    * ndarray addition benchmark, separate int64 and float64 multiplication benchmarks
    
    * bump!

--- benchmark/python/benchmark_hail/run/linalg_benchmarks.py
+++ benchmark/python/benchmark_hail/run/linalg_benchmarks.py
@@ -15,3 +15,18 @@ def make_ndarray_bench():
     ht = hl.utils.range_table(200_000)
     ht = ht.annotate(x=hl._nd.array(hl.range(200_000)))
     ht._force_count()
+
+@benchmark()
+def ndarray_addition_benchmark():
+    arr = hl._nd.ones((1024, 1024))
+    hl.eval(arr + arr)
+
+@benchmark()
+def ndarray_matmul_int64_benchmark():
+    arr = hl._nd.arange(1024 * 1024).map(hl.int64).reshape((1024, 1024))
+    hl.eval(arr @ arr)
+
+@benchmark()
+def ndarray_matmul_float64_benchmark():
+    arr = hl._nd.arange(1024 * 1024).map(hl.float64).reshape((1024, 1024))
+    hl.eval(arr @ arr)
