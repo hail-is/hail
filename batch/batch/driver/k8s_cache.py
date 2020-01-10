f1ef7f3b517918b4e9d0fdf4d31ee89d7f728bec
Jackie Goldstein <jigold@broadinstitute.org>
2020-01-10 11:41:48 -0500

    fix

--- batch/batch/driver/k8s_cache.py
+++ batch/batch/driver/k8s_cache.py
@@ -8,46 +8,46 @@ class K8sCache:
         self.refresh_time = refresh_time
         self.max_size = max_size
 
-        self.secrets_update_time = {}
-        self.secrets = sortedcontainers.SortedDict(
-            key=lambda id: self.secrets_update_time[id])
+        self.secrets = {}
+        self.secret_ids = sortedcontainers.SortedSet(
+            key=lambda id: self.secrets[id][1])
 
-        self.service_accounts_update_time = {}
-        self.service_accounts = sortedcontainers.SortedDict(
-            key=lambda id: self.service_accounts_update_time[id])
+        self.service_accounts = {}
+        self.service_account_ids = sortedcontainers.SortedSet(
+            key=lambda id: self.service_accounts[id][1])
 
     async def read_secret(self, name, namespace, timeout):
         id = (name, namespace)
 
-        time_updated = self.secrets_update_time.get(id, None)
+        secret, time_updated = self.secrets.get(id, (None, None))
         if time_updated and time.time() < time_updated + self.refresh_time:
-            secret = self.secrets[id]
             return secret
 
         if len(self.secrets) == self.max_size:
-            self.secrets.popitem(0)
+            head_id = self.secret_ids.pop(0)
+            del self.secrets[head_id]
 
         secret = await self.client.read_namespaced_secret(
             name, namespace, _request_timeout=timeout)
-        self.secrets_update_time[id] = time.time()
-        self.secrets[id] = secret
+        self.secrets[id] = (secret, time.time())
+        self.secret_ids.add(id)
 
         return secret
 
     async def read_service_account(self, name, namespace, timeout):
         id = (name, namespace)
 
-        time_updated = self.service_accounts_update_time.get(id, None)
+        sa, time_updated = self.service_accounts.get(id, (None, None))
         if time_updated and time.time() < time_updated + self.refresh_time:
-            sa = self.service_accounts[id]
             return sa
 
         if len(self.service_accounts) == self.max_size:
-            self.service_accounts.popitem(0)
+            head_id = self.service_account_ids.pop(0)
+            del self.service_accounts[head_id]
 
         sa = await self.client.read_namespaced_service_account(
             name, namespace, _request_timeout=timeout)
-        self.service_accounts_update_time[id] = time.time()
-        self.service_accounts[id] = sa
+        self.service_accounts[id] = (sa, time.time())
+        self.service_account_ids.add(id)
 
         return sa
