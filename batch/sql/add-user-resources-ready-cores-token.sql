b562d77bc57e8aac1ea045394268ec7d68b3ba45
Jackie Goldstein <jigold@broadinstitute.org>
2020-01-14 18:08:27 -0500

    add indexes

--- batch/sql/add-user-resources-ready-cores-token.sql
+++ batch/sql/add-user-resources-ready-cores-token.sql
@@ -32,6 +32,8 @@ CREATE TEMPORARY TABLE tmp_resources AS (
   GROUP BY batch_id, user, closed
 );
 ALTER TABLE tmp_resources ADD INDEX (user);
+ALTER TABLE tmp_resources ADD INDEX (batch_id);
+ALTER TABLE tmp_resources ADD INDEX (closed);
 
 SELECT COALESCE(SUM(ready_cores_mcpu), 0) INTO @closed_ready_cores_mcpu
 FROM tmp_resources
