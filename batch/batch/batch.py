4b4db6b9af86f776b7d9d70b872a4d81bbda6ea2
jigold <jigold@users.noreply.github.com>
2020-01-17 12:24:09 -0500

    [batch] worker shouldn't mark complete for deleted job (#7890)
    * [batch] worker shouldn't mark complete for deleted job
    
    * fix lint error
    
    * bump

--- batch/batch/batch.py
+++ batch/batch/batch.py
@@ -365,14 +365,14 @@ async def schedule_job(app, record, instance):
                        f'/api/v1alpha/batches/jobs/create')
                 await session.post(url, json=body)
                 await instance.mark_healthy()
-        except Exception as e:
-            if (isinstance(e, aiohttp.ClientResponseError) and
-                    e.status == 403):  # pylint: disable=no-member
-                await instance.mark_healthy()
+        except aiohttp.ClientResponseError as e:
+            await instance.mark_healthy()
+            if e.status == 403:
                 log.info(f'attempt already exists for job {id} on {instance}, aborting')
-            else:
-                await instance.incr_failed_request_count()
             raise e
+        except Exception:
+            await instance.incr_failed_request_count()
+            raise
 
         log.info(f'schedule job {id} on {instance}: called create job')
 
