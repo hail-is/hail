401ab30032cd85edc98e0ffae585e057a643c450
Daniel King <dking@broadinstitute.org>
2020-01-17 14:25:54 -0500

    add index

--- batch/sql/add-batch-create-token.sql
+++ batch/sql/add-batch-create-token.sql
@@ -1,1 +1,2 @@
-ALTER TABLE `batches` ADD COLUMN (token VARCHAR(100) DEFAULT NULL);
+ALTER TABLE `batches` ADD COLUMN (`token` VARCHAR(100) DEFAULT NULL);
+CREATE INDEX `batches_token` ON `batches` (`token`);
