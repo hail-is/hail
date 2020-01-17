9287d0842da05bc75f416ad6d4e0ffdf3c49407f
Tim Poterba <tpoterba@gmail.com>
2020-01-17 12:39:57 -0500

    fix

--- hail/src/main/scala/is/hail/expr/ir/Simplify.scala
+++ hail/src/main/scala/is/hail/expr/ir/Simplify.scala
@@ -711,7 +711,7 @@ object Simplify {
         && child.typ.key.nonEmpty && canRepartition =>
       TableAggregateByKey(child, expr)
 
-    case TableAggregateByKey(TableKeyBy(child, keys, false), expr) if canRepartition && ! child.typ.key.startsWith(keys) =>
+    case TableAggregateByKey(x@TableKeyBy(child, keys, false), expr) if canRepartition && !x.definitelyDoesNotShuffle =>
       TableKeyByAndAggregate(child, expr, MakeStruct(keys.map(k => k -> GetField(Ref("row", child.typ.rowType), k))))
 
     case TableParallelize(TableCollect(child), _) if isDeterministicallyRepartitionable(child) => child
