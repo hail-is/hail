e503929198c615b00fef4ee90f127749080ef64e
Daniel King <dking@broadinstitute.org>
2020-01-17 19:17:44 -0500

    [batch] fix insertion bug error
    There's a lot of error codes that indicate duplicate row for key

--- batch/batch/front_end/front_end.py
+++ batch/batch/front_end/front_end.py
@@ -500,9 +500,10 @@ VALUES (%s, %s, %s, %s, %s, %s, %s);
 ''',
                                           jobs_args)
                 except pymysql.err.IntegrityError as err:
-                    if err.args[1] == 1022:
+                    if err.args[1] in (1022, 1062, 1586, 1859, 3026):
                         log.info(f'bunch containing job {(batch_id, jobs_args[0][1])} already inserted ({err})')
                         return web.Response()
+                    raise
                 await tx.execute_many('''
 INSERT INTO `job_parents` (batch_id, job_id, parent_id)
 VALUES (%s, %s, %s);
