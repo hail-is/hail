976e747b8507562bf21cbb11213615a03cd9d4ac
Tim Poterba <tpoterba@broadinstitute.org>
2020-02-03 16:47:22 -0500

    [hail] Remove `window_by_locus` (#7947)
    Experimental, essentially unused, and quite hard to lower.

--- hail/python/test/hail/methods/test_misc.py
+++ hail/python/test/hail/methods/test_misc.py
@@ -184,25 +184,6 @@ class Tests(unittest.TestCase):
                                  hl.Struct(locus=hl.Locus('20', 10644700), alleles=['A', 'T']))]
         self.assertEqual(hl.filter_intervals(ds, intervals).count_rows(), 3)
 
-    def test_window_by_locus(self):
-        mt = hl.utils.range_matrix_table(100, 2, n_partitions=10)
-        mt = mt.annotate_rows(locus=hl.locus('1', mt.row_idx + 1))
-        mt = mt.key_rows_by('locus')
-        mt = mt.annotate_entries(e_row_idx=mt.row_idx, e_col_idx=mt.col_idx)
-        mt = hl.window_by_locus(mt, 5).cache()
-
-        self.assertEqual(mt.count_rows(), 100)
-
-        rows = mt.rows()
-        self.assertTrue(rows.all((rows.row_idx < 5) | (rows.prev_rows.length() == 5)))
-        self.assertTrue(rows.all(hl.all(lambda x: (rows.row_idx - 1 - x[0]) == x[1].row_idx,
-                                        hl.zip_with_index(rows.prev_rows))))
-
-        entries = mt.entries()
-        self.assertTrue(entries.all(hl.all(lambda x: x.e_col_idx == entries.col_idx, entries.prev_entries)))
-        self.assertTrue(entries.all(hl.all(lambda x: entries.row_idx - 1 - x[0] == x[1].e_row_idx,
-                                           hl.zip_with_index(entries.prev_entries))))
-
     def test_summarize_variants(self):
         mt = hl.utils.range_matrix_table(3, 3)
         variants = hl.literal({0: hl.Struct(locus=hl.Locus('1', 1), alleles=['A', 'T', 'C']),
