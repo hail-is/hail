a0137741f5767ee88e89e10215d287aedac2a875
wang <wang@broadinstitute.org>
2020-02-11 12:05:19 -0500

    implement Pretty

--- hail/src/main/scala/is/hail/expr/ir/Pretty.scala
+++ hail/src/main/scala/is/hail/expr/ir/Pretty.scala
@@ -294,6 +294,7 @@ object Pretty {
             case NDArrayMap(_, name, _) => prettyIdentifier(name)
             case NDArrayMap2(_, _, lName, rName, _) => prettyIdentifier(lName) + " " + prettyIdentifier(rName)
             case NDArrayReindex(_, indexExpr) => prettyInts(indexExpr)
+            case NDArrayConcat(_, axis) => axis.toString
             case NDArrayAgg(_, axes) => prettyInts(axes)
             case ArraySort(_, l, r, _) => prettyIdentifier(l) + " " + prettyIdentifier(r)
             case ApplyIR(function, _) => prettyIdentifier(function) + " " + ir.typ.parsableString()
