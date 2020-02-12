b4022e7b9705cf2088439c5e92ac89311d4619df
Chris Vittal <chris@vittal.dev>
2020-02-12 12:19:45 -0500

    Add python only combiner benchmarks

--- benchmark/python/benchmark_hail/run/combiner_benchmarks.py
+++ benchmark/python/benchmark_hail/run/combiner_benchmarks.py
@@ -15,12 +15,30 @@ def chunks(seq, size):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
 
+def setup(path):
+    interval = [hl.eval(hl.parse_locus_interval('chr1:START-END', reference_genome='GRCh38'))]
+    return hl.import_vcfs([path], interval, reference_genome='GRCh38')[0]
+
+
 @benchmark(args=empty_gvcf.handle())
 def compile_10k_merge(path):
-    interval = [hl.eval(hl.parse_locus_interval('chr1:START-END', reference_genome='GRCh38'))]
-    vcfs = hl.import_vcfs([path], interval, reference_genome='GRCh38')
-    vcfs = vcfs * MAX_TO_COMBINE
+    vcf = setup(path)
+    vcfs = [vcf] * MAX_TO_COMBINE
     mts = [comb.transform_gvcf(vcf) for vcf in vcfs]
     combined = [comb.combine_gvcfs(mts) for mts in chunks(mts, COMBINE_GVCF_MAX)]
     with TemporaryDirectory() as tmpdir:
         hl.experimental.write_matrix_tables(combined, os.path.join(tmpdir, 'combiner-multi-write'), overwrite=True)
+
+
+@benchmark(args=empty_gvcf.handle())
+def python_only_10k_transform(path):
+    vcf = setup(path)
+    vcfs = [vcf] * 10_000
+    _ = [comb.transform_gvcf(vcf) for vcf in vcfs]
+
+@benchmark(args=empty_gvcf.handle())
+def python_only_10k_combine(path):
+    vcf = setup(path)
+    mt = comb.transform_gvcf(vcf)
+    mts = [mt] * 10_000
+    _ = [comb.combine_gvcfs(mts) for mts in chunks(mts, COMBINE_GVCF_MAX)]
