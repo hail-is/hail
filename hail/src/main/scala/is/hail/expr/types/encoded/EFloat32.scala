1277fc0bf9e48d3d520b6045560a8322a7de920c
Tim Poterba <tpoterba@gmail.com>
2020-02-16 12:43:47 -0500

    [hail] Refactor getOrDefineMethod to be on FB, not EFB

--- hail/src/main/scala/is/hail/expr/types/encoded/EFloat32.scala
+++ hail/src/main/scala/is/hail/expr/types/encoded/EFloat32.scala
@@ -2,7 +2,6 @@ package is.hail.expr.types.encoded
 
 import is.hail.annotations.Region
 import is.hail.asm4s._
-import is.hail.expr.ir.EmitMethodBuilder
 import is.hail.expr.types.BaseType
 import is.hail.expr.types.physical._
 import is.hail.expr.types.virtual._
@@ -13,18 +12,18 @@ case object EFloat32Optional extends EFloat32(false)
 case object EFloat32Required extends EFloat32(true)
 
 class EFloat32(override val required: Boolean) extends EType {
-  def _buildEncoder(pt: PType, mb: EmitMethodBuilder, v: Code[_], out: Code[OutputBuffer]): Code[Unit] = {
+  def _buildEncoder(pt: PType, mb: MethodBuilder, v: Code[_], out: Code[OutputBuffer]): Code[Unit] = {
     out.writeFloat(coerce[Float](v))
   }
 
   def _buildDecoder(
     pt: PType,
-    mb: EmitMethodBuilder,
+    mb: MethodBuilder,
     region: Code[Region],
     in: Code[InputBuffer]
   ): Code[Float] = in.readFloat()
 
-  def _buildSkip(mb: EmitMethodBuilder, r: Code[Region], in: Code[InputBuffer]): Code[Unit] = in.skipFloat()
+  def _buildSkip(mb: MethodBuilder, r: Code[Region], in: Code[InputBuffer]): Code[Unit] = in.skipFloat()
 
   override def _compatible(pt: PType): Boolean = pt.isInstanceOf[PFloat32]
 
