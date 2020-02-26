1e0c7e25fa2f6586c3f71d5eb79a746534e1c609
jigold <jigold@users.noreply.github.com>
2020-02-26 17:51:08 -0500

    [batch] fix service accounts (#8169)
    * [batch] fix service accounts
    
    * fix

--- batch/batch/front_end/front_end.py
+++ batch/batch/front_end/front_end.py
@@ -471,14 +471,11 @@ def check_service_account_permissions(user, sa):
     if sa is None:
         return
     if user == 'ci':
-        if sa['name'] == 'ci-agent' and sa['namespace'] == DEFAULT_NAMESPACE:
-            return
-        if sa['name'] == 'ci-agent' and sa['namespace'] == BATCH_PODS_NAMESPACE:
-            return
-        if sa['name'] == 'admin' and sa['namespace'] == DEFAULT_NAMESPACE:
-            return
-        if sa['name'] == 'admin' and sa['namespace'] == BATCH_PODS_NAMESPACE:
-            return
+        if sa['name'] in ('ci-agent', 'admin'):
+            if DEFAULT_NAMESPACE == 'default':  # real-ci needs access to all namespaces
+                return
+            if sa['namespace'] == BATCH_PODS_NAMESPACE:
+                return
     if user == 'test':
         if sa['name'] == 'test-batch-sa' and sa['namespace'] == BATCH_PODS_NAMESPACE:
             return
