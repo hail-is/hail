22477a67b919812af3d90d567bdd78ac6f82ce9f
Daniel King <dking@broadinstitute.org>
2020-02-28 11:52:29 -0500

    simplify a bit

--- hail/src/main/scala/is/hail/expr/ir/CompileAndEvaluate.scala
+++ hail/src/main/scala/is/hail/expr/ir/CompileAndEvaluate.scala
@@ -11,12 +11,10 @@ object CompileAndEvaluate {
     ir0: IR,
     optimize: Boolean = true
   ): T = {
-    ctx.timer.time("CompileAndEvaluate - outer") {
-      ctx.timer.time("ConvertToSafeValue") {
-        _apply(ctx, ir0, optimize) match {
-          case Left(()) => ().asInstanceOf[T]
-          case Right((t, off)) => SafeRow(t, ctx.r, off).getAs[T](0)
-        }
+    ctx.timer.time("CompileAndEvaluate") {
+      _apply(ctx, ir0, optimize) match {
+        case Left(()) => ().asInstanceOf[T]
+        case Right((t, off)) => SafeRow(t, ctx.r, off).getAs[T](0)
       }
     }
   }
@@ -26,20 +24,18 @@ object CompileAndEvaluate {
     ir0: IR,
     optimize: Boolean = true
   ): Either[Unit, (PTuple, Long)] = {
-    ctx.timer.time("CompileAndEvaluate - inner") {
-      val ir = LoweringPipeline.relationalLowerer.apply(ctx, ir0, optimize).asInstanceOf[IR]
+    val ir = LoweringPipeline.relationalLowerer.apply(ctx, ir0, optimize).asInstanceOf[IR]
 
-      if (ir.typ == TVoid)
-        // void is not really supported by IR utilities
-        return Left(())
+    if (ir.typ == TVoid)
+      // void is not really supported by IR utilities
+      return Left(())
 
-      val (resultPType: PTuple, f) = ctx.timer.time("Compile")(Compile[Long](ctx,
-        MakeTuple.ordered(FastSeq(ir)), None, optimize = false))
+    val (resultPType: PTuple, f) = ctx.timer.time("Compile")(Compile[Long](ctx,
+      MakeTuple.ordered(FastSeq(ir)), None, optimize = false))
 
-      val fRunnable = ctx.timer.time("InitializeCompiledFunction")(f(0, ctx.r))
-      val resultAddress = ctx.timer.time("RunCompiledFunction")(fRunnable(ctx.r))
+    val fRunnable = ctx.timer.time("InitializeCompiledFunction")(f(0, ctx.r))
+    val resultAddress = ctx.timer.time("RunCompiledFunction")(fRunnable(ctx.r))
 
-      Right((resultPType, resultAddress))
-    }
+    Right((resultPType, resultAddress))
   }
 }
