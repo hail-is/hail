4fc384e77a42ef4bf84ef95c1884826f214f6be3
Daniel King <dking@broadinstitute.org>
2020-02-28 16:41:55 -0500

    wtf numpy

--- hail/python/test/hail/expr/test_ndarrays.py
+++ hail/python/test/hail/expr/test_ndarrays.py
@@ -673,25 +673,25 @@ def test_numpy_interop():
     a = [[2, 3]]
     b = [[3], [5]]
 
-    assert hl.eval(np.array(v) * hl.literal(3)) == np.array([6, 9])
-    assert hl.eval(hl.literal(3) * np.array(v)) == np.array([6, 9])
+    assert np.array_equal(hl.eval(np.array(v) * hl.literal(3)), np.array([6, 9]))
+    assert np.array_equal(hl.eval(hl.literal(3) * np.array(v)), np.array([6, 9]))
 
-    assert hl.eval(np.array(v) * hl.nd.array(w)) == np.array([6, 15])
-    assert hl.eval(hl.nd.array(w) * np.array(v)) == np.array([6, 15])
+    assert np.array_equal(hl.eval(np.array(v) * hl.nd.array(w)), np.array([6, 15]))
+    assert np.array_equal(hl.eval(hl.nd.array(w) * np.array(v)), np.array([6, 15]))
 
-    assert hl.eval(np.array(v) + hl.literal(3)) == np.array([5, 6])
-    assert hl.eval(hl.literal(3) + np.array(v)) == np.array([5, 6])
+    assert np.array_equal(hl.eval(np.array(v) + hl.literal(3)), np.array([5, 6]))
+    assert np.array_equal(hl.eval(hl.literal(3) + np.array(v)), np.array([5, 6]))
 
-    assert hl.eval(np.array(v) + hl.nd.array(w)) == np.array([5, 8])
-    assert hl.eval(hl.nd.array(w) + np.array(v)) == np.array([5, 8])
+    assert np.array_equal(hl.eval(np.array(v) + hl.nd.array(w)), np.array([5, 8]))
+    assert np.array_equal(hl.eval(hl.nd.array(w) + np.array(v)), np.array([5, 8]))
 
-    assert hl.eval(np.array(v) @ hl.nd.array(w)) == 21
-    assert hl.eval(hl.nd.array(v) @ np.array(w)) == 21
+    assert np.array_equal(hl.eval(np.array(v) @ hl.nd.array(w)), 21)
+    assert np.array_equal(hl.eval(hl.nd.array(v) @ np.array(w)), 21)
 
-    assert hl.eval(np.array(a) @ hl.nd.array(b)) == np.array([[21]])
-    assert hl.eval(hl.nd.array(a) @ np.array(b)) == np.array([[21]])
+    assert np.array_equal(hl.eval(np.array(a) @ hl.nd.array(b)), np.array([[21]]))
+    assert np.array_equal(hl.eval(hl.nd.array(a) @ np.array(b)), np.array([[21]]))
 
-    assert hl.eval(hl.nd.array(b) @ np.array(a)) == \
-        np.array([[6, 9], [10, 15]])
-    assert hl.eval(np.array(b) @ hl.nd.array(a)) == \
-        np.array([[6, 9], [10, 15]])
+    assert np.array_equal(hl.eval(hl.nd.array(b) @ np.array(a)),
+                          np.array([[6, 9], [10, 15]]))
+    assert np.array_equal(hl.eval(np.array(b) @ hl.nd.array(a)),
+                          np.array([[6, 9], [10, 15]]))
