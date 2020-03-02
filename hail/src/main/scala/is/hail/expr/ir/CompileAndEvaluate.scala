13df017ece466a3d444100015241e4dcf1ccfa2f
Daniel King <daniel.zidan.king@gmail.com>
2020-03-02 12:06:19 -0500

    [experimental][hail] fix codec.py (#8197)
    * [experimental][hail] make codec.py work again
    
    * wip
    
    * add test
    
    * fix
    
    * fixes
    
    * simplify a bit

--- hail/src/main/scala/is/hail/expr/ir/CompileAndEvaluate.scala
+++ hail/src/main/scala/is/hail/expr/ir/CompileAndEvaluate.scala
@@ -2,7 +2,7 @@ package is.hail.expr.ir
 
 import is.hail.annotations.SafeRow
 import is.hail.expr.ir.lowering.LoweringPipeline
-import is.hail.expr.types.physical.PBaseStruct
+import is.hail.expr.types.physical.{PTuple, PBaseStruct}
 import is.hail.expr.types.virtual.TVoid
 import is.hail.utils.FastSeq
 
@@ -12,21 +12,30 @@ object CompileAndEvaluate {
     optimize: Boolean = true
   ): T = {
     ctx.timer.time("CompileAndEvaluate") {
+      _apply(ctx, ir0, optimize) match {
+        case Left(()) => ().asInstanceOf[T]
+        case Right((t, off)) => SafeRow(t, ctx.r, off).getAs[T](0)
+      }
+    }
+  }
 
+  def _apply(
+    ctx: ExecuteContext,
+    ir0: IR,
+    optimize: Boolean = true
+  ): Either[Unit, (PTuple, Long)] = {
     val ir = LoweringPipeline.relationalLowerer.apply(ctx, ir0, optimize).asInstanceOf[IR]
 
     if (ir.typ == TVoid)
-    // void is not really supported by IR utilities
-      return ().asInstanceOf[T]
+      // void is not really supported by IR utilities
+      return Left(())
 
-    val (resultPType, f) = ctx.timer.time("Compile")(Compile[Long](ctx,
+    val (resultPType: PTuple, f) = ctx.timer.time("Compile")(Compile[Long](ctx,
       MakeTuple.ordered(FastSeq(ir)), None, optimize = false))
 
     val fRunnable = ctx.timer.time("InitializeCompiledFunction")(f(0, ctx.r))
     val resultAddress = ctx.timer.time("RunCompiledFunction")(fRunnable(ctx.r))
 
-    ctx.timer.time("ConvertToSafeValue")(
-      SafeRow(resultPType.asInstanceOf[PBaseStruct], ctx.r, resultAddress).getAs[T](0))
-    }
+    Right((resultPType, resultAddress))
   }
 }
