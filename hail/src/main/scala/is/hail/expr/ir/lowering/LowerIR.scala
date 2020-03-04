47601e578795ce7f64fef19b3c5e3fc4c5a80277
wang <wang@broadinstitute.org>
2020-03-04 13:45:07 -0500

    fix

--- hail/src/main/scala/is/hail/expr/ir/lowering/LowerIR.scala
+++ hail/src/main/scala/is/hail/expr/ir/lowering/LowerIR.scala
@@ -4,17 +4,17 @@ import is.hail.expr.ir.{BlockMatrixIR, Copy, IR, MatrixIR, Pretty, TableIR}
 
 object LowerIR {
   def lower(ir: IR, typesToLower: DArrayLowering.Type): IR = ir match {
-    case node if node.children.forall(n => n.isInstanceOf[TableIR] || n.isInstanceOf[IR] ) =>
+    case node if node.children.forall(_.isInstanceOf[IR]) =>
+      Copy(node, ir.children.map { case c: IR => lower(c, typesToLower) })
+
+    case node if node.children.exists(n => n.isInstanceOf[TableIR]) &&  node.children.forall(n => n.isInstanceOf[TableIR] || n.isInstanceOf[IR] ) =>
       LowerTableIR.lower(ir, typesToLower)
+
+    case node if node.children.exists(n => n.isInstanceOf[BlockMatrixIR]) &&  node.children.forall(n => n.isInstanceOf[BlockMatrixIR] || n.isInstanceOf[IR] ) =>
+      LowerBlockMatrixIR.lower(ir, typesToLower)
 
     case node if node.children.exists( _.isInstanceOf[MatrixIR] ) =>
       throw new LowererUnsupportedOperation(s"MatrixIR nodes must be lowered to TableIR nodes separately: \n${ Pretty(node) }")
-
-    case node if node.children.forall(n => n.isInstanceOf[IR] || n.isInstanceOf[BlockMatrixIR] ) =>
-      LowerBlockMatrixIR.lower(ir, typesToLower)
-
-    case node if node.children.forall(_.isInstanceOf[IR]) =>
-      Copy(node, ir.children.map { case c: IR => lower(c, typesToLower) })
 
     case node =>
       throw new LowererUnsupportedOperation(s"Cannot lower: \n${ Pretty(node) }")
