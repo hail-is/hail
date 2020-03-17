4a573e7c703366562043ff9b627d063ed6f3e88e
Chris Vittal <chris@vittal.dev>
2020-03-17 13:10:04 -0400

    Add flag to explicitly turn off Transposed Array of Struct Encoding

--- hail/src/main/scala/is/hail/expr/types/encoded/EType.scala
+++ hail/src/main/scala/is/hail/expr/types/encoded/EType.scala
@@ -244,7 +244,9 @@ object EType {
     }
   }
 
-  def defaultFromPType(pt: PType): EType = {
+  def defaultFromPType(pt: PType,
+    useTransposedArrayOfStructs: Boolean = true
+  ): EType = {
     pt.fundamentalType match {
       case t: PInt32 => EInt32(t.required)
       case t: PInt64 => EInt64(t.required)
@@ -257,16 +259,17 @@ object EType {
           HailContext.get.flags.get("use_packed_int_encoding") != null =>
          EPackedIntArray(t.required, t.elementType.required)
       // FIXME(chrisvittal): Turn this on when it works
-      case t: PArray if t.elementType.isInstanceOf[PBaseStruct] &&
+      case t: PArray if t.elementType.isInstanceOf[PBaseStruct] && useTransposedArrayOfStructs &&
           HailContext.get.flags.get("use_column_encoding") != null =>
         val et = t.elementType.asInstanceOf[PBaseStruct]
         ETransposedArrayOfStructs(
-          et.fields.map(f => EField(f.name, defaultFromPType(f.typ), f.index)),
+          et.fields.map(f => EField(f.name, defaultFromPType(f.typ, useTransposedArrayOfStructs), f.index)),
           required = t.required,
           structRequired = et.required
         )
-      case t: PArray => EArray(defaultFromPType(t.elementType), t.required)
-      case t: PBaseStruct => EBaseStruct(t.fields.map(f => EField(f.name, defaultFromPType(f.typ), f.index)), t.required)
+      case t: PArray => EArray(defaultFromPType(t.elementType, useTransposedArrayOfStructs), t.required)
+      case t: PBaseStruct => EBaseStruct(t.fields.map(f =>
+          EField(f.name, defaultFromPType(f.typ, useTransposedArrayOfStructs), f.index)), t.required)
     }
   }
 
