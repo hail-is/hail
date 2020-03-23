a9ac345391dcc3235a08c38eb22f750698b53dfd
Chris Vittal <chris@vittal.dev>
2020-03-23 16:31:07 -0400

    Add PStringCode

--- hail/src/main/scala/is/hail/expr/ir/PCode.scala
+++ hail/src/main/scala/is/hail/expr/ir/PCode.scala
@@ -248,3 +248,27 @@ class PCanonicalBinaryCode(val pt: PBinary, a: Code[Long]) extends PBinaryCode {
 
   def store(mb: EmitMethodBuilder[_], r: Value[Region], dst: Code[Long]): Code[Unit] = Region.storeAddress(dst, a)
 }
+
+abstract class PStringCode extends PCode {
+  def loadLength(): Code[Int]
+
+  def bytesAddress(): Code[Long]
+
+  def loadString(): Code[String]
+}
+
+class PCanonicalStringCode(val pt: PString, a: Code[Long]) extends PStringCode {
+  def code: Code[_] = a
+
+  def loadLength(): Code[Int] = pt.loadLength(a)
+
+  def bytesAddress(): Code[Long] = pt.bytesAddress(a)
+
+  def loadString(): Code[String] = pt.loadString(a)
+
+  def memoize(cb: EmitCodeBuilder, name: String): PValue = defaultMemoizeImpl(cb, name)
+
+  def memoizeField(cb: EmitCodeBuilder, name: String): PValue = defaultMemoizeFieldImpl(cb, name)
+
+  def store(mb: EmitMethodBuilder[_], r: Value[Region], dst: Code[Long]): Code[Unit] = Region.storeAddress(dst, a)
+}
