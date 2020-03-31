c2854074cee8ddad8d32eabdfa777af8898a6f94
John Compitello <j301600@gmail.com>
2020-03-31 15:03:00 -0400

    The right fix, make it once in the default

--- hail/src/main/scala/is/hail/rvd/RVDContext.scala
+++ hail/src/main/scala/is/hail/rvd/RVDContext.scala
@@ -7,7 +7,7 @@ import scala.collection.mutable
 object RVDContext {
   def default: RVDContext = {
     val partRegion = Region()
-    val ctx = new RVDContext(Region(), Region())
+    val ctx = new RVDContext(partRegion, Region())
     ctx.own(partRegion)
     ctx
   }
@@ -21,7 +21,6 @@ class RVDContext(val partitionRegion: Region, val r: Region) extends AutoCloseab
     assert(children.remove(child))
 
   own(r)
-  own(partitionRegion)
 
   def freshContext: RVDContext = {
     val ctx = new RVDContext(partitionRegion, Region())
