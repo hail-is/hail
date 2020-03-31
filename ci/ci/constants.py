c0843bdd5622b053dfa7d16b8be8e1e02d0ffe3d
Daniel King <daniel.zidan.king@gmail.com>
2020-03-31 05:30:51 -0400

    [ci] statuses should reflect GitHub's belief, not our belief (#7983)
    * [ci] statuses should reflect GitHub's belief, not our belief
    
    * change
    
    * fixes
    
    * fix
    
    * bump
    
    * bump
    
    * bump
    
    * address commentsg

--- ci/ci/constants.py
+++ ci/ci/constants.py
@@ -1,6 +1,7 @@
 from hailtop.auth import get_userinfo
 
 GITHUB_CLONE_URL = 'https://github.com/'
+GITHUB_STATUS_CONTEXT = 'ci-test'
 
 userinfo = get_userinfo()
 BUCKET = f'gs://{userinfo["bucket_name"]}'
