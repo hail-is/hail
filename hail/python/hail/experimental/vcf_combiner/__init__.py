095f7e1f4a3b7fd2b419740f91ad1cfd5b532360
Tim Poterba <tpoterba@gmail.com>
2020-04-01 09:25:57 -0400

    expose functions

--- hail/python/hail/experimental/vcf_combiner/__init__.py
+++ hail/python/hail/experimental/vcf_combiner/__init__.py
@@ -1,10 +1,13 @@
-from .vcf_combiner import run_combiner
+from .vcf_combiner import run_combiner, transform_gvcf, transform_one, parse_as_fields
 from .sparse_split_multi import sparse_split_multi
 from .sparse_mt_utils import lgt_to_gt
 from .densify import densify
 
 __all__ = [
     'run_combiner',
+    'transform_gvcf',
+    'transform_one',
+    'parse_as_fields',
     'sparse_split_multi',
     'lgt_to_gt',
     'densify',
