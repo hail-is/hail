65c030a437cadfbfd43bdca677e25f3be602eb2c
Alex V. Kotlar <akotlar@bu.edu>
2020-04-01 17:45:10 -0400

    experimental registered functions (#8425)

--- hail/src/main/scala/is/hail/experimental/ExperimentalFunctions.scala
+++ hail/src/main/scala/is/hail/experimental/ExperimentalFunctions.scala
@@ -1,6 +1,7 @@
 package is.hail.experimental
 
 import is.hail.expr.ir.functions._
+import is.hail.expr.types.physical.{PCanonicalArray, PFloat64, PType}
 import is.hail.expr.types.virtual.{TArray, TFloat64, TInt32}
 
 object ExperimentalFunctions extends RegistryFunctions {
@@ -8,8 +9,7 @@ object ExperimentalFunctions extends RegistryFunctions {
   def registerAll() {
     val experimentalPackageClass = Class.forName("is.hail.experimental.package$")
 
-    registerScalaFunction("filtering_allele_frequency", Array(TInt32, TInt32, TFloat64), TFloat64, null)(experimentalPackageClass, "calcFilterAlleleFreq")
-    registerWrappedScalaFunction("haplotype_freq_em", TArray(TInt32), TArray(TFloat64), null)(experimentalPackageClass, "haplotypeFreqEM")
-
+    registerScalaFunction("filtering_allele_frequency", Array(TInt32, TInt32, TFloat64), TFloat64, (pt: Seq[PType]) => PFloat64())(experimentalPackageClass, "calcFilterAlleleFreq")
+    registerWrappedScalaFunction("haplotype_freq_em", TArray(TInt32), TArray(TFloat64), (pt: PType) => PCanonicalArray(PFloat64(true)))(experimentalPackageClass, "haplotypeFreqEM")
   }
 }
