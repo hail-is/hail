02273832adf36a1b24f08189810717021cdb1f88
Cotton Seed <cotton@alum.mit.edu>
2020-04-02 17:30:37 +0100

    fix

--- hail/python/hail/experimental/codec.py
+++ hail/python/hail/experimental/codec.py
@@ -2,10 +2,10 @@ from hail.utils.java import Env
 
 
 def encode(expression, codec='{"name":"BlockingBufferSpec","blockSize":65536,"child":{"name":"StreamBlockBufferSpec"}}'):
-    v = Env.hc()._jhc.backend().encodeToBytes(Env.backend()._to_java_ir(expression._ir), codec)
+    v = Env.backend()._jbackend.encodeToBytes(Env.backend()._to_java_ir(expression._ir), codec)
     return (v._1(), v._2())
 
 
 def decode(typ, ptype_string, bytes, codec='{"name":"BlockingBufferSpec","blockSize":65536,"child":{"name":"StreamBlockBufferSpec"}}'):
     return typ._from_json(
-        Env.hc()._jhc.backend().decodeToJSON(ptype_string, bytes, codec))
+        Env.backend()._jbackend.decodeToJSON(ptype_string, bytes, codec))
