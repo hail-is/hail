1ac9aa035d468314ac3d723c1f1d4ebced4ef329
Daniel King <dking@broadinstitute.org>
2020-04-06 11:40:10 -0400

    wip

--- hail/python/hailtop/pipeline/utils.py
+++ hail/python/hailtop/pipeline/utils.py
@@ -7,7 +7,7 @@ __ARG_MAX = None
 def arg_max():
     global __ARG_MAX
     if __ARG_MAX is None:
-        __ARG_MAX = int(sp.check_output(['getconf', 'ARGMAX']))
+        __ARG_MAX = int(sp.check_output(['getconf', 'ARG_MAX']))
     return __ARG_MAX
 
 
