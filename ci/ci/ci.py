7291cff4b87e5edca96716652df66043c489d6bd
Daniel King <dking@broadinstitute.org>
2020-04-08 12:26:26 -0400

    [ci] fix PR number link
    `wb.repo` is not a thing, the thing is `repo` which is `hail-is/hail`.

--- ci/ci/ci.py
+++ ci/ci/ci.py
@@ -101,7 +101,6 @@ async def get_pr(request, userdata):  # pylint: disable=unused-argument
     pr = wb.prs[pr_number]
 
     page_context = {}
-    page_context['wb'] = wb
     page_context['repo'] = wb.branch.repo.short_str()
     page_context['pr'] = pr
     # FIXME
