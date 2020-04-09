433b8b12d9384867e305a06a0559f00c2a316f0f
Cotton Seed <cotton@alum.mit.edu>
2020-04-09 12:13:35 +0100

    fix

--- hail/python/hail/ir/export_type.py
+++ hail/python/hail/ir/export_type.py
@@ -7,8 +7,9 @@ class ExportType:
     PARALLEL_SEPARATE_HEADER = "separate_header"
     PARALLEL_HEADER_IN_SHARD = "header_per_shard"
 
+    @staticmethod
     def default(export_type):
         if export_type is None:
-            return CONCATENATED
+            return ExportType.CONCATENATED
         else:
             return export_type
