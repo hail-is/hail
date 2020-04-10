ecb8fadaffcb92ed8ffe9863f0e24e74c0c976be
Daniel King <dking@broadinstitute.org>
2020-04-10 17:33:46 -0400

    [hail] automate deploy in build.yaml
    I have not tested this, though I faithfully copied the commands from existing
    deploy scripts (except for creating a github release).
    
    A change that I think is valuable regardless of automation is the conversion of
    deploy from a series of Makefile targets to a bash script.
    
    I also add a deploy build.yaml step which simply calls the deploy script,
    setting up appropriate credentials.
    
    I only had to add one set of credentials: the PyPI credentials. I've already
    created that secret in the cluster.
    
    Hand deploys are still very easy. You need curl >=7.55.0 (that version
    implemented reading headers from a file). You need to set up two things:
    1. create $HOME/.pypirc and put this there:
    ```
    [pypi]
    username: hailteam
    password: GET_THIS_FROM_THE_USUAL_PLACE
    ```
    2. get a github access token with repo
       privileges (https://github.com/settings/tokens), create
       $HOME/.github-oauth-header, and put this there:
    ```
    Authorization: token YOUR_ACCESS_TOKEN_HERE
    ```
    Now, to do a hand deploy run:
    ```
    make deploy GITHUB_OAUTH_HEADER_FILE=$HOME/.github-oauth-header DEPLOY_REMOTE=THE_REMOTE_FOR_hail-is/hail
    ```
    
    The github credentials are used to create a GitHub release.

---
+++ hail/deploy.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+set -ex
+
+[[ $1 ]] || (echo "./deploy.sh HAIL_PIP_VERSION HAIL_VERSION GIT_VERSION REMOTE WHEEL GITHUB_OAUTH_HEADER_FILE" ; exit 1)
+[[ $2 ]] || (echo "./deploy.sh HAIL_PIP_VERSION HAIL_VERSION GIT_VERSION REMOTE WHEEL GITHUB_OAUTH_HEADER_FILE" ; exit 1)
+[[ $3 ]] || (echo "./deploy.sh HAIL_PIP_VERSION HAIL_VERSION GIT_VERSION REMOTE WHEEL GITHUB_OAUTH_HEADER_FILE" ; exit 1)
+git cat-file -e $3^{commit} || (echo "bad sha $3" ; exit 1)
+[[ $4 ]] || (echo "./deploy.sh HAIL_PIP_VERSION HAIL_VERSION GIT_VERSION REMOTE WHEEL GITHUB_OAUTH_HEADER_FILE" ; exit 1)
+[[ $5 ]] || (echo "./deploy.sh HAIL_PIP_VERSION HAIL_VERSION GIT_VERSION REMOTE WHEEL GITHUB_OAUTH_HEADER_FILE" ; exit 1)
+[[ $6 ]] || (echo "./deploy.sh HAIL_PIP_VERSION HAIL_VERSION GIT_VERSION REMOTE WHEEL GITHUB_OAUTH_HEADER_FILE" ; exit 1)
+
+HAIL_PIP_VERSION=$1
+HAIL_VERSION=$2
+GIT_VERSION=$3
+REMOTE=$4
+WHEEL=$5
+GITHUB_OAUTH_HEADER_FILE=$6
+
+if git ls-remote --exit-code --tags $REMOTE $HAIL_PIP_VERSION
+then
+    echo "tag $HAIL_PIP_VERSION already exists"
+    exit 0
+fi
+
+pip install hail== 2>&1 \
+    | head -n 1 \
+    | sed 's/.*versions: //' \
+    | sed 's/)//' \
+    | sed 's/ //g' \
+    | tr ',' '\n' \
+         > pip_versions
+
+if grep -q -e $HAIL_PIP_VERSION pip_versions
+then
+    echo "package $HAIL_PIP_VERSION already exists"
+    exit 1
+fi
+
+if curl -sf https://github.com/hail-is/hail/releases/tag/$HAIL_PIP_VERSION >/dev/null
+then
+    echo "release $HAIL_PIP_VERSOIN already exists"
+    exit 1
+fi
+
+docs_location=gs://hail-common/builds/0.2/docs/hail-0.2-docs-$GIT_VERSION.tar.gz
+
+if ! gsutil ls $docs_location
+then
+    echo "docs for $GIT_VERSION do not exist"
+    exit 1
+fi
+
+
+# push git tag
+git tag $HAIL_PIP_VERSION -m "Hail version $HAIL_PIP_VERSION."
+git push origin $HAIL_PIP_VERSION
+
+# make GitHub release
+curl -XPOST -H @$GITHUB_OAUTH_HEADER_FILE https://api.github.com/repos/hail-is/hail/releases -d '{
+  "tag_name": "'$HAIL_PIP_VERSION'",
+  "target_commitish": "master",
+  "name": "'$HAIL_PIP_VERSION'",
+  "body": "Hail version '$HAIL_PIP_VERSION'",
+  "draft": false,
+  "prerelease": false
+}'
+
+# deploy to PyPI
+twine upload $WHEEL
+
+# update docs sha
+cloud_sha_location=gs://hail-common/builds/0.2/latest-hash/cloudtools-5-spark-2.4.0.txt
+gsutil cp $docs_location $cloud_sha_location
+gsutil acl set public-read $cloud_sha_location
+
+# deploy annotation db json
+annotation_db_json_url=gs://hail-common/annotationdb/$HAIL_VERSION/annotation_db.json
+gsutil cp python/hail/experimental/annotation_db.json $annotation_db_json_url
+gsutil -m retention temp set $annotation_db_json_url
