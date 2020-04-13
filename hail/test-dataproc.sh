810c9869028f61e524c5611b50ca7e48a2f543dc
Daniel King <dking@broadinstitute.org>
2020-04-13 16:43:11 -0400

    fixes

--- hail/test-dataproc.sh
+++ hail/test-dataproc.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+project=hail-vdc
+zone=us-central-1a
 cluster_name_37=cluster-$(whoami)-$(LC_ALL=C tr -dc '[:lower:]' </dev/urandom | head -c 6)
 cluster_name_38=cluster-$(whoami)-$(LC_ALL=C tr -dc '[:lower:]' </dev/urandom | head -c 6)
 
@@ -20,14 +22,32 @@ trap stop_dataproc EXIT
 cluster_37_test_files=$(ls python/cluster-tests/*.py | grep -ve 'python/cluster-tests/cluster-vep-check-GRCh38.py')
 cluster_38_test_files=$(ls python/cluster-tests/*.py | grep -ve 'python/cluster-tests/cluster-vep-check-GRCh37.py')
 
-hailctl dataproc start $cluster_name_37 --max-idle 10m --vep GRCh37 --requester-pays-allow-buckets hail-us-vep
+hailctl dataproc \
+        --project $project \
+        --zone $zone \
+        start $cluster_name_37 \
+        --max-idle 10m \
+        --vep GRCh37 \
+        --requester-pays-allow-buckets hail-us-vep
 for file in $cluster_37_test_files
 do
-    hailctl dataproc submit $cluster_name_37 $file
+    hailctl --project $project \
+            --zone $zone \
+            dataproc submit \
+            $cluster_name_37 $file
 done
 
-hailctl dataproc start $cluster_name_38 --max-idle 10m --vep GRCh38 --requester-pays-allow-buckets hail-us-vep
+hailctl dataproc \
+        --project $project \
+        --zone $zone \
+        start $cluster_name_38 \
+        --max-idle 10m \
+        --vep GRCh38 \
+        --requester-pays-allow-buckets hail-us-vep
 for file in $cluster_38_test_files
 do
-    hailctl dataproc submit $cluster_name_38 $filen
+    hailctl --project $project \
+            --zone $zone \
+            dataproc submit \
+            $cluster_name_38 $filen
 done
