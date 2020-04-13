be1d835d5afb829c2d1901ace716a69660b7291b
akotlar <akotlar@bu.edu>
2020-04-13 07:09:56 -0400

    cleanup

--- hail/src/main/scala/is/hail/expr/ir/functions/ReferenceGenomeFunctions.scala
+++ hail/src/main/scala/is/hail/expr/ir/functions/ReferenceGenomeFunctions.scala
@@ -12,30 +12,26 @@ class ReferenceGenomeFunctions(rg: ReferenceGenome) extends RegistryFunctions {
   def rgCode(mb: EmitMethodBuilder[_], rg: ReferenceGenome): Code[ReferenceGenome] = mb.getReferenceGenome(rg)
   var registered: Set[(String, Type, Seq[Type], Seq[Type])] = Set()
 
-  def registerRGCode(mname: String, typeArgs: Array[Type], args: Array[Type], rt: Type, pt: (Type, Seq[PType]) => PType)(
-                      impl: (EmitRegion, PType, Array[Type], Array[(PType, Code[_])]) => Code[_]
-                    ): Unit = {
+  def registerRGCode(mname: String, typeArgs: Array[Type], args: Array[Type], rt: Type, pt: (Type, Seq[PType]) => PType)
+    (impl: (EmitRegion, PType, Array[Type], Array[(PType, Code[_])]) => Code[_]): Unit = {
     registered += ((mname, rt, typeArgs, args))
     registerCode(mname, typeArgs, args, rt, pt)(impl)
   }
 
-  def registerRGCode[A1](mname: String, typeArg1: Type, arg1: Type, rt: Type, pt: (Type, PType) => PType)(
-    impl: (EmitRegion, PType, Type, (PType, Code[A1])) => Code[_]): Unit =
+  def registerRGCode[A1](mname: String, typeArg1: Type, arg1: Type, rt: Type, pt: (Type, PType) => PType)
+    (impl: (EmitRegion, PType, Type, (PType, Code[A1])) => Code[_]): Unit =
     registerRGCode(mname, Array(typeArg1), Array[Type](arg1), rt, unwrappedApply(pt)) {
       case (r, rt, Array(t1: TLocus), Array(a1: (PType, Code[A1]) @unchecked)) => impl(r, rt, t1, a1)
     }
 
-  def registerRGCode[A1, A2](mname: String, typeArg1: Type, arg1: Type, arg2: Type, rt: Type, pt: (Type, PType, PType) => PType)(
-    impl: (EmitRegion, PType, Type, (PType, Code[A1]), (PType, Code[A2])) => Code[_]
-  ): Unit =
+  def registerRGCode[A1, A2](mname: String, typeArg1: Type, arg1: Type, arg2: Type, rt: Type, pt: (Type, PType, PType) => PType)
+    (impl: (EmitRegion, PType, Type, (PType, Code[A1]), (PType, Code[A2])) => Code[_]): Unit =
     registerRGCode(mname, Array(typeArg1), Array[Type](arg1, arg2), rt, unwrappedApply(pt)) {
       case (r, rt, Array(t1: TLocus), Array(a1: (PType, Code[A1]) @unchecked, a2: (PType, Code[A2]) @unchecked)) => impl(r, rt, t1, a1, a2)
     }
 
-  def registerRGCode[A1, A2, A3, A4](
-                                      mname: String, typeArg1: Type, arg1: Type, arg2: Type, arg3: Type, arg4: Type, rt: Type, pt: (Type, PType, PType, PType, PType) => PType)(
-                                      impl: (EmitRegion, PType, Type, (PType, Code[A1]), (PType, Code[A2]), (PType, Code[A3]), (PType, Code[A4])) => Code[_]
-                                    ): Unit =
+  def registerRGCode[A1, A2, A3, A4](mname: String, typeArg1: Type, arg1: Type, arg2: Type, arg3: Type, arg4: Type, rt: Type, pt: (Type, PType, PType, PType, PType) => PType)
+    (impl: (EmitRegion, PType, Type, (PType, Code[A1]), (PType, Code[A2]), (PType, Code[A3]), (PType, Code[A4])) => Code[_]): Unit =
     registerRGCode(mname, Array(typeArg1), Array[Type](arg1, arg2, arg3, arg4), rt, unwrappedApply(pt)) {
       case (r, rt, Array(t1: TLocus), Array(
       a1: (PType, Code[A1]) @unchecked,
