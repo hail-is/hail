e00f14f783474879d7d27b954c03b59807609086
Daniel King <dking@broadinstitute.org>
2020-04-13 18:05:06 -0400

    fix

--- hail/test-dataproc.sh
+++ hail/test-dataproc.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-project=hail-vdc
-zone=us-central-1a
 cluster_name_37=cluster-$(whoami)-$(LC_ALL=C tr -dc '[:lower:]' </dev/urandom | head -c 6)
 cluster_name_38=cluster-$(whoami)-$(LC_ALL=C tr -dc '[:lower:]' </dev/urandom | head -c 6)
 
@@ -12,14 +10,8 @@ stop_dataproc () {
 
     set +e
 
-    hailctl dataproc \
-            --project $project \
-            --zone $zone \
-            stop $cluster_name_37
-    hailctl dataproc
-            --project $project \
-            --zone $zone \
-            stop $cluster_name_38
+    hailctl dataproc stop $cluster_name_37
+    hailctl dataproc stop $cluster_name_38
 
     exit $exit_code
 }
@@ -29,8 +21,6 @@ cluster_37_test_files=$(ls python/cluster-tests/*.py | grep -ve 'python/cluster-
 cluster_38_test_files=$(ls python/cluster-tests/*.py | grep -ve 'python/cluster-tests/cluster-vep-check-GRCh37.py')
 
 hailctl dataproc \
-        --project $project \
-        --zone $zone \
         start $cluster_name_37 \
         --max-idle 10m \
         --vep GRCh37 \
@@ -38,15 +28,11 @@ hailctl dataproc \
 for file in $cluster_37_test_files
 do
     hailctl dataproc \
-            --project $project \
-            --zone $zone \
             submit \
             $cluster_name_37 $file
 done
 
 hailctl dataproc \
-        --project $project \
-        --zone $zone \
         start $cluster_name_38 \
         --max-idle 10m \
         --vep GRCh38 \
@@ -54,8 +40,6 @@ hailctl dataproc \
 for file in $cluster_38_test_files
 do
     hailctl dataproc \
-            --project $project \
-            --zone $zone \
             submit \
             $cluster_name_38 $filen
 done
