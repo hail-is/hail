94326e52c00cdc4ff9bbdd5f4afefdb8d9c457a5
Daniel King <dking@broadinstitute.org>
2020-04-15 18:05:55 -0400

    wip

--- auth/sql/copy-paste-tokens.sql
+++ auth/sql/copy-paste-tokens.sql
@@ -2,7 +2,7 @@ CREATE TABLE `copy_paste_tokens` (
   `id` VARCHAR(255) NOT NULL,
   `session_id` VARCHAR(255) NOT NULL,
   `user_id` INT(11) NOT NULL,
-  `max_age_secs` INT(11) UNSIGNED DEFAULT NOT NULL,
+  `max_age_secs` INT(11) UNSIGNED NOT NULL,
   `created` TIMESTAMP DEFAULT NOW(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
