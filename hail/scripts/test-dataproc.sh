46341ae654b64b3f9b8d8d0fceea3788b5db99ed
Daniel King <dking@broadinstitute.org>
2020-04-17 16:19:52 -0400

    wip revert me

--- hail/scripts/test-dataproc.sh
+++ hail/scripts/test-dataproc.sh
@@ -5,17 +5,17 @@ set -ex
 cluster_name_37=cluster-$(whoami)-$(LC_ALL=C tr -dc '[:lower:]' </dev/urandom | head -c 6)
 cluster_name_38=cluster-$(whoami)-$(LC_ALL=C tr -dc '[:lower:]' </dev/urandom | head -c 6)
 
-stop_dataproc () {
-    exit_code=$?
+# stop_dataproc () {
+#     exit_code=$?
 
-    set +e
+#     set +e
 
-    hailctl dataproc stop $cluster_name_37 || true  # max-idle or max-age will delete it
-    hailctl dataproc stop $cluster_name_38 || true  # max-idle or max-age will delete it
+#     hailctl dataproc stop $cluster_name_37 || true  # max-idle or max-age will delete it
+#     hailctl dataproc stop $cluster_name_38 || true  # max-idle or max-age will delete it
 
-    exit $exit_code
-}
-trap stop_dataproc EXIT
+#     exit $exit_code
+# }
+# trap stop_dataproc EXIT
 
 cluster_37_test_files=$(ls python/cluster-tests/*.py | grep -ve 'python/cluster-tests/cluster-vep-check-GRCh38.py')
 cluster_38_test_files=$(ls python/cluster-tests/*.py | grep -ve 'python/cluster-tests/cluster-vep-check-GRCh37.py')
