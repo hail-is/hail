610ccdc43f846d1e1e599352ae48292d4cce53d7
Daniel King <dking@broadinstitute.org>
2020-04-19 15:45:24 -0400

    fix

--- auth/auth/auth.py
+++ auth/auth/auth.py
@@ -111,22 +111,23 @@ async def handle_copy_paste_token(request):
     dbpool = request.app['dbpool']
     session = await aiohttp_session.get_session(request)
     session_id = session['session_id']
-    copy_paste_token = await create_copy_paste_token(dbpool, session_id)
-    return web.Response(
-        body=f'Your copy paste token is: {copy_paste_token}')
+    return await create_copy_paste_token(dbpool, session_id)
 
 
 @routes.post('/copy-paste-token')
 @check_csrf_token
 @web_authenticated_users_only()
 async def get_copy_paste_token(request, userdata):  # pylint: disable=unused-argument
-    return await handle_copy_paste_token(request)
+    copy_paste_token = await handle_copy_paste_token(request)
+    return web.Response(
+        body=f'Your copy paste token is: {copy_paste_token}')
 
 
 @routes.post('/api/v1alpha/copy-paste-token')
 @rest_authenticated_users_only
 async def get_copy_paste_token_api(request, userdata):  # pylint: disable=unused-argument
-    return await handle_copy_paste_token(request)
+    copy_paste_token = await handle_copy_paste_token(request)
+    return web.Response(body=copy_paste_token)
 
 
 @routes.post('/logout')
