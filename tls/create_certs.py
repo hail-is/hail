80232ada9409e954ad763dddb5e70e74498126b9
Daniel King <dking@broadinstitute.org>
2020-04-22 13:08:59 -0400

    no trust

--- tls/create_certs.py
+++ tls/create_certs.py
@@ -1,13 +1,11 @@
 import argparse
 import base64
 import os
-import sys
 import json
 import yaml
 import shutil
 import subprocess as sp
 import tempfile
-from collections import defaultdict
 
 parser = argparse.ArgumentParser(prog='create_certs.py',
                                  description='create hail certs')
@@ -15,13 +13,11 @@ parser.add_argument('namespace', type=str, help='kubernetes namespace')
 parser.add_argument('config_file', type=str, help='YAML format config file')
 parser.add_argument('root_key_file', type=str, help='the root key file')
 parser.add_argument('root_cert_file', type=str, help='the root cert file')
-parser.add_argument('--check', help='just check the config file', action='store_true')
 
 args = parser.parse_args()
 namespace = args.namespace
 with open(args.config_file) as f:
     arg_config = yaml.safe_load(f)
-just_check = args.check
 root_key_file = args.root_key_file
 root_cert_file = args.root_cert_file
 
@@ -67,7 +63,7 @@ def create_key_and_cert(p):
     return {'key': key_file, 'cert': cert_file}
 
 
-def create_trust(principal, trust_type, trusted_principals):  # pylint: disable=unused-argument
+def create_trust(principal, trust_type):  # pylint: disable=unused-argument
     trust_file = f'{principal}-{trust_type}.pem'
     with open(trust_file, 'w') as out:
         # FIXME: mTLS, only trust certain principals
@@ -130,11 +126,11 @@ def create_config(principal, incoming_trust, outgoing_trust, key, cert, kind):
     return create_nginx_config(principal, incoming_trust, outgoing_trust, key, cert)
 
 
-def create_principal(principal, incoming_principals, outgoing_principals, domain, kind, key, cert, unmanaged):
+def create_principal(principal, domain, kind, key, cert, unmanaged):
     if unmanaged and namespace != 'default':
         return
-    incoming_trust = create_trust(principal, 'incoming', incoming_principals)
-    outgoing_trust = create_trust(principal, 'outgoing', outgoing_principals)
+    incoming_trust = create_trust(principal, 'incoming')
+    outgoing_trust = create_trust(principal, 'outgoing')
     configs = create_config(principal, incoming_trust, outgoing_trust, key, cert, kind)
     with tempfile.NamedTemporaryFile() as k8s_secret:
         sp.check_call(
@@ -182,40 +178,16 @@ principal_by_name = {
     for p in arg_config['principals']
 }
 
-untrusted_clients = defaultdict(list)
-for client, data in principal_by_name.items():
-    for server in data.get('outgoing', []):
-        if client not in principal_by_name[server].get('incoming', []):
-            untrusted_clients[client].append(server)
-
-untrusted_servers = defaultdict(list)
-for server, data in principal_by_name.items():
-    for client in data.get('incoming', []):
-        if server not in principal_by_name[client].get('outgoing', []):
-            untrusted_servers[server].append(client)
-
-if untrusted_clients or untrusted_servers:
-    if untrusted_clients:
-        print(f'These clients intend to make requests to servers which do not trust them:', file=sys.stderr)
-        yaml.safe_dump(dict(**untrusted_clients), stream=sys.stderr)
-    if untrusted_servers:
-        print(f'These servers incorrectly expect incoming requests from these clients:', file=sys.stderr)
-        yaml.safe_dump(dict(**untrusted_servers), stream=sys.stderr)
-    sys.exit(1)
-
-if not just_check:
-    principal_by_name = {
-        p['name']: {**p,
-                    **create_key_and_cert(p)}
-        for p in arg_config['principals']
-    }
-    download_previous_certs()
-    for name, p in principal_by_name.items():
-        create_principal(name,
-                         p.get('incoming', []),
-                         p.get('outgoing', []),
-                         p['domain'],
-                         p['kind'],
-                         p['key'],
-                         p['cert'],
-                         p.get('unmanaged', False))
+principal_by_name = {
+    p['name']: {**p,
+                **create_key_and_cert(p)}
+    for p in arg_config['principals']
+}
+download_previous_certs()
+for name, p in principal_by_name.items():
+    create_principal(name,
+                     p['domain'],
+                     p['kind'],
+                     p['key'],
+                     p['cert'],
+                     p.get('unmanaged', False))
