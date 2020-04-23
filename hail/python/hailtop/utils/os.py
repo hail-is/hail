912f4fcaa013acc3936754fe6eaacd9a738b12b0
Jackie Goldstein <jigold@broadinstitute.org>
2020-04-23 15:41:32 -0400

    address comments

--- hail/python/hailtop/utils/os.py
+++ hail/python/hailtop/utils/os.py
@@ -6,71 +6,6 @@ from functools import wraps
 from .utils import blocking_to_async, flatten
 
 
-wildcards = ('*', '?', '[', ']', '{', '}')
-
-
-# need a custom escape because escaped characters are not treated properly with glob.escape
-# and fnmatch doesn't work with escaped characters like \?
-def escape(path):
-    new_path = []
-    n = len(path)
-    i = 0
-    while i < n:
-        if i < n - 1 and path[i] == '\\' and path[i + 1] in wildcards:
-            new_path.append('[')
-            new_path.append(path[i + 1])
-            new_path.append(']')
-            i += 2
-            continue
-
-        new_path.append(path[i])
-        i += 1
-    return ''.join(new_path)
-
-
-def contains_wildcard(c):
-    i = 0
-    n = len(c)
-    while i < n:
-        if i < n - 1 and c[i] == '\\' and c[i + 1] in wildcards:
-            i += 2
-            continue
-        elif c[i] in wildcards:
-            return True
-        i += 1
-    return False
-
-
-def unescape_escaped_wildcards(c):
-    new_c = []
-    i = 0
-    n = len(c)
-    while i < n:
-        if i < n - 1 and c[i] == '\\' and c[i + 1] in wildcards:
-            new_c.append(c[i + 1])
-            i += 2
-            continue
-        new_c.append(c[i])
-        i += 1
-    return ''.join(new_c)
-
-
-def prefix_wout_wildcard(c):
-    new_c = []
-    i = 0
-    n = len(c)
-    while i < n:
-        if i < n - 1 and c[i] == '\\' and c[i + 1] in wildcards:
-            new_c.append(c[i + 1])
-            i += 2
-            continue
-        elif c[i] in wildcards:
-            return ''.join(new_c)
-        new_c.append(c[i])
-        i += 1
-    return ''.join(new_c)
-
-
 def make_parent_dirs(path):
     path = os.path.abspath(path)
     os.makedirs(os.path.dirname(path), exist_ok=True)
@@ -87,7 +22,7 @@ def remove(path):
         os.remove(path)
 
 
-def new_file(path, size):
+def touch(path, size=0):
     with open(path, 'ab') as fp:
         fp.truncate(size)
 
@@ -99,7 +34,7 @@ class AsyncOS:
         self._wrapped_cp = self._wrap(cp)
         self._wrapped_listdir = self._wrap(os.listdir)
         self._wrapped_remove = self._wrap(remove)
-        self._wrapped_new_file = self._wrap(new_file)
+        self._wrapped_touch = self._wrap(touch)
         self._wrapped_glob = self._wrap(glob.glob)
 
     def _wrap(self, fun):
@@ -123,8 +58,8 @@ class AsyncOS:
     async def remove(self, path):
         return await self._wrapped_remove(path)
 
-    async def new_file(self, path, size):
-        return await self._wrapped_new_file(path, size)
+    async def touch(self, path, size):
+        return await self._wrapped_touch(path, size)
 
     async def glob(self, pattern, recursive=False):
         return await self._wrapped_glob(pattern, recursive=recursive)
