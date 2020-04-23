ae7ca8aa9d4a1eeb78a90a6f623f261b9dfc5dfa
Jackie Goldstein <jigold@broadinstitute.org>
2020-04-23 08:41:59 -0400

    [hailtop,ci] Move flatten to hailtop

--- ci/ci/utils.py
+++ ci/ci/utils.py
@@ -7,7 +7,3 @@ def generate_token(size=12):
     alpha = string.ascii_lowercase
     alnum = string.ascii_lowercase + string.digits
     return secrets.choice(alpha) + ''.join([secrets.choice(alnum) for _ in range(size - 1)])
-
-
-def flatten(xxs):
-    return [x for xs in xxs for x in xs]
