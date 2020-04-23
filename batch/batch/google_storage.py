bd482538809e1d0a8282ecc177488eaf847174d3
Jackie Goldstein <jigold@broadinstitute.org>
2020-04-23 15:35:32 -0400

    address comments

--- batch/batch/google_storage.py
+++ batch/batch/google_storage.py
@@ -37,7 +37,7 @@ class GCS:
         self._wrapped_delete_gs_file = self._wrap_network_call(GCS._delete_gs_file)
         self._wrapped_delete_gs_files = self._wrap_network_call(GCS._delete_gs_files)
         self._wrapped_copy_gs_file = self._wrap_network_call(GCS._copy_gs_file)
-        self._wrapped_list_gs_files = self._wrap_network_call(GCS._list_gs_files)
+        self._wrapped_list_all_blobs_with_prefix = self._wrap_network_call(GCS._list_all_blobs_with_prefix)
         self._wrapped_compose_gs_file = self._wrap_network_call(GCS._compose_gs_file)
         self._wrapped_get_blob = self._wrap_network_call(GCS._get_blob)
 
@@ -77,8 +77,8 @@ class GCS:
         return await retry_transient_errors(self._wrapped_compose_gs_file,
                                             self, sources, dest, *args, **kwargs)
 
-    async def list_gs_files(self, uri, max_results=None):
-        return await retry_transient_errors(self._wrapped_list_gs_files,
+    async def list_all_blobs_with_prefix(self, uri, max_results=None):
+        return await retry_transient_errors(self._wrapped_list_all_blobs_with_prefix,
                                             self, uri, max_results=max_results)
 
     async def get_blob(self, uri):
@@ -95,46 +95,44 @@ class GCS:
         return wrapped
 
     def _write_gs_file_from_string(self, uri, string, *args, **kwargs):
-        f = self._get_blob(uri)
-        f.metadata = {'Cache-Control': 'no-cache'}
-        f.upload_from_string(string, *args, **kwargs)
+        b = self._get_blob(uri)
+        b.metadata = {'Cache-Control': 'no-cache'}
+        b.upload_from_string(string, *args, **kwargs)
 
     def _write_gs_file_from_file(self, uri, file_name, *args, **kwargs):
         with open(file_name, 'r') as file:
-            f = self._get_blob(uri)
-            f.metadata = {'Cache-Control': 'no-cache'}
-            f.upload_from_file(file, *args, **kwargs)
+            b = self._get_blob(uri)
+            b.metadata = {'Cache-Control': 'no-cache'}
+            b.upload_from_file(file, *args, **kwargs)
 
     def _read_gs_file(self, uri, *args, **kwargs):
         content = self._read_binary_gs_file(uri, *args, **kwargs)
         return content.decode('utf-8')
 
     def _read_binary_gs_file(self, uri, *args, **kwargs):
-        f = self._get_blob(uri)
-        f.metadata = {'Cache-Control': 'no-cache'}
-        content = f.download_as_string(*args, **kwargs)
+        b = self._get_blob(uri)
+        b.metadata = {'Cache-Control': 'no-cache'}
+        content = b.download_as_string(*args, **kwargs)
         return content
 
     def _read_gs_file_to_file(self, uri, file_name, offset, *args, **kwargs):
         with open(file_name, 'r+b') as file:
             file.seek(offset)
-            f = self._get_blob(uri)
-            f.metadata = {'Cache-Control': 'no-cache'}
-            f.download_to_file(file, *args, **kwargs)
+            b = self._get_blob(uri)
+            b.metadata = {'Cache-Control': 'no-cache'}
+            b.download_to_file(file, *args, **kwargs)
 
-    def _delete_gs_files(self, prefix):
-        bucket, prefix = GCS._parse_uri(prefix)
-        bucket = self.gcs_client.bucket(bucket)
-        for blob in bucket.list_blobs(prefix=prefix):
+    def _delete_gs_files(self, uri):
+        for blob in self._list_all_blobs_with_prefix(uri):
             try:
                 blob.delete()
             except google.api_core.exceptions.NotFound:
                 continue
 
     def _delete_gs_file(self, uri):
-        f = self._get_blob(uri)
+        b = self._get_blob(uri)
         try:
-            f.delete()
+            b.delete()
         except google.api_core.exceptions.NotFound:
             return
 
@@ -143,13 +141,12 @@ class GCS:
         src_bucket = self.gcs_client.bucket(src_bucket)
         dest_bucket, dest_path = GCS._parse_uri(dest)
         dest_bucket = self.gcs_client.bucket(dest_bucket)
-        src_f = src_bucket.blob(src_path)
-        src_bucket.copy_blob(src_f, dest_bucket, new_name=dest_path, *args, **kwargs)
+        src_blob = src_bucket.blob(src_path)
+        src_bucket.copy_blob(src_blob, dest_bucket, new_name=dest_path, *args, **kwargs)
 
-    def _list_gs_files(self, uri, max_results=None):
-        bucket_name, prefix = GCS._parse_uri(uri)
-        bucket = self.gcs_client.bucket(bucket_name)
-        return iter(bucket.list_blobs(prefix=prefix, max_results=max_results))
+    def _list_all_blobs_with_prefix(self, uri, max_results=None):
+        b = self._get_blob(uri)
+        return iter(b.bucket.list_blobs(prefix=b.name, max_results=max_results))
 
     def _compose_gs_file(self, sources, dest, *args, **kwargs):
         assert sources
