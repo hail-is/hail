e8da3478dfbda6069e5ea471bce385373c3da443
Tim Poterba <tpoterba@gmail.com>
2020-04-25 20:23:34 -0400

    [query] delete dead code in Annotation.scala

--- hail/src/main/scala/is/hail/annotations/Annotation.scala
+++ hail/src/main/scala/is/hail/annotations/Annotation.scala
@@ -5,32 +5,7 @@ import is.hail.utils._
 import org.apache.spark.sql.Row
 
 object Annotation {
-
-  final val COL_HEAD = "sa"
-
-  final val ROW_HEAD = "va"
-
-  final val GLOBAL_HEAD = "global"
-
-  final val ENTRY_HEAD = "g"
-
   val empty: Annotation = Row()
-
-  def emptyIndexedSeq(n: Int): IndexedSeq[Annotation] = Array.fill[Annotation](n)(Annotation.empty)
-
-  def printAnnotation(a: Any, nSpace: Int = 0): String = {
-    val spaces = " " * nSpace
-    a match {
-      case null => "Null"
-      case r: Row =>
-        "Struct:\n" +
-          r.toSeq.zipWithIndex.map { case (elem, index) =>
-            s"""$spaces[$index] ${ printAnnotation(elem, nSpace + 4) }"""
-          }
-            .mkString("\n")
-      case a => a.toString + ": " + a.getClass.getSimpleName
-    }
-  }
 
   def apply(args: Any*): Annotation = Row.fromSeq(args)
 
@@ -64,31 +39,5 @@ object Annotation {
 
       case TInt32 | TInt64 | TFloat32 | TFloat64 | TBoolean | TString | TCall | _: TLocus | TBinary => a
     }
-  }
-
-  def isSafe(typ: Type, a: Annotation): Boolean = {
-    a == null || (typ match {
-      case t: TBaseStruct =>
-        val r = a.asInstanceOf[Row]
-        !r.isInstanceOf[UnsafeRow] && Array.range(0, t.size).forall(i => Annotation.isSafe(t.types(i), r(i)))
-
-      case t: TArray =>
-        !a.isInstanceOf[UnsafeIndexedSeq] && a.asInstanceOf[IndexedSeq[Annotation]].forall(Annotation.isSafe(t.elementType, _))
-
-      case t: TSet =>
-        a.asInstanceOf[Set[Annotation]].forall(Annotation.isSafe(t.elementType, _))
-
-      case t: TDict =>
-        a.asInstanceOf[Map[Annotation, Annotation]]
-          .forall { case (k, v) => Annotation.isSafe(t.keyType, k) && Annotation.isSafe(t.valueType, v) }
-
-      case t: TInterval =>
-        val i = a.asInstanceOf[Interval]
-        Annotation.isSafe(t.pointType, i.start) && Annotation.isSafe(t.pointType, i.end)
-
-      case t: TNDArray => isSafe(t.representation, a)
-
-      case TInt32 | TInt64 | TFloat32 | TFloat64 | TBoolean | TString | TCall | _: TLocus | TBinary => true
-    })
   }
 }
