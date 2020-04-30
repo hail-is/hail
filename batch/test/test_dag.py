8abb6bc90c211bd5f6154829dfdc927c98da8474
Jackie Goldstein <jigold@broadinstitute.org>
2020-04-30 12:20:07 -0400

    fix

--- batch/test/test_dag.py
+++ batch/test/test_dag.py
@@ -140,6 +140,10 @@ def test_callback(client):
         # verify msec_mcpu, cost present
         callback_body.pop('cost')
         callback_body.pop('msec_mcpu')
+        callback_body.pop('time_created')
+        callback_body.pop('time_closed')
+        callback_body.pop('time_completed')
+        callback_body.pop('duration')
         assert (callback_body == {
             'id': b.id,
             'billing_project': 'test',
