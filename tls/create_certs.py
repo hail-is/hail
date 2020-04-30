af6400a5bb5788403f388305974a8c14dc41cbef
Daniel King <dking@broadinstitute.org>
2020-04-30 19:19:18 -0400

    fix

--- tls/create_certs.py
+++ tls/create_certs.py
@@ -50,26 +50,26 @@ def create_key_and_cert(p):
         '-key', key_file,
         '-out', csr_file
     ])
-    with tempfile.NamedTemporaryFile(mode='w') as extfile:
-        # this whole extfile nonsense is because OpenSSL has known, unfixed bugs
-        # in the x509 command. These really ought to be in the CSR.
-        # https://www.openssl.org/docs/man1.1.0/man1/x509.html#BUGS
-        # https://security.stackexchange.com/questions/150078/missing-x509-extensions-with-an-openssl-generated-certificate
-        extfile.write(f'[v3_ca]')
-        extfile.write(f'subjectAltName = {",".join("DNS:" + n for n in names)}\n')
-        extfile.flush()
-        echo_check_call([
-            'openssl', 'x509',
-            '-req',
-            '-in', csr_file,
-            '-CA', root_cert_file,
-            '-CAkey', root_key_file,
-            '-extensions', 'v3_ca',
-            '-extfile', extfile.name,
-            '-CAcreateserial',
-            '-out', cert_file,
-            '-days', '365'
-        ])
+    extfile = tempfile.NamedTemporaryFile(mode='w', delete=False)
+    # this whole extfile nonsense is because OpenSSL has known, unfixed bugs
+    # in the x509 command. These really ought to be in the CSR.
+    # https://www.openssl.org/docs/man1.1.0/man1/x509.html#BUGS
+    # https://security.stackexchange.com/questions/150078/missing-x509-extensions-with-an-openssl-generated-certificate
+    extfile.write(f'[v3_ca]')
+    extfile.write(f'subjectAltName = {",".join("DNS:" + n for n in names)}\n')
+    extfile.close()
+    echo_check_call([
+        'openssl', 'x509',
+        '-req',
+        '-in', csr_file,
+        '-CA', root_cert_file,
+        '-CAkey', root_key_file,
+        '-extensions', 'v3_ca',
+        '-extfile', extfile.name,
+        '-CAcreateserial',
+        '-out', cert_file,
+        '-days', '365'
+    ])
     return {'key': key_file, 'cert': cert_file}
 
 
