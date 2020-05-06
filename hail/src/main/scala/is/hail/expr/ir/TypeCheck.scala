e6a52b23febf332cab75ccabc194dfe8b43c092a
Daniel King <dking@broadinstitute.org>
2020-05-06 12:45:11 -0400

    wip

--- hail/src/main/scala/is/hail/expr/ir/TypeCheck.scala
+++ hail/src/main/scala/is/hail/expr/ir/TypeCheck.scala
@@ -347,7 +347,7 @@ object TypeCheck {
       case _: SerializeAggs =>
       case _: DeserializeAggs =>
       case Begin(xs) =>
-        xs.dropRight(1).zipWithIndex.foreach { case (x, i) =>
+        xs.dropRight(1).foreach { x =>
           assert(x.typ == TVoid)
         }
       case x@ApplyAggOp(initOpArgs, seqOpArgs, aggSig) =>
