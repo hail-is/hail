aa699ba92f966c88a807ef30381567d74af6bda9
John Compitello <johnc@broadinstitute.org>
2020-05-06 16:18:29 -0400

    Lower TableHead (#8695)
    * WIP
    
    * Almost there, just need to figure out why getEmitParam is going out of bounds
    
    * Got numPartitionsNeeded right, but need to figure out how to get right number of rows from last partition
    
    * Test passing
    
    * Refactored to avoid IR duplication of partitionSizeArray
    
    * Cleaned up, added many more test cases
    
    * Handle the zero partition case
    
    * Remove mapContexts, didn't end up using
    
    * Removed InferType assert messages, fixed paren spacing
    
    * Added mapContexts, streamSumIR, fixed TableHead not to scan whole table
    
    * Added ArrayLen simplify rule
    
    * Removed overly restrive ExecStrat

--- hail/src/main/scala/is/hail/expr/ir/lowering/LowerTableIR.scala
+++ hail/src/main/scala/is/hail/expr/ir/lowering/LowerTableIR.scala
@@ -34,6 +34,13 @@ abstract class TableStage(
     }
   }
 
+  def mapContexts(f: IR => IR): TableStage = {
+    val outer = this
+    new TableStage(letBindings, broadcastVals, globals, partitioner, f(contexts)) {
+      override def partition(ctxRef: Ref): IR = outer.partition(ctxRef)
+    }
+  }
+
   def mapGlobals(f: IR => IR): TableStage = {
     val newGlobals = f(globals)
     val newID = genUID()
@@ -188,6 +195,70 @@ object LowerTableIR {
           val loweredChild = lower(child)
           val env: Env[IR] = Env("row" -> row, "global" -> loweredChild.globals)
           loweredChild.mapPartition(rows => StreamFilter(rows, row.name, Subst(cond, BindingEnv(env))))
+
+        case TableHead(child, targetNumRows) =>
+          val loweredChild = lower(child)
+
+          val partitionSizeArrayFunc = genUID()
+          val howManyPartsToTry = Ref(genUID(), TInt32)
+          val partitionSizeArray = TailLoop(partitionSizeArrayFunc, FastIndexedSeq(howManyPartsToTry.name -> 4),
+            bindIR(loweredChild.mapContexts(ctxs => StreamTake(ctxs, howManyPartsToTry)).mapPartition(rows =>  ArrayLen(ToArray(rows))).collect()) { counts =>
+              If((Cast(streamSumIR(ToStream(counts)), TInt64) >= targetNumRows) || (ArrayLen(ToArray(loweredChild.contexts)) <= ArrayLen(counts)),
+                counts,
+                Recur(partitionSizeArrayFunc, FastIndexedSeq(howManyPartsToTry * 4), TArray(TInt32))
+              )
+            }
+          )
+          val partitionSizeArrayRef = Ref(genUID(), partitionSizeArray.typ)
+
+          val answerTuple = bindIR(ArrayLen(partitionSizeArrayRef)) { numPartitions =>
+            val howManyPartsToKeep = genUID()
+            val i = Ref(genUID(), TInt32)
+            val numLeft = Ref(genUID(), TInt64)
+            def makeAnswer(howManyParts: IR, howManyFromLast: IR) = MakeTuple(FastIndexedSeq((0, howManyParts), (1, howManyFromLast)))
+
+            If(numPartitions ceq 0,
+              makeAnswer(0, 0L),
+              TailLoop(howManyPartsToKeep, FastIndexedSeq(i.name -> 0, numLeft.name -> targetNumRows),
+                If((i ceq numPartitions - 1) || ((numLeft - Cast(ArrayRef(partitionSizeArrayRef, i), TInt64)) <= 0L),
+                  makeAnswer(i + 1, numLeft),
+                  Recur(howManyPartsToKeep, FastIndexedSeq(i + 1, numLeft - Cast(ArrayRef(partitionSizeArrayRef, i), TInt64)), TTuple(TInt32, TInt64))
+                )
+              )
+            )
+          }
+
+          new TableStage(
+            loweredChild.letBindings :+ partitionSizeArrayRef.name -> partitionSizeArray,
+            loweredChild.broadcastVals,
+            loweredChild.globals,
+            loweredChild.partitioner,
+            {
+              val contexts = loweredChild.contexts
+              val contextElementType = contexts.typ.asInstanceOf[TStream].elementType
+              bindIR(answerTuple) { answerTupleRef =>
+                val numParts = GetTupleElement(answerTupleRef, 0)
+                val numElementsFromLastPart = GetTupleElement(answerTupleRef, 1)
+                val onlyNeededPartitions = StreamTake(contexts, numParts)
+                val howManyFromEachPart = mapIR(rangeIR(numParts)) { idxRef =>
+                  If(idxRef ceq (numParts - 1),
+                    Cast(numElementsFromLastPart, TInt32),
+                    ArrayRef(partitionSizeArrayRef, idxRef)
+                  )
+                }
+                StreamZip(FastIndexedSeq(onlyNeededPartitions, howManyFromEachPart), FastIndexedSeq("part", "howMany"),
+                  MakeStruct(FastIndexedSeq("numberToTake" -> Ref("howMany", TInt32), "old" -> Ref("part", contextElementType))),
+                  ArrayZipBehavior.AssumeSameLength
+                )
+              }
+            }
+          ) {
+            override def partition(ctxRef: Ref): IR = {
+              bindIR(GetField(ctxRef, "old")) { oldRef =>
+                StreamTake(loweredChild.partition(oldRef), GetField(ctxRef, "numberToTake"))
+              }
+            }
+          }
 
         case TableMapRows(child, newRow) =>
           if (ContainsScan(newRow))
