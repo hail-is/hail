0c1f6df11568956f81906ca1280d3bd812859dfc
Cotton Seed <cotton@alum.mit.edu>
2020-05-10 21:14:13 +0100

    fix

--- hail/python/hail/typecheck/__init__.py
+++ hail/python/hail/typecheck/__init__.py
@@ -1,7 +1,7 @@
 from .check import TypeChecker, typecheck, typecheck_method, anytype, anyfunc, \
-    nullable, sequenceof, tupleof, sized_tupleof, sliceof, dictof, setof, \
-    oneof, exactly, numeric, char, lazy, enumeration, identity, transformed, \
-    func_spec, table_key_type, TypecheckFailure
+    nullable, sequenceof, tupleof, sized_tupleof, sliceof, dictof, \
+    linked_list, setof, oneof, exactly, numeric, char, lazy, enumeration, \
+    identity, transformed, func_spec, table_key_type, TypecheckFailure
 
 __all__ = [
     'TypeChecker',
@@ -15,6 +15,7 @@ __all__ = [
     'sized_tupleof',
     'sliceof',
     'dictof',
+    'linked_list',
     'setof',
     'oneof',
     'exactly',
