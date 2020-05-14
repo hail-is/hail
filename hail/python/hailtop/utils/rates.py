1291051fd7358da9c7b777aa6f6631f914d28e5d
Jackie Goldstein <jigold@broadinstitute.org>
2020-05-14 09:00:32 -0400

    address comments

--- hail/python/hailtop/utils/rates.py
+++ hail/python/hailtop/utils/rates.py
@@ -3,17 +3,17 @@ def rate_cpu_hour_to_mcpu_msec(rate_cpu_hour):
     return rate_cpu_sec * 0.001 * 0.001
 
 
-def rate_gb_hour_to_mb_msec(rate_gb_hour):
-    rate_mb_hour = rate_gb_hour / 1024
+def rate_gib_hour_to_mb_msec(rate_gib_hour):
+    rate_mb_hour = rate_gib_hour / 1024
     rate_mb_sec = rate_mb_hour / 3600
     return rate_mb_sec * 0.001
 
 
-def rate_gb_month_to_mb_msec(rate_gb_month):
+def rate_gib_month_to_mb_msec(rate_gib_month):
     # average number of days per month = 365.25 / 12 = 30.4375
     avg_n_days_per_month = 30.4375
-    rate_gb_hour = rate_gb_month / avg_n_days_per_month / 24
-    return rate_gb_hour_to_mb_msec(rate_gb_hour)
+    rate_gib_hour = rate_gib_month / avg_n_days_per_month / 24
+    return rate_gib_hour_to_mb_msec(rate_gib_hour)
 
 
 def rate_instance_hour_to_fraction_msec(rate_instance_hour, base):
