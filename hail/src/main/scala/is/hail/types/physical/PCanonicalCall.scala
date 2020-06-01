2259e30ebacad8a8fd7e1a43baf438ea7bdb8a93
Daniel King <dking@broadinstitute.org>
2020-06-01 17:45:01 -0400

    [hail] fix formatting in PCanonicalCall

--- hail/src/main/scala/is/hail/types/physical/PCanonicalCall.scala
+++ hail/src/main/scala/is/hail/types/physical/PCanonicalCall.scala
@@ -7,18 +7,18 @@ import is.hail.utils.FastIndexedSeq
 import is.hail.variant.Genotype
 
 final case class PCanonicalCall(required: Boolean = false) extends PCall {
-    def _asIdent = "call"
+  def _asIdent = "call"
 
-    override def _pretty(sb: StringBuilder, indent: Int, compact: Boolean): Unit = sb.append("PCCall")
+  override def _pretty(sb: StringBuilder, indent: Int, compact: Boolean): Unit = sb.append("PCCall")
 
-    val representation: PType = PInt32(required)
+  val representation: PType = PInt32(required)
 
-    def codeOrdering(mb: EmitMethodBuilder[_], other: PType): CodeOrdering = {
-      assert(other isOfType this)
-      PInt32().codeOrdering(mb)
-    }
+  def codeOrdering(mb: EmitMethodBuilder[_], other: PType): CodeOrdering = {
+    assert(other isOfType this)
+    PInt32().codeOrdering(mb)
+  }
 
-    def setRequired(required: Boolean) = if(required == this.required) this else PCanonicalCall(required)
+  def setRequired(required: Boolean) = if(required == this.required) this else PCanonicalCall(required)
 }
 
 object PCanonicalCallSettable {
