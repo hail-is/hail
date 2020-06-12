066dde77f3ddcfff9432f427d703666c8ae30657
Tim Poterba <tpoterba@broadinstitute.org>
2020-06-12 11:47:17 -0400

    [query] Add requiredness analysis to LowerToCDA (#8951)
    Also rename "LowerIR" to "LowerToCDA"
    
    This is necessary for TableAggregate.

--- hail/src/main/scala/is/hail/expr/ir/lowering/LowerToCDA.scala
+++ hail/src/main/scala/is/hail/expr/ir/lowering/LowerToCDA.scala
@@ -1,17 +1,23 @@
 package is.hail.expr.ir.lowering
 
-import is.hail.expr.ir.{BlockMatrixIR, Copy, ExecuteContext, IR, MatrixIR, Pretty, TableIR}
+import is.hail.expr.ir.{BlockMatrixIR, Copy, ExecuteContext, IR, MatrixIR, Pretty, Requiredness, RequirednessAnalysis, TableIR}
 
-object LowerIR {
-  def lower(ir: IR, typesToLower: DArrayLowering.Type, ctx: ExecuteContext): IR = ir match {
+object LowerToCDA {
+
+  def apply(ir: IR, typesToLower: DArrayLowering.Type, ctx: ExecuteContext): IR = {
+    val r = Requiredness(ir, ctx)
+    lower(ir, typesToLower, ctx, r)
+  }
+
+  def lower(ir: IR, typesToLower: DArrayLowering.Type, ctx: ExecuteContext, r: RequirednessAnalysis): IR = ir match {
     case node if node.children.forall(_.isInstanceOf[IR]) =>
-      Copy(node, ir.children.map { case c: IR => lower(c, typesToLower, ctx) })
+      Copy(node, ir.children.map { case c: IR => lower(c, typesToLower, ctx, r) })
 
     case node if node.children.exists(n => n.isInstanceOf[TableIR]) && node.children.forall(n => n.isInstanceOf[TableIR] || n.isInstanceOf[IR]) =>
-      LowerTableIR(ir, typesToLower, ctx)
+      LowerTableIR(ir, typesToLower, ctx, r)
 
     case node if node.children.exists(n => n.isInstanceOf[BlockMatrixIR]) && node.children.forall(n => n.isInstanceOf[BlockMatrixIR] || n.isInstanceOf[IR]) =>
-      LowerBlockMatrixIR(ir, typesToLower, ctx)
+      LowerBlockMatrixIR(ir, typesToLower, ctx, r)
 
     case node if node.children.exists(_.isInstanceOf[MatrixIR]) =>
       throw new LowererUnsupportedOperation(s"MatrixIR nodes must be lowered to TableIR nodes separately: \n${ Pretty(node) }")
