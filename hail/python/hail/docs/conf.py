6b79059a44bf676c510cc182d7b55b3c7b7a8ad3
akotlar <akotlar@bu.edu>
2020-06-13 10:06:33 -0400

    fix  make-docs

--- hail/python/hail/docs/conf.py
+++ hail/python/hail/docs/conf.py
@@ -224,7 +224,14 @@ html_static_path = ['_static']
 #
 www_root = os.path.dirname(os.path.abspath(__file__))
 _i = 0
-while _i < 50 and not os.path.isdir(os.path.join(www_root, 'www')):
+while _i < 50:
+    if(os.path.isdir(os.path.join(www_root, 'www'))):
+        break
+
+    if(os.path.isdir(os.path.join(www_root, 'site'))):
+        www_root = os.path.join(www_root, 'site')
+        break
+
     www_root = os.path.join(www_root, '..')
     _i += 1
 html_extra_path = [os.path.join(www_root, 'www/hail-logo-cropped.png'),
