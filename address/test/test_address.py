f5f5024c09653673dc17dbe9d2580aaafebb8f8b
Daniel King <dking@broadinstitute.org>
2020-06-24 18:35:32 -0400

    fix

--- address/test/test_address.py
+++ address/test/test_address.py
@@ -4,8 +4,7 @@ from hailtop.config import get_deploy_config
 from hailtop.tls import ssl_client_session
 from hailtop.utils import request_retry_transient_errors
 
-pytestmark = pytest.mark.asyncio
-
+@pytest.mark.asyncio
 async def test_connect_to_address_on_pod_ip():
     async with ssl_client_session(
             raise_for_status=True, timeout=aiohttp.ClientTimeout(total=60)) as session:
