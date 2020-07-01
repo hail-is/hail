75cb0929693f22cce61b8c44eeab290bfcf0f933
Daniel King <dking@broadinstitute.org>
2020-07-01 10:03:48 -0400

    fix tests

--- hail/python/test/hailtop/utils/test_utils.py
+++ hail/python/test/hailtop/utils/test_utils.py
@@ -6,16 +6,17 @@ def test_partition_zero_empty():
 
 
 def test_partition_even_small():
-    assert list(partition(3, range(3))) == [[0], [1], [2]]
+    assert list(partition(3, range(3))) == [range(0, 1), range(1, 2), range(2, 3)]
 
 
 def test_partition_even_big():
-    assert list(partition(3, range(9))) == [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    assert list(partition(3, range(9))) == [range(0, 3), range(3, 6), range(6, 9)]
 
 
 def test_partition_uneven_big():
-    assert list(partition(2, range(9))) == [[0, 1, 2, 3, 4], [5, 6, 7, 8]]
+    assert list(partition(2, range(9))) == [range(0, 5), range(5, 9)]
 
 
 def test_partition_toofew():
-    assert list(partition(6, range(3))) == [[0], [1], [2], [], [], []]
+    assert list(partition(6, range(3))) == [range(0, 1), range(1, 2), range(2, 3),
+                                            range(3, 3), range(3, 3), range(3, 3)]
