ce0c6364927ec3f35222d9c4f2b30833adc3d07a
John Compitello <j301600@gmail.com>
2020-07-09 13:12:35 -0400

    WIP

--- hail/src/main/scala/is/hail/expr/ir/agg/NDArraySumAggregator.scala
+++ hail/src/main/scala/is/hail/expr/ir/agg/NDArraySumAggregator.scala
@@ -2,18 +2,30 @@ package is.hail.expr.ir.agg
 
 import is.hail.annotations.StagedRegionValueBuilder
 import is.hail.expr.ir.{EmitCode, EmitCodeBuilder, IR}
-import is.hail.types.physical.{PCode, PNDArray, PNDArrayCode, PNDArrayValue, PType}
+import is.hail.types.physical.{PCanonicalTuple, PCode, PNDArray, PNDArrayCode, PNDArrayValue, PType}
 
-class NDArraySumAggregator (typ: PNDArray, knownShape: Option[IR]) extends StagedAggregator {
+object NDArraySumAggregator {
+
+}
+
+class NDArraySumAggregator (ndTyp: PNDArray, knownShape: Option[IR]) extends StagedAggregator {
   override type State = TypedRegionBackedAggState
 
-  override def resultType: PType = typ // Input and output same type seems fine.
+  // State needs:
+  // 1. Initialized or not
+  // 2.
+  val stateType = PCanonicalTuple(true, ???, ndTyp)
+
+  override def resultType: PType = ndTyp
 
   override def initOpTypes: Seq[PType] = ???
 
   override def seqOpTypes: Seq[PType] = ???
 
-  override protected def _initOp(cb: EmitCodeBuilder, state: State, init: Array[EmitCode]): Unit = ???
+  override protected def _initOp(cb: EmitCodeBuilder, state: State, init: Array[EmitCode]): Unit = {
+
+    ???
+  }
 
   override protected def _seqOp(cb: EmitCodeBuilder, state: State, seq: Array[EmitCode]): Unit = ???
 
@@ -21,12 +33,12 @@ class NDArraySumAggregator (typ: PNDArray, knownShape: Option[IR]) extends Stage
     // Need to:
     // 1. Check the shapes match and the strides match.
     // 2. If they match, add.
-    val leftCode = PCode(typ, state.off).asInstanceOf[PNDArrayCode]
-    val rightCode = PCode(typ, other.off).asInstanceOf[PNDArrayCode]
+    val leftCode = PCode(ndTyp, state.off).asInstanceOf[PNDArrayCode]
+    val rightCode = PCode(ndTyp, other.off).asInstanceOf[PNDArrayCode]
     val leftValue = cb.memoize(leftCode, "left_pndarraycode").asInstanceOf[PNDArrayValue]
     val rightValue = cb.memoize(rightCode, "right_pndarraycode").asInstanceOf[PNDArrayValue]
-    val comparator = typ.codeOrdering(cb.emb, typ)
-    
+    val comparator = ndTyp.codeOrdering(cb.emb, ndTyp)
+
     ???
   }
 
