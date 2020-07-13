d252025f69cfaff6627e9531f28b8acaf7db6b9b
Daniel King <dking@broadinstitute.org>
2020-07-13 18:17:49 -0400

    [dnd] add DNDArray.inner_product
    Implement an arbitrary inner product operation. This is the critical piece of
    infrastructure that underlies all relatedness inference algorithms. The typical
    inner product on real number matrices is defined as:
    
    ```
    L_ij : matrix of shape a by b
    M_kl : matrix of shape b by c
    N_il : matrix of shape a by c, the inner product of L and M.
    
    N_il = Sum_k (L_ik * M_kl)
    ```
    
    This PR allows the user to define what `*` means and what `Sum` means. For
    example, the KING paper defines an estimator for relatedness of homogeneous
    populations called KING-homo. KING-homo's numerator is given by
    `score_difference` below.
    
    ```python3
    mt = hl.balding_nichols_model(2, 5, 5)
    mt = mt.select_entries(genotype_score=hl.float(mt.GT.n_alt_alleles()))
    da = hl.experimental.dnd.array(mt, 'genotype_score', block_size=3)
    score_difference = da.T.inner_product(
            da,
            lambda l, r: sqr(l - r),
            lambda l, r: l + r,
            hl.float(0),
            hl.agg.sum
    )
    ```
    
    The rest of KING-homo is just manipulation of row fields.
    
    Eventually we need to implement `ndarray_inner_product(self, other, product,
    sum)` which does a cache-friendly pass over the data but applies arbitrary user
    product and sum operations.

--- hail/python/test/hail/experimental/test_dnd_array.py
+++ hail/python/test/hail/experimental/test_dnd_array.py
@@ -110,3 +110,50 @@ def test_medium_matmul():
     a_result = a @ a.T
 
     assert np.array_equal(da_result, a_result)
+
+
+def test_matmul_via_inner_product():
+    n_variants = 10
+    n_samples = 10
+    block_size = 3
+    n_blocks = 16
+    mt = hl.utils.range_matrix_table(n_variants, n_samples)
+    mt = mt.select_entries(x=mt.row_idx * mt.col_idx)
+
+    da = hl.experimental.dnd.array(mt, 'x', block_size=block_size)
+    prod = (da @ da.T).checkpoint(new_temp_file())
+    assert prod._force_count_blocks() == n_blocks
+    prod_result = prod.collect().reshape(n_variants, n_variants)
+
+    ip_result = da.inner_product(da.T,
+                                 lambda l, r: l * r,
+                                 lambda l, r: l + r,
+                                 hl.float(0.0),
+                                 lambda prod: hl.agg.sum(prod)
+    ).collect().reshape(n_variants, n_variants)
+
+    assert np.array_equal(prod_result, ip_result)
+
+
+def test_king_homo_estimator():
+    hl.set_global_seed(1)
+    mt = hl.balding_nichols_model(2, 5, 5)
+    mt = mt.select_entries(genotype_score=hl.float(mt.GT.n_alt_alleles()))
+    da = hl.experimental.dnd.array(mt, 'genotype_score', block_size=3)
+
+    def sqr(x):
+        return x * x
+    score_difference = da.T.inner_product(
+        da,
+        lambda l, r: sqr(l - r),
+        lambda l, r: l + r,
+        hl.float(0),
+        hl.agg.sum
+    ).checkpoint(new_temp_file())
+    assert np.array_equal(
+        score_difference.collect(),
+        np.array([[0., 6., 4., 2., 4.],
+                  [6., 0., 6., 4., 6.],
+                  [4., 6., 0., 6., 0.],
+                  [2., 4., 6., 0., 6.],
+                  [4., 6., 0., 6., 0.]]))
