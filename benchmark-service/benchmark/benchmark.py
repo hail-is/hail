65470f9a6c92df416b8a20acea81937c2d5d5581
Dania Abuhijleh <abuhijleh.d@northeastern.edu>
2020-07-14 10:42:11 -0400

    small fixes

--- benchmark-service/benchmark/benchmark.py
+++ benchmark-service/benchmark/benchmark.py
@@ -7,18 +7,28 @@ import jinja2
 from aiohttp import web
 import logging
 
+
 app = web.Application()
 router = web.RouteTableDef()
 logging.basicConfig(level=logging.DEBUG)
 
 
+async def init_app() -> web.Application:
+    app.add_routes(router)
+    aiohttp_jinja2.setup(
+        app, loader=jinja2.ChoiceLoader([
+            jinja2.PackageLoader('benchmark', 'templates')
+        ]))
+    return app
+
+
 aiohttp_jinja2.setup(
     app, loader=jinja2.ChoiceLoader([
-        jinja2.PackageLoader('benchmark', 'templates')
+        jinja2.PackageLoader('benchmark')
     ]))
 
 
-@router.get("/healthcheck")
+@router.get('/healthcheck')
 async def healthcheck() -> web.Response:
     return web.Response()
 
@@ -27,32 +37,22 @@ async def healthcheck() -> web.Response:
 async def greet_user(request: web.Request) -> web.Response:
 
     context = {
-        'username': request.match_info.get("username", ""),
+        'username': request.match_info.get('username', ''),
         'current_date': 'July 10, 2020'
     }
-    response = aiohttp_jinja2.render_template("user.html", request,
+    response = aiohttp_jinja2.render_template('user.html', request,
                                               context=context)
     return response
 
 
-@router.get("/")
+@router.get('/')
 async def index(request: web.Request) -> Dict[str, Any]:
     context = {
         'current_date': 'July 10, 2020'
     }
-    response = aiohttp_jinja2.render_template("index.html", request,
+    response = aiohttp_jinja2.render_template('index.html', request,
                                               context=context)
     return response
-
-
-async def init_app() -> web.Application:
-    app = web.Application()
-    app.add_routes(router)
-    aiohttp_jinja2.setup(
-        app, loader=jinja2.ChoiceLoader([
-            jinja2.PackageLoader('benchmark', 'templates')
-        ]))
-    return app
 
 
 web.run_app(init_app(), port=5000)
