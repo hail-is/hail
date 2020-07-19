88aa36ce94ffd3501602784887fd40ece917834a
Nick Watts <nwatts@broadinstitute.org>
2020-07-19 11:16:16 -0400

    Add tests for hailctl dataproc modify --update-hail-version (#9103)

---
+++ hail/python/hailtop/hailctl/dataproc/deploy_metadata.py
@@ -0,0 +1,11 @@
+import pkg_resources
+import yaml
+
+
+def get_deploy_metadata():
+    if not pkg_resources.resource_exists("hailtop.hailctl", "deploy.yaml"):
+        raise RuntimeError(f"package has no 'deploy.yaml' file")
+
+    deploy_metadata = yaml.safe_load(pkg_resources.resource_stream("hailtop.hailctl", "deploy.yaml"))
+
+    return deploy_metadata["dataproc"]
