2eda40ed3fe8ce68dc020f99a0f502413c10702f
Daniel King <dking@broadinstitute.org>
2020-07-23 15:30:50 -0400

    fix

--- hail/generate_splits.py
+++ hail/generate_splits.py
@@ -2,7 +2,24 @@ import os
 import random
 import subprocess as sp
 import jinja2
-from hailtop.utils import partition
+
+
+def partition(k, ls):
+    assert k > 0
+    assert ls
+
+    n = len(ls)
+    parts = [(n - i + k - 1) // k for i in range(k)]
+    assert sum(parts) == n
+    assert max(parts) - min(parts) <= 1
+
+    out = []
+    start = 0
+    for part in parts:
+        out.append(ls[start:start + part])
+        start += part
+    return out
+
 
 sp.run("find src/test \\( -name \"*.scala\" -o -name \"*.java\" \\) -type f > classes",
        shell=True,
@@ -16,7 +33,7 @@ with open('classes', 'r') as f:
 
 random.shuffle(classes, lambda: 0.0)
 
-splits = list(partition(n_splits, classes))
+splits = partition(n_splits, classes)
 
 with open('testng-splits.xml', 'r') as f:
     template = jinja2.Template(f.read(), undefined=jinja2.StrictUndefined, trim_blocks=True, lstrip_blocks=True)
