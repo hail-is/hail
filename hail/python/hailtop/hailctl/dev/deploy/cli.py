6c6c62003f12fef7b864ebaaef5a7a7e8fdcffa9
Daniel King <dking@broadinstitute.org>
2020-07-22 22:28:51 -0400

    fix import in deploy cli .py

--- hail/python/hailtop/hailctl/dev/deploy/cli.py
+++ hail/python/hailtop/hailctl/dev/deploy/cli.py
@@ -5,7 +5,7 @@ import sys
 
 from hailtop.config import get_deploy_config
 from hailtop.auth import service_auth_headers
-from hailtop.tls import get_context_specific_client_ssl_context
+from hailtop.tls import get_context_specific_ssl_client_session
 
 
 def init_parser(parser):
