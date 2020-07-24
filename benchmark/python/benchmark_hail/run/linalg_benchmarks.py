640ed3f8f2c815cd90f245992c67f9161bf1fed5
Tim Poterba <tpoterba@broadinstitute.org>
2020-07-24 17:10:46 -0400

    [query] Move slow/bad memory test to benchmarks, improve BM.WFEE pruning (#9150)
    * Improves pruning of BlockMatrix.write_from_entry_expr to avoid large
    allocation in the slow test.
    * Delete the slow test.
    * Add two benchmarks that should catch this

--- benchmark/python/benchmark_hail/run/linalg_benchmarks.py
+++ benchmark/python/benchmark_hail/run/linalg_benchmarks.py
@@ -16,17 +16,35 @@ def make_ndarray_bench():
     ht = ht.annotate(x=hl.nd.array(hl.range(200_000)))
     ht._force_count()
 
+
 @benchmark()
 def ndarray_addition_benchmark():
     arr = hl.nd.ones((1024, 1024))
     hl.eval(arr + arr)
+
 
 @benchmark()
 def ndarray_matmul_int64_benchmark():
     arr = hl.nd.arange(1024 * 1024).map(hl.int64).reshape((1024, 1024))
     hl.eval(arr @ arr)
 
+
 @benchmark()
 def ndarray_matmul_float64_benchmark():
     arr = hl.nd.arange(1024 * 1024).map(hl.float64).reshape((1024, 1024))
     hl.eval(arr @ arr)
+
+
+@benchmark()
+def blockmatrix_write_from_entry_expr_range_mt():
+    mt = hl.utils.range_matrix_table(40_000, 40_000, n_partitions=4)
+    path = hl.utils.new_temp_file(extension='bm')
+    hl.linalg.BlockMatrix.write_from_entry_expr(mt.row_idx + mt.col_idx, path)
+
+
+@benchmark()
+def blockmatrix_write_from_entry_expr_range_mt_standardize():
+    mt = hl.utils.range_matrix_table(40_000, 40_000, n_partitions=4)
+    path = hl.utils.new_temp_file(extension='bm')
+    hl.linalg.BlockMatrix.write_from_entry_expr(mt.row_idx + mt.col_idx, path, mean_inpute=True, center=True,
+                                                normalize=True)
