fd2deabea26afb9c397aa4507a52294430eb5793
Dania Abuhijleh <abuhijleh.d@northeastern.edu>
2020-07-29 11:09:10 -0400

    add table templating

--- benchmark-service/benchmark/benchmark.py
+++ benchmark-service/benchmark/benchmark.py
@@ -7,13 +7,44 @@ from hailtop.config import get_deploy_config
 from hailtop.tls import get_in_cluster_server_ssl_context
 from hailtop.hail_logging import AccessLogger, configure_logging
 from web_common import setup_aiohttp_jinja2, setup_common_static_routes
-
+import json
+import re
 
 configure_logging()
 router = web.RouteTableDef()
 logging.basicConfig(level=logging.DEBUG)
 deploy_config = get_deploy_config()
 log = logging.getLogger('benchmark')
+
+
+file_path = '/Users/dabuhijl/hail/0.2.45-ac6815ee857c-master.json'
+with open(file_path) as f:
+    pre_data = json.load(f)
+print(json.dumps(pre_data, indent=4, sort_keys=True))
+
+x = re.findall('.*/+(.*)-(.*)-(.*)?\.json', file_path)
+sha = x[0][1]
+
+data = list()
+prod_of_means = 1
+for d in pre_data['benchmarks']:
+    stats = dict()
+    stats['name'] = d['name']
+    stats['failed'] = d['failed']
+    if not (d['failed']):
+        prod_of_means *= d['mean']
+        stats['f-stat'] = d['f-stat']
+        stats['mean'] = d['mean']
+        stats['median'] = d['median']
+        stats['p-value'] = d['p-value']
+        stats['stdev'] = d['stdev']
+    data.append(stats)
+geometric_mean = prod_of_means ** (1.0 / len(pre_data['benchmarks']))
+
+benchmarks = dict()
+benchmarks['sha'] = sha
+benchmarks['geometric-mean'] = geometric_mean
+benchmarks['data'] = data
 
 
 @router.get('/healthcheck')
@@ -41,7 +72,7 @@ async def index(request: web.Request, userdata) -> Dict[str, Any]:  # pylint: di
         'current_date': 'July 10, 2020'
     }
     response = aiohttp_jinja2.render_template('index.html', request,
-                                              context=context)
+                                              context=benchmarks)
     return response
 
 
