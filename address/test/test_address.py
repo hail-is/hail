eb76d326e6878714f32678e81668d99deacba6de
Daniel King <dking@broadinstitute.org>
2020-07-30 17:35:52 -0400

    fix arg

--- address/test/test_address.py
+++ address/test/test_address.py
@@ -14,8 +14,7 @@ async def test_connect_to_address_on_pod_ip():
     async with in_cluster_no_hostname_checks_ssl_client_connection(
             raise_for_status=True,
             timeout=aiohttp.ClientTimeout(total=60),
-            headers=service_auth_headers(deploy_config, 'address'),
-            check_hostname=False) as session:
+            headers=service_auth_headers(deploy_config, 'address')) as session:
         address, port = await deploy_config.address('address')
         await request_retry_transient_errors(
             session,
