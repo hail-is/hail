2b8e7d7f0d40ff9145ee850bcf6a21681cebbb19
Jackie Goldstein <jigold@broadinstitute.org>
2020-07-30 16:49:30 -0400

    [batch] cleanup content in worker.log

--- batch/batch/worker/worker.py
+++ batch/batch/worker/worker.py
@@ -317,7 +317,6 @@ class Container:
                 return None
             raise
 
-        log.info(f'{self} container info {c}')
         cstate = c['State']
         status = {
             'state': cstate['Status'],
@@ -365,7 +364,7 @@ class Container:
 
             async with self.step('creating'):
                 config = self.container_config()
-                log.info(f'starting {self} config {config}')
+                log.info(f'starting {self}')
                 self.container = await docker_call_retry(MAX_DOCKER_OTHER_OPERATION_SECS, f'{self}')(
                     create_container, config, name=f'batch-{self.job.batch_id}-job-{self.job.job_id}-{self.name}')
 
@@ -395,7 +394,6 @@ class Container:
                     timed_out = True
 
             self.container_status = await self.get_container_status()
-            log.info(f'{self}: container status {self.container_status}')
 
             async with self.step('uploading_log'):
                 await worker.log_store.write_log_file(
@@ -712,7 +710,7 @@ class Job:
         return (self.batch_id, self.job_id)
 
     async def run(self, worker):
-        async with worker.cpu_sem(self.cpu_in_mcpu, f'{self}'):
+        async with worker.cpu_sem(self.cpu_in_mcpu):
             self.start_time = time_msecs()
 
             try:
