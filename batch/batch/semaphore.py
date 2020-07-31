16ed82dfc795b505f82d353e690fb73c71a3fe77
jigold <jigold@users.noreply.github.com>
2020-07-31 11:50:28 -0400

    [batch] cleanup content in worker.log (#9184)

--- batch/batch/semaphore.py
+++ batch/batch/semaphore.py
@@ -19,16 +19,15 @@ class NullWeightedSemaphore:
 
 
 class FIFOWeightedSemaphoreContextManager:
-    def __init__(self, sem, weight, id):
+    def __init__(self, sem, weight):
         self.sem = sem
         self.weight = weight
-        self.id = id
 
     async def __aenter__(self):
-        await self.sem.acquire(self.weight, self.id)
+        await self.sem.acquire(self.weight)
 
     async def __aexit__(self, exc_type, exc, tb):
-        self.sem.release(self.weight, self.id)
+        self.sem.release(self.weight)
 
 
 class FIFOWeightedSemaphore:
@@ -36,34 +35,28 @@ class FIFOWeightedSemaphore:
         self.value = value
         self.queue = collections.deque()
 
-    async def acquire(self, weight, id):
+    async def acquire(self, weight):
         if not self.queue and self.value >= weight:
             self.value -= weight
-            log.info(f'acquired semaphore for {id} with weight {weight} without waiting, new value = {self.value}')
             return
 
         event = asyncio.Event()
-        self.queue.append((event, weight, id))
+        self.queue.append((event, weight))
         event.clear()
         await event.wait()
 
-    def release(self, weight, id):
+    def release(self, weight):
         self.value += weight
-        log.info(f'released semaphore for {id} with weight {weight}, new value = {self.value}')
-
         n_notified = 0
-        log.info(f'found {len(self.queue)} events waiting in the queue')
         while self.queue:
-            head_event, head_weight, head_id = self.queue[0]
+            head_event, head_weight = self.queue[0]
             if self.value >= head_weight:
                 head_event.set()
                 self.queue.popleft()
                 self.value -= head_weight
                 n_notified += 1
-                log.info(f'notified event for {head_id} with weight {head_weight}, new value = {self.value}')
             else:
-                log.info(f'notified {n_notified} events before being blocked by {head_id} with weight {head_weight}, value = {self.value}')
                 break
 
-    def __call__(self, weight, id):
-        return FIFOWeightedSemaphoreContextManager(self, weight, id)
+    def __call__(self, weight):
+        return FIFOWeightedSemaphoreContextManager(self, weight)
