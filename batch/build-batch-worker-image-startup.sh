5981c4e7cb9a2883b4e57fb785f001debe6716dd
Jackie Goldstein <jigold@broadinstitute.org>
2020-07-31 12:58:10 -0400

    address comments

--- batch/build-batch-worker-image-startup.sh
+++ batch/build-batch-worker-image-startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh
+set -ex
+
+curl --silent --show-error --remote-name --fail https://dl.google.com/cloudagents/add-logging-agent-repo.sh
 bash add-logging-agent-repo.sh
 
 apt-get update
