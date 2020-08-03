d78efd21c802381eaed2545c94d7906670c03edf
Tim Poterba <tpoterba@gmail.com>
2020-08-03 09:23:48 -0400

    [query] Fix errors in LowerToCDA.
    * Fix error in TableKeyByAndAggregate caused by field name clobbering
    * Fix error in TableLeftJoinRightDistinct with non-strict left tables
    * Fix erroneous key preservation in TableStage.mapPartition
    * Add Consume to TypeCheck
    * Fix error where globals were not exposed in TableAggregate
    * Fix error where globals were not exposed in TableAggregate
    
    New local backend success rate:
    
    ```
    271 failed, 496 passed, 75 skipped, 15 warnings in 368.17 seconds
    ```

--- hail/src/main/scala/is/hail/expr/ir/lowering/LoweringPipeline.scala
+++ hail/src/main/scala/is/hail/expr/ir/lowering/LoweringPipeline.scala
@@ -1,6 +1,6 @@
 package is.hail.expr.ir.lowering
 
-import is.hail.expr.ir.{BaseIR, ExecuteContext}
+import is.hail.expr.ir.{BaseIR, ExecuteContext, TypeCheck}
 import is.hail.utils._
 
 case class LoweringPipeline(lowerings: LoweringPass*) {
@@ -16,6 +16,12 @@ case class LoweringPipeline(lowerings: LoweringPass*) {
         case e: Throwable =>
           log.error(s"error while applying lowering '${ l.context }'")
           throw e
+      }
+      try {
+        TypeCheck(x)
+      } catch {
+        case e: Throwable =>
+          fatal(s"error after applying ${ l.context }", e)
       }
     }
 
@@ -61,7 +67,9 @@ object LoweringPipeline {
       OptimizePass("darrayLowerer, after LowerMatrixToTable"),
       LiftRelationalValuesToRelationalLets,
       LowerToDistributedArrayPass(DArrayLowering.All),
-      OptimizePass("darrayLowerer, after LowerToCDA")
+      OptimizePass("darrayLowerer, after LowerToCDA"),
+      OptimizePass("darrayLowerer, after LowerToCDA AGAIN"),
+      OptimizePass("darrayLowerer, after LowerToCDA AGAIN")
     ),
     DArrayLowering.TableOnly -> LoweringPipeline(
       OptimizePass("darrayLowerer, initial IR"),
