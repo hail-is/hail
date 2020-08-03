a83cc041cc9add988980a24403e76053f6f89a24
Dania Abuhijleh <abuhijleh.d@northeastern.edu>
2020-08-03 10:48:25 -0400

    wip

--- benchmark-service/benchmark/benchmark.py
+++ benchmark-service/benchmark/benchmark.py
@@ -36,11 +36,14 @@ def get_benchmarks(file_path):
     # get bucket with name
     bucket = storage_client.get_bucket('hail-benchmarks')
     # get bucket data as blob
-    blob = bucket.get_blob(file_path)
+    blob = bucket.blob(file_path)
     # convert to string
     json_data = blob.download_as_string()
-    pre_data = json.load(json_data)
-    
+    pre_data = json.loads(json_data)
+
+    # with open('file-to-download-to') as file_obj:
+    #     storage_client.download_blob_to_file(file_path, file_obj)
+
     # with open(file_path) as f:
     #     pre_data = json.load(f)
 
