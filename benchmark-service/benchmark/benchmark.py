673add9d0a82d0809e8ac554d54398e2dac88408
Dania Abuhijleh <abuhijleh.d@northeastern.edu>
2020-08-06 15:41:16 -0400

    wip

--- benchmark-service/benchmark/benchmark.py
+++ benchmark-service/benchmark/benchmark.py
@@ -116,7 +116,8 @@ async def index(request: web.Request, userdata) -> Dict[str, Any]:  # pylint: di
 @check_csrf_token
 @web_authenticated_developers_only(redirect=False)
 async def lookup(request, userdata):  # pylint: disable=unused-argument
-    data = await request.post()
+    # data = await request.post()
+    data = await request.get()
     file = data['file']
     benchmarks_context = get_benchmarks(file)
     params = {'filepath': file}
