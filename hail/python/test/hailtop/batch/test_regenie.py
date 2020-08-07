daaa7d158abb0c3e704254b26237b5611bfb57b3
akotlar <akotlar@bu.edu>
2020-08-07 14:23:25 -0400

    nosplit

--- hail/python/test/hailtop/batch/test_regenie.py
+++ hail/python/test/hailtop/batch/test_regenie.py
@@ -1,7 +1,7 @@
 import unittest
 import hailtop.batch.genetics.regenie as br
 import os
-from shutil import which
+from shutil import which, rmtree
 
 
 rdir = "hailtop/batch/genetics/regenie"
@@ -19,56 +19,65 @@ def assert_same_file(file1, file2):
 class LocalTests(unittest.TestCase):
     def setUp(self):
         self.cwd = os.getcwd()
+        self.outdir = "batch"
+        self.step2_out_prefix = 'test_bin_out_firth'
 
     @unittest.skipIf(not which("docker"), "docker command is missing")
     def test_regenie(self):
         os.chdir(rdir)
-        out_prefix = "batch"
-        args = br.parse_input_args(["--demo", "--out", out_prefix])
+
+        args = br.parse_input_args(["--demo", "--outdir", self.outdir])
         br.run(args)
 
-        out_log = f"{out_prefix}.log"
-        out1 = f"{out_prefix}.test_bin_out_firth_Y1.regenie"
-        out2 = f"{out_prefix}.test_bin_out_firth_Y2.regenie"
+        out_log = f"{self.outdir}/{self.step2_out_prefix}.log"
+        out1 = f"{self.outdir}/{self.step2_out_prefix}.Y1.regenie"
+        out2 = f"{self.outdir}/{self.step2_out_prefix}.Y2.regenie"
         expected = "regenie/example/example.test_bin_out_firth_Y1.regenie"
 
         assert len(read(out_log)) > 0
         assert_same_file(out1, expected)
         assert len(read(out2)) > 0
 
-        os.remove(out_log)
-        os.remove(out1)
-        os.remove(out2)
+        rmtree(self.outdir)
 
         os.chdir(self.cwd)
 
     @unittest.skipIf(not which("docker"), "docker command is missing")
     def test_regenie_1pheno(self):
         os.chdir(rdir)
-        out_prefix = "batch"
         args = br.parse_input_args(["--local", "--step1", "example/step1.txt", "--step2",
-                                    "example/step2-phenoCol.txt", "--out", out_prefix])
+                                    "example/step2-phenoCol.txt", "--outdir", self.outdir])
         br.run(args)
 
-        out_log = f"{out_prefix}.log"
-        out1 = f"{out_prefix}.test_bin_out_firth_Y1.regenie"
-        out2 = f"{out_prefix}.test_bin_out_firth_Y2.regenie"
+        out1 = f"{self.outdir}/{self.step2_out_prefix}.Y1.regenie"
+        out2 = f"{self.outdir}/{self.step2_out_prefix}.Y2.regenie"
         expected = "regenie/example/example.test_bin_out_firth_Y1.regenie"
 
         assert_same_file(out1, expected)
         assert not os.path.isfile(out2)
 
-        os.remove(out_log)
-        os.remove(out1)
+        rmtree(self.outdir)
 
         args = br.parse_input_args(["--local", "--step1", "example/step1.txt", "--step2",
-                                    "example/step2-phenoColList.txt", "--out", out_prefix])
+                                    "example/step2-phenoColList.txt", "--outdir", self.outdir])
         br.run(args)
 
         assert len(read(out2)) > 0
         assert not os.path.isfile(out1)
 
-        os.remove(out_log)
-        os.remove(out2)
+        rmtree(self.outdir)
 
         os.chdir(self.cwd)
+
+    @unittest.skipIf(not which("docker"), "docker command is missing")
+    def test_regenie_nosplit(self):
+        os.chdir(rdir)
+        args = br.parse_input_args(["--local", "--step1", "example/step1.txt", "--step2",
+                                    "example/step2-nosplit.txt", "--outdir", self.outdir])
+        br.run(args)
+
+        out1 = f"{self.outdir}/{self.step2_out_prefix}.regenie"
+
+        assert len(read(out1)) > 0
+
+        rmtree(self.outdir)
