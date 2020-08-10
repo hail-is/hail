f659abac04064812c9df62241be67e8e081920ca
Jackie Goldstein <jigold@broadinstitute.org>
2020-08-10 12:02:54 -0400

    add service.id and sku.id

--- monitoring/sql/initial.sql
+++ monitoring/sql/initial.sql
@@ -7,10 +7,12 @@ INSERT INTO `monitoring_billing_mark` (mark) VALUES (NULL);
 CREATE TABLE IF NOT EXISTS `monitoring_billing_data` (
   `year` INT NOT NULL,
   `month` INT NOT NULL,
+  `service_id` VARCHAR(100) NOT NULL,
   `service_description` VARCHAR(100) NOT NULL,
+  `sku_id` VARCHAR(100) NOT NULL,
   `sku_description` VARCHAR(100) NOT NULL,
   `source` VARCHAR(40),
   `cost` DOUBLE NOT NULL,
-  UNIQUE(`year`, `month`, `service_description`, `sku_description`, `source`)
+  UNIQUE(`year`, `month`, `service_id`, `sku_id`, `source`)
 ) ENGINE = InnoDB;
 CREATE INDEX monitoring_billing_data_time_period ON `monitoring_billing_data` (`year`, `month`);
