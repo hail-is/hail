29d56496ff31217f7a5db00ad069ac418916cb45
Dania-Abuhijleh <abuhijleh.d@northeastern.edu>
2020-08-11 15:43:54 -0400

    [Benchmark] get JSON file from google bucket (#9214)
    * add table templating
    
    * add json file
    
    * fix
    
    * fix
    
    * remove readiness probe
    
    * change parent html template
    
    * change parent html template
    
    * fix template
    
    * fix template
    
    * change parent html
    
    * change parent html
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * remove unused import
    
    * move function to utils
    
    * fix parameters
    
    * fix parameters
    
    * add else
    
    * add else
    
    * remove else
    
    * remove else
    
    * fix 400
    
    * fix 400
    
    * remove comments

--- benchmark-service/benchmark/benchmark.py
+++ benchmark-service/benchmark/benchmark.py
@@ -1,13 +1,15 @@
-from typing import Any, Dict
-import aiohttp_jinja2
 from aiohttp import web
 import logging
 from gear import setup_aiohttp_session, web_authenticated_developers_only
 from hailtop.config import get_deploy_config
 from hailtop.tls import get_in_cluster_server_ssl_context
 from hailtop.hail_logging import AccessLogger, configure_logging
-from web_common import setup_aiohttp_jinja2, setup_common_static_routes
-
+from web_common import setup_aiohttp_jinja2, setup_common_static_routes, render_template
+from benchmark.utils import ReadGoogleStorage, get_geometric_mean, parse_file_path, enumerate_list_index
+import json
+import re
+import plotly
+import plotly.express as px
 
 configure_logging()
 router = web.RouteTableDef()
@@ -15,34 +17,87 @@ logging.basicConfig(level=logging.DEBUG)
 deploy_config = get_deploy_config()
 log = logging.getLogger('benchmark')
 
+BENCHMARK_FILE_REGEX = re.compile(r'gs://((?P<bucket>[^/]+)/)((?P<user>[^/]+)/)((?P<version>[^-]+)-)((?P<sha>[^-]+))(-(?P<tag>[^\.]+))?\.json')
+
+
+def get_benchmarks(file_path):
+    read_gs = ReadGoogleStorage()
+    try:
+        json_data = read_gs.get_data_as_string(file_path)
+        pre_data = json.loads(json_data)
+    except Exception:
+        message = f'could not find file, {file_path}'
+        log.info('could not get blob: ' + message, exc_info=True)
+        raise web.HTTPBadRequest(text=message)
+
+    data = []
+    prod_of_means = 1
+    for d in pre_data['benchmarks']:
+        stats = dict()
+        stats['name'] = d['name']
+        stats['failed'] = d['failed']
+        if not d['failed']:
+            prod_of_means *= d['mean']
+            stats['f-stat'] = round(d['f-stat'], 6)
+            stats['mean'] = round(d['mean'], 6)
+            stats['median'] = round(d['median'], 6)
+            stats['p-value'] = round(d['p-value'], 6)
+            stats['stdev'] = round(d['stdev'], 6)
+            stats['times'] = d['times']
+            stats['trials'] = d['trials']
+        data.append(stats)
+    geometric_mean = get_geometric_mean(prod_of_means, len(pre_data['benchmarks']))
+
+    file_info = parse_file_path(BENCHMARK_FILE_REGEX, file_path)
+    sha = file_info['sha']
+    benchmarks = dict()
+    benchmarks['sha'] = sha
+    benchmarks['geometric_mean'] = geometric_mean
+    benchmarks['data'] = sorted(data, key=lambda i: i['name'])
+    return benchmarks
+
 
 @router.get('/healthcheck')
 async def healthcheck(request: web.Request) -> web.Response:  # pylint: disable=unused-argument
     return web.Response()
 
 
-@router.get('/{username}')
-async def greet_user(request: web.Request) -> web.Response:
+@router.get('/name/{name}')
+@web_authenticated_developers_only(redirect=False)
+async def show_name(request: web.Request, userdata) -> web.Response:  # pylint: disable=unused-argument
+    file_path = request.query.get('file')
+    benchmarks = get_benchmarks(file_path)
+    name_data = next((item for item in benchmarks['data'] if item['name'] == str(request.match_info['name'])),
+                     None)
+
+    try:
+        fig = px.scatter(x=enumerate_list_index(name_data['trials']), y=name_data['times'])
+        plot = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    except Exception:
+        message = 'could not find name'
+        log.info('name is of type NoneType: ' + message, exc_info=True)
+        raise web.HTTPBadRequest(text=message)
 
     context = {
-        'username': request.match_info.get('username', ''),
-        'current_date': 'July 10, 2020'
+        'name': request.match_info.get('name', ''),
+        'plot': plot
     }
-    response = aiohttp_jinja2.render_template('user.html', request,
-                                              context=context)
-    return response
+
+    return await render_template('benchmark', request, userdata, 'name.html', context)
 
 
 @router.get('/')
 @router.get('')
 @web_authenticated_developers_only(redirect=False)
-async def index(request: web.Request, userdata) -> Dict[str, Any]:  # pylint: disable=unused-argument
-    context = {
-        'current_date': 'July 10, 2020'
-    }
-    response = aiohttp_jinja2.render_template('index.html', request,
-                                              context=context)
-    return response
+async def index(request, userdata):  # pylint: disable=unused-argument
+    file = request.query.get('file')
+    if file is None:
+        benchmarks_context = None
+    else:
+        benchmarks_context = get_benchmarks(file)
+    context = {'file': file,
+               'benchmarks': benchmarks_context}
+    return await render_template('benchmark', request, userdata, 'index.html', context)
 
 
 def init_app() -> web.Application:
