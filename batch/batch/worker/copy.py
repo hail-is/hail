cc1bc45a6d02e67d4c6bd6bfeb7602ece18c6d46
Jackie Goldstein <jigold@broadinstitute.org>
2020-08-13 13:44:45 -0400

    [batch] revert some of file cache changes

--- batch/batch/worker/copy.py
+++ batch/batch/worker/copy.py
@@ -1,11 +1,8 @@
-import os
 import re
 import argparse
 from shlex import quote as shq
 
 from hailtop.utils import sync_check_shell
-
-from .flock import Flock
 
 
 GCS_PATH_REGEX = re.compile('gs://(?P<bucket>[^/]+)/(?P<path>.+)')
@@ -31,7 +28,7 @@ def check_call(script):
     return sync_check_shell(script, echo=True)
 
 
-def copy(src, dst, user, io_host_path, cache_path, requester_pays_project):
+def copy(src, dst, requester_pays_project):
     if contains_wildcard(src):
         raise NotImplementedError(f'glob wildcards are not allowed in file paths, got source {src}')
 
@@ -49,42 +46,7 @@ def copy(src, dst, user, io_host_path, cache_path, requester_pays_project):
     else:
         requester_pays_project = ''
 
-    cache_src = f'{cache_path}/{user}{src[4:]}'
-    escaped_src = re.escape(os.path.basename(src))
-    dst = f'/host{io_host_path}{dst[3:]}'
-
-    match = GCS_PATH_REGEX.fullmatch(src)
-    if not match:
-        raise NotImplementedError(f'source is not a valid GCS path, got source {src}')
-    src_path = match.groupdict()['path']
-    src_tail_dir = src_path.rstrip('/').split('/')[-1]
-    rsync_dst = f'{dst.rstrip("/")}/{src_tail_dir}/'
-
-    rsync_file_cmd = f'''
-gsutil {requester_pays_project} -m -q rsync -x '(?!^{escaped_src}$)' {shq(os.path.dirname(src))} {shq(os.path.dirname(dst))}
-'''
-    rsync_dir_cmd = f'''
-mkdir -p {shq(rsync_dst)} && gsutil {requester_pays_project} -m -q rsync -r -d {shq(src)} {shq(rsync_dst)} || \
-rm -rf {shq(rsync_dst)} && gsutil {requester_pays_project} -m -q cp -r {shq(src)} {shq(dst)}
-'''
-
-    with Flock(cache_src):
-        os.makedirs(os.path.dirname(dst), exist_ok=True)
-        os.makedirs(os.path.dirname(cache_src), exist_ok=True)
-        check_call(f'cp -p -R --reflink {shq(cache_src)} {shq(dst)} || true')
-        if src.endswith('/'):
-            check_call(rsync_dir_cmd)
-        else:
-            check_call(f'''
-gsutil {requester_pays_project} -q stat {shq(src)}
-if [ $? = 0 ]; then
-  {rsync_file_cmd}
-else
-  {rsync_dir_cmd}
-fi
-''')
-        check_call(f'rm -Rf {shq(cache_src)}')
-        check_call(f'cp -p -R --reflink {shq(dst)} {shq(cache_src)}')
+    check_call(f'gsutil {requester_pays_project} -m cp -R {shq(src)} {shq(dst)}')
 
 
 if __name__ == '__main__':
@@ -97,4 +59,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     for src, dest in args.files:
-        copy(src, dest, args.user, args.io_host_path, args.cache_path, args.requester_pays_project)
+        copy(src, dest, args.requester_pays_project)
