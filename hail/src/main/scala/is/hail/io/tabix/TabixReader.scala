da03a4b7a04d7a141d993f574203cd02ee25d862
Christopher Vittal <cvittal@broadinstitute.org>
2020-08-20 16:28:56 -0400

    [query] Fix corner case assertion failure in tabix read (#9304)
    * [query] Fix corner case assertion failure in tabix read
    
    The tabix line iterator works by building a list of virtual offsets from
    the index that correspond to a requested interval. We recently
    discovered an issue that would lead to a runtime exception if the
    following conditions held:
    
    * An offset pair ended exactly on a block boundary.
    * The block boundary was exactly the start of a line.
    
    In a blocked file with virtual offsets, there are two ways to point to
    the start of every block, (previous block start offset, previous block size) or
    (current block start offset, 0).
    
    Because of the way curOff is calculated in TabixLineIterator, this would
    lead to a situation where curOff was the (previous block start offset, previous block size)
    value, causing the jump to next chunk comparision `!TbiOrd.less64(curOff, offsets(i)._2)`
    to fail when it should succeed, causing an extra line to be read, which
    then makes the assertion check in the next iteration.
    
    The key invariant of TabixLineIterator must be to keep bufferCursor
    inside the block currently being read. We therefore make a check to see
    if we need to refresh the buffer in any circumstance where we read
    a line. Coupled with a reflow of control in readLine to increase
    readability and fix a bug where lines greater than 64k would not be read
    properly.
    
    * Add long line gvcf import test
    
    I used something a little more interesting than just a repeated
    character. Unzip the file if you want to see :P
    
    * Remove assertion in TabixLineIterator.next
    
    This assertion is a sanity check to make sure that if the reader reads
    to the end of a line, it does so exactly. We don't position the reader
    exactly, so the assertion is invalid in our case. We also don't care, if
    the offset is a little past the end of a line (as it will basically only
    happen for empty blocks). We're always going to either stop reading or
    seek at this point.

--- hail/src/main/scala/is/hail/io/tabix/TabixReader.scala
+++ hail/src/main/scala/is/hail/io/tabix/TabixReader.scala
@@ -321,12 +321,27 @@ final class TabixLineIterator(
   private var isEof = false
   private var is = new BGzipInputStream(fsBc.value.openNoCompression(filePath))
 
-  private var buffer = new Array[Byte](1 << 16)  // gvcf block is 64k; this can decode an entire block with one call to read()
+  // The line iterator buffer and associated state, we use this to avoid making
+  // endless calls to read() (the no arg version returning int) on the input
+  // stream, we start with 64k to make to make sure that we can always read
+  // a block, and then grow it as neccessary if a line is too large.
+  //
+  // The key invariant here is that bufferCursor should, except possibly in
+  // readLine itself, be less than bufferLen. Should bufferCursor be greater
+  // than or equal to bufferLen, we call refreshBuffer, which also saves the
+  // current input stream virtual offset. If we uphold this invariant, then the
+  // bufferCursor to bufferLen range within the buffer will always contain data
+  // from the current block we are reading, meaning getVirtualOffset will
+  // always return the same value as if we were calling is.read() to read the
+  // lines and is.getVirtualOffset to update the current file pointer for every
+  // call to next
+  private var buffer = new Array[Byte](1 << 16)
   private var bufferCursor: Int = 0
   private var bufferLen: Int = 0
   private var bufferEOF: Boolean = false
-  private var virtualFileOffsetAtLastRead = 0L
   private var bufferPositionAtLastRead = 0L
+
+  private var virtualFileOffsetAtLastRead = 0L
 
   private def getVirtualOffset: Long = {
     virtualFileOffsetAtLastRead + (bufferCursor - bufferPositionAtLastRead)
@@ -348,6 +363,7 @@ final class TabixLineIterator(
       bufferEOF = true
     else
       bufferLen = start + bytesRead
+    bufferCursor = start
   }
 
   def decodeString(start: Int, end: Int): String = {
@@ -366,41 +382,46 @@ final class TabixLineIterator(
     var start = bufferCursor
 
     while (true) {
+      var str: String = null;
       while (bufferCursor < bufferLen && buffer(bufferCursor) != '\n') {
         bufferCursor += 1
       }
 
-      if (bufferCursor >= bufferLen) {
-      // no newline before end of buffer
+      if (bufferCursor == bufferLen) { // no newline before end of buffer
         if (bufferEOF) {
+          // `is` indicates end of file
           isEof = true
-          val str = decodeString(start, bufferCursor)
-          bufferCursor += 1
-          return str
+          str = decodeString(start, bufferCursor)
+        } else if (bufferLen == buffer.length) {
+          // line overflows buffer, need to increase buffer size
+          val tmp = new Array[Byte](buffer.length * 2)
+          System.arraycopy(buffer, 0, tmp, 0, buffer.length)
+          buffer = tmp
+          refreshBuffer(bufferLen)
+        } else if (start > 0) {
+          // line does not overflow buffer, but spans buffer.
+          // Copy line left to the beginning of buffer and continue
+          val nToCopy = bufferLen - start
+          System.arraycopy(buffer, start, buffer, 0, nToCopy)
+          start = 0
+          refreshBuffer(nToCopy)
         } else {
-
-          if (bufferCursor == 0) {
-            // line overflows buffer, need to increase buffer size
-
-            assert(bufferLen == buffer.length)
-            val tmp = new Array[Byte](buffer.length * 2)
-            System.arraycopy(buffer, 0, tmp, 0, buffer.length)
-            buffer = tmp
-
-            refreshBuffer(bufferLen)
-          } else {
-            // line does not overflow buffer, but spans buffer. Copy line left to the beginning of buffer and continue
-
-            val nToCopy = bufferLen - start
-            System.arraycopy(buffer, start, buffer, 0, nToCopy)
-            start = 0
-            bufferCursor = nToCopy
-            refreshBuffer(bufferCursor)
-          }
+          // line does not span buffer, but does not fill buffer either, read more
+          refreshBuffer(bufferCursor)
         }
-      } else {
-        val str = decodeString(start, bufferCursor)
+      } else { // found a newline
+        str = decodeString(start, bufferCursor)
+      }
+
+      if (str != null) {
         bufferCursor += 1
+        // we refresh here to make sure that the cursor is never pointing to
+        // one past the end of a block, making it so getVirtualOffset will
+        // never return the pointer pointing past the end of a block (the one
+        // that overlaps with the start of the next block).
+        if (bufferCursor >= bufferLen) {
+          refreshBuffer(0)
+        }
         return str
       }
     }
@@ -412,11 +433,6 @@ final class TabixLineIterator(
     while (s == null && !isEof) {
       val curOff = getVirtualOffset
       if (i < 0 || curOff == 0 || !TbiOrd.less64(curOff, offsets(i)._2)) { // jump to next chunk
-
-        if (i >= 0 && curOff != offsets(i)._2)
-          throw new RuntimeException(s"error reading tabix-indexed file $filePath: " +
-            s"i=$i, curOff=$curOff, expected=${ offsets(i)._2 }")
-
         if (i == offsets.length - 1) {
           isEof = true
           return s
