fef443e03a94748bc576ae293b6c7d7e4246756d
Cotton Seed <cotton@alum.mit.edu>
2020-08-24 14:18:04 -0400

    [many] put shared make config in config.mk file

---
+++ config.mk
@@ -0,0 +1,6 @@
+DOCKER_ROOT_IMAGE := ubuntu:18.04
+INTERNAL_IP := 10.128.0.57
+IP := 35.188.91.25
+KUBERNETES_SERVER_URL := https://104.198.230.143
+PROJECT := $(shell gcloud config get-value project)
+ZONE := us-central1-a
