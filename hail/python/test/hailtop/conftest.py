1af87674596a02283947a5490213317b5374dff3
Daniel King <daniel.zidan.king@gmail.com>
2020-09-01 17:45:08 -0400

    [batch] parallelize batch tests (#9381)
    * [batch] parallelize batch tests
    
    * fix build.yaml
    
    * fixes
    
    * fixes
    
    * use conftest everywhere
    
    * fix indexing
    
    * fix

--- hail/python/test/hailtop/conftest.py
+++ hail/python/test/hailtop/conftest.py
@@ -1,16 +1,7 @@
 import hashlib
 import os
-import logging
+
 import pytest
-
-log = logging.getLogger(__name__)
-
-
-@pytest.fixture(autouse=True)
-def log_before_after():
-    log.info('starting test')
-    yield
-    log.info('ending test')
 
 
 def pytest_collection_modifyitems(config, items):
