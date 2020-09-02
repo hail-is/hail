41f7e03ee4a7b24116a0e97a29a98007c1074358
Dania Abuhijleh <abuhijleh.d@northeastern.edu>
2020-09-02 09:18:18 -0400

    remove duplicated func

--- benchmark-service/benchmark/utils.py
+++ benchmark-service/benchmark/utils.py
@@ -1,6 +1,5 @@
 from google.cloud import storage
 import re
-import functools
 
 BENCHMARK_BUCKETS = ['hail-benchmarks', 'hail-benchmarks-2']
 
@@ -62,10 +61,4 @@ class ReadGoogleStorage:
         bucket = self.storage_client.get_bucket(bucket_name)
         for blob in bucket.list_blobs():
             list_of_files.append('gs://' + bucket_name + '/' + blob.name)
-        return list_of_files
-
-    def list_files(self):
-        list_of_files = []
-        list_of_files.extend(self.list_files_in_bucket('hail-benchmarks'))
-        list_of_files.extend(self.list_files_in_bucket('hail-benchmarks-2'))
         return list_of_files
