991229559504c8b956ef270b85b253c7551812ba
Daniel King <dking@broadinstitute.org>
2020-09-02 19:00:22 -0400

    make default private

--- batch/batch/worker/worker.py
+++ batch/batch/worker/worker.py
@@ -303,7 +303,7 @@ class Container:
 
         network = self.spec.get('network')
         if network is None:
-            network = 'public'
+            network = 'private'
         host_config['NetworkMode'] = network  # not documented, I used strace to inspect the packets
 
         config['HostConfig'] = host_config
