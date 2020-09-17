0b99533af980a56008ecf327c9b34844775872c0
John Compitello <j301600@gmail.com>
2020-09-17 17:44:15 -0400

    Flake fix

--- hail/python/hail/methods/statgen.py
+++ hail/python/hail/methods/statgen.py
@@ -419,7 +419,6 @@ def _linear_regression_rows_nd(y, x, covariates, block_size=16, pass_through=())
     # NEW STUFF
     entries_field_name = 'ent'
     sample_field_name = "by_sample"
-    X_field_name = entries_field_name + "_nd"
 
     def all_defined(struct_root, field_names):
         defined_array = hl.array([hl.is_defined(struct_root[field_name]) for field_name in field_names])
