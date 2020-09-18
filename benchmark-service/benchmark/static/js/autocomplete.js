ead1343d7ca0da11123fccce31c05f0055160d84
Dania Abuhijleh <abuhijleh.d@northeastern.edu>
2020-09-18 11:52:26 -0400

    clean up

--- benchmark-service/benchmark/static/js/autocomplete.js
+++ benchmark-service/benchmark/static/js/autocomplete.js
@@ -1,39 +1,37 @@
-function auto_complete(place_holder, str_selector) {
+function auto_complete(place_holder, input_selector) {
      return new autoComplete({
-                   data: {                              // Data src [Array, Function, Async] | (REQUIRED)
+                   data: {
                      src: files,
                      cache: true
                    },
-                   sort: (a, b) => {                    // Sort rendered results ascendingly | (Optional)
+                   sort: (a, b) => {
                        if (a.match < b.match) return -1;
                        if (a.match > b.match) return 1;
                        return 0;
                    },
-                   placeHolder: place_holder,     // Place Holder text                 | (Optional)
-                   selector: str_selector,           // Input field selector              | (Optional)
-                   threshold: 2,                        // Min. Chars length to start Engine | (Optional)
-                   debounce: 300,                       // Post duration for engine to start | (Optional)
-                   searchEngine: "strict",              // Search Engine type/mode           | (Optional)
-                   resultsList: {                       // Rendered results list object      | (Optional)
+                   placeHolder: place_holder,
+                   selector: input_selector,
+                   threshold: 2,
+                   debounce: 300,
+                   searchEngine: "strict",
+                   resultsList: {
                        render: true,
-                       /* if set to false, add an eventListener to the selector for event type
-                          "autoComplete" to handle the result */
                        container: source => {
                            source.setAttribute("class", "file_list");
                        },
-                       destination: document.querySelector(str_selector),
+                       destination: document.querySelector(input_selector),
                        position: "afterend",
                        element: "ul"
                    },
-                   maxResults: 15,                         // Max. number of rendered results | (Optional)
-                   highlight: true,                       // Highlight matching results      | (Optional)
-                   resultItem: {                          // Rendered result item            | (Optional)
+                   maxResults: 15,
+                   highlight: true,
+                   resultItem: {
                        content: (data, source) => {
                            source.innerHTML = data.match;
                        },
                        element: "li"
                    },
-                   noResults: () => {                     // Action script on noResults      | (Optional)
+                   noResults: () => {
                        const result = document.createElement("li");
                        result.setAttribute("class", "no_result");
                        result.setAttribute("tabindex", "1");
@@ -42,7 +40,7 @@ function auto_complete(place_holder, str_selector) {
                    },
                    onSelection: feedback => {
                    feedback.event.preventDefault();
-                   document.querySelector(str_selector).value = feedback.selection.value;
+                   document.querySelector(input_selector).value = feedback.selection.value;
              }
            });
 }
