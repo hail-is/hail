bc1ff91e111241be6ca7c2ce6d5fd1e4d53b095a
Daniel King <daniel.zidan.king@gmail.com>
2020-09-25 15:30:34 -0400

    [hailctl] update dependencies on modify (#9502)
    * [hailctl] update dependencies on modify
    
    CHANGELOG: Fix hailctl dataproc modify to install dependencies of the wheel file.
    
    * fixed
    
    * fix
    
    * more fixes
    
    * tests
    
    * FJSADLFHDSAFDSAFDSA

--- hail/python/hailtop/hailctl/dataproc/modify.py
+++ hail/python/hailtop/hailctl/dataproc/modify.py
@@ -106,7 +106,9 @@ def main(args, pass_through_args):
                 '--',
                 f'sudo gsutil cp {wheel} /tmp/ && '
                 'sudo /opt/conda/default/bin/pip uninstall -y hail && '
-                f'sudo /opt/conda/default/bin/pip install --no-dependencies /tmp/{wheelfile}'
+                f'sudo /opt/conda/default/bin/pip install --no-dependencies /tmp/{wheelfile} && '
+                f"unzip /tmp/{wheelfile} && "
+                "grep 'Requires-Dist: ' hail*dist-info/METADATA | sed 's/Requires-Dist: //' | sed 's/ (//' | sed 's/)//' | grep -v 'pyspark' | xargs /opt/conda/default/bin/pip install"
             ])
         else:
             cmds.extend([
@@ -124,7 +126,9 @@ def main(args, pass_through_args):
                     f'--zone={zone}',
                     '--',
                     'sudo /opt/conda/default/bin/pip uninstall -y hail && '
-                    f'sudo /opt/conda/default/bin/pip install --no-dependencies /tmp/{wheelfile}'
+                    f'sudo /opt/conda/default/bin/pip install --no-dependencies /tmp/{wheelfile} && '
+                    f"unzip /tmp/{wheelfile} && "
+                    "grep 'Requires-Dist: ' hail*dist-info/METADATA | sed 's/Requires-Dist: //' | sed 's/ (//' | sed 's/)//' | grep -v 'pyspark' | xargs /opt/conda/default/bin/pip install"
                 ]
             ])
 
