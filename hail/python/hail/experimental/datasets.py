82e6bba78354d08d350488c57cd616f9dacdff5c
Patrick Cummings <42842025+pwc2@users.noreply.github.com>
2020-09-24 23:40:22 -0400

    Reverted datasets.py, implemented cleaner changes
    To use datasets.json file that includes separate version and reference_genome fields

--- hail/python/hail/experimental/datasets.py
+++ hail/python/hail/experimental/datasets.py
@@ -1,8 +1,7 @@
-import json
 import os
-import re
-import hail as hl
 import pkg_resources
+import json
+import hail as hl
 
 
 def load_dataset(name,
@@ -41,44 +40,36 @@ def load_dataset(name,
         raise ValueError(f'Specify valid region parameter, received: region={region}. '
                          f'Valid regions are {valid_regions}.')
 
-    config_path = pkg_resources.resource_filename(__name__, 'annotation_db.json')
+    config_path = pkg_resources.resource_filename(__name__, 'datasets.json')
     assert os.path.exists(config_path), f'{config_path} does not exist'
     with open(config_path) as f:
         datasets = json.load(f)
 
-    names = set(dataset for dataset in datasets)
+    names = set([dataset for dataset in datasets])
     if name not in names:
         raise ValueError('{} is not a dataset available in the repository.'.format(repr(name)))
 
-    version_ref_genomes = set(x['version'] for x in datasets[name]['versions'])
-    versions = set(x.replace("GRCh37", "").replace("GRCh38", "").rstrip("-") for x in version_ref_genomes)
-    versions = [None if x == '' else x for x in versions]
+    versions = set(dataset['version'] for dataset in datasets[name]['versions'])
     if version not in versions:
         raise ValueError("""Version {0} not available for dataset {1}.
                             Available versions: {{{2}}}.""".format(repr(version),
                                                                    repr(name),
                                                                    repr('","'.join(versions))))
 
-    get_ref_genomes = [re.findall("GRCh\\d{2}$", x) for x in version_ref_genomes]
-    reference_genomes = set(x[0] for x in get_ref_genomes if x)
-    if not reference_genomes:
-        reference_genomes = [None]
+    reference_genomes = set(dataset['reference_genome'] for dataset in datasets[name]['versions'])
     if reference_genome not in reference_genomes:
         raise ValueError("""Reference genome build {0} not available for dataset {1}.
                             Available reference genome builds: {{'{2}'}}.""".format(repr(reference_genome),
                                                                                     repr(name),
                                                                                     '\',\''.join(reference_genomes)))
 
-    if version and not reference_genome:
-        get_version = version
-    elif reference_genome and not version:
-        get_version = reference_genome
-    else:
-        get_version = "-".join([version, reference_genome])
+    path = [dataset['url'][region]
+            for dataset in datasets[name]['versions']
+            if all([dataset['version'] == version,
+                    dataset['reference_genome'] == reference_genome])]
+    assert len(path) == 1
+    path = path[0]
 
-    path = [dataset_version['url'][region]
-            for dataset_version in datasets[name]['versions']
-            if dataset_version['version'] == get_version][0]
     if path.endswith('.ht'):
         dataset = hl.read_table(path)
     else:
