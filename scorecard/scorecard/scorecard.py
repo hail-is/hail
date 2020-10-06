36dc4f3883be1e6d31c59a16288c7b73ff21c3a5
jigold <jigold@users.noreply.github.com>
2020-10-05 21:24:26 -0400

    [scorecard] Added Asana integration (#9488)
    * [scorecard] Added Asana integration
    
    - Use the scorecard tag or design-docs tag to have Asana tasks show up in scorecard
    - Easy to add different tags to track
    - Currently using my personal access token. Not sure if that's ideal, but seemed fine for now.
    
    * address comments
    
    * rm typeVar?
    
    * delint

--- scorecard/scorecard/scorecard.py
+++ scorecard/scorecard/scorecard.py
@@ -3,16 +3,22 @@ import datetime
 import os
 import asyncio
 import aiohttp
-from aiohttp import web
+from aiohttp import web, ClientSession
 import gidgethub.aiohttp
 import random
 import humanize
 import logging
+from collections import defaultdict
+
 from hailtop.config import get_deploy_config
 from hailtop.tls import get_in_cluster_server_ssl_context
 from hailtop.hail_logging import AccessLogger
 from gear import setup_aiohttp_session, web_maybe_authenticated_user
 from web_common import setup_aiohttp_jinja2, setup_common_static_routes, render_template
+
+from types import TracebackType
+from typing import Any, Optional, Type
+
 
 log = logging.getLogger('scorecard')
 
@@ -28,10 +34,107 @@ repos = {
     'hail': 'hail-is/hail',
 }
 
+asana_tags = {
+    'design-docs': '1194895524341386',
+    'scorecard': '1194895524341389'
+}
+
+asana_to_github_user = {
+    'Jackie Goldstein': 'jigold',
+    'Cotton Seed': 'cseed',
+    'Daniel King': 'danking',
+    'John Compitello': 'johnc1231',
+    'Tim Poterba': 'tpoterba',
+    'Arcturus Wang': 'catoverdrive',
+    'Dania Abuhijleh': 'Dania-Abuhijleh',
+    'Patrick Schultz': 'patrick-schultz',
+    'Chris Vittal': 'chrisvittal',
+    'Patrick Cummings': 'pwc2'
+}
+
 routes = web.RouteTableDef()
 
 data = None
 timestamp = None
+
+
+class AsanaClient:
+    _session: Optional[ClientSession]
+
+    def __init__(self, *, token: Optional[str] = None):
+        self._base_url = 'https://app.asana.com/api/1.0'
+
+        if token is None:
+            asana_token_file = os.environ.get('ASANA_TOKEN_PATH', '/asana/token.txt')
+            with open(asana_token_file, 'r') as f:
+                token = f.read().strip()
+
+        self._session = ClientSession(headers={'Authorization': f'Bearer {token}'})
+
+    async def get(self, path: str, **kwargs) -> Any:
+        async with await self._session.get(
+                f'{self._base_url}{path}', **kwargs) as resp:
+            return await resp.json()
+
+    async def post(self, path: str, **kwargs) -> Any:
+        async with await self._session.post(
+                f'{self._base_url}{path}', **kwargs) as resp:
+            return await resp.json()
+
+    async def delete(self, path: str, **kwargs) -> None:
+        async with await self._session.delete(
+                f'{self._base_url}{path}', **kwargs) as resp:
+            return await resp.json()
+
+    async def close(self) -> None:
+        if self._session is not None:
+            await self._session.close()
+            self._session = None
+
+    async def __aenter__(self: 'AsanaClient') -> 'AsanaClient':
+        return self
+
+    async def __aexit__(self,
+                        exc_type: Optional[Type[BaseException]],
+                        exc_val: Optional[BaseException],
+                        exc_tb: Optional[TracebackType]) -> None:
+        await self.close()
+
+
+async def get_tag_assignees(client, tag_gid):
+    assignees = defaultdict(lambda: [])
+
+    async def get_task_data(task_gid):
+        try:
+            data = await client.get(f'/tasks/{task_gid}')
+        except Exception:
+            return None
+        return data['data']
+
+    result = await client.get(f'/tags/{tag_gid}/tasks')
+    task_data = await asyncio.gather(*[get_task_data(task['gid'])
+                                       for task in result['data']])
+
+    for task in task_data:
+        if task is None:
+            continue
+
+        gid = task['gid']
+        name = task['name']
+        assignee = task['assignee']
+        if assignee:
+            assignee_name = asana_to_github_user.get(assignee['name'])
+        else:
+            assignee_name = None
+        permalink_url = task['permalink_url']
+
+        if assignee_name and not task['completed']:
+            assignees[assignee_name].append({
+                'gid': gid,
+                'url': permalink_url,
+                'name': name})
+
+    return assignees
 
 
 @routes.get('/healthcheck')
@@ -76,7 +179,8 @@ def get_users():
     user_data = collections.defaultdict(
         lambda: {'CHANGES_REQUESTED': [],
                  'NEEDS_REVIEW': [],
-                 'ISSUES': []})
+                 'ISSUES': [],
+                 'ASANA': []})
 
     urgent_issues = []
 
@@ -106,7 +210,7 @@ def get_users():
             else:
                 d['ISSUES'].append(issue)
 
-    for _, repo_data in cur_data.items():
+    for _, repo_data in cur_data['github'].items():
         for pr in repo_data['prs']:
             if len(pr['assignees']) == 0:
                 unassigned.append(pr)
@@ -116,6 +220,10 @@ def get_users():
 
         for issue in repo_data['issues']:
             add_issue(issue)
+
+    for user, asana_data in cur_data['asana'].items():
+        d = user_data[user]
+        d['ASANA'] = asana_data
 
     list.sort(urgent_issues, key=lambda issue: issue['timedelta'], reverse=True)
 
@@ -135,10 +243,11 @@ def get_user(user):
         'CHANGES_REQUESTED': [],
         'NEEDS_REVIEW': [],
         'FAILING': [],
-        'ISSUES': []
+        'ISSUES': [],
+        'ASANA': []
     }
 
-    for _, repo_data in cur_data.items():
+    for _, repo_data in cur_data['github'].items():
         for pr in repo_data['prs']:
             state = pr['state']
             if state == 'CHANGES_REQUESTED':
@@ -157,8 +266,13 @@ def get_user(user):
             if user in issue['assignees']:
                 user_data['ISSUES'].append(issue)
 
+    asana_data = data['asana'].get(user)
+    if asana_data:
+        user_data['ASANA'] = asana_data
+
     updated = humanize.naturaltime(
         datetime.datetime.now() - cur_timestamp)
+
     return (user_data, updated)
 
 
@@ -218,17 +332,26 @@ def get_issue_data(repo_name, issue):
     }
 
 
-async def update_data(gh_client):
+async def get_asana_data(asana_client):
+    result = defaultdict(lambda: [])
+    for _, tag_gid in asana_tags.items():
+        tag_assignees = await get_tag_assignees(asana_client, tag_gid)
+        for assignee, data in tag_assignees.items():
+            result[assignee].extend(data)
+    return result
+
+
+async def update_data(gh_client, asana_client):
     global data, timestamp
 
     rate_limit = await gh_client.getitem("/rate_limit")
     log.info(f'rate_limit {rate_limit}')
     log.info('start updating_data')
 
-    new_data = {}
+    new_github_data = {}
 
     for repo_name in repos:
-        new_data[repo_name] = {
+        new_github_data[repo_name] = {
             'prs': [],
             'issues': []
         }
@@ -237,13 +360,16 @@ async def update_data(gh_client):
         for repo_name, fq_repo in repos.items():
             async for pr in gh_client.getiter(f'/repos/{fq_repo}/pulls?state=open'):
                 pr_data = await get_pr_data(gh_client, fq_repo, repo_name, pr)
-                new_data[repo_name]['prs'].append(pr_data)
+                new_github_data[repo_name]['prs'].append(pr_data)
 
             async for issue in gh_client.getiter(f'/repos/{fq_repo}/issues?state=open'):
                 print(issue)
                 if 'pull_request' not in issue:
                     issue_data = get_issue_data(repo_name, issue)
-                    new_data[repo_name]['issues'].append(issue_data)
+                    new_github_data[repo_name]['issues'].append(issue_data)
+
+        new_asana_data = await get_asana_data(asana_client)
+
     except Exception:  # pylint: disable=broad-except
         log.exception('update failed due to except')
         return
@@ -252,16 +378,19 @@ async def update_data(gh_client):
 
     now = datetime.datetime.now()
 
-    data = new_data
+    data = {
+        'github': new_github_data,
+        'asana': new_asana_data
+    }
     timestamp = now
 
 
-async def poll(gh_client):
+async def poll(gh_client, asana_client):
     while True:
         await asyncio.sleep(180)
         try:
             log.info('run update_data')
-            await update_data(gh_client)
+            await update_data(gh_client, asana_client)
             log.info('update_data returned')
         except Exception:  # pylint: disable=broad-except
             log.exception('update_data failed with exception')
@@ -278,13 +407,21 @@ async def on_startup(app):
     gh_client = gidgethub.aiohttp.GitHubAPI(session, 'scorecard', oauth_token=token)
     app['gh_client'] = gh_client
 
-    await update_data(gh_client)
-    asyncio.ensure_future(poll(gh_client))
+    asana_client = AsanaClient()
+    app['asana_client'] = asana_client
+
+    await update_data(gh_client, asana_client)
+    asyncio.ensure_future(poll(gh_client, asana_client))
+
+
+async def on_shutdown(app):
+    await app['asana_client'].close()
 
 
 def run():
     app = web.Application()
     app.on_startup.append(on_startup)
+    app.on_shutdown.append(on_shutdown)
 
     setup_aiohttp_jinja2(app, 'scorecard')
     setup_aiohttp_session(app)
