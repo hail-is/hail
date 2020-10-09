42ad49dcd8c32f79790d1fb5458fd171b196d6d6
Daniel King <dking@broadinstitute.org>
2020-10-09 19:16:48 -0400

    fix

--- address/address/address.py
+++ address/address/address.py
@@ -9,7 +9,7 @@ import kubernetes_asyncio as kube
 from gear import (setup_aiohttp_session, web_authenticated_developers_only,
                   rest_authenticated_users_only)
 from hailtop.config import get_deploy_config
-from hailtop.tls import get_in_cluster_server_ssl_context
+from hailtop.tls import internal_server_ssl_context
 from hailtop.hail_logging import AccessLogger
 from web_common import setup_aiohttp_jinja2, setup_common_static_routes, render_template
 import uvloop
@@ -153,4 +153,4 @@ def run():
                 host='0.0.0.0',
                 port=5000,
                 access_log_class=AccessLogger,
-                ssl_context=get_in_cluster_server_ssl_context())
+                ssl_context=internal_server_ssl_context())
