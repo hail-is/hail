2bb4a608770af7ed9f0abe0b7fb798796981b6e5
Daniel King <dking@broadinstitute.org>
2020-10-09 19:35:57 -0400

    fix

--- monitoring/test/test_monitoring.py
+++ monitoring/test/test_monitoring.py
@@ -5,7 +5,7 @@ import aiohttp
 
 from hailtop.config import get_deploy_config
 from hailtop.auth import service_auth_headers
-from hailtop.httpx import in_cluster_ssl_client_session
+from hailtop.httpx import client_session
 import hailtop.utils as utils
 
 pytestmark = pytest.mark.asyncio
@@ -18,7 +18,7 @@ async def test_billing_monitoring():
     deploy_config = get_deploy_config()
     monitoring_deploy_config_url = deploy_config.url('monitoring', '/api/v1alpha/billing')
     headers = service_auth_headers(deploy_config, 'monitoring')
-    async with in_cluster_ssl_client_session(
+    async with client_session(
             raise_for_status=True,
             timeout=aiohttp.ClientTimeout(total=60)) as session:
 
