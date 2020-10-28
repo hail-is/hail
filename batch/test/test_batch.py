a1db9ee3ca90baf1d3d6fd01a967dcc3b9e77485
Jackie Goldstein <jigold@broadinstitute.org>
2020-10-28 16:06:11 -0400

    make test stronger

--- batch/test/test_batch.py
+++ batch/test/test_batch.py
@@ -183,7 +183,7 @@ def test_attached_disk(client):
     builder = client.create_batch()
     resources = {'cpu': '0.25', 'memory': '10M', 'storage': '400Gi'}
     j = builder.create_job('ubuntu:18.04',
-                           ['/bin/sh', '-c', 'true'],
+                           ['/bin/sh', '-c', 'fallocate -l 350GiB /io/foo'],
                            resources=resources)
     builder.submit()
     status = j.wait()
