2e4ee0f2105d3417f61aa85d0ea25018095deb22
Dania Abuhijleh <abuhijleh.d@northeastern.edu>
2020-10-29 14:56:13 -0400

    wip

--- benchmark-service/benchmark/benchmark.py
+++ benchmark-service/benchmark/benchmark.py
@@ -193,10 +193,12 @@ async def show_name(request: web.Request, userdata) -> web.Response:  # pylint: 
 @router.get('')
 async def index(request):
     userdata = {}
+    global benchmark_data
     d = {
         'dates': benchmark_data['dates'],
         'geo_means': benchmark_data['geo_means']
     }
+    assert len(d['dates']) == len(d['geo_means']), d
     df = pd.DataFrame(d)
     # fig = px.scatter(df, x=df.dates, y=df.geo_means)
     fig = px.line(df, x=df.dates, y=df.geo_means)
