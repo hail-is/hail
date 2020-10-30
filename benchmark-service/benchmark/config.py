6205816bd20e7d5138dda1301e6f95b317b63710
Dania Abuhijleh <abuhijleh.d@northeastern.edu>
2020-10-30 10:16:32 -0400

    improve

--- benchmark-service/benchmark/config.py
+++ benchmark-service/benchmark/config.py
@@ -4,5 +4,6 @@ import datetime
 HAIL_BENCHMARK_BUCKET_NAME = os.environ['HAIL_BENCHMARK_BUCKET_NAME']
 START_POINT = os.environ.get('START POINT')
 if START_POINT is None:
-    now = datetime.datetime()
+    now = datetime.datetime.now()
     start_point = now - datetime.timedelta(days=1)
+    START_POINT = start_point.strftime("%Y-%m-%dT%H:%M:%SZ")
