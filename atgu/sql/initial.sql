83899bf42fd9a5070cff8000e9e8febb1bf3ee08
Cotton Seed <cotton@alum.mit.edu>
2020-11-04 03:36:25 -0500

    fix typo

--- atgu/sql/initial.sql
+++ atgu/sql/initial.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `atgu_resources` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `title` TEXT NOT NULL,
-  `description` TEXT NOT NULL
+  `description` TEXT NOT NULL,
   `contents` LONGTEXT NOT NULL,
   `tags` TEXT NOT NULL,
   `attachments` TEXT NOT NULL
