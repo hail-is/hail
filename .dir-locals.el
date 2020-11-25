b06cdb62c68132fce7c12f305bae86a292b2b40e
Daniel King <dking@broadinstitute.org>
2020-11-25 11:29:39 -0500

    include some flycheck dirlocals

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((flycheck-pylintrc . "pylintrc")))
+ (nil . ((flycheck-gcc-language-standard . "c++2a")))
+ (nil . ((flycheck-clang-language-standard . "c++2a"))))
