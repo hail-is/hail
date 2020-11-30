63ad53b2cfba7377d2f50adb0b65354e6d68e781
Dania Abuhijleh <abuhijleh.d@northeastern.edu>
2020-11-30 14:21:07 -0500

    wip

--- benchmark-service/benchmark/config.py
+++ benchmark-service/benchmark/config.py
@@ -4,7 +4,7 @@ from hailtop.utils import secret_alnum_string
 
 HAIL_BENCHMARK_BUCKET_NAME = os.environ['HAIL_BENCHMARK_BUCKET_NAME']
 
-INSTANCE_ID = os.environ.get('INSTANCE_ID', 'R8kZlZhmqGJk')
+INSTANCE_ID = os.environ.get('INSTANCE_ID')
 if INSTANCE_ID is None:
     INSTANCE_ID = secret_alnum_string(12)
 
