86ae16d0157d34758d16ad3d00b8b1c851fcae6e
Daniel King <dking@broadinstitute.org>
2020-11-30 18:23:59 -0500

    [auth] fix delete-auth-tables.sql

--- auth/sql/delete-auth-tables.sql
+++ auth/sql/delete-auth-tables.sql
@@ -6,3 +6,4 @@ DROP TABLE IF EXISTS `sessions`;
 DROP TABLE IF EXISTS `users`;
 DROP TABLE IF EXISTS `auth_migration_version`;
 DROP TABLE IF EXISTS `auth_migrations`;
+DROP TABLE IF EXISTS `roles`;
