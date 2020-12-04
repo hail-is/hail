ea39beef967e23a1d2ed4280c92bc2402cf9ec6f
Daniel King <daniel.zidan.king@gmail.com>
2020-12-04 14:58:17 -0500

    [ci] use {{ to get { (#9797)

--- ci/ci/build.py
+++ ci/ci/build.py
@@ -636,7 +636,7 @@ echo {shq(config)} | kubectl apply -f -
             if self.namespace_name == 'default':
                 script += f'''
 kubectl -n {self.namespace_name} get -o json secret global-config \
-  | jq '{apiVersion:"v1",kind:"Secret","type":"Opaque",metadata:{name:"global-config",namespace:"{self._name}"},data:(.data + {default_namespace:("{self._name}" | @base64)})}' \
+  | jq '{{apiVersion:"v1",kind:"Secret","type":"Opaque",metadata:{{name:"global-config",namespace:"{self._name}"}},data:(.data + {{default_namespace:("{self._name}" | @base64)}})}}' \
   | kubectl -n {self._name} apply -f -
 '''
 
