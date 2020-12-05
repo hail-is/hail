7becf8daaa9e4e688c93bfaaa9970520fa6b8bda
Cotton Seed <cotton@alum.mit.edu>
2020-12-05 11:55:53 -0500

    fixes

--- config.mk
+++ config.mk
@@ -2,8 +2,8 @@ PROJECT := hail-vdc-staging
 DOCKER_ROOT_IMAGE := gcr.io/$(PROJECT)/ubuntu:18.04
 DOMAIN := staging.hail.is
 INTERNAL_IP := 10.128.0.2
-IP := 34.121.206.184
-KUBERNETES_SERVER_URL := https://34.71.246.49
+IP := 35.224.188.20
+KUBERNETES_SERVER_URL := https://34.123.185.11
 REGION := us-central1
 ZONE := us-central1-a
 ifeq ($(NAMESPACE),default)
