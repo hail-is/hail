fcb960ac552b480801e23c525003604e08b72b48
Jackie Goldstein <jigold@broadinstitute.org>
2020-12-07 14:14:00 -0500

    wip

--- docker/python-dill/push.sh
+++ docker/python-dill/push.sh
@@ -2,9 +2,9 @@
 
 for version in 3.6 3.6-slim 3.7 3.7-slim 3.8 3.8-slim
 do
-#    sed "s/@PYTHON_VERSION@/$version/g" Dockerfile > Dockerfile.out
-#    docker build --tag hailgenetics/python-dill:$version - <Dockerfile.out
-#    docker push hailgenetics/python-dill:$version
+    sed "s/@PYTHON_VERSION@/$version/g" Dockerfile > Dockerfile.out
+    docker build --tag hailgenetics/python-dill:$version - <Dockerfile.out
+    docker push hailgenetics/python-dill:$version
     docker tag hailgenetics/python-dill:$version gcr.io/$PROJECT/python-dill:$version
     docker push gcr.io/$PROJECT/python-dill:$version
 done
