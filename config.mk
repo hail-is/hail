e5631d3dd0e98bcbcbb367535e0f971b670d9302
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2020-12-08 16:46:37 +1100

    Update config.mk for CPG.

--- config.mk
+++ config.mk
@@ -1,11 +1,11 @@
-PROJECT := hail-vdc-staging
+PROJECT := hail-295901
 DOCKER_ROOT_IMAGE := gcr.io/$(PROJECT)/ubuntu:18.04
-DOMAIN := staging.hail.is
-INTERNAL_IP := 10.128.0.2
-IP := 35.224.188.20
-KUBERNETES_SERVER_URL := https://34.123.185.11
-REGION := us-central1
-ZONE := us-central1-a
+DOMAIN := hail.populationgenomics.org.au
+INTERNAL_IP := 10.152.0.2
+IP := 35.201.29.236
+KUBERNETES_SERVER_URL := https://34.87.199.41
+REGION := australia-southeast1
+ZONE := australia-southeast1-b
 ifeq ($(NAMESPACE),default)
 SCOPE = deploy
 DEPLOY = true
