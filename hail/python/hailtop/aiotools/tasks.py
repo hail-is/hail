cefc109f8f539ed43acde14890c527c0676878dd
Daniel King <daniel.zidan.king@gmail.com>
2020-12-09 17:35:33 -0500

    [sync] alternative to ksync (#9759)
    * [sync] alternative to ksync
    
    I got fed up with ksync not working the way I wanted and wrote my own
    version. Assuming `repo/devbin` is on your path, then:
    
    ```
    sync.sh batch dking
    ```
    
    will keep the web_common, gear, hailtop, and batch repositories all up to date
    in k8s. It just copies all the files over from your local machine, so its not
    the fastest thing in the world. It takes maybe 2 seconds to get back to a
    working container.
    
    It also assumes there's one container per-pod.
    
    It is completely disabled in the default namespace (I remove it in
    deployment.yaml if we are in deploy mode).
    
    * wip py rewrite
    
    * fixes
    
    * fixes
    
    * fxes
    
    * fix
    
    * fix
    
    * fix permissions
    
    * hopefully fix last bugs
    
    * Update tasks.py
    
    * fix
    
    * fix the deployment

--- hail/python/hailtop/aiotools/tasks.py
+++ hail/python/hailtop/aiotools/tasks.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import asyncio
 import logging
 import weakref
@@ -7,11 +8,18 @@ log = logging.getLogger('aiotools.tasks')
 
 
 class BackgroundTaskManager:
-    def __init__(self):
+    def __init__(self, loop: Optional[asyncio.AbstractEventLoop] = None):
+        if loop is None:
+            loop = asyncio.get_event_loop()
+
         self.tasks: weakref.WeakSet = weakref.WeakSet()
+        self.loop = loop
 
     def ensure_future(self, coroutine):
         self.tasks.add(asyncio.ensure_future(coroutine))
+
+    def ensure_future_threadsafe(self, coroutine):
+        self.tasks.add(asyncio.run_coroutine_threadsafe(coroutine, self.loop))
 
     def shutdown(self):
         for task in self.tasks:
