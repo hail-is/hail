bf4603f6f80a5ef83f05a6be15a198a3719ac04a
Daniel King <dking@broadinstitute.org>
2021-01-22 16:31:14 -0500

    fix unused import

--- batch/batch/public_gcr_images.py
+++ batch/batch/public_gcr_images.py
@@ -1,5 +1,5 @@
 from typing import List
-from hailtop.batch import hail_genetics_images
+
 
 def public_gcr_images(project: str) -> List[str]:
     # the worker cannot import batch_configuration because it does not have all the environment
