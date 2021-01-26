9f9437afbf9da8f61b069960d371bd352fe5f860
Daniel King <daniel.zidan.king@gmail.com>
2021-01-25 19:11:27 -0500

    [hail] intelligently discover the spark home directory (#9916)
    If SPARK_HOME is not set, we might still be able to call find_spark_home.py and get the spark
    home directory from that. We already do this in query.py. We now do this in local backend too.

--- query/query/query.py
+++ query/query/query.py
@@ -1,4 +1,3 @@
-import subprocess
 import traceback
 import os
 import base64
@@ -9,7 +8,7 @@ import asyncio
 from aiohttp import web
 import kubernetes_asyncio as kube
 from py4j.java_gateway import JavaGateway, GatewayParameters, launch_gateway
-from hailtop.utils import blocking_to_async, retry_transient_errors
+from hailtop.utils import blocking_to_async, retry_transient_errors, find_spark_home
 from hailtop.config import get_deploy_config
 from hailtop.tls import internal_server_ssl_context
 from hailtop.hail_logging import AccessLogger
@@ -190,18 +189,7 @@ async def on_startup(app):
     thread_pool = concurrent.futures.ThreadPoolExecutor(max_workers=16)
     app['thread_pool'] = thread_pool
 
-    spark_home = os.environ.get('SPARK_HOME')
-    if spark_home is None:
-        find_spark_home = subprocess.run('find_spark_home.py',
-                                         capture_output=True,
-                                         check=False)
-        if find_spark_home.returncode != 0:
-            raise ValueError(f'''SPARK_HOME is not set and find_spark_home.py returned non-zero exit code:
-STDOUT:
-{find_spark_home.stdout}
-STDERR:
-{find_spark_home.stderr}''')
-        spark_home = find_spark_home.stdout.decode().strip()
+    spark_home = find_spark_home()
     port = launch_gateway(die_on_exit=True, classpath=f'{spark_home}/jars/*:/hail.jar')
     gateway = JavaGateway(
         gateway_parameters=GatewayParameters(port=port),
