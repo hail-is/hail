15a45cfb9b0f8da01b2d0408993556f8391749e3
Tim Poterba <tpoterba@broadinstitute.org>
2021-02-02 16:18:25 -0500

    [query] Refactor NDArray constructor interfaces (#9960)
    * [query] Refactor NDArray constructor interfaces
    
    * construct doesn't take structs, takes IndexedSeq[Value[Long]]
    * Implement the nested loops in one place
    * Delete some methods from type interface, insert PCanonical casts
    
    * Fix shape/strides copy paste bug
    
    * Switch forEachIndex to do column major order
    
    * Delete stray comment
    
    * Add good print for UnsafeNDArray
    
    * The problem was that dataAddress was pointing to first element, but we were acting like it was pointing to the length of the array
    
    * Can't reduce an empty list
    
    Co-authored-by: John Compitello <j301600@gmail.com>

--- hail/src/main/scala/is/hail/types/physical/PNDArray.scala
+++ hail/src/main/scala/is/hail/types/physical/PNDArray.scala
@@ -4,13 +4,9 @@ import is.hail.annotations.{CodeOrdering, Region, StagedRegionValueBuilder}
 import is.hail.asm4s.{Code, _}
 import is.hail.expr.Nat
 import is.hail.expr.ir.{EmitCodeBuilder, EmitMethodBuilder}
+import is.hail.types.physical.stypes.SCode
 import is.hail.types.physical.stypes.interfaces.{SBaseStructCode, SNDArrayCode, SNDArrayValue}
 import is.hail.types.virtual.TNDArray
-
-final class StaticallyKnownField[T, U](
-  val pType: T,
-  val load: Code[Long] => Code[U]
-)
 
 abstract class PNDArray extends PType {
   val elementType: PType
@@ -23,36 +19,27 @@ abstract class PNDArray extends PType {
 
   def codeOrdering(mb: EmitMethodBuilder[_], other: PType): CodeOrdering = throw new UnsupportedOperationException
 
-  val shape: StaticallyKnownField[PTuple, Long]
-  val data: StaticallyKnownField[PArray, Long]
-
-  val representation: PStruct
-
   def dataFirstElementPointer(ndAddr: Code[Long]): Code[Long]
   def dataPArrayPointer(ndAddr: Code[Long]): Code[Long]
 
-  def loadShape(cb: EmitCodeBuilder, off: Code[Long], idx: Int): Code[Long]
-
   def loadShape(off: Long, idx: Int): Long
 
-  def loadStride(cb: EmitCodeBuilder, off: Code[Long], idx: Int): Code[Long]
+  def loadShapes(cb: EmitCodeBuilder, addr: Value[Long], settables: IndexedSeq[Settable[Long]]): Unit
+  def loadStrides(cb: EmitCodeBuilder, addr: Value[Long], settables: IndexedSeq[Settable[Long]]): Unit
 
   def numElements(shape: IndexedSeq[Value[Long]]): Code[Long]
+  
+  def makeRowMajorStrides(sourceShapeArray: IndexedSeq[Value[Long]], region: Value[Region], cb: EmitCodeBuilder): IndexedSeq[Value[Long]]
 
-  def makeShapeStruct(shapeArray: IndexedSeq[Value[Long]], region: Value[Region], cb: EmitCodeBuilder): SBaseStructCode
-
-  def makeRowMajorStridesStruct(sourceShapeArray: IndexedSeq[Value[Long]], region: Value[Region], cb: EmitCodeBuilder): SBaseStructCode
-
-  def makeColumnMajorStridesStruct(sourceShapeArray: IndexedSeq[Value[Long]], region: Value[Region], cb: EmitCodeBuilder): SBaseStructCode
+  def makeColumnMajorStrides(sourceShapeArray: IndexedSeq[Value[Long]], region: Value[Region], cb: EmitCodeBuilder): IndexedSeq[Value[Long]]
 
   def getElementAddress(indices: IndexedSeq[Long], nd: Long): Long
 
-  def loadElement(cb: EmitCodeBuilder, indices: IndexedSeq[Value[Long]], ndAddress: Value[Long]): Code[Long]
-  def loadElementToIRIntermediate(indices: IndexedSeq[Value[Long]], ndAddress: Value[Long], cb: EmitCodeBuilder): Code[_]
+  def loadElement(cb: EmitCodeBuilder, indices: IndexedSeq[Value[Long]], ndAddress: Value[Long]): SCode
 
   def construct(
-    shapeCode: SBaseStructCode,
-    stridesCode: SBaseStructCode,
+    shape: IndexedSeq[Value[Long]],
+    strides: IndexedSeq[Value[Long]],
     data: Code[Long],
     mb: EmitCodeBuilder,
     region: Value[Region]
