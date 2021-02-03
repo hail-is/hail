2f8e03a790db0f2a71339d333a8baaaae503c187
Vlad Savelyev <vladislav.sav@gmail.com>
2021-02-03 20:21:21 +1100

    Conda pkg: fix for hailctl (#22)
    * For building cpg conda package, skip uploading artefacts and use the official packages ones

--- conda/hail/build.sh
+++ conda/hail/build.sh
@@ -3,5 +3,5 @@
 # Build instructions
 # https://hail.is/docs/0.2/getting_started_developing.html#requirements
 pushd $SRC_DIR/hail
-make install HAIL_COMPILE_NATIVES='build' -j ${CPU_COUNT}
+make install HAIL_COMPILE_NATIVES='build' -j ${CPU_COUNT} SKIP_UPLOAD_ARTIFACTS=1
 popd
