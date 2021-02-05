cd7627af060852f1f2c257d63e1762d82ad007e7
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-02-05 23:02:33 +1100

    Strip down ci service to the minimum functionality for dev deploy.

--- ci/ci/ci.py
+++ ci/ci/ci.py
@@ -1,37 +1,23 @@
 import traceback
 import json
-import os
 import logging
-import asyncio
-import concurrent.futures
 import aiohttp
 from aiohttp import web
-import aiohttp_session
 import uvloop
-from gidgethub import aiohttp as gh_aiohttp, routing as gh_routing, sansio as gh_sansio
+from gidgethub import aiohttp as gh_aiohttp
 from hailtop.utils import collect_agen, humanize_timedelta_msecs
 from hailtop.batch_client.aioclient import BatchClient
 from hailtop.config import get_deploy_config
 from hailtop.tls import internal_server_ssl_context
 from hailtop.hail_logging import AccessLogger
-from hailtop import aiotools
 from gear import (
     setup_aiohttp_session,
     rest_authenticated_developers_only,
     web_authenticated_developers_only,
-    check_csrf_token,
-    create_database_pool,
 )
-from web_common import setup_aiohttp_jinja2, setup_common_static_routes, render_template, set_message
+from web_common import setup_aiohttp_jinja2, setup_common_static_routes, render_template
 
-from .environment import BUCKET
-from .github import Repo, FQBranch, WatchedBranch, UnwatchedBranch, MergeFailureBatch
-
-oauth_token = None
-HAIL_CI_OAUTH_TOKEN = os.getenv('HAIL_CI_OAUTH_TOKEN')
-if HAIL_CI_OAUTH_TOKEN:
-    with open(HAIL_CI_OAUTH_TOKEN, 'r') as f:
-        oauth_token = f.read().strip()
+from .github import FQBranch, UnwatchedBranch
 
 log = logging.getLogger('ci')
 
@@ -39,58 +25,7 @@ uvloop.install()
 
 deploy_config = get_deploy_config()
 
-watched_branches = [
-    WatchedBranch(index, FQBranch.from_short_str(bss), deployable)
-    for (index, [bss, deployable]) in enumerate(json.loads(os.environ.get('HAIL_WATCHED_BRANCHES', '[]')))
-]
-
 routes = web.RouteTableDef()
-
-
-def wb_and_pr_from_request(request):
-    watched_branch_index = int(request.match_info['watched_branch_index'])
-    pr_number = int(request.match_info['pr_number'])
-
-    if watched_branch_index < 0 or watched_branch_index >= len(watched_branches):
-        raise web.HTTPNotFound()
-    wb = watched_branches[watched_branch_index]
-
-    if not wb.prs or pr_number not in wb.prs:
-        raise web.HTTPNotFound()
-    return wb, wb.prs[pr_number]
-
-
-@routes.get('/watched_branches/{watched_branch_index}/pr/{pr_number}')
-@web_authenticated_developers_only()
-async def get_pr(request, userdata):  # pylint: disable=unused-argument
-    wb, pr = wb_and_pr_from_request(request)
-
-    page_context = {}
-    page_context['repo'] = wb.branch.repo.short_str()
-    page_context['wb'] = wb
-    page_context['pr'] = pr
-    # FIXME
-    batch = pr.batch
-    if batch:
-        if hasattr(batch, 'id'):
-            status = await batch.last_known_status()
-            jobs = await collect_agen(batch.jobs())
-            for j in jobs:
-                j['duration'] = humanize_timedelta_msecs(j['duration'])
-            page_context['batch'] = status
-            page_context['jobs'] = jobs
-            page_context['artifacts'] = f'/{BUCKET}/build/{batch.attributes["token"]}'
-        else:
-            page_context['exception'] = '\n'.join(
-                traceback.format_exception(None, batch.exception, batch.exception.__traceback__)
-            )
-
-    batch_client = request.app['batch_client']
-    batches = batch_client.list_batches(f'test=1 pr={pr.number}')
-    batches = sorted([b async for b in batches], key=lambda b: b.id, reverse=True)
-    page_context['history'] = [await b.last_known_status() for b in batches]
-
-    return await render_template('ci', request, userdata, 'pr.html', page_context)
 
 
 @routes.get('/batches')
@@ -139,112 +74,6 @@ async def healthcheck(request):  # pylint: disable=unused-argument
     return web.Response(status=200)
 
 
-gh_router = gh_routing.Router()
-
-
-@gh_router.register('pull_request')
-async def pull_request_callback(event):
-    gh_pr = event.data['pull_request']
-    number = gh_pr['number']
-    target_branch = FQBranch.from_gh_json(gh_pr['base'])
-    for wb in watched_branches:
-        if (wb.prs and number in wb.prs) or (wb.branch == target_branch):
-            await wb.notify_github_changed(event.app)
-
-
-@gh_router.register('push')
-async def push_callback(event):
-    data = event.data
-    ref = data['ref']
-    if ref.startswith('refs/heads/'):
-        branch_name = ref[len('refs/heads/') :]
-        branch = FQBranch(Repo.from_gh_json(data['repository']), branch_name)
-        for wb in watched_branches:
-            if wb.branch == branch or any(pr.branch == branch for pr in wb.prs.values()):
-                await wb.notify_github_changed(event.app)
-
-
-@gh_router.register('pull_request_review')
-async def pull_request_review_callback(event):
-    gh_pr = event.data['pull_request']
-    number = gh_pr['number']
-    for wb in watched_branches:
-        if number in wb.prs:
-            await wb.notify_github_changed(event.app)
-
-
-async def github_callback_handler(request):
-    event = gh_sansio.Event.from_http(request.headers, await request.read())
-    event.app = request.app
-    await gh_router.dispatch(event)
-
-
-@routes.post('/github_callback')
-async def github_callback(request):
-    await asyncio.shield(github_callback_handler(request))
-    return web.Response(status=200)
-
-
-async def batch_callback_handler(request):
-    app = request.app
-    params = await request.json()
-    log.info(f'batch callback {params}')
-    attrs = params.get('attributes')
-    if attrs:
-        target_branch = attrs.get('target_branch')
-        if target_branch:
-            for wb in watched_branches:
-                if wb.branch.short_str() == target_branch:
-                    log.info(f'watched_branch {wb.branch.short_str()} notify batch changed')
-                    await wb.notify_batch_changed(app)
-
-
-@routes.get('/api/v1alpha/deploy_status')
-@rest_authenticated_developers_only
-async def deploy_status(request, userdata):  # pylint: disable=unused-argument
-    batch_client = request.app['batch_client']
-
-    async def get_failure_information(batch):
-        if isinstance(batch, MergeFailureBatch):
-            return batch.exception
-        jobs = await collect_agen(batch.jobs())
-
-        async def fetch_job_and_log(j):
-            full_job = await batch_client.get_job(j['batch_id'], j['job_id'])
-            log = await full_job.log()
-            return {**full_job._status, 'log': log}
-
-        return await asyncio.gather(*[fetch_job_and_log(j) for j in jobs if j['state'] in ('Error', 'Failed')])
-
-    wb_configs = [
-        {
-            'branch': wb.branch.short_str(),
-            'sha': wb.sha,
-            'deploy_batch_id': wb.deploy_batch.id if wb.deploy_batch and hasattr(wb.deploy_batch, 'id') else None,
-            'deploy_state': wb.deploy_state,
-            'repo': wb.branch.repo.short_str(),
-            'failure_information': None
-            if wb.deploy_state == 'success'
-            else await get_failure_information(wb.deploy_batch),
-        }
-        for wb in watched_branches
-    ]
-    return web.json_response(wb_configs)
-
-
-@routes.post('/api/v1alpha/update')
-@rest_authenticated_developers_only
-async def post_update(request, userdata):  # pylint: disable=unused-argument
-    log.info('developer triggered update')
-
-    async def update_all():
-        for wb in watched_branches:
-            await wb.update(request.app)
-
-    request.app['task_manager'].ensure_future(update_all())
-    return web.Response(status=200)
-
-
 @routes.post('/api/v1alpha/dev_deploy_branch')
 @rest_authenticated_developers_only
 async def dev_deploy_branch(request, userdata):
@@ -287,40 +116,15 @@ async def dev_deploy_branch(request, userdata):
     return web.json_response({'sha': sha, 'batch_id': batch_id})
 
 
-@routes.post('/api/v1alpha/batch_callback')
-async def batch_callback(request):
-    await asyncio.shield(batch_callback_handler(request))
-    return web.Response(status=200)
-
-
-async def update_loop(app):
-    while True:
-        try:
-            for wb in watched_branches:
-                log.info(f'updating {wb.branch.short_str()}')
-                await wb.update(app)
-        except concurrent.futures.CancelledError:
-            raise
-        except Exception:  # pylint: disable=broad-except
-            log.exception(f'{wb.branch.short_str()} update failed due to exception')
-        await asyncio.sleep(300)
-
-
 async def on_startup(app):
     app['gh_client_session'] = aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=5))
-    app['github_client'] = gh_aiohttp.GitHubAPI(app['gh_client_session'], 'ci', oauth_token=oauth_token)
+    app['github_client'] = gh_aiohttp.GitHubAPI(app['gh_client_session'], 'ci')
     app['batch_client'] = BatchClient('ci')
-
-    app['task_manager'] = aiotools.BackgroundTaskManager()
-    app['task_manager'].ensure_future(update_loop(app))
 
 
 async def on_cleanup(app):
-    try:
-        await app['gh_client_session'].close()
-        await app['batch_client'].close()
-    finally:
-        app['task_manager'].shutdown()
+    await app['gh_client_session'].close()
+    await app['batch_client'].close()
 
 
 def run():
