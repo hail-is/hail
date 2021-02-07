40c71b49730ce2efe32c44e7eb156ba80ea43997
Tim Poterba <tpoterba@gmail.com>
2021-02-07 16:29:31 -0500

    [query] Add missing benchmarks for group_by/aggregate

--- benchmark/python/benchmark_hail/run/shuffle_benchmarks.py
+++ benchmark/python/benchmark_hail/run/shuffle_benchmarks.py
@@ -35,3 +35,16 @@ def shuffle_key_rows_by_65k_byte_rows():
     mt = mt.annotate_entries(entry=mt.row_idx * mt.col_idx)
     mt = mt.key_rows_by(backward_rows_idx=-mt.row_idx)
     mt._force_count_rows()
+
+
+@benchmark(args=many_ints_table.handle('ht'))
+def shuffle_key_by_aggregate_bad_locality(ht_path):
+    ht = hl.read_table(ht_path)
+    ht.group_by(x=ht.i0 % 1000).aggregate(c=hl.agg.count(), m=hl.agg.mean(ht.i2))._force_count()
+
+
+@benchmark(args=many_ints_table.handle('ht'))
+def shuffle_key_by_aggregate_good_locality(ht_path):
+    ht = hl.read_table(ht_path)
+    divisor = (7_500_000 / 51)  # should ensure each partition never overflows default buffer size
+    ht.group_by(x=ht.idx // divisor).aggregate(c=hl.agg.count(), m=hl.agg.mean(ht.i2))._force_count()
