181448746dba33ab8b9114cd8dc632d08d183831
Tim Poterba <tpoterba@gmail.com>
2021-02-11 10:55:11 -0500

    [query] Fix 0-partition matrix table writes.
    CHANGELOG: Fix crashes when writing/reading matrix tables with 0 partitions.

--- hail/python/test/hail/matrixtable/test_matrix_table.py
+++ hail/python/test/hail/matrixtable/test_matrix_table.py
@@ -1185,6 +1185,14 @@ class Tests(unittest.TestCase):
         mt2 = hl.read_matrix_table(f)
         self.assertTrue(mt._same(mt2))
 
+    def test_write_no_parts(self):
+        mt = hl.utils.range_matrix_table(10, 10, 2).filter_rows(False)
+        path = new_temp_file(extension='mt')
+        path2 = new_temp_file(extension='mt')
+        assert mt.checkpoint(path)._same(mt)
+        hl.read_matrix_table(path, _drop_rows=True).write(path2)
+
+
     def test_nulls_in_distinct_joins(self):
 
         # MatrixAnnotateRowsTable uses left distinct join
