dfe2d7e9063ec653dc77138c05b07608b54bf137
Tim Poterba <tpoterba@gmail.com>
2021-02-16 17:07:39 -0500

    remove comments

--- hail/src/main/scala/is/hail/io/CodecSpec.scala
+++ hail/src/main/scala/is/hail/io/CodecSpec.scala
@@ -17,9 +17,6 @@ trait AbstractTypedCodecSpec extends Spec {
   def encodedType: EType
   def encodedVirtualType: Type
 
-//  type StagedEncoderF = (EmitCodeBuilder, Value[Region], Value[Long], Value[OutputBuffer]) => Unit
-//  type StagedDecoderF = (EmitCodeBuilder, Value[Region], Value[InputBuffer]) => Code[Long]
-
   def buildEncoder(ctx: ExecuteContext, t: PType): (OutputStream) => Encoder
 
   def encodeValue(ctx: ExecuteContext, t: PType, valueAddr: Long): Array[Byte] = {
@@ -52,41 +49,6 @@ trait AbstractTypedCodecSpec extends Spec {
   def buildCodeInputBuffer(is: Code[InputStream]): Code[InputBuffer]
 
   def buildCodeOutputBuffer(os: Code[OutputStream]): Code[OutputBuffer]
-
-//  def buildEmitDecoder(requestedType: Type, cb: EmitClassBuilder[_]): (Value[Region], Value[InputBuffer]) => PCode = {
-//    def typedBuilder[T](ti: TypeInfo[T]): (Value[Region], Value[InputBuffer]) => PCode = {
-//      val (ptype, dec) = buildTypedEmitDecoderF[T](requestedType, cb);
-//      { (r: Value[Region], ib: Value[InputBuffer]) => PCode(ptype, dec(r, ib)) }
-//    }
-//    typedBuilder(typeToTypeInfo(decodedPType(requestedType)))
-//  }
-//
-//  def buildEmitEncoder(t: PType, cb: EmitClassBuilder[_]): (EmitCodeBuilder, Value[Region], PValue, Value[OutputBuffer]) => Unit = {
-//    def typedBuilder[T](ti: TypeInfo[T]): (Value[Region], PValue, Value[OutputBuffer]) => Code[Unit] = {
-//      val enc = buildTypedEmitEncoderF(t, cb);
-//      { (r: Value[Region], v: PValue, ob: Value[OutputBuffer]) =>
-//        enc(r, v.value.asInstanceOf[Value[T]], ob)
-//      }
-//    }
-//    typedBuilder(typeToTypeInfo(t))
-//  }
-//
-//  def buildTypedEmitDecoderF(requestedType: Type, cb: EmitClassBuilder[_]): (PType, StagedDecoderF) = {
-//    val rt = encodedType.decodedPType(requestedType)
-//    val mb = encodedType.buildDecoderMethod(requestedType, cb)
-//    (rt, { (cb: EmitCodeBuilder, region: Value[Region], buf: Value[InputBuffer]) =>
-//      rt.store(cb, region, cb.invokePCode(mb, region, buf), false)
-//    })
-//  }
-//
-//  def buildEmitDecoderF(cb: EmitClassBuilder[_]): (PType, StagedDecoderF) =
-//    buildTypedEmitDecoderF(encodedVirtualType, cb)
-//
-//  def buildTypedEmitEncoderF(t: PType, cb: EmitClassBuilder[_]): StagedEncoderF = {
-//    val mb = encodedType.buildEncoderMethod(t.sType, cb)
-//    (cb: EmitCodeBuilder, region: Value[Region], off: Value[Long], buf: Value[OutputBuffer]) =>
-//      cb.invokeVoid(mb, t.loadCheapPCode(cb, off), buf)
-//  }
 
   // FIXME: is there a better place for this to live?
   def decodeRDD(ctx: ExecuteContext, requestedType: Type, bytes: RDD[Array[Byte]]): (PType, ContextRDD[Long]) = {
