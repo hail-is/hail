0d8b623b5024900bcb07610cadb21f3b52bd8d0a
Tim Poterba <tpoterba@gmail.com>
2021-02-17 18:15:54 -0500

    [query] Improve code generation of If/Coalesce

--- hail/src/main/scala/is/hail/expr/ir/Emit.scala
+++ hail/src/main/scala/is/hail/expr/ir/Emit.scala
@@ -770,7 +770,7 @@ class Emit[C](
 
         val emittedValues = values.map(v => EmitCode.fromI(cb.emb)(cb => emitInNewBuilder(cb, v)))
         val Ldefined = CodeLabel()
-        val Lend = CodeLabel()
+        val Lmissing = CodeLabel()
 
         emittedValues.foreach { value =>
           value.toI(cb).consume(cb,
@@ -781,15 +781,12 @@ class Emit[C](
             })
         }
 
-        // base case
-        cb.assign(missing, true)
-        cb.goto(Lend)
+        cb.goto(Lmissing)
 
         cb.define(Ldefined)
         cb.assign(missing, false)
-        cb.define(Lend)
 
-        IEmitCode(cb, missing, coalescedValue.load())
+        IEmitCode(Lmissing, Ldefined, coalescedValue.load())
 
       case If(cond, cnsq, altr) =>
         assert(cnsq.typ == altr.typ)
@@ -799,25 +796,27 @@ class Emit[C](
           val codeCnsq = EmitCode.fromI(cb.emb)(cb => emitInNewBuilder(cb, cnsq))
           val codeAltr = EmitCode.fromI(cb.emb)(cb => emitInNewBuilder(cb, altr))
 
-          val b = cb.newLocal[Boolean]("if_m", false)
+          val Lmissing = CodeLabel()
+          val Ldefined = CodeLabel()
           val out = mb.newPLocal(pt)
           cb.ifx(condValue.asBoolean.boolCode(cb), {
             codeCnsq.toI(cb).consume(cb,
               {
-                cb.assign(b, true)
+                cb.goto(Lmissing)
               }, {sc =>
                 cb.assign(out, sc.castTo(cb, region.code, pt))
               })
           }, {
             codeAltr.toI(cb).consume(cb,
               {
-                cb.assign(b, true)
+                cb.goto(Lmissing)
               }, {sc =>
                 cb.assign(out, sc.castTo(cb, region.code, pt))
               })
           })
+          cb.goto(Ldefined)
 
-          IEmitCode(cb, b, out.load())
+          IEmitCode(Lmissing, Ldefined, out.load())
         }
 
       case x@MakeStruct(fields) =>
