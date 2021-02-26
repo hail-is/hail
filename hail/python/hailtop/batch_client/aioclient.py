b7e877e9c9cc9af976d5006acf048d8622c59071
Jackie Goldstein <jigold@broadinstitute.org>
2021-02-26 13:13:15 -0500

    fix

--- hail/python/hailtop/batch_client/aioclient.py
+++ hail/python/hailtop/batch_client/aioclient.py
@@ -741,5 +741,5 @@ class BatchClient:
     async def __aenter__(self):
         return self
 
-    async def __aexit__(self):
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
         await self.close()
