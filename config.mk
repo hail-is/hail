7acbdff26b42800503e59bb66c376ae858ac6b91
Daniel King <dking@broadinstitute.org>
2021-03-02 14:06:15 -0500

    fixup config.mk

--- config.mk
+++ config.mk
@@ -1,9 +1,9 @@
 PROJECT := hail-vdc
 DOCKER_ROOT_IMAGE := gcr.io/$(PROJECT)/ubuntu:18.04
 DOMAIN := hail.is
-INTERNAL_IP := 10.128.0.2
-IP := 35.224.188.20
-KUBERNETES_SERVER_URL := https://34.123.185.11
+INTERNAL_IP := 10.128.0.57
+IP := 35.188.91.25
+KUBERNETES_SERVER_URL := https://104.198.230.143
 REGION := us-central1
 ZONE := us-central1-a
 ifeq ($(NAMESPACE),default)
