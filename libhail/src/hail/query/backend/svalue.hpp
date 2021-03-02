8c12d5afad9f7490afda7ed44443e31eeee0c38b
John Compitello <j301600@gmail.com>
2021-03-02 15:45:02 -0500

    Fix overriding functions to match SArrayValue

--- libhail/src/hail/query/backend/svalue.hpp
+++ libhail/src/hail/query/backend/svalue.hpp
@@ -102,14 +102,15 @@ public:
   static const Tag self_tag = SValue::Tag::ARRAY;
   const SCanonicalArray *const stype;
   SArrayValue(const SCanonicalArray *stype): SValue(self_tag, stype), stype(stype) {};
-  virtual SValue getElement(SInt64Value idx)=0;
-  virtual SInt64 getLength()=0;
+  virtual SValue* get_element(SInt64Value idx) = 0;
+  virtual SInt64Value* get_length(TypeContext *tc) = 0;
  };
 
 class SCanonicalArrayValue : public SArrayValue {
   public:
     SCanonicalArrayValue(const SCanonicalArray *stype, llvm::Value *length, llvm::Value *missing, llvm::Value *data);
-    SInt64Value* get_length(TypeContext *tc);
+    SInt64Value* get_length(TypeContext *tc) override;
+    SValue* get_element(SInt64Value idx) override;
     ~SCanonicalArrayValue();
   private:
     llvm::Value *data;
