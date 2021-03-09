e395f6d0c9dbdfbaf43d5279825b0554ba502c11
Dan King <daniel.zidan.king@gmail.com>
2021-03-09 16:46:49 -0500

    [batch] add more logs and do not wait for asyncgens (#10136)
    * [batch] add more logs and do not wait for asyncgens
    
    I think there is some unresolved issue with asyncgen shutdown that is keeping
    workers alive. This is not an issue in worker because worker calls sys.exit
    which forcibly stops execution. cc: @daniel-goldstein @jigold.
    
    * fix lint

--- batch/batch/worker/worker.py
+++ batch/batch/worker/worker.py
@@ -1323,8 +1323,6 @@ class Worker:
                 await app_runner.cleanup()
                 log.info('cleaned up app runner')
 
-            self.shutdown()
-
     async def kill_1(self, request):  # pylint: disable=unused-argument
         log.info('killed')
         self.stop_event.set()
@@ -1480,12 +1478,14 @@ async def async_main():
     finally:
         try:
             worker.shutdown()
+            log.info('worker shutdown')
         finally:
             await docker.close()
+            log.info('docker closed')
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(async_main())
-loop.run_until_complete(loop.shutdown_asyncgens())
+log.info('closing loop')
 loop.close()
 log.info('closed')
 sys.exit(0)
