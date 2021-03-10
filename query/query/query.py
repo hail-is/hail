dd29d2be76291303deadf4a9f63bf1cd0aafbaf7
Dan King <daniel.zidan.king@gmail.com>
2021-03-10 17:11:38 -0500

    [query-service] fix receive logic (#10159)
    * [query-service] fix receive logic
    
    Only one coro waits on receive now. We still error if a message is sent before
    we make our first response.
    
    * fix
    
    * fix

--- query/query/query.py
+++ query/query/query.py
@@ -6,7 +6,6 @@ import concurrent
 import logging
 import uvloop
 import asyncio
-import aiohttp
 from aiohttp import web
 import kubernetes_asyncio as kube
 from collections import defaultdict
@@ -105,13 +104,12 @@ async def handle_ws_response(request, userdata, endpoint, f):
         user_queries[body['token']] = query
 
     try:
-        receive = asyncio.ensure_future(ws.receive())  # receive automatically ping-pongs which keeps the socket alive
+        receive = asyncio.ensure_future(ws.receive_str())  # receive automatically ping-pongs which keeps the socket alive
         await asyncio.wait([receive, query], return_when=asyncio.FIRST_COMPLETED)
         if receive.done():
             # we expect no messages from the client
             response = receive.result()
-            assert response.type in (aiohttp.WSMsgType.CLOSE, aiohttp.WSMsgType.CLOSING), (
-                f'{endpoint}: Received websocket message. Expected CLOSE or CLOSING, got {response}')
+            raise AssertionError(f'{endpoint}: client broke the protocol by sending: {response}')
         if not query.done():
             return
         if query.exception() is not None:
@@ -120,7 +118,7 @@ async def handle_ws_response(request, userdata, endpoint, f):
             await ws.send_json({'status': 500, 'value': exc_str})
         else:
             await ws.send_json({'status': 200, 'value': query.result()})
-        assert await ws.receive_str() == 'bye'
+        assert (await receive) == 'bye'
         del user_queries[body['token']]
     finally:
         receive.cancel()
