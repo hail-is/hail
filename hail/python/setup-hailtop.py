26ed97734e5cae64cc89270e498e9a6f3defd58c
MIchael Franklin <michael.franklin@populationgenomics.org.au>
2021-03-12 16:43:46 +1100

    Add similar changes to setup-hailtop.py

--- hail/python/setup-hailtop.py
+++ hail/python/setup-hailtop.py
@@ -17,8 +17,9 @@ setup(
     package_dir={
         'hailtop': 'hailtop'},
     package_data={
-        "hailtop": ["py.typed"],
-        'hailtop.hailctl': ['hail_version', 'deploy.yaml']},
+        "hailtop": ["py.typed", "hail_version"],
+        'hailtop.hailctl': ['hail_version', 'deploy.yaml']
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -27,5 +28,6 @@ setup(
     entry_points={
         'console_scripts': ['hailctl = hailtop.hailctl.__main__:main']
     },
-    setup_requires=["pytest-runner", "wheel"]
+    setup_requires=["pytest-runner", "wheel"],
+    include_package_data=True,
 )
