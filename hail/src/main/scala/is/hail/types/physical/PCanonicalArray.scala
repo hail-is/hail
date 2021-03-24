c91536104b4f14eb0652af3e624ba29f7bd52ce7
John Compitello <j301600@gmail.com>
2021-03-24 16:05:26 -0400

    Remove error checking

--- hail/src/main/scala/is/hail/types/physical/PCanonicalArray.scala
+++ hail/src/main/scala/is/hail/types/physical/PCanonicalArray.scala
@@ -123,13 +123,8 @@ final case class PCanonicalArray(elementType: PType, required: Boolean = false) 
   def firstElementOffset(aoff: Long, length: Int): Long =
     aoff + elementsOffset(length)
 
-  def firstElementOffset(aoff: Long): Long = {
-    val loadedLength = loadLength(aoff)
-    if (loadedLength < 0L) {
-      throw new IllegalStateException(s"Loaded length was negative. aoff = ${aoff}, length = ${loadedLength}")
-    }
+  def firstElementOffset(aoff: Long): Long =
     aoff + elementsOffset(loadLength(aoff))
-  }
 
   def firstElementOffset(aoff: Code[Long], length: Code[Int]): Code[Long] =
     aoff + elementsOffset(length)
