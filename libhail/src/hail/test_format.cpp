90f568bf0d108323d55a390b3bef745dc5963aa8
Cotton Seed <cotton@alum.mit.edu>
2021-03-30 10:11:39 -0400

    [libhail] add missing file

---
+++ libhail/src/hail/test_format.cpp
@@ -0,0 +1,26 @@
+#include <cassert>
+#include <hail/allocators.hpp>
+#include <hail/format.hpp>
+#include <hail/test.hpp>
+#include <hail/type.hpp>
+#include <hail/value.hpp>
+#include <hail/vtype.hpp>
+
+namespace hail {
+
+TEST_CASE(test_render) {
+  assert(render(5) == "5");
+
+  std::string s = "foo";
+  assert(render(5, s, 2.5) == "5foo2.5");
+}
+
+TEST_CASE(test_format_address) {
+  CHECK_EQ(render(FormatAddress(nullptr)), "0000000000000000");
+}
+
+TEST_CASE(test_indent) {
+  assert(render(Indent(5), "and then") == "     and then");
+}
+
+}
