02e606dea589ef20c687adafab5746bdc22a63d0
Dan King <daniel.zidan.king@gmail.com>
2021-04-05 12:19:09 -0400

    [k8s] never delete a secret (#10217)
    For some reason we delete secrets when re-creating them. This creates a race condition which Batch
    appears to encounter whenever it is under heavy load during a deploy.
    
    @cseed, do you recall why you chose to delete and then use apply in ci/create_database.py?
    
    I am using the command sequence suggested here: https://stackoverflow.com/questions/45879498/how-can-i-update-a-secret-on-kubernetes-when-it-is-generated-from-a-file.

--- tls/create_certs.py
+++ tls/create_certs.py
@@ -176,7 +176,7 @@ def create_principal(principal, kind, key, cert, key_store, unmanaged):
              f'--from-file={outgoing_trust["trust"]}',
              f'--from-file={outgoing_trust["trust_store"]}',
              *[f'--from-file={c}' for c in configs],
-             '--dry-run', '-o', 'yaml'],
+             '--save-config', '--dry-run=client', '-o', 'yaml'],
             stdout=k8s_secret)
         sp.check_call(['kubectl', 'apply', '-f', k8s_secret.name])
 
