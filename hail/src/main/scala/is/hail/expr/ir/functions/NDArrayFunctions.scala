bed742d7c8aa7b1885a991871ad3a46acea7123d
John Compitello <j301600@gmail.com>
2021-04-07 13:02:38 -0400

    Linear solve wip

--- hail/src/main/scala/is/hail/expr/ir/functions/NDArrayFunctions.scala
+++ hail/src/main/scala/is/hail/expr/ir/functions/NDArrayFunctions.scala
@@ -1,6 +1,6 @@
 package is.hail.expr.ir.functions
 
-import is.hail.expr.NatVariable
+import is.hail.expr.{Nat, NatVariable}
 import is.hail.expr.ir._
 import is.hail.types.coerce
 import is.hail.types.virtual._
@@ -27,6 +27,17 @@ object NDArrayFunctions extends RegistryFunctions {
 
         NDArrayMap2(l, r, lid, rid, irOp(lElemRef, rElemRef))
       }
+    }
+
+    registerIEmitCode2("linear_solve", TNDArray(TFloat64, Nat(2)), TNDArray(TFloat64, Nat(2)), TNDArray(TFloat64, Nat(2)), { (t, p1, p2) => p2 }) { case (cb, r, pt, aec, bec) =>
+      aec.toI(cb).flatMap(cb){ apc =>
+        bec.toI(cb).map(cb){ bpc =>
+          val a = apc.asNDArray.memoize(cb, "A")
+          val b = bpc.asNDArray.memoize(cb, "B")
+          val IndexedSeq(m, k) = b.shapes(cb)
+        }
+      }
+      ???
     }
   }
 }
