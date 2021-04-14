26478641a6592b2660cef4a6d7335379dc4880ca
John Compitello <j301600@gmail.com>
2021-04-14 17:54:13 -0400

    Actually running the arrayRef test, but the arrayLen one is commented out since it only lets me run one function currently

--- libhail/src/hail/test_array.cpp
+++ libhail/src/hail/test_array.cpp
@@ -7,19 +7,14 @@
 #include <hail/query/backend/jit.hpp>
 
 namespace hail {
-    HeapAllocator heap;
-    ArenaAllocator arena(heap);
-    TypeContext tc(heap);
-
-    auto vint32 = cast<VInt32>(tc.get_vtype(tc.tint32));
-    auto vint64 = cast<VInt64>(tc.get_vtype(tc.tint64));
-    auto vfloat64 = cast<VFloat64>(tc.get_vtype(tc.tfloat64));
-    auto vstr = cast<VStr>(tc.get_vtype(tc.tstr));
-    auto vbool = cast<VBool>(tc.get_vtype(tc.tbool));
 
     // TEST_CASE(test_array_length) {
+    //     HeapAllocator heap;
+    //     ArenaAllocator arena(heap);
+    //     TypeContext tc(heap);
     //     auto region = std::make_shared<ArenaAllocator>(heap);
     //     auto varray = cast<VArray>(tc.get_vtype(tc.tarray(tc.tfloat64)));
+    //     auto vfloat64 = cast<VFloat64>(tc.get_vtype(tc.tfloat64));
 
     //     int array_length = 8;
     //     auto my_array = Value::make_array(varray, region, array_length);
@@ -29,7 +24,6 @@ namespace hail {
     //         Value element(vfloat64, 5.2 + i);
     //         my_array.set_element(i, element);
     //     }
-    //     print(my_array);
 
     //     IRContext xc(heap);
     //     Module *m = xc.make_module();
@@ -62,9 +56,16 @@ namespace hail {
     // }
 
     TEST_CASE(test_array_ref) {
+        HeapAllocator heap;
+        ArenaAllocator arena(heap);
+        TypeContext tc(heap);
         IRContext xc(heap);
         Module *m = xc.make_module();
         JIT jit;
+        auto vfloat64 = cast<VFloat64>(tc.get_vtype(tc.tfloat64));
+        auto vint64 = cast<VInt64>(tc.get_vtype(tc.tint64));
+
+
         std::vector<const Type *> param_types;
         std::vector<const VType *> param_vtypes;
         std::vector<IR *> array_elements;
@@ -89,6 +90,6 @@ namespace hail {
 
         auto compiled = jit.compile(heap, tc, m, param_vtypes, return_vtype);
         auto ans = compiled.invoke(region, {});
-        //assert(ans.as_float64() == 6.2);
+        assert(ans.as_float64() == 6.2);
     }
 }
