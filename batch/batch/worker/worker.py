e56cbecdb6b6a52d4dbb37465ce0ce052cc617cf
Daniel Goldstein <danielgold95@gmail.com>
2021-04-15 18:59:01 -0400

    return early if no other tasks

--- batch/batch/worker/worker.py
+++ batch/batch/worker/worker.py
@@ -1507,6 +1507,8 @@ async def async_main():
             log.debug('Tasks immediately after docker close')
             dump_all_stacktraces()
             other_tasks = [t for t in asyncio.tasks() if t != asyncio.current_task()]
+            if not other_tasks:
+                return
             _, pending = await asyncio.wait(other_tasks, timeout=10 * 60, return_when=asyncio.ALL_COMPLETED)
             for t in pending:
                 log.debug('Dangling task:')
