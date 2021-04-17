578d5822c1361af52f6c7c7ffe74a5dc873e79ef
Chris Vittal <chris@vittal.dev>
2021-04-16 20:22:00 -0400

    flesh out python module more

--- libhail/src/hail/python/_hail/module.cpp
+++ libhail/src/hail/python/_hail/module.cpp
@@ -3,6 +3,7 @@
 
 #include "hail/allocators.hpp"
 #include "hail/format.hpp"
+#include "hail/tunion.hpp"
 #include "hail/type.hpp"
 
 namespace {
@@ -21,95 +22,144 @@ struct HailTypeObject {
   const hail::Type *type;
 };
 
+/* basic hail type methods */
 static PyObject *HailType_str(HailTypeObject *self);
+static PyObject *HailType_repr(HailTypeObject *self);
+static PyObject *HailType_richcmp(HailTypeObject *self, PyObject *other, int op);
+static Py_hash_t HailType_hash(HailTypeObject *self);
+
 static PyTypeObject HailType = {
   .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
   .tp_name = "_hail.HailType",
   .tp_basicsize = sizeof(HailTypeObject),
   .tp_itemsize = 0, /* itemsize */
+  .tp_repr = (reprfunc)HailType_repr,
+  .tp_hash = (hashfunc)HailType_hash,
   .tp_str = (reprfunc)HailType_str,
   .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
   .tp_doc = "Hail type superclass",
+  .tp_richcompare = (richcmpfunc)HailType_richcmp,
   .tp_new = PyType_GenericNew,
 };
 
-struct TVoidObject : public HailTypeObject {};
-struct TBoolObject : public HailTypeObject {};
-struct TInt32Object : public HailTypeObject {};
-struct TInt64Object : public HailTypeObject {};
-struct TFloat32Object : public HailTypeObject {};
-struct TFloat64Object : public HailTypeObject {};
-struct TStrObject : public HailTypeObject {};
-
-static PyObject *HailType_NewTVoid(PyTypeObject *subtype, PyObject *args, PyObject *kwds);
-static PyObject *HailType_NewTBool(PyTypeObject *subtype, PyObject *args, PyObject *kwds);
-static PyObject *HailType_NewTInt32(PyTypeObject *subtype, PyObject *args, PyObject *kwds);
-static PyObject *HailType_NewTInt64(PyTypeObject *subtype, PyObject *args, PyObject *kwds);
-static PyObject *HailType_NewTFloat32(PyTypeObject *subtype, PyObject *args, PyObject *kwds);
-static PyObject *HailType_NewTFloat64(PyTypeObject *subtype, PyObject *args, PyObject *kwds);
-static PyObject *HailType_NewTStr(PyTypeObject *subtype, PyObject *args, PyObject *kwds);
-
+static PyObject *TVoid_new(PyTypeObject *subtype, PyObject *args, PyObject *kwds);
 static PyTypeObject TVoid = {
   .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
   .tp_name = "_hail._tvoid",
   .tp_flags = Py_TPFLAGS_DEFAULT,
   .tp_doc = "Hail type for boolean (true or false) values",
   .tp_base = &HailType,
-  .tp_new = HailType_NewTVoid,
+  .tp_new = TVoid_new,
 };
 
+static PyObject *TBool_new(PyTypeObject *subtype, PyObject *args, PyObject *kwds);
 static PyTypeObject TBool = {
   .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
   .tp_name = "_hail._tbool",
   .tp_flags = Py_TPFLAGS_DEFAULT,
   .tp_doc = "Hail type for boolean (true or false) values",
   .tp_base = &HailType,
-  .tp_new = HailType_NewTBool,
+  .tp_new = TBool_new,
 };
 
+static PyObject *TInt32_new(PyTypeObject *subtype, PyObject *args, PyObject *kwds);
 static PyTypeObject TInt32 = {
   .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
   .tp_name = "_hail._tint32",
   .tp_flags = Py_TPFLAGS_DEFAULT,
   .tp_doc = "Hail type for 32 bit signed integers",
   .tp_base = &HailType,
-  .tp_new = HailType_NewTInt32,
+  .tp_new = TInt32_new,
 };
 
+static PyObject *TInt64_new(PyTypeObject *subtype, PyObject *args, PyObject *kwds);
 static PyTypeObject TInt64 = {
   .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
   .tp_name = "_hail._tint64",
   .tp_flags = Py_TPFLAGS_DEFAULT,
   .tp_doc = "Hail type for 64 bit signed integers",
   .tp_base = &HailType,
-  .tp_new = HailType_NewTInt64,
+  .tp_new = TInt64_new,
 };
 
+static PyObject *TFloat32_new(PyTypeObject *subtype, PyObject *args, PyObject *kwds);
 static PyTypeObject TFloat32 = {
   .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
   .tp_name = "_hail._tfloat32",
   .tp_flags = Py_TPFLAGS_DEFAULT,
   .tp_doc = "Hail type for IEEE-754 32 bit floating point numbers",
   .tp_base = &HailType,
-  .tp_new = HailType_NewTFloat32,
+  .tp_new = TFloat32_new,
 };
 
+static PyObject *TFloat64_new(PyTypeObject *subtype, PyObject *args, PyObject *kwds);
 static PyTypeObject TFloat64 = {
   .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
   .tp_name = "_hail._tfloat64",
   .tp_flags = Py_TPFLAGS_DEFAULT,
   .tp_doc = "Hail type for IEEE-754 64 bit floating point numbers",
   .tp_base = &HailType,
-  .tp_new = HailType_NewTFloat64,
+  .tp_new = TFloat64_new,
 };
 
+static PyObject *TStr_new(PyTypeObject *subtype, PyObject *args, PyObject *kwds);
 static PyTypeObject TStr = {
   .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
   .tp_name = "_hail._tstr",
   .tp_flags = Py_TPFLAGS_DEFAULT,
   .tp_doc = "Hail type for strings",
   .tp_base = &HailType,
-  .tp_new = HailType_NewTStr,
+  .tp_new = TStr_new,
+};
+
+static int TArray_init(HailTypeObject *self, PyObject *args, PyObject *kwds);
+static PyObject *TArray_get_element_type(HailTypeObject *self, void *closure);
+static PyGetSetDef TArray_getset[] = {
+    {"element_type", (getter) TArray_get_element_type, nullptr, "element type", nullptr},
+    {nullptr, nullptr, nullptr, nullptr, nullptr}  /* Sentinel */
+};
+static PyTypeObject TArray = {
+  .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
+  .tp_name = "_hail.tarray",
+  .tp_flags = Py_TPFLAGS_DEFAULT,
+  .tp_doc = "Hail type for arrays",
+  .tp_getset = TArray_getset,
+  .tp_base = &HailType,
+  .tp_init = (initproc)TArray_init,
+};
+
+static int TStream_init(HailTypeObject *self, PyObject *args, PyObject *kwds);
+static PyObject *TStream_get_element_type(HailTypeObject *self, void *closure);
+static PyGetSetDef TStream_getset[] = {
+    {"element_type", (getter) TStream_get_element_type, nullptr, "element type", nullptr},
+    {nullptr, nullptr, nullptr, nullptr, nullptr}  /* Sentinel */
+};
+static PyTypeObject TStream = {
+  .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
+  .tp_name = "_hail.tstream",
+  .tp_flags = Py_TPFLAGS_DEFAULT,
+  .tp_doc = "Hail type for streams",
+  .tp_getset = TStream_getset,
+  .tp_base = &HailType,
+  .tp_init = (initproc)TStream_init,
+};
+
+static int TTuple_init(HailTypeObject *self, PyObject *args, PyObject *kwds);
+static Py_ssize_t TTuple_len(HailTypeObject *self);
+static PyObject *TTuple_getitem(HailTypeObject *self, Py_ssize_t index);
+static PySequenceMethods TTuple_SeqMethods = {
+  .sq_length = (lenfunc)TTuple_len,
+  .sq_item = (ssizeargfunc)TTuple_getitem,
+};
+// FIXME possibly add a faster iterator
+static PyTypeObject TTuple = {
+  .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
+  .tp_name = "_hail.ttuple",
+  .tp_as_sequence = &TTuple_SeqMethods,
+  .tp_flags = Py_TPFLAGS_DEFAULT,
+  .tp_doc = "Hail type for tuples",
+  .tp_base = &HailType,
+  .tp_init = (initproc)TTuple_init,
 };
 
 PyDoc_STRVAR(_hail_module_doc, "hail native methods/interface");
@@ -126,12 +176,13 @@ static struct PyModuleDef hail_types_module = {
   nullptr, /* m_free */
 };
 
+// public object singletons
+static PyObject *tvoid, *tbool, *tint32, *tint64, *tfloat32, *tfloat64, *tstr;
+
 PyMODINIT_FUNC
 PyInit__hail(void)
 {
   PyObject *mod = PyModule_Create(&hail_types_module);
-  PyObject *tvoid{}, *tbool{}, *tint32{}, *tint64{}, *tfloat32{}, *tfloat64{};
-  PyObject *tstr{};
   if (mod == nullptr) {
     goto error;
   }
@@ -143,7 +194,10 @@ PyInit__hail(void)
       || PyType_Ready(&TInt64) < 0
       || PyType_Ready(&TFloat32) < 0
       || PyType_Ready(&TFloat64) < 0
-      || PyType_Ready(&TStr) < 0)
+      || PyType_Ready(&TStr) < 0
+      || PyType_Ready(&TArray) < 0
+      || PyType_Ready(&TStream) < 0
+      || PyType_Ready(&TTuple) < 0)
   {
     goto error;
   }
@@ -201,6 +255,24 @@ PyInit__hail(void)
     goto error;
   }
 
+  Py_INCREF(&TArray);
+  if (PyModule_AddObject(mod, "tarray", (PyObject *)&TArray) < 0) {
+    Py_DECREF(&TArray);
+    goto error;
+  }
+
+  Py_INCREF(&TStream);
+  if (PyModule_AddObject(mod, "tstream", (PyObject *)&TStream) < 0) {
+    Py_DECREF(&TStream);
+    goto error;
+  }
+
+  Py_INCREF(&TTuple);
+  if (PyModule_AddObject(mod, "ttuple", (PyObject *)&TTuple) < 0) {
+    Py_DECREF(&TStream);
+    goto error;
+  }
+
   return mod;
 
 error:
@@ -229,6 +301,59 @@ HailType_str(HailTypeObject *self)
 }
 
 static PyObject *
+HailType_repr(HailTypeObject *self)
+{
+  PyObject *str = HailType_str(self);
+
+  if (!PyUnicode_Check(str)) {
+    // this is None (or NULL), return it
+    return str;
+  }
+
+  // constants, intermidiate, and return strings
+  PyObject *substr{}, *repstr{}, *tmpstr{}, *retstr{};
+  substr = PyUnicode_FromString("'");
+  if (substr == nullptr) goto hailtype_repr_out;
+  repstr = PyUnicode_FromString("\\'");
+  if (repstr == nullptr) goto hailtype_repr_out;
+  tmpstr = PyUnicode_Replace(str, substr, repstr, -1);
+  if (tmpstr == nullptr) goto hailtype_repr_out;
+  retstr = PyUnicode_FromFormat("dtype('%U')", tmpstr);
+
+hailtype_repr_out:
+  Py_DECREF(str);
+  Py_XDECREF(substr);
+  Py_XDECREF(repstr);
+  Py_XDECREF(tmpstr);
+  return retstr;
+}
+
+static Py_hash_t
+HailType_hash(HailTypeObject *self)
+{
+  /* bottom 3 or 4 bits are likely to be 0; rotate y by 4 to avoid
+     excessive hash collisions for dicts and sets */
+  uintptr_t y = (size_t)self->type;
+  y = (y >> 4) | (y << (8 * SIZEOF_VOID_P - 4));
+  Py_hash_t hash = y;
+  return hash == -1 ? -2 : hash;
+}
+
+static PyObject *
+HailType_richcmp(HailTypeObject *self, PyObject *other, int op)
+{
+  if (op != Py_EQ && op != Py_NE) {
+    Py_RETURN_NOTIMPLEMENTED;
+  }
+  switch (PyObject_IsInstance(other, (PyObject*)&HailType)) {
+    case -1: return nullptr;
+    case 1: break;
+    default: Py_RETURN_FALSE;
+  }
+  Py_RETURN_RICHCOMPARE(self->type, ((HailTypeObject*)other)->type, op);
+}
+
+static PyObject *
 HailType_New(PyTypeObject *type, const hail::Type * hail_type)
 {
   HailTypeObject *self = (HailTypeObject *)type->tp_alloc(type, 0);
@@ -237,46 +362,173 @@ HailType_New(PyTypeObject *type, const hail::Type * hail_type)
   return (PyObject *)self;
 }
 
+#define RETURN_TVOID do { Py_INCREF(tvoid); return tvoid; } while (0)
+#define RETURN_TBOOL do { Py_INCREF(tbool); return tbool; } while (0)
+#define RETURN_TINT32 do { Py_INCREF(tint32); return tint32; } while (0)
+#define RETURN_TINT64 do { Py_INCREF(tint64); return tint64; } while (0)
+#define RETURN_TFLOAT32 do { Py_INCREF(tfloat32); return tfloat32; } while (0)
+#define RETURN_TFLOAT64 do { Py_INCREF(tfloat64); return tfloat64; } while (0)
+#define RETURN_TSTR do { Py_INCREF(tstr); return tstr; } while (0)
+
+// utility to construct the appropriate HailType python subclass
+// from a hail::Type *
 static PyObject *
-HailType_NewTVoid(PyTypeObject *type, PyObject *args, PyObject *kwds)
+HailType_from_Type(const hail::Type *type)
+{
+  PyObject *obj{};
+  switch (type->tag) {
+    case hail::Type::Tag::VOID:
+      RETURN_TVOID;
+    case hail::Type::Tag::BOOL:
+      RETURN_TBOOL;
+    case hail::Type::Tag::INT32:
+      RETURN_TINT32;
+    case hail::Type::Tag::INT64:
+      RETURN_TINT64;
+    case hail::Type::Tag::FLOAT32:
+      RETURN_TFLOAT32;
+    case hail::Type::Tag::FLOAT64:
+      RETURN_TFLOAT64;
+    case hail::Type::Tag::STR:
+      RETURN_TSTR;
+    case hail::Type::Tag::ARRAY:
+      obj = TArray.tp_new(&TArray, NULL, NULL);
+      break;
+    case hail::Type::Tag::STREAM:
+      obj = TStream.tp_new(&TStream, NULL, NULL);
+      break;
+    case hail::Type::Tag::TUPLE:
+      obj = TTuple.tp_new(&TTuple, NULL, NULL);
+      break;
+    default:
+      abort();
+  }
+  if (obj) {
+    ((HailTypeObject *)obj)->type = type;
+  }
+  return obj;
+}
+
+static PyObject *
+TVoid_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
   return HailType_New(type, pytcx.tvoid);
 }
 
 static PyObject *
-HailType_NewTBool(PyTypeObject *type, PyObject *args, PyObject *kwds)
+TBool_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
   return HailType_New(type, pytcx.tbool);
 }
 
 static PyObject *
-HailType_NewTInt32(PyTypeObject *type, PyObject *args, PyObject *kwds)
+TInt32_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
   return HailType_New(type, pytcx.tint32);
 }
 
 static PyObject *
-HailType_NewTInt64(PyTypeObject *type, PyObject *args, PyObject *kwds)
+TInt64_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
   return HailType_New(type, pytcx.tint64);
 }
 
 static PyObject *
-HailType_NewTFloat32(PyTypeObject *type, PyObject *args, PyObject *kwds)
+TFloat32_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
   return HailType_New(type, pytcx.tfloat32);
 }
 
 static PyObject *
-HailType_NewTFloat64(PyTypeObject *type, PyObject *args, PyObject *kwds)
+TFloat64_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
   return HailType_New(type, pytcx.tfloat64);
 }
 
 static PyObject *
-HailType_NewTStr(PyTypeObject *type, PyObject *args, PyObject *kwds)
+TStr_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
   return HailType_New(type, pytcx.tstr);
+}
+
+static int
+TArray_init(HailTypeObject *self, PyObject *args, PyObject *kwds)
+{
+  const char *kw_names[] = {"element_type", nullptr};
+  HailTypeObject *py_element_type{};
+  if (!PyArg_ParseTupleAndKeywords(args, kwds, "O!:__init__", (char **)kw_names,
+                                   &HailType, &py_element_type)) {
+    return -1;
+  }
+  const hail::TArray *type = pytcx.tarray(py_element_type->type);
+  self->type = type;
+  return 0;
+}
+
+static PyObject *
+TArray_get_element_type(HailTypeObject *self, void *closure)
+{
+  return HailType_from_Type(hail::cast<hail::TArray>(self->type)->element_type);
+}
+
+static int
+TStream_init(HailTypeObject *self, PyObject *args, PyObject *kwds)
+{
+  const char *kw_names[] = {"element_type", nullptr};
+  HailTypeObject *py_element_type{};
+  if (!PyArg_ParseTupleAndKeywords(args, kwds, "O!:__init__", (char **)kw_names,
+                                   &HailType, &py_element_type)) {
+    return -1;
+  }
+  const hail::TStream *type = pytcx.tstream(py_element_type->type);
+  self->type = type;
+  return 0;
+}
+
+static PyObject *
+TStream_get_element_type(HailTypeObject *self, void *closure)
+{
+  return HailType_from_Type(hail::cast<hail::TStream>(self->type)->element_type);
+}
+
+static int
+TTuple_init(HailTypeObject *self, PyObject *args, PyObject *kwds)
+{
+  std::vector<const hail::Type *> element_types;
+  Py_ssize_t n_args = PyTuple_Size(args);
+  for (auto i = 0; i < n_args; i++) {
+    PyObject *item = PyTuple_GET_ITEM(args, i);
+    switch (PyObject_IsInstance(item, (PyObject*)&HailType)) {
+      case -1: return -1;
+      case 0:
+        PyErr_Format(PyExc_TypeError,
+                     "expected argument %d of __init__() to be %.200s, "
+                     "not %.200s", i + 1, HailType.tp_name,
+                     _PyType_Name(Py_TYPE(item)));
+        return -1;
+      case 1: break;
+      default: abort();
+    }
+    element_types.push_back(reinterpret_cast<HailTypeObject *>(item)->type);
+  }
+  self->type = pytcx.ttuple(element_types);
+  return 0;
+}
+
+static Py_ssize_t
+TTuple_len(HailTypeObject *self)
+{
+  return hail::cast<hail::TTuple>(self->type)->element_types.size();
+}
+
+static PyObject *TTuple_getitem(HailTypeObject *self, Py_ssize_t index)
+{
+  auto types = hail::cast<hail::TTuple>(self->type)->element_types;
+  if (index >= (Py_ssize_t)types.size()) {
+    PyErr_SetString(PyExc_IndexError, "tuple index out of range");
+    return nullptr;
+  }
+  return HailType_from_Type(types[index]);
 }
 
 } // namespace
