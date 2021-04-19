e8b2fc8f663ed820811cd6cfbd194ddab49d2aef
Daniel King <dking@broadinstitute.org>
2021-04-19 15:04:31 -0400

    worker needs to await cleanup

--- batch/batch/worker/worker.py
+++ batch/batch/worker/worker.py
@@ -1126,8 +1126,8 @@ class JVMJob(Job):
 
                 self.state = 'error'
                 self.error = traceback.format_exc()
-                self.cleanup()
-            self.cleanup()
+                await self.cleanup()
+            await self.cleanup()
 
     async def cleanup(self):
         self.end_time = time_msecs()
