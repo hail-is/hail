e66a389a55f7bfae68415073a64411ff71500436
Cotton Seed <cotton@alum.mit.edu>
2021-04-22 22:42:33 -0400

    [fs] LocalAsyncFS: validate file URL

--- hail/python/hailtop/aiotools/fs.py
+++ hail/python/hailtop/aiotools/fs.py
@@ -235,7 +235,15 @@ class LocalAsyncFS(AsyncFS):
     def _get_path(url):
         parsed = urllib.parse.urlparse(url)
         if parsed.scheme and parsed.scheme != 'file':
-            raise ValueError(f"invalid scheme, expected file: {parsed.scheme}")
+            raise ValueError(f"invalid file URL: {url}, invalid scheme: expected file, got {parsed.scheme}")
+        if parsed.netloc and parsed.netloc != 'localhost':
+            raise ValueError(f"invalid file URL: {url}, invalid netloc: expected localhost or empty, got {parsed.netloc}")
+        if parsed.params:
+            raise ValueError(f"invalid file URL: params not allowed: {url}")
+        if parsed.query:
+            raise ValueError(f"invalid file URL: query not allowed: {url}")
+        if parsed.fragment:
+            raise ValueError(f"invalid file URL: fragment not allowed: {url}")
         return parsed.path
 
     async def open(self, url: str) -> ReadableStream:
