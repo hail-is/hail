1b6884fb645115f98956565b9d7012f85c6acaa3
Daniel King <dking@broadinstitute.org>
2021-04-23 11:29:34 -0400

    [batch] avoid unnecessary locks
    1. msec_mcpu is not used any more, so don't bother. This causes a conflict with *every other job in
       the batch*, effectively serializing changes to every attempt in the same batch.
    
    2. There is no need to eagerly take update lock on the instances table in add_attempt. A share lock
       prevents other rows from mutating that value while we're looking at it. We do not care if they
       read an old value for free_cores_mcpu between us reading `state` and us updating
       `free_cores_mcpu`.
    
    3. In `schedule_job` there is no need to take an update lock on the `batches` table (which
       effectively serializes all scheduling on the same batch).
    
    4. In `unschedule_job`, I made the formatting match other selects.
    
    5. In `unschedule_job`, same thing about update locks on the `instances` table.
    
    6. In `mark_job_creating`, same thing about update locks on the `batches` table.
    
    7. In `mark_job_started`, same thing about update locks on the `batches` table.
    
    8. In `mark_job_complete`, same thing about update locks on the `instances` table.

--- batch/sql/estimated-current.sql
+++ batch/sql/estimated-current.sql
@@ -360,10 +360,6 @@ BEGIN
 
   SET msec_mcpu_diff = msec_diff * job_cores_mcpu;
 
-  UPDATE batches
-  SET msec_mcpu = batches.msec_mcpu + msec_mcpu_diff
-  WHERE id = NEW.batch_id;
-
   UPDATE jobs
   SET msec_mcpu = jobs.msec_mcpu + msec_mcpu_diff
   WHERE batch_id = NEW.batch_id AND job_id = NEW.job_id;
@@ -899,7 +895,7 @@ BEGIN
 
   IF NOT attempt_exists AND in_attempt_id IS NOT NULL THEN
     INSERT INTO attempts (batch_id, job_id, attempt_id, instance_name) VALUES (in_batch_id, in_job_id, in_attempt_id, in_instance_name);
-    SELECT state INTO cur_instance_state FROM instances WHERE name = in_instance_name FOR UPDATE;
+    SELECT state INTO cur_instance_state FROM instances WHERE name = in_instance_name LOCK IN SHARE MODE;
     # instance pending when attempt is from a job private instance
     IF cur_instance_state = 'pending' OR cur_instance_state = 'active' THEN
       UPDATE instances SET free_cores_mcpu = free_cores_mcpu - in_cores_mcpu WHERE name = in_instance_name;
@@ -926,13 +922,18 @@ BEGIN
 
   START TRANSACTION;
 
-  SELECT jobs.state, cores_mcpu, attempt_id,
-    (jobs.cancelled OR batches.cancelled) AND NOT always_run
-  INTO cur_job_state, cur_cores_mcpu, cur_attempt_id, cur_job_cancel
+  SELECT state, cores_mcpu, attempt_id
+  INTO cur_job_state, cur_cores_mcpu, cur_attempt_id
+  FROM jobs
+  WHERE batch_id = in_batch_id AND job_id = in_job_id
+  FOR UPDATE;
+
+  SELECT (jobs.cancelled OR batches.cancelled) AND NOT jobs.always_run
+  INTO cur_job_cancel
   FROM jobs
   INNER JOIN batches ON batches.id = jobs.batch_id
   WHERE batch_id = in_batch_id AND job_id = in_job_id
-  FOR UPDATE;
+  LOCK IN SHARE MODE;
 
   SELECT is_pool
   INTO cur_instance_is_pool
@@ -990,10 +991,12 @@ BEGIN
 
   SELECT state, cores_mcpu, attempt_id
   INTO cur_job_state, cur_cores_mcpu, cur_attempt_id
-  FROM jobs WHERE batch_id = in_batch_id AND job_id = in_job_id
+  FROM jobs
+  WHERE batch_id = in_batch_id AND job_id = in_job_id
   FOR UPDATE;
 
-  SELECT end_time INTO cur_end_time FROM attempts
+  SELECT end_time INTO cur_end_time
+  FROM attempts
   WHERE batch_id = in_batch_id AND job_id = in_job_id AND attempt_id = in_attempt_id
   FOR UPDATE;
 
@@ -1001,7 +1004,7 @@ BEGIN
   SET end_time = new_end_time, reason = new_reason
   WHERE batch_id = in_batch_id AND job_id = in_job_id AND attempt_id = in_attempt_id;
 
-  SELECT state INTO cur_instance_state FROM instances WHERE name = in_instance_name FOR UPDATE;
+  SELECT state INTO cur_instance_state FROM instances WHERE name = in_instance_name LOCK IN SHARE MODE;
 
   IF cur_instance_state = 'active' AND cur_end_time IS NULL THEN
     UPDATE instances
@@ -1039,13 +1042,18 @@ BEGIN
 
   START TRANSACTION;
 
-  SELECT jobs.state, cores_mcpu,
-    (jobs.cancelled OR batches.cancelled) AND NOT always_run
-  INTO cur_job_state, cur_cores_mcpu, cur_job_cancel
+  SELECT state, cores_mcpu
+  INTO cur_job_state, cur_cores_mcpu
+  FROM jobs
+  WHERE batch_id = in_batch_id AND job_id = in_job_id
+  FOR UPDATE;
+
+  SELECT (jobs.cancelled OR batches.cancelled) AND NOT jobs.always_run
+  INTO cur_job_cancel
   FROM jobs
   INNER JOIN batches ON batches.id = jobs.batch_id
   WHERE batch_id = in_batch_id AND job_id = in_job_id
-  FOR UPDATE;
+  LOCK IN SHARE MODE;
 
   CALL add_attempt(in_batch_id, in_job_id, in_attempt_id, in_instance_name, cur_cores_mcpu, delta_cores_mcpu);
 
@@ -1062,6 +1070,7 @@ BEGIN
   SELECT 0 as rc, delta_cores_mcpu;
 END $$
 
+DROP PROCEDURE IF EXISTS mark_job_started $$
 CREATE PROCEDURE mark_job_started(
   IN in_batch_id BIGINT,
   IN in_job_id INT,
@@ -1078,13 +1087,18 @@ BEGIN
 
   START TRANSACTION;
 
-  SELECT jobs.state, cores_mcpu,
-    (jobs.cancelled OR batches.cancelled) AND NOT always_run
-  INTO cur_job_state, cur_cores_mcpu, cur_job_cancel
+  SELECT state, cores_mcpu
+  INTO cur_job_state, cur_cores_mcpu
+  FROM jobs
+  WHERE batch_id = in_batch_id AND job_id = in_job_id
+  FOR UPDATE;
+
+  SELECT (jobs.cancelled OR batches.cancelled) AND NOT jobs.always_run
+  INTO cur_job_cancel
   FROM jobs
   INNER JOIN batches ON batches.id = jobs.batch_id
   WHERE batch_id = in_batch_id AND job_id = in_job_id
-  FOR UPDATE;
+  LOCK IN SHARE MODE;
 
   CALL add_attempt(in_batch_id, in_job_id, in_attempt_id, in_instance_name, cur_cores_mcpu, delta_cores_mcpu);
 
@@ -1140,7 +1154,7 @@ BEGIN
   SET start_time = new_start_time, end_time = new_end_time, reason = new_reason
   WHERE batch_id = in_batch_id AND job_id = in_job_id AND attempt_id = in_attempt_id;
 
-  SELECT state INTO cur_instance_state FROM instances WHERE name = in_instance_name FOR UPDATE;
+  SELECT state INTO cur_instance_state FROM instances WHERE name = in_instance_name LOCK IN SHARE MODE;
   IF cur_instance_state = 'active' AND cur_end_time IS NULL THEN
     UPDATE instances
     SET free_cores_mcpu = free_cores_mcpu + cur_cores_mcpu
