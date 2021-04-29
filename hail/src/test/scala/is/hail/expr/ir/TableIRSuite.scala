9172c71d1a8cdafe971b89c756c175ec0ce0c259
Tim Poterba <tpoterba@gmail.com>
2021-04-29 08:51:36 -0400

    add back all joins

--- hail/src/test/scala/is/hail/expr/ir/TableIRSuite.scala
+++ hail/src/test/scala/is/hail/expr/ir/TableIRSuite.scala
@@ -433,28 +433,25 @@ class TableIRSuite extends HailSuite {
   def joinData(): Array[Array[Any]] = {
     val defaultLParts = 2
     val defaultRParts = 2
-    val (defaultJ, defaultP) = joinTypes(0)
     val defaultLeftProject = Set(1, 2)
     val defaultRightProject = Set(1, 2)
 
     val ab = new BoxedArrayBuilder[Array[Any]]()
-    for {
-      lParts <- Array[Integer](1, 2, 3); rParts <- Array[Integer](1, 2, 3)
-    } {
-
-      ab += Array[Any](lParts, rParts, defaultJ, defaultP, defaultLeftProject, defaultRightProject)
-    }
-
     for ((j, p) <- joinTypes) {
-      ab += Array[Any](defaultLParts, defaultRParts, j, p, defaultLeftProject, defaultRightProject)
-    }
+      for {
+        lParts <- Array[Integer](1, 2, 3); rParts <- Array[Integer](1, 2, 3)
+      } {
 
-    for {
-      leftProject <- Seq[Set[Int]](Set(), Set(1), Set(2), Set(1, 2))
-        rightProject <- Seq[Set[Int]](Set(), Set(1), Set(2), Set(1, 2))
-        if !leftProject.contains(1) || rightProject.contains(1)
-    } {
-      ab += Array[Any](defaultLParts, defaultRParts, defaultJ, defaultP, leftProject, rightProject)
+        ab += Array[Any](lParts, rParts, j, p, defaultLeftProject, defaultRightProject)
+      }
+
+      for {
+        leftProject <- Seq[Set[Int]](Set(), Set(1), Set(2), Set(1, 2))
+          rightProject <- Seq[Set[Int]](Set(), Set(1), Set(2), Set(1, 2))
+          if !leftProject.contains(1) || rightProject.contains(1)
+      } {
+        ab += Array[Any](defaultLParts, defaultRParts, j, p, leftProject, rightProject)
+      }
     }
     ab.result()
   }
