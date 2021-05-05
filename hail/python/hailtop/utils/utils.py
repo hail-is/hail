8c0e3bd72ffc4c518d6abf5d3cd47b439bd728ef
Daniel King <dking@broadinstitute.org>
2021-05-04 20:26:02 -0400

    [utils] YATE: Yet Another Transient Error

--- hail/python/hailtop/utils/utils.py
+++ hail/python/hailtop/utils/utils.py
@@ -550,6 +550,9 @@ def is_transient_error(e):
         return True
     if isinstance(e, aiohttp.client_exceptions.ClientConnectorError):
         return hasattr(e, 'os_error') and is_transient_error(e.os_error)
+    if isinstance(e, aiohttp.ClientOSError):
+        # aiohttp/client_reqrep.py wraps all OSError instances with a ClientOSError
+        return is_transient_error(e.__cause__)
     # appears to happen when the connection is lost prematurely, see:
     # https://github.com/aio-libs/aiohttp/issues/4581
     # https://github.com/aio-libs/aiohttp/blob/v3.7.4/aiohttp/client_proto.py#L85
