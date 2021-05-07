f3b39bd4567e37468f26e90be74bdf63f67eb45f
Daniel Goldstein <danielgold95@gmail.com>
2021-05-07 18:37:58 -0400

    [batch] remove unused test utility

--- batch/test/test_batch.py
+++ batch/test/test_batch.py
@@ -22,19 +22,6 @@ DOCKER_ROOT_IMAGE = os.environ.get('DOCKER_ROOT_IMAGE', 'gcr.io/hail-vdc/ubuntu:
 SCOPE = os.environ.get('HAIL_SCOPE', 'test')
 
 
-def poll_until(p, max_polls=None):
-    i = 0
-    while True and (max_polls is None or i < max_polls):
-        x = p()
-        if x:
-            return x
-        # max 4.5s
-        j = random.randrange(math.floor(1.1 ** min(i, 40)))
-        time.sleep(0.100 * j)
-        i = i + 1
-    raise ValueError(f'poll_until: exceeded max polls: {i} {max_polls}')
-
-
 @pytest.fixture
 def client():
     client = BatchClient('test')
