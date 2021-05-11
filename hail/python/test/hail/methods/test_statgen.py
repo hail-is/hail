0648a6f5e4c2d7f93bf658adf4e1f3414fefa6ee
John Compitello <j301600@gmail.com>
2021-05-11 10:00:43 -0400

    We don't have a lowered bgen importer, so these tests don't pass yet

--- hail/python/test/hail/methods/test_statgen.py
+++ hail/python/test/hail/methods/test_statgen.py
@@ -552,6 +552,8 @@ class Tests(unittest.TestCase):
             self.assertTrue(is_constant(results[9]))
             self.assertTrue(is_constant(results[10]))
 
+    @fails_service_backend()
+    @fails_local_backend()
     def test_logistic_regression_wald_test_multi_pheno_bgen_dosage(self):
         covariates = hl.import_table(resource('regressionLogisticMultiPheno.cov'),
                                      key='Sample',
@@ -625,6 +627,8 @@ class Tests(unittest.TestCase):
             self.assertTrue(is_constant(results[9]))
             self.assertTrue(is_constant(results[10]))
 
+    @fails_service_backend()
+    @fails_local_backend()
     def test_logistic_regression_wald_dosage(self):
         covariates = hl.import_table(resource('regressionLogistic.cov'),
                                      key='Sample',
