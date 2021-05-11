13c1406e522141c846608ccd82a20f26de7d716d
John Compitello <j301600@gmail.com>
2021-05-11 10:43:39 -0400

    Last style fix

--- hail/python/hail/methods/statgen.py
+++ hail/python/hail/methods/statgen.py
@@ -838,7 +838,7 @@ def logreg_fit(X, y, null_fit=None, max_iter=25, tol=1E-6):
     m = X.shape[1]
 
     if null_fit is None:
-        b = hl.nd.hstack([hl.nd.array([hl.log(y.sum() / n)]),  hl.nd.zeros((hl.int32(m - 1)))])
+        b = hl.nd.hstack([hl.nd.array([hl.log(y.sum() / n)]), hl.nd.zeros((hl.int32(m - 1)))])
         mu = sigmoid(X @ b)
         score = X.T @ (y - mu)
         # Reshape so we do a rowwise multiply
