2630dd23b30b8ce8184471ef1e8c021040643e27
John Compitello <j301600@gmail.com>
2021-05-14 17:02:29 -0400

    All slice tests passing, correctly handling scaling by steps

--- hail/python/test/hail/linalg/test_linalg.py
+++ hail/python/test/hail/linalg/test_linalg.py
@@ -606,7 +606,6 @@ class Tests(unittest.TestCase):
             (slice(4, None), slice(4, None)),
             (slice(None, None), slice(None, None))
         ]:
-            print(f"Trying slice {indices}")
             self._assert_eq(bm[indices], nd[indices])
             self._assert_eq(bm[indices][:, :2], nd[indices][:, :2])
             self._assert_eq(bm[indices][:2, :], nd[indices][:2, :])
