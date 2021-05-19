5e24ba876b18570ece3b59641e450accc58ca3c9
John Compitello <j301600@gmail.com>
2021-05-19 09:42:55 -0400

    Fixed loop generation, got started on map2 and concat

--- hail/src/main/scala/is/hail/expr/ir/ndarrays/EmitNDArray.scala
+++ hail/src/main/scala/is/hail/expr/ir/ndarrays/EmitNDArray.scala
@@ -48,6 +48,51 @@ object EmitNDArray {
             }
           }
         }
+        case NDArrayMap2(lChild, rChild, lName, rName, body) => {
+          deforest(lChild).flatMap(cb) { leftProducer =>
+            deforest(rChild).map(cb) { rightProducer =>
+              val leftShapeValues = leftProducer.shape
+              val rightShapeValues = rightProducer.shape
+
+              val (newSetupShape, shapeArray) = NDArrayEmitter.unifyShapes2(cb.emb, leftShapeValues, rightShapeValues)
+
+              val lElemRef = cb.emb.newPresentEmitField(lName, leftProducer.elementType)
+              val rElemRef = cb.emb.newPresentEmitField(rName, rightProducer.elementType)
+              val bodyEnv = env.bind(lName, lElemRef)
+                .bind(rName, rElemRef)
+              val bodyEC = EmitCode.fromI(cb.emb)(cb => emitI(body, cb, env = bodyEnv))
+
+              new NDArrayProducer {
+                override def elementType: SType = leftProducer.elementType
+
+                override val shape: IndexedSeq[Value[Long]] = shapeArray
+                override val initAll: EmitCodeBuilder => Unit = {
+                  cb => {
+                    leftProducer.initAll(cb)
+                    rightProducer.initAll(cb)
+                    cb.append(newSetupShape)
+                  }
+                }
+                override val initAxis: IndexedSeq[EmitCodeBuilder => Unit] = shape.indices.map { idx =>  { cb: EmitCodeBuilder  =>
+                  leftProducer.initAxis(idx)(cb)
+                  rightProducer.initAxis(idx)(cb)
+                }}
+                override val stepAxis: IndexedSeq[(EmitCodeBuilder, Value[Long]) => Unit] = shape.indices.map { idx => { (cb: EmitCodeBuilder, axis: Value[Long]) =>
+                  // FIXME: Add rules for broadcasting so you don't step out of bounds
+                  leftProducer.stepAxis(idx)(cb, axis)
+                  rightProducer.stepAxis(idx)(cb, axis)
+                }}
+
+                override def loadElementAtCurrentAddr(cb: EmitCodeBuilder): SCode = {
+                  cb.assign(lElemRef, leftProducer.loadElementAtCurrentAddr(cb).toPCode(cb, region))
+                  cb.assign(rElemRef, rightProducer.loadElementAtCurrentAddr(cb).toPCode(cb, region))
+
+                  bodyEC.toI(cb).get(cb, "NDArrayMap2 body cannot be missing")
+                }
+              }
+            }
+          }
+        }
         case NDArrayReindex(child, indexExpr) =>
           deforest(child).map(cb) { childProducer =>
 
@@ -67,34 +112,66 @@ object EmitNDArray {
               override def loadElementAtCurrentAddr(cb: EmitCodeBuilder): SCode = childProducer.loadElementAtCurrentAddr(cb)
             }
           }
+        case NDArrayConcat(nds, axis) =>
+          emitI(nds, cb).flatMap(cb) { ndsPCode =>
+            val ndsArrayPValue = ndsPCode.asIndexable.memoize(cb, "ndarray_concat_array_of_nds")
+            val arrLength = ndsArrayPValue.loadLength()
+            cb.ifx(arrLength ceq 0, {
+              cb._fatal("need at least one ndarray to concatenate")
+            })
+
+            val missing: Code[Boolean] = {
+              if (ndsArrayPValue.st.elementEmitType.required)
+                const(false)
+              else {
+                val missing = cb.newLocal[Boolean]("ndarray_concat_result_missing")
+                cb.assign(missing, false)
+                // Need to check if the any of the ndarrays are missing.
+                val missingCheckLoopIdx = cb.newLocal[Int]("ndarray_concat_missing_check_idx")
+                cb.forLoop(cb.assign(missingCheckLoopIdx, 0), missingCheckLoopIdx < arrLength, cb.assign(missingCheckLoopIdx, missingCheckLoopIdx + 1),
+                  cb.assign(missing, missing | ndsArrayPValue.isElementMissing(missingCheckLoopIdx))
+                )
+                missing
+              }
+            }
+
+            IEmitCode(cb, missing,  {
+              ???
+            })
+          }
         case _ => {
           val ndI = emitI(x, cb)
           ndI.map(cb) { ndPCode =>
             val ndPv = ndPCode.asNDArray.memoize(cb, "deforestNDArray_fall_through_ndarray")
             val ndPvShape = ndPv.shapes(cb)
             val strides = ndPv.strides(cb)
+            val counters = ndPvShape.indices.map(i => cb.newLocal[Long](s"ndarray_produceer_fall_through_idx_${i}"))
 
             new NDArrayProducer {
               override def elementType: SType = ndPv.st.elementType
 
-              val curOffset = cb.newLocal[Long]("ndarray_producer_fall_through_curr_addr")
 
               override val shape: IndexedSeq[Value[Long]] = ndPvShape
-              override val initAll: EmitCodeBuilder => Unit = cb => cb.assign(curOffset, 0L)
+              override val initAll: EmitCodeBuilder => Unit = cb => {
+                counters.foreach(ctr => cb.assign(ctr, 0L))
+              }
               override val initAxis: IndexedSeq[EmitCodeBuilder => Unit] = {
-                shape.indices.map(i => (cb: EmitCodeBuilder) => cb.assign(curOffset, curOffset % strides(i)))
+                shape.indices.map(i => (cb: EmitCodeBuilder) => {
+                  cb.assign(counters(i), 0L)
+                })
               }
               override val stepAxis: IndexedSeq[(EmitCodeBuilder, Value[Long]) => Unit] = {
                 shape.indices.map{ i =>
                   (cb: EmitCodeBuilder, step: Value[Long]) => {
-                    cb.assign(curOffset, curOffset + strides(i) * step)
+                    cb.assign(counters(i), counters(i) + strides(i))
                   }
                 }
               }
 
               override def loadElementAtCurrentAddr(cb: EmitCodeBuilder): SCode = {
+                val offset = counters.foldLeft[Code[Long]](const(0L)){ (a, b) => a + b}
                 // TODO: Safe to canonicalPType here?
-                elementType.loadFrom(cb, region, ndPv.st.elementType.canonicalPType(), ndPv.firstDataAddress(cb) + curOffset)
+                elementType.loadFrom(cb, region, ndPv.st.elementType.canonicalPType(), ndPv.firstDataAddress(cb) + offset)
               }
             }
           }
@@ -137,6 +214,7 @@ abstract class NDArrayProducer {
           () => {
             cb.forLoop({
               initAxis(dimIdx)(cb)
+              cb.assign(dimVar, 0L)
             }, dimVar < shape(dimIdx), {
               stepAxis(dimIdx)(cb, 1L)
               cb.assign(dimVar, dimVar + 1L)
