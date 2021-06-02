fda3f4f25d30eaa3fb7935e967f773c7a2a05ff5
Daniel Goldstein <danielgold95@gmail.com>
2021-06-02 15:58:12 -0400

    remove unused file

--- batch2/svelte-batch/src/batch-client.ts
+++
@@ -1,9 +0,0 @@
-import type { GetJobsResult, GetBatchesResult } from '../../../js_common/src/batch-client'
-
-export function getJobsStore(id: number): Writable<Maybe<GetJobsResult>> {
-  return streamingApiStore<GetJobsResult>(`/api/v1alpha/batches/${id}/jobs`);
-}
-
-export function getBatchesStore(): Writable<Maybe<GetBatchesResult>> {
-  return streamingApiStore<GetBatchesResult>('/api/v1alpha/batches');
-}
