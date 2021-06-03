149785e735cd54e093bd3c5295c0b34e415f734d
Tim Poterba <tpoterba@gmail.com>
2021-06-03 17:09:00 -0400

    delete compilationUnitSize, use flag for method size limit

--- hail/src/main/scala/is/hail/expr/ir/analyses/ComputeMethodSplits.scala
+++ hail/src/main/scala/is/hail/expr/ir/analyses/ComputeMethodSplits.scala
@@ -1,12 +1,14 @@
 package is.hail.expr.ir.analyses
 
+import is.hail.HailContext
 import is.hail.expr.ir._
 
 object ComputeMethodSplits {
-  val splitThreshold: Int = 16
-
   def apply(ir: IR, controlFlowPreventsSplit: Memo[Unit]): Memo[Unit] = {
     val m = Memo.empty[Unit]
+
+    val splitThreshold = HailContext.getFlag("method_split_ir_limit").toInt
+    require(splitThreshold > 0, s"invalid method_split_ir_limit")
 
     def recurAndComputeSizeUnderneath(x: IR): Int = {
       val sizeUnderneath = x.children.iterator.map { case child: IR => recurAndComputeSizeUnderneath(child) }.sum
