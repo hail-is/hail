5f6a55923b0be0786c49f2e487dc19b160f18116
ammekk <emma.kelminson001@umb.edu>
2021-06-10 17:24:16 -0400

    constant folding-6 test running

--- hail/src/test/scala/is/hail/expr/ir/FoldConstantsSuite.scala
+++ hail/src/test/scala/is/hail/expr/ir/FoldConstantsSuite.scala
@@ -1,7 +1,9 @@
 package is.hail.expr.ir
 
 import is.hail.HailSuite
-import is.hail.types.virtual.{TFloat64, TInt32, TTuple}
+import is.hail.TestUtils.assertEvalsTo
+import is.hail.types.virtual.{TArray, TFloat64, TInt32, TTuple}
+import is.hail.utils.{FastIndexedSeq, FastSeq}
 import org.apache.spark.sql.Row
 import org.scalatest.testng.TestNGSuite
 import org.testng.annotations.{DataProvider, Test}
@@ -37,13 +39,29 @@ class FoldConstantsSuite extends HailSuite {
     val i4 = I32(4)
     val i8 = I32(8)
     val ref = "x"
+    val vRef = "y"
     val range = StreamRange(0, 10, 1)
-
+    val refIR = Ref(ref, TInt32)
+    val vRefIR = Ref(vRef, TInt32)
+    val streamIRA = ToStream(Literal(TArray(TInt32), FastIndexedSeq(12, 2, 4, refIR)))
+    val streamIRB = ToStream(Literal(TArray(TInt32), FastIndexedSeq(1, vRefIR, 2, 4)))
+    val behavior = ArrayZipBehavior.TakeMinLength
+    val toZip = IndexedSeq(streamIRA, streamIRB)
+    val zipNames = IndexedSeq("A", "B")
     val oppIR = ApplyBinaryPrimOp(Add(), i4, i8)
-    val refOppIR = ApplyBinaryPrimOp(Add(), Ref(ref, TInt32), i8)
+    val refOppIR = ApplyBinaryPrimOp(Add(), refIR, i8)
+
+    val streamFoldBodyIR = ApplyBinaryPrimOp(Add(), vRefIR, refOppIR)
     val letIR = Let(ref, i4, refOppIR)
     val streamMapIR = StreamMap(range, ref, refOppIR)
-
+    val streamFoldIR = StreamFold(range, refIR, ref, vRef, streamFoldBodyIR)
+    val streamFoldLetIR = Let(ref, i4, streamFoldIR)
+    val letZipIR = Let(ref, i8, Let(vRef, i4, StreamZip(toZip, zipNames, True(), behavior)))
+    val streamFold2IR = Let(ref, i8, Let(vRef, i4, StreamFold2(streamIRA,
+                        FastIndexedSeq((ref, I32(0)), (vRef, I32(8))),
+                        "val", FastIndexedSeq(Ref("val", TInt32) + Ref(ref, TInt32),
+                        Coalesce(FastSeq(Ref(vRef, TInt32), Ref("val", TInt32)))),
+                        MakeStruct(FastSeq((ref, Ref(ref, TInt32)), (vRef, Ref(vRef, TInt32)))))))
 
     val oppTest = FoldConstants.findConstantSubTrees(oppIR)
     assert(oppTest.contains(oppIR))
@@ -53,5 +71,14 @@ class FoldConstantsSuite extends HailSuite {
 
     val streamMapTest = FoldConstants.findConstantSubTrees(streamMapIR)
     assert(streamMapTest.contains(streamMapIR))
+
+    val streamFoldTest = FoldConstants.findConstantSubTrees(streamFoldLetIR)
+    assert(streamFoldTest.contains(streamFoldLetIR))
+
+    val zipTest = FoldConstants.findConstantSubTrees(letZipIR)
+    assert(zipTest.contains(letZipIR))
+
+    val streamfold2Test = FoldConstants.findConstantSubTrees(streamFold2IR)
+    assert(streamfold2Test.contains(streamFold2IR))
   }
 }
