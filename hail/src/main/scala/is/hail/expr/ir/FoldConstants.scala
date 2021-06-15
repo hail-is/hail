e55e4cfbda8cc32db2ffe2e86defab42b2a4cb9b
ammekk <emma.kelminson001@umb.edu>
2021-06-15 16:03:54 -0400

    compiled constants simplified approach

--- hail/src/main/scala/is/hail/expr/ir/FoldConstants.scala
+++ hail/src/main/scala/is/hail/expr/ir/FoldConstants.scala
@@ -15,11 +15,12 @@ object FoldConstants {
     }
 
   def mainMethod(ctx: ExecuteContext, ir : BaseIR): Row = {
+    println(Pretty(ir))
     val constantSubTrees = Memo.empty[Unit]
     val usesAndDefs = ComputeUsesAndDefs(ir)
-    findConstantSubTreesHelper(ir, constantSubTrees, usesAndDefs)
+    val constantRefs = Set[String]()
+    visitIR(ir, constantRefs, constantSubTrees)
     val parents = ParentPointers(ir)
-    removeUnrealizableConstants(ir, constantSubTrees, parents, usesAndDefs)
     val constants = ArrayBuffer[IR]()
     val bindings = ArrayBuffer[(String,IR)]()
     getConstantIRsAndRefs(ir, constantSubTrees, constants, bindings)
@@ -73,125 +74,57 @@ object FoldConstants {
 
   }
 
-  def findConstantSubTrees(baseIR: BaseIR): Memo[Unit] = {
-    val constantSubTrees = Memo.empty[Unit]
-    val usesAndDefs = ComputeUsesAndDefs(baseIR)
-    findConstantSubTreesHelper(baseIR, constantSubTrees, usesAndDefs)
-    constantSubTrees
-  }
-  def findConstantSubTreesHelper(ir: BaseIR, memo: Memo[Unit], usesAndDefs: UsesAndDefs): Unit = {
-    def recur(ir: BaseIR): Unit = findConstantSubTreesHelper(ir, memo, usesAndDefs)
-    def bindRefRecur(args: IndexedSeq[IR], body: IR, names : Option[IndexedSeq[String]] = None): Unit = {
-      args.foreach( arg => recur(arg))
-      if (names.isEmpty) bindRefs()
-      else {
-        val refNames = names.get
-        stringIRSeqBind(refNames.zip(args))
-      }
-      recur(body)
-    }
-    def bindRefs(name: Option[String] = None): Unit = {
+  def visitIR(baseIR: BaseIR, constantRefs: Set[String], memo: Memo[Unit]): Option[Set[String]] = {
 
-      val refs = if (!name.isEmpty)  usesAndDefs.uses(ir).filter(ref => ref.t.name == name.get)
-                 else usesAndDefs.uses(ir)
-      refs.foreach(ref => memo.bind(ref, ()))
-    }
-    def stringIRSeqBind(seq: IndexedSeq[(String, IR)]): Unit = {
-     seq.foreach { case (name, streamIr) => if (memo.contains(streamIr)) bindRefs(Some(name))}
-   }
-
-     if (IsConstant(ir)) {
-       memo.bind(ir, ())
-     }
-    else if (ir.isInstanceOf[Ref]) {}
-    else {
-      ir match {
-        case Let(name, value, body) => bindRefRecur(IndexedSeq(value), body)
-        case StreamMap(a, name, body) => bindRefRecur(IndexedSeq(a), body)
-        case StreamZip(as, names, body, _) => bindRefRecur(as, body, Some(names))
-        case StreamZipJoin(as, key, curKey, curVals, body) => {
-          as.foreach(seq => recur(seq))
-          val allConstant = as.forall(streamIR => memo.contains(streamIR))
-          if (allConstant) bindRefs()
-          recur(body)
-        }
-        case StreamFlatMap(a, name, body) => bindRefRecur(IndexedSeq(a), body)
-        case StreamFilter(a, name, body) => bindRefRecur(IndexedSeq(a), body)
-        case StreamFold(a, zero, accumName, valueName, body) =>
-          bindRefRecur(IndexedSeq(a, zero), body, Some(IndexedSeq(valueName, accumName)))
-        case StreamFold2(a, accum, valueName, seq, result) => {
-          recur(a)
-          accum.map(item => item._2).foreach(streamIr => recur(streamIr))
-          if(memo.contains(a)) bindRefs(Some(valueName))
-          stringIRSeqBind(accum)
-          seq.foreach(seqIR => recur(seqIR))
-          recur(result)   //prob not right
-        }
-        case RunAggScan(a, name, _, _, _, _) => ???
-        case StreamScan(a, zero, valueName, accumName, body) =>
-          bindRefRecur(IndexedSeq(a, zero), body, Some(IndexedSeq(valueName, accumName)))
-        case StreamAggScan(a, name, body) => bindRefRecur(IndexedSeq(a), body)
-        case StreamJoinRightDistinct(ll, rr, lkeys, rkeys, l, r, body, _) =>
-          bindRefRecur(IndexedSeq(ll, rr), body)
-        case ArraySort(a, left, right, body) => bindRefRecur(IndexedSeq(a), body)
-        case AggArrayPerElement(a, element, indexName, aggBody, knownLength, _) => ???
-        case NDArrayMap(nd, name, body) => bindRefRecur(IndexedSeq(nd), body)
-        case NDArrayMap2(l, r, lName, rName, body) =>
-          bindRefRecur(IndexedSeq(l, r), body, Some(IndexedSeq(lName, rName)))
-        case _ =>
-          ir.children.foreach(child => {
-            findConstantSubTreesHelper(child, memo, usesAndDefs)
-          })
+    baseIR match {
+      case Ref(name, _) => {
+        if (constantRefs.contains(name)) return Some(Set())
+        else return Some(Set(name))
       }
-      val isConstantSubtree = ir.children.forall(child => {
-        memo.contains(child)
-      }) && !badIRs(ir) && ir.isInstanceOf[IR]
-      if (isConstantSubtree) {
-        memo.bind(ir, ())
+      case let@Let(name, value, body) => {
+        val valueDeps = visitIR(value, constantRefs, memo)
+        if (!valueDeps.isEmpty) {
+          val bodyConstantRefs = if (valueDeps.get.isEmpty) {
+            constantRefs + name
+          }
+          else constantRefs
+          val bodyDeps = visitIR(body, bodyConstantRefs, memo)
+          val nodeDeps = bodyDeps.map(bD => valueDeps.get ++ (bD - name))
+          nodeDeps.map(nD => if (nD.isEmpty) memo.bind(let, ()))
+          return nodeDeps
+        }
+        return valueDeps
+      }
+      case _ => {
+        val childrenDeps = baseIR.children.map { child => visitIR(child, constantRefs, memo) }
+        val allConstantChildren = childrenDeps.forall(child => !child.isEmpty)
+        if (!allConstantChildren) return None
+        val constChildrenDeps = childrenDeps.map(child => child.get)
+        val nodeDeps = baseIR.children.zip(constChildrenDeps).zipWithIndex.map { case ((child, childDep), index) =>
+          childDep -- Bindings.apply(baseIR, index).map(ref => ref._1)
+        }.foldLeft(Set[String]())((accum, elem) => elem ++ accum)
+        baseIR match {
+          case ir: IR =>
+            if (nodeDeps.isEmpty && ir.typ.isRealizable && !badIRs(ir)) {
+              memo.bind(ir, ())
+              return Some(nodeDeps)
+            }
+            else if (!badIRs(ir))
+              return Some(nodeDeps)
+            else return None
+
+          case _ => None
+        }
       }
     }
   }
+
+
 
   def badIRs(baseIR: BaseIR): Boolean = {
     baseIR.isInstanceOf[ApplySeeded] || baseIR.isInstanceOf[UUID4] || baseIR.isInstanceOf[In]||
       baseIR.isInstanceOf[TailLoop] || baseIR.typ == TVoid
   }
-
-//  def removeUnrealizableConstants(ir: BaseIR, constantSubTrees: Memo[Unit], usesAndDefs: UsesAndDefs) : Unit = {
-//    val parents = ParentPointers(ir)
-//    removeUnrealizableConstants(ir, constantSubTrees, parents, usesAndDefs)
-//
-//  }
-
-  def removeUnrealizableConstants(ir : BaseIR, constantSubTrees: Memo[Unit], parents: Memo[BaseIR],
-                                  usesAndDefs: UsesAndDefs): Unit = {
-    if (!constantSubTrees.contains(ir)) {
-      ir.children.foreach(child => removeUnrealizableConstants(child, constantSubTrees,
-        ParentPointers(child), usesAndDefs))
-    }
-    else {
-      ir match {
-        case ir : IR =>
-          if (!ir.typ.isRealizable) {
-            constantSubTrees.delete(ir)
-            usesAndDefs.uses(ir).foreach(ref => {
-              var current: BaseIR = ref.t
-              while (constantSubTrees.contains(current)) {
-                constantSubTrees.delete(ref)
-                current = parents.get(current).getOrElse(current)
-              }
-            })
-          }
-      }
-    }
-  }
-//  def getConstantIRsAndRefs(ir: BaseIR, constantSubTrees: Memo[Unit]): (IndexedSeq[IR], IndexedSeq[(String, IR)]) = {
-//    val constants = ArrayBuffer[IR]()
-//    val refs = ArrayBuffer[(String,IR)]()
-//    val results = getConstantIRsAndRefsHelper(ir, constantSubTrees, constants, refs)
-//    (constants.toIndexedSeq , refs.toIndexedSeq)
-//
-//  }
 
   def getConstantIRsAndRefs(ir: BaseIR, constantSubTrees: Memo[Unit], constants : ArrayBuffer[IR],
                                   refs : ArrayBuffer[(String,IR)]) : Unit  = {
@@ -206,7 +139,6 @@ object FoldConstants {
         else getConstantIRsAndRefs(value, constantSubTrees, constants, refs)
         getConstantIRsAndRefs(body, constantSubTrees, constants, refs)
       }
-
       case _ => ir.children.foreach(child => getConstantIRsAndRefs(child, constantSubTrees, constants, refs))
     }
   }
