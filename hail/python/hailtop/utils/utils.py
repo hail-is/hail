c471df720d28441d3d358e893248dc081531e855
Jackie Goldstein <jigold@broadinstitute.org>
2021-06-28 11:04:39 -0400

    delint?

--- hail/python/hailtop/utils/utils.py
+++ hail/python/hailtop/utils/utils.py
@@ -559,8 +559,8 @@ def is_transient_error(e):
         # 503 service unavailable, 504 gateway timeout
         return True
     if isinstance(e, hailtop.httpx.ClientResponseError) and (
-            e.status == 403 and ('rateLimitExceeded' in e.body or
-                                 'QUOTA_EXCEEDED' in e.body)):
+            e.status == 403 and ('rateLimitExceeded' in e.body
+                                 or 'QUOTA_EXCEEDED' in e.body)):
         return True
     if isinstance(e, aiohttp.ServerTimeoutError):
         return True
