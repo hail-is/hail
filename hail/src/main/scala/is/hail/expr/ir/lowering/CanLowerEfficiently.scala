86bc77f5254765d89762b717cbf3807fece02ec4
Tim Poterba <tpoterba@gmail.com>
2021-07-23 12:16:35 -0400

    Add tree agg, remove keyby

--- hail/src/main/scala/is/hail/expr/ir/lowering/CanLowerEfficiently.scala
+++ hail/src/main/scala/is/hail/expr/ir/lowering/CanLowerEfficiently.scala
@@ -1,5 +1,6 @@
 package is.hail.expr.ir.lowering
 
+import is.hail.HailContext
 import is.hail.expr.ir._
 import is.hail.expr.ir.functions.TableToValueFunction
 import is.hail.io.TextMatrixReader
@@ -10,6 +11,9 @@ import is.hail.methods.{ForceCountTable, NPartitionsTable}
 
 object CanLowerEfficiently {
   def apply(ir0: BaseIR): Option[String] = {
+
+    if (HailContext.getFlag("no_whole_stage_codegen") != null)
+      return Some("flag 'no_whole_stage_codegen' is enabled")
 
     var prohibitiveReason: Option[String] = None
 
@@ -37,6 +41,9 @@ object CanLowerEfficiently {
         case t: TableParallelize =>
         case t: TableRange =>
         case TableKeyBy(child, keys, isSorted) =>
+          if (!(isSorted || child.typ.key.startsWith(keys)))
+            fail(s" lowered spark shuffles immediately persist to disk, which makes some benchmarks slower (but more resilient)")
+
           val doesStrangeDNDArrayOneToOneRekey = isSorted &&
             child.typ.key.zip(keys).takeWhile { case (l, r) => l == r }.isEmpty
           if (doesStrangeDNDArrayOneToOneRekey)
@@ -61,6 +68,8 @@ object CanLowerEfficiently {
         case t: TableKeyByAndAggregate => fail("TableKeyByAndAggregate has no map-side combine")
         case t: TableAggregateByKey =>
         case t: TableOrderBy =>
+          if (!(t.sortFields.forall(_.sortOrder == Ascending) && t.child.typ.key.startsWith(t.sortFields.map(_.field))))
+            fail(s" lowered spark shuffles immediately persist to disk, which makes some benchmarks slower (but more resilient)")
         case t: TableRename =>
         case t: TableFilterIntervals => fail(s"TableFilterIntervals does a linear scan")
         case t: TableToTableApply => fail(s"TableToTableApply")
@@ -79,7 +88,7 @@ object CanLowerEfficiently {
         case TableToValueApply(_, ForceCountTable()) =>
         case TableToValueApply(_, NPartitionsTable()) =>
         case TableToValueApply(_, f: TableToValueFunction) => fail(s"TableToValueApply: no lowering for ${ f.getClass.getName }")
-        case TableAggregate(_, _) => fail("TableAggregate needs a tree aggregate implementation to scale")
+        case TableAggregate(_, _) =>
         case TableCollect(_) =>
         case TableGetGlobals(_) =>
 
