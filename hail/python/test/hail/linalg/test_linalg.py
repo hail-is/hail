Fixed failing type calculation

hail/python/test/hail/linalg/test_linalg.py
@@ -481,8 +481,6 @@ class Tests(unittest.TestCase):
         self._assert_eq((m @ m.T).diagonal(), np.array([[14.0, 77.0]]))
 
 
-    @fails_service_backend()
-    @fails_local_backend()
     def test_matrix_sums(self):
         nm = np.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
         m = BlockMatrix.from_ndarray(hl.nd.array(nm), block_size=2)
@@ -532,7 +530,6 @@ class Tests(unittest.TestCase):
                 self._assert_eq(bm_fifty_by_sixty.tree_matmul(bm_fifty_by_sixty.T, splits=split_size), fifty_by_sixty @ fifty_by_sixty.T)
                 self._assert_eq(bm_fifty_by_sixty.tree_matmul(bm_sixty_by_twenty_five, splits=split_size), fifty_by_sixty @ sixty_by_twenty_five)
 
-
     def test_fill(self):
         nd = np.ones((3, 5))
         bm = BlockMatrix.fill(3, 5, 1.0)
@@ -550,7 +547,7 @@ class Tests(unittest.TestCase):
 
         self.assert_sums_agree(bm, nd)
 
-    @skip_unless_spark_backend()
+    @fails_local_backend
     def test_sum_with_sparsify(self):
         nd = np.zeros(shape=(5, 7))
         nd[2, 4] = 1.0