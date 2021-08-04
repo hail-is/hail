88f5ae7080f65338915c180d4b4c1cbb3a35d3e6
ammekk <emma.kelminson001@umb.edu>
2021-08-04 17:22:07 -0400

    added iterator

--- hail/src/main/scala/is/hail/expr/ir/StringTableReader.scala
+++ hail/src/main/scala/is/hail/expr/ir/StringTableReader.scala
@@ -1,13 +1,18 @@
 package is.hail.expr.ir
 import is.hail.annotations.{Region, RegionValueBuilder}
+import is.hail.asm4s.{Code, CodeLabel, Settable, Value}
 import is.hail.expr.TableAnnotationImpex
 import is.hail.expr.ir.StringTableReader.getFileStatuses
+import is.hail.expr.ir.functions.StringFunctions
 import is.hail.expr.ir.lowering.TableStage
+import is.hail.expr.ir.streams.StreamProducer
 import is.hail.io.fs.{FS, FileStatus}
 import is.hail.rvd.RVDPartitioner
-import is.hail.types.TableType
+import is.hail.types.physical.stypes.concrete.{SJavaString, SStackStruct}
+import is.hail.types.physical.stypes.interfaces.SStreamCode
+import is.hail.types.{BaseTypeWithRequiredness, RStruct, TableType, TypeWithRequiredness}
 import is.hail.types.physical.{PCanonicalString, PCanonicalStruct, PField, PStruct, PType}
-import is.hail.types.virtual.{Field, TString, TStruct}
+import is.hail.types.virtual.{Field, TString, TStruct, Type}
 import is.hail.utils.{FastIndexedSeq, fatal}
 import org.apache.spark.sql.Row
 import org.json4s.{Formats, JValue}
@@ -33,6 +38,79 @@ object StringTableReader {
       fatal(s"arguments refer to no files: ${files.toIndexedSeq}.")
     status
   }
+}
+case class StringTablePartitionReader(lines: GenericLines) extends PartitionReader{
+  override def contextType: Type = lines.contextType
+
+  override def fullRowType: Type = TStruct("file"-> TString, "text"-> TString)
+
+  override def rowRequiredness(requestedType: Type): TypeWithRequiredness = {
+    val req = BaseTypeWithRequiredness.apply(requestedType).asInstanceOf[RStruct]
+    req.fields.foreach(field => field.typ.hardSetRequiredness(true))
+    req.hardSetRequiredness(true)
+    req
+  }
+
+  override def emitStream(
+     ctx: ExecuteContext,
+     cb: EmitCodeBuilder,
+     context: EmitCode,
+     partitionRegion: Value[Region],
+     requestedType: Type): IEmitCode = {
+
+     context.toI(cb).map(cb) { partitionContext =>
+       val iter = cb.emb.genFieldThisRef[CloseableIterator[GenericLines]]("iter")
+
+       val ctxMemo = partitionContext.asBaseStruct.memoize(cb, "string_table_reader_ctx")
+       val fileName = cb.emb.genFieldThisRef[String]("fileName")
+       val line = cb.emb.genFieldThisRef[String]("line")
+
+       SStreamCode(new StreamProducer {
+        override val length: Option[EmitCodeBuilder => Code[Int]] = None
+
+           override def initialize(cb: EmitCodeBuilder): Unit = {
+             val contextAsJavaValue = StringFunctions.scodeToJavaValue(cb, partitionRegion, ctxMemo)
+
+             cb.assign(fileName, ctxMemo.loadField(cb, "file").get(cb).asString.loadString())
+
+             cb.assign(iter,
+               cb.emb.getObject[Function1[AnyRef, CloseableIterator[GenericLine]]](lines.body)
+               .invoke[AnyRef, CloseableIterator[GenericLines]]("apply", contextAsJavaValue)
+             )
+           }
+
+           override val elementRegion: Settable[Region] =
+             cb.emb.genFieldThisRef[Region]("string_table_reader_region")
+
+           override val requiresMemoryManagementPerElement: Boolean = true
+
+        override val LproduceElement: CodeLabel = cb.emb.defineAndImplementLabel { cb =>
+          val hasNext = iter.invoke[Boolean]("hasNext")
+          cb.ifx(hasNext, {
+            val gLine = iter.invoke[GenericLine]("next")
+            cb.assign(line, gLine.invoke[String]("toString"))
+            cb.goto(LproduceElementDone)
+          }, {
+            cb.goto(LendOfStream)
+          })
+        }
+        override val element: EmitCode = EmitCode.fromI(cb.emb) { cb =>
+
+          val reqType: TStruct = requestedType.asInstanceOf[TStruct]
+
+          IEmitCode.present(cb, SStackStruct.constructFromArgs(cb, elementRegion, reqType,
+            EmitCode.fromI(cb.emb)(cb => IEmitCode.present(cb, SJavaString.construct(line))),
+            EmitCode.fromI(cb.emb)(cb => IEmitCode.present(cb, SJavaString.construct(fileName)))))
+        }
+
+        override def close(cb: EmitCodeBuilder): Unit = {
+          cb += iter.invoke[Unit]("close")
+        }
+       })
+     }
+  }
+
+  override def toJValue: JValue = ???
 }
 
 class StringTableReader(
