98f8934ab8e94c98440fd63e48a1e5710e3bf020
Tim Poterba <tpoterba@gmail.com>
2021-08-05 17:10:37 -0400

    [compiler] Use lowering rule to implement TableJoin.execute

--- hail/src/main/scala/is/hail/expr/ir/lowering/LowerTableIR.scala
+++ hail/src/main/scala/is/hail/expr/ir/lowering/LowerTableIR.scala
@@ -980,36 +980,7 @@ object LowerTableIR {
           val loweredLeft = lower(left)
           val loweredRight = lower(right)
 
-          val lKeyFields = left.typ.key.take(joinKey)
-          val lValueFields = left.typ.rowType.fieldNames.filter(f => !lKeyFields.contains(f))
-          val rKeyFields = right.typ.key.take(joinKey)
-          val rValueFields = right.typ.rowType.fieldNames.filter(f => !rKeyFields.contains(f))
-          val lReq = r.lookup(left).asInstanceOf[RTable]
-          val rReq = r.lookup(right).asInstanceOf[RTable]
-
-          val joinedStage = loweredLeft.orderedJoin(
-            loweredRight, joinKey, joinType,
-            (lGlobals, rGlobals) => {
-              val rGlobalType = rGlobals.typ.asInstanceOf[TStruct]
-              val rGlobalRef = Ref(genUID(), rGlobalType)
-              Let(rGlobalRef.name, rGlobals,
-                  InsertFields(lGlobals, rGlobalType.fieldNames.map(f => f -> GetField(rGlobalRef, f))))
-            },
-            (lEltRef, rEltRef) => {
-            MakeStruct(
-              (lKeyFields, rKeyFields).zipped.map { (lKey, rKey) =>
-                if (joinType == "outer" && lReq.field(lKey).required && rReq.field(rKey).required)
-                  lKey -> Coalesce(FastSeq(GetField(lEltRef, lKey), GetField(rEltRef, rKey), Die("TableJoin expected non-missing key", left.typ.rowType.fieldType(lKey), -1)))
-                else
-                  lKey -> Coalesce(FastSeq(GetField(lEltRef, lKey), GetField(rEltRef, rKey)))
-              }
-                ++ lValueFields.map(f => f -> GetField(lEltRef, f))
-                ++ rValueFields.map(f => f -> GetField(rEltRef, f)))
-          })
-
-          assert(joinedStage.rowType == tj.typ.rowType)
-          joinedStage
-
+          LowerTableIRHelpers.lowerTableJoin(ctx, tj, loweredLeft, loweredRight, r)
         case x@TableUnion(children) =>
           val lowered = children.map(lower)
           val keyType = x.typ.keyType
---
+++ hail/src/main/scala/is/hail/expr/ir/lowering/LowerTableIRHelpers.scala
@@ -0,0 +1,43 @@
+package is.hail.expr.ir.lowering
+
+import is.hail.expr.ir.{Coalesce, Die, ExecuteContext, GetField, IR, InsertFields, Let, MakeStruct, Ref, RequirednessAnalysis, TableIR, TableJoin, genUID}
+import is.hail.types.RTable
+import is.hail.types.virtual.TStruct
+import is.hail.utils.FastSeq
+
+object LowerTableIRHelpers {
+
+  def lowerTableJoin(ctx: ExecuteContext, tj: TableJoin, loweredLeft: TableStage, loweredRight: TableStage, r: RequirednessAnalysis): TableStage = {
+    val TableJoin(left, right, joinType, joinKey) = tj
+
+    val lKeyFields = left.typ.key.take(joinKey)
+    val lValueFields = left.typ.rowType.fieldNames.filter(f => !lKeyFields.contains(f))
+    val rKeyFields = right.typ.key.take(joinKey)
+    val rValueFields = right.typ.rowType.fieldNames.filter(f => !rKeyFields.contains(f))
+    val lReq = r.lookup(left).asInstanceOf[RTable]
+    val rReq = r.lookup(right).asInstanceOf[RTable]
+
+    val joinedStage = loweredLeft.orderedJoin(
+      loweredRight, joinKey, joinType,
+      (lGlobals, rGlobals) => {
+        val rGlobalType = rGlobals.typ.asInstanceOf[TStruct]
+        val rGlobalRef = Ref(genUID(), rGlobalType)
+        Let(rGlobalRef.name, rGlobals,
+          InsertFields(lGlobals, rGlobalType.fieldNames.map(f => f -> GetField(rGlobalRef, f))))
+      },
+      (lEltRef, rEltRef) => {
+        MakeStruct(
+          (lKeyFields, rKeyFields).zipped.map { (lKey, rKey) =>
+            if (joinType == "outer" && lReq.field(lKey).required && rReq.field(rKey).required)
+              lKey -> Coalesce(FastSeq(GetField(lEltRef, lKey), GetField(rEltRef, rKey), Die("TableJoin expected non-missing key", left.typ.rowType.fieldType(lKey), -1)))
+            else
+              lKey -> Coalesce(FastSeq(GetField(lEltRef, lKey), GetField(rEltRef, rKey)))
+          }
+            ++ lValueFields.map(f => f -> GetField(lEltRef, f))
+            ++ rValueFields.map(f => f -> GetField(rEltRef, f)))
+      })
+
+    assert(joinedStage.rowType == tj.typ.rowType)
+    joinedStage
+  }
+}
