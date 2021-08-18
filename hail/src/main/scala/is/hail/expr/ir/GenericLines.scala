6f1893be766011f80f1aa6a45fc3a7a4166ecae5
Tim Poterba <tpoterba@gmail.com>
2021-08-18 13:12:19 -0400

    use maxArraySize of Int.MaxValue - 8

--- hail/src/main/scala/is/hail/expr/ir/GenericLines.scala
+++ hail/src/main/scala/is/hail/expr/ir/GenericLines.scala
@@ -161,9 +161,13 @@ object GenericLines {
             val n = bufPos - begin
             if (linePos + n > lineData.length) {
               val copySize = linePos.toLong + n
-              if (copySize > Int.MaxValue)
+
+              // Maximum array size compatible with common JDK implementations
+              // https://github.com/openjdk/jdk14u/blob/84917a040a81af2863fddc6eace3dda3e31bf4b5/src/java.base/share/classes/jdk/internal/util/ArraysSupport.java#L577
+              val maxArraySize = Int.MaxValue - 8
+              if (copySize > maxArraySize)
                 fatal(s"GenericLines: line size reached: cannot read a line with more than 2^31-1 bytes")
-              val newSize = Math.min(copySize * 2, Int.MaxValue).toInt
+              val newSize = Math.min(copySize * 2, maxArraySize).toInt
               if (newSize > (1 << 20)) {
                 log.info(s"GenericLines: growing line buffer to $newSize")
               }
