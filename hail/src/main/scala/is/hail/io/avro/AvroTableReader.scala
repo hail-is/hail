42cbd1ead92b4d96e89672ba29e913debcb99962
Christopher Vittal <cvittal@broadinstitute.org>
2021-08-25 15:24:45 -0400

    AvroTableReader.lower

--- hail/src/main/scala/is/hail/io/avro/AvroTableReader.scala
+++ hail/src/main/scala/is/hail/io/avro/AvroTableReader.scala
@@ -1,41 +1,45 @@
 package is.hail.io.avro
 
 import is.hail.expr.ir.lowering.{TableStage, TableStageDependency}
-import is.hail.expr.ir.{ExecuteContext, MakeArray, MakeStruct, PartitionReader, ReadPartition, Str, TableRead, TableReader, TableStageIntermediate, TableValue, ToStream}
+import is.hail.expr.ir.{ExecuteContext, MakeArray, MakeStruct, PartitionReader, ReadPartition, Str, TableRead, TableReader, TableStageIntermediate, TableValue}
 import is.hail.rvd.RVDPartitioner
 import is.hail.types.TableType
 import is.hail.types.physical.{PCanonicalStruct, PStruct}
 import is.hail.types.virtual.TStruct
-import org.json4s.{DefaultFormats, JValue}
+import org.json4s.JValue
 
 class AvroTableReader(partitionReader: AvroPartitionReader, paths: IndexedSeq[String]) extends TableReader {
   def pathsUsed: Seq[String] = paths
-
-  def apply(tr: TableRead, ctx: ExecuteContext): TableValue = {
-    val partitioner = RVDPartitioner.unkeyed(paths.length)
-    val globals = MakeStruct(Seq())
-    val contexts = MakeArray(paths.map(Str): _*)
-    val ts = TableStage(
-      globals,
-      partitioner,
-      TableStageDependency.none,
-      contexts,
-      { ctx =>
-        ReadPartition(ctx, tr.typ.rowType, partitionReader)
-      }
-    )
-    new TableStageIntermediate(ts).asTableValue(ctx)
-  }
 
   def partitionCounts: Option[IndexedSeq[Long]] = None
 
   def fullType: TableType = TableType(partitionReader.fullRowType, IndexedSeq(), TStruct())
 
   def rowAndGlobalPTypes(ctx: ExecuteContext, requestedType: TableType): (PStruct, PStruct) =
-    (partitionReader.rowRequiredness(requestedType.rowType).canonicalPType(requestedType.rowType).asInstanceOf[PStruct],
-     PCanonicalStruct(required = true))
+  (partitionReader.rowRequiredness(requestedType.rowType).canonicalPType(requestedType.rowType).asInstanceOf[PStruct],
+  PCanonicalStruct(required = true))
 
   def renderShort(): String = defaultRender()
+
+  def apply(tr: TableRead, ctx: ExecuteContext): TableValue = {
+    val ts = lower(ctx, tr.typ)
+    new TableStageIntermediate(ts).asTableValue(ctx)
+  }
+
+  override def lower(ctx: ExecuteContext, requestedType: TableType): TableStage = {
+    val partitioner = RVDPartitioner.unkeyed(paths.length)
+    val globals = MakeStruct(Seq())
+    val contexts = MakeArray(paths.map(Str): _*)
+    TableStage(
+      globals,
+      partitioner,
+      TableStageDependency.none,
+      contexts,
+      { ctx =>
+        ReadPartition(ctx, requestedType.rowType, partitionReader)
+      }
+    )
+  }
 }
 
 object AvroTableReader {
