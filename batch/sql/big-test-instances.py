09e0a626e48377ab80b1f983bc9bdbc51434b528
Daniel King <dking@broadinstitute.org>
2021-09-01 17:17:16 -0400

    more test instances

---
+++ batch/sql/big-test-instances.py
@@ -0,0 +1,31 @@
+import os
+import asyncio
+from gear import Database
+
+
+async def main():
+    scope = os.environ['HAIL_SCOPE']
+
+    if scope == 'deploy':
+        return
+
+    db = Database()
+    await db.async_init()
+
+    await db.execute_update(
+        '''
+UPDATE inst_colls
+WHERE `name`='standard'
+SET max_instances = 8, pool_size = 8;
+''')
+
+    await db.execute_update(
+        '''
+UPDATE pools
+WHERE `name`='standard' AND `worker_type`='standard'
+SET worker_cores = 8;
+''')
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+
