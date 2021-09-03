c6064f63270a47d117a52151fde9d24f1cfa5bfb
Daniel King <dking@broadinstitute.org>
2021-09-03 19:02:49 -0400

    el fixo

--- batch/test/test_batch.py
+++ batch/test/test_batch.py
@@ -236,7 +236,11 @@ def test_list_batches(client):
 
     def assert_batch_ids(expected, q=None):
         # list_batches returns all batches for all prev run tests so we set a limit
-        batches = client.list_batches(q, last_batch_id=expected, limit=200)
+        ids = [x.id for x in expected]
+        max_expected_id = max(ids)
+        min_expeccted_id = min(ids)
+        span = max_expected_id - min_expeccted_id + 1
+        batches = client.list_batches(q, last_batch_id=max_expected_id + 1, limit=span)
         actual = set([b.id for b in batches]).intersection({b1.id, b2.id})
         assert actual == expected
 
