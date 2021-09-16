0bedf5032b28d8c3645cffbbcba85a8b1f082a53
Chris Vittal <chris@vittal.dev>
2021-09-16 12:57:57 -0400

    export the combiner module from the vds module

--- hail/python/hail/vds/__init__.py
+++ hail/python/hail/vds/__init__.py
@@ -1,6 +1,7 @@
 from .methods import filter_intervals, filter_samples, filter_variants, sample_qc, split_multi, to_dense_mt, to_merged_sparse_mt
 from .functions import lgt_to_gt
 from .variant_dataset import VariantDataset, read_vds
+from . import combiner
 
 __all__ = [
     'VariantDataset',
@@ -12,5 +13,6 @@ __all__ = [
     'split_multi',
     'to_dense_mt',
     'to_merged_sparse_mt',
-    'lgt_to_gt'
+    'lgt_to_gt',
+    'combiner',
 ]
