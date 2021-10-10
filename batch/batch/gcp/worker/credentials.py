bad187a2a1926bb88a9be4721384c1e7550d08af
Jackie Goldstein <jigold@broadinstitute.org>
2021-10-09 20:18:43 -0400

    [batch] Make user credentials cloud-agnostic

---
+++ batch/batch/gcp/worker/credentials.py
@@ -0,0 +1,18 @@
+from typing import Dict
+import base64
+
+from ...worker.credentials import CloudUserCredentials
+
+
+class GCPUserCredentials(CloudUserCredentials):
+    def __init__(self, data: Dict[str, bytes]):
+        self.secret_data = data
+        self.secret_name = 'gsa-key'
+        self.file_name = 'key.json'
+        self.cloud_env_name = 'GOOGLE_APPLICATION_CREDENTIALS'
+        self.hail_env_name = 'HAIL_GSA_KEY_FILE'
+        self.username = '_json_key'
+
+    @property
+    def password(self):
+        return base64.b64decode(self.secret_data['key.json']).decode()
---
+++ batch/batch/worker/credentials.py
@@ -0,0 +1,16 @@
+import abc
+from typing import Dict
+
+
+class CloudUserCredentials(abc.ABC):
+    secret_name: str
+    secret_data: Dict[str, bytes]
+    file_name: str
+    cloud_env_name: str
+    hail_env_name: str
+    username: str
+    password: str
+
+    @property
+    def mount_path(self):
+        return f'/{self.secret_name}/{self.file_name}'
