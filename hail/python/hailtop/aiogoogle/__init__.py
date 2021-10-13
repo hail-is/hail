f13c75216be53d69f0c7b7236d42e71399d8527a
Dan King <daniel.zidan.king@gmail.com>
2021-10-13 17:52:50 -0400

    [hailtop] restore aiogoogle (#10974)
    * [hailtop] restore aiogoogle
    
    Restore backwards compatibility of aiogoogle
    
    * pacify pylint

---
+++ hail/python/hailtop/aiogoogle/__init__.py
@@ -0,0 +1,6 @@
+import warnings
+from ..aiocloud.aiogoogle import *  # noqa: F403,F401
+
+warnings.warn("importing hailtop.aiogoogle is deprecated, please use hailtop.aiocloud.aiogoogle",
+              DeprecationWarning,
+              stacklevel=2)
