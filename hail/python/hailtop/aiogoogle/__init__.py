9627402aac04dc9f98397f62b83c28bfd7eb41bf
Daniel King <dking@broadinstitute.org>
2021-10-13 15:54:06 -0400

    [hailtop] restore aiogoogle
    Restore backwards compatibility of aiogoogle

---
+++ hail/python/hailtop/aiogoogle/__init__.py
@@ -0,0 +1,6 @@
+import warnings
+from ..aiocloud.aiogoogle import *
+
+warnings.warn("importing hailtop.aiogoogle is deprecated, please use hailtop.aiocloud.aiogoogle",
+              DeprecationWarning,
+              stacklevel=2)
