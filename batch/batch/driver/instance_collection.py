792cb5e76dda99573178e9dbddf941ab51d79d42
Dan King <daniel.zidan.king@gmail.com>
2021-10-18 12:17:48 -0400

    [batch] eliminate unhealthy and missing instances (#10956)
    * [batch] eliminate unhealthy and missing instances
    
    For some reason, we bail out *before* we check if the instance is unhealthy
    and before we check if the instance does not even exist. This PR changes
    that behavior so we actually remove such instances.
    
    * move up one block

--- batch/batch/driver/instance_collection.py
+++ batch/batch/driver/instance_collection.py
@@ -118,6 +118,14 @@ class InstanceCollection:
 
     async def check_on_instance(self, instance):
         active_and_healthy = await instance.check_is_active_and_healthy()
+
+        if (instance.state == 'active'
+                and instance.failed_request_count > 5
+                and time_msecs() - instance.last_updated > 5 * 60 * 1000):
+            log.exception(f'deleting {instance} with {instance.failed_request_count} failed request counts after more than 5 minutes')
+            await self.call_delete_instance(instance, 'not_responding')
+            return
+
         if active_and_healthy:
             return
 
@@ -128,14 +136,6 @@ class InstanceCollection:
                 await self.remove_instance(instance, 'does_not_exist')
                 return
             raise
-
-        if (instance.state == 'active'
-                and instance.failed_request_count > 5
-                and time_msecs() - instance.last_updated > 5 * 60 * 1000):
-            log.exception(f'deleting {instance} with {instance.failed_request_count} failed request counts after more than 5 minutes')
-            await self.call_delete_instance(instance, 'not_responding')
-            return
-
         # PROVISIONING, STAGING, RUNNING, STOPPING, TERMINATED
         gce_state = spec['status']
 
