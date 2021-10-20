f928534268890868f1aa679dad1d59a46fac49bf
Daniel Goldstein <danielgold95@gmail.com>
2021-10-20 15:49:01 -0400

    fix

--- infra/install_bootstrap_dependencies.sh
+++ infra/install_bootstrap_dependencies.sh
@@ -10,5 +10,5 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
 
-pip3 install --upgrade pip
+python3 -m pip install --upgrade pip
 python3 -m pip install -r $HOME/hail/docker/requirements.txt
