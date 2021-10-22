4ebd90f9acc0243f2b5cea772e39a0859d87d4d8
patrick-schultz <pschultz@broadinstitute.org>
2021-10-22 11:41:12 -0400

    add test case

--- hail/python/test/hail/experimental/test_local_whitening.py
+++ hail/python/test/hail/experimental/test_local_whitening.py
@@ -8,16 +8,17 @@ from ..helpers import *
 setUpModule = startTestHailContext
 tearDownModule = stopTestHailContext
 
+
 def naive_whiten(X, w):
     m, n = np.shape(X)
     Xw = np.zeros((m, n))
     for j in range(n):
         Q, _ = np.linalg.qr(X[:, max(j - w, 0): j])
-        Xw[:,j] = X[:,j] - Q @ (Q.T @ X[:,j])
+        Xw[:, j] = X[:, j] - Q @ (Q.T @ X[:, j])
     return Xw.T
 
-class Tests(unittest.TestCase):
 
+class Tests(unittest.TestCase):
     def run_local_whitening_test(self, vec_size, num_rows, chunk_size, window_size, partition_size, initial_num_partitions):
         rng = default_rng()
         data = rng.normal(size=(num_rows, vec_size))
@@ -30,11 +31,21 @@ class Tests(unittest.TestCase):
         whitened_naive = naive_whiten(data.T, window_size)
         np.testing.assert_allclose(whitened_hail, whitened_naive)
 
-    def test_local_whitening(self):
+    def test_local_whitening(self)\
+            :
         self.run_local_whitening_test(
             vec_size=100,
             num_rows=10000,
             chunk_size=32,
             window_size=64,
-            partition_size=32*40,
+            partition_size=32 * 40,
+            initial_num_partitions=50)
+
+    def test_local_whitening_singleton_final_partition(self):
+        self.run_local_whitening_test(
+            vec_size=100,
+            num_rows=32 * 40 * 8 + 1,  # = 10241
+            chunk_size=32,
+            window_size=64,
+            partition_size=32 * 40,
             initial_num_partitions=50)
