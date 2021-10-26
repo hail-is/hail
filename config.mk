6d09c14fb447ae6d627399d3408f697a931e8d1b
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-10-27 01:07:21 +1100

    Fix internal gateway IP

--- config.mk
+++ config.mk
@@ -5,7 +5,7 @@ DOCKER_PREFIX := $(REGION)-docker.pkg.dev/$(PROJECT)/hail
 DOCKER_ROOT_IMAGE := $(DOCKER_PREFIX)/ubuntu:18.04
 HAIL_TEST_GCS_BUCKET := cpg-hail-test
 DOMAIN := hail.populationgenomics.org.au
-INTERNAL_IP := 10.152.0.2
+INTERNAL_IP := 10.152.0.10
 IP := 35.201.29.236
 KUBERNETES_SERVER_URL := https://34.87.199.41
 
