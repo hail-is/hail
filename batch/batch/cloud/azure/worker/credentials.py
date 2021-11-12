d803707a8a561bb6e7f0ac3066a5e0c64432d8ec
Daniel Goldstein <danielgold95@gmail.com>
2021-11-12 11:12:56 -0500

    change credentials fields to properties

--- batch/batch/cloud/azure/worker/credentials.py
+++ batch/batch/cloud/azure/worker/credentials.py
@@ -7,17 +7,37 @@ from ....worker.credentials import CloudUserCredentials
 
 class AzureUserCredentials(CloudUserCredentials):
     def __init__(self, data: Dict[str, bytes]):
-        self.secret_data = data
-        self.secret_name = 'azure-credentials'
-        self.file_name = 'key.json'
-        self.cloud_env_name = 'AZURE_APPLICATION_CREDENTIALS'
-        self.hail_env_name = 'HAIL_AZURE_CREDENTIAL_FILE'
-        self.credentials = json.loads(base64.b64decode(self.secret_data['key.json']).decode())
+        self._data = data
+        self._credentials = json.loads(base64.b64decode(data['key.json']).decode())
+
+    @property
+    def secret_name(self) -> str:
+        return 'azure-credentials'
+
+    @property
+    def secret_data(self) -> Dict[str, bytes]:
+        return self._data
+
+    @property
+    def file_name(self) -> str:
+        return 'key.json'
+
+    @property
+    def cloud_env_name(self) -> str:
+        return 'AZURE_APPLICATION_CREDENTIALS'
+
+    @property
+    def hail_env_name(self) -> str:
+        return 'HAIL_AZURE_CREDENTIAL_FILE'
 
     @property
     def username(self):
-        return self.credentials['appId']
+        return self._credentials['appId']
 
     @property
     def password(self):
-        return self.credentials['password']
+        return self._credentials['password']
+
+    @property
+    def mount_path(self):
+        return f'/{self.secret_name}/{self.file_name}'
