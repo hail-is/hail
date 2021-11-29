ccdcad217623a0dd509c325e7ae5445e1cf17102
Daniel King <dking@broadinstitute.org>
2021-11-29 18:16:26 -0500

    fix

--- hail/python/test/hailtop/hailctl/dataproc/test_cli.py
+++ hail/python/test/hailtop/hailctl/dataproc/test_cli.py
@@ -10,8 +10,8 @@ def test_required_gcloud_version_met(monkeypatch):
     monkeypatch.setattr("hailtop.hailctl.dataproc.gcloud.get_version", Mock(return_value=cli.MINIMUM_REQUIRED_GCLOUD_VERSION))
 
     mock_list = Mock()
-    async def async_return_mock_list():
-        return mock_list()
+    async def async_return_mock_list(*args, **kwargs):
+        return mock_list(*args, **kwargs)
     monkeypatch.setattr(list_clusters, "main", async_return_mock_list)
     cli.main(["list"])
 
@@ -22,8 +22,8 @@ def test_required_gcloud_version_unmet(monkeypatch, capsys):
     monkeypatch.setattr("hailtop.hailctl.dataproc.gcloud.get_version", Mock(return_value=(200, 0, 0)))
 
     mock_list = Mock()
-    async def async_return_mock_list():
-        return mock_list()
+    async def async_return_mock_list(*args, **kwargs):
+        return mock_list(*args, **kwargs)
     monkeypatch.setattr(list_clusters, "main", async_return_mock_list)
     with pytest.raises(SystemExit):
         cli.main(["list"])
@@ -37,8 +37,8 @@ def test_unable_to_determine_version(monkeypatch):
     monkeypatch.setattr("hailtop.hailctl.dataproc.gcloud.get_version", Mock(side_effect=ValueError))
 
     mock_list = Mock()
-    async def async_return_mock_list():
-        return mock_list()
+    async def async_return_mock_list(*args, **kwargs):
+        return mock_list(*args, **kwargs)
     monkeypatch.setattr(list_clusters, "main", async_return_mock_list)
     cli.main(["list"])
 
