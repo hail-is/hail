1451d82038e80b753b7771279a3b55d01c644256
Daniel King <dking@broadinstitute.org>
2021-11-29 16:19:35 -0500

    fix tests

--- hail/python/test/hailtop/hailctl/dataproc/test_cli.py
+++ hail/python/test/hailtop/hailctl/dataproc/test_cli.py
@@ -6,34 +6,37 @@ from hailtop.hailctl.dataproc import cli
 from hailtop.hailctl.dataproc import list_clusters
 
 
-def test_required_gcloud_version_met(monkeypatch):
+@pytest.mark.asyncio
+async def test_required_gcloud_version_met(monkeypatch):
     monkeypatch.setattr("hailtop.hailctl.dataproc.gcloud.get_version", Mock(return_value=cli.MINIMUM_REQUIRED_GCLOUD_VERSION))
 
     mock_list = Mock()
     monkeypatch.setattr(list_clusters, "main", mock_list)
-    cli.main(["list"])
+    await cli.main(["list"])
 
     assert mock_list.called
 
 
-def test_required_gcloud_version_unmet(monkeypatch, capsys):
+@pytest.mark.asyncio
+async def test_required_gcloud_version_unmet(monkeypatch, capsys):
     monkeypatch.setattr("hailtop.hailctl.dataproc.gcloud.get_version", Mock(return_value=(200, 0, 0)))
 
     mock_list = Mock()
     monkeypatch.setattr(list_clusters, "main", mock_list)
     with pytest.raises(SystemExit):
-        cli.main(["list"])
+        await cli.main(["list"])
 
     assert "hailctl dataproc requires Google Cloud SDK (gcloud) version" in capsys.readouterr().err
 
     assert not mock_list.called
 
 
-def test_unable_to_determine_version(monkeypatch):
+@pytest.mark.asyncio
+async def test_unable_to_determine_version(monkeypatch):
     monkeypatch.setattr("hailtop.hailctl.dataproc.gcloud.get_version", Mock(side_effect=ValueError))
 
     mock_list = Mock()
     monkeypatch.setattr(list_clusters, "main", mock_list)
-    cli.main(["list"])
+    await cli.main(["list"])
 
     assert mock_list.called
