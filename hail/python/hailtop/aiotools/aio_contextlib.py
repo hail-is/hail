b10c5ead035880d68e938718d51cf04551c16aed
Daniel King <dking@broadinstitute.org>
2021-11-29 15:37:27 -0500

    missing file

---
+++ hail/python/hailtop/aiotools/aio_contextlib.py
@@ -0,0 +1,23 @@
+class closing:
+    """Async equivalent of :meth:`.contextlib.closing`.
+
+    Code like this:
+
+        with closing(await fs.create("gs://abc/123")) as obj:
+            <block>
+
+    is equivalent to this:
+
+        f = await fs.create("gs://abc/123")
+        try:
+            <block>
+        finally:
+            await f.close()
+
+    """
+    def __init__(self, thing):
+        self.thing = thing
+    async def __aenter__(self):
+        return self.thing
+    async def __aexit__(self, *exc_info):
+        await self.thing.close()
