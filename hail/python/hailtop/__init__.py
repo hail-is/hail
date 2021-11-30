83fb0e120eaa23a0ee327132e0ca8c772543dc3b
Dan King <daniel.zidan.king@gmail.com>
2021-11-30 13:39:29 -0500

    [hailtop.batch] cloud agnostic copy and delete (#11091)
    * [hailtop.batch] cloud agnostic copy and delete
    
    Instead of using `gsutil` we use hailtop.aiotools.copy from the new `HAILGENETICS_HAIL_IMAGE`.  Previously, deploying the pip-versioned image was a manual asynchronous step that mostly happened in response to user requests.
    
    1. Actually build and test hailgenetics/hail and hailgenetics/genetics on every build.
    2. On deploy, push the newly built hailgenetics/hail and hailgenetics/genetics images to both docker hub and gcr.io/hail-vdc/
    3. Provide the built-for-this-PR hailgenetics/hail as an env var to the tests.
    4. By default use the hailgenetics/hail image for the currently published pip version for FS operations. Allow overriding by environment variable.
    
    * pylint
    
    * fix spacing
    
    * fix paths
    
    * fix _cp bug
    
    * fix
    
    * fix names
    
    * tighten type on _external_outputs and _internal_outputs
    
    * fix
    
    * fix
    
    * fix
    
    * fix perms in king
    
    * avoid division by zero
    
    * remove hailgenetics_genetics

--- hail/python/hailtop/__init__.py
+++ hail/python/hailtop/__init__.py
@@ -4,5 +4,9 @@ import pkg_resources
 _VERSION = pkg_resources.resource_string(__name__, 'hail_version').decode().strip()
 
 
+def pip_version() -> str:
+    return _VERSION.split('-')[0]
+
+
 def version() -> str:
     return _VERSION
