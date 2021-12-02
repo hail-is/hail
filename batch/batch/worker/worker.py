1d96f799f4fc738f573b1b641ffe6ee0d46e3911
Daniel Goldstein <danielgold95@gmail.com>
2021-12-02 11:47:41 -0500

    [batch] Add auth.hail to job /etc/hosts (#11098)
    * [batch] Add auth.hail to job /etc/hosts
    
    * copy subdomains.txt into worker image and use for jobs

--- batch/batch/worker/worker.py
+++ batch/batch/worker/worker.py
@@ -74,6 +74,9 @@ from .credentials import CloudUserCredentials
 
 # uvloop.install()
 
+with open('/subdomains.txt', 'r') as subdomains_file:
+    HAIL_SERVICES = [line.rstrip() for line in subdomains_file.readlines()]
+
 oldwarn = warnings.warn
 
 
@@ -210,7 +213,8 @@ class NetworkNamespace:
             hosts.write('127.0.0.1 localhost\n')
             hosts.write(f'{self.job_ip} {self.hostname}\n')
             if NAMESPACE == 'default':
-                hosts.write(f'{INTERNAL_GATEWAY_IP} batch.hail\n')
+                for service in HAIL_SERVICES:
+                    hosts.write(f'{INTERNAL_GATEWAY_IP} {service}.hail\n')
             hosts.write(f'{INTERNAL_GATEWAY_IP} internal.hail\n')
 
         # Jobs on the private network should have access to the metadata server
