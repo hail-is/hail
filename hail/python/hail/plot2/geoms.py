89a2fca9a2bc3e2ea03063680bdae2ede4bf6076
John Compitello <j301600@gmail.com>
2021-12-06 15:45:26 -0500

    Got colors

--- hail/python/hail/plot2/geoms.py
+++ hail/python/hail/plot2/geoms.py
@@ -17,10 +17,14 @@ class Geom(FigureAttribute):
 
 
 def lookup(struct, mapping_field_name, target):
-    maybe = struct["figure_mapping"].get(target, struct[mapping_field_name].get(target))
+    maybe = lookup_opt(struct, mapping_field_name, target)
     if maybe is None:
         raise KeyError(f"aesthetic field {target} expected but not found")
     return maybe
+
+
+def lookup_opt(struct, mapping_field_name, target):
+    return struct["figure_mapping"].get(target, struct[mapping_field_name].get(target))
 
 
 class GeomPoint(Geom):
@@ -31,7 +35,8 @@ class GeomPoint(Geom):
     def apply_to_fig(self, collected, mapping_field_name, fig_so_far):
         x = [lookup(element, mapping_field_name, "x") for element in collected]
         y = [lookup(element, mapping_field_name, "y") for element in collected]
-        fig_so_far.add_scatter(x=x, y=y, mode="markers")
+        color = [lookup_opt(element, mapping_field_name, "color") for element in collected]
+        fig_so_far.add_scatter(x=x, y=y, marker_color=color, mode="markers")
 
 
 def geom_point(mapping=aes()):
@@ -46,7 +51,9 @@ class GeomLine(Geom):
     def apply_to_fig(self, collected, mapping_field_name, fig_so_far):
         x = [lookup(element, mapping_field_name, "x") for element in collected]
         y = [lookup(element, mapping_field_name, "y") for element in collected]
-        fig_so_far.add_scatter(x=x, y=y, mode="lines")
+        color = [lookup_opt(element, mapping_field_name, "color") for element in collected]
+        #FIXME: How should this work? All the colors have to match, there can be only one.
+        fig_so_far.add_scatter(x=x, y=y, line_color=color[0], mode="lines")
 
 
 def geom_line(mapping=aes()):
