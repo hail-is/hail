2c4929e1a8094e93a8547b8a9076b957ad70234c
Daniel King <dking@broadinstitute.org>
2021-12-09 11:26:52 -0500

    use one global deployment

--- batch/batch/cloud/azure/driver/resource_manager.py
+++ batch/batch/cloud/azure/driver/resource_manager.py
@@ -134,7 +134,7 @@ class AzureResourceManager(CloudResourceManager):
         )
 
         try:
-            await self.arm_client.put(f'/deployments/{machine_name}-dplmnt', json=vm_config)
+            await self.arm_client.put(f'/deployments/hail-hail-hail-is-awesome', json=vm_config)
             log.info(f'created machine {machine_name}')
         except Exception:
             log.exception(f'error while creating machine {machine_name}')
