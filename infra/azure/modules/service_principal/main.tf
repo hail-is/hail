aad56c995e9e50b11a09779b68bd947cffcb750d
Daniel Goldstein <danielgold95@gmail.com>
2021-12-16 10:50:28 -0500

    fix service principals stuff

--- infra/azure/modules/service_principal/main.tf
+++ infra/azure/modules/service_principal/main.tf
@@ -6,19 +6,24 @@ resource "azuread_application_password" "password" {
   application_object_id = var.application_object_id
 }
 
+locals {
+  credentials = {
+    appId       = var.application_id
+    displayName = azuread_service_principal.sp.display_name
+    password    = azuread_application_password.password.value
+    tenant      = azuread_service_principal.sp.application_tenant_id
+    objectId    = azuread_service_principal.sp.object_id
+    appObjectId = var.application_object_id
+  }
+}
+
 resource "kubernetes_secret" "gsa_key" {
   metadata {
     name = "${var.name}-gsa-key"
   }
 
   data = {
-    "key.json" = jsonencode({
-      appId       = var.application_id
-      displayName = azuread_service_principal.sp.display_name
-      password    = azuread_application_password.password.value
-      tenant      = azuread_service_principal.sp.application_tenant_id
-      objectId    = azuread_service_principal.sp.object_id
-    })
+    "key.json" = jsonencode(credentials)
   }
 }
 
--- infra/azure/modules/service_principal/outputs.tf
+++ infra/azure/modules/service_principal/outputs.tf
@@ -3,13 +3,6 @@ output principal_id {
 }
 
 output credentials {
-  value = {
-    appId       = var.application_id
-    displayName = azuread_service_principal.sp.display_name
-    password    = azuread_application_password.password.value
-    tenant      = azuread_service_principal.sp.application_tenant_id
-    objectId    = azuread_service_principal.sp.object_id
-    appObjectId = var.application_object_id
-  }
+  value = local.credentials
   sensitive = true
 }
