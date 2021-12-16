eaa750d099238eb10f9b8587b0a6e7e98b57f713
John Compitello <j301600@gmail.com>
2021-12-16 10:06:24 -0500

    Turn off flag forcing use of distributed sort

--- hail/src/main/scala/is/hail/HailContext.scala
+++ hail/src/main/scala/is/hail/HailContext.scala
@@ -482,7 +482,7 @@ object HailFeatureFlags {
     ("write_ir_files", ("HAIL_WRITE_IR_FILES" -> null)),
     ("method_split_ir_limit", ("HAIL_DEV_METHOD_SPLIT_LIMIT" -> "16")),
     ("use_new_shuffle", ("HAIL_USE_NEW_SHUFFLE" -> null)),
-    ("shuffle_cutoff_to_local_sort", ("HAIL_SHUFFLE_CUTOFF" -> "6"))
+    ("shuffle_cutoff_to_local_sort", ("HAIL_SHUFFLE_CUTOFF" -> null))
   )
 }
 
