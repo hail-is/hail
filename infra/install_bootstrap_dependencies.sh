0881db16745ac5712451006b1e1733f7e2601711
Daniel King <dking@broadinstitute.org>
2021-12-20 14:34:51 -0500

    [curl] pervasively use retries, report errors, follow redirects
    I set a `.curlrc` file to ensure we use the right options by default. In a few places that are
    intended to be run on a developer's laptop, I had to explicitly specify the necessary
    options.
    
    I replaced all the short flags with long flags, the important conversions to know:
    - `-L` `--location`, follow redirects
    - `-O` `--remote-name`, use the basename of the URL as the name of a local file in which to save the data
    - `-s` `--silent`, do not print anything to stdout/stderr
    - `-S` `--show-error`, when used with `-s`, shows errors on stderr
    - `-f` `--fail`, return non-zero exit code if the response is 4xx or 5xx (normally the response would be printed / saved and curl will exit successfully)

--- infra/install_bootstrap_dependencies.sh
+++ infra/install_bootstrap_dependencies.sh
@@ -7,7 +7,16 @@ sudo snap install --classic kubectl
 sudo usermod -a -G docker $USER
 
 # Terraform
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+curl --connect-timeout 5 \
+     --max-time 10 \
+     --retry 5 \
+     --retry-all-errors \
+     --retry-max-time 40 \
+     --location \
+     --fail \
+     --silent \
+     --show-error \
+     https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
 
