43904429380e57ed7d40cb1cee89e09f97a255bf
ammekk <emma.kelminson001@umb.edu>
2022-01-05 12:01:47 -0500

    trying to write test

--- hail/src/test/scala/is/hail/expr/ir/EmitStreamSuite.scala
+++ hail/src/test/scala/is/hail/expr/ir/EmitStreamSuite.scala
@@ -385,10 +385,10 @@ class EmitStreamSuite extends HailSuite {
   }
 
   @Test def testStreamBufferedAggregatorMultipleAggregators(): Unit = {
-    val resultArray = IndexedSeq(Row(Row(1), Row(3, IndexedSeq(1, 2, 3))), Row(Row(2), Row(2, IndexedSeq(2, 4))), Row(Row(3), Row(3, IndexedSeq(1, 2, 3))),
-                            Row(Row(4), Row(1, IndexedSeq(4))), Row(Row(5), Row(1, IndexedSeq(1))), Row(Row(6), Row(1, IndexedSeq(3))),
-                            Row(Row(7), Row(1, IndexedSeq(4))), Row(Row(8), Row(1, IndexedSeq(1))), Row(Row(8), Row(1, IndexedSeq(2))),
-                            Row(Row(9), Row(1, IndexedSeq(3))), Row(Row(10), Row(2, IndexedSeq(4, 4))))
+    val resultArray = IndexedSeq(Row(Row(1), Row(3, IndexedSeq(1L, 2L, 3L))), Row(Row(2), Row(2, IndexedSeq(2L, 4L))), Row(Row(3), Row(3, IndexedSeq(1L, 2L, 3L))),
+                            Row(Row(4), Row(1, IndexedSeq(4L))), Row(Row(5), Row(1, IndexedSeq(1L))), Row(Row(6), Row(1, IndexedSeq(3L))),
+                            Row(Row(7), Row(1, IndexedSeq(4L))), Row(Row(8), Row(1, IndexedSeq(1L))), Row(Row(8), Row(1, IndexedSeq(2L))),
+                            Row(Row(9), Row(1, IndexedSeq(3L))), Row(Row(10), Row(2, IndexedSeq(4L, 4L))))
     val streamType = TStream(TStruct("a" -> TInt64, "b" -> TInt64))
     val elemOne = MakeStruct(Seq(("a", I64(1)), ("b", I64(1))))
     val elemTwo = MakeStruct(Seq(("a", I64(2)), ("b", I64(2))))
@@ -435,18 +435,17 @@ class EmitStreamSuite extends HailSuite {
             MakeTuple.ordered(IndexedSeq(ResultOp(0, countAggSig), ResultOp(1, collectAggSig))),
             IndexedSeq(countAggSig.state, collectAggSig.state))
       ))}
-    val compare = true
 
     println(evalStream(result))
-    println((resultArray))
-    evalStream(result).zip(resultArray).foreach{ case (a, b) => print(f"${a.asInstanceOf[Row].get(1).asInstanceOf[Row].get(1).asInstanceOf[IndexedSeq].mkString(" ")}" +
-      f"${b.get(1).asInstanceOf[Row].get(1).asInstanceOf[IndexedSeq].mkString(" ")}\n") }
-      //  case (aElem, bElem) =>)
-    //assert(evalStream(result).zip(resultArray).forall{
-    //  case (aElem, bElem) => aElem.asInstanceOf[Row].get(0) == bElem.get(0) &&
-    //    aElem.asInstanceOf[Row].get(1).asInstanceOf[Row].get(0) == bElem.get(1).asInstanceOf[Row].get(0) &&
-    //    aElem.asInstanceOf[Row].get(1).asInstanceOf[Row].get(1).asInstanceOf  zip(bElem.get(1).asInstanceOf[Row].get(1))})
-    //println(evalStream(result))
+    println(resultArray)
+    //assert(evalStream(result) == resultArray)
+
+    assert(evalStream(result).zip(resultArray).forall{
+      case (aElem, bElem) => aElem.asInstanceOf[Row].get(0) == bElem.get(0) &&
+        aElem.asInstanceOf[Row].get(1).asInstanceOf[Row].get(0) == bElem.get(1).asInstanceOf[Row].get(0) &&
+        aElem.asInstanceOf[Row].get(1).asInstanceOf[Row].get(1).asInstanceOf[IndexedSeq[Long]]
+          .zip(bElem.get(1).asInstanceOf[Row].get(1).asInstanceOf[IndexedSeq[Long]])
+          .forall{ case (cElem, dElem) => cElem == dElem}})
   }
 
 
