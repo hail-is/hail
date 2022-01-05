9c00d3e517e41c99d8377b6aef9931d7c1c8a7b4
jigold <jigold@users.noreply.github.com>
2022-01-05 18:09:34 -0500

    [batch] Add GC loop for Azure deployments (#11174)
    * [batch] Add GC loop for Azure deployments
    
    * address comment

--- hail/python/hailtop/aiocloud/aioazure/client/arm_client.py
+++ hail/python/hailtop/aiocloud/aioazure/client/arm_client.py
@@ -1,3 +1,5 @@
+from typing import AsyncGenerator, Any, Optional
+
 from .base_client import AzureBaseClient
 
 
@@ -10,3 +12,10 @@ class AzureResourceManagerClient(AzureBaseClient):
             params['api-version'] = '2021-04-01'
         super().__init__(f'https://management.azure.com/subscriptions/{subscription_id}/resourceGroups/{resource_group_name}/providers/Microsoft.Resources',
                          **kwargs)
+
+    async def list_deployments(self, filter: Optional[str] = None) -> AsyncGenerator[Any, None]:
+        # https://docs.microsoft.com/en-us/rest/api/resources/deployments/list-by-resource-group
+        params = {}
+        if filter is not None:
+            params['$filter'] = filter
+        return self._paged_get('/deployments', params=params)
