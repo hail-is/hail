ae2dc42c5b36b947ceace7b523a9db558da7bfd6
Vedant Rautela <v.rautela01@gmail.com>
2022-01-20 11:46:19 -0500

    change number of jobs to depend on scheduling duration

--- batch/load-test/test-max-scheduling-rate.py
+++ batch/load-test/test-max-scheduling-rate.py
@@ -4,9 +4,9 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='Load test the Batch service using for a given max scheduling rate')
-parser.add_argument('msr', type=int, nargs=1, help='the maximum scheduling rate for the Batch Driver (in jobs/sec)')
-parser.add_argument('n', type=int, nargs=1, help='the max number of machines that can be provisioned for this batch')
-parser.add_argument('duration', type=int, nargs=1, help='the duration of the load test (in sec)')
+parser.add_argument('msr', type=int, help='the maximum scheduling rate for the Batch Driver (in jobs/sec)')
+parser.add_argument('n', type=int, help='the max number of machines that can be provisioned for this batch')
+parser.add_argument('duration', type=int, help='how long the batch driver will schedule jobs (in sec)')
 
 args = parser.parse_args()
 msr = args.msr
@@ -21,9 +21,8 @@ b = hb.Batch(backend=backend, name='load-test')
 max_concurrent_quarter_core_jobs = 64 * n
 # the amount of time for which each job sleeps
 sleep_time = math.floor(max_concurrent_quarter_core_jobs / msr)
-# the number of jobs is the max scheduling rate times the scheduling duration (which is sleep_time less
-# than the total duration since the batch completes sleep_time seconds after the last job is scheduled)
-n_jobs = msr * (duration - sleep_time)
+# the number of jobs is the max scheduling rate times the scheduling duration
+n_jobs = msr * duration
 
 for idx in range(n_jobs):
     j = b.new_job(name=f'job_{idx}')
