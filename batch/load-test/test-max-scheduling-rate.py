9af1aaff95eb4759c440e3bb1c13b840c9986e70
Vedant Rautela <v.rautela01@gmail.com>
2022-01-20 09:43:24 -0500

    rename scheduling rate load test and add duration parameter

--- batch/load-test/test-max-scheduling-rate.py
+++ batch/load-test/test-max-scheduling-rate.py
@@ -4,12 +4,14 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='Load test the Batch service using for a given max scheduling rate')
-parser.add_argument('msr', type=int, nargs=1, help='the maximum scheduling rate for the Batch Driver')
+parser.add_argument('msr', type=int, nargs=1, help='the maximum scheduling rate for the Batch Driver (in jobs/sec)')
 parser.add_argument('n', type=int, nargs=1, help='the max number of machines that can be provisioned for this batch')
+parser.add_argument('duration', type=int, nargs=1, help='the duration of the load test (in sec)')
 
 args = parser.parse_args()
 msr = args.msr
 n = args.n
+duration = args.duration
 
 # Need to set hailctl remote_tmpdir
 backend = hb.ServiceBackend('test')
@@ -17,12 +19,12 @@ b = hb.Batch(backend=backend, name='load-test')
 
 # the number of quarter-core jobs that can be running on n machines (assuming each machines has 16 cores)
 max_concurrent_quarter_core_jobs = 64 * n
+# the amount of time for which each job sleeps
+sleep_time = math.floor(max_concurrent_quarter_core_jobs / msr)
+# the number of jobs to schedule such that the load test lasts for the given duration
+n_jobs = msr * (duration - sleep_time)
 
-# TODO: should the factor of 10 be user-specificable?
-# this load test will require the driver to schedule 10 times as many jobs as can possibly be running at once
-n_jobs = 10 * max_concurrent_quarter_core_jobs
 for idx in range(n_jobs):
-    sleep_time = math.ceil(max_concurrent_quarter_core_jobs / msr)
     j = b.new_job(name=f'job_{idx}')
     j.cpu('250m')
     j.command(f'sleep {sleep_time}')
