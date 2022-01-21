8e0c3508bb3dad8a19aaf03c4809636448337e11
Daniel King <dking@broadinstitute.org>
2022-01-21 16:06:33 -0500

    fix

--- hail/python/test/hail/methods/test_pca.py
+++ hail/python/test/hail/methods/test_pca.py
@@ -292,26 +292,26 @@ def spectra_and_moments_helper(spec_func):
         np.testing.assert_allclose(moments, true_moments, rtol=1e-04)
 
 
-@fails_service_backend(reason='persist_ir')
+@skip_when_service_backend(message='intermittently hangs')
 def test_spectra_and_moments_1():
     spectra_and_moments_helper(spec1)
 
 
-@fails_service_backend(reason='persist_ir')
+@skip_when_service_backend(message='intermittently hangs')
 def test_spectra_and_moments_2():
     spectra_and_moments_helper(spec2)
 
 
-@fails_service_backend(reason='persist_ir')
+@skip_when_service_backend(message='intermittently hangs')
 def test_spectra_and_moments_3():
     spectra_and_moments_helper(spec3)
 
 
-@skip_when_service_backend(message='very slow / hangs')
+@skip_when_service_backend(message='intermittently hangs')
 def test_spectra_and_moments_4():
     spectra_and_moments_helper(spec4)
 
 
-@skip_when_service_backend(message='very slow / hangs')
+@skip_when_service_backend(message='intermittently hangs')
 def test_spectra_and_moments_5():
     spectra_and_moments_helper(spec5)
