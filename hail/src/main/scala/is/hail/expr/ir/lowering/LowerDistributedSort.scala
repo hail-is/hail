601e7196a97b75e81dc42d687a1ff9a7c0abaa37
John Compitello <j301600@gmail.com>
2022-01-27 13:46:11 -0500

    Successfully doing a best effort partitioning of segments into roughly even partitions

--- hail/src/main/scala/is/hail/expr/ir/lowering/LowerDistributedSort.scala
+++ hail/src/main/scala/is/hail/expr/ir/lowering/LowerDistributedSort.scala
@@ -294,17 +294,7 @@ object LowerDistributedSort {
       i = i + 1
     }
 
-    val unorderedSegments = loopState.smallSegments.map(x => (x, false)) ++ loopState.largeSortedSegments.map(x => (x, true))
-    val orderedSegments = unorderedSegments.sortWith{ (srt1, srt2) => lessThanForSegmentIndices(srt1._1.indices, srt2._1.indices)}
-
-    // TODO: Partitions are not broken up evenly.
-
-    // Ok, what if I write out all the unsorted segments for the purposes of sorting them? Then I can just do
-    // segmentToPartitionData to make the partitions?
-
-    val (needSorting, alreadySorted) = orderedSegments.zipWithIndex.partition { case ((sr, isSorted), idx) => isSorted}
-    val needSortingFilenames = needSorting.map(_._1._1.chunks.map(_.filename))
-    val needSortingPositionInSegmentList = needSorting.map(x => x._2)
+    val needSortingFilenames = loopState.smallSegments.map(_.chunks.map(_.filename))
     val needSortingFilenamesContext = Literal(TArray(TArray(TString)), needSortingFilenames)
 
     val sortedFilenamesIR = cdaIR(ToStream(needSortingFilenamesContext), MakeStruct(Seq())) { case (ctxRef, _) =>
@@ -319,32 +309,37 @@ object LowerDistributedSort {
       WritePartition(sortedStream, UUID4(), writer)
     }
 
-    val mySortedFilenames = CompileAndEvaluate[Annotation](ctx, sortedFilenamesIR).asInstanceOf[IndexedSeq[Row]].map(_(0).asInstanceOf[String])
-    val filesWithPositions = mySortedFilenames.zip(needSortingPositionInSegmentList)
+    val myReturn = CompileAndEvaluate[Annotation](ctx, sortedFilenamesIR).asInstanceOf[IndexedSeq[Row]]
+    val mySortedFilenames = myReturn.map(_(0).asInstanceOf[String])
+    val newlySortedSegments = loopState.smallSegments.zip(mySortedFilenames).map { case (sr, newFilename) =>
+      SegmentResult(sr.indices, sr.interval, IndexedSeq(Chunk(initialTmpPath + newFilename, sr.chunks.foldLeft(0)((size, chunk) => size + chunk.size))))
+    }
 
-    val contextData = orderedSegments.map { case (segment, isSorted) => Row(segment.chunks.map(chunk => chunk.filename), isSorted) }
-    val contexts = ToStream(Literal(TArray(TStruct("files" -> TArray(TString), "isSorted" -> TBoolean)), contextData))
+    val unorderedSegments = newlySortedSegments ++ loopState.largeSortedSegments
+    val orderedSegments = unorderedSegments.sortWith{ (srt1, srt2) => lessThanForSegmentIndices(srt1.indices, srt2.indices)}
+
+    // Now let's treat the whole thing as one segment that can be partitioned by the segmentToPartitionData method.
+    val megaSegment = SegmentResult(IndexedSeq(), null, orderedSegments.flatMap(sr => sr.chunks))
+    val partitioned = segmentsToPartitionData(IndexedSeq(megaSegment), idealNumberOfRowsPerPart).flatten
+
+    val contextData = partitioned.map { part => Row(part.files) }
+    val contexts = ToStream(Literal(TArray(TStruct("files" -> TArray(TString))), contextData))
 
     // Note: If all of the sort fields are not ascending, the the resulting table is sorted, but not keyed.
     val keyed = sortFields.forall(sf => sf.sortOrder == Ascending)
     val (partitionerKey, intervals) = if (keyed) {
-      (keyToSortBy, orderedSegments.map{ case (segment, _) => segment.interval})
+      (keyToSortBy, orderedSegments.map{ segment => segment.interval})
     } else {
       (TStruct(), orderedSegments.map{ _ => Interval(Row(), Row(), true, false)})
     }
+
     val partitioner = new RVDPartitioner(partitionerKey, intervals)
     val finalTs = TableStage(initialGlobalsLiteral, partitioner, TableStageDependency.none, contexts, { ctxRef =>
       val filenames = GetField(ctxRef, "files")
       val partitionInputStream = flatMapIR(ToStream(filenames)) { fileName =>
         ReadPartition(fileName, spec._vType, reader)
       }
-      val newKeyFieldNames = keyToSortBy.fields.map(_.name)
-      If(GetField(ctxRef, "isSorted"),
-        partitionInputStream,
-        ToStream(sortIR(partitionInputStream) { (refLeft, refRight) =>
-          ApplyComparisonOp(StructLT(keyToSortBy, sortFields), SelectFields(refLeft, newKeyFieldNames), SelectFields(refRight, newKeyFieldNames))
-        })
-      )
+      partitionInputStream
     })
 
     finalTs
@@ -402,7 +397,7 @@ object LowerDistributedSort {
         if (chunk.size > 0) {
           currentFiles.append(chunk.filename)
           currentPartSize += chunk.size
-          if (currentPartSize > idealNumberOfRowsPerPart) {
+          if (currentPartSize >= idealNumberOfRowsPerPart) {
             groupedIntoParts.append(PartitionInfo(sr.indices, currentFiles.result().toIndexedSeq, currentPartSize))
             currentFiles.clear()
             currentPartSize = 0
