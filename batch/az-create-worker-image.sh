b5844279cb9579574d22a4fca8e7c2f7d9af1e2e
Daniel Goldstein <danielgold95@gmail.com>
2022-01-31 15:52:51 -0500

    get global-config data from k8s and not manual input

--- batch/az-create-worker-image.sh
+++ batch/az-create-worker-image.sh
@@ -3,25 +3,25 @@
 set -ex
 
 if [ -z "$1" ] || [ -z  "$2" ] || [ -z "$3" ]; then
-    echo "Usage: ./az-create-worker-image.sh <RESOURCE_GROUP> <REGION> <USERNAME>"
+    echo "Usage: ./az-create-worker-image.sh <SSH_USERNAME>"
     exit 1
 fi
 
-RESOURCE_GROUP=$1
-REGION=$2
-USERNAME=$3
+USERNAME=$1
 
-CONTAINER_REGISTRY_NAME=${RESOURCE_GROUP}
+download-secret global-config
+SUBSCRIPTION_ID=$(cat contents/azure_subscription_id)
+RESOURCE_GROUP=$(cat contents/azure_resource_group)
+LOCATION=$(cat contents/azure_location)
+CONTAINER_REGISTRY_NAME=$(cat contents/container_registry_name)
+DOCKER_ROOT_IMAGE=$(cat contents/docker_root_image)
+cd -
+
 SHARED_GALLERY_NAME="${RESOURCE_GROUP}_batch"
 BUILD_IMAGE_RESOURCE_GROUP="${RESOURCE_GROUP}-build-batch-worker-image"
 VM_NAME=build-batch-worker-image
 WORKER_VERSION=0.0.12
 
-DOCKER_ROOT_IMAGE=$(kubectl get secret global-config \
-    --template={{.data.docker_root_image}} \
-    | base64 --decode)
-
-SUBSCRIPTION_ID=$(az account list | jq -rj '.[0].id')
 BATCH_WORKER_PRINCIPAL_ID=$(az identity show \
     --resource-group ${RESOURCE_GROUP} \
     --name batch-worker \
@@ -36,7 +36,7 @@ BATCH_WORKER_IDENTITY=$(az identity show \
 echo "Creating $BUILD_IMAGE_RESOURCE_GROUP resource group..."
 
 az group delete --name $BUILD_IMAGE_RESOURCE_GROUP --yes || true
-az group create --name $BUILD_IMAGE_RESOURCE_GROUP --location ${REGION}
+az group create --name $BUILD_IMAGE_RESOURCE_GROUP --location ${LOCATION}
 
 az role assignment create \
     --resource-group $BUILD_IMAGE_RESOURCE_GROUP \
@@ -95,7 +95,7 @@ az sig image-version create \
     --gallery-name ${SHARED_GALLERY_NAME} \
     --gallery-image-definition batch-worker \
     --gallery-image-version ${WORKER_VERSION} \
-    --target-regions ${REGION} \
+    --target-regions ${LOCATION} \
     --replica-count 1 \
     --managed-image "/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${BUILD_IMAGE_RESOURCE_GROUP}/providers/Microsoft.Compute/virtualMachines/${VM_NAME}"
 
