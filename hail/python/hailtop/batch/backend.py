cf3178f0510d40f44ea72bc704db52c26388c42e
Jackie Goldstein <jigold@broadinstitute.org>
2022-02-08 16:07:23 -0500

    fix

--- hail/python/hailtop/batch/backend.py
+++ hail/python/hailtop/batch/backend.py
@@ -445,14 +445,12 @@ class ServiceBackend(Backend[bc.Batch]):
         gcs_kwargs = {'project': google_project}
         self.__fs: AsyncFS = RouterAsyncFS(default_scheme='file', gcs_kwargs=gcs_kwargs)
 
-        self._batch_client: Optional[BatchClient] = None
-
     @property
     def _fs(self):
         return self.__fs
 
     def _close(self):
-        if self._batch_client is not None:
+        if hasattr(self, '_batch_client'):
             self._batch_client.close()
         async_to_blocking(self._fs.close())
 
