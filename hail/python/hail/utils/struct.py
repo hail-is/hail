ec8547d37a5937a57216153a8179d8e76bbb57a4
Carolin Diaz <63973811+CDiaz96@users.noreply.github.com>
2022-02-23 14:30:00 -0500

    addressed changes

--- hail/python/hail/utils/struct.py
+++ hail/python/hail/utils/struct.py
@@ -115,28 +115,26 @@ class Struct(Mapping):
         :class:`.Struct`
             Struct with new or updated fields.
 
-
         Examples
         --------
         
         Define a Struct `s`
         
-        >>> s = hl.Struct(**{'food': 8, 'fruit': 5})
-        >>> pprint(s)
+        >>> s = hl.Struct(food=8, fruit=5)
         
         Add a new field to `s`
         
-        >>> s.annotate(**{'bar': 2})
+        >>> s.annotate(bar=2)
         Struct(food=8, fruit=5, bar=2)
         
         Add multiple fields to `s`
         
-        >>> s.annotate(**{'banana': 2, 'apple': 3})
+        >>> s.annotate(banana=2, apple=3)
         Struct(food=8, fruit=5, banana=2, apple=3)
         
         Recompute an existing field in `s`
         
-        >>> s.annotate(**{'bar': 4, 'fuit': 2})
+        >>> s.annotate(bar=4, fuit=2)
         Struct(food=8, fruit=2, bar=4)
         """
         d = OrderedDict(self.items())
