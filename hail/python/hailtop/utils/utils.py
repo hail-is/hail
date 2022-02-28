0a21c9135ba03564b9d745aa192fc7b29d33fcbb
Carolin Diaz <63973811+CDiaz96@users.noreply.github.com>
2022-02-27 22:28:49 -0500

    added type annotation to digits_needed (#11421)

--- hail/python/hailtop/utils/utils.py
+++ hail/python/hailtop/utils/utils.py
@@ -100,7 +100,7 @@ def secret_alnum_string(n=22, *, case=None):
     return ''.join([secrets.choice(alphabet) for _ in range(n)])
 
 
-def digits_needed(i: int):
+def digits_needed(i: int) -> int:
     assert i >= 0
     if i < 10:
         return 1
