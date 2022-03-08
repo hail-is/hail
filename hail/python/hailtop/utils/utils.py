6b9be5ba502da290b378eeacbc94c08e96dfbbd4
Carolin Diaz <63973811+CDiaz96@users.noreply.github.com>
2022-03-08 10:50:04 -0500

    update

--- hail/python/hailtop/utils/utils.py
+++ hail/python/hailtop/utils/utils.py
@@ -116,7 +116,7 @@ def grouped(n: int, ls: List[Any]) -> Iterable[Any]:
         yield group
 
 
-def partition(k: int, ls: List[int]) -> Iterable[Any]:
+def partition(k: int, ls: List[int]) -> Iterable[List]:
     if k == 0:
         assert not ls
         return []
