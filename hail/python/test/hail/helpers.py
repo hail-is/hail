2e6e0fd091c6b0deed8f26cb79738c4a18b5616b
Daniel King <dking@broadinstitute.org>
2022-03-17 11:40:03 -0400

    skip golden master

--- hail/python/test/hail/helpers.py
+++ hail/python/test/hail/helpers.py
@@ -130,6 +130,17 @@ def skip_unless_spark_backend():
 
     return wrapper
 
+def skip_when_service_backend(reason='skipping for Service Backend'):
+    from hail.backend.service_backend import ServiceBackend
+    @decorator
+    def wrapper(func, *args, **kwargs):
+        if isinstance(hl.utils.java.Env.backend(), ServiceBackend):
+            return func(*args, **kwargs)
+        else:
+            raise unittest.SkipTest(reason)
+
+    return wrapper
+
 
 fails_local_backend = pytest.mark.xfail(
     os.environ.get('HAIL_QUERY_BACKEND') == 'local',
