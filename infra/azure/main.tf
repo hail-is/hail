062ccbea8888af09a8c169e95019e33884a11dd0
Daniel Goldstein <danielgold95@gmail.com>
2022-03-21 15:29:02 -0400

    appease azurerm provider

--- infra/azure/main.tf
+++ infra/azure/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.97.0"
+      version = "=2.99.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
