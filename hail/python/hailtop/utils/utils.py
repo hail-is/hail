d5806318f6a7f2f27137c4d97cc687c29e243db0
Daniel Goldstein <danielgold95@gmail.com>
2022-03-23 11:55:55 -0400

    better clarification

--- hail/python/hailtop/utils/utils.py
+++ hail/python/hailtop/utils/utils.py
@@ -837,7 +837,8 @@ async def run_if_changed(changed, f, *args, **kwargs):
     while True:
         changed.clear()
         should_wait = await f(*args, **kwargs)
-        # Require half a second between iterations to avoid
+        # 0.5 is arbitrary, but should be short enough not to greatly
+        # increase latency and long enough to reduce the impact of
         # wasteful spinning when `should_wait` is always true and the
         # event is constantly being set. This was instated to
         # avoid wasteful repetition of scheduling loops, but
