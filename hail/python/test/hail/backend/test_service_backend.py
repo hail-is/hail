385aa48512d3367c8cf3d547cb4aa9c89212ce5e
Daniel King <dking@broadinstitute.org>
2022-03-27 15:34:02 -0400

    need a bigger table to trigger memory issues in the standard driver

--- hail/python/test/hail/backend/test_service_backend.py
+++ hail/python/test/hail/backend/test_service_backend.py
@@ -5,7 +5,7 @@ from ..helpers import skip_unless_service_backend
 @skip_unless_service_backend()
 def test_tiny_driver_has_tiny_memory():
     try:
-        hl.utils.range_table(10_000_000, 50).to_pandas()
+        hl.utils.range_table(100_000_000, 50).to_pandas()
     except Exception as exc:
         assert 'java.lang.OutOfMemoryError: Java heap space' in exc.args[0]
     else:
@@ -18,7 +18,7 @@ def test_big_driver_has_big_memory():
     try:
         hl.current_backend().driver_cores = 8
         hl.current_backend().driver_memory = 'highmem'
-        hl.utils.range_table(10_000_000, 50).to_pandas()
+        hl.utils.range_table(100_000_000, 50).to_pandas()
     finally:
         hl.current_backend().driver_cores = old_driver_cores
         hl.current_backend().driver_memory = old_driver_memory
