8d8800221feca14113530680eb0e7f3c6a7a79ba
Jackie Goldstein <jigold@broadinstitute.org>
2022-04-08 10:16:18 -0400

    [batch] Reattempt adding resource usage plots
    ```
    32 bytes per measurement * 12 measurements per minute * 60 minutes / 1024 = 22.5KB
    ```

--- hail/python/hailtop/utils/__init__.py
+++ hail/python/hailtop/utils/__init__.py
@@ -1,4 +1,6 @@
-from .time import time_msecs, time_msecs_str, humanize_timedelta_msecs, parse_timestamp_msecs
+from .time import (
+    time_msecs, time_msecs_str, humanize_timedelta_msecs, parse_timestamp_msecs,
+    time_ns)
 from .utils import (
     unzip, async_to_blocking, blocking_to_async, AsyncWorkerPool,
     bounded_gather, grouped, sync_sleep_and_backoff, sleep_and_backoff, is_transient_error,
@@ -91,4 +93,5 @@ __all__ = [
     'parse_timestamp_msecs',
     'Timings',
     'is_retry_once_error',
+    'time_ns',
 ]
