7304f29d879f14ed6a1697475258c967c8d7534f
Daniel King <dking@broadinstitute.org>
2022-04-11 09:24:01 -0400

    support HadoopFS

--- hail/python/test/hail/utils/test_utils.py
+++ hail/python/test/hail/utils/test_utils.py
@@ -121,11 +121,14 @@ class Tests(unittest.TestCase):
         self.assertEqual(stat2['is_dir'], False)
         self.assertTrue('path' in stat2)
 
+    @fails_local_backend()
     def test_hadoop_no_glob_in_bucket(self):
         try:
             hl.hadoop_ls('gs://glob*bucket')
         except ValueError as err:
             assert 'glob pattern only allowed in path (e.g. not in bucket): gs://glob*bucket' in err.args[0]
+        except FatalError as err:
+            assert "Invalid bucket name 'glob*bucket': bucket name must contain only 'a-z0-9_.-' characters." in err.args[0]
         else:
             assert False
 
