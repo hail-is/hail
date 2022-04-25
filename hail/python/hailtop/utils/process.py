bd791f1d3d6a423e1e42259f6f453d4ad80f7c3d
Daniel Goldstein <danielgold95@gmail.com>
2022-04-25 10:36:34 -0400

    fix signature

--- hail/python/hailtop/utils/process.py
+++ hail/python/hailtop/utils/process.py
@@ -52,11 +52,10 @@ def sync_check_shell(script: str, echo=False) -> None:
     sync_check_shell_output(script, echo)
 
 
-def sync_check_exec(command: str, *args: str, echo: bool = False, capture_output: bool = False) -> None:
-    command_args = [command, *args]
+def sync_check_exec(*command_args: str, echo: bool = False, capture_output: bool = False) -> None:
     if echo:
         print(command_args)
     try:
         subprocess.run(command_args, check=True, capture_output=capture_output)
     except subprocess.CalledProcessError as e:
-        raise CalledProcessError(command_args, e.returncode, (e.stdout, e.stderr)) from e
+        raise CalledProcessError(list(command_args), e.returncode, (e.stdout, e.stderr)) from e
