0dd5d5719f71b98830cca0d2bdca6bbeaae158b0
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-05-26 22:59:47 +1000

    Add label as allowed resource key

--- batch/batch/front_end/validate.py
+++ batch/batch/front_end/validate.py
@@ -86,6 +86,7 @@ job_validator = keyed(
                 'cpu': regex(CPU_REGEXPAT, CPU_REGEX),
                 'storage': regex(STORAGE_REGEXPAT, STORAGE_REGEX),
                 'machine_type': str_type,
+                'label': str_type,
                 'preemptible': bool_type,
             }
         ),
