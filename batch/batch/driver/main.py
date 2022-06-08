b3c94127ee0798b32e3176c524099171332d9e12
Jackie Goldstein <jigold@broadinstitute.org>
2022-06-08 17:27:55 -0400

    [batch] Fix access logger filters

--- batch/batch/driver/main.py
+++ batch/batch/driver/main.py
@@ -1236,8 +1236,8 @@ class BatchDriverAccessLogger(AccessLogger):
         ]
 
     def log(self, request, response, time):
-        for scheme, path_expr in self.exclude:
-            if path_expr.fullmatch(request.path) and scheme == request.scheme:
+        for method, path_expr in self.exclude:
+            if path_expr.fullmatch(request.path) and method == request.method:
                 return
 
         super().log(request, response, time)
