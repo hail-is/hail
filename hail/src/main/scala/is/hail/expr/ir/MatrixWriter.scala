hail-is/hail

commit 4ea7537d2029faa0c73906ba151fd898df15226e

[query] Fix text export part file overwrite due to speculation (#11910)
We do not make guarantees about the exact contents of a folder after
a parallel export. We do guarantee that all data is in the folder
however, so we must make it speculation safe. We do so by adding a UUID
to each part file we write for in lowered text export.

hail/src/main/scala/is/hail/expr/ir/MatrixWriter.scala
@@ -427,7 +427,7 @@ case class MatrixVCFWriter(
 
     ts.mapContexts { oldCtx =>
       val d = digitsNeeded(ts.numPartitions)
-      val partFiles = Literal(TArray(TString), Array.tabulate(ts.numPartitions)(i => s"$folder/${ partFile(d, i) }$ext").toFastIndexedSeq)
+      val partFiles = Literal(TArray(TString), Array.tabulate(ts.numPartitions)(i => s"$folder/${ partFile(d, i) }-").toFastIndexedSeq)
 
       zip2(oldCtx, ToStream(partFiles), ArrayZipBehavior.AssertSameLength) { (ctxElt, pf) =>
         MakeStruct(FastSeq(
@@ -435,9 +435,10 @@ case class MatrixVCFWriter(
           "partFile" -> pf))
       }
     }(GetField(_, "oldCtx")).mapCollectWithContextsAndGlobals(relationalLetsAbove) { (rows, ctxRef) =>
+      val partFile = GetField(ctxRef, "partFile") + UUID4() + Str(ext)
       val ctx = MakeStruct(FastSeq(
         "cols" -> GetField(ts.globals, colsFieldName),
-        "partFile" -> GetField(ctxRef, "partFile")))
+        "partFile" -> partFile))
       WritePartition(rows, ctx, lineWriter)
     }{ (parts, globals) =>
       val ctx = MakeStruct(FastSeq("cols" -> GetField(globals, colsFieldName), "partFiles" -> parts))
@@ -831,7 +832,7 @@ case class MatrixGENWriter(
 
     ts.mapContexts { oldCtx =>
       val d = digitsNeeded(ts.numPartitions)
-      val partFiles = Literal(TArray(TString), Array.tabulate(ts.numPartitions)(i => s"$folder/${ partFile(d, i) }").toFastIndexedSeq)
+      val partFiles = Literal(TArray(TString), Array.tabulate(ts.numPartitions)(i => s"$folder/${ partFile(d, i) }-").toFastIndexedSeq)
 
       zip2(oldCtx, ToStream(partFiles), ArrayZipBehavior.AssertSameLength) { (ctxElt, pf) =>
         MakeStruct(FastSeq(
@@ -839,7 +840,7 @@ case class MatrixGENWriter(
           "partFile" -> pf))
       }
     }(GetField(_, "oldCtx")).mapCollectWithContextsAndGlobals(relationalLetsAbove) { (rows, ctxRef) =>
-      val ctx = GetField(ctxRef, "partFile")
+      val ctx = GetField(ctxRef, "partFile") + UUID4()
       WritePartition(rows, ctx, lineWriter)
     }{ (parts, globals) =>
       val cols = ToStream(GetField(globals, colsFieldName))

hail/src/main/scala/is/hail/expr/ir/TableWriter.scala
@@ -465,7 +465,7 @@ case class TableTextWriter(
 
     ts.mapContexts { oldCtx =>
       val d = digitsNeeded(ts.numPartitions)
-      val partFiles = Literal(TArray(TString), Array.tabulate(ts.numPartitions)(i => s"$folder/${ partFile(d, i) }$ext").toFastIndexedSeq)
+      val partFiles = Literal(TArray(TString), Array.tabulate(ts.numPartitions)(i => s"$folder/${ partFile(d, i) }-").toFastIndexedSeq)
 
       zip2(oldCtx, ToStream(partFiles), ArrayZipBehavior.AssertSameLength) { (ctxElt, pf) =>
         MakeStruct(FastSeq(
@@ -473,7 +473,7 @@ case class TableTextWriter(
           "partFile" -> pf))
       }
     }(GetField(_, "oldCtx")).mapCollectWithContextsAndGlobals(relationalLetsAbove) { (rows, ctxRef) =>
-      val file = GetField(ctxRef, "partFile")
+      val file = GetField(ctxRef, "partFile") + UUID4() + Str(ext)
       WritePartition(rows, file, lineWriter)
     } { (parts, _) =>
       val commit = TableTextFinalizer(path, ts.rowType, delimiter, header, exportType)

commit b6d74c34f04ea76729c9c46c61ae931a9e943df9

[query] Remove unexpected files from parallel text exports
This fixes an issue introduced by #11910, where extra part files may be
created by speculative execution. This is an issue for parallel export
where we did not delete any files.

Now we list the output directory, and compare it to the proper list of
files. The paths that are not in the list of 'good' paths are deleted.

hail/src/main/scala/is/hail/expr/ir/MatrixWriter.scala
@@ -760,23 +760,22 @@ case class VCFExportFinalizer(typ: MatrixType, outputPath: String, append: Optio
 
     val annotations = writeAnnotations.get(cb).asBaseStruct
 
+    val partPaths = annotations.loadField(cb, "partFiles").get(cb)
+    val files = partPaths.castTo(cb, region, SJavaArrayString(true), false).asInstanceOf[SJavaArrayStringValue].array
     exportType match {
       case ExportType.CONCATENATED =>
         val headerStr = header(cb, annotations)
 
-        val partPaths = annotations.loadField(cb, "partFiles").get(cb)
-        val files = partPaths.castTo(cb, region, SJavaArrayString(true), false)
         val headerFilePath = ctx.createTmpPath("header", ext)
         val os = cb.memoize(cb.emb.create(const(headerFilePath)))
         cb += os.invoke[Array[Byte], Unit]("write", headerStr.invoke[Array[Byte]]("getBytes"))
         cb += os.invoke[Int, Unit]("write", '\n')
         cb += os.invoke[Unit]("close")
 
-        val partFiles = files.asInstanceOf[SJavaArrayStringValue].array
-        val jFiles = cb.memoize(Code.newArray[String](partFiles.length + 1))
+        val jFiles = cb.memoize(Code.newArray[String](files.length + 1))
         cb += (jFiles(0) = const(headerFilePath))
         cb += Code.invokeStatic5[System, Any, Int, Any, Int, Int, Unit](
-          "arraycopy", partFiles /*src*/, 0 /*srcPos*/, jFiles /*dest*/, 1 /*destPos*/, partFiles.length /*len*/)
+          "arraycopy", files /*src*/, 0 /*srcPos*/, jFiles /*dest*/, 1 /*destPos*/, files.length /*len*/)
 
         cb += cb.emb.getFS.invoke[Array[String], String, Unit]("concatenateFiles", jFiles, const(outputPath))
 
@@ -790,9 +789,11 @@ case class VCFExportFinalizer(typ: MatrixType, outputPath: String, append: Optio
         }
 
       case ExportType.PARALLEL_HEADER_IN_SHARD =>
+        cb += Code.invokeScalaObject3[FS, String, Array[String], Unit](TableTextFinalizer.getClass, "cleanup", cb.emb.getFS, outputPath, files)
         cb += cb.emb.getFS.invoke[String, Unit]("touch", const(outputPath).concat("/_SUCCESS"))
 
       case ExportType.PARALLEL_SEPARATE_HEADER =>
+        cb += Code.invokeScalaObject3[FS, String, Array[String], Unit](TableTextFinalizer.getClass, "cleanup", cb.emb.getFS, outputPath, files)
         val headerFilePath = s"$outputPath/header$ext"
         val headerStr = header(cb, annotations)
 

hail/src/main/scala/is/hail/expr/ir/TableWriter.scala
@@ -509,6 +509,14 @@ case class TableTextPartitionWriter(rowType: TStruct, delimiter: String, writeHe
   }
 }
 
+object TableTextFinalizer {
+  def cleanup(fs: FS, outputPath: String, files: Array[String]): Unit = {
+    val outputFiles = fs.listStatus(fs.makeQualified(outputPath)).map(_.getPath).toSet
+    val fileSet = files.map(fs.makeQualified(_)).toSet
+    outputFiles.diff(fileSet).foreach(fs.delete(_, false))
+  }
+}
+
 case class TableTextFinalizer(outputPath: String, rowType: TStruct, delimiter: String,
     header: Boolean = true, exportType: String = ExportType.CONCATENATED) extends MetadataWriter {
   def annotationType: Type = TArray(TString)
@@ -516,9 +524,9 @@ case class TableTextFinalizer(outputPath: String, rowType: TStruct, delimiter: S
     val ctx: ExecuteContext = cb.emb.ctx
     val ext = ctx.fs.getCodecExtension(outputPath)
     val partPaths = writeAnnotations.get(cb, "write annotations cannot be missing!")
+    val files = partPaths.castTo(cb, region, SJavaArrayString(true), false).asInstanceOf[SJavaArrayStringValue].array
     exportType match {
       case ExportType.CONCATENATED =>
-        val files = partPaths.castTo(cb, region, SJavaArrayString(true), false)
         val jFiles = if (header) {
           val headerFilePath = ctx.createTmpPath("header", ext)
           val headerStr = rowType.fields.map(_.name).mkString(delimiter)
@@ -527,14 +535,13 @@ case class TableTextFinalizer(outputPath: String, rowType: TStruct, delimiter: S
           cb += os.invoke[Int, Unit]("write", '\n')
           cb += os.invoke[Unit]("close")
 
-          val jFiles = files.asInstanceOf[SJavaArrayStringValue].array
-          val allFiles = cb.memoize(Code.newArray[String](jFiles.length + 1))
+          val allFiles = cb.memoize(Code.newArray[String](files.length + 1))
           cb += (allFiles(0) = const(headerFilePath))
           cb += Code.invokeStatic5[System, Any, Int, Any, Int, Int, Unit](
-            "arraycopy", jFiles /*src*/, 0 /*srcPos*/, allFiles /*dest*/, 1 /*destPos*/, jFiles.length /*len*/)
+            "arraycopy", files /*src*/, 0 /*srcPos*/, allFiles /*dest*/, 1 /*destPos*/, files.length /*len*/)
           allFiles
         } else {
-          files.asInstanceOf[SJavaArrayStringValue].array
+          files
         }
 
         cb += cb.emb.getFS.invoke[Array[String], String, Unit]("concatenateFiles", jFiles, const(outputPath))
@@ -545,9 +552,11 @@ case class TableTextFinalizer(outputPath: String, rowType: TStruct, delimiter: S
         })
 
       case ExportType.PARALLEL_HEADER_IN_SHARD =>
+        cb += Code.invokeScalaObject3[FS, String, Array[String], Unit](TableTextFinalizer.getClass, "cleanup", cb.emb.getFS, outputPath, files)
         cb += cb.emb.getFS.invoke[String, Unit]("touch", const(outputPath).concat("/_SUCCESS"))
 
       case ExportType.PARALLEL_SEPARATE_HEADER =>
+        cb += Code.invokeScalaObject3[FS, String, Array[String], Unit](TableTextFinalizer.getClass, "cleanup", cb.emb.getFS, outputPath, files)
         if (header) {
           val headerFilePath = s"$outputPath/header$ext"
           val headerStr = rowType.fields.map(_.name).mkString(delimiter)

hail/python/test/hail/methods/test_impex.py
@@ -567,6 +567,7 @@ class VCFTests(unittest.TestCase):
                        if os.path.splitext(info.path)[-1] == '.bgz']
         assert shard_paths
         shard_paths.sort()
+        print(shard_paths)
         nf = new_temp_file(extension='vcf.bgz')
         concat_files(nf, shard_paths)
 