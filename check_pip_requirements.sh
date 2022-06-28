a11ce830184c6a95f3168cb530b63ab1c70b42b5
Dan King <daniel.zidan.king@gmail.com>
2022-06-28 17:32:55 -0400

    [build.yaml] on difference, dump up-to-date pinned requirements (#11953)
    * [build.yaml] on difference, dump up-to-date pinned requirements
    
    * Update check_pip_requirements.sh

--- check_pip_requirements.sh
+++ check_pip_requirements.sh
@@ -12,4 +12,8 @@ pip-compile --quiet $reqs $pinned --output-file=$new_pinned
 # Get rid of comments that might differ despite requirements being the same
 cat $pinned | sed '/#/d' > $pinned_no_comments
 cat $new_pinned | sed '/#/d' > $new_pinned_no_comments
-diff $pinned_no_comments $new_pinned_no_comments
+diff $pinned_no_comments $new_pinned_no_comments || {
+    echo '>>> up-to-date pinned requirements <<<'
+    cat $new_pinned
+    exit 1
+}
