131d94e12799b3e8e1a728ff8b4213bdc0590b5f
Daniel King <dking@broadinstitute.org>
2022-06-29 17:50:30 -0400

    fix gcs version to 2.2.7, 2.7.1 is the hadoop version we use

--- batch/download-gcs-connector.py
+++ batch/download-gcs-connector.py
@@ -2,6 +2,6 @@ import os
 import shutil
 import urllib.request
 
-with urllib.request.urlopen("https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop2-2.7.1.jar") as input:
-    with open(os.environ['SPARK_HOME'] + "/jars/gcs-connector-hadoop2-2.7.1.jar", 'wb') as output:
+with urllib.request.urlopen("https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop2-2.2.7.jar") as input:
+    with open(os.environ['SPARK_HOME'] + "/jars/gcs-connector-hadoop2-2.2.7.jar", 'wb') as output:
         shutil.copyfileobj(input, output)
