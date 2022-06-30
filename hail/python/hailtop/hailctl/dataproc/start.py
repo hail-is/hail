9578db2f9a7aa29901a58a259e055066521abbcb
Daniel King <dking@broadinstitute.org>
2022-06-30 09:48:40 -0400

    [dataproc] update dataproc 2.0.44 and Spark 3.1.3
    CHANGELOG: Update to latest dataproc image: 2.0.44-debian10. Update to Spark 2.1.3.

--- hail/python/hailtop/hailctl/dataproc/start.py
+++ hail/python/hailtop/hailctl/dataproc/start.py
@@ -135,7 +135,7 @@ REGION_TO_REPLICATE_MAPPING = {
 
 ANNOTATION_DB_BUCKETS = ["hail-datasets-us", "hail-datasets-eu"]
 
-IMAGE_VERSION = '2.0.29-debian10'
+IMAGE_VERSION = '2.0.44-debian10'
 
 
 def init_parser(parser):
