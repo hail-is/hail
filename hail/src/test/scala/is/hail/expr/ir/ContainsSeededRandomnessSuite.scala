3dd44cdaf1db774a83970e8c737f9d6cbf1bf3cd
Tim Poterba <tpoterba@gmail.com>
2022-07-22 13:55:29 -0400

    fix rand invoke

--- hail/src/test/scala/is/hail/expr/ir/ContainsSeededRandomnessSuite.scala
+++ hail/src/test/scala/is/hail/expr/ir/ContainsSeededRandomnessSuite.scala
@@ -2,6 +2,7 @@ package is.hail.expr.ir
 
 import is.hail.HailSuite
 import is.hail.types.virtual.TBoolean
+import is.hail.utils.FastIndexedSeq
 import org.testng.annotations.Test
 
 class ContainsSeededRandomnessSuite extends HailSuite {
@@ -17,7 +18,8 @@ class ContainsSeededRandomnessSuite extends HailSuite {
     val m2 = ContainsSeededRandomness.analyze(ir2)
     assert(m2.get(ir2.newRow).contains(false))
 
-    val ir3 = TableAggregate(tr, invokeSeeded("rand_bool", 1L, TBoolean, F64(0.5)))
+    val rand = ApplySeeded("rand_bool", FastIndexedSeq(0.5), RNGStateLiteral(), seed=0, TBoolean)
+    val ir3 = TableAggregate(tr, rand)
     val m3 = ContainsSeededRandomness.analyze(ir3)
     assert(m3.get(ir3.query).contains(true))
     assert(m3.get(ir3).contains(false))
