7583a3881268c551d845f95bca08dee5942db639
jigold <jigold@users.noreply.github.com>
2022-07-29 14:58:51 -0400

    [batch] Revert trigger back compat change to attempt resources (#11995)

--- batch/sql/estimated-current.sql
+++ batch/sql/estimated-current.sql
@@ -588,43 +588,40 @@ BEGIN
   DECLARE cur_start_time BIGINT;
   DECLARE cur_end_time BIGINT;
   DECLARE cur_billing_project VARCHAR(100);
-  DECLARE cur_format_version INT;
   DECLARE msec_diff BIGINT;
   DECLARE cur_n_tokens INT;
   DECLARE rand_token INT;
   DECLARE cur_resource VARCHAR(100);
 
-  SELECT billing_project, format_version INTO cur_billing_project, cur_format_version
+  SELECT billing_project INTO cur_billing_project
   FROM batches WHERE id = NEW.batch_id;
 
-  IF cur_format_version >= 3 THEN
-    SELECT n_tokens INTO cur_n_tokens FROM globals LOCK IN SHARE MODE;
-    SET rand_token = FLOOR(RAND() * cur_n_tokens);
+  SELECT n_tokens INTO cur_n_tokens FROM globals LOCK IN SHARE MODE;
+  SET rand_token = FLOOR(RAND() * cur_n_tokens);
 
-    SELECT resource INTO cur_resource FROM resources WHERE resource_id = NEW.resource_id;
+  SELECT resource INTO cur_resource FROM resources WHERE resource_id = NEW.resource_id;
 
-    SELECT start_time, end_time INTO cur_start_time, cur_end_time
-    FROM attempts
-    WHERE batch_id = NEW.batch_id AND job_id = NEW.job_id AND attempt_id = NEW.attempt_id
-    LOCK IN SHARE MODE;
+  SELECT start_time, end_time INTO cur_start_time, cur_end_time
+  FROM attempts
+  WHERE batch_id = NEW.batch_id AND job_id = NEW.job_id AND attempt_id = NEW.attempt_id
+  LOCK IN SHARE MODE;
 
-    SET msec_diff = GREATEST(COALESCE(cur_end_time - cur_start_time, 0), 0);
+  SET msec_diff = GREATEST(COALESCE(cur_end_time - cur_start_time, 0), 0);
 
-    INSERT INTO aggregated_billing_project_resources (billing_project, resource, token, `usage`)
-    VALUES (cur_billing_project, cur_resource, rand_token, NEW.quantity * msec_diff)
-    ON DUPLICATE KEY UPDATE
-      `usage` = `usage` + NEW.quantity * msec_diff;
+  INSERT INTO aggregated_billing_project_resources (billing_project, resource, token, `usage`)
+  VALUES (cur_billing_project, cur_resource, rand_token, NEW.quantity * msec_diff)
+  ON DUPLICATE KEY UPDATE
+    `usage` = `usage` + NEW.quantity * msec_diff;
 
-    INSERT INTO aggregated_batch_resources (batch_id, resource, token, `usage`)
-    VALUES (NEW.batch_id, cur_resource, rand_token, NEW.quantity * msec_diff)
-    ON DUPLICATE KEY UPDATE
-      `usage` = `usage` + NEW.quantity * msec_diff;
+  INSERT INTO aggregated_batch_resources (batch_id, resource, token, `usage`)
+  VALUES (NEW.batch_id, cur_resource, rand_token, NEW.quantity * msec_diff)
+  ON DUPLICATE KEY UPDATE
+    `usage` = `usage` + NEW.quantity * msec_diff;
 
-    INSERT INTO aggregated_job_resources (batch_id, job_id, resource, `usage`)
-    VALUES (NEW.batch_id, NEW.job_id, cur_resource, NEW.quantity * msec_diff)
-    ON DUPLICATE KEY UPDATE
-      `usage` = `usage` + NEW.quantity * msec_diff;
-  END IF;
+  INSERT INTO aggregated_job_resources (batch_id, job_id, resource, `usage`)
+  VALUES (NEW.batch_id, NEW.job_id, cur_resource, NEW.quantity * msec_diff)
+  ON DUPLICATE KEY UPDATE
+    `usage` = `usage` + NEW.quantity * msec_diff;
 END $$
 
 DROP PROCEDURE IF EXISTS recompute_incremental $$
