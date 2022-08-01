00ffbb69a231fd1bd46fcf3e9e43fa0ddf147772
Sabrina <9669990+violetbrina@users.noreply.github.com>
2022-08-01 10:12:55 +1000

    FIX: minor improvements

--- letsencrypt/letsencrypt.sh
+++ letsencrypt/letsencrypt.sh
@@ -10,7 +10,7 @@ else
     KUBECTL_APPLY=cat
 fi
 
-certbot certonly -v --standalone $CERTBOT_FLAGS --cert-name $DOMAIN -n --agree-tos -m sabrina.yan@populationgenomics.org.au -d $DOMAINS
+certbot certonly -v --standalone $CERTBOT_FLAGS --cert-name $DOMAIN -n --agree-tos -m cseed@broadinstitute.org -d $DOMAINS
 
 # https://github.com/certbot/certbot/blob/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
 cat >/options-ssl-nginx.conf <<EOF
