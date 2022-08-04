faab2663690035c8dcbfa672971a9f44caf65e89
Daniel Goldstein <danielgold95@gmail.com>
2022-08-04 10:17:46 -0400

    dont waste cycles on loops that have not done anything useful recently

--- hail/python/hailtop/utils/utils.py
+++ hail/python/hailtop/utils/utils.py
@@ -880,14 +880,17 @@ async def retry_long_running(name, f, *args, **kwargs):
 async def run_if_changed(changed, f, *args, **kwargs):
     while True:
         changed.clear()
+        start = time_msecs()
         should_wait = await f(*args, **kwargs)
+        end = time_msecs()
         # 0.5 is arbitrary, but should be short enough not to greatly
         # increase latency and long enough to reduce the impact of
         # wasteful spinning when `should_wait` is always true and the
         # event is constantly being set. This was instated to
         # avoid wasteful repetition of scheduling loops, but
         # might not always be desirable, especially in very low-latency batches.
-        # await asyncio.sleep(0.5)
+        if end - start < 100:
+            await asyncio.sleep(0.5)
         if should_wait:
             await changed.wait()
 
