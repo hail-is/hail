e472cdee5dc3feed9fe11d5ed77c5f989f381000
Jackie Goldstein <jigold@broadinstitute.org>
2022-08-22 11:16:23 -0400

    fix

--- batch/batch/front_end/front_end.py
+++ batch/batch/front_end/front_end.py
@@ -1176,14 +1176,14 @@ LOCK IN SHARE MODE''',
 
         bp_cost_record = await tx.execute_and_fetchone(
             '''
-SELECT COALESCE(SUM(t.`usage` * t.rate), 0) AS cost
+SELECT COALESCE(SUM(t.`usage` * rate), 0) AS cost
 FROM (
-  SELECT aggregated_billing_project_user_resources_v2.resource_id, CAST(COALESCE(SUM(`usage`), 0) AS SIGNED) AS `usage`
+  SELECT resource_id, CAST(COALESCE(SUM(`usage`), 0) AS SIGNED) AS `usage`
   FROM aggregated_billing_project_user_resources_v2
-  LEFT JOIN resources on resources.resource_id = aggregated_billing_project_user_resources_v2.resource_id
   WHERE billing_project = %s
-  GROUP BY aggregated_billing_project_user_resources_v2.resource_id
-) AS t;
+  GROUP BY resource_id
+) AS t
+LEFT JOIN resources on resources.resource_id = t.resource_id;
 ''',
             (billing_project,),
         )
