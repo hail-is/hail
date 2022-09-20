589a0b12c610c51b108f13cd8a2402805793f9fa
Tim Poterba <tpoterba@gmail.com>
2022-09-20 16:21:37 -0400

    fix path calculation

--- hail/python/hail/methods/impex.py
+++ hail/python/hail/methods/impex.py
@@ -3234,7 +3234,7 @@ def import_gvs(refs: 'List[List[str]]',
         vds_paths.append(path)
 
         if idx < intermediate_resume_point:
-            n_samples += hl.vds.read_vds(idx).n_samples()
+            n_samples += hl.vds.read_vds(path).n_samples()
             info(f'import_gvs: skipping group {idx+1}/{len(refs)}...')
             continue
 
