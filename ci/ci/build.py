7ce1a10ba8dce19330a0d20d64dc522b76c4ade3
Daniel Goldstein <danielgold95@gmail.com>
2022-09-27 15:59:43 -0400

    [ci] Add production prefix to tags deployed in production

--- ci/ci/build.py
+++ ci/ci/build.py
@@ -250,7 +250,6 @@ class BuildImage2Step(Step):
 
         image_name = self.publish_as or 'ci-intermediate'
         self.base_image = f'{DOCKER_PREFIX}/{image_name}'
-        self.image = f'{self.base_image}:{self.token}'
         self.main_branch_cache_repository = f'{self.base_image}:cache'
 
         if params.scope == 'deploy':
@@ -258,15 +257,19 @@ class BuildImage2Step(Step):
                 # CIs that don't live in default doing a deploy
                 # should not clobber the main `cache` tag
                 self.cache_repository = f'{self.base_image}:cache-{DEFAULT_NAMESPACE}-deploy'
+                self.image = f'{self.base_image}:{self.token}'
             else:
                 self.cache_repository = self.main_branch_cache_repository
+                self.image = f'{self.base_image}:production-{self.token}'
         elif params.scope == 'dev':
             dev_user = params.code.config()['user']
             self.cache_repository = f'{self.base_image}:cache-{dev_user}'
+            self.image = f'{self.base_image}:{self.token}'
         else:
             assert params.scope == 'test'
             pr_number = params.code.config()['number']
             self.cache_repository = f'{self.base_image}:cache-pr-{pr_number}'
+            self.image = f'{self.base_image}:{self.token}'
 
         self.job = None
 
