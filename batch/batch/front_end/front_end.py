97d07ea1ebf4f3e89f2a8e9d9dfb909b028da208
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-09-29 12:40:56 +1000

    Allow finer granularity for JVM memory (#228)

--- batch/batch/front_end/front_end.py
+++ batch/batch/front_end/front_end.py
@@ -909,8 +909,8 @@ WHERE user = %s AND id = %s AND NOT deleted;
 
         if spec['process']['type'] == 'jvm':
             jvm_requested_cpu = parse_cpu_in_mcpu(resources.get('cpu', BATCH_JOB_DEFAULT_CPU))
-            if 'cpu' in resources and jvm_requested_cpu not in (1000, 8000):
-                raise web.HTTPBadRequest(reason='invalid cpu for jvm jobs. must be 1 or 8')
+            if 'cpu' in resources and jvm_requested_cpu not in (1000, 2000, 4000, 8000):
+                raise web.HTTPBadRequest(reason='invalid cpu for jvm jobs. must be 1, 2, 4, or 8')
             if 'memory' in resources and resources['memory'] == 'lowmem':
                 raise web.HTTPBadRequest(reason='jvm jobs cannot be on lowmem machines')
             if 'storage' in resources:
