7ea3e3a19ef18e262763ba8b9c96dd5041beb059
Jackie Goldstein <jigold@broadinstitute.org>
2022-10-03 14:50:39 -0400

    fix bad query

--- batch/batch/driver/instance_collection/pool.py
+++ batch/batch/driver/instance_collection/pool.py
@@ -368,7 +368,6 @@ FROM (
     INNER JOIN job_regions ON ready_jobs.batch_id = job_regions.batch_id AND ready_jobs.job_id = job_regions.job_id
     LEFT JOIN region_ids ON job_regions.region_id = region_ids.region_id
     GROUP BY ready_jobs.batch_id, ready_jobs.job_id
-    ORDER BY region ASC
   ) AS regions ON ready_jobs.batch_id = regions.batch_id AND ready_jobs.job_id = regions.job_id
 ) AS ordered_jobs
 GROUP BY regions, row_num_overall - row_num_by_regions
@@ -654,7 +653,6 @@ LEFT JOIN (
   INNER JOIN job_regions ON ready_jobs.batch_id = job_regions.batch_id AND ready_jobs.job_id = job_regions.job_id
   LEFT JOIN region_ids ON job_regions.region_id = region_ids.region_id
   GROUP BY ready_jobs.batch_id, ready_jobs.job_id
-  ORDER BY region ASC
 ) AS t ON ready_jobs.batch_id = t.batch_id AND ready_jobs.job_id = t.job_id
 ORDER BY scheduling_iteration, ready_jobs.batch_id, always_run DESC, -n_regions DESC, JSON_UNQUOTE(regions), ready_jobs.job_id;
 ''',
