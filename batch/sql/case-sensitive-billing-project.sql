d04ea5bb0504dcf650a91bb79126db398306a1a9
Jackie Goldstein <jigold@broadinstitute.org>
2022-10-14 13:01:33 -0400

    fix migration

--- batch/sql/case-sensitive-billing-project.sql
+++ batch/sql/case-sensitive-billing-project.sql
@@ -7,5 +7,5 @@ SET name_cs = `name`;
 UPDATE billing_project_users
 SET user_cs = user;
 
-ALTER TABLE billing_projects MODIFY COLUMN name_cs VARCHAR(100) COLLATE utf8mb4_0900_as_cs;
-ALTER TABLE billing_project_users MODIFY COLUMN user_cs VARCHAR(100) COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE billing_projects MODIFY COLUMN name_cs VARCHAR(100) NOT NULL COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE billing_project_users MODIFY COLUMN user_cs VARCHAR(100) NOT NULL COLLATE utf8mb4_0900_as_cs;
