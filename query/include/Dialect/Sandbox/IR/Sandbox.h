53b1bea3ef13092b9c11ea763938bc00c59fc3ae
Tim Poterba <tpoterba@gmail.com>
2022-11-03 14:43:35 -0400

    Fix up includes

--- query/include/Dialect/Sandbox/IR/Sandbox.h
+++ query/include/Dialect/Sandbox/IR/Sandbox.h
@@ -6,16 +6,10 @@
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/OpImplementation.h"
 #include "mlir/Interfaces/CastInterfaces.h"
-#include "mlir/Interfaces/InferIntRangeInterface.h"
-#include "mlir/Interfaces/InferTypeOpInterface.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
-#include "mlir/Interfaces/VectorInterfaces.h"
 
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Builders.h"
-#include "mlir/Support/LLVM.h"
-
-#include "llvm/ADT/StringRef.h"
 
 #include "Dialect/Sandbox/IR/SandboxOpsDialect.h.inc"
 #include "Dialect/Sandbox/IR/SandboxOpsEnums.h.inc"
