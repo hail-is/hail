ebe13958434c40e8b8382773116bfde54ac960e8
patrick-schultz <pschultz@broadinstitute.org>
2022-11-03 16:27:29 -0400

    partially implement sandbox lowering

--- query/lib/Conversion/LowerSandbox/LowerSandbox.cpp
+++ query/lib/Conversion/LowerSandbox/LowerSandbox.cpp
@@ -2,6 +2,9 @@
 #include "Dialect/Sandbox/IR/Sandbox.h"
 #include "../PassDetail.h"
 
+#include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/BuiltinTypes.h"
 #include "mlir/Transforms/DialectConversion.h"
 
 namespace hail {
@@ -10,6 +13,43 @@ struct LowerSandboxPass
     : public LowerSandboxBase<LowerSandboxPass> {
   void runOnOperation() override;
 };
+
+namespace {
+
+mlir::Value castToMLIR(mlir::ConversionPatternRewriter &rewriter, mlir::Location loc, mlir::Value value) {
+  auto castOp = rewriter.create<mlir::UnrealizedConversionCastOp>(
+    loc,
+     mlir::TypeRange{rewriter.getType<mlir::IntegerType>(32)},
+      mlir::ValueRange{value});
+  return castOp.getResult(0);
+}
+struct AddIOpConversion : public mlir::OpConversionPattern<ir::AddIOp> {
+  AddIOpConversion(mlir::MLIRContext *context)
+      : OpConversionPattern<ir::AddIOp>(context, /*benefit=*/1) {}
+
+  mlir::LogicalResult
+  matchAndRewrite(ir::AddIOp op, OpAdaptor adaptor,
+                  mlir::ConversionPatternRewriter &rewriter) const override {
+    auto lhs = castToMLIR(rewriter, op->getLoc(), adaptor.lhs());
+    auto rhs = castToMLIR(rewriter, op->getLoc(), adaptor.rhs());
+    rewriter.replaceOpWithNewOp<mlir::arith::AddIOp>(op, lhs, rhs);
+    return mlir::success();
+  }
+};
+
+struct ConstantOpConversion : public mlir::OpConversionPattern<ir::ConstantOp> {
+  ConstantOpConversion(mlir::MLIRContext *context)
+      : OpConversionPattern<ir::ConstantOp>(context, /*benefit=*/1) {}
+
+  mlir::LogicalResult
+  matchAndRewrite(ir::ConstantOp op, OpAdaptor adaptor,
+                  mlir::ConversionPatternRewriter &rewriter) const override {
+    rewriter.replaceOpWithNewOp<mlir::arith::ConstantOp>(op, adaptor.valueAttr(), rewriter.getType<mlir::IntegerType>(32));
+    return mlir::success();
+  }
+};
+
+} // end namespace
 
 void LowerSandboxPass::runOnOperation() {
   mlir::RewritePatternSet patterns(&getContext());
@@ -26,7 +66,7 @@ void LowerSandboxPass::runOnOperation() {
 
 void populateLowerSandboxConversionPatterns(
     mlir::RewritePatternSet &patterns) {
-  // patterns.add<>(patterns.getContext());
+  patterns.add<ConstantOpConversion, AddIOpConversion>(patterns.getContext());
 }
 
 std::unique_ptr<mlir::Pass> createLowerSandboxPass() {
