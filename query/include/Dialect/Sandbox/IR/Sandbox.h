160d9a7dcc323f17918c63e7c4e5088243744b7d
Tim Poterba <tpoterba@gmail.com>
2022-11-03 14:20:08 -0400

    add bool type, bool lit & compare ops

--- query/include/Dialect/Sandbox/IR/Sandbox.h
+++ query/include/Dialect/Sandbox/IR/Sandbox.h
@@ -1,12 +1,24 @@
 #ifndef DIALECT_SANDBOX_SANDBOX_H
 #define DIALECT_SANDBOX_SANDBOX_H
 
-#include "mlir/IR/BuiltinTypes.h"
+
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/OpImplementation.h"
+#include "mlir/Interfaces/CastInterfaces.h"
+#include "mlir/Interfaces/InferIntRangeInterface.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
+#include "mlir/Interfaces/VectorInterfaces.h"
+
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/Support/LLVM.h"
+
+#include "llvm/ADT/StringRef.h"
 
 #include "Dialect/Sandbox/IR/SandboxOpsDialect.h.inc"
+#include "Dialect/Sandbox/IR/SandboxOpsEnums.h.inc"
 
 #define GET_TYPEDEF_CLASSES
 #include "Dialect/Sandbox/IR/SandboxOpsTypes.h.inc"
--- query/test/current.mlir
+++ query/test/current.mlir
@@ -1,7 +1,9 @@
-func.func @foo(%in: !sb.int) -> (!sb.int) {
+func.func @foo(%in: !sb.int) -> (!sb.bool) {
     %i1 = sb.constant 5 : !sb.int
     %i2 = sb.constant 7 : !sb.int
     %i3 = sb.addi %in %i1
     %i4 = sb.addi %i3 %i2
-    func.return %i4 : !sb.int
+    %i5 = sb.constant 6 : !sb.int
+    %i6 = sb.compare eq, %i4, %i5 : !sb.bool
+    func.return %i6 : !sb.bool
 }
