bca779703a100a57e37846d61dfa651c296ce7ec
Daniel Goldstein <danielgold95@gmail.com>
2022-11-14 15:34:13 -0800

    lint

--- hail/python/hailtop/aiocloud/aiogoogle/client/base_client.py
+++ hail/python/hailtop/aiocloud/aiogoogle/client/base_client.py
@@ -10,7 +10,7 @@ class GoogleBaseClient(CloudBaseClient):
     _session: GoogleSession
 
     def __init__(self, base_url: str, *, session: Optional[GoogleSession] = None,
-                 rate_limit: RateLimit = None, **kwargs):
+                 rate_limit: Optional[RateLimit] = None, **kwargs):
         if session is None:
             session = GoogleSession(**kwargs)
         super().__init__(base_url, session, rate_limit=rate_limit)
