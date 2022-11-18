1541dab2957bb5bff33c4065dd90cd912c0820da
Dan King <daniel.zidan.king@gmail.com>
2022-11-18 14:34:50 -0500

    [batch] make Batch.delete idempotent (#12478)
    * [batch] make Batch.delete idempotent
    
    `_delete` retries transient errors. The `.../delete` endpoints return 404 for batches that do not
    exist, which, I suppose, is reasonable. On the client-side, we need to ignore 404s to ensure
    idempotency.
    
    * lint
    
    * do not test for 404 in response to nonextant batch

--- hail/python/hailtop/batch_client/aioclient.py
+++ hail/python/hailtop/batch_client/aioclient.py
@@ -456,7 +456,11 @@ class Batch:
         return {'status': batch_status, 'jobs': jobs}
 
     async def delete(self):
-        await self._client._delete(f'/api/v1alpha/batches/{self.id}')
+        try:
+            await self._client._delete(f'/api/v1alpha/batches/{self.id}')
+        except httpx.ClientResponseError as err:
+            if err.code != 404:
+                raise
 
 
 class BatchBuilder:
