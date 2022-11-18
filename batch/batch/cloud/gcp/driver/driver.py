51ff00e03c9708c885fda75ab5d72c19ace0648f
Jackie Goldstein <jigold@broadinstitute.org>
2022-11-18 11:28:36 -0500

    requires valid credentials and not anonymous credentials

--- batch/batch/cloud/gcp/driver/driver.py
+++ batch/batch/cloud/gcp/driver/driver.py
@@ -3,7 +3,6 @@ import asyncio
 from gear import Database
 from gear.cloud_config import get_gcp_config
 from hailtop import aiotools
-import hailtop.aiocloud as aiocloud
 from hailtop.aiocloud import aiogoogle
 from hailtop.utils import RateLimit, periodically_call
 
@@ -64,7 +63,7 @@ ON DUPLICATE KEY UPDATE region = region;
             rate_limit=RateLimit(10, 60),
         )
 
-        billing_client = aiogoogle.GoogleBillingClient(credentials=aiocloud.AnonymousCloudCredentials())
+        billing_client = aiogoogle.GoogleBillingClient(credentials_file=credentials_file)
 
         zone_monitor = await ZoneMonitor.create(compute_client, regions, zone)
         billing_manager = await GCPBillingManager.create(db, billing_client, regions)
