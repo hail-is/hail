6b40f62eb7ca8c9a8ce818c3051bf880c8d0d783
Jackie Goldstein <jigold@broadinstitute.org>
2022-11-23 14:51:34 -0500

    fix sql

--- batch/sql/estimated-current.sql
+++ batch/sql/estimated-current.sql
@@ -1081,7 +1081,7 @@ BEGIN
               jobs.cancelled = CASE jobs.run_condition
                                  WHEN 'always' THEN jobs.cancelled
                                  WHEN 'all_succeeded' THEN t.n_succeeded_parents != t.n_parents - t.n_pending_parents OR jobs.cancelled
-                                 WHEN 'any_succeeded' THEN (t.n_parents > 0 AND t.n_pending_parents = 0 AND t.n_succeeded = 0) OR jobs.cancelled
+                                 WHEN 'any_succeeded' THEN (t.n_parents > 0 AND t.n_pending_parents = 0 AND t.n_succeeded_parents = 0) OR jobs.cancelled
                                  ELSE t.n_succeeded_parents != t.n_parents - t.n_pending_parents OR jobs.cancelled  # backwards compatibility
                                END
           WHERE jobs.batch_id = in_batch_id AND jobs.job_id >= cur_update_start_job_id AND
