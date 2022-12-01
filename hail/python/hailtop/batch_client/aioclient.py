60b43d435f7ce5c660f33a6eae2b910e6ada586f
Daniel Goldstein <danielgold95@gmail.com>
2022-11-30 19:49:01 -0500

    [hailctl] Add hailctl batch submit (#12471)
    * [hailctl] Add hailctl batch submit
    
    * make name an option not an argument
    
    * lint'
    
    * use relative file paths for files
    
    * add basic test
    
    * use the current hailgenetics/hail image not the dockerhub one
    
    * get quiet mode working to fully test hailctl batch submit
    
    * fix
    
    * cleanup
    
    * submit sets HAIL_QUERY_BACKEND to batch
    
    * fix
    
    * fixes
    
    * lint
    
    * name the batch that the submitted job spawns

--- hail/python/hailtop/batch_client/aioclient.py
+++ hail/python/hailtop/batch_client/aioclient.py
@@ -444,7 +444,7 @@ class Batch:
             description += ': '
         if progress is not None:
             return await self._wait(description, progress, disable_progress_bar, starting_job)
-        with BatchProgressBar() as progress2:
+        with BatchProgressBar(disable=disable_progress_bar) as progress2:
             return await self._wait(description, progress2, disable_progress_bar, starting_job)
 
     async def debug_info(self):
--- hail/python/hailtop/hailctl/batch/wait.py
+++ hail/python/hailtop/hailctl/batch/wait.py
@@ -1,9 +1,14 @@
+import json
 import sys
 from .batch_cli_utils import get_batch_if_exists
 
 
 def init_parser(parser):
     parser.add_argument('batch_id', type=int)
+    parser.add_argument("--quiet", "-q",
+                        action="store_true",
+                        help="Do not print a progress bar for the batch")
+    parser.add_argument('-o', type=str, default='text', choices=['text', 'json'])
 
 
 def main(args, pass_through_args, client):  # pylint: disable=unused-argument
@@ -13,4 +18,9 @@ def main(args, pass_through_args, client):  # pylint: disable=unused-argument
         sys.exit(1)
 
     batch = maybe_batch
-    print(batch.wait())
+    quiet = args.quiet or args.o != 'text'
+    out = batch.wait(disable_progress_bar=quiet)
+    if args.o == 'json':
+        print(json.dumps(out))
+    else:
+        print(out)
