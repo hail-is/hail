d2accff07ef19f11959a162fd239c9d45b555a5c
Daniel Goldstein <danielgold95@gmail.com>
2022-12-05 18:21:11 -0500

    add permission again

--- check_pip_requirements.sh
+++ check_pip_requirements.sh
@@ -8,7 +8,7 @@ new_pinned=$(mktemp)
 pinned_no_comments=$(mktemp)
 new_pinned_no_comments=$(mktemp)
 
-pip-compile --quiet $reqs $pinned --output-file=$new_pinned
+PATH="$PATH:$HOME/.local/bin" pip-compile --quiet $reqs $pinned --output-file=$new_pinned
 # Get rid of comments that might differ despite requirements being the same
 cat $pinned | sed '/#/d' > $pinned_no_comments
 cat $new_pinned | sed '/#/d' > $new_pinned_no_comments
