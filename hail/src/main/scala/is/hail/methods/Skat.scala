16df662b5b0ac3a6201539d12cda8e10ffae416f
Dan King <dking@broadinstitute.org>
2023-01-27 16:17:07 -0500

    [query] delete davies.cpp
    CHANGELOG: Fix bug where `hl.skat` did not work on Apple M1 machines.

--- hail/src/main/scala/is/hail/methods/Skat.scala
+++ hail/src/main/scala/is/hail/methods/Skat.scala
@@ -119,22 +119,6 @@ object Skat {
   def computeGramian(st: Array[SkatTuple], useSmallN: Boolean): (Double, BDM[Double]) =
     if (useSmallN) computeGramianSmallN(st) else computeGramianLargeN(st)
 
-  /** Davies Algorithm original C code
-  *   Citation:
-  *     Davies, Robert B. "The distribution of a linear combination of
-  *     x2 random variables." Applied Statistics 29.3 (1980): 323-333.
-  *   Software Link:
-  *     http://www.robertnz.net/QF.htm
-  */
-  @native
-  def qfWrapper(lb1: Array[Double], nc1: Array[Double], n1: Array[Int], r1: Int,
-    sigma: Double, c1: Double, lim1: Int, acc: Double, trace: Array[Double],
-    ifault: IntByReference): Double
-
-  // NativeCode needs to control the initial loading of the libhail DLL, and
-  // the call to getHailName() guarantees that.
-  Native.register("hail")
-
   // gramian is the m x m matrix (G * sqrt(W)).t * P_0 * (G * sqrt(W)) which has the same non-zero eigenvalues
   // as the n x n matrix in the paper P_0^{1/2} * (G * W * G.t) * P_0^{1/2}
   def computePval(q: Double, gramian: BDM[Double], accuracy: Double, iterations: Int): (Double, Int) = {
@@ -150,7 +134,14 @@ object Skat {
     val trace = Array.fill[Double](7)(0.0)
     val fault = new IntByReference()
     val s = 0.0
-    val x = qfWrapper(evals, noncentrality, dof, terms, s, q, iterations, accuracy, trace, fault)
+
+    val result = GeneralizedChiSquaredDistribution.cdfReturnExceptions(
+      q, dof, evals, noncentrality, s, iterations, accuracy
+    )
+    val x = result.value
+    val nIntegrations = result.nIterations
+    val converged = result.converged
+    val fault = result.fault
     val pval = 1 - x
 
     (pval, fault.getValue)
