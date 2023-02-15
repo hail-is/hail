2fbf68bab11265341ecfbe00f3b3e9bdd790f41b
Tim Poterba <tpoterba@gmail.com>
2023-02-15 13:43:35 -0500

    [query] Make caching in PartitionNativeIntervalReader more aggressive (#12600)
    * [query] Make caching in PartitionNativeIntervalReader more aggressive
    
    Add finalizers to HailTaskContext to clean up open indices.
    
    * remove log
    
    * comment

--- hail/src/main/scala/is/hail/backend/HailTaskContext.scala
+++ hail/src/main/scala/is/hail/backend/HailTaskContext.scala
@@ -3,6 +3,26 @@ package is.hail.backend
 import is.hail.annotations.RegionPool
 import is.hail.utils._
 
+import java.io.Closeable
+
+class TaskFinalizer {
+  val closeables = new BoxedArrayBuilder[Closeable]()
+
+  def clear(): Unit = {
+    closeables.clear()
+  }
+  
+  def addCloseable(c: Closeable): Unit = {
+    closeables += c
+  }
+
+  def closeAll(): Unit = {
+    (0 until closeables.size).foreach { i =>
+      closeables(i).close()
+    }
+  }
+}
+
 abstract class HailTaskContext extends AutoCloseable {
   def stageId(): Int
 
@@ -20,10 +40,21 @@ abstract class HailTaskContext extends AutoCloseable {
     s"${ stageId() }-${ partitionId() }-${ attemptNumber() }-$fileUUID"
   }
 
+  val finalizers = new BoxedArrayBuilder[TaskFinalizer]()
+
+  def newFinalizer(): TaskFinalizer = {
+    val f = new TaskFinalizer
+    finalizers += f
+    f
+  }
+
   def close(): Unit = {
     log.info(s"TaskReport: stage=${ stageId() }, partition=${ partitionId() }, attempt=${ attemptNumber() }, " +
       s"peakBytes=${ thePool.getHighestTotalUsage }, peakBytesReadable=${ formatSpace(thePool.getHighestTotalUsage) }, "+
       s"chunks requested=${thePool.getUsage._1}, cache hits=${thePool.getUsage._2}")
+    (0 until finalizers.size).foreach { i =>
+      finalizers(i).closeAll()
+    }
     thePool.close()
   }
 }
