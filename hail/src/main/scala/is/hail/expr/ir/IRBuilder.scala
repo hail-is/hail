0864850a31203b32840213bdfd6425fdab5ca5b2
patrick-schultz <pschultz@broadinstitute.org>
2023-02-16 13:20:27 -0500

    move IRBuilder

---
+++ hail/src/main/scala/is/hail/expr/ir/IRBuilder.scala
@@ -0,0 +1,32 @@
+package is.hail.expr.ir
+
+import scala.collection.mutable
+
+object IRBuilder {
+  def scoped(f: IRBuilder => IR): IR = {
+    val ctx = new IRBuilder()
+    val result = f(ctx)
+    ctx.wrap(result)
+  }
+}
+
+class IRBuilder() {
+  private val bindings: mutable.ArrayBuffer[(String, IR)] = mutable.ArrayBuffer()
+
+  def getBindings: IndexedSeq[(String, IR)] = bindings.result()
+
+  def memoize(ir: IR): TrivialIR = ir match {
+    case ir: TrivialIR => ir
+    case _ => strictMemoize(ir)
+  }
+
+  def strictMemoize(ir: IR): Ref = {
+    val name = genUID()
+    bindings += name -> ir
+    Ref(name, ir.typ)
+  }
+
+  def wrap(ir: IR): IR = {
+    bindings.foldRight[IR](ir) { case ((f, v), accum) => Let(f, v, accum) }
+  }
+}
