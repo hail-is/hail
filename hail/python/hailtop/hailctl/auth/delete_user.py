614796ac6d0fc13a178cc6da86a85e73bf9c1f3a
Daniel Goldstein <danielgold95@gmail.com>
2023-02-16 17:39:41 -0500

    create specific exceptions to appease pylint

--- hail/python/hailtop/hailctl/auth/delete_user.py
+++ hail/python/hailtop/hailctl/auth/delete_user.py
@@ -4,6 +4,10 @@ from hailtop.utils import sleep_and_backoff
 from hailtop.auth import async_delete_user, async_get_user
 
 
+class DeleteUserException(Exception):
+    pass
+
+
 def init_parser(parser):
     parser.add_argument("username", type=str,
                         help="User name to delete.")
@@ -14,22 +18,25 @@ def init_parser(parser):
 
 
 async def async_main(args):
-    await async_delete_user(args.username, args.namespace)
+    try:
+        await async_delete_user(args.username, args.namespace)
 
-    if not args.wait:
-        return
+        if not args.wait:
+            return
 
-    async def _poll():
-        delay = 5
-        while True:
-            user = await async_get_user(args.username, args.namespace)
-            if user['state'] == 'deleted':
-                print(f"Deleted user '{args.username}'")
-                return
-            assert user['state'] == 'deleting'
-            delay = await sleep_and_backoff(delay)
+        async def _poll():
+            delay = 5
+            while True:
+                user = await async_get_user(args.username, args.namespace)
+                if user['state'] == 'deleted':
+                    print(f"Deleted user '{args.username}'")
+                    return
+                assert user['state'] == 'deleting'
+                delay = await sleep_and_backoff(delay)
 
-    await _poll()
+        await _poll()
+    except Exception as e:
+        raise DeleteUserException(f"Error while deleting user '{args.username}'") from e
 
 
 def main(args, pass_through_args):  # pylint: disable=unused-argument
