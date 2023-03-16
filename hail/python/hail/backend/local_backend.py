019273400101ed158bd795351cf9815e6d7fed5e
Tim Poterba <tpoterba@gmail.com>
2023-03-16 12:15:17 -0400

    fix local backend call

--- hail/python/hail/backend/local_backend.py
+++ hail/python/hail/backend/local_backend.py
@@ -147,7 +147,11 @@ class LocalBackend(Py4JBackend):
         self._jbackend = hail_package.backend.local.LocalBackend.apply(
             tmpdir,
             gcs_requester_pays_project,
-            gcs_requester_pays_buckets
+            gcs_requester_pays_buckets,
+            log,
+            True,
+            append,
+            skip_logging_configuration
         )
         self._jhc = hail_package.HailContext.apply(
             self._jbackend, branching_factor, optimizer_iterations)
