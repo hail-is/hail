8fe6df1ccf1d19734857a8cd90256bba9547f5bd
Tim Poterba <tpoterba@gmail.com>
2023-03-27 15:38:38 -0400

    [build] stop evaluating upload-qob-jar variables on makefile parsing (#12616)
    Requires network requests -- I had to comment this out to build during a
    power outage :)
    
    Totally open to better ways to do this than converting it to a shell
    script -- this is not my expertise.

---
+++ hail/scripts/upload_qob_jar.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -ex
+
+NAMESPACE=$1
+REVISION=$2
+SHADOW_JAR=$3
+PATH_FILE=$4
+
+
+
+QUERY_STORAGE_URI=$(kubectl get secret global-config --template={{.data.query_storage_uri}} | base64 --decode)
+TEST_STORAGE_URI=$(kubectl get secret global-config --template={{.data.test_storage_uri}} | base64 --decode)
+
+if [[ "${NAMESPACE}" -eq "default" ]]; then
+  if [[ "${UPLOAD_RELEASE_JAR}" -eq "true" ]]; then
+    JAR_LOCATION="${QUERY_STORAGE_URI}/jars/${REVISION}.jar"
+    else
+    JAR_LOCATION="${TEST_STORAGE_URI}/${NAMESPACE}/jars/${TOKEN}/${REVISION}.jar"
+  fi
+  else
+    JAR_LOCATION="${TEST_STORAGE_URI}/${NAMESPACE}/jars/${TOKEN}/${REVISION}.jar"
+fi
+
+gcloud storage cp ${SHADOW_JAR} ${JAR_LOCATION}
+echo ${JAR_LOCATION} > ${PATH_FILE}
