9f0f2b9cc7969f438759acb2e9a0e4485243ffc4
Tim Poterba <tpoterba@gmail.com>
2023-03-30 17:16:37 -0400

    [query] Fix bug in force_count requiredness (#12830)

--- hail/src/main/scala/is/hail/expr/ir/Requiredness.scala
+++ hail/src/main/scala/is/hail/expr/ir/Requiredness.scala
@@ -464,6 +464,7 @@ class Requiredness(val usesAndDefs: UsesAndDefs, ctx: ExecuteContext) {
       // union top-level
       case _: ApplyBinaryPrimOp |
            _: ApplyUnaryPrimOp |
+           _: Consume |
            _: ArrayLen |
            _: StreamLen |
            _: ArrayZeros |
@@ -477,7 +478,7 @@ class Requiredness(val usesAndDefs: UsesAndDefs, ctx: ExecuteContext) {
         requiredness.union(node.children.forall { case c: IR => lookup(c).required })
 
       // always required
-      case _: I32 | _: I64 | _: F32 | _: F64 | _: Str | True() | False() | _: IsNA | _: Die | _: UUID4 | _: Consume | _: RNGStateLiteral | _: RNGSplit =>
+      case _: I32 | _: I64 | _: F32 | _: F64 | _: Str | True() | False() | _: IsNA | _: Die | _: UUID4 | _: RNGStateLiteral | _: RNGSplit =>
       case _: CombOpValue | _: AggStateValue =>
       case Trap(child) =>
         // error message field is missing if the child runs without error
