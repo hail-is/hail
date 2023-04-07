82c9d81593abf873a606fdaf61468e5d4879e9f4
Dan King <dking@broadinstitute.org>
2023-04-07 12:20:05 -0400

    fix nested asyncio loop

--- batch/test/test_dag.py
+++ batch/test/test_dag.py
@@ -114,6 +114,10 @@ def test_cancel_left_after_tail(client):
 
 
 async def test_callback(client):
+    import nest_asyncio
+    nest_asyncio()
+    from aiohttp import web
+
     app = web.Application()
     callback_bodies = []
     callback_event = asyncio.Event()
