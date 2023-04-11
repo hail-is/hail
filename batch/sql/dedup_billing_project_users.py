a3365918da4eeb5ee332cfd9d7e0e84ac3400a24
Jackie Goldstein <jigold@broadinstitute.org>
2023-04-11 11:39:10 -0400

    chunk the audit

--- batch/sql/dedup_billing_project_users.py
+++ batch/sql/dedup_billing_project_users.py
@@ -91,34 +91,66 @@ async def audit_changes(db):
     bp_user_audit_start = time.time()
     print('starting auditing billing project user records')
 
-    bad_bp_user_records = db.select_and_fetchall(
-        '''
-SELECT old.billing_project, old.`user`, old.`usage`, new.`usage`, ABS(new.`usage` - old.`usage`) AS usage_diff
+    chunk_offsets = [None]
+    for offset in await find_chunk_offsets_for_audit(db, 100):
+        chunk_offsets.append(offset)
+    chunk_offsets = list(zip(chunk_offsets[:-1], chunk_offsets[1:]))
+
+    if chunk_offsets != [(None, None)]:
+        for offsets in chunk_offsets:
+            start, end = offsets
+            if start is not None and end is not None:
+                start_bp, start_user = start
+                end_bp, end_user = end
+                where_statement = '''
+WHERE ((billing_project > %s) OR (billing_project = %s AND `user` >= %s))
+  AND ((billing_project < %s) OR (billing_project = %s AND `user` < %s))
+'''
+                where_args = [start_bp, start_bp, start_user,
+                              end_bp, end_bp, end_user]
+            elif start is None and end is not None:
+                end_bp, end_user = end
+                where_statement = 'WHERE (billing_project < %s) OR (billing_project = %s AND `user` < %s)'
+                where_args = [end_bp, end_bp, end_user]
+            else:
+                assert start is not None and end is None
+                start_bp, start_user = start
+                where_statement = 'WHERE (billing_project > %s) OR (billing_project = %s AND `user` >= %s)'
+                where_args = [start_bp, start_bp, start_user]
+
+            bad_bp_user_records = db.select_and_fetchall(
+                f'''
+SELECT old.billing_project, old.`user`, old.resource_id, old.`usage`, new.`usage`, ABS(new.`usage` - old.`usage`) AS usage_diff
 FROM (
   SELECT billing_project, `user`, deduped_resource_id, CAST(COALESCE(SUM(`usage`), 0) AS SIGNED) AS `usage`
   FROM aggregated_billing_project_user_resources_v2
   LEFT JOIN resources ON resources.resource_id = aggregated_billing_project_user_resources_v2.resource_id
+  {where_statement}
   GROUP BY billing_project, `user`, deduped_resource_id
+  LOCK IN SHARE MODE
 ) AS old
 LEFT JOIN (
   SELECT billing_project, `user`, resource_id, CAST(COALESCE(SUM(`usage`), 0) AS SIGNED) AS `usage`
   FROM aggregated_billing_project_user_resources_v3
+  {where_statement}
   GROUP BY billing_project, `user`, resource_id
+  LOCK IN SHARE MODE
 ) AS new ON old.billing_project = new.billing_project AND old.`user` = new.`user` AND old.deduped_resource_id = new.resource_id
 WHERE new.`usage` != old.`usage`
 LIMIT 100;
-''')
+''',
+            where_args + where_args)
 
-    bad_bp_user_records = [record async for record in bad_bp_user_records]
-    failing_bp_users = []
-    for record in bad_bp_user_records:
-        print(f'found bad bp user record {record}')
-        failing_bp_users.append((record['billing_project'], record['user']))
+            bad_bp_user_records = [record async for record in bad_bp_user_records]
+            failing_bp_users = []
+            for record in bad_bp_user_records:
+                print(f'found bad bp user record {record}')
+                failing_bp_users.append((record['billing_project'], record['user']))
+
+            if bad_bp_user_records:
+                raise Exception(f'errors found in audit')
 
     print(f'finished auditing bp user records in {time.time() - bp_user_audit_start}s')
-
-    if bad_bp_user_records:
-        raise Exception(f'errors found in audit')
 
 
 async def find_chunk_offsets(db, size):
@@ -139,6 +171,32 @@ WHERE MOD((@rank := @rank + 1), %s) = 0;
 
         offsets = tx.execute_and_fetchall(query, (size,))
         offsets = [(offset['billing_project'], offset['user'], offset['resource_id']) async for offset in offsets]
+        offsets.append(None)
+
+        print(f'found chunk offsets in {round(time.time() - start_time, 4)}s')
+        return offsets
+
+    return await _find_chunks()
+
+
+async def find_chunk_offsets_for_audit(db, size):
+    @transaction(db)
+    async def _find_chunks(tx) -> List[Optional[Tuple[int, int, str]]]:
+        start_time = time.time()
+
+        await tx.just_execute('SET @rank=0;')
+
+        query = f'''
+SELECT t.billing_project, t.`user` FROM (
+  SELECT billing_project, `user`
+  FROM aggregated_billing_project_user_resources_v2
+  ORDER BY billing_project ASC, `user` ASC
+) AS t
+WHERE MOD((@rank := @rank + 1), %s) = 0;
+'''
+
+        offsets = tx.execute_and_fetchall(query, (size,))
+        offsets = [(offset['billing_project'], offset['user']) async for offset in offsets]
         offsets.append(None)
 
         print(f'found chunk offsets in {round(time.time() - start_time, 4)}s')
