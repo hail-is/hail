1a4b2a2aa8b41f7d657bb74f627d00bd269c0471
Jackie Goldstein <jigold@broadinstitute.org>
2023-04-12 12:43:24 -0400

    chunk audit

--- batch/sql/dedup_agg_batch_resources.py
+++ batch/sql/dedup_agg_batch_resources.py
@@ -83,34 +83,67 @@ async def audit_changes(db):
     batch_audit_start = time.time()
     print('starting auditing batch records')
 
-    bad_batch_records = db.select_and_fetchall(
+    chunk_offsets = [None]
+    for offset in await find_chunk_offsets_for_audit(db, 100):
+        chunk_offsets.append(offset)
+    chunk_offsets = list(zip(chunk_offsets[:-1], chunk_offsets[1:]))
+
+    async def _process_audit_chunk(db, start_id, end_id):
+        if start_id is not None and end_id is not None:
+            where_statement = '''
+WHERE batch_id >= %s AND batch_id < %s
         '''
+            where_args = [start_id, end_id]
+        elif start_id is None and end_id is not None:
+            where_statement = 'WHERE batch_id < %s'
+            where_args = [end_id]
+        else:
+            assert start_id is not None and end_id is None
+            where_statement = 'WHERE batch_id >= %s'
+            where_args = [start_id]
+
+        bad_batch_records = db.select_and_fetchall(
+            f'''
 SELECT old.batch_id, old.deduped_resource_id, old.`usage`, new.`usage`, ABS(new.`usage` - old.`usage`) AS usage_diff
 FROM (
   SELECT batch_id, deduped_resource_id, CAST(COALESCE(SUM(`usage`), 0) AS SIGNED) AS `usage`
   FROM aggregated_batch_resources_v2
-  LEFT JOIN resources ON aggregated_batch_resources_v2.resource_id = resources.resource_id
+  LEFT JOIN resources ON resources.resource_id = aggregated_batch_resources_v2.resource_id
+  {where_statement}
   GROUP BY batch_id, deduped_resource_id
+  LOCK IN SHARE MODE
 ) AS old
 LEFT JOIN (
   SELECT batch_id, resource_id, CAST(COALESCE(SUM(`usage`), 0) AS SIGNED) AS `usage`
   FROM aggregated_batch_resources_v3
+  {where_statement}
   GROUP BY batch_id, resource_id
+  LOCK IN SHARE MODE
 ) AS new ON old.batch_id = new.batch_id AND old.deduped_resource_id = new.resource_id
 WHERE new.`usage` != old.`usage`
 LIMIT 100;
-''')
+''',
+            where_args + where_args)
 
-    bad_batch_records = [record async for record in bad_batch_records]
-    failing_batch_ids = []
-    for record in bad_batch_records:
-        print(f'found bad batch record {record}')
-        failing_batch_ids.append(record['batch_id'])
+        bad_batch_records = [record async for record in bad_batch_records]
+        failing_batches = []
+        for record in bad_batch_records:
+            print(f'found bad batch record {record}')
+            failing_batches.append((record['batch_id']))
+
+        if bad_batch_records:
+            raise Exception(f'errors found in audit')
+
+    if chunk_offsets != [(None, None)]:
+        random.shuffle(chunk_offsets)
+
+        await bounded_gather(
+            *[functools.partial(_process_audit_chunk, db, start_offset, end_offset)
+              for start_offset, end_offset in chunk_offsets],
+            parallelism=10
+        )
 
     print(f'finished auditing batch records in {time.time() - batch_audit_start}s')
-
-    if bad_batch_records:
-        raise Exception('bad audit')
 
 
 async def find_chunk_offsets(db, size):
@@ -131,6 +164,32 @@ WHERE MOD((@rank := @rank + 1), %s) = 0;
 
         offsets = tx.execute_and_fetchall(query, (size,))
         offsets = [(offset['batch_id'], offset['resource_id']) async for offset in offsets]
+        offsets.append(None)
+
+        print(f'found chunk offsets in {round(time.time() - start_time, 4)}s')
+        return offsets
+
+    return await _find_chunks()
+
+
+async def find_chunk_offsets_for_audit(db, size):
+    @transaction(db)
+    async def _find_chunks(tx) -> List[Optional[Tuple[int, int, str]]]:
+        start_time = time.time()
+
+        await tx.just_execute('SET @rank=0;')
+
+        query = f'''
+SELECT t.batch_id FROM (
+  SELECT batch_id
+  FROM aggregated_batch_resources_v2
+  ORDER BY batch_id ASC
+) AS t
+WHERE MOD((@rank := @rank + 1), %s) = 0;
+'''
+
+        offsets = tx.execute_and_fetchall(query, (size,))
+        offsets = [(offset['batch_id']) async for offset in offsets]
         offsets.append(None)
 
         print(f'found chunk offsets in {round(time.time() - start_time, 4)}s')
