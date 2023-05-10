e521af7f92651568b22f64d8ab146d90536899ab
Dan King <dking@broadinstitute.org>
2023-05-10 17:55:24 -0400

    explicitly pass the shaded HttpClient

--- hail/src/main/scala/is/hail/io/fs/AzureStorageFS.scala
+++ hail/src/main/scala/is/hail/io/fs/AzureStorageFS.scala
@@ -5,6 +5,7 @@ import is.hail.shadedazure.com.azure.identity.{ClientSecretCredential, ClientSec
 import is.hail.shadedazure.com.azure.storage.blob.models.{BlobProperties, BlobRange, ListBlobsOptions, BlobStorageException}
 import is.hail.shadedazure.com.azure.storage.blob.specialized.BlockBlobClient
 import is.hail.shadedazure.com.azure.storage.blob.{BlobClient, BlobContainerClient, BlobServiceClient, BlobServiceClientBuilder}
+import is.hail.shadedazure.reactor.netty.http.client.HttpClient
 import is.hail.services.retryTransientErrors
 import is.hail.io.fs.FSUtil.{containsWildcard, dropTrailingSlash}
 import org.apache.log4j.Logger
@@ -124,16 +125,16 @@ object AzureStorageFileStatus {
 }
 
 class AzureBlobServiceClientCache(credential: TokenCredential) {
-  private lazy val clientBuilder: BlobServiceClientBuilder = new BlobServiceClientBuilder()
-  private lazy val clients: mutable.Map[(String, String), BlobServiceClient] = mutable.Map()
+  private[this] lazy val clients = mutable.Map[(String, String), BlobServiceClient]()
 
   def getServiceClient(account: String, container: String): BlobServiceClient = {
     clients.get((account, container)) match {
       case Some(client) => client
       case None =>
-        val blobServiceClient = clientBuilder
+        val blobServiceClient = new BlobServiceClientBuilder()
           .credential(credential)
           .endpoint(s"https://$account.blob.core.windows.net")
+          .httpClient(HttpClient.create())
           .buildClient()
         clients += ((account, container) -> blobServiceClient)
         blobServiceClient
@@ -142,8 +143,9 @@ class AzureBlobServiceClientCache(credential: TokenCredential) {
 
   def setPublicAccessServiceClient(account: String, container: String): Unit = {
     val blobServiceClient = new BlobServiceClientBuilder()
-        .endpoint(s"https://$account.blob.core.windows.net")
-        .buildClient()
+      .endpoint(s"https://$account.blob.core.windows.net")
+      .httpClient(HttpClient.create())
+      .buildClient()
     clients += ((account, container) -> blobServiceClient)
   }
 }
