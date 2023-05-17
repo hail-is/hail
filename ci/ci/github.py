e4eae2c7fa3a3cc53e815d0392e5117b64604c41
Daniel Goldstein <danielgold95@gmail.com>
2023-05-17 12:51:25 -0400

    [batch] Use same size VMs in dev and PR as we use in prod (#12974)
    Now that test databases are hosted on their own servers instead of the
    single cloud-hosted MySQL, we can ramp up the parallelism both in our
    tests and in the number of PRs that we run at once. I recall that even
    before we had this DB bottleneck we still restricted the number of PRs
    running at once for cost reasons, but if that's not the case we could
    remove that restriction entirely.

--- ci/ci/github.py
+++ ci/ci/github.py
@@ -40,7 +40,7 @@ if os.path.exists("/zulip-config/.zuliprc"):
 
 TRACKED_PRS = pc.Gauge('ci_tracked_prs', 'PRs currently being monitored by CI', ['build_state', 'review_state'])
 
-MAX_CONCURRENT_PR_BATCHES = 3
+MAX_CONCURRENT_PR_BATCHES = 6
 
 
 class GithubStatus(Enum):
