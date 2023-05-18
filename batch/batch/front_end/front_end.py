23cca62c3e2b559c3c5940d4f6d59f1bf7b0bf17
Dan King <dking@broadinstitute.org>
2023-05-18 18:14:46 -0400

    Revert "fix"
    This reverts commit cf068c24b173a18994df25349f3e1fe7419c60ea.

--- batch/batch/front_end/front_end.py
+++ batch/batch/front_end/front_end.py
@@ -950,8 +950,8 @@ WHERE batch_updates.batch_id = %s AND batch_updates.update_id = %s AND user = %s
 
         if spec['process']['type'] == 'jvm':
             jvm_requested_cpu = parse_cpu_in_mcpu(resources.get('cpu', BATCH_JOB_DEFAULT_CPU))
-            if 'cpu' in resources and jvm_requested_cpu not in (500, 1000, 2000, 4000, 8000):
-                raise web.HTTPBadRequest(reason='invalid cpu for jvm jobs. must be 0.5, 1, 2, 4, or 8')
+            if 'cpu' in resources and jvm_requested_cpu not in (1000, 2000, 4000, 8000):
+                raise web.HTTPBadRequest(reason='invalid cpu for jvm jobs. must be 1, 2, 4, or 8')
             if 'memory' in resources and resources['memory'] == 'lowmem':
                 raise web.HTTPBadRequest(reason='jvm jobs cannot be on lowmem machines')
             if machine_type is not None:
