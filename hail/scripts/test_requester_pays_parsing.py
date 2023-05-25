11085b0815021b92831ce841411707ecb4a6e2d2
Dan King <dking@broadinstitute.org>
2023-05-25 14:55:58 -0400

    pytest.raises

--- hail/scripts/test_requester_pays_parsing.py
+++ hail/scripts/test_requester_pays_parsing.py
@@ -1,3 +1,5 @@
+import pytest
+
 from hailtop.config.user_config import get_gcs_requester_pays_configuration, spark_conf_path
 from hailtop.utils.process import check_exec_output
 
@@ -20,12 +22,8 @@ def test_no_project_is_error():
     with open(SPARK_CONF_PATH, 'w') as f:
         f.write('spark.hadoop.fs.gs.requester.pays.mode AUTO\n')
 
-    try:
+    with pytest.raises(ValueError, match='.*a project must be set if a mode other than DISABLED is set.*'):
         get_gcs_requester_pays_configuration()
-    except ValueError as err:
-        assert 'a project must be set if a mode other than DISABLED is set' in err.args[0]
-    else:
-        assert False
 
 
 def test_auto_with_project():
@@ -41,13 +39,8 @@ def test_custom_no_buckets():
     with open(SPARK_CONF_PATH, 'w') as f:
         f.write('spark.hadoop.fs.gs.requester.pays.project.id my_project\n')
         f.write('spark.hadoop.fs.gs.requester.pays.mode CUSTOM\n')
-    try:
+    with pytest.raises(ValueError, match='.*with mode CUSTOM buckets must be set.*'):
         get_gcs_requester_pays_configuration()
-    except ValueError as err:
-        assert 'with mode CUSTOM buckets must be set' in err.args[0]
-    else:
-        assert False
-
 
 
 def test_custom_with_buckets():
