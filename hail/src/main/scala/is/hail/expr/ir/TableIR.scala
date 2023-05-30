244b15ce6b0f9caa4c8f5d41859c6f1b0dac352c
Chris Vittal <cvittal@broadinstitute.org>
2023-05-30 17:21:43 -0500

    revert unnecessary uncompressed buffer use

--- hail/src/main/scala/is/hail/expr/ir/TableIR.scala
+++ hail/src/main/scala/is/hail/expr/ir/TableIR.scala
@@ -1704,7 +1704,7 @@ case class TableParallelize(rowsAndGlobal: IR, nPartitions: Option[Int] = None) 
 
     val rowsT = ptype.types(0).asInstanceOf[PArray]
     val rowT = rowsT.elementType.asInstanceOf[PStruct].setRequired(true)
-    val spec = TypedCodecSpec(rowT, BufferSpec.blockedUncompressed)
+    val spec = TypedCodecSpec(rowT, BufferSpec.wireSpec)
 
     val makeEnc = spec.buildEncoder(ctx, rowT)
     val rowsAddr = ptype.loadField(res, 0)
@@ -3347,7 +3347,7 @@ case class TableOrderBy(child: TableIR, sortFields: IndexedSeq[SortField]) exten
 
     val act = implicitly[ClassTag[Annotation]]
 
-    val codec = TypedCodecSpec(prev.rvd.rowPType, BufferSpec.blockedUncompressed)
+    val codec = TypedCodecSpec(prev.rvd.rowPType, BufferSpec.wireSpec)
     val rdd = prev.rvd.keyedEncodedRDD(ctx, codec, sortFields.map(_.field)).sortBy(_._1)(ord, act)
     val (rowPType: PStruct, orderedCRDD) = codec.decodeRDD(ctx, rowType, rdd.map(_._2))
     new TableValueIntermediate(TableValue(ctx, typ, prev.globals, RVD.unkeyed(rowPType, orderedCRDD)))
