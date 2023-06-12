4d627195acd872abd68b1af4a8823da2bd022b9b
Jackie Goldstein <jigold@broadinstitute.org>
2023-06-12 13:26:23 -0400

    fix index

--- batch/sql/add-billing-table-compaction-indices.sql
+++ batch/sql/add-billing-table-compaction-indices.sql
@@ -1,2 +1,2 @@
-CREATE INDEX aggregated_billing_project_user_resources_v2_token ON `aggregated_billing_project_user_resources_v2` (`token`);
-CREATE INDEX aggregated_billing_project_user_resources_by_date_v2_token ON `aggregated_billing_project_user_resources_by_date_v2` (`token`);
+CREATE INDEX aggregated_billing_project_user_resources_v2_all ON `aggregated_billing_project_user_resources_v2` (`billing_project`, `user`, resource_id, `token`);
+CREATE INDEX aggregated_billing_project_user_resources_by_date_v2_token ON `aggregated_billing_project_user_resources_by_date_v2` (billing_date, billing_project, `user`, `token`);
