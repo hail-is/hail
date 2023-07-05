3519c8b8afa3c51456f984a621b19966891da127
Jackie Goldstein <jigold@broadinstitute.org>
2023-07-05 12:12:05 -0400

    address comments

--- hail/python/hailtop/hailctl/config/cli.py
+++ hail/python/hailtop/hailctl/config/cli.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 import os
 import sys
 import re
@@ -9,7 +10,6 @@ from rich import print
 import typer
 from typer import Argument as Arg
 
-from hailtop.batch_client.parse import CPU_REGEXPAT, MEMORY_REGEXPAT
 
 app = typer.Typer(
     name='config',
@@ -19,103 +19,99 @@ app = typer.Typer(
 )
 
 
-_config_variables = None
+ConfigVariable = namedtuple('ConfigVariable', ['name', 'help_msg', 'validation'])
 
 
 def config_variables():
     from hailtop.aiotools.router_fs import RouterAsyncFS  # pylint: disable=import-outside-toplevel
-
-    global _config_variables
-
-    if _config_variables is not None:
-        return _config_variables
+    from hailtop.batch_client.parse import CPU_REGEXPAT, MEMORY_REGEXPAT
 
     variables = [
-        (
-            'domain',
-            'Domain of the Batch service',
-            (lambda x: re.fullmatch(r'.+\..+', x) is not None, 'should be valid domain'),
+        ConfigVariable(
+            name='domain',
+            help_msg='Domain of the Batch service',
+            validation=(lambda x: re.fullmatch(r'.+\..+', x) is not None, 'should be valid domain'),
         ),
-        (
-            'gcs_requester_pays/project',
-            'Project when using requester pays buckets in GCS',
-            (lambda x: re.fullmatch(r'[^:/\s]+', x) is not None, 'should be valid GCS project name'),
+        ConfigVariable(
+            name='gcs_requester_pays/project',
+            help_msg='Project when using requester pays buckets in GCS',
+            validation=(lambda x: re.fullmatch(r'[^:/\s]+', x) is not None, 'should be valid GCS project name'),
         ),
-        (
-            'gcs_requester_pays/buckets',
-            'Allowed buckets when using requester pays in GCS',
-            (lambda x: re.fullmatch(r'[^:/\s]+(,[^:/\s]+)*', x) is not None, 'should be comma separated list of bucket names'),
+        ConfigVariable(
+            name='gcs_requester_pays/buckets',
+            help_msg='Allowed buckets when using requester pays in GCS',
+            validation=(lambda x: re.fullmatch(r'[^:/\s]+(,[^:/\s]+)*', x) is not None, 'should be comma separated list of bucket names'),
         ),
-        (
-            'batch/bucket',
-            'Deprecated - Name of GCS bucket to use as a temporary scratch directory',
-            (lambda x: re.fullmatch(r'[^:/\s]+', x) is not None, 'should be valid Google Bucket identifier, with no gs:// prefix'),
+        ConfigVariable(
+            name='batch/bucket',
+            help_msg='Deprecated - Name of GCS bucket to use as a temporary scratch directory',
+            validation=(lambda x: re.fullmatch(r'[^:/\s]+', x) is not None, 'should be valid Google Bucket identifier, with no gs:// prefix'),
         ),
-        (
-            'batch/remote_tmpdir',
-            'Cloud storage URI to use as a temporary scratch directory',
-            (RouterAsyncFS.valid_url, 'should be valid cloud storage URI such as gs://my-bucket/batch-tmp/'),
+        ConfigVariable(
+            name='batch/remote_tmpdir',
+            help_msg='Cloud storage URI to use as a temporary scratch directory',
+            validation=(RouterAsyncFS.valid_url, 'should be valid cloud storage URI such as gs://my-bucket/batch-tmp/'),
         ),
-        (
-            'batch/regions',
-            'Comma-separated list of regions to run jobs in',
-            (lambda x: re.fullmatch(r'[^\s]+(,[^\s]+)*', x) is not None, 'should be comma separated list of regions'),
+        ConfigVariable(
+            name='batch/regions',
+            help_msg='Comma-separated list of regions to run jobs in',
+            validation=(lambda x: re.fullmatch(r'[^\s]+(,[^\s]+)*', x) is not None, 'should be comma separated list of regions'),
         ),
-        (
-            'batch/billing_project',
-            'Batch billing project',
-            (lambda x: re.fullmatch(r'[^:/\s]+', x) is not None, 'should be valid Batch billing project name'),
+        ConfigVariable(
+            name='batch/billing_project',
+            help_msg='Batch billing project',
+            validation=(lambda x: re.fullmatch(r'[^:/\s]+', x) is not None, 'should be valid Batch billing project name'),
         ),
-        (
-            'batch/backend',
-            'Backend to use. One of local or service.',
-            (lambda x: x in ('local', 'service'), 'should be one of "local" or "service"'),
+        ConfigVariable(
+            name='batch/backend',
+            help_msg='Backend to use. One of local or service.',
+            validation=(lambda x: x in ('local', 'service'), 'should be one of "local" or "service"'),
         ),
-        (
-            'query/backend',
-            'Backend to use for Hail Query. One of spark, local, batch.',
-            (lambda x: x in ('local', 'spark', 'batch'), 'should be one of "local", "spark", or "batch"'),
+        ConfigVariable(
+            name='query/backend',
+            help_msg='Backend to use for Hail Query. One of spark, local, batch.',
+            validation=(lambda x: x in ('local', 'spark', 'batch'), 'should be one of "local", "spark", or "batch"'),
         ),
-        (
-            'query/jar_url',
-            'Cloud storage URI to a Query JAR',
-            (RouterAsyncFS.valid_url, 'should be valid cloud storage URI such as gs://my-bucket/jars/sha.jar')
+        ConfigVariable(
+            name='query/jar_url',
+            help_msg='Cloud storage URI to a Query JAR',
+            validation=(RouterAsyncFS.valid_url, 'should be valid cloud storage URI such as gs://my-bucket/jars/sha.jar')
         ),
-        (
-            'query/batch_driver_cores',
-            'Cores specification for the query driver',
-            (lambda x: re.fullmatch(CPU_REGEXPAT, x) is not None, 'should be an integer which is a power of two from 1 to 16 inclusive'),
+        ConfigVariable(
+            name='query/batch_driver_cores',
+            help_msg='Cores specification for the query driver',
+            validation=(lambda x: re.fullmatch(CPU_REGEXPAT, x) is not None, 'should be an integer which is a power of two from 1 to 16 inclusive'),
         ),
-        (
-            'query/batch_driver_memory',
-            'Memory specification for the query driver',
-            (lambda x: re.fullmatch(MEMORY_REGEXPAT, x) is not None or x in ('standard', 'lowmem', 'highmem'),
-             'should be a valid string specifying memory "[+]?((?:[0-9]*[.])?[0-9]+)([KMGTP][i]?)?B?" or one of standard, lowmem, highmem'),
+        ConfigVariable(
+            name='query/batch_driver_memory',
+            help_msg='Memory specification for the query driver',
+            validation=(lambda x: re.fullmatch(MEMORY_REGEXPAT, x) is not None or x in ('standard', 'lowmem', 'highmem'),
+                        'should be a valid string specifying memory "[+]?((?:[0-9]*[.])?[0-9]+)([KMGTP][i]?)?B?" or one of standard, lowmem, highmem'),
         ),
-        (
-            'query/batch_worker_cores',
-            'Cores specification for the query worker',
-            (lambda x: re.fullmatch(CPU_REGEXPAT, x) is not None, 'should be an integer which is a power of two from 1 to 16 inclusive'),
+        ConfigVariable(
+            name='query/batch_worker_cores',
+            help_msg='Cores specification for the query worker',
+            validation=(lambda x: re.fullmatch(CPU_REGEXPAT, x) is not None, 'should be an integer which is a power of two from 1 to 16 inclusive'),
         ),
-        (
-            'query/batch_worker_memory',
-            'Memory specification for the query worker',
-            (lambda x: re.fullmatch(MEMORY_REGEXPAT, x) is not None or x in ('standard', 'lowmem', 'highmem'),
-             'should be a valid string specifying memory "[+]?((?:[0-9]*[.])?[0-9]+)([KMGTP][i]?)?B?" or one of standard, lowmem, highmem'),
+        ConfigVariable(
+            name='query/batch_worker_memory',
+            help_msg='Memory specification for the query worker',
+            validation=(lambda x: re.fullmatch(MEMORY_REGEXPAT, x) is not None or x in ('standard', 'lowmem', 'highmem'),
+                        'should be a valid string specifying memory "[+]?((?:[0-9]*[.])?[0-9]+)([KMGTP][i]?)?B?" or one of standard, lowmem, highmem'),
         ),
-        (
-            'query/name_prefix',
-            'Name used when displaying query progress in a progress bar',
-            (lambda x: re.fullmatch(r'[^\s]+', x) is not None, 'should be single word without spaces'),
+        ConfigVariable(
+            name='query/name_prefix',
+            help_msg='Name used when displaying query progress in a progress bar',
+            validation=(lambda x: re.fullmatch(r'[^\s]+', x) is not None, 'should be single word without spaces'),
         ),
-        (
-            'query/disable_progress_bar',
-            'Disable the progress bar with a value of 1. Enable the progress bar with a value of 0',
-            (lambda x: x in ('0', '1'), 'should be a value of 0 or 1'),
+        ConfigVariable(
+            name='query/disable_progress_bar',
+            help_msg='Disable the progress bar with a value of 1. Enable the progress bar with a value of 0',
+            validation=(lambda x: x in ('0', '1'), 'should be a value of 0 or 1'),
         ),
     ]
-    _config_variables = variables
-    return _config_variables
+
+    return variables
 
 
 def get_section_key_path(parameter: str) -> Tuple[str, str, Tuple[str, ...]]:
@@ -143,14 +139,13 @@ A parameter with more than one slash is invalid, for example:
 
 
 def complete_config_variable(incomplete: str):
-    for name, help_text, _ in config_variables():
-        if name.startswith(incomplete):
-            completion_item = (name, help_text)
-            yield completion_item
+    for var in config_variables():
+        if var.name.startswith(incomplete):
+            yield (var.name, var.help_msg)
 
 
 @app.command()
-def set(parameter: Ann[str, Arg(default=..., help="Configuration variable to set", autocompletion=complete_config_variable)], value: str):
+def set(parameter: Ann[str, Arg(help="Configuration variable to set", autocompletion=complete_config_variable)], value: str):
     '''Set a Hail configuration parameter.'''
     from hailtop.config import get_user_config, get_user_config_path  # pylint: disable=import-outside-toplevel
 
@@ -158,11 +153,7 @@ def set(parameter: Ann[str, Arg(default=..., help="Configuration variable to set
     config_file = get_user_config_path()
     section, key, path = get_section_key_path(parameter)
 
-    validations = {name: validation for name, _, validation in config_variables()}
-
-    if parameter not in validations:
-        print(f"Error: unknown parameter {parameter}", file=sys.stderr)
-        sys.exit(1)
+    validations = {config.name: config.validation for config in config_variables()}
 
     validation_func, msg = validations.get(parameter, (lambda _: True, ''))  # type: ignore
     if not validation_func(value):
@@ -199,14 +190,16 @@ def get_config_variable(incomplete: str):
                 path = f'{section_name}/{item_name}'
             elements.append((path, value))
 
+    config_items = {config.name: config.help_msg for config in config_variables()}
+
     for name, current_value in elements:
         if name.startswith(incomplete):
-            completion_item = (name, current_value)
-            yield completion_item
+            help_msg = config_items.get(name)
+            yield (name, help_msg)
 
 
 @app.command()
-def unset(parameter: Ann[str, Arg(default=..., help="Configuration variable to unset", autocompletion=get_config_variable)]):
+def unset(parameter: Ann[str, Arg(help="Configuration variable to unset", autocompletion=get_config_variable)]):
     '''Unset a Hail configuration parameter (restore to default behavior).'''
     from hailtop.config import get_user_config, get_user_config_path  # pylint: disable=import-outside-toplevel
 
@@ -220,7 +213,7 @@ def unset(parameter: Ann[str, Arg(default=..., help="Configuration variable to u
 
 
 @app.command()
-def get(parameter: Ann[str, Arg(default=..., help="Configuration variable to get", autocompletion=get_config_variable)]):
+def get(parameter: Ann[str, Arg(help="Configuration variable to get", autocompletion=get_config_variable)]):
     '''Get the value of a Hail configuration parameter.'''
     from hailtop.config import get_user_config  # pylint: disable=import-outside-toplevel
 
