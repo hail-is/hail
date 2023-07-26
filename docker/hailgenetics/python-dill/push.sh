489bc15b9e46583d6893f03fcbf056bee5da62f4
Chris Vittal <cvittal@broadinstitute.org>
2023-07-26 18:06:10 -0400

    more updates
    - new debian version no longer uses libopenblas-base (now uses libopenblas0)
    - need to change into the script directory in push.sh

--- docker/hailgenetics/python-dill/push.sh
+++ docker/hailgenetics/python-dill/push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd $(dirname $0)
 set -ex
 
 for version in 3.8 3.8-slim 3.9 3.9-slim 3.10 3.10-slim 3.11 3.11-slim
@@ -7,6 +7,7 @@ do
     public=hailgenetics/python-dill:$version
 
     DOCKER_BUILDKIT=1 docker build \
+        --platform linux/amd64 \
         --build-arg PYTHON_VERSION=$version \
         --file Dockerfile \
         --build-arg BUILDKIT_INLINE_CACHE=1 \
