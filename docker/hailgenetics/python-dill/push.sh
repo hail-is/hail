be37cb8f34e304922bb9be940857fcfdb4f63c95
Christopher Vittal <cvittal@broadinstitute.org>
2023-07-27 16:56:26 -0500

    [batch] Support python 3.11 in default python jobs (#13310)
    Co-authored-by: Daniel Goldstein <danielgold95@gmail.com>

--- docker/hailgenetics/python-dill/push.sh
+++ docker/hailgenetics/python-dill/push.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-
+cd $(dirname $0)
 set -ex
 
-for version in 3.8 3.8-slim 3.9 3.9-slim 3.10 3.10-slim
+for version in 3.8 3.8-slim 3.9 3.9-slim 3.10 3.10-slim 3.11 3.11-slim
 do
     public=hailgenetics/python-dill:$version
 
     DOCKER_BUILDKIT=1 docker build \
+        --platform linux/amd64 \
         --build-arg PYTHON_VERSION=$version \
-        --file Dockerfile.out \
+        --file Dockerfile \
         --build-arg BUILDKIT_INLINE_CACHE=1 \
         --tag ${public} \
         .
