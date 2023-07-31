4e2a02ee050605bfc4fc450b3b242acb032cbb83
Daniel Goldstein <danielgold95@gmail.com>
2023-07-31 11:19:19 -0400

    [k8s] Dont require access to default secrets when make deploying

--- config.mk
+++ config.mk
@@ -1,9 +1,9 @@
 # These values should be lazy so you don't need kubectl for targets that don't
 # require it but also only evaluated at most once every make invocation
 # https://make.mad-scientist.net/deferred-simple-variable-expansion/
-DOCKER_PREFIX = $(eval DOCKER_PREFIX := $$(shell kubectl get secret global-config --template={{.data.docker_prefix}} | base64 --decode))$(DOCKER_PREFIX)
-DOMAIN = $(eval DOMAIN := $$(shell kubectl get secret global-config --template={{.data.domain}} | base64 --decode))$(DOMAIN)
-CLOUD = $(eval CLOUD := $$(shell kubectl get secret global-config --template={{.data.cloud}} | base64 --decode))$(CLOUD)
+DOCKER_PREFIX = $(eval DOCKER_PREFIX := $$(shell kubectl -n $(NAMESPACE) get secret global-config --template={{.data.docker_prefix}} | base64 --decode))$(DOCKER_PREFIX)
+DOMAIN = $(eval DOMAIN := $$(shell kubectl -n $(NAMESPACE) get secret global-config --template={{.data.domain}} | base64 --decode))$(DOMAIN)
+CLOUD = $(eval CLOUD := $$(shell kubectl -n $(NAMESPACE) get secret global-config --template={{.data.cloud}} | base64 --decode))$(CLOUD)
 
 ifeq ($(NAMESPACE),default)
 SCOPE = deploy
