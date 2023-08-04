8a0987739d2e6d8e44e4cf1ed0f66b9c27ca62b4
Dan King <dking@broadinstitute.org>
2023-08-04 11:25:17 -0400

    fix syntax

--- batch/sql/jobs-after-update-simple.sql
+++ batch/sql/jobs-after-update-simple.sql
@@ -10,6 +10,42 @@ DECLARE cur_batch_cancelled BOOLEAN;
 DECLARE cur_n_tokens INT;
 DECLARE rand_token INT;
 
+DECLARE always_run boolean;
+DECLARE cores_mcpu boolean;
+
+DECLARE was_marked_cancelled boolean;
+DECLARE was_cancelled        boolean;
+DECLARE was_cancellable      boolean;
+
+DECLARE now_marked_cancelled boolean;
+DECLARE now_cancelled        boolean;
+DECLARE now_cancellable      boolean;
+
+DECLARE was_ready boolean;
+DECLARE now_ready boolean;
+
+DECLARE was_running boolean;
+DECLARE now_running boolean;
+
+DECLARE was_creating boolean;
+DECLARE now_creating boolean;
+
+DECLARE delta_n_ready_cancellable_jobs          int;
+DECLARE delta_ready_cancellable_cores_mcpu   bigint;
+DECLARE delta_n_ready_jobs                      int;
+DECLARE delta_ready_cores_mcpu               bigint;
+DECLARE delta_n_cancelled_ready_jobs            int;
+
+DECLARE delta_n_running_cancellable_jobs        int;
+DECLARE delta_running_cancellable_cores_mcpu bigint;
+DECLARE delta_n_running_jobs                    int;
+DECLARE delta_running_cores_mcpu             bigint;
+DECLARE delta_n_cancelled_running_jobs          int;
+
+DECLARE delta_n_creating_cancellable_jobs       int;
+DECLARE delta_n_creating_jobs                   int;
+DECLARE delta_n_cancelled_creating_jobs         int;
+
 SELECT user INTO cur_user FROM batches WHERE id = NEW.batch_id;
 
 SET cur_batch_cancelled = EXISTS (SELECT TRUE
@@ -20,49 +56,41 @@ SET cur_batch_cancelled = EXISTS (SELECT TRUE
 SELECT n_tokens INTO cur_n_tokens FROM globals LOCK IN SHARE MODE;
 SET rand_token = FLOOR(RAND() * cur_n_tokens);
 
-# NB: we cannot write something sane like
-#
-#     let x = y;
-#
-# so we instead write
-#
-#     let x type not null default y;
-#
-declare always_run boolean not null default old.always_run; # always_run is immutable
-declare cores_mcpu boolean not null default old.cores_mcpu; # cores_mcpu is immutable
+set always_run = old.always_run; # always_run is immutable
+set cores_mcpu = old.cores_mcpu; # cores_mcpu is immutable
 
-declare was_marked_cancelled boolean not null default old.cancelled or cur_batch_cancelled;
-declare was_cancelled        boolean not null default not always_run and was_marked_cancelled;
-declare was_cancellable      boolean not null default not always_run and not was_marked_cancelled;
+set was_marked_cancelled = old.cancelled or cur_batch_cancelled;
+set was_cancelled        = not always_run and was_marked_cancelled;
+set was_cancellable      = not always_run and not was_marked_cancelled;
 
-declare now_marked_cancelled boolean not null default new.cancelled or cur_batch_cancelled;
-declare now_cancelled        boolean not null default not always_run and now_marked_cancelled;
-declare now_cancellable      boolean not null default not always_run and not now_marked_cancelled;
+set now_marked_cancelled = new.cancelled or cur_batch_cancelled;
+set now_cancelled        = not always_run and now_marked_cancelled;
+set now_cancellable      = not always_run and not now_marked_cancelled;
 
 # NB: was_cancelled => now_cancelled b/c you cannot be uncancelled
 
-declare was_ready boolean not null default old.state = 'Ready';
-declare now_ready boolean not null default new.state = 'Ready';
-declare was_running boolean not null default old.state = 'Running';
-declare now_running boolean not null default new.state = 'Running';
-declare was_creating boolean not null default old.state = 'Creating';
-declare now_creating boolean not null default new.state = 'Creating';
+set was_ready	 = old.state = 'Ready';
+set now_ready	 = new.state = 'Ready';
+set was_running	 = old.state = 'Running';
+set now_running	 = new.state = 'Running';
+set was_creating = old.state = 'Creating';
+set now_creating = new.state = 'Creating';
 
-declare delta_n_ready_cancellable_jobs          int not null default  (-1 * was_ready    * was_cancellable  ) + (now_ready    * now_cancellable  );
-declare delta_ready_cancellable_cores_mcpu   bigint not null default ((-1 * was_ready    * was_cancellable  ) + (now_ready    * now_cancellable  )) * cores_mcpu;
-declare delta_n_ready_jobs                      int not null default  (-1 * was_ready    * not was_cancelled) + (now_ready    * not now_cancelled);
-declare delta_ready_cores_mcpu               bigint not null default ((-1 * was_ready    * not was_cancelled) + (now_ready    * not now_cancelled)) * cores_mcpu;
-declare delta_n_cancelled_ready_jobs            int not null default  (-1 * was_ready    * was_cancelled    ) + (now_ready    * now_cancelled    );
+set delta_n_ready_cancellable_jobs              =  (-1      * was_ready    *  was_cancellable  )     + (now_ready    *  now_cancellable  )               ;
+set delta_ready_cancellable_cores_mcpu          = ((-1      * was_ready    *  was_cancellable  )     + (now_ready    *  now_cancellable  ))  * cores_mcpu;
+set delta_n_ready_jobs                          =  (-1      * was_ready    * (not was_cancelled))    + (now_ready    * (not now_cancelled))              ;
+set delta_ready_cores_mcpu                      = ((-1      * was_ready    * (not was_cancelled))    + (now_ready    * (not now_cancelled))) * cores_mcpu;
+set delta_n_cancelled_ready_jobs                =  (-1      * was_ready    *  was_cancelled    )     + (now_ready    *  now_cancelled    )               ;
 
-declare delta_n_running_cancellable_jobs        int not null default  (-1 * was_running  * was_cancellable  ) + (now_running  * now_cancellable  );
-declare delta_running_cancellable_cores_mcpu bigint not null default ((-1 * was_running  * was_cancellable  ) + (now_running  * now_cancellable  )) * cores_mcpu;
-declare delta_n_running_jobs                    int not null default  (-1 * was_running  * not was_cancelled) + (now_running  * not now_cancelled);
-declare delta_running_cores_mcpu             bigint not null default ((-1 * was_running  * not was_cancelled) + (now_running  * not now_cancelled)) * cores_mcpu;
-declare delta_n_cancelled_running_jobs          int not null default  (-1 * was_running  * was_cancelled    ) + (now_running  * now_cancelled    );
+set delta_n_running_cancellable_jobs            =  (-1      * was_running  *  was_cancellable  )     + (now_running  *  now_cancellable  )               ;
+set delta_running_cancellable_cores_mcpu        = ((-1      * was_running  *  was_cancellable  )     + (now_running  *  now_cancellable  ))  * cores_mcpu;
+set delta_n_running_jobs                        =  (-1      * was_running  * (not was_cancelled))    + (now_running  * (not now_cancelled))              ;
+set delta_running_cores_mcpu                    = ((-1      * was_running  * (not was_cancelled))    + (now_running  * (not now_cancelled))) * cores_mcpu;
+set delta_n_cancelled_running_jobs              =  (-1      * was_running  *  was_cancelled    )     + (now_running  *  now_cancelled    )               ;
 
-declare delta_n_creating_cancellable_jobs       int not null default  (-1 * was_creating * was_cancellable  ) + (now_creating * now_cancellable  );
-declare delta_n_creating_jobs                   int not null default  (-1 * was_creating * not was_cancelled) + (now_creating * not now_cancelled);
-declare delta_n_cancelled_creating_jobs         int not null default  (-1 * was_creating * was_cancelled    ) + (now_creating * now_cancelled    );
+set delta_n_creating_cancellable_jobs           =  (-1      * was_creating *  was_cancellable  )     + (now_creating *  now_cancellable  )               ;
+set delta_n_creating_jobs                       =  (-1      * was_creating * (not was_cancelled))    + (now_creating * (not now_cancelled))              ;
+set delta_n_cancelled_creating_jobs             =  (-1      * was_creating *  was_cancelled    )     + (now_creating *  now_cancelled    )               ;
 
 INSERT INTO batch_inst_coll_cancellable_resources (batch_id, update_id, inst_coll, token,
   n_ready_cancellable_jobs,
