67cfe0b2ed1e4cc99a8575482293784060c6e1aa
Dan King <dking@broadinstitute.org>
2023-08-09 15:41:01 -0400

    fix lints

--- ci/bootstrap_create_accounts.py
+++ ci/bootstrap_create_accounts.py
@@ -1,21 +1,19 @@
-from hailtop.hail_logging import configure_logging
+from hailtop.hail_logging import configure_logging  # noqa: I001
 
-# configure logging before importing anything else
 configure_logging()
 
+import base64  # noqa: E402 pylint: disable=wrong-import-position
+import json  # noqa: E402 pylint: disable=wrong-import-position
+import os  # noqa: E402 pylint: disable=wrong-import-position
 
-import base64
-import json
-import os
+import kubernetes_asyncio.client  # noqa: E402 pylint: disable=wrong-import-position
+import kubernetes_asyncio.config  # noqa: E402 pylint: disable=wrong-import-position
 
-import kubernetes_asyncio.client
-import kubernetes_asyncio.config
-
-from auth.driver.driver import create_user
-from gear import Database, transaction
-from gear.clients import get_identity_client
-from gear.cloud_config import get_global_config
-from hailtop.utils import async_to_blocking
+from auth.driver.driver import create_user  # noqa: E402 pylint: disable=wrong-import-position
+from gear import Database, transaction  # noqa: E402 pylint: disable=wrong-import-position
+from gear.clients import get_identity_client  # noqa: E402 pylint: disable=wrong-import-position
+from gear.cloud_config import get_global_config  # noqa: E402 pylint: disable=wrong-import-position
+from hailtop.utils import async_to_blocking  # noqa: E402 pylint: disable=wrong-import-position,ungrouped-imports
 
 CLOUD = get_global_config()['cloud']
 SCOPE = os.environ['HAIL_SCOPE']
