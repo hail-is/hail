8920ad70f8e6ad893b7fb77fe05aa2c975076aed
Dan King <dking@broadinstitute.org>
2023-08-11 12:32:09 -0400

    test

---
+++ batch/sql/set-test-and-dev-pools-to-max-4-standing-4.py
@@ -0,0 +1,34 @@
+import os
+import asyncio
+from gear import Database
+
+
+async def main():
+    if os.environ['HAIL_SCOPE'] == 'deploy':
+        return
+
+    max_instances = 4
+    max_live_instances = 4
+    standing_worker_cores = 4
+
+    db = Database()
+    await db.async_init()
+
+    await db.execute_update(
+        '''
+UPDATE inst_colls
+SET max_instances = %s, max_live_instances = %s
+''', (max_instances, max_live_instances))
+
+    if os.environ['HAIL_SCOPE'] == 'dev':
+        return
+
+    await db.execute_update(
+        '''
+UPDATE pools
+SET standing_worker_cores = %s
+''', (standing_worker_cores,))
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
