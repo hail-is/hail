ca4ab159b43243963057d8e716353552008a60e9
jigold <jigold@users.noreply.github.com>
2023-08-14 16:31:20 -0400

    [hailctl] Autocomplete for hailctl config {get,set,unset} (#13224)
    To get this to work, I had to run the following command:
    ```
    hailctl --install-completion zsh
    ```
    
    And then add this line to the end of the ~/.zshrc file: `autoload -Uz
    compinit && compinit`

--- hail/python/hailtop/config/__init__.py
+++ hail/python/hailtop/config/__init__.py
@@ -1,6 +1,7 @@
 from .user_config import (get_user_config, get_user_config_path,
                           get_remote_tmpdir, configuration_of)
 from .deploy_config import get_deploy_config, DeployConfig
+from .variables import ConfigVariable
 
 __all__ = [
     'get_deploy_config',
@@ -8,5 +9,6 @@ __all__ = [
     'get_user_config_path',
     'get_remote_tmpdir',
     'DeployConfig',
-    'configuration_of'
+    'ConfigVariable',
+    'configuration_of',
 ]
---
+++ hail/python/hailtop/config/variables.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+
+class ConfigVariable(str, Enum):
+    DOMAIN = 'domain'
+    GCS_REQUESTER_PAYS_PROJECT = 'gcs_requester_pays/project'
+    GCS_REQUESTER_PAYS_BUCKETS = 'gcs_requester_pays/buckets'
+    BATCH_BUCKET = 'batch/bucket'
+    BATCH_REMOTE_TMPDIR = 'batch/remote_tmpdir'
+    BATCH_REGIONS = 'batch/regions'
+    BATCH_BILLING_PROJECT = 'batch/billing_project'
+    BATCH_BACKEND = 'batch/backend'
+    QUERY_BACKEND = 'query/backend'
+    QUERY_JAR_URL = 'query/jar_url'
+    QUERY_BATCH_DRIVER_CORES = 'query/batch_driver_cores'
+    QUERY_BATCH_WORKER_CORES = 'query/batch_worker_cores'
+    QUERY_BATCH_DRIVER_MEMORY = 'query/batch_driver_memory'
+    QUERY_BATCH_WORKER_MEMORY = 'query/batch_worker_memory'
+    QUERY_NAME_PREFIX = 'query/name_prefix'
+    QUERY_DISABLE_PROGRESS_BAR = 'query/disable_progress_bar'
