070fa1c4205fc83870b2cc7d60cbb470d03bdf17
Daniel Goldstein <danielgold95@gmail.com>
2023-08-24 13:33:29 -0400

    update azure terraform

--- infra/azure/modules/batch/main.tf
+++ infra/azure/modules/batch/main.tf
@@ -39,6 +39,21 @@ resource "azurerm_shared_image" "batch_worker" {
   }
 }
 
+resource "azurerm_shared_image" "batch_worker_22_04" {
+  name                = "batch-worker-22-04"
+  gallery_name        = azurerm_shared_image_gallery.batch.name
+  resource_group_name = var.resource_group.name
+  location            = var.resource_group.location
+  os_type             = "Linux"
+  specialized	        = false
+
+  identifier {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "22.04-LTS"
+  }
+}
+
 resource "kubernetes_secret" "batch_worker_ssh_public_key" {
   metadata {
     name = "batch-worker-ssh-public-key"
