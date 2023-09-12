d6bb4c07f7f3de872a50fa3a4ebf50c7292d758d
Sophie Parsa <parsa@wm9c6-e4e.broadinstitute.org>
2023-09-12 14:51:34 -0400

    remove gpu sku hard coding

--- batch/batch/cloud/gcp/driver/pricing.py
+++ batch/batch/cloud/gcp/driver/pricing.py
@@ -23,99 +23,6 @@ from ..resources import (
 log = logging.getLogger('pricing')
 
 
-G2_CPU_SKUS = [
-    # G2 Instance Core running in Iowa
-    'D18E-3563-415F',
-    # G2 Instance Core running in Columbus
-    'A312-C1EC-7F1A',
-    # Spot Preemptible G2 Instance Core running in Iowa
-    '59A6-9C49-BDDB',
-    # Spot Preemptible G2 Instance Core running in Columbus
-    'B56F-7169-FA2D',
-    # Spot Preemptible G2 Instance Core running in Las Vegas
-    'E7D9-CF86-B013',
-    # G2 Instance Core running in Virginia
-    'CF07-A027-C390',
-    # G2 Instance Core running in Los Angeles
-    'F81A-966F-28CD',
-    # Spot Preemptible G2 Instance Core running in Dallas
-    '36AA-92D4-D377',
-    # Spot Preemptible G2 Instance Core running in Los Angeles
-    'BA15-120B-D484',
-    # G2 Instance Core running in Salt Lake City
-    '321F-5134-70E7',
-    # Spot Preemptible G2 Instance Core running in Salt Lake City
-    '38F3-C0B3-4807',
-    # Spot Preemptible G2 Instance Core running in Virginia
-    '55E2-9EB8-778F',
-    # G2 Instance Core running in Dallas
-    'B911-29A9-DC06',
-    # G2 Instance Core running in Las Vegas
-    '29FF-9F02-F5CE',
-]
-
-G2_RAM_SKUS = [
-    # G2 Instance Ram running in Virginia
-    'A15F-B56F-97FE',
-    # G2 Instance Ram running in Iowa
-    'F0A4-E3D3-33BD',
-    # Spot Preemptible G2 Instance Ram running in Iowa
-    'BF80-650A-78E6',
-    # Spot Preemptible G2 Instance Ram running in Los Angeles
-    '5076-3195-EAF2',
-    # G2 Instance Ram running in Columbus
-    'AD5C-91E0-93B5',
-    # Spot Preemptible G2 Instance Ram running in Virginia
-    'CE56-B589-1E68',
-    # Spot Preemptible G2 Instance Ram running in Dallas
-    '92B9-A092-A5E8',
-    # G2 Instance Ram running in Los Angeles
-    'E560-4373-83C8',
-    # Spot Preemptible G2 Instance Ram running in Las Vegas
-    '22F3-4DB2-9B5C',
-    # Spot Preemptible G2 Instance Ram running in Salt Lake City
-    'BE68-B175-373C',
-    # Spot Preemptible G2 Instance Ram running in Columbus
-    '3E11-DA70-84B2',
-    # G2 Instance Ram running in Dallas
-    'BAC8-4077-EA4B',
-    # G2 Instance Ram running in Salt Lake City
-    'D79F-7937-D293',
-    # G2 Instance Ram running in Las Vegas
-    'BB7E-DDF7-ACB4',
-]
-
-NVIDIA_L4_GPUS = [
-    # Nvidia L4 GPU running in Dallas
-    '4E35-7276-6341',
-    # Nvidia L4 GPU running in Virginia
-    '1D4C-3419-0297',
-    # Nvidia L4 GPU attached to Spot Preemptible VMs running in Virginia
-    'E548-B46D-71CD',
-    # Nvidia L4 GPU attached to Spot Preemptible VMs running in Las Vegas
-    '25E8-AAEC-A70B',
-    # Nvidia L4 GPU running in Las Vegas
-    '6E7A-3CB4-0441',
-    # Nvidia L4 GPU attached to Spot Preemptible VMs running in Los Angeles
-    'A73C-2F6F-F66F',
-    'F20C-EDFB-A03A',
-    # Nvidia L4 GPU running in Iowa
-    'A88A-5A60-E821',
-    # Nvidia L4 GPU running in Columbus
-    '1BBE-D828-4747',
-    # Nvidia L4 GPU running in Los Angeles
-    '455E-02FE-F1F8',
-    # Nvidia L4 GPU attached to Spot Preemptible VMs running in Iowa
-    'AEC2-3D5C-61BF',
-    # Nvidia L4 GPU attached to Spot Preemptible VMs running in Salt Lake City
-    '229F-226E-AB3E',
-    # Nvidia L4 GPU attached to Spot Preemptible VMs running in Dallas
-    'A7DD-B2F1-3095',
-    # Nvidia L4 GPU running in Salt Lake City
-    '18DB-5EFF-CABE',
-]
-
-
 class GCPComputePrice(Price):
     def __init__(
         self,
@@ -317,8 +224,6 @@ def process_compute_sku(sku: dict, regions: List[str]) -> List[GCPComputePrice]:
     service_regions = sku['serviceRegions']
     for service_region in service_regions:
         if service_region in regions:
-            if instance_family == 'g2':
-                assert sku["skuId"] in G2_CPU_SKUS, sku
             compute_prices.append(
                 GCPComputePrice(instance_family, preemptible, service_region, cost_per_hour, effective_start_date)
             )
@@ -352,8 +257,6 @@ def process_accelerator_sku(sku: dict, regions: List[str]) -> List[GCPAccelerato
     service_regions = sku['serviceRegions']
     for service_region in service_regions:
         if service_region in regions:
-            if accelerator_family == 'l4':
-                assert sku["skuId"] in NVIDIA_L4_GPUS, sku
             compute_prices.append(
                 GCPAcceleratorPrice(
                     accelerator_family, preemptible, service_region, cost_per_hour, effective_start_date
@@ -385,8 +288,6 @@ def process_memory_sku(sku: dict, regions: List[str]) -> List[GCPMemoryPrice]:
     service_regions = sku['serviceRegions']
     for service_region in service_regions:
         if service_region in regions:
-            if instance_family == 'g2':
-                assert sku["skuId"] in G2_RAM_SKUS, sku
             memory_prices.append(
                 GCPMemoryPrice(instance_family, preemptible, service_region, cost_per_hour, effective_start_date)
             )
@@ -441,31 +342,6 @@ def process_disk_sku(sku: dict, regions: List[str]) -> List[GCPDiskPrice]:
         if service_region in regions:
             disk_prices.append(GCPDiskPrice('pd-ssd', service_region, cost_per_month, effective_start_date))
     return disk_prices
-
-
-# Not called but this logic is how I got the gpu SKUs
-async def get_gpu_skus(billing_client: aiogoogle.GoogleBillingClient, currency_code: str) -> List[set]:
-    params = {'currencyCode': currency_code}
-    g2_ram_total = set()
-    g2_cpu_total = set()
-    gpu_total = set()
-    async for sku in billing_client.list_skus('/6F81-5844-456A/skus', params=params):
-        category = sku['category']
-        if (
-            sku['description'].startswith("G2 Instance")
-            or sku['description'].startswith("Spot Preemptible G2 Instance")
-        ) and "us-" in sku['serviceRegions'][0]:
-            if 'Core' in sku['description']:
-                g2_cpu_total.add(sku["skuId"])
-            elif 'Ram' in sku['description']:
-                g2_ram_total.add(sku["skuId"])
-        if (
-            sku['description'].startswith("Nvidia L4 GPU")
-            and 'GPU' in category['resourceGroup']
-            and "us-" in sku['serviceRegions'][0]
-        ):
-            gpu_total.add(sku["skuId"])
-    return [g2_cpu_total, g2_ram_total, gpu_total]
 
 
 async def fetch_prices(
