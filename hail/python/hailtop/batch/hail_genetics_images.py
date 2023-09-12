26f4133ef02af498c29199cf7353167148e53035
Daniel Goldstein <danielgold95@gmail.com>
2023-09-12 19:39:28 -0400

    [docker] Build hailgenetics/hail images with all supported python versions

--- hail/python/hailtop/batch/hail_genetics_images.py
+++ hail/python/hailtop/batch/hail_genetics_images.py
@@ -1,5 +1,7 @@
 import sys
 
+from hailtop import pip_version
+
 
 HAIL_GENETICS = 'hailgenetics/'
 HAIL_GENETICS_IMAGES = [
@@ -7,11 +9,11 @@ HAIL_GENETICS_IMAGES = [
     for name in ('hail', 'hailtop', 'genetics', 'python-dill', 'vep-grch37-85', 'vep-grch38-95')]
 
 
-def hailgenetics_python_dill_image_for_current_python_version():
+def hailgenetics_hail_image_for_current_python_version():
     version = sys.version_info
     if version.major != 3 or version.minor not in (9, 10, 11):
         raise ValueError(
             f'You must specify an "image" for Python jobs and the BatchPoolExecutor if you are '
             f'using a Python version other than 3.9, 3.10, or 3.11 (you are using {version})'
         )
-    return f'hailgenetics/python-dill:{version.major}.{version.minor}-slim'
+    return f'hailgenetics/hail:{pip_version()}-py{version.major}.{version.minor}'
