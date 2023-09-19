6c6d96b2ca41dc2a93cbfd71823aff07d0176227
Dan King <daniel.zidan.king@gmail.com>
2023-09-19 17:20:57 -0400

    [batch] disable bunch submission progress bar when fewer than 100 (#13669)
    I think this was inadvertantly broken. The `Progress.disable` property
    needs to be set *before* you start the progress bar, otherwise it has no
    effect. Clients of all these progress bars should not touch their
    `Progress` instance, that is an implementation detail. I have changed
    rich_progress_bar.py to use `_progress` to clearly communicate this.
    
    Unrelatedly, it seems that `add_task` accepts varargs which become
    arbitrary additional metadata for the task. There is no `disable`
    keyword, that is just added as additional metadata. Instead, we must use
    the `visible` keyword to enable/disable the task.
    
    I verified this looks right now:
    
    ```python3
    In [1]: import hailtop.batch_client.aioclient as ac
       ...: try:
       ...:     client = await ac.BatchClient.create('hail')
       ...:     b = client.create_batch()
       ...:     resources = {'machine_type': "g2-standard-4", 'storage': '100Gi'}
       ...:     j = b.create_job(
       ...:         '2.0.1-debian-11-r122',
       ...:         ['python', '-c', 'import torch; assert torch.cuda.is_available()'],
       ...:         resources=resources,
       ...:     )
       ...:     await b.submit()
       ...:     await b.wait()
       ...: finally:
       ...:     await client.close()
    https://batch.hail.is/batches/8038881 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100% 1/1 0:00:00 0:01:53
    ```

--- hail/python/hailtop/batch_client/aioclient.py
+++ hail/python/hailtop/batch_client/aioclient.py
@@ -782,16 +782,12 @@ class Batch:
                       max_bunch_bytesize: int,
                       max_bunch_size: int,
                       disable_progress_bar: bool,
-                      min_bunches_for_progress_bar: Optional[int],
                       progress: BatchProgressBar) -> Optional[int]:
         n_jobs = len(self._jobs)
         byte_job_specs_bunches, job_bunch_sizes = self._create_bunches(self._job_specs, max_bunch_bytesize, max_bunch_size)
         n_job_bunches = len(byte_job_specs_bunches)
 
-        if min_bunches_for_progress_bar is not None and n_job_bunches < 100:
-            progress.progress.disable = True
-
-        with progress.with_task('submit job bunches', total=n_jobs, disable=disable_progress_bar) as job_progress_task:
+        with progress.with_task('submit job bunches', total=n_jobs, disable=(disable_progress_bar or n_job_bunches < 100)) as job_progress_task:
             if not self.is_created:
                 if n_job_bunches == 0:
                     await self._open_batch()
@@ -836,10 +832,10 @@ class Batch:
         assert max_bunch_size > 0
 
         if progress:
-            start_job_id = await self._submit(max_bunch_bytesize, max_bunch_size, disable_progress_bar, None, progress)
+            start_job_id = await self._submit(max_bunch_bytesize, max_bunch_size, disable_progress_bar, progress)
         else:
             with BatchProgressBar(disable=disable_progress_bar) as progress2:
-                start_job_id = await self._submit(max_bunch_bytesize, max_bunch_size, disable_progress_bar, 100, progress2)
+                start_job_id = await self._submit(max_bunch_bytesize, max_bunch_size, disable_progress_bar, progress2)
 
         assert self.is_created
 
