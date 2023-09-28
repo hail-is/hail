a4ea3e5566822b6f8ba829a487b1f1c70b1c97d4
Edmund Higham <ehigham@users.noreply.github.com>
2023-09-28 14:40:42 -0400

    [query] Deprecate `collect_all` in `hail.plots.*` (use `n_divisions` instead) (#13738)
    Fixes #13696.
    `n_divisions` is a downsampling factor. It conflicts with `collect_all`
    when `collect_all=True` is specified.
    Deprecate `collect_all` and issue warning when users supply a non-`None`
    `collect_all` argument.
    Use `n_divisions=None` to not downsample (implies `collect_all=True`).
    Throw when `collect_all=True` and `n_divisions` is not `None` - this is
    a conflict.

--- hail/python/test/hail/plot/test_plot.py
+++ hail/python/test/hail/plot/test_plot.py
@@ -1,3 +1,6 @@
+import pytest
+from unittest.mock import patch
+
 import hail as hl
 
 
@@ -52,11 +55,38 @@ def test_qq():
     ht = hl.utils.range_table(100)
     hl.plot.qq(ht.idx / 100)
 
-
 def test_manhattan():
     ht = hl.balding_nichols_model(1, n_variants=100, n_samples=1).rows()
     ht = ht.add_index('idx')
     hl.plot.manhattan(ht.idx / 100)
+
+
+@pytest.mark.parametrize(
+    'name, plot'
+    , [ ('manhattan', hl.plot.manhattan                                     )
+      , ('scatter',   lambda x, **kwargs: hl.plot.scatter(x, x, **kwargs)   )
+      , ('join_plot', lambda x, **kwargs: hl.plot.joint_plot(x, x,** kwargs))
+      , ('qq',        hl.plot.qq                                            )
+      ]
+    )
+def test_plots_deprecated_collect_all(name, plot):
+    ht = hl.balding_nichols_model(1, n_variants=100, n_samples=1).rows()
+    ht = ht.add_index('idx')
+
+    with pytest.raises(ValueError):
+        plot(ht.idx, collect_all=True)
+
+    with pytest.raises(ValueError):
+        plot(ht.idx, n_divisions=0)
+
+    with patch('warnings.warn') as mock:
+        plot(ht.idx, n_divisions=None)
+        mock.assert_not_called()
+
+    with patch('warnings.warn') as mock:
+        plot(ht.idx, n_divisions=None, collect_all=True)
+        mock.assert_called()
+        assert name in mock.call_args_list[0][0][0]
 
 
 def test_visualize_missingness():
