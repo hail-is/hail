2d47d81010daf49f763d5e5d49c2c677642e5dca
Daniel Goldstein <danielgold95@gmail.com>
2023-09-28 10:35:07 -0400

    [linting] Add git to linting image and catch SQL modifications again

--- check-sql.sh
+++ check-sql.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
-# Verify the the working tree modifices no sql files relative to the main
+set -e
+
+# Verify the the working tree modifies no sql files relative to the main
 # branch. This will always pass on a deploy because the working tree is an
 # unmodified copy of the main branch.
 
 target_treeish=${HAIL_TARGET_SHA:-$(git merge-base main HEAD)}
-
-modified_sql_file_list=$(mktemp)
 
 if [ ! -d sql ]; then
     echo 'No migrations to check, exiting.'
     exit 0
 fi
 
+set +e
 git diff --name-status $target_treeish sql \
-    | grep -Ev $'^A|^M|^D\t[^/]+/sql/(estimated-current.sql|delete-[^ ]+-tables.sql)' \
-           > $modified_sql_file_list
+    | grep -Ev $'^A|^M\t[^/]+/sql/estimated-current.sql|^D\t[^/]+/sql/delete-[^ ]+-tables.sql'
+grep_exit_code=$?
 
-if [ "$(cat $modified_sql_file_list | wc -l)" -ne 0 ]
+
+if [[ $grep_exit_code -eq 0 ]]
 then
-    cat $modified_sql_file_list
     echo 'At least one migration file was modified. See above.'
     exit 1
+elif [[ $grep_exit_code -eq 1 ]]
+then
+    # Exit code 1 means nothing survived grep's filter, so no illegal changes were made
+    # https://www.gnu.org/software/grep/manual/html_node/Exit-Status.html#Exit-Status-1
+    exit 0
 fi
+
+exit $grep_exit_code
