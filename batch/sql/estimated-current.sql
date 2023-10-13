19980c9e9ba4194d4692c9810322d14e487b233e
Jackie Goldstein <jigold@broadinstitute.org>
2023-10-13 13:47:25 -0400

    rename

--- batch/sql/estimated-current.sql
+++ batch/sql/estimated-current.sql
@@ -210,8 +210,8 @@ CREATE INDEX `job_groups_time_created` ON `job_groups` (`batch_id`, `time_create
 CREATE INDEX `job_groups_time_completed` ON `job_groups` (`batch_id`, `time_completed`);  # used in list job groups and UI
 CREATE INDEX `job_groups_state_cancel_after_n_failures` ON `job_groups` (`state`, `cancel_after_n_failures`);  # used in cancelling any cancel fast job groups
 
-DROP TABLE IF EXISTS `job_group_tree`;
-CREATE TABLE IF NOT EXISTS `job_group_tree` (
+DROP TABLE IF EXISTS `job_group_self_and_ancestors`;
+CREATE TABLE IF NOT EXISTS `job_group_self_and_ancestors` (
   `batch_id` BIGINT NOT NULL,
   `job_group_id` INT NOT NULL,
   `parent_id` INT NOT NULL,
@@ -220,8 +220,8 @@ CREATE TABLE IF NOT EXISTS `job_group_tree` (
   FOREIGN KEY (`batch_id`, `job_group_id`) REFERENCES job_groups (`batch_id`, `job_group_id`) ON DELETE CASCADE,
   FOREIGN KEY (`batch_id`, `parent_id`) REFERENCES job_groups (`batch_id`, `job_group_id`) ON DELETE CASCADE
 ) ENGINE = InnoDB;
-CREATE INDEX `job_group_tree_parent_id_level` ON `job_group_tree` (`batch_id`, `parent_id`, `level`);
-CREATE INDEX `job_group_tree_job_group_id_level` ON `job_group_tree` (`batch_id`, `job_group_id`, `level`);
+CREATE INDEX `job_group_self_and_ancestors_parent_id_level` ON `job_group_self_and_ancestors` (`batch_id`, `parent_id`, `level`);
+CREATE INDEX `job_group_self_and_ancestors_job_group_id_level` ON `job_group_self_and_ancestors` (`batch_id`, `job_group_id`, `level`);
 
 CREATE TABLE IF NOT EXISTS `batch_updates` (
   `batch_id` BIGINT NOT NULL,
@@ -702,7 +702,7 @@ BEGIN
     INSERT INTO job_groups (batch_id, job_group_id, `user`, cancel_after_n_failures, `state`, n_jobs, time_created, time_completed, callback, attributes)
     VALUES (NEW.id, 0, NEW.`user`, NEW.cancel_after_n_failures, NEW.state, NEW.n_jobs, NEW.time_created, NEW.time_completed, NEW.callback, NEW.attributes);
 
-    INSERT INTO job_group_tree (batch_id, job_group_id, parent_id, `level`)
+    INSERT INTO job_group_self_and_ancestors (batch_id, job_group_id, parent_id, `level`)
     VALUES (NEW.id, 0, 0, 0);
   END IF;
 END $$
@@ -1570,7 +1570,7 @@ BEGIN
 
   DECLARE job_group_cursor CURSOR FOR
   SELECT parent_id
-  FROM job_group_tree
+  FROM job_group_self_and_ancestors
   WHERE batch_id = in_batch_id AND job_group_id = in_job_group_id
   ORDER BY job_group_id ASC;
 
@@ -1696,7 +1696,7 @@ BEGIN
     UPDATE batches_n_jobs_in_complete_states
     INNER JOIN (
       SELECT batch_id, parent_id
-      FROM job_group_tree
+      FROM job_group_self_and_ancestors
       WHERE batch_id = in_batch_id AND job_group_id = cur_job_group_id AND job_group_id != 0
       ORDER BY job_group_id ASC
     ) AS t ON batches_n_jobs_in_complete_states.id = t.batch_id AND batches_n_jobs_in_complete_states.job_group_id = t.parent_id
