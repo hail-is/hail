77c21d0b2bbe6131fda4833356d7f71a3bded194
Daniel Goldstein <danielgold95@gmail.com>
2023-10-23 11:33:14 -0400

    bring back wirespec

--- hail/src/main/scala/is/hail/expr/ir/EmitClassBuilder.scala
+++ hail/src/main/scala/is/hail/expr/ir/EmitClassBuilder.scala
@@ -333,7 +333,7 @@ class EmitClassBuilder[C](
   private[this] def encodeLiterals(): Array[AnyRef] = {
     val (literals, preEncodedLiterals) = emodb.literalsResult()
     val litType = PCanonicalTuple(true, literals.map(_._1.canonicalPType.setRequired(true)): _*)
-    val spec = TypedCodecSpec(litType, BufferSpec.unblockedUncompressed)  // TODO
+    val spec = TypedCodecSpec(litType, BufferSpec.wireSpec)  // TODO
 
     cb.addInterface(typeInfo[FunctionWithLiterals].iname)
     val mb2 = newEmitMethod("addAndDecodeLiterals", FastSeq[ParamType](typeInfo[Array[AnyRef]]), typeInfo[Unit])
