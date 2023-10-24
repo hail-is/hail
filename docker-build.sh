1cca2be8e8d8e3cc8ba9e75a417259b3e1c84dce
Daniel Goldstein <danielgold95@gmail.com>
2023-10-24 09:13:41 -0400

    [make] Separate image building from image pushing

--- docker-build.sh
+++ docker-build.sh
@@ -2,20 +2,14 @@
 
 set -ex
 
-if [ -z "${NAMESPACE}" ]; then
-    echo "Must specify a NAMESPACE environment variable"
-    exit 1;
-fi
-
 CONTEXT="$(cd $1 && pwd)"
 DOCKERFILE="$CONTEXT/$2"
-REMOTE_IMAGE_NAME=$3
-EXTRA_CACHE=$4
+IMAGE_NAME=$3
 
-WITHOUT_TAG=$(echo $REMOTE_IMAGE_NAME | sed -E 's/(:[^:]+)(@[^@]+)?$//')
+WITHOUT_TAG=$(echo $IMAGE_NAME | sed -E 's/(:[^:]+)(@[^@]+)?$//')
 MAIN_CACHE=${WITHOUT_TAG}:cache
 
-if [ "${NAMESPACE}" == "default" ]; then
+if [ "${NAMESPACE:-default}" == "default" ]; then
     CACHE_IMAGE_NAME=${MAIN_CACHE}
 else
     DEV_CACHE=${WITHOUT_TAG}:cache-${NAMESPACE}
@@ -27,12 +21,8 @@ DOCKER_BUILDKIT=1 docker build \
        --file ${DOCKERFILE} \
        --cache-from ${MAIN_CACHE} \
        ${DEV_CACHE:+--cache-from ${DEV_CACHE}} \
-       ${EXTRA_CACHE:+--cache-from ${EXTRA_CACHE}} \
        --build-arg BUILDKIT_INLINE_CACHE=1 \
        ${DOCKER_BUILD_ARGS} \
-       --tag ${REMOTE_IMAGE_NAME} \
+       --tag ${IMAGE_NAME} \
        --tag ${CACHE_IMAGE_NAME} \
        ${CONTEXT}
-
-time DOCKER_BUILDKIT=1 docker push ${REMOTE_IMAGE_NAME}
-time DOCKER_BUILDKIT=1 docker push ${CACHE_IMAGE_NAME}
