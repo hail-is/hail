5a332a662ccf994c249b8587398acc9044a580c5
Dan King <dking@broadinstitute.org>
2023-10-25 11:39:07 -0400

    lints

--- hail/python/hailtop/hail_event_loop.py
+++ hail/python/hailtop/hail_event_loop.py
@@ -1,12 +1,12 @@
 import asyncio
+import nest_asyncio
+
 
 def hail_event_loop():
     '''If a running event loop exists, use nest_asyncio to allow Hail's event loops to nest inside
     it.
     If no event loop exists, ask asyncio to get one for us.
     '''
-    import asyncio  # pylint: disable=import-outside-toplevel
-    import nest_asyncio  # pylint: disable=import-outside-toplevel
 
     try:
         asyncio.get_running_loop()
