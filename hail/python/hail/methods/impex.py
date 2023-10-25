8779dc7ce8d628673fb5e5d16e89490b51892b36
Dan King <dking@broadinstitute.org>
2023-10-25 12:04:34 -0400

    [query] also prevent *reading* from cold storage

--- hail/python/hail/methods/impex.py
+++ hail/python/hail/methods/impex.py
@@ -717,6 +717,8 @@ def import_locus_intervals(
         Interval-keyed table.
     """
 
+    hl.current_backend().validate_file(path)
+
     if contig_recoding is not None:
         contig_recoding = hl.literal(contig_recoding)
 
@@ -895,6 +897,8 @@ def import_bed(path, reference_genome='default', skip_invalid_intervals=False, c
         Interval-keyed table.
     """
 
+    hl.current_backend().validate_file(path)
+
     # UCSC BED spec defined here: https://genome.ucsc.edu/FAQ/FAQformat.html#format1
 
     t = import_table(
@@ -1004,6 +1008,8 @@ def import_fam(path, quant_pheno=False, delimiter=r'\\s+', missing='NA') -> Tabl
     -------
     :class:`.Table`
     """
+    hl.current_backend().validate_file(path)
+
     type_and_data = Env.backend().import_fam(path, quant_pheno, delimiter, missing)
     typ = hl.dtype(type_and_data['type'])
     return hl.Table.parallelize(hl.tarray(typ)._convert_from_json_na(type_and_data['data']), typ, key=['id'])
@@ -1253,6 +1259,12 @@ def import_bgen(
 
     """
 
+    if isinstance(path, list):
+        for p in path:
+            hl.current_backend().validate_file(p)
+    else:
+        hl.current_backend().validate_file(path)
+
     if n_partitions is None and block_size is None:
         block_size = 128
 
@@ -1445,6 +1457,13 @@ def import_gen(
     -------
     :class:`.MatrixTable`
     """
+
+    if isinstance(path, list):
+        for p in path:
+            hl.current_backend().validate_file(p)
+    else:
+        hl.current_backend().validate_file(path)
+
     gen_table = import_lines(path, min_partitions)
     sample_table = import_lines(sample_file)
     rg = reference_genome.name if reference_genome else None
@@ -1759,6 +1778,13 @@ def import_table(
     -------
     :class:`.Table`
     """
+
+    if isinstance(paths, list):
+        for p in paths:
+            hl.current_backend().validate_file(p)
+    else:
+        hl.current_backend().validate_file(paths)
+
     if len(delimiter) < 1:
         raise ValueError('import_table: empty delimiter is not supported')
 
@@ -1946,6 +1972,9 @@ def import_lines(paths, min_partitions=None, force_bgz=False, force=False, file_
     """
 
     paths = wrap_to_list(paths)
+
+    for p in path:
+        hl.current_backend().validate_file(p)
 
     if file_per_partition and min_partitions is not None:
         if min_partitions > len(paths):
@@ -2140,10 +2169,14 @@ def import_matrix_table(
     :class:`.MatrixTable`
         MatrixTable constructed from imported data.
     """
+
     row_key = wrap_to_list(row_key)
     comment = wrap_to_list(comment)
     paths = [hl.current_backend().fs.canonicalize_path(p) for p in wrap_to_list(paths)]
     missing_list = wrap_to_list(missing)
+
+    for p in paths:
+        hl.current_backend().validate_file(p)
 
     def comment_filter(table):
         return (
@@ -2643,6 +2676,9 @@ def import_plink(
 
     """
 
+    for p in (bed, bim, fam):
+        hl.current_backend().validate_file(p)
+
     if contig_recoding is None:
         if reference_genome is None:
             contig_recoding = {}
@@ -2710,6 +2746,8 @@ def read_matrix_table(
     -------
     :class:`.MatrixTable`
     """
+    hl.current_backend().validate_file(path)
+
     if _load_refs:
         for rg_config in Env.backend().load_references_from_dataset(path):
             hl.ReferenceGenome._from_config(rg_config)
@@ -2794,6 +2832,8 @@ def get_vcf_metadata(path):
     -------
     :obj:`dict` of :class:`str` to (:obj:`dict` of :obj:`str` to (:obj:`dict` of :obj:`str` to :obj:`str`))
     """
+
+    hl.current_backend().validate_file(path)
 
     return Env.backend().parse_vcf_metadata(path)
 
@@ -3109,6 +3149,8 @@ def import_gvcf_interval(
     find=None,
     replace=None,
 ):
+    hl.current_backend().validate_file(path)
+
     indices, aggs = hl.expr.unify_all(path, file_num, contig, start, end)
     stream_ir = ir.ReadPartition(
         hl.struct(fileNum=file_num, path=path, contig=contig, start=start, end=end)._ir,
@@ -3188,6 +3230,7 @@ def index_bgen(
         If ``True``, skip loci that are not consistent with `reference_genome`.
 
     """
+
     rg_t = hl.tlocus(reference_genome) if reference_genome else hl.tstruct(contig=hl.tstr, position=hl.tint32)
     if index_file_map is None:
         index_file_map = {}
@@ -3198,6 +3241,7 @@ def index_bgen(
     fs = hl.current_backend().fs
     paths = []
     for p in raw_paths:
+        hl.current_backend().validate_file(p)
         if fs.is_file(p):
             paths.append(p)
         else:
@@ -3283,6 +3327,8 @@ def read_table(
     -------
     :class:`.Table`
     """
+    hl.current_backend().validate_file(path)
+
     if _load_refs:
         for rg_config in Env.backend().load_references_from_dataset(path):
             hl.ReferenceGenome._from_config(rg_config)
@@ -3336,6 +3382,9 @@ def export_elasticsearch(t, host, port, index, index_type, block_size, config=No
 
 @typecheck(paths=sequenceof(str), key=nullable(sequenceof(str)), intervals=nullable(sequenceof(anytype)))
 def import_avro(paths, *, key=None, intervals=None):
+    for p in path:
+        hl.current_backend().validate_file(path)
+
     if not paths:
         raise ValueError('import_avro requires at least one path')
     if (key is None) != (intervals is None):
