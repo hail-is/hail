7d069c5de0d4086984a1349ba3e00801097f06a9
Dan King <dking@broadinstitute.org>
2023-10-25 15:07:08 -0400

    fixes

--- hail/src/main/scala/is/hail/io/bgen/LoadBgen.scala
+++ hail/src/main/scala/is/hail/io/bgen/LoadBgen.scala
@@ -243,12 +243,12 @@ object LoadBgen {
     val indexFiles = getIndexFileNames(fs, files, indexFileMap)
 
     val bgenFilesWhichAreMisssingIdx2Files = files.zip(indexFiles).filterNot {
-      case (f, index) => fs.isFile(index) && index.endsWith("idx2")
+      case (f, index) => index.endsWith("idx2") && fs.isFile(index + "/index") && fs.isFile(index + "/metadata.json.gz")
     }.map(_._1.getPath)
 
     if (bgenFilesWhichAreMisssingIdx2Files.nonEmpty)
       fatal(
-        s"""The following BGEN .idx2 index files do not exist. Use 'index_bgen' to create the index file once before calling 'import_bgen':
+        s"""The following BGEN files have no .idx2 index file. Use 'index_bgen' to create the index file once before calling 'import_bgen':
            |  ${ bgenFilesWhichAreMisssingIdx2Files.mkString("\n  ") }""".stripMargin)
     indexFiles
   }
