e41b828ee67652ee4a9de1ee51cbec9e5d09a9dd
Chris Vittal <cvittal@broadinstitute.org>
2023-10-27 15:10:26 -0400

    fix link?

--- hail/python/hail/genetics/reference_genome.py
+++ hail/python/hail/genetics/reference_genome.py
@@ -51,11 +51,12 @@ class ReferenceGenome:
     Note
     ----
     Hail allows setting a default reference so that the ``reference_genome``
-    argument of :func:`~hail.import_vcf` does not need to be used constantly.
-    It is a current limitation of Hail that a custom reference genome cannot be
-    used as the ``default_reference`` argument of :func:`~hail.init`. In order
-    to set a custom reference genome as default, pass the reference as an argument
-    to :func:`~hail.default_reference` after initializing Hail.
+    argument of :func:`~hail.methods.import_vcf` does not need to be used
+    constantly. It is a current limitation of Hail that a custom reference
+    genome cannot be used as the ``default_reference`` argument of
+    :func:`~hail.init`. In order to set a custom reference genome as default,
+    pass the reference as an argument to :func:`~hail.default_reference` after
+    initializing Hail.
 
     Parameters
     ----------
