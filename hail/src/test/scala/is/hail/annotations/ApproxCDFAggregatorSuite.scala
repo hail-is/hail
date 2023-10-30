63c74fd1f8b7a55cb9db3afddad53cc6fc5e04e6
Patrick Schultz <pschultz@broadinstitute.org>
2023-10-30 13:23:43 -0400

    [query] refactor approx_cdf to support manual combining (#13935)
    This PR enables users to combine results from multiple independent
    approx_cdf aggregators, for instance to allow updating quantile
    summaries when adding new samples.
    
    In more detail:
    * Change the result type of the internal aggregator to be a lossless
    representation of the internal state.
    * Add a registered function which expose the CombOp of the aggregator.
    * Move the function which computes the old result type from the internal
    state to python.
    * Add a flag `_raw` to `approx_cdf` which produces the internal result
    type, which supports combining; otherwise convert to the old result
    type.

--- hail/src/test/scala/is/hail/annotations/ApproxCDFAggregatorSuite.scala
+++ hail/src/test/scala/is/hail/annotations/ApproxCDFAggregatorSuite.scala
@@ -18,7 +18,7 @@ class ApproxCDFAggregatorSuite extends TestNGSuite {
     val levels: Array[Int] = Array(0,4,7,10)
     val items: Array[Double] = Array(7,2,6,4, 1,3,8, 0,5,9)
     val compactionCounts: Array[Int] = Array(0, 0, 0)
-    val combiner = new ApproxCDFCombiner(levels, items, compactionCounts, 3, Double.NaN, rand)
+    val combiner = new ApproxCDFCombiner(levels, items, compactionCounts, 3, rand)
     combiner.compactLevel(0)
     assert(items.view(1,10) sameElements Array(2,7, 1,3,6,8, 0,5,9))
   }
@@ -29,19 +29,8 @@ class ApproxCDFAggregatorSuite extends TestNGSuite {
     val levels: Array[Int] = Array(0,3,6,9)
     val items: Array[Double] = Array(7,2,4, 1,3,8, 0,5,9)
     val compactionCounts: Array[Int] = Array(0, 0, 0)
-    val combiner = new ApproxCDFCombiner(levels, items, compactionCounts, 3, Double.NaN, rand)
+    val combiner = new ApproxCDFCombiner(levels, items, compactionCounts, 3, rand)
     combiner.compactLevel(1)
     assert(items.view(1,9) sameElements Array(7,2,4, 1, 0,5,8,9))
-  }
-
-  @Test
-  def testCompactLevelKeep() {
-    val rand = new java.util.Random(1) // first Boolean is `true`
-    val levels: Array[Int] = Array(0,3,7,10)
-    val items: Array[Double] = Array(7,2,4, 1,3,6,8, 0,5,9)
-    val compactionCounts: Array[Int] = Array(0, 0, 0)
-    val combiner = new ApproxCDFCombiner(levels, items, compactionCounts, 3, .01, rand)
-    combiner.compactLevel(1)
-    assert(items.view(1,10) sameElements Array(7,2,4, 1,8, 0,5,6,9))
   }
 }
