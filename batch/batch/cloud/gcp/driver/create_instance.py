b5732d05135a5d84b656dbb26ceb7e6c117076b2
Jackie Goldstein <jigold@broadinstitute.org>
2023-11-06 11:34:41 -0500

    [batch] Prevent the worker container from being killed on OOM event

--- batch/batch/cloud/gcp/driver/create_instance.py
+++ batch/batch/cloud/gcp/driver/create_instance.py
@@ -370,6 +370,8 @@ docker run \
 --security-opt apparmor:unconfined \
 --network host \
 --cgroupns host \
+--oom-score-adj -1000 \
+--oom-kill-disable \
 {docker_run_gpu_args} \
 $BATCH_WORKER_IMAGE \
 python3 -u -m batch.worker.worker >worker.log 2>&1
