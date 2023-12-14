4b725ccf060f2b51779ccc40c0c019792bb4f035
Chris Vittal <cvittal@broadinstitute.org>
2023-12-14 12:45:23 -0500

    Add long listing

--- hail/python/hailtop/hailctl/fs/cli.py
+++ hail/python/hailtop/hailctl/fs/cli.py
@@ -1,8 +1,8 @@
-import pprint
-
+import tabulate
 import typer
 import hailtop.fs
 
+from datetime import datetime
 from typing import List, Optional, Annotated as Ann
 
 from typer import Option as Opt, Argument as Arg
@@ -18,6 +18,7 @@ app = typer.Typer(
 @app.command()
 def ls(ctx: typer.Context,
        paths: Ann[Optional[List[str]], Arg()] = None,
+       long: Ann[bool, Opt('-l', '--long', help='use long listing format')] = False,
        ):
     '''
     List objects
@@ -26,4 +27,12 @@ def ls(ctx: typer.Context,
         paths = ['.']
     for path in paths:
         listing = hailtop.fs.ls(path)
-        print(*(item.path for item in listing), sep='\n')
+        if long:
+            listing = [(item.typ,
+                        item.size if item.size > 0 else None,
+                        datetime.utcfromtimestamp(item.modification_time)
+                        if item.modification_time is not None else None,
+                        item.path) for item in listing]
+            print(tabulate.tabulate(listing, tablefmt='plain'))
+        else:
+            print(*(item.path for item in listing), sep='\n')
