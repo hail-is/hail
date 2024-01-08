8727906ac8ebe516a7b8e274247276451d19726f
Jackie Goldstein <jigold@broadinstitute.org>
2024-01-08 14:23:31 -0500

    full stdout

--- hail/python/test/hailtop/hailctl/batch/test_submit.py
+++ hail/python/test/hailtop/hailctl/batch/test_submit.py
@@ -108,4 +108,5 @@ def test_dir_outside_curdir(runner: CliRunner):
         write_hello(f'{dir}/hello2.txt')
         write_script(dir, '/hello1.txt')
         res = runner.invoke(cli.app, ['submit', '--files', f'{dir}/:/', '../test_job.py'])
+        print(res.stdout)
         assert res.exit_code == 0, (res.exit_code, res.stdout, res.stderr)
