4d8800685198093dbce4943bf31c68e1094537af
Chris Vittal <cvittal@broadinstitute.org>
2024-01-11 14:50:45 -0500

    fmt

--- hail/python/hailtop/hailctl/fs/cli.py
+++ hail/python/hailtop/hailctl/fs/cli.py
@@ -35,10 +35,13 @@ async def async_du(fs, path: str, human_readable: bool, summarize: bool) -> int:
 
 
 @app.command()
-def du(paths: Ann[Optional[List[str]], Arg()] = None,
-       human_readable: Ann[bool, Opt('-h', '--human-readable', help='print sizes in human readable format (base 10)')] = False,
-       summarize: Ann[bool, Opt('-s', '--summarize', help='display only a total for each argument')] = False,
-       ):
+def du(
+    paths: Ann[Optional[List[str]], Arg()] = None,
+    human_readable: Ann[
+        bool, Opt('-h', '--human-readable', help='print sizes in human readable format (base 10)')
+    ] = False,
+    summarize: Ann[bool, Opt('-s', '--summarize', help='display only a total for each argument')] = False,
+):
     '''
     Display storage resourse usage
     '''
@@ -59,10 +62,11 @@ def du(paths: Ann[Optional[List[str]], Arg()] = None,
 
 
 @app.command()
-def ls(paths: Ann[Optional[List[str]], Arg()] = None,
-       long: Ann[bool, Opt('-l', '--long', help='use long listing format')] = False,
-       human_readable: Ann[bool, Opt('-h', '--human-readable', help='print human readable file sizes (base 10)')] = False,
-       ):
+def ls(
+    paths: Ann[Optional[List[str]], Arg()] = None,
+    long: Ann[bool, Opt('-l', '--long', help='use long listing format')] = False,
+    human_readable: Ann[bool, Opt('-h', '--human-readable', help='print human readable file sizes (base 10)')] = False,
+):
     '''
     List objects
     '''
@@ -85,13 +89,18 @@ def ls(paths: Ann[Optional[List[str]], Arg()] = None,
             listing = fs.ls(path)
             listing.sort(key=lambda item: item.path)
             if long:
-                listing = [(item.typ,
-                            display_bytes(item.size),
-                            datetime.utcfromtimestamp(item.modification_time)
-                            if item.modification_time is not None else None,
-                            item.path) for item in listing]
-                print(tabulate.tabulate(listing, tablefmt='plain',
-                                        colalign=('global', 'right', 'global', 'global')))
+                listing = [
+                    (
+                        item.typ,
+                        display_bytes(item.size),
+                        datetime.utcfromtimestamp(item.modification_time)
+                        if item.modification_time is not None
+                        else None,
+                        item.path,
+                    )
+                    for item in listing
+                ]
+                print(tabulate.tabulate(listing, tablefmt='plain', colalign=('global', 'right', 'global', 'global')))
             else:
                 print(*(item.path for item in listing), sep='\n')
     finally:
