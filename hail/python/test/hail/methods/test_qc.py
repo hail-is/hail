2386c9c168247ca1481d98ad0f29341c0bce4e96
Jackie Goldstein <jigold@broadinstitute.org>
2024-01-12 11:59:05 -0500

    delint

--- hail/python/test/hail/methods/test_qc.py
+++ hail/python/test/hail/methods/test_qc.py
@@ -527,18 +527,16 @@ class Tests(unittest.TestCase):
         hail_vep_result = hl.vep(loftee_variants, self.vep_config_grch38_95)
         hail_vep_result = hail_vep_result.annotate(
             vep=hail_vep_result.vep.annotate(
-                input=hl.str('\t').join(
-                    [
-                        hail_vep_result.locus.contig,
-                        hl.str(hail_vep_result.locus.position),
-                        ".",
-                        hail_vep_result.alleles[0],
-                        hail_vep_result.alleles[1],
-                        ".",
-                        ".",
-                        "GT",
-                    ]
-                )
+                input=hl.str('\t').join([
+                    hail_vep_result.locus.contig,
+                    hl.str(hail_vep_result.locus.position),
+                    ".",
+                    hail_vep_result.alleles[0],
+                    hail_vep_result.alleles[1],
+                    ".",
+                    ".",
+                    "GT",
+                ])
             )
         )
         hail_vep_result = hail_vep_result.select('vep')
