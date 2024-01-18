4f65ac26c9bef2c14b07c42a06d32fc0882f2d52
Daniel Goldstein <danielgold95@gmail.com>
2024-01-18 15:27:28 -0500

    fix numeric project id

--- hail/python/hailtop/aiocloud/aiogoogle/client/metadata_server_client.py
+++ hail/python/hailtop/aiocloud/aiogoogle/client/metadata_server_client.py
@@ -13,13 +13,12 @@ class GoogleMetadataServerClient:
 
     async def numeric_project_id(self):
         if self._numeric_project_id is None:
-            self._numeric_project_id = await self._request('/project/numeric-project-id')
+            self._numeric_project_id = await retry_transient_errors(self._get_text, '/project/numeric-project-id')
         return self._numeric_project_id
 
-    async def _request(self, path: str) -> str:
-        return await retry_transient_errors(
-            self._session.get_read_json,
-            f'http://metadata.google.internal/computeMetadata/v1{path}',
-            headers={'Metadata-Flavor': 'Google'},
-            timeout=aiohttp.ClientTimeout(total=60),  # type: ignore
-        )
+    async def _get_text(self, path: str) -> str:
+        url = f'http://metadata.google.internal/computeMetadata/v1{path}'
+        headers = {'Metadata-Flavor': 'Google'}
+        timeout = aiohttp.ClientTimeout(total=60)
+        res = await self._session.get_read(url, headers=headers, timeout=timeout)
+        return res.decode('utf-8')
