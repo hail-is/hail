586d350bd165536d9d5dd42f53791b2605d7fd72
Dan King <dking@broadinstitute.org>
2024-01-23 17:47:11 -0500

    remove it

--- hail/python/test/hailtop/hailctl/batch/test_submit.py
+++ hail/python/test/hailtop/hailctl/batch/test_submit.py
@@ -13,10 +13,6 @@ def runner():
     yield CliRunner(mix_stderr=False)
 
 
-def expose_envvar_to_hailctl(name: str) -> bool:
-    return name in ('GOOGLE_APPLICATION_CREDENTIALS', 'AZURE_APPLICATION_CREDENTIALS', 'HAIL_IDENTITY_PROVIDER_JSON')
-
-
 def write_script(dir: str, filename: str):
     with open(f'{dir}/test_job.py', 'w') as f:
         f.write(f"""
@@ -37,10 +33,7 @@ def test_file_with_no_dest(runner: CliRunner):
         write_hello(f'{dir}/hello.txt')
         write_script(dir, f'{dir}/hello.txt')
         res = runner.invoke(
-            cli.app,
-            ['submit', '--wait', '--files', 'hello.txt', 'test_job.py'],
-            catch_exceptions=False,
-            env={k: v for k, v in os.environ.items() if expose_envvar_to_hailctl(k)},
+            cli.app, ['submit', '--wait', '--files', 'hello.txt', 'test_job.py'], catch_exceptions=False
         )
         assert res.exit_code == 0, repr((res.output, res.stdout, res.stderr, res.exception))
 
@@ -52,10 +45,7 @@ def test_file_in_current_dir(runner: CliRunner):
         write_hello(f'{dir}/hello.txt')
         write_script(dir, f'/hello.txt')
         res = runner.invoke(
-            cli.app,
-            ['submit', '--wait', '--files', 'hello.txt:/', 'test_job.py'],
-            catch_exceptions=False,
-            env={k: v for k, v in os.environ.items() if expose_envvar_to_hailctl(k)},
+            cli.app, ['submit', '--wait', '--files', 'hello.txt:/', 'test_job.py'], catch_exceptions=False
         )
         assert res.exit_code == 0, repr((res.output, res.stdout, res.stderr, res.exception))
 
@@ -67,10 +57,7 @@ def test_file_mount_in_child_dir(runner: CliRunner):
         write_hello(f'{dir}/hello.txt')
         write_script(dir, '/child/hello.txt')
         res = runner.invoke(
-            cli.app,
-            ['submit', '--wait', '--files', 'hello.txt:/child/', 'test_job.py'],
-            catch_exceptions=False,
-            env={k: v for k, v in os.environ.items() if expose_envvar_to_hailctl(k)},
+            cli.app, ['submit', '--wait', '--files', 'hello.txt:/child/', 'test_job.py'], catch_exceptions=False
         )
         assert res.exit_code == 0, repr((res.output, res.stdout, res.stderr, res.exception))
 
@@ -82,10 +69,7 @@ def test_file_mount_in_child_dir_to_root_dir(runner: CliRunner):
         write_hello(f'{dir}/child/hello.txt')
         write_script(dir, '/hello.txt')
         res = runner.invoke(
-            cli.app,
-            ['submit', '--wait', '--files', 'child/hello.txt:/', 'test_job.py'],
-            catch_exceptions=False,
-            env={k: v for k, v in os.environ.items() if expose_envvar_to_hailctl(k)},
+            cli.app, ['submit', '--wait', '--files', 'child/hello.txt:/', 'test_job.py'], catch_exceptions=False
         )
         assert res.exit_code == 0, repr((res.output, res.stdout, res.stderr, res.exception))
 
@@ -109,7 +93,6 @@ def test_mount_multiple_files(runner: CliRunner):
                 'test_job.py',
             ],
             catch_exceptions=False,
-            env={k: v for k, v in os.environ.items() if expose_envvar_to_hailctl(k)},
         )
         assert res.exit_code == 0, repr((res.output, res.stdout, res.stderr, res.exception))
 
@@ -122,10 +105,7 @@ def test_dir_mount_in_child_dir_to_child_dir(runner: CliRunner):
         write_hello(f'{dir}/child/hello2.txt')
         write_script(dir, '/child/hello1.txt')
         res = runner.invoke(
-            cli.app,
-            ['submit', '--wait', '--files', 'child/:/child/', 'test_job.py'],
-            catch_exceptions=False,
-            env={k: v for k, v in os.environ.items() if expose_envvar_to_hailctl(k)},
+            cli.app, ['submit', '--wait', '--files', 'child/:/child/', 'test_job.py'], catch_exceptions=False
         )
         assert res.exit_code == 0, repr((res.output, res.stdout, res.stderr, res.exception))
 
@@ -138,10 +118,7 @@ def test_file_outside_curdir(runner: CliRunner):
         write_hello(f'{dir}/hello.txt')
         write_script(dir, '/hello.txt')
         res = runner.invoke(
-            cli.app,
-            ['submit', '--wait', '--files', f'{dir}/hello.txt:/', '../test_job.py'],
-            catch_exceptions=False,
-            env={k: v for k, v in os.environ.items() if expose_envvar_to_hailctl(k)},
+            cli.app, ['submit', '--wait', '--files', f'{dir}/hello.txt:/', '../test_job.py'], catch_exceptions=False
         )
         assert res.exit_code == 0, repr((res.output, res.stdout, res.stderr, res.exception))
 
@@ -157,9 +134,6 @@ def test_dir_outside_curdir(runner: CliRunner):
         dir_basename = os.path.basename(dir)
         write_script(dir, f'/{dir_basename}/hello1.txt')
         res = runner.invoke(
-            cli.app,
-            ['submit', '--wait', '--files', f'{dir}/:/', '../test_job.py'],
-            catch_exceptions=False,
-            env={k: v for k, v in os.environ.items() if expose_envvar_to_hailctl(k)},
+            cli.app, ['submit', '--wait', '--files', f'{dir}/:/', '../test_job.py'], catch_exceptions=False
         )
         assert res.exit_code == 0, repr((res.exit_code, res.stdout, res.stderr, res.exception))
