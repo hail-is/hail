198fc1ab96227a1b9097e95a3f7d090b709422b7
Dan King <dking@broadinstitute.org>
2024-01-31 17:55:21 -0500

    agg sample qc

--- hail/python/hail/vds/methods.py
+++ hail/python/hail/vds/methods.py
@@ -1,8 +1,10 @@
-from typing import Sequence
+from typing import Sequence, Optional
 
 import hail as hl
 from hail import ir
 from hail.expr import expr_any, expr_array, expr_interval, expr_locus, expr_str, expr_bool
+from hail.expr.expressions import Expression
+from hail.expr.expressions.typed_expressions import StructExpression
 from hail.matrixtable import MatrixTable
 from hail.methods.misc import require_first_key_field_locus
 from hail.table import Table
@@ -384,6 +386,207 @@ def sample_qc(
 
     joined_results = joined_results.annotate_globals(gq_bins=hl.tuple(gq_bins), **dp_bins_field)
     return joined_results
+
+
+@typecheck(rmt=MatrixTable, gq_bins=sequenceof(int), dp_bins=sequenceof(int), dp_field=nullable(str))
+def ref_sample_qc(
+    rmt: 'MatrixTable',
+    *,
+    gq_bins: 'Sequence[int]' = (0, 20, 60),
+    dp_bins: 'Sequence[int]' = (0, 1, 10, 20, 30),
+    dp_field=None,
+) -> 'StructExpression':
+    """"""
+    require_first_key_field_locus(rmt, 'ref_sample_qc')
+
+    if 'DP' in rmt.entry:
+        dp_field_to_use = 'DP'
+    elif 'MIN_DP' in rmt.entry:
+        dp_field_to_use = 'MIN_DP'
+    else:
+        dp_field_to_use = dp_field
+
+    ref_dp_expr = {}
+    if dp_field_to_use is not None:
+        ref_dp_expr['ref_bases_over_dp_threshold'] = hl.tuple(
+            hl.agg.filter(rmt[dp_field_to_use] >= x, hl.agg.sum(1 + rmt.END - rmt.locus.position)) for x in dp_bins
+        )
+    return hl.struct(
+        bases_over_gq_threshold=hl.tuple(
+            hl.agg.filter(rmt.GQ >= x, hl.agg.sum(1 + rmt.END - rmt.locus.position)) for x in gq_bins
+        ),
+        **ref_dp_expr,
+    )
+
+
+@typecheck(global_gt=Expression, alleles=Expression)
+def vmt_sample_qc_variant_annotations(
+    *,
+    global_gt: 'Expression',
+    alleles: 'Expression',
+) -> 'Expression':
+    """"""
+    from hail.expr.functions import _num_allele_type, _allele_types
+
+    allele_types = _allele_types[:]
+    allele_types.extend(['Transition', 'Transversion'])
+    allele_enum = {i: v for i, v in enumerate(allele_types)}
+    allele_ints = {v: k for k, v in allele_enum.items()}
+
+    def allele_type(ref, alt):
+        return hl.bind(
+            lambda at: hl.if_else(
+                at == allele_ints['SNP'],
+                hl.if_else(hl.is_transition(ref, alt), allele_ints['Transition'], allele_ints['Transversion']),
+                at,
+            ),
+            _num_allele_type(ref, alt),
+        )
+
+    return hl.struct(
+        variant_ac=hl.agg.call_stats(global_gt, alleles).AC,
+        variant_atypes=alleles[1:].map(lambda alt: allele_type(alleles[0], alt)),
+    )
+
+
+@typecheck(
+    global_gt=Expression,
+    gq=Expression,
+    variant_qc=Expression,
+    variant_atypes=Expression,
+    dp=Expression,
+    gq_bins=sequenceof(int),
+    dp_field=nullable(str),
+)
+def vmt_sample_qc(
+    *,
+    global_gt: 'Expression',
+    gq: 'Expression',
+    variant_ac: 'Expression',
+    variant_atypes: 'Expression',
+    dp: Optional['Expression'] = None,
+    gq_bins: 'Sequence[int]' = (0, 20, 60),
+    dp_bins: 'Sequence[int]' = (0, 1, 10, 20, 30),
+    dp_field: Optional[str] = None,
+) -> 'Expression':
+    """"""
+    from hail.expr.functions import _allele_types
+
+    allele_types = _allele_types[:]
+    allele_types.extend(['Transition', 'Transversion'])
+    allele_enum = {i: v for i, v in enumerate(allele_types)}
+    allele_ints = {v: k for k, v in allele_enum.items()}
+
+    bound_exprs = {}
+
+    bound_exprs['n_het'] = hl.agg.count_where(global_gt.is_het())
+    bound_exprs['n_hom_var'] = hl.agg.count_where(global_gt.is_hom_var())
+    bound_exprs['n_singleton'] = hl.agg.sum(
+        hl.rbind(
+            global_gt,
+            lambda global_gt: hl.sum(
+                hl.range(0, global_gt.ploidy).map(
+                    lambda i: hl.rbind(global_gt[i], lambda gti: (gti != 0) & (variant_ac[gti] == 1))
+                )
+            ),
+        )
+    )
+    bound_exprs['n_singleton_ti'] = hl.agg.sum(
+        hl.rbind(
+            global_gt,
+            lambda global_gt: hl.sum(
+                hl.range(0, global_gt.ploidy).map(
+                    lambda i: hl.rbind(
+                        global_gt[i],
+                        lambda gti: (gti != 0)
+                        & (variant_ac[gti] == 1)
+                        & (variant_atypes[gti - 1] == allele_ints['Transition']),
+                    )
+                )
+            ),
+        )
+    )
+    bound_exprs['n_singleton_tv'] = hl.agg.sum(
+        hl.rbind(
+            global_gt,
+            lambda global_gt: hl.sum(
+                hl.range(0, global_gt.ploidy).map(
+                    lambda i: hl.rbind(
+                        global_gt[i],
+                        lambda gti: (gti != 0)
+                        & (variant_ac[gti] == 1)
+                        & (variant_atypes[gti - 1] == allele_ints['Transversion']),
+                    )
+                )
+            ),
+        )
+    )
+
+    bound_exprs['allele_type_counts'] = hl.agg.explode(
+        lambda allele_type: hl.tuple(hl.agg.count_where(allele_type == i) for i in range(len(allele_ints))),
+        (
+            hl.range(0, global_gt.ploidy)
+            .map(lambda i: global_gt[i])
+            .filter(lambda allele_idx: allele_idx > 0)
+            .map(lambda allele_idx: variant_atypes[allele_idx - 1])
+        ),
+    )
+
+    dp_exprs = {}
+    if dp is not None:
+        dp_exprs['dp'] = hl.tuple(hl.agg.count_where(dp >= x) for x in dp_bins)
+
+    gq_dp_exprs = hl.struct(**{'gq': hl.tuple(hl.agg.count_where(gq >= x) for x in gq_bins)}, **dp_exprs)
+
+    return hl.rbind(
+        hl.struct(**bound_exprs),
+        lambda x: hl.rbind(
+            hl.struct(**{
+                'gq_dp_exprs': gq_dp_exprs,
+                'n_het': x.n_het,
+                'n_hom_var': x.n_hom_var,
+                'n_non_ref': x.n_het + x.n_hom_var,
+                'n_singleton': x.n_singleton,
+                'n_singleton_ti': x.n_singleton_ti,
+                'n_singleton_tv': x.n_singleton_tv,
+                'n_snp': (
+                    x.allele_type_counts[allele_ints['Transition']] + x.allele_type_counts[allele_ints['Transversion']]
+                ),
+                'n_insertion': x.allele_type_counts[allele_ints['Insertion']],
+                'n_deletion': x.allele_type_counts[allele_ints['Deletion']],
+                'n_transition': x.allele_type_counts[allele_ints['Transition']],
+                'n_transversion': x.allele_type_counts[allele_ints['Transversion']],
+                'n_star': x.allele_type_counts[allele_ints['Star']],
+            }),
+            lambda s: s.annotate(
+                r_ti_tv=divide_null(hl.float64(s.n_transition), s.n_transversion),
+                r_ti_tv_singleton=divide_null(hl.float64(s.n_singleton_ti), s.n_singleton_tv),
+                r_het_hom_var=divide_null(hl.float64(s.n_het), s.n_hom_var),
+                r_insertion_deletion=divide_null(hl.float64(s.n_insertion), s.n_deletion),
+            ),
+        ),
+    )
+
+
+def combine_sample_qc(
+    rmt_sample_qc: Expression,
+    vmt_sample_qc: Expression,
+) -> Expression:
+    assert 'gq_dp_exprs' in vmt_sample_qc
+    assert 'bases_over_gq_threshold' in rmt_sample_qc
+
+    joined_dp_expr = {}
+    if 'bases_over_dp_threshold' in vmt_sample_qc:
+        joined_dp_expr['bases_over_dp_threshold'] = hl.tuple(
+            x + y for x, y in zip(vmt_sample_qc.gq_dp_exprs.dp, rmt_sample_qc.bases_over_dp_threshold)
+        )
+
+    return hl.struct(
+        bases_over_gq_threshold=hl.tuple(
+            x + y for x, y in zip(vmt_sample_qc.gq_dp_exprs.gq, rmt_sample_qc.bases_over_gq_threshold)
+        ),
+        **joined_dp_expr,
+    )
 
 
 @typecheck(vds=VariantDataset, samples=oneof(Table, expr_array(expr_str)), keep=bool, remove_dead_alleles=bool)
