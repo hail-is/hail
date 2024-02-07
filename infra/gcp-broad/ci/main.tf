b5f458e344655ceec72bd1696441aa217cedbd70
Dan King <dking@broadinstitute.org>
2024-02-07 18:27:29 -0500

    [terraform] restore CI access to ci bucket after UBLA is applied
    Already applied.

--- infra/gcp-broad/ci/main.tf
+++ infra/gcp-broad/ci/main.tf
@@ -41,7 +41,7 @@ resource "google_storage_bucket" "bucket" {
 
 resource "google_storage_bucket_iam_member" "ci_bucket_admin" {
   bucket = google_storage_bucket.bucket.name
-  role = "roles/storage.legacyBucketWriter"
+  role = "roles/storage.objectAdmin"
   member = "serviceAccount:${var.ci_email}"
 }
 
