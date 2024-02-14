5f348f8476017c157f46a0c930927286f4819c7e
Jackie Goldstein <jigold@broadinstitute.org>
2024-02-14 13:13:27 -0500

    delint

--- batch/batch/front_end/front_end.py
+++ batch/batch/front_end/front_end.py
@@ -111,8 +111,8 @@ from ..utils import (
 )
 from .query import (
     CURRENT_QUERY_VERSION,
-    parse_job_group_jobs_query_v2,
     parse_job_group_jobs_query_v1,
+    parse_job_group_jobs_query_v2,
     parse_list_batches_query_v1,
     parse_list_batches_query_v2,
     parse_list_job_groups_query_v1,
