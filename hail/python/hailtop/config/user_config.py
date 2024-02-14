21ae0f6755a2400370b57cc8693fdc6080e59f78
Daniel Goldstein <danielgold95@gmail.com>
2024-02-14 10:03:55 -0500

    [fs] Drop support for the deprecated hail-az scheme

--- hail/python/hailtop/config/user_config.py
+++ hail/python/hailtop/config/user_config.py
@@ -138,7 +138,7 @@ def get_remote_tmpdir(
             )
         remote_tmpdir = f'gs://{bucket}/batch'
     else:
-        schemes = {'gs', 'hail-az', 'https'}
+        schemes = {'gs', 'https'}
         found_scheme = any(remote_tmpdir.startswith(f'{scheme}://') for scheme in schemes)
         if not found_scheme:
             raise ValueError(
