28070c47c030eb27f390e835a12498054f3d8a23
Chris Vittal <cvittal@broadinstitute.org>
2024-02-15 02:05:58 -0500

    [query] Refactor vds.sample_qc to use independent aggregators
    This will allow expert users to import these functions directly and use
    them in, for example, grouped aggregators.
    
    ```python3
    from hail.vds.sample_qc import vmt_sample_qc, vmt_sample_qc_variant_annotations
    
    vmt = vds.variant_data
    vmt = vmt.annotate_entries(GT=hl.vds.lgt_to_gt(vmt.LGT, vmt.LA))
    ac_and_atype = _vmt_sample_qc_variant_annotations(global_gt=vmt.GT, alleles=vmt.alleles)
    vmt = vmt.annotate_rows(group=compute_variant_group(vmt), **ac_and_atype)
    vmt = vmt.annotate_cols(
        qc=hl.agg.group_by(
            vmt.group,
            vmt_sample_qc(
                global_gt=vmt.GT,
                gq=vmt.GQ,
                ac=vmt.ac,
                atype=vmt.atype,
                dp=vmt.DP
            )
        )
    )
    ```

--- hail/python/hail/vds/__init__.py
+++ hail/python/hail/vds/__init__.py
@@ -4,7 +4,6 @@ from .methods import (
     filter_intervals,
     filter_samples,
     filter_variants,
-    sample_qc,
     split_multi,
     to_dense_mt,
     to_merged_sparse_mt,
@@ -17,6 +16,7 @@ from .methods import (
     truncate_reference_blocks,
     merge_reference_blocks,
 )
+from .sample_qc import sample_qc
 from .variant_dataset import VariantDataset, read_vds, store_ref_block_max_length
 from .combiner import load_combiner, new_combiner
 
