5c3a5e9e2e37430aeb29287d04e6193b1504c409
willtyler <will.tyler11@gmail.com>
2024-02-27 17:48:31 +0000

    Use Blanczos PCA, hwe_normalize for standardization

--- hail/python/hail/methods/relatedness/pc_air.py
+++ hail/python/hail/methods/relatedness/pc_air.py
@@ -10,10 +10,9 @@ from hail import (
     MatrixTable,
     expr_numeric,
     matrix_table_source,
-    SetExpression,
-    expr_any,
 )
-from hail.linalg import BlockMatrix, _eigh
+from hail.linalg import BlockMatrix
+from hail.methods.pca import hwe_normalize
 from hail.typecheck import typecheck
 
 
@@ -137,70 +136,6 @@ def _partition_samples(
         samples.at[selected_sample, 'eta'] = 0
 
 
-@typecheck(genotypes=expr_call, unrelated=expr_any)
-def _standardize(genotypes: CallExpression, unrelated: Union[set, SetExpression]):
-    """
-    Standardize the genotypes.
-
-    If the number of alternate alleles at locus :math:`s` for sample :math:`i` is :math:`g_{is}`
-    and the unrelated set is :math:`\\mathcal{U}_s`,
-    then the standardized genotype for individual :math:`i` at locus :math:`s` is
-
-    .. math::
-
-       z_{is} = \frac{g_{is} - 2 \\hat{p}^u_s}{\\sqrt{2 \\hat{p}^u_s (1 - \\hat{p}^u_s)}},
-
-    where
-
-    .. math::
-
-       \\hat{p}^u_s = \frac{1}{2 |\\mathcal{U}_s|} \\sum_{i \\in \\mathcal{U}_s} g_{is},
-
-    Notes
-    -----
-    This method gives missing genotypes a standardized genotype of 0.
-
-    The standardized genotypes have a variant-wise mean of 0 and standard deviation of 1.
-
-    Parameters
-    ----------
-    genotypes : :class:`.CallExpression`
-        A call expression representing the genotype calls.
-    unrelated : :class:`.SetExpression`
-        The keys of the samples in the unrelated set.
-    """
-    matrix_table = matrix_table_source('_standardize/genotypes', genotypes)
-
-    # Calculate allele frequency estimates
-    matrix_table = matrix_table.select_entries(alt_allele_count=genotypes.n_alt_alleles())
-    matrix_table = matrix_table.annotate_rows(
-        alt_allele_sum=hl.agg.sum(
-            hl.if_else(unrelated.contains(matrix_table.col_key), matrix_table.alt_allele_count, 0)
-        )
-    )
-    matrix_table = matrix_table.annotate_rows(
-        sample_count=hl.agg.count_where(
-            unrelated.contains(matrix_table.col_key) & hl.is_defined(matrix_table.alt_allele_count)
-        )
-    )
-    matrix_table = matrix_table.annotate_rows(
-        alt_allele_frequency_estimate=1 / 2 * matrix_table.alt_allele_sum / matrix_table.sample_count
-    )
-
-    # Calculate the standardized genotypes
-    matrix_table = matrix_table.annotate_entries(
-        standardized_genotype=hl.if_else(
-            hl.is_defined(matrix_table.alt_allele_count),
-            (matrix_table.alt_allele_count - 2 * matrix_table.alt_allele_frequency_estimate)
-            / hl.sqrt(
-                2 * matrix_table.alt_allele_frequency_estimate * (1 - matrix_table.alt_allele_frequency_estimate)
-            ),
-            0,
-        )
-    )
-    return matrix_table.standardized_genotype
-
-
 @typecheck(genotypes=expr_call, relatedness_threshold=expr_numeric, divergence_threshold=expr_numeric)
 def pc_air(
     genotypes: CallExpression,
@@ -235,39 +170,21 @@ def pc_air(
 
     Returns
     -------
-    :class:`.BlockMatrix`
-        A block matrix with the principal components as the columns.
+    eigenvalues : :obj:`list` of :obj:`float`
+        The eigenvalues of the principal components.
+    scores : :class:`.BlockMatrix`
+        The principal component scores.
+    loadings : :class:`.Table`
+        The principal component loadings.
     """
-    unrelated, related = _partition_samples(genotypes, relatedness_threshold, divergence_threshold)
-    standardized_genotypes = _standardize(genotypes, unrelated)
-    matrix_table = matrix_table_source('pc_air/standardized_genotypes', standardized_genotypes)
-    field_name = matrix_table._fields_inverse[standardized_genotypes]
-    unrelated_genotypes = matrix_table.filter_cols(unrelated.contains(matrix_table.col_key))[field_name]
-    related_genotypes = matrix_table.filter_cols(related.contains(matrix_table.col_key))[field_name]
-
-    # https://en.m.wikipedia.org/wiki/Principal_component_analysis#Singular_value_decomposition
-    # https://en.m.wikipedia.org/wiki/Singular_value_decomposition#Relation_to_eigenvalue_decomposition
-
-    # The variable names below correspond to the notation in the PC-AiR paper
-    # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4836868/#S2title
-
-    snp_count = matrix_table.count_rows()
-    zu = BlockMatrix.from_entry_expr(unrelated_genotypes).T
-    phi = zu @ zu.T / snp_count
-
-    # Here it is necessary to localize because Hail does not offer a method to compute the full
-    # singular value decomposition (SVD) of a BlockMatrix.
-    # (Hail's BlockMatrix.svd() method computes the reduced SVD, not the full SVD.)
-    # TODO: Avoid localizing
-    phi = phi.to_numpy() if isinstance(phi, BlockMatrix) else phi
-    l, v = _eigh(phi)
-    w = zu.T @ v
-
-    zr = BlockMatrix.from_entry_expr(related_genotypes).T
-    q = zr @ w @ np.linalg.inv(np.diag(l)) / snp_count
-    q = q.to_numpy() if isinstance(q, BlockMatrix) else q
-
-    # Stack v on top of q
-    big_gamma = np.row_stack((v, q))
-
-    return BlockMatrix.from_numpy(big_gamma)
+    partition_table = _partition_samples(genotypes, relatedness_threshold, divergence_threshold)
+    unrelated_table = partition_table.filter(partition_table.is_in_unrelated)
+    matrix_table = matrix_table_source('pc_air/genotypes', genotypes)
+    field_name = matrix_table._fields_inverse[genotypes]
+    unrelated_genotypes = matrix_table.semi_join_cols(unrelated_table)[field_name]
+    eigenvalues, _, loadings = hl._hwe_normalized_blanczos(unrelated_genotypes, compute_loadings=True)
+    _loadings = np.array(loadings.loadings.collect())
+    standardized_genotypes = hwe_normalize(genotypes)
+    standardized_genotypes = BlockMatrix.from_entry_expr(standardized_genotypes).T
+    scores = standardized_genotypes @ _loadings
+    return eigenvalues, scores, loadings
--- hail/python/test/hail/methods/relatedness/test_pc_air.py
+++ hail/python/test/hail/methods/relatedness/test_pc_air.py
@@ -1,12 +1,7 @@
-import math
 import unittest
 
-import numpy as np
-
 import hail as hl
-from hail import Struct
-from hail.methods.relatedness.pc_air import _partition_samples, _standardize, pc_air
-
+from hail.methods.relatedness.pc_air import _partition_samples
 from test.hail.helpers import resource
 
 
@@ -50,28 +45,10 @@ class Tests(unittest.TestCase):
         self.assertEqual(expected_related, related)
         self.assertEqual(expected_unrelated, unrelated)
 
-    def test_standardize(self):
-        calls = [
-            hl.call(0, 0),
-            hl.call(0, 1),
-            hl.missing(hl.tcall),
-            hl.call(1, 1),
-        ]
-        # Create a matrix table with calls
-        matrix_table = hl.utils.range_matrix_table(1, 1)
-        matrix_table = matrix_table.annotate_cols(genotypes=calls)
-        # Explode the calls
-        matrix_table = matrix_table.explode_cols(matrix_table.genotypes)
-
-        unrelated = {Struct(col_idx=0)}
-        standardized_genotypes = _standardize(matrix_table.genotypes, unrelated)
-        self.assertTrue(np.allclose([-math.sqrt(2), 0, 0, math.sqrt(2)], standardized_genotypes.collect()))
-
     def test_pc_air(self):
         plink_path = resource('fastlmmTest')
         mt = hl.import_plink(
             bed=f'{plink_path}.bed', bim=f'{plink_path}.bim', fam=f'{plink_path}.fam', reference_genome=None
         )
-        result = pc_air(mt.GT, relatedness_threshold=0.05).to_numpy()
-        expected_result = np.load(resource('pc_air.npy'))
-        self.assertTrue(np.allclose(result, expected_result))
+        # TODO
+        raise NotImplementedError
