2863e0eb192c8b845e022f6ed87907eb6f67093a
Daniel Goldstein <danielgold95@gmail.com>
2024-03-04 13:36:05 -0500

    [infra] Update Azure provider 3.93.0 (#14385)
    The major version upgrade of the azure provider changed some defaults,
    so in order to keep our infrastructure the same I explicitly set these
    changed fields back to their previous v2.99.0 values. The
    `min_tls_version` now defaults to 1.2, which *should* be fine as I
    presume all our services are using TLS 1.3, but we should independently
    verify that before making that change. I will make an issue accordingly.

--- infra/azure/modules/batch/main.tf
+++ infra/azure/modules/batch/main.tf
@@ -72,6 +72,9 @@ resource "azurerm_storage_account" "batch" {
   location                 = var.resource_group.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  min_tls_version          = "TLS1_0"
+
+  allow_nested_items_to_be_public   = false
 
   blob_properties {
     last_access_time_enabled = true
@@ -96,6 +99,9 @@ resource "azurerm_storage_account" "test" {
   location                 = var.resource_group.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  min_tls_version          = "TLS1_0"
+
+  allow_nested_items_to_be_public   = false
 
   blob_properties {
     last_access_time_enabled = true
