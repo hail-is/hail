1e149d9f30997564ec0a920dc7c5adb9d1252b4f
Iris Rademacher <irademac@broadinstitute.org>
2024-03-11 14:05:23 -0400

    review

--- hail/python/hailtop/aiogoogle/__init__.py
+++ hail/python/hailtop/aiogoogle/__init__.py
@@ -1,5 +1,43 @@
 import warnings
 
+from ..aiocloud.aiogoogle import (
+    GCSRequesterPaysConfiguration,
+    GoogleApplicationDefaultCredentials,
+    GoogleBigQueryClient,
+    GoogleBillingClient,
+    GoogleComputeClient,
+    GoogleContainerClient,
+    GoogleCredentials,
+    GoogleIAmClient,
+    GoogleInstanceMetadataCredentials,
+    GoogleLoggingClient,
+    GoogleMetadataServerClient,
+    GoogleServiceAccountCredentials,
+    GoogleStorageAsyncFS,
+    GoogleStorageAsyncFSFactory,
+    GoogleStorageClient,
+    get_gcs_requester_pays_configuration,
+)
+
 warnings.warn(
     "importing hailtop.aiogoogle is deprecated, please use hailtop.aiocloud.aiogoogle", DeprecationWarning, stacklevel=2
 )
+
+__all__ = [
+    'GCSRequesterPaysConfiguration',
+    'GoogleCredentials',
+    'GoogleApplicationDefaultCredentials',
+    'GoogleServiceAccountCredentials',
+    'GoogleInstanceMetadataCredentials',
+    'GoogleBigQueryClient',
+    'GoogleBillingClient',
+    'GoogleContainerClient',
+    'GoogleComputeClient',
+    'GoogleIAmClient',
+    'GoogleLoggingClient',
+    'GoogleMetadataServerClient',
+    'GoogleStorageClient',
+    'GoogleStorageAsyncFS',
+    'GoogleStorageAsyncFSFactory',
+    'get_gcs_requester_pays_configuration',
+]
