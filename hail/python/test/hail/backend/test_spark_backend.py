c7ef0cb61cb10c74eb79cde854fb353e6b4d803e
Edmund Higham <edhigham@gmail.com>
2024-04-04 18:54:15 -0400

    [query] Copy spark driver log to remote tmpdir on error Adds `copy_spark_log_on_error` init configuration option. When true, driver logs are copied to the remote tmpdir if an error occurs. This is useful in support cases where users cannot copy logs off the dataproc server themselves as it has already shut down.

---
+++ hail/python/test/hail/backend/test_spark_backend.py
@@ -0,0 +1,33 @@
+import os
+from test.hail.helpers import skip_unless_spark_backend
+
+import pytest
+
+import hail as hl
+
+
+def fatal(typ: hl.HailType, msg: str="") -> hl.Expression:
+    return hl.construct_expr(hl.ir.Die(hl.to_expr(msg, hl.tstr)._ir, typ), typ)
+
+
+@skip_unless_spark_backend()
+@pytest.mark.parametrize('copy', [True, False])
+def test_copy_spark_log(copy):
+    hl.stop()
+    hl.init(copy_spark_log_on_error=copy)
+
+    expr = fatal(hl.tint32)
+    with pytest.raises(Exception):
+        hl.eval(expr)
+
+    from hail.utils.java import Env
+
+    hc = Env.hc()
+    _, filename = os.path.split(hc._log)
+    log = os.path.join(hc._tmpdir, filename)
+
+    assert (
+        Env.fs().exists(log)
+        if copy
+        else not Env.fs().exists(log)
+    )
