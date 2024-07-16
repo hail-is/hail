bc665db6993bb46d76e90e1a4ef7a15f661fa22d
Daniel Goldstein <danielgold95@gmail.com>
2024-07-16 11:24:09 -0400

    [gateway] Fix top level domain for website (#14617)
    #14609 broke the routing for https://hail.is. The `domains` variable
    here indicates to `gateway` which domains in incoming requests should be
    routed to the given service. Since #14609 changed the `service`
    parameter from `str` to `Service`, it silently broke this branch.
    
    This wasn't covered by the envoy config generation tests because we
    didn't have a `www` service in the test configuration. I've added it so
    that this branch is covered.
    
    Fixes #14616

--- ci/test/test_envoy.py
+++ ci/test/test_envoy.py
@@ -6,6 +6,9 @@ import yaml
 
 from ci.envoy import Service, create_cds_response, create_rds_response
 
+PROD_SERVICES = [Service('foo'), Service('www')]
+TEST_SERVICES = {'test': [Service('bar', 100)]}
+
 
 @pytest.mark.parametrize(
     'proxy, expected_config',
@@ -17,7 +20,7 @@ from ci.envoy import Service, create_cds_response, create_rds_response
 def test_gateway_cds_generation(proxy, expected_config):
     with open(Path(os.path.dirname(__file__)) / 'envoy' / expected_config, encoding='utf-8') as f:
         expected_cluster_config = yaml.safe_load(f.read())
-    actual_cluster_config = create_cds_response([Service('foo')], {'test': [Service('bar', 100)]}, proxy)
+    actual_cluster_config = create_cds_response(PROD_SERVICES, TEST_SERVICES, proxy)
     assert expected_cluster_config == actual_cluster_config
 
 
@@ -31,7 +34,5 @@ def test_gateway_cds_generation(proxy, expected_config):
 def test_gateway_rds_generation(proxy, expected_config):
     with open(Path(os.path.dirname(__file__)) / 'envoy' / expected_config, encoding='utf-8') as f:
         expected_routes_config = yaml.safe_load(f.read())
-    actual_routes_config = create_rds_response(
-        [Service('foo')], {'test': [Service('bar', 100)]}, proxy, domain='hail.is'
-    )
+    actual_routes_config = create_rds_response(PROD_SERVICES, TEST_SERVICES, proxy, domain='hail.is')
     assert expected_routes_config == actual_routes_config
