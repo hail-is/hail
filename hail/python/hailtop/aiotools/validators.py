ab730f122ca4d54867b0be2431b301a477607284
iris <84595986+iris-garden@users.noreply.github.com>
2024-07-29 12:25:41 -0400

    [fs] allows reading from public access buckets (#14292)
    Closes #14291.

--- hail/python/hailtop/aiotools/validators.py
+++ hail/python/hailtop/aiotools/validators.py
@@ -8,8 +8,8 @@ from hailtop.aiotools.router_fs import RouterAsyncFS
 
 async def validate_file(uri: str, router_async_fs: RouterAsyncFS, *, validate_scheme: Optional[bool] = False) -> None:
     """
-    Validates a URI's scheme if a file scheme cache was provided, and its cloud location's default storage policy if
-    the URI points to a cloud with an ``AsyncFS`` implementation that supports checking that policy.
+    Validates a URI's scheme if the ``validate_scheme`` flag was provided, and its cloud location's default storage
+    policy if the URI points to a cloud with an ``AsyncFS`` implementation that supports checking that policy.
 
     Raises
     ------
@@ -25,20 +25,18 @@ async def validate_file(uri: str, router_async_fs: RouterAsyncFS, *, validate_sc
             )
     fs = await router_async_fs._get_fs(uri)
     if isinstance(fs, GoogleStorageAsyncFS):
-        location = fs.storage_location(uri)
-        if location not in fs.allowed_storage_locations:
-            if not await fs.is_hot_storage(location):
-                raise ValueError(
-                    dedent(f"""\
-                        GCS Bucket '{location}' is configured to use cold storage by default. Accessing the blob
-                        '{uri}' would incur egress charges. Either
+        if not await fs.is_hot_storage(uri):
+            location = fs.storage_location(uri)
+            raise ValueError(
+                dedent(f"""\
+                    GCS Bucket '{location}' is configured to use cold storage by default. Accessing the blob
+                    '{uri}' would incur egress charges. Either
 
-                        * avoid the increased cost by changing the default storage policy for the bucket
-                          (https://cloud.google.com/storage/docs/changing-default-storage-class) and the individual
-                          blobs in it (https://cloud.google.com/storage/docs/changing-storage-classes) to 'Standard', or
+                    * avoid the increased cost by changing the default storage policy for the bucket
+                      (https://cloud.google.com/storage/docs/changing-default-storage-class) and the individual
+                      blobs in it (https://cloud.google.com/storage/docs/changing-storage-classes) to 'Standard', or
 
-                        * accept the increased cost by adding '{location}' to the 'gcs_bucket_allow_list' configuration
-                          variable (https://hail.is/docs/0.2/configuration_reference.html).
-                        """)
-                )
-            fs.allowed_storage_locations.append(location)
+                    * accept the increased cost by adding '{location}' to the 'gcs_bucket_allow_list' configuration
+                      variable (https://hail.is/docs/0.2/configuration_reference.html).
+                    """)
+            )
