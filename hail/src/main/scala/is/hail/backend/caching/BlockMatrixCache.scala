a7f6c50fc000177c7bd500784f31ff870879fbc7
Edmund Higham <edhigham@gmail.com>
2024-09-17 17:45:02 -0400

    [query] Remove BlockMatrix persist from Backend interface

---
+++ hail/src/main/scala/is/hail/backend/caching/BlockMatrixCache.scala
@@ -0,0 +1,33 @@
+package is.hail.backend.caching
+
+import is.hail.linalg.BlockMatrix
+
+import scala.collection.mutable
+
+class BlockMatrixCache extends mutable.AbstractMap[String, BlockMatrix] with AutoCloseable {
+
+  private[this] val blockmatrices: mutable.Map[String, BlockMatrix] =
+    mutable.LinkedHashMap.empty
+
+  override def +=(kv: (String, BlockMatrix)): BlockMatrixCache.this.type = {
+    blockmatrices += kv; this
+  }
+
+  override def -=(key: String): BlockMatrixCache.this.type = {
+    get(key).foreach { bm => bm.unpersist(); blockmatrices -= key }; this
+  }
+
+  override def get(key: String): Option[BlockMatrix] =
+    blockmatrices.get(key)
+
+  override def iterator: Iterator[(String, BlockMatrix)] =
+    blockmatrices.iterator
+
+  override def clear(): Unit = {
+    blockmatrices.values.foreach(_.unpersist())
+    blockmatrices.clear()
+  }
+
+  override def close(): Unit =
+    clear()
+}
