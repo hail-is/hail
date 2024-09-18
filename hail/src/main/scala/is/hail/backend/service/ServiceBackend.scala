bfa84f0d7a3e5c4e4c7e766c62e15d53a25ddf83
Edmund Higham <edhigham@gmail.com>
2024-09-18 12:45:29 -0400

    [query] Remove `persistedIr` from `Backend` interface

--- hail/src/main/scala/is/hail/backend/service/ServiceBackend.scala
+++ hail/src/main/scala/is/hail/backend/service/ServiceBackend.scala
@@ -392,6 +392,7 @@ class ServiceBackend(
         new IrMetadata(),
         ImmutableMap.empty,
         mutable.Map.empty,
+        ImmutableMap.empty,
       )(f)
     }
 
--- hail/src/main/scala/is/hail/backend/spark/SparkBackend.scala
+++ hail/src/main/scala/is/hail/backend/spark/SparkBackend.scala
@@ -354,6 +354,7 @@ class SparkBackend(
 
   private[this] val bmCache = new BlockMatrixCache()
   private[this] val codeCache = new Cache[CodeCacheKey, CompiledFunction[_]](50)
+  private[this] val persistedIr = mutable.Map.empty[Int, BaseIR]
 
   def createExecuteContextForTests(
     timer: ExecutionTimer,
@@ -378,6 +379,7 @@ class SparkBackend(
       new IrMetadata(),
       ImmutableMap.empty,
       mutable.Map.empty,
+      ImmutableMap.empty,
     )
 
   override def withExecuteContext[T](f: ExecuteContext => T)(implicit E: Enclosing): T =
@@ -399,6 +401,7 @@ class SparkBackend(
         new IrMetadata(),
         bmCache,
         codeCache,
+        persistedIr,
       )(f)
     }
 
