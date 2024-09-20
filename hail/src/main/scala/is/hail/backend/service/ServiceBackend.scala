c008427b0da43cdc5d478d1e7e0741e3be93c11d
Edmund Higham <edhigham@gmail.com>
2024-09-20 18:06:07 -0400

    [query] Lift backend state into `{Service|Py4J}BackendApi`

--- hail/src/main/scala/is/hail/backend/service/ServiceBackend.scala
+++ hail/src/main/scala/is/hail/backend/service/ServiceBackend.scala
@@ -1,10 +1,10 @@
 package is.hail.backend.service
 
-import is.hail.{CancellingExecutorService, HailContext, HailFeatureFlags}
+import is.hail.{CancellingExecutorService, HailFeatureFlags}
 import is.hail.annotations._
 import is.hail.asm4s._
 import is.hail.backend._
-import is.hail.backend.caching.NoCaching
+import is.hail.backend.api.BatchJobConfig
 import is.hail.backend.service.ServiceBackend.MaxAvailableGcsConnections
 import is.hail.expr.Validate
 import is.hail.expr.ir.{
@@ -14,36 +14,19 @@ import is.hail.expr.ir.analyses.SemanticHash
 import is.hail.expr.ir.compile.Compile
 import is.hail.expr.ir.lowering._
 import is.hail.io.fs._
-import is.hail.io.reference.{IndexedFastaSequenceFile, LiftOver}
 import is.hail.services._
 import is.hail.services.JobGroupStates.Failure
 import is.hail.types._
 import is.hail.types.physical._
 import is.hail.types.physical.stypes.PTypeReferenceSingleCodeType
-import is.hail.types.virtual.{Kinds, TVoid}
+import is.hail.types.virtual.TVoid
 import is.hail.utils._
-import is.hail.utils.ExecutionTimer.Timings
-import is.hail.variant.ReferenceGenome
 
-import scala.annotation.switch
-import scala.collection.mutable
 import scala.reflect.ClassTag
 
 import java.io._
 import java.nio.charset.StandardCharsets
-import java.nio.file.Path
 import java.util.concurrent._
-
-import org.json4s.{DefaultFormats, Formats}
-import org.json4s.JsonAST._
-import org.json4s.jackson.JsonMethods
-import sourcecode.Enclosing
-
-case class ServiceBackendContext(
-  remoteTmpDir: String,
-  jobConfig: BatchJobConfig,
-  override val executionCache: ExecutionCache,
-) extends BackendContext with Serializable
 
 object ServiceBackend {
   val MaxAvailableGcsConnections = 1000
@@ -53,17 +36,23 @@ class ServiceBackend(
   val name: String,
   batchClient: BatchClient,
   jarSpec: JarSpec,
-  theHailClassLoader: HailClassLoader,
   val batchConfig: BatchConfig,
-  rpcConfig: ServiceBackendRPCPayload,
   jobConfig: BatchJobConfig,
-  flags: HailFeatureFlags,
-  val fs: FS,
-  references: mutable.Map[String, ReferenceGenome],
 ) extends Backend with Logging {
 
+  case class Context(
+    remoteTmpDir: String,
+    batchConfig: BatchConfig,
+    jobConfig: BatchJobConfig,
+    flags: HailFeatureFlags,
+    override val executionCache: ExecutionCache,
+  ) extends BackendContext
+
   private[this] var stageCount = 0
-  private[this] val executor = Executors.newFixedThreadPool(MaxAvailableGcsConnections)
+
+  private[this] val executor = lazily {
+    Executors.newFixedThreadPool(MaxAvailableGcsConnections)
+  }
 
   def defaultParallelism: Int = 4
 
@@ -81,6 +70,15 @@ class ServiceBackend(
     }
   }
 
+  override def backendContext(ctx: ExecuteContext): BackendContext =
+    Context(
+      remoteTmpDir = ctx.tmpdir,
+      batchConfig = batchConfig,
+      jobConfig = jobConfig,
+      flags = ctx.flags,
+      executionCache = ExecutionCache.fromFlags(ctx.flags, ctx.fs, ctx.tmpdir),
+    )
+
   private[this] def readString(in: DataInputStream): String = {
     val n = in.readInt()
     val bytes = new Array[Byte](n)
@@ -89,6 +87,7 @@ class ServiceBackend(
   }
 
   private[this] def submitJobGroupAndWait(
+    ctx: Context,
     collection: IndexedSeq[Array[Byte]],
     token: String,
     root: String,
@@ -98,7 +97,7 @@ class ServiceBackend(
       JvmJob(
         command = null,
         spec = jarSpec,
-        profile = flags.get("profile") != null,
+        profile = ctx.flags.get("profile") != null,
       )
 
     val defaultJob =
@@ -144,7 +143,7 @@ class ServiceBackend(
     batchClient.waitForJobGroup(batchConfig.batchId, jobGroupId)
   }
 
-  private[this] def readResult(root: String, i: Int): Array[Byte] = {
+  private[this] def readResult(fs: FS, root: String, i: Int): Array[Byte] = {
     val bytes = fs.readNoCompression(s"$root/result.$i")
     if (bytes(0) != 0) {
       bytes.slice(1, bytes.length)
@@ -169,7 +168,7 @@ class ServiceBackend(
     f: (Array[Byte], HailTaskContext, HailClassLoader, FS) => Array[Byte]
   ): (Option[Throwable], IndexedSeq[(Array[Byte], Int)]) = {
 
-    val backendContext = _backendContext.asInstanceOf[ServiceBackendContext]
+    val backendContext = _backendContext.asInstanceOf[Context]
 
     val token = tokenUrlSafe
     val root = s"${backendContext.remoteTmpDir}/parallelizeAndComputeWithIndex/$token"
@@ -207,13 +206,13 @@ class ServiceBackend(
     uploadFunction.get()
     uploadContexts.get()
 
-    val jobGroup = submitJobGroupAndWait(parts, token, root, stageIdentifier)
+    val jobGroup = submitJobGroupAndWait(backendContext, parts, token, root, stageIdentifier)
 
     log.info(s"parallelizeAndComputeWithIndex: $token: reading results")
     val startTime = System.nanoTime()
     val r @ (error, results) = runAllKeepFirstError(new CancellingExecutorService(executor)) {
       (partIdxs, parts.indices).zipped.map { (partIdx, jobIndex) =>
-        (() => readResult(root, jobIndex), partIdx)
+        (() => readResult(fs, root, jobIndex), partIdx)
       }
     }
 
@@ -233,7 +232,7 @@ class ServiceBackend(
   }
 
   override def close(): Unit = {
-    executor.shutdownNow()
+    if (executor.isEvaluated) executor.shutdownNow()
     batchClient.close()
   }
 
@@ -292,253 +291,7 @@ class ServiceBackend(
   def tableToTableStage(ctx: ExecuteContext, inputIR: TableIR, analyses: LoweringAnalyses)
     : TableStage =
     LowerTableIR.applyTable(inputIR, DArrayLowering.All, ctx, analyses)
-
-  override def withExecuteContext[T](f: ExecuteContext => T)(implicit E: Enclosing): (T, Timings) =
-    ExecutionTimer.time { timer =>
-      ExecuteContext.scoped(
-        rpcConfig.tmp_dir,
-        rpcConfig.remote_tmpdir,
-        this,
-        fs,
-        timer,
-        null,
-        theHailClassLoader,
-        flags,
-        ServiceBackendContext(
-          rpcConfig.remote_tmpdir,
-          jobConfig,
-          ExecutionCache.fromFlags(flags, fs, rpcConfig.remote_tmpdir),
-        ),
-        new IrMetadata(),
-        references,
-        NoCaching,
-        NoCaching,
-        NoCaching,
-        NoCaching,
-      )(f)
-    }
 }
 
-class EndOfInputException extends RuntimeException
 class HailBatchFailure(message: String) extends RuntimeException(message)
 
-case class Request(
-  backend: ServiceBackend,
-  fs: FS,
-  outputUrl: String,
-  action: Int,
-  payload: JValue,
-)
-
-object ServiceBackendAPI extends HttpLikeBackendRpc[Request] with Logging {
-
-  def main(argv: Array[String]): Unit = {
-    assert(argv.length == 7, argv.toFastSeq)
-
-    val scratchDir = argv(0)
-    // val logFile = argv(1)
-    val jarLocation = argv(2)
-    val kind = argv(3)
-    assert(kind == Main.DRIVER)
-    val name = argv(4)
-    val inputURL = argv(5)
-    val outputURL = argv(6)
-
-    implicit val fmts: Formats = DefaultFormats
-
-    val deployConfig = DeployConfig.fromConfigFile("/deploy-config/deploy-config.json")
-    DeployConfig.set(deployConfig)
-    sys.env.get("HAIL_SSL_CONFIG_DIR").foreach(tls.setSSLConfigFromDir)
-
-    var fs = RouterFS.buildRoutes(
-      CloudStorageFSConfig.fromFlagsAndEnv(
-        Some(Path.of(scratchDir, "secrets/gsa-key/key.json")),
-        HailFeatureFlags.fromEnv(),
-      )
-    )
-
-    val (rpcConfig, jobConfig, action, payload) =
-      using(fs.openNoCompression(inputURL)) { is =>
-        val input = JsonMethods.parse(is)
-        (
-          (input \ "rpc_config").extract[ServiceBackendRPCPayload],
-          (input \ "job_config").extract[BatchJobConfig],
-          (input \ "action").extract[Int],
-          input \ "payload",
-        )
-      }
-
-    // requester pays config is conveyed in feature flags currently
-    val featureFlags = HailFeatureFlags.fromEnv(rpcConfig.flags)
-    fs = RouterFS.buildRoutes(
-      CloudStorageFSConfig.fromFlagsAndEnv(
-        Some(Path.of(scratchDir, "secrets/gsa-key/key.json")),
-        featureFlags,
-      )
-    )
-
-    val references = mutable.Map[String, ReferenceGenome]()
-    references ++= ReferenceGenome.builtinReferences()
-    rpcConfig.custom_references.toFastSeq.view.map(ReferenceGenome.fromJSON).foreach { rg =>
-      references += rg.name -> rg
-    }
-
-    rpcConfig.liftovers.foreach { case (sourceGenome, liftoversForSource) =>
-      liftoversForSource.foreach { case (destGenome, chainFile) =>
-        references(sourceGenome).addLiftover(references(destGenome), LiftOver(fs, chainFile))
-      }
-    }
-
-    rpcConfig.sequences.foreach { case (rg, seq) =>
-      references(rg).addSequence(IndexedFastaSequenceFile(fs, seq.fasta, seq.index))
-    }
-
-    // FIXME: when can the classloader be shared? (optimizer benefits!)
-    val backend = new ServiceBackend(
-      name,
-      BatchClient(deployConfig, Path.of(scratchDir, "secrets/gsa-key/key.json")),
-      JarUrl(jarLocation),
-      new HailClassLoader(getClass.getClassLoader),
-      BatchConfig.fromConfigFile(Path.of(scratchDir, "batch-config/batch-config.json")),
-      rpcConfig,
-      jobConfig,
-      featureFlags,
-      fs,
-      references,
-    )
-
-    log.info("ServiceBackend allocated.")
-    if (HailContext.isInitialized) {
-      HailContext.get.backend = backend
-      log.info("Default references added to already initialized HailContexet.")
-    } else {
-      HailContext(backend, 50, 3)
-      log.info("HailContexet initialized.")
-    }
-
-    runRpc(Request(backend, fs, outputURL, action, payload))
-  }
-
-  implicit override protected object Ask extends Routing {
-    import Routes._
-
-    override def route(a: Request): Route =
-      (a.action: @switch) match {
-        case 1 => TypeOf(Kinds.Value)
-        case 2 => TypeOf(Kinds.Table)
-        case 3 => TypeOf(Kinds.Matrix)
-        case 4 => TypeOf(Kinds.BlockMatrix)
-        case 5 => Execute
-        case 6 => ParseVcfMetadata
-        case 7 => ImportFam
-        case 8 => LoadReferencesFromDataset
-        case 9 => LoadReferencesFromFASTA
-      }
-
-    override def payload(a: Request): JValue = a.payload
-  }
-
-  implicit override protected object Write extends Write[Request] {
-
-    // service backend doesn't support sending timings back to the python client
-    override def timings(env: Request)(t: Timings): Unit =
-      ()
-
-    override def result(env: Request)(result: Array[Byte]): Unit =
-      retryTransientErrors {
-        using(env.fs.createNoCompression(env.outputUrl)) { outputStream =>
-          val output = new HailSocketAPIOutputStream(outputStream)
-          output.writeBool(true)
-          output.writeBytes(result)
-        }
-      }
-
-    override def error(env: Request)(t: Throwable): Unit =
-      retryTransientErrors {
-        val (shortMessage, expandedMessage, errorId) =
-          t match {
-            case t: HailWorkerException =>
-              log.error(
-                "A worker failed. The exception was written for Python but we will also throw an exception to fail this driver job.",
-                t,
-              )
-              (t.shortMessage, t.expandedMessage, t.errorId)
-            case _ =>
-              log.error(
-                "An exception occurred in the driver. The exception was written for Python but we will re-throw to fail this driver job.",
-                t,
-              )
-              handleForPython(t)
-          }
-
-        using(env.fs.createNoCompression(env.outputUrl)) { outputStream =>
-          val output = new HailSocketAPIOutputStream(outputStream)
-          output.writeBool(false)
-          output.writeString(shortMessage)
-          output.writeString(expandedMessage)
-          output.writeInt(errorId)
-        }
-
-        throw t
-      }
-  }
-
-  implicit override protected object Context extends Context[Request] {
-    override def scoped[A](env: Request)(f: ExecuteContext => A): (A, Timings) =
-      env.backend.withExecuteContext(f)
-  }
-}
-
-private class HailSocketAPIOutputStream(
-  private[this] val out: OutputStream
-) extends AutoCloseable {
-  private[this] var closed: Boolean = false
-  private[this] val dummy = new Array[Byte](8)
-
-  def writeBool(b: Boolean): Unit =
-    out.write(if (b) 1 else 0)
-
-  def writeInt(v: Int): Unit = {
-    Memory.storeInt(dummy, 0, v)
-    out.write(dummy, 0, 4)
-  }
-
-  def writeLong(v: Long): Unit = {
-    Memory.storeLong(dummy, 0, v)
-    out.write(dummy)
-  }
-
-  def writeBytes(bytes: Array[Byte]): Unit = {
-    writeInt(bytes.length)
-    out.write(bytes)
-  }
-
-  def writeString(s: String): Unit = writeBytes(s.getBytes(StandardCharsets.UTF_8))
-
-  def close(): Unit =
-    if (!closed) {
-      out.close()
-      closed = true
-    }
-}
-
-case class SequenceConfig(fasta: String, index: String)
-
-case class ServiceBackendRPCPayload(
-  tmp_dir: String,
-  remote_tmpdir: String,
-  flags: Map[String, String],
-  custom_references: Array[String],
-  liftovers: Map[String, Map[String, String]],
-  sequences: Map[String, SequenceConfig],
-)
-
-case class BatchJobConfig(
-  token: String,
-  billing_project: String,
-  worker_cores: String,
-  worker_memory: String,
-  storage: String,
-  cloudfuse_configs: Array[CloudfuseConfig],
-  regions: Array[String],
-)
--- hail/src/main/scala/is/hail/backend/spark/SparkBackend.scala
+++ hail/src/main/scala/is/hail/backend/spark/SparkBackend.scala
@@ -1,14 +1,11 @@
 package is.hail.backend.spark
 
-import is.hail.{HailContext, HailFeatureFlags}
+import is.hail.HailContext
 import is.hail.annotations._
 import is.hail.asm4s._
 import is.hail.backend._
-import is.hail.backend.caching.{BlockMatrixCache, NoCaching}
-import is.hail.backend.py4j.Py4JBackendExtensions
 import is.hail.expr.Validate
 import is.hail.expr.ir._
-import is.hail.expr.ir.LoweredTableReader.LoweredTableReaderCoercer
 import is.hail.expr.ir.analyses.SemanticHash
 import is.hail.expr.ir.compile.Compile
 import is.hail.expr.ir.lowering._
@@ -20,8 +17,6 @@ import is.hail.types.physical.{PStruct, PTuple}
 import is.hail.types.physical.stypes.PTypeReferenceSingleCodeType
 import is.hail.types.virtual._
 import is.hail.utils._
-import is.hail.utils.ExecutionTimer.Timings
-import is.hail.variant.ReferenceGenome
 
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
@@ -31,8 +26,6 @@ import scala.util.control.NonFatal
 
 import java.io.PrintWriter
 
-import org.apache.hadoop
-import org.apache.hadoop.conf.Configuration
 import org.apache.spark._
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
@@ -205,15 +198,11 @@ object SparkBackend {
     append: Boolean = false,
     skipLoggingConfiguration: Boolean = false,
     minBlockSize: Long = 1L,
-    tmpdir: String = "/tmp",
-    localTmpdir: String = "file:///tmp",
-    gcsRequesterPaysProject: String = null,
-    gcsRequesterPaysBuckets: String = null,
   ): SparkBackend = synchronized {
     if (theSparkBackend == null)
       return SparkBackend(sc, appName, master, local, logFile, quiet, append,
         skipLoggingConfiguration,
-        minBlockSize, tmpdir, localTmpdir, gcsRequesterPaysProject, gcsRequesterPaysBuckets)
+        minBlockSize)
 
     // there should be only one SparkContext
     assert(sc == null || (sc eq theSparkBackend.sc))
@@ -249,10 +238,6 @@ object SparkBackend {
     append: Boolean = false,
     skipLoggingConfiguration: Boolean = false,
     minBlockSize: Long = 1L,
-    tmpdir: String,
-    localTmpdir: String,
-    gcsRequesterPaysProject: String = null,
-    gcsRequesterPaysBuckets: String = null,
   ): SparkBackend = synchronized {
     require(theSparkBackend == null)
 
@@ -267,32 +252,19 @@ object SparkBackend {
 
     checkSparkConfiguration(sc1)
 
-    if (!quiet)
-      ProgressBarBuilder.build(sc1)
+    if (!quiet) ProgressBarBuilder.build(sc1)
 
     sc1.uiWebUrl.foreach(ui => info(s"SparkUI: $ui"))
 
-    theSparkBackend =
-      new SparkBackend(
-        tmpdir,
-        localTmpdir,
-        sc1,
-        mutable.Map(ReferenceGenome.builtinReferences().toSeq: _*),
-        gcsRequesterPaysProject,
-        gcsRequesterPaysBuckets,
-      )
+    theSparkBackend = new SparkBackend(sc1)
     theSparkBackend
   }
 
   def stop(): Unit = synchronized {
     if (theSparkBackend != null) {
+      if (theSparkBackend.sparkSession.isEvaluated) theSparkBackend.sparkSession.close()
       theSparkBackend.sc.stop()
       theSparkBackend = null
-      // Hadoop does not honor the hadoop configuration as a component of the cache key for file
-      // systems, so we blow away the cache so that a new configuration can successfully take
-      // effect.
-      // https://github.com/hail-is/hail/pull/12133#issuecomment-1241322443
-      hadoop.fs.FileSystem.closeAll()
     }
   }
 }
@@ -303,104 +275,31 @@ class AnonymousDependency[T](val _rdd: RDD[T]) extends NarrowDependency[T](_rdd)
   override def getParents(partitionId: Int): Seq[Int] = Seq.empty
 }
 
-class SparkBackend(
-  val tmpdir: String,
-  val localTmpdir: String,
-  val sc: SparkContext,
-  override val references: mutable.Map[String, ReferenceGenome],
-  gcsRequesterPaysProject: String,
-  gcsRequesterPaysBuckets: String,
-) extends Backend with Py4JBackendExtensions {
+class SparkBackend(val sc: SparkContext) extends Backend {
 
-  assert(gcsRequesterPaysProject != null || gcsRequesterPaysBuckets == null)
-  lazy val sparkSession: SparkSession = SparkSession.builder().config(sc.getConf).getOrCreate()
+  private case class Context(
+    maxStageParallelism: Int,
+    override val executionCache: ExecutionCache,
+  ) extends BackendContext
 
-  private[this] val theHailClassLoader: HailClassLoader =
-    new HailClassLoader(getClass().getClassLoader())
+  val sparkSession: Lazy[SparkSession] =
+    lazily {
+      SparkSession.builder().config(sc.getConf).getOrCreate()
+    }
 
   override def canExecuteParallelTasksOnDriver: Boolean = false
-
-  val fs: HadoopFS = {
-    val conf = new Configuration(sc.hadoopConfiguration)
-    if (gcsRequesterPaysProject != null) {
-      if (gcsRequesterPaysBuckets == null) {
-        conf.set("fs.gs.requester.pays.mode", "AUTO")
-        conf.set("fs.gs.requester.pays.project.id", gcsRequesterPaysProject)
-      } else {
-        conf.set("fs.gs.requester.pays.mode", "CUSTOM")
-        conf.set("fs.gs.requester.pays.project.id", gcsRequesterPaysProject)
-        conf.set("fs.gs.requester.pays.buckets", gcsRequesterPaysBuckets)
-      }
-    }
-    new HadoopFS(new SerializableHadoopConfiguration(conf))
-  }
-
-  override def backend: Backend = this
-  override val flags: HailFeatureFlags = HailFeatureFlags.fromEnv()
-
-  override val longLifeTempFileManager: TempFileManager =
-    new OwningTempFileManager(fs)
-
-  private[this] val bmCache = new BlockMatrixCache()
-  private[this] val codeCache = new Cache[CodeCacheKey, CompiledFunction[_]](50)
-  private[this] val persistedIr = mutable.Map.empty[Int, BaseIR]
-  private[this] val coercerCache = new Cache[Any, LoweredTableReaderCoercer](32)
-
-  def createExecuteContextForTests(
-    timer: ExecutionTimer,
-    region: Region,
-  ): ExecuteContext =
-    new ExecuteContext(
-      tmpdir,
-      localTmpdir,
-      this,
-      fs,
-      region,
-      timer,
-      null,
-      theHailClassLoader,
-      flags,
-      new BackendContext {
-        override val executionCache: ExecutionCache =
-          ExecutionCache.forTesting
-      },
-      new IrMetadata(),
-      references,
-      ImmutableMap.empty,
-      NoCaching,
-      ImmutableMap.empty,
-      NoCaching,
-    )
-
-  override def withExecuteContext[T](f: ExecuteContext => T)(implicit E: Enclosing): (T, Timings) =
-    ExecutionTimer.time { timer =>
-      ExecuteContext.scoped(
-        tmpdir,
-        localTmpdir,
-        this,
-        fs,
-        timer,
-        null,
-        theHailClassLoader,
-        flags,
-        new BackendContext {
-          override val executionCache: ExecutionCache =
-            ExecutionCache.fromFlags(flags, fs, tmpdir)
-        },
-        new IrMetadata(),
-        references,
-        bmCache,
-        codeCache,
-        persistedIr,
-        coercerCache,
-      )(f)
-    }
 
   def broadcast[T: ClassTag](value: T): BroadcastValue[T] =
     new SparkBroadcastValue[T](sc.broadcast(value))
 
+  override def backendContext(ctx: ExecuteContext): BackendContext =
+    Context(
+      ctx.flags.get(SparkBackend.Flags.MaxStageParallelism).toInt,
+      ExecutionCache.fromFlags(ctx.flags, ctx.fs, ctx.tmpdir),
+    )
+
   override def parallelizeAndComputeWithIndex(
-    backendContext: BackendContext,
+    ctx: BackendContext,
     fs: FS,
     contexts: IndexedSeq[Array[Byte]],
     stageIdentifier: String,
@@ -430,13 +329,12 @@ class SparkBackend(
         }
       }
 
-    val chunkSize = flags.get(SparkBackend.Flags.MaxStageParallelism).toInt
     val partsToRun = partitions.getOrElse(contexts.indices)
     val buffer = new ArrayBuffer[(Array[Byte], Int)](partsToRun.length)
     var failure: Option[Throwable] = None
 
     try {
-      for (subparts <- partsToRun.grouped(chunkSize)) {
+      for (subparts <- partsToRun.grouped(ctx.asInstanceOf[Context].maxStageParallelism)) {
         sc.runJob(
           rdd,
           (_: TaskContext, it: Iterator[Array[Byte]]) => it.next(),
@@ -456,11 +354,8 @@ class SparkBackend(
 
   override def asSpark(implicit E: Enclosing): SparkBackend = this
 
-  def close(): Unit = {
-    bmCache.close()
+  def close(): Unit =
     SparkBackend.stop()
-    longLifeTempFileManager.close()
-  }
 
   def startProgressBar(): Unit =
     ProgressBarBuilder.build(sc)
@@ -493,8 +388,7 @@ class SparkBackend(
         case (false, true) => DArrayLowering.BMOnly
         case (false, false) => throw new LowererUnsupportedOperation("no lowering enabled")
       }
-      val ir =
-        LoweringPipeline.darrayLowerer(optimize)(typesToLower).apply(ctx, ir0).asInstanceOf[IR]
+      val ir = LoweringPipeline.darrayLowerer(optimize)(typesToLower)(ctx, ir0).asInstanceOf[IR]
 
       if (!Compilable(ir))
         throw new LowererUnsupportedOperation(s"lowered to uncompilable IR: ${Pretty(ctx, ir)}")
@@ -532,11 +426,11 @@ class SparkBackend(
       Validate(ir)
       ctx.irMetadata.semhash = SemanticHash(ctx)(ir)
       try {
-        val lowerTable = flags.get("lower") != null
-        val lowerBM = flags.get("lower_bm") != null
+        val lowerTable = ctx.flags.get("lower") != null
+        val lowerBM = ctx.flags.get("lower_bm") != null
         _jvmLowerAndExecute(ctx, ir, optimize = true, lowerTable, lowerBM)
       } catch {
-        case e: LowererUnsupportedOperation if flags.get("lower_only") != null => throw e
+        case e: LowererUnsupportedOperation if ctx.flags.get("lower_only") != null => throw e
         case _: LowererUnsupportedOperation =>
           CompileAndEvaluate._apply(ctx, ir, optimize = true)
       }
@@ -549,7 +443,7 @@ class SparkBackend(
     rt: RTable,
     nPartitions: Option[Int],
   ): TableReader = {
-    if (flags.get("use_new_shuffle") != null)
+    if (ctx.flags.get("use_new_shuffle") != null)
       return LowerDistributedSort.distributedSort(ctx, stage, sortFields, rt)
 
     val (globals, rvd) = TableStageToRVD(ctx, stage)
