b88dee2dd21af6a4f81e2ae69722c3615573ee5d
Edmund Higham <edhigham@gmail.com>
2024-09-20 18:06:07 -0400

    [query] Lift backend state into `{Service|Py4J}BackendApi`

---
+++ hail/src/main/scala/is/hail/backend/api/Py4JBackendApi.scala
@@ -0,0 +1,405 @@
+package is.hail.backend.api
+
+import is.hail.HailFeatureFlags
+import is.hail.asm4s.HailClassLoader
+import is.hail.backend._
+import is.hail.backend.caching.BlockMatrixCache
+import is.hail.backend.spark.SparkBackend
+import is.hail.expr.{JSONAnnotationImpex, SparkAnnotationImpex}
+import is.hail.expr.ir.{
+  BaseIR, BlockMatrixIR, CodeCacheKey, CompiledFunction, EncodedLiteral, GetFieldByIdx, IRParser,
+  Interpret, MatrixIR, MatrixNativeReader, MatrixRead, NativeReaderOptions, TableLiteral, TableValue,
+}
+import is.hail.expr.ir.IRParser.parseType
+import is.hail.expr.ir.LoweredTableReader.LoweredTableReaderCoercer
+import is.hail.expr.ir.functions.IRFunctionRegistry
+import is.hail.expr.ir.lowering.IrMetadata
+import is.hail.io.fs._
+import is.hail.io.reference.{IndexedFastaSequenceFile, LiftOver}
+import is.hail.linalg.RowMatrix
+import is.hail.types.physical.PStruct
+import is.hail.types.virtual.{TArray, TInterval}
+import is.hail.types.virtual.Kinds.{BlockMatrix, Matrix, Table, Value}
+import is.hail.utils._
+import is.hail.utils.ExecutionTimer.Timings
+import is.hail.variant.ReferenceGenome
+
+import scala.collection.mutable
+import scala.jdk.CollectionConverters.{asScalaBufferConverter, seqAsJavaListConverter}
+
+import java.io.Closeable
+import java.net.InetSocketAddress
+import java.util
+import java.util.concurrent._
+
+import com.google.api.client.http.HttpStatusCodes
+import com.sun.net.httpserver.{HttpExchange, HttpServer}
+import org.apache.hadoop
+import org.apache.hadoop.conf.Configuration
+import org.apache.spark.sql.DataFrame
+import org.json4s
+import org.json4s._
+import org.json4s.jackson.{JsonMethods, Serialization}
+import sourcecode.Enclosing
+
+final class Py4JBackendApi(backend: Backend) extends Closeable with ErrorHandling {
+
+  private[this] val tmpdir: String = ???
+  private[this] val localTmpdir: String = ???
+  private[this] val longLifeTempFileManager = null: TempFileManager
+  private[this] val flags: HailFeatureFlags = HailFeatureFlags.fromEnv()
+  private[this] val theHailClassLoader = new HailClassLoader(getClass.getClassLoader)
+  private[this] val references = mutable.Map(ReferenceGenome.builtinReferences().toSeq: _*)
+  private[this] val bmCache = new BlockMatrixCache()
+  private[this] val codeCache = new Cache[CodeCacheKey, CompiledFunction[_]](50)
+  private[this] val persistedIr = mutable.Map[Int, BaseIR]()
+  private[this] val coercerCache = new Cache[Any, LoweredTableReaderCoercer](32)
+
+  private[this] def cloudfsConfig = CloudStorageFSConfig.fromFlagsAndEnv(None, flags)
+
+  def fs: FS = backend match {
+    case s: SparkBackend =>
+      val conf = new Configuration(s.sc.hadoopConfiguration)
+      cloudfsConfig.google.flatMap(_.requester_pays_config).foreach {
+        case RequesterPaysConfig(prj, bkts) =>
+          bkts
+            .map { buckets =>
+              conf.set("fs.gs.requester.pays.mode", "CUSTOM")
+              conf.set("fs.gs.requester.pays.project.id", prj)
+              conf.set("fs.gs.requester.pays.buckets", buckets.mkString(","))
+            }
+            .getOrElse {
+              conf.set("fs.gs.requester.pays.mode", "AUTO")
+              conf.set("fs.gs.requester.pays.project.id", prj)
+            }
+      }
+      new HadoopFS(new SerializableHadoopConfiguration(conf))
+
+    case _ =>
+      RouterFS.buildRoutes(cloudfsConfig)
+  }
+
+  def pyGetFlag(name: String): String =
+    flags.get(name)
+
+  def pySetFlag(name: String, value: String): Unit =
+    flags.set(name, value)
+
+  def pyAvailableFlags: java.util.ArrayList[String] =
+    flags.available
+
+  private[this] var irID: Int = 0
+
+  private[this] def nextIRID(): Int = {
+    irID += 1
+    irID
+  }
+
+  private[this] def addJavaIR(ctx: ExecuteContext, ir: BaseIR): Int = {
+    val id = nextIRID()
+    ctx.IrCache += (id -> ir)
+    id
+  }
+
+  def pyRemoveJavaIR(id: Int): Unit =
+    persistedIr.remove(id)
+
+  def pyAddSequence(name: String, fastaFile: String, indexFile: String): Unit =
+    references(name).addSequence(IndexedFastaSequenceFile(fs, fastaFile, indexFile))
+
+  def pyRemoveSequence(name: String): Unit =
+    references(name).removeSequence()
+
+  def pyExportBlockMatrix(
+    pathIn: String,
+    pathOut: String,
+    delimiter: String,
+    header: String,
+    addIndex: Boolean,
+    exportType: String,
+    partitionSize: java.lang.Integer,
+    entries: String,
+  ): Unit =
+    withExecuteContext() { ctx =>
+      val rm = RowMatrix.readBlockMatrix(ctx.fs, pathIn, partitionSize)
+      entries match {
+        case "full" =>
+          rm.export(ctx, pathOut, delimiter, Option(header), addIndex, exportType)
+        case "lower" =>
+          rm.exportLowerTriangle(ctx, pathOut, delimiter, Option(header), addIndex, exportType)
+        case "strict_lower" =>
+          rm.exportStrictLowerTriangle(
+            ctx,
+            pathOut,
+            delimiter,
+            Option(header),
+            addIndex,
+            exportType,
+          )
+        case "upper" =>
+          rm.exportUpperTriangle(ctx, pathOut, delimiter, Option(header), addIndex, exportType)
+        case "strict_upper" =>
+          rm.exportStrictUpperTriangle(
+            ctx,
+            pathOut,
+            delimiter,
+            Option(header),
+            addIndex,
+            exportType,
+          )
+      }
+    }
+
+  def pyRegisterIR(
+    name: String,
+    typeParamStrs: java.util.ArrayList[String],
+    argNameStrs: java.util.ArrayList[String],
+    argTypeStrs: java.util.ArrayList[String],
+    returnType: String,
+    bodyStr: String,
+  ): Unit =
+    withExecuteContext() { ctx =>
+      IRFunctionRegistry.registerIR(
+        ctx,
+        name,
+        typeParamStrs.asScala.toArray,
+        argNameStrs.asScala.toArray,
+        argTypeStrs.asScala.toArray,
+        returnType,
+        bodyStr,
+      )
+    }
+
+  def pyExecuteLiteral(irStr: String): Int =
+    withExecuteContext() { ctx =>
+      val ir = IRParser.parse_value_ir(ctx, irStr)
+      assert(ir.typ.isRealizable)
+      backend.execute(ctx, ir) match {
+        case Left(_) => throw new HailException("Can't create literal")
+        case Right((pt, addr)) =>
+          val field = GetFieldByIdx(EncodedLiteral.fromPTypeAndAddress(pt, addr, ctx), 0)
+          addJavaIR(ctx, field)
+      }
+    }._1
+
+  def pyFromDF(df: DataFrame, jKey: java.util.List[String]): (Int, String) =
+    withExecuteContext(selfContainedExecution = false) { ctx =>
+      val key = jKey.asScala.toArray.toFastSeq
+      val signature =
+        SparkAnnotationImpex.importType(df.schema).setRequired(true).asInstanceOf[PStruct]
+      val tir = TableLiteral(
+        TableValue(
+          ctx,
+          signature.virtualType,
+          key,
+          df.rdd,
+          Some(signature),
+        ),
+        ctx.theHailClassLoader,
+      )
+      val id = addJavaIR(ctx, tir)
+      (id, JsonMethods.compact(tir.typ.toJSON))
+    }._1
+
+  def pyToDF(s: String): DataFrame =
+    withExecuteContext() { ctx =>
+      val tir = IRParser.parse_table_ir(ctx, s)
+      Interpret(tir, ctx).toDF()
+    }._1
+
+  def pyReadMultipleMatrixTables(jsonQuery: String): util.List[MatrixIR] =
+    withExecuteContext() { ctx =>
+      log.info("pyReadMultipleMatrixTables: got query")
+      val kvs = JsonMethods.parse(jsonQuery) match {
+        case json4s.JObject(values) => values.toMap
+      }
+
+      val paths = kvs("paths").asInstanceOf[json4s.JArray].arr.toArray.map {
+        case json4s.JString(s) => s
+      }
+
+      val intervalPointType = parseType(kvs("intervalPointType").asInstanceOf[json4s.JString].s)
+      val intervalObjects =
+        JSONAnnotationImpex.importAnnotation(kvs("intervals"), TArray(TInterval(intervalPointType)))
+          .asInstanceOf[IndexedSeq[Interval]]
+
+      val opts = NativeReaderOptions(intervalObjects, intervalPointType)
+      val matrixReaders: IndexedSeq[MatrixIR] = paths.map { p =>
+        log.info(s"creating MatrixRead node for $p")
+        val mnr = MatrixNativeReader(ctx.fs, p, Some(opts))
+        MatrixRead(mnr.fullMatrixTypeWithoutUIDs, false, false, mnr): MatrixIR
+      }
+      log.info("pyReadMultipleMatrixTables: returning N matrix tables")
+      matrixReaders.asJava
+    }._1
+
+  def pyAddReference(jsonConfig: String): Unit =
+    addReference(ReferenceGenome.fromJSON(jsonConfig))
+
+  def pyRemoveReference(name: String): Unit =
+    removeReference(name)
+
+  def pyAddLiftover(name: String, chainFile: String, destRGName: String): Unit =
+    references(name).addLiftover(references(destRGName), LiftOver(fs, chainFile))
+
+  def pyRemoveLiftover(name: String, destRGName: String): Unit =
+    references(name).removeLiftover(destRGName)
+
+  def parse_blockmatrix_ir(s: String): BlockMatrixIR =
+    withExecuteContext(selfContainedExecution = false) { ctx =>
+      IRParser.parse_blockmatrix_ir(ctx, s)
+    }._1
+
+  private[this] def addReference(rg: ReferenceGenome): Unit = {
+    references.get(rg.name) match {
+      case Some(rg2) =>
+        if (rg != rg2) {
+          fatal(
+            s"Cannot add reference genome '${rg.name}', a different reference with that name already exists. Choose a reference name NOT in the following list:\n  " +
+              s"@1",
+            references.keys.truncatable("\n  "),
+          )
+        }
+      case None =>
+        references += (rg.name -> rg)
+    }
+  }
+
+  private[this] def removeReference(name: String): Unit =
+    references -= name
+
+  private def withExecuteContext[T](
+    selfContainedExecution: Boolean = true
+  )(
+    f: ExecuteContext => T
+  )(implicit E: Enclosing
+  ): (T, Timings) =
+    ExecutionTimer.time { timer =>
+      ExecuteContext.scoped(
+        tmpdir = tmpdir,
+        localTmpdir = localTmpdir,
+        backend = backend,
+        fs = fs,
+        timer = timer,
+        tempFileManager =
+          if (selfContainedExecution) null
+          else NonOwningTempFileManager(longLifeTempFileManager),
+        theHailClassLoader = theHailClassLoader,
+        flags = flags,
+        irMetadata = new IrMetadata(),
+        references = references,
+        blockMatrixCache = bmCache,
+        codeCache = codeCache,
+        irCache = persistedIr,
+        coercerCache = coercerCache,
+      )(f)
+    }
+
+  override def close(): Unit =
+    synchronized {
+      bmCache.close()
+      codeCache.clear()
+      persistedIr.clear()
+      coercerCache.clear()
+      backend.close()
+
+      if (backend.isInstanceOf[SparkBackend]) {
+        // Hadoop does not honor the hadoop configuration as a component of the cache key for file
+        // systems, so we blow away the cache so that a new configuration can successfully take
+        // effect.
+        // https://github.com/hail-is/hail/pull/12133#issuecomment-1241322443
+        hadoop.fs.FileSystem.closeAll()
+      }
+    }
+
+  def pyHttpServer: HttpLikeBackendRpc[HttpExchange] with Closeable =
+    new HttpLikeBackendRpc[HttpExchange] with Closeable {
+      implicit object Handler extends Routing with Write[HttpExchange] with Context[HttpExchange] {
+
+        override def route(req: HttpExchange): Route =
+          req.getRequestURI.getPath match {
+            case "/value/type" => Routes.TypeOf(Value)
+            case "/table/type" => Routes.TypeOf(Table)
+            case "/matrixtable/type" => Routes.TypeOf(Matrix)
+            case "/blockmatrix/type" => Routes.TypeOf(BlockMatrix)
+            case "/execute" => Routes.Execute
+            case "/vcf/metadata/parse" => Routes.ParseVcfMetadata
+            case "/fam/import" => Routes.ImportFam
+            case "/references/load" => Routes.LoadReferencesFromDataset
+            case "/references/from_fasta" => Routes.LoadReferencesFromFASTA
+          }
+
+        override def payload(req: HttpExchange): JValue =
+          using(req.getRequestBody)(JsonMethods.parse(_))
+
+        override def timings(req: HttpExchange)(t: Timings): Unit = {
+          val ts = Serialization.write(Map("timings" -> t))
+          req.getResponseHeaders.add("X-Hail-Timings", ts)
+        }
+
+        override def result(req: HttpExchange)(result: Array[Byte]): Unit =
+          respond(req)(HttpStatusCodes.STATUS_CODE_OK, result)
+
+        override def error(req: HttpExchange)(t: Throwable): Unit =
+          respond(req)(
+            HttpStatusCodes.STATUS_CODE_SERVER_ERROR,
+            jsonToBytes {
+              val (shortMessage, expandedMessage, errorId) = handleForPython(t)
+              JObject(
+                "short" -> JString(shortMessage),
+                "expanded" -> JString(expandedMessage),
+                "error_id" -> JInt(errorId),
+              )
+            },
+          )
+
+        private[this] def respond(req: HttpExchange)(code: Int, payload: Array[Byte]): Unit = {
+          req.sendResponseHeaders(code, payload.length)
+          using(req.getResponseBody)(_.write(payload))
+        }
+
+        override def scoped[A](req: HttpExchange)(f: ExecuteContext => A): (A, Timings) =
+          withExecuteContext()(f)
+      }
+
+      // 0 => let the OS pick an available port
+      private[this] val httpServer = HttpServer.create(new InetSocketAddress(0), 10)
+
+      private[this] val thread = {
+        // This HTTP server *must not* start non-daemon threads because such threads keep the JVM
+        // alive. A living JVM indicates to Spark that the job is incomplete. This does not manifest
+        /* when you run jobs in a local pyspark (because you'll Ctrl-C out of Python regardless of
+         * the */
+        // JVM's state) nor does it manifest in a Notebook (again, you'll kill the Notebook kernel
+        // explicitly regardless of the JVM). It *does* manifest when submitting jobs with
+        //
+        //     gcloud dataproc submit ...
+        //
+        // or
+        //
+        //     spark-submit
+        //
+        // setExecutor(null) ensures the server creates no new threads:
+        //
+        /* > If this method is not called (before start()) or if it is called with a null Executor,
+         * then */
+        /* > a default implementation is used, which uses the thread which was created by the
+         * start() */
+        // > method.
+        //
+        // Source:
+        /* https://docs.oracle.com/en/java/javase/11/docs/api/jdk.httpserver/com/sun/net/httpserver/HttpServer.html#setExecutor(java.util.concurrent.Executor) */
+        //
+        httpServer.createContext("/", runRpc(_: HttpExchange))
+        httpServer.setExecutor(null)
+        val t = Executors.defaultThreadFactory().newThread(() => httpServer.start())
+        t.setDaemon(true)
+        t
+      }
+
+      def port: Int = httpServer.getAddress.getPort
+      override def close(): Unit = httpServer.stop(10)
+
+      thread.start()
+    }
+}
--- hail/src/test/scala/is/hail/backend/ServiceBackendSuite.scala
+++ hail/src/test/scala/is/hail/backend/ServiceBackendSuite.scala
@@ -1,17 +1,13 @@
 package is.hail.backend
 
-import is.hail.HailFeatureFlags
-import is.hail.asm4s.HailClassLoader
-import is.hail.backend.service.{
-  BatchJobConfig, ServiceBackend, ServiceBackendContext, ServiceBackendRPCPayload,
-}
-import is.hail.io.fs.{CloudStorageFSConfig, RouterFS}
+import is.hail.HailSuite
+import is.hail.backend.api.BatchJobConfig
+import is.hail.backend.service.ServiceBackend
 import is.hail.services._
 import is.hail.services.JobGroupStates.Success
 import is.hail.utils.{tokenUrlSafe, using}
 
-import scala.collection.mutable
-import scala.reflect.io.{Directory, Path}
+import scala.reflect.io.Directory
 import scala.util.Random
 
 import java.io.Closeable
@@ -21,136 +17,105 @@ import org.mockito.IdiomaticMockito
 import org.mockito.MockitoSugar.when
 import org.scalatest.OptionValues
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
-import org.scalatestplus.testng.TestNGSuite
 import org.testng.annotations.Test
 
-class ServiceBackendSuite extends TestNGSuite with IdiomaticMockito with OptionValues {
+class ServiceBackendSuite extends HailSuite with IdiomaticMockito with OptionValues {
 
   @Test def testCreateJobPayload(): Unit =
-    withMockDriverContext { case (rpcConfig, jobConfig) =>
+    withObjectSpied[is.hail.utils.UtilsType] {
+      // not obvious how to pull out `tokenUrlSafe` and inject this directory
+      // using a spy is a hack and i don't particularly like it.
+      when(is.hail.utils.tokenUrlSafe) thenAnswer "TOKEN"
+
+      val jobConfig = BatchJobConfig(
+        token = tokenUrlSafe,
+        billing_project = "fancy",
+        worker_cores = "128",
+        worker_memory = "a lot.",
+        storage = "a big ssd?",
+        cloudfuse_configs = Array(),
+        regions = Array("lunar1"),
+      )
+
       val batchClient = mock[BatchClient]
-      using(ServiceBackend(batchClient, rpcConfig, jobConfig)) { backend =>
-        val contexts = Array.tabulate(1)(_.toString.getBytes)
+      using(ServiceBackend(batchClient, jobConfig)) { backend =>
+        using(LocalTmpFolder) { tmp =>
+          val contexts = Array.tabulate(1)(_.toString.getBytes)
 
-        // verify that
-        // - the number of jobs matches the number of partitions, and
-        // - each job is created in the specified region, and
-        // - each job's resource configuration matches the rpc config
+          // verify that
+          // - the number of jobs matches the number of partitions, and
+          // - each job is created in the specified region, and
+          // - each job's resource configuration matches the rpc config
 
-        when(batchClient.newJobGroup(any[JobGroupRequest])) thenAnswer {
-          jobGroup: JobGroupRequest =>
-            jobGroup.batch_id shouldBe backend.batchConfig.batchId
-            jobGroup.absolute_parent_id shouldBe backend.batchConfig.jobGroupId
-            val jobs = jobGroup.jobs
-            jobs.length shouldEqual contexts.length
-            jobs.foreach { payload =>
-              payload.regions.value shouldBe jobConfig.regions
-              payload.resources.value shouldBe JobResources(
-                preemptible = true,
-                cpu = Some(jobConfig.worker_cores),
-                memory = Some(jobConfig.worker_memory),
-                storage = Some(jobConfig.storage),
+          when(batchClient.newJobGroup(any[JobGroupRequest])) thenAnswer {
+            jobGroup: JobGroupRequest =>
+              jobGroup.batch_id shouldBe backend.batchConfig.batchId
+              jobGroup.absolute_parent_id shouldBe backend.batchConfig.jobGroupId
+              val jobs = jobGroup.jobs
+              jobs.length shouldEqual contexts.length
+              jobs.foreach { payload =>
+                payload.regions.value shouldBe jobConfig.regions
+                payload.resources.value shouldBe JobResources(
+                  preemptible = true,
+                  cpu = Some(jobConfig.worker_cores),
+                  memory = Some(jobConfig.worker_memory),
+                  storage = Some(jobConfig.storage),
+                )
+              }
+
+              backend.batchConfig.jobGroupId + 1
+          }
+
+          // the service backend expects that each job write its output to a well-known
+          // location when it finishes.
+          when(batchClient.waitForJobGroup(any[Int], any[Int])) thenAnswer {
+            (id: Int, jobGroupId: Int) =>
+              id shouldEqual backend.batchConfig.batchId
+              jobGroupId shouldEqual backend.batchConfig.jobGroupId + 1
+
+              val resultsDir = tmp / "parallelizeAndComputeWithIndex" / tokenUrlSafe
+
+              resultsDir.createDirectory()
+              for (i <- contexts.indices) (resultsDir / f"result.$i").toFile.writeAll("11")
+              JobGroupResponse(
+                batch_id = id,
+                job_group_id = jobGroupId,
+                state = Success,
+                complete = true,
+                n_jobs = contexts.length,
+                n_completed = contexts.length,
+                n_succeeded = contexts.length,
+                n_failed = 0,
+                n_cancelled = 0,
               )
-            }
+          }
 
-            backend.batchConfig.jobGroupId + 1
+          ctx.local(tmpdir = tmp.toString()) { ctx =>
+            val (failure, _) =
+              backend.parallelizeAndComputeWithIndex(
+                backend.backendContext(ctx),
+                ctx.fs,
+                contexts,
+                "stage1",
+              )((bytes, _, _, _) => bytes)
+
+            failure.foreach(throw _)
+          }
+          batchClient.newJobGroup(any) wasCalled once
+          batchClient.waitForJobGroup(any, any) wasCalled once
+
         }
-
-        // the service backend expects that each job write its output to a well-known
-        // location when it finishes.
-        when(batchClient.waitForJobGroup(any[Int], any[Int])) thenAnswer {
-          (id: Int, jobGroupId: Int) =>
-            id shouldEqual backend.batchConfig.batchId
-            jobGroupId shouldEqual backend.batchConfig.jobGroupId + 1
-
-            val resultsDir =
-              Path(rpcConfig.remote_tmpdir) /
-                "parallelizeAndComputeWithIndex" /
-                tokenUrlSafe
-
-            resultsDir.createDirectory()
-            for (i <- contexts.indices) (resultsDir / f"result.$i").toFile.writeAll("11")
-            JobGroupResponse(
-              batch_id = id,
-              job_group_id = jobGroupId,
-              state = Success,
-              complete = true,
-              n_jobs = contexts.length,
-              n_completed = contexts.length,
-              n_succeeded = contexts.length,
-              n_failed = 0,
-              n_cancelled = 0,
-            )
-        }
-
-        val (failure, _) =
-          backend.parallelizeAndComputeWithIndex(
-            ServiceBackendContext(
-              remoteTmpDir = rpcConfig.remote_tmpdir,
-              jobConfig = jobConfig,
-              executionCache = ExecutionCache.noCache,
-            ),
-            backend.fs,
-            contexts,
-            "stage1",
-          )((bytes, _, _, _) => bytes)
-
-        failure.foreach(throw _)
-
-        batchClient.newJobGroup(any) wasCalled once
-        batchClient.waitForJobGroup(any, any) wasCalled once
       }
     }
 
-  def ServiceBackend(
-    client: BatchClient,
-    rpcConfig: ServiceBackendRPCPayload,
-    jobConfig: BatchJobConfig,
-  ): ServiceBackend = {
-    val flags = HailFeatureFlags.fromEnv()
-    val fs = RouterFS.buildRoutes(CloudStorageFSConfig())
+  def ServiceBackend(client: BatchClient, jobConfig: BatchJobConfig): ServiceBackend =
     new ServiceBackend(
       name = "name",
       batchClient = client,
       jarSpec = GitRevision("123"),
-      theHailClassLoader = new HailClassLoader(getClass.getClassLoader),
       batchConfig = BatchConfig(batchId = Random.nextInt(), jobGroupId = Random.nextInt()),
-      rpcConfig = rpcConfig,
       jobConfig = jobConfig,
-      flags = flags,
-      fs = fs,
-      references = mutable.Map.empty,
     )
-  }
-
-  def withMockDriverContext(test: (ServiceBackendRPCPayload, BatchJobConfig) => Any): Any =
-    using(LocalTmpFolder) { tmp =>
-      withObjectSpied[is.hail.utils.UtilsType] {
-        // not obvious how to pull out `tokenUrlSafe` and inject this directory
-        // using a spy is a hack and i don't particularly like it.
-        when(is.hail.utils.tokenUrlSafe) thenAnswer "TOKEN"
-
-        test(
-          ServiceBackendRPCPayload(
-            tmp_dir = tmp.path,
-            remote_tmpdir = tmp.path,
-            flags = Map(),
-            custom_references = Array(),
-            liftovers = Map(),
-            sequences = Map(),
-          ),
-          BatchJobConfig(
-            token = tokenUrlSafe,
-            billing_project = "fancy",
-            worker_cores = "128",
-            worker_memory = "a lot.",
-            storage = "a big ssd?",
-            cloudfuse_configs = Array(),
-            regions = Array("lunar1"),
-          ),
-        )
-      }
-    }
 
   def LocalTmpFolder: Directory with Closeable =
     new Directory(Directory.makeTemp("hail-testing-tmp").jfile) with Closeable {
