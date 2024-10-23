5480673ad4688f8cb365b08cb5deba1e491acbb4
Edmund Higham <edhigham@gmail.com>
2024-10-23 18:01:22 -0400

    job ids are 1-based

--- hail/src/main/scala/is/hail/backend/service/Main.scala
+++ hail/src/main/scala/is/hail/backend/service/Main.scala
@@ -3,11 +3,13 @@ package is.hail.backend.service
 object Main {
   val WORKER = "worker"
   val DRIVER = "driver"
+  val TEST = "test"
 
   def main(argv: Array[String]): Unit =
     argv(3) match {
       case WORKER => Worker.main(argv)
       case DRIVER => ServiceBackendAPI.main(argv)
+      case TEST => ()
       case kind => throw new RuntimeException(s"unknown kind: $kind")
     }
 }
