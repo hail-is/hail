b4d039b60b37cd5a24d27ab4622743a80dc8a1bd
Christopher Vittal <cvittal@broadinstitute.org>
2024-11-05 16:21:47 -0500

    fmt

--- hail/src/main/scala/is/hail/backend/service/Main.scala
+++ hail/src/main/scala/is/hail/backend/service/Main.scala
@@ -9,18 +9,15 @@ object Main {
   /* This constraint should be overridden as early as possible. See:
    * - https://github.com/hail-is/hail/issues/14580
    * - https://github.com/hail-is/hail/issues/14749
-   * - The note on this setting in is.hail.backend.Backend
-   */
+   * - The note on this setting in is.hail.backend.Backend */
   StreamReadConstraints.overrideDefaultStreamReadConstraints(
     StreamReadConstraints.builder().maxStringLength(Integer.MAX_VALUE).build()
   )
 
-  def main(argv: Array[String]): Unit = {
-
+  def main(argv: Array[String]): Unit =
     argv(3) match {
       case WORKER => Worker.main(argv)
       case DRIVER => ServiceBackendAPI.main(argv)
       case kind => throw new RuntimeException(s"unknown kind: $kind")
     }
-  }
 }
