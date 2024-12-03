341f1dd22a7ebe998218a6d03a2286c713d078a7
Edmund Higham <edhigham@gmail.com>
2024-12-03 15:58:46 -0500

    [query] Various Benchmark Suite Improvements

--- hail/python/benchmark/hail/conftest.py
+++ hail/python/benchmark/hail/conftest.py
@@ -8,7 +8,6 @@ from datetime import datetime, timezone
 from typing import Any, Dict, List, Literal
 
 import pytest
-from _pytest.runner import pytest_runtest_protocol as runner_runtest_protocol
 
 import hail as hl
 from benchmark.hail.fixtures import (
@@ -66,6 +65,7 @@ from benchmark.hail.fixtures import (
     chr22_gvcfs,
     empty_gvcf,
     gnomad_dp_sim,
+    local_tmpdir,
     many_ints_ht,
     many_ints_tsv,
     many_partitions_ht,
@@ -85,14 +85,14 @@ from benchmark.hail.utils import (
     get_peak_task_memory,
     init_hail_for_benchmarks,
     run_with_timeout,
-    select,
 )
+from benchmark.tools import maybe, prune
 from hail.utils.java import Env
 
 
 @contextmanager
-def init_hail(run_config):
-    init_hail_for_benchmarks(run_config)
+def init_hail(config):
+    init_hail_for_benchmarks(config)
     try:
         yield
     finally:
@@ -105,7 +105,8 @@ results = pytest.StashKey[Dict[str, Dict[str, Any]]]()
 start = pytest.StashKey[datetime]()
 end = pytest.StashKey[datetime]()
 iteration = pytest.StashKey[int]()
-runs = pytest.StashKey[List[Dict[str, Any]]]()
+iterations = pytest.StashKey[List[Dict[str, Any]]]()
+consecutive_fail_count = pytest.StashKey[int]()
 
 # used internally
 context = pytest.StashKey[Literal['burn_in', 'benchmark']]()
@@ -113,43 +114,47 @@ context = pytest.StashKey[Literal['burn_in', 'benchmark']]()
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_runtest_protocol(item, nextitem):
-    run_config = item.session.config.run_config
+    runner = item.config.pluginmanager.get_plugin('runner')
     # Initialise hail before running every benchmark for two reasons:
     # - each benchmark runs in a clean hail session
     # - our means of getting max task memory is quite crude (regex on logs)
     #   and a fresh session provides a new log
-    with init_hail(run_config):
-        if run_config.iterations is not None:
+    with init_hail(item.config):
+        if (num_iterations := item.config.getoption('iterations')) is not None:
             burn_in_iterations = 0
-            iterations = run_config.iterations
             logging.info(
                 msg=(
                     f'Picked up iterations override. Config: '
                     f'burn_in_iterations: {burn_in_iterations}, '
-                    f'iterations: {iterations}.'
+                    f'iterations: {num_iterations}.'
                 )
             )
 
         else:
-            burn_in_iterations, iterations = select(
-                ['burn_in_iterations', 'iterations'], **(item.get_closest_marker('benchmark').kwargs)
-            )
+            burn_in_iterations = 1
+            num_iterations = 5
 
         s = item.stash
         s[start] = datetime.now(timezone.utc).isoformat()
-        s[runs] = []
+        s[iterations] = []
+        s[consecutive_fail_count] = 0
         s[end] = None
 
         logging.info(
             msg=(
                 f'Executing "{item.nodeid}" with '
                 f'{burn_in_iterations} burn in iterations and '
-                f'{iterations} timed iterations.'
+                f'{num_iterations} timed iterations.'
             )
         )
 
+        max_failures = item.config.getoption('max_failures')
+
         s[context] = 'burn_in'
         for k in range(burn_in_iterations):
+            if max_failures and s[consecutive_fail_count] >= max_failures:
+                break
+
             s[iteration] = k
             # `nextitem` is used to determine which fixtures need to be torn-down
             # after the test finishes. For example, if `nextitem` is `None`, then
@@ -157,17 +162,25 @@ def pytest_runtest_protocol(item, nextitem):
             # Since we're invoking this benchmark repeatedly, we want to tear-down
             # function/method level fixtures only, leaving module and session
             # fixtures in place; `item.parent` is one such `Item` that represents this.
-            runner_runtest_protocol(item, nextitem=item.parent)
+            runner.pytest_runtest_protocol(item, nextitem=item.parent)
 
         s[context] = 'benchmark'
-        total_iterations = burn_in_iterations + iterations
+        total_iterations = burn_in_iterations + num_iterations
         for k in range(burn_in_iterations, total_iterations):
+            if max_failures and s[consecutive_fail_count] >= max_failures:
+                break
+
             s[iteration] = k
             # on the final iteration, perform the required teardown for the test
             is_final_iteration = k == total_iterations - 1
-            runner_runtest_protocol(item, nextitem=nextitem if is_final_iteration else item.parent)
+            runner.pytest_runtest_protocol(item, nextitem=nextitem if is_final_iteration else item.parent)
 
         s[end] = datetime.now(timezone.utc).isoformat()
+
+        if max_failures and s[consecutive_fail_count] >= max_failures:
+            logging.error(
+                msg=(f'Benchmarking "{item.nodeid}" aborted due to too many consecutive failures (max={max_failures})')
+            )
 
     # prevent other plugins running that might invoke the benchmark again
     return True
@@ -176,7 +189,7 @@ def pytest_runtest_protocol(item, nextitem):
 @pytest.hookimpl(tryfirst=True)
 def pytest_pyfunc_call(pyfuncitem):
     with run_with_timeout(
-        pyfuncitem.config.run_config,
+        pyfuncitem.config.getoption('max_duration'),
         pyfuncitem.obj,
         **{arg: pyfuncitem.funcargs[arg] for arg in pyfuncitem._fixtureinfo.argnames},
     ) as (time, timed_out, traceback):
@@ -184,16 +197,22 @@ def pytest_pyfunc_call(pyfuncitem):
 
         is_burn_in = s[context] == 'burn_in'
 
-        s[runs].append({
-            'iteration': s[iteration],
-            'time': time,
-            'is_burn_in': is_burn_in,
-            'timed_out': timed_out,
-            'failure': traceback,
-            'task_memory': get_peak_task_memory(Env.hc()._log),
-        })
+        if timed_out or traceback is not None:
+            s[consecutive_fail_count] += 1
+        else:
+            s[consecutive_fail_count] = 0
 
-        logging.info(f'{"(burn in) " if is_burn_in else ""}iteration {s[iteration]}, time: {time}s')
+        s[iterations].append(
+            prune({
+                'time': time,
+                'is_burn_in': is_burn_in,
+                'timed_out': timed_out,
+                'failure': maybe(json.dumps, traceback),
+                'task_memory': get_peak_task_memory(Env.hc()._log),
+            })
+        )
+
+        logging.info(msg=f'{"(burn in) " if is_burn_in else ""}iteration {s[iteration]}, time: {time}s')
 
     # prevent other plugins running that might invoke the benchmark again
     return True
@@ -210,33 +229,43 @@ def open_file_or_stdout(file):
 
 @pytest.hookimpl
 def pytest_sessionfinish(session):
-    if not session.config.option.collectonly:
-        run_config = session.config.run_config
-
+    if hasattr(session, 'items') and len(session.items) > 0 and not session.config.option.collectonly:
         meta = {
-            'uname': platform.uname()._asdict(),
+            'platform': platform.platform(),
+            'python': f"{platform.python_implementation()} {platform.python_version()}",
             'version': hl.__version__,
-            **({'batch_id': batch} if (batch := os.getenv('HAIL_BATCH_ID')) else {}),
-            **({'job_id': job} if (job := os.getenv('HAIL_JOB_ID')) else {}),
-            **({'trial': trial} if (trial := os.getenv('BENCHMARK_TRIAL_ID')) else {}),
+            'batch_id': maybe(int, os.getenv('HAIL_BATCH_ID')),
+            'job_id': maybe(int, os.getenv('HAIL_JOB_ID')),
+            'attempt_id': os.getenv('HAIL_ATTEMPT_ID'),
         }
 
         now = datetime.now(timezone.utc).isoformat()
-        with open_file_or_stdout(run_config.output) as out:
+        with open_file_or_stdout(session.config.getoption('output')) as out:
             for item in session.items:
                 path, _, name = item.location
                 json.dump(
-                    {
+                    prune({
                         'path': path,
                         'name': name,
                         **meta,
                         'start': item.stash[start],
                         'end': item.stash.get(end, now),
-                        'runs': item.stash[runs],
-                    },
+                        'iterations': item.stash[iterations],
+                    }),
                     out,
                 )
                 out.write('\n')
+
+
+@pytest.fixture(autouse=True)
+def new_query_tmpdir(tmp_path):
+    backend = hl.current_backend()
+    local, remote = backend.local_tmpdir, backend.remote_tmpdir
+    backend.local_tmpdir, backend.remote_tmpdir = [str(tmp_path / f) for f in ('local', 'remote')]
+    try:
+        yield
+    finally:
+        backend.local_tmpdir, backend.remote_tmpdir = local, remote
 
 
 # make fixtures discoverable to `pytest --fixtures` as well as all tests
@@ -296,11 +325,13 @@ __all__ = [
     'chr22_gvcfs',
     'empty_gvcf',
     'gnomad_dp_sim',
+    'local_tmpdir',
     'many_ints_ht',
     'many_ints_tsv',
     'many_partitions_ht',
     'many_strings_ht',
     'many_strings_tsv',
+    'new_query_tmpdir',
     'onekg_chr22',
     'profile25_mt',
     'profile25_vcf',
