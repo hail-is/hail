314321a53bf31e2b41ef6c5fb63da1983dddcbf6
Edmund Higham <edhigham@gmail.com>
2025-01-22 16:46:53 -0500

    [query] Move `is.hail.check` to test code

--- hail/hail/src/is/hail/types/virtual/TString.scala
+++ hail/hail/src/is/hail/types/virtual/TString.scala
@@ -2,8 +2,6 @@ package is.hail.types.virtual
 
 import is.hail.annotations._
 import is.hail.backend.HailStateManager
-import is.hail.check.Arbitrary._
-import is.hail.check.Gen
 
 case object TString extends Type {
   def _toPretty = "String"
@@ -14,8 +12,6 @@ case object TString extends Type {
   override def _showStr(a: Annotation): String = "\"" + a.asInstanceOf[String] + "\""
 
   def _typeCheck(a: Any): Boolean = a.isInstanceOf[String]
-
-  override def genNonmissingValue(sm: HailStateManager): Gen[Annotation] = arbitrary[String]
 
   override def mkOrdering(sm: HailStateManager, missingEqual: Boolean): ExtendedOrdering =
     ExtendedOrdering.extendToNull(implicitly[Ordering[String]], missingEqual)
