8aa335566f17a484a81738a05bc6aed66b394400
Chris Llanwarne <cjllanwarne@gmail.com>
2025-02-03 12:05:47 -0500

    fixup regex

--- auth/auth/auth.py
+++ auth/auth/auth.py
@@ -155,7 +155,7 @@ def validate_credentials_secret_name_input(secret_name: Optional[str]):
     if secret_name is None:
         return
 
-    regex = re.compile(r'^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$')
+    regex = re.compile(r'^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$')
     if not regex.match(secret_name):
         raise AuthUserError(
             f'invalid credentials_secret_name {secret_name}. Must match RFC1123 (lowercase alphanumeric plus "." and "-", start and end with alphanumeric)',
