b473c53ebebb5d3036700df538d6efc133aecbf5
grohli <22306963+grohli@users.noreply.github.com>
2025-02-04 10:58:05 -0500

    Making session timeout duration configurable

--- gear/gear/auth_utils.py
+++ gear/gear/auth_utils.py
@@ -1,9 +1,13 @@
+import os
 import secrets
 from typing import Optional
 
 from hailtop.auth import session_id_encode_to_str
 
 from .database import Database
+
+# Default value: 86400 seconds (1 day)
+MAX_AGE_SECS = os.environ["SESSION_MAX_AGE_SECS"]
 
 
 async def insert_user(db, spec):
@@ -18,8 +22,7 @@ VALUES ({', '.join([f'%({k})s' for k in spec.keys()])})
     )
 
 
-# 2592000s = 30d
-async def create_session(db: Database, user_id: int, max_age_secs: Optional[int] = 2592000) -> str:
+async def create_session(db: Database, user_id: int, max_age_secs: Optional[int] = MAX_AGE_SECS) -> str:
     session_id = session_id_encode_to_str(secrets.token_bytes(32))
     await db.just_execute(
         'INSERT INTO sessions (session_id, user_id, max_age_secs) VALUES (%s, %s, %s);',
