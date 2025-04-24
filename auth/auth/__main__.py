759f83cad4c0bed6cc1378db6ba9f75d3d94c7ee
Christopher Vittal <cvittal@broadinstitute.org>
2025-04-24 13:10:59 -0400

    DEBUG: set auth logging to debug/trace

--- auth/auth/__main__.py
+++ auth/auth/__main__.py
@@ -1,7 +1,7 @@
 from hailtop.hail_logging import configure_logging
 
 # configure logging before importing anything else
-configure_logging()
+configure_logging(level=1)
 
 from .auth import run  # noqa: E402 pylint: disable=wrong-import-position
 
