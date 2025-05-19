cc352ff78dd9321b4c1cbb7dd6e6da796306165f
Jackie Goldstein <jigold@broadinstitute.org>
2025-05-19 16:30:36 -0400

    [hailctl] Configuration profiles

--- hail/python/hailtop/config/__init__.py
+++ hail/python/hailtop/config/__init__.py
@@ -1,10 +1,13 @@
 from .deploy_config import DeployConfig, get_deploy_config
 from .user_config import (
     configuration_of,
+    get_config_profile_name,
+    get_config_from_file,
     get_hail_config_path,
     get_remote_tmpdir,
     get_user_config,
     get_user_config_path,
+    get_user_config_path_by_profile_name,
     get_user_identity_config_path,
 )
 from .variables import ConfigVariable
@@ -13,10 +16,13 @@ __all__ = [
     'ConfigVariable',
     'DeployConfig',
     'configuration_of',
+    'get_config_from_file',
+    'get_config_profile_name',
     'get_deploy_config',
     'get_hail_config_path',
     'get_remote_tmpdir',
     'get_user_config',
     'get_user_config_path',
+    'get_user_config_path_by_profile_name',
     'get_user_identity_config_path',
 ]
