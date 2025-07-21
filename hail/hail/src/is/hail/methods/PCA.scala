c5ac8970bff8fb9fd9fab354f71478536a483d56
Edmund Higham <edhigham@gmail.com>
2025-07-21 12:32:08 -0400

    [query] remove unnecessary references to `HailContext`

--- hail/hail/src/is/hail/methods/PCA.scala
+++ hail/hail/src/is/hail/methods/PCA.scala
@@ -1,6 +1,4 @@
 package is.hail.methods
-
-import is.hail.HailContext
 import is.hail.annotations._
 import is.hail.backend.ExecuteContext
 import is.hail.expr.ir.{MatrixValue, TableValue}
@@ -63,7 +61,7 @@ case class PCA(entryField: String, k: Int, computeLoadings: Boolean) extends Mat
     ) ++ TStruct("loadings" -> TArray(TFloat64)))
       .setRequired(true)
       .asInstanceOf[PStruct]
-    val rowKeysBc = HailContext.backend.broadcast(collectRowKeys())
+    val rowKeysBc = ctx.backend.broadcast(collectRowKeys())
     val localRowKeySignature = mv.typ.rowKeyStruct.types
 
     val crdd: ContextRDD[Long] = if (computeLoadings) {
