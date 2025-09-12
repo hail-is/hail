d9a0a1b609b92024b97a53754ff0f98771a741a1
grohli <22306963+grohli@users.noreply.github.com>
2025-09-12 10:47:00 -0400

    Added batch test to validate nvidia driver installation

--- batch/test/test_batch.py
+++ batch/test/test_batch.py
@@ -1531,6 +1531,26 @@ async def test_gpu_accesibility_g2(client: BatchClient):
         pass
 
 
+@skip_in_azure
+async def test_nvidia_driver_accesibility_g2(client: BatchClient):
+    b = create_batch(client)._async_batch
+    resources = {'machine_type': "g2-standard-4", 'storage': '100Gi'}
+    j = b.create_job(
+        os.environ['HAIL_GPU_IMAGE'],
+        ['nvidia-smi'],
+        resources=resources,
+    )
+    await b.submit()
+    try:
+        status = await asyncio.wait_for(j.wait(), timeout=5 * 60)
+        assert status['state'] == 'Success', str((status, b.debug_info()))
+    except asyncio.TimeoutError:
+        # G2 instances are not always available within a time window
+        # acceptable for CI. This test is permitted to time out
+        # but not otherwise fail
+        pass
+
+
 def test_job_private_instance_preemptible(client: BatchClient):
     b = create_batch(client)
     resources = {'machine_type': smallest_machine_type()}
