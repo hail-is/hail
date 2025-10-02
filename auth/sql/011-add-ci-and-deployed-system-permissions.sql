488abe40e8e1a996c724b83f4ae0fde677690da2
Chris Llanwarne <cjllanwarne@gmail.com>
2025-10-02 11:29:09 -0400

    status=>state

--- auth/sql/011-add-ci-and-deployed-system-permissions.sql
+++ auth/sql/011-add-ci-and-deployed-system-permissions.sql
@@ -1,8 +1,8 @@
 -- Add new CI and Deployed System permissions to the system_permissions table
 INSERT INTO `system_permissions` (`name`) VALUES ('read_ci');
 INSERT INTO `system_permissions` (`name`) VALUES ('manage_ci');
-INSERT INTO `system_permissions` (`name`) VALUES ('read_deployed_system_status');
-INSERT INTO `system_permissions` (`name`) VALUES ('update_deployed_system_status');
+INSERT INTO `system_permissions` (`name`) VALUES ('read_deployed_system_state');
+INSERT INTO `system_permissions` (`name`) VALUES ('update_deployed_system_state');
 
 -- Assign these permissions to the developer role
 INSERT INTO `system_role_permissions` (`role_id`, `permission_id`) 
@@ -15,8 +15,8 @@ WHERE r.name = 'developer' AND p.name = 'manage_ci';
 
 INSERT INTO `system_role_permissions` (`role_id`, `permission_id`) 
 SELECT r.id, p.id FROM `system_roles` r, `system_permissions` p 
-WHERE r.name = 'developer' AND p.name = 'read_deployed_system_status';
+WHERE r.name = 'developer' AND p.name = 'read_deployed_system_state';
 
 INSERT INTO `system_role_permissions` (`role_id`, `permission_id`) 
 SELECT r.id, p.id FROM `system_roles` r, `system_permissions` p 
-WHERE r.name = 'developer' AND p.name = 'update_deployed_system_status';
+WHERE r.name = 'developer' AND p.name = 'update_deployed_system_state';
