0fa72f3699daf07d1fb052afb6236e5da91c51bb
Chris Llanwarne <cjllanwarne@gmail.com>
2025-10-02 16:54:21 -0400

    system state management for admins not devs

--- auth/sql/011-add-ci-and-deployed-system-permissions.sql
+++ auth/sql/011-add-ci-and-deployed-system-permissions.sql
@@ -4,19 +4,19 @@ INSERT INTO `system_permissions` (`name`) VALUES ('manage_ci');
 INSERT INTO `system_permissions` (`name`) VALUES ('read_deployed_system_state');
 INSERT INTO `system_permissions` (`name`) VALUES ('update_deployed_system_state');
 
--- Assign these permissions to the developer role
+-- Assign these permissions to the admin role
 INSERT INTO `system_role_permissions` (`role_id`, `permission_id`) 
 SELECT r.id, p.id FROM `system_roles` r, `system_permissions` p 
-WHERE r.name = 'developer' AND p.name = 'read_ci';
+WHERE r.name = 'admin' AND p.name = 'read_ci';
 
 INSERT INTO `system_role_permissions` (`role_id`, `permission_id`) 
 SELECT r.id, p.id FROM `system_roles` r, `system_permissions` p 
-WHERE r.name = 'developer' AND p.name = 'manage_ci';
+WHERE r.name = 'admin' AND p.name = 'manage_ci';
 
 INSERT INTO `system_role_permissions` (`role_id`, `permission_id`) 
 SELECT r.id, p.id FROM `system_roles` r, `system_permissions` p 
-WHERE r.name = 'developer' AND p.name = 'read_deployed_system_state';
+WHERE r.name = 'admin' AND p.name = 'read_deployed_system_state';
 
 INSERT INTO `system_role_permissions` (`role_id`, `permission_id`) 
 SELECT r.id, p.id FROM `system_roles` r, `system_permissions` p 
-WHERE r.name = 'developer' AND p.name = 'update_deployed_system_state';
+WHERE r.name = 'admin' AND p.name = 'update_deployed_system_state';
