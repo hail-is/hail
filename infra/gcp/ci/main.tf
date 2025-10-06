7749b3f2f347ce9eabac721e80baf3e551eb23e5
Kush Chandra <kchandra@broadinstitute.org>
2025-10-06 16:55:15 -0400

    Copy gcp-broad changes to gcp template

--- infra/gcp/ci/main.tf
+++ infra/gcp/ci/main.tf
@@ -28,6 +28,10 @@ resource "kubernetes_secret" "ci_config" {
     watched_branches = jsonencode(var.watched_branches)
     github_context   = var.github_context
   }
+
+  lifecycle {
+    ignore_changes = [metadata.0.annotations]
+  }
 }
 
 resource "kubernetes_secret" "zulip_config" {
@@ -39,6 +43,10 @@ resource "kubernetes_secret" "zulip_config" {
   data = {
     ".zuliprc" = fileexists("~/.hail/.zuliprc") ? file("~/.hail/.zuliprc") : ""
   }
+
+  lifecycle {
+    ignore_changes = [metadata.0.annotations]
+  }
 }
 
 resource "kubernetes_secret" "hail_ci_0_1_github_oauth_token" {
@@ -49,6 +57,10 @@ resource "kubernetes_secret" "hail_ci_0_1_github_oauth_token" {
   data = {
     "oauth-token" = var.github_oauth_token
   }
+
+  lifecycle {
+    ignore_changes = [metadata.0.annotations]
+  }
 }
 
 resource "kubernetes_secret" "hail_ci_0_1_service_account_key" {
@@ -58,5 +70,9 @@ resource "kubernetes_secret" "hail_ci_0_1_service_account_key" {
 
   data = {
     "user1" = var.github_user1_oauth_token
+  }
+
+  lifecycle {
+    ignore_changes = [metadata.0.annotations]
   }
 }
