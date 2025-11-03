dc1324d78a2a3b02b94e0674a551b3bbe0c85b0b
Chris Llanwarne <cjllanwarne@users.noreply.github.com>
2025-11-03 15:05:04 -0500

    [services] Update pip dependencies (#15162)
    ## Change Description
    
    Fixes #15148 and https://github.com/hail-is/hail-security/issues/66 
    
    ## Security Assessment
    
    Delete all except the correct answer:
    - This change potentially impacts the Hail Batch instance as deployed by
    Broad Institute in GCP
    
    ### Impact Rating
    
    - This change has a low security impact
    
    ### Impact Description
    
    Regular dependency update, plus updating aiomysql past a known
    vulnerability (though the specific issue wasn't a risk for us, it still
    shows up in scans)
    
    ### Appsec Review
    
    - [x] Required: The impact has been assessed and approved by appsec

--- hail/python/hailtop/uvloopx.py
+++ hail/python/hailtop/uvloopx.py
@@ -3,12 +3,12 @@ import sys
 try:
     import uvloop
 
-    def install():
+    def install():  # pyright: ignore[reportRedeclaration]
         return uvloop.install()
 
 except ImportError as e:
     if not sys.platform.startswith('win32'):
         raise e
 
-    def install():
+    def install():  # pyright: ignore[reportRedeclaration]
         pass
