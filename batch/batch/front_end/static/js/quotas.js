95c2374b69ff4ec4c19904103093c03601c8e115
Kush Chandra <kchandra@broadinstitute.org>
2025-11-05 11:56:36 -0500

    [batch] Refactor js to remove unsafe-inline CSP

--- batch/batch/front_end/static/js/quotas.js
+++
@@ -1,4 +0,0 @@
-if (document.getElementById('plotly-timeseries') !== undefined) {
-    var graphJson = JSON.parse(document.getElementById('plotly-timeseries').dataset.graph);
-    Plotly.plot('plotly-timeseries', graphJson, {});
-}
