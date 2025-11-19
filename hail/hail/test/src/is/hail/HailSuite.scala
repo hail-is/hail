0d301d8dfbc278ed4fbe4f34072f94e48cfeb52d
Edmund Higham <ehigham@users.noreply.github.com>
2025-11-18 22:02:14 -0500

    [query] make tests work with mill's sandboxing (#15197)
    Mill runs tests in a sandbox directory by default.
    This breaks our tests that try to read from test resources.
    Use "MILL_TEST_RESOURCE_DIR" to get to test resource directory.
    
    See the following for more information: 
    https://mill-build.org/mill/scalalib/testing.html#_running_tests
    
    This change cannot affect the Broad-managed hail batch deployment in
    GCP.

--- hail/hail/test/src/is/hail/HailSuite.scala
+++ hail/hail/test/src/is/hail/HailSuite.scala
@@ -47,7 +47,10 @@ class HailSuite extends TestNGSuite with TestUtils with Logging {
   def sc: SparkContext = ctx.backend.asSpark.sc
   def theHailClassLoader: HailClassLoader = ctx.theHailClassLoader
 
-  def getTestResource(localPath: String): String = s"hail/test/resources/$localPath"
+  private[this] lazy val resources: String =
+    sys.env.getOrElse("MILL_TEST_RESOURCE_DIR", "hail/test/resources")
+
+  def getTestResource(localPath: String): String = s"$resources/$localPath"
 
   @BeforeSuite
   def setupBackend(): Unit = {
