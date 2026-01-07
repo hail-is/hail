3e654a4d5f833cd777f971bd77fb2980f78cb6c3
Edmund Higham <edhigham@gmail.com>
2026-01-07 15:01:54 -0500

    continue to use AnyRefMap for scala 2.12

--- hail/hail/src-2.13/is/hail/utils/compat/mutable/package.scala
+++ hail/hail/src-2.13/is/hail/utils/compat/mutable/package.scala
@@ -1,6 +1,9 @@
 package is.hail.utils.compat
 
 package object mutable {
+  type AnyRefMap[K, V] = scala.collection.mutable.HashMap[K, V]
+  val AnyRefMap = scala.collection.mutable.HashMap
+
   type Growable[-A] = scala.collection.mutable.Growable[A]
   type GrowableCompat[-A] = Growable[A]
   type Shrinkable[-A] = scala.collection.mutable.Shrinkable[A]
