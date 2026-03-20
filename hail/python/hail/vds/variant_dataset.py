a86f0ba2e51a74c4c89580626e72be8fdf1871c0
Chris Vittal <chris@vittal.dev>
2026-03-20 15:34:44 -0400

    fix

--- hail/python/hail/vds/variant_dataset.py
+++ hail/python/hail/vds/variant_dataset.py
@@ -386,7 +386,7 @@ class VariantDataset:
         if res.negative_len:
             raise _validate_err(
                 'found records in reference data with negative LEN\n  '
-                + '\n  '.join(str(x) for x in res.end_before_position)
+                + '\n  '.join(str(x) for x in res.negative_len)
             )
         blocks_too_long = res.get('blocks_too_long', [])
         if blocks_too_long:
