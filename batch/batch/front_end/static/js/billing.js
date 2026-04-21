436dec0493618d8e1c84bfdabd61675d3080a8ce
Chris Llanwarne <cjllanwarne@gmail.com>
2026-04-21 11:57:46 -0400

    replace var with let

--- batch/batch/front_end/static/js/billing.js
+++ batch/batch/front_end/static/js/billing.js
@@ -7,7 +7,7 @@
 
     function toIso(mmddyyyy) {
         if (!mmddyyyy) {
-            var now = new Date();
+            let now = new Date();
             return now.getFullYear() + '-'
                 + String(now.getMonth() + 1).padStart(2, '0') + '-'
                 + String(now.getDate()).padStart(2, '0');
