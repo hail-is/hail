7b56d19579443cda6328808630138971f5f5cd9b
Chris Llanwarne <cjllanwarne@gmail.com>
2026-04-27 11:57:31 -0400

    constant vigilance

--- batch/batch/front_end/static/js/billing.js
+++ batch/batch/front_end/static/js/billing.js
@@ -10,9 +10,9 @@
         try {
             if (!mmddyyyy) {
                 const now = new Date();
-                yyyy = now.getFullYear();
-                mm = String(now.getMonth() + 1).padStart(2, '0');
-                dd = String(now.getDate()).padStart(2, '0');
+                const yyyy = now.getFullYear();
+                const mm = String(now.getMonth() + 1).padStart(2, '0');
+                const dd = String(now.getDate()).padStart(2, '0');
                 return `${yyyy}-${mm}-${dd}`;
             } else {
                 const [mm, dd, yyyy] = mmddyyyy.split('/');
