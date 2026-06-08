8bde24306b816c9816aad6b3474bfed66d571a4b
Chris Llanwarne <cjllanwarne@users.noreply.github.com>
2026-06-08 13:55:55 -0400

    [make] Fix ubuntu updater (#15528)
    ## Change Description
    
    - Fixes the regex check in the dockerhub scan, to accept `.1`s in (eg)
    `noble-20260509.1`
    - Fixes the `NAMESPACE=` in the image copy command, to match the
    instructions in
    https://github.com/hail-is/hail/blob/main/dev-docs/services/updating-third-party-images.md
    - Makes sure skopeo is installed before beginning the copy. Although
    "optional"... it's not really.
    
    
    ## Security Assessment
    
    - This change cannot impact the Hail Batch instance as deployed by Broad
    Institute in GCP
    
    No impact in production, but modifies a makefile so appsec review is
    still requested
    
    ### Appsec Review
    
    - [ ] Required: The impact has been assessed and approved by appsec

--- update-hail-ubuntu.py
+++ update-hail-ubuntu.py
@@ -14,7 +14,7 @@ print('Querying Docker Hub for latest ubuntu noble tag...')
 with urllib.request.urlopen(url) as r: # nosec B310 - ignore bandit rule because this url is https
     data = json.loads(r.read())
 
-pattern = re.compile(r'^noble-(\d{8})$')
+pattern = re.compile(r'^noble-(\d{8}(?:\.\d+)?)$')
 tags = [result['name'] for result in data['results'] if pattern.match(result['name'])]
 if not tags:
     print('ERROR: No noble-YYYYMMDD tags found', file=sys.stderr)
@@ -22,7 +22,7 @@ if not tags:
 latest_tag = sorted(tags)[-1]
 
 images_text = IMAGES_FILE.read_text()
-m = re.search(r'^ubuntu:(noble-\d{8})$', images_text, re.MULTILINE)
+m = re.search(r'^ubuntu:(noble-\d{8}(?:\.\d+)?)$', images_text, re.MULTILINE)
 if not m:
     print('ERROR: No ubuntu:noble-YYYYMMDD line found in images.txt', file=sys.stderr)
     sys.exit(1)
