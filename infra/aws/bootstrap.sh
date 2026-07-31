0d9256e6345330f7a6c5d30acdc2a74fbbd00fc8
Kush Chandra <kchandra@broadinstitute.org>
2026-07-30 21:59:00 -0400

    Fix error signal

--- infra/aws/bootstrap.sh
+++ infra/aws/bootstrap.sh
@@ -13,7 +13,9 @@ function install_external_secrets() {
         --create-namespace \
         --set installCRDs=true
 
+    kubectl -n external-secrets rollout status deployments --selector=app.kubernetes.io/instance=external-secrets
     kubectl annotate serviceaccount -n external-secrets external-secrets eks.amazonaws.com/role-arn="$EXTERNAL_SECRETS_ROLE"
+    sleep 10s       # CRD creation can lag behind deployment rollout before they're discoverable
 
     cat >external-secret-config.yaml <<EOF
 apiVersion: external-secrets.io/v1
@@ -34,7 +36,7 @@ spec:
 apiVersion: external-secrets.io/v1
 kind: ExternalSecret
 metadata:
-  name: "global-config-synced"
+  name: "global-config"
   namespace: "default"
 spec:
   refreshInterval: 1h
@@ -50,14 +52,10 @@ EOF
 
 function install_load_balancer_controller() {
     ACCOUNT_ID=$1
-    LOAD_BALANCER_CONTROLLER_ROLE=$2
+    CLUSTER_NAME=$2
+    LOAD_BALANCER_CONTROLLER_ROLE=$3
 
-    helm repo add eks https://aws.github.io/eks-charts
-    helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
-        -n kube-system \
-        --set serviceAccount.name=aws-load-balancer-controller
-
-    curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.1.2/docs/install/iam_policy.json
+    curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json
     aws iam create-policy \
         --policy-name AWSLoadBalancerControllerIAMPolicy \
         --policy-document file://iam-policy.json
@@ -65,7 +63,26 @@ function install_load_balancer_controller() {
         --role-name "$LOAD_BALANCER_CONTROLLER_ROLE" \
         --policy-arn "arn:aws:iam::$ACCOUNT_ID:policy/AWSLoadBalancerControllerIAMPolicy"
 
-    kubectl annotate serviceaccount -n kube-system aws-load-balancer-controller eks.amazonaws.com/role-arn="$LOAD_BALANCER_CONTROLLER_ROLE"
+    cat >load-balancer-sa.yaml <<EOF
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  annotations:
+    eks.amazonaws.com/role-arn: "arn:aws:iam::$ACCOUNT_ID:role/$LOAD_BALANCER_CONTROLLER_ROLE"
+  name: aws-load-balancer-controller
+  namespace: kube-system
+EOF
+    kubectl apply -f load-balancer-sa.yaml
+
+    helm repo add eks https://aws.github.io/eks-charts
+    helm repo update eks
+    helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
+        -n kube-system \
+        --set clusterName="$CLUSTER_NAME" \
+        --set serviceAccount.create=false \
+        --set serviceAccount.name=aws-load-balancer-controller
+
+    kubectl -n kube-system rollout status deployments --selector=app.kubernetes.io/instance=aws-load-install_load_balancer_controller
 }
 
 "$@"
