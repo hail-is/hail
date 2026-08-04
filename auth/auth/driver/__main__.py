b8e5173b378a6e97b85c417e13ba1014fb7c1120
Chris Llanwarne <cjllanwarne@gmail.com>
2026-08-04 11:55:58 -0400

    update process loops for new uvloop version

--- auth/auth/driver/__main__.py
+++ auth/auth/driver/__main__.py
@@ -8,5 +8,6 @@ import asyncio
 
 from .driver import async_main
 
-loop = asyncio.get_event_loop()
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
 loop.run_until_complete(async_main())
