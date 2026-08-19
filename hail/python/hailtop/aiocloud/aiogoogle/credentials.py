6e304000134efd7c6aedca10492031719638cfbb
Chris Llanwarne <cjllanwarne@gmail.com>
2026-08-19 15:32:40 -0400

    fix lint

--- hail/python/hailtop/aiocloud/aiogoogle/credentials.py
+++ hail/python/hailtop/aiocloud/aiogoogle/credentials.py
@@ -9,6 +9,7 @@ from urllib.parse import urlencode
 import jwt
 
 from hailtop import httpx
+from hailtop.tls import internal_client_ssl_context
 from hailtop.utils import first_extant_file, retry_transient_errors
 
 from ..common.credentials import AnonymousCloudCredentials, CloudCredentials
@@ -129,8 +130,6 @@ class GoogleCredentials(CloudCredentials):
 
     async def access_token_with_expiration(self) -> Tuple[str, Optional[float]]:
         if self._access_token is None or self._access_token.expired():
-            from hailtop.tls import internal_client_ssl_context
-
             log.info('ssl session stats before token fetch: %s', internal_client_ssl_context().session_stats())
             self._access_token = await self._get_access_token()
         return self._access_token.token, self._access_token._expiry_time
