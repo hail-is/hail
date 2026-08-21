a098b9b710187d67280a7ea994603e6ddc7110af
Chris Llanwarne <cjllanwarne@gmail.com>
2026-08-21 17:22:27 -0400

    codacy 2

--- services/ui/src/shared/authUtils.ts
+++ services/ui/src/shared/authUtils.ts
@@ -1,7 +1,7 @@
-type SystemPermissions = Record<string, boolean>;
-
+// Note that this enables UI element visibility checks, but is not a security check.
+// All data and requests still have to go via the backend, and the backend enforces auth independently.
 export function hasPermission(name: string): boolean {
   const content = document.head.querySelector('meta[name="system-permissions"]')?.getAttribute('content') ?? '{}';
-  const perms: SystemPermissions = JSON.parse(content) as SystemPermissions;
-  return perms[name] === true;
+  const perms = new Map<string, boolean>(Object.entries(JSON.parse(content) as Record<string, boolean>));
+  return perms.get(name) === true;
 }
