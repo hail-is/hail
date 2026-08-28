5c67ecac70d7f6bb748af990deb31e35ae98f3a8
Chris Llanwarne <cjllanwarne@gmail.com>
2026-08-28 16:51:31 -0400

    better running attempt distinction

--- batch/batch/front_end/front_end.py
+++ batch/batch/front_end/front_end.py
@@ -490,38 +490,38 @@ async def _read_job_container_log_from_cloud_storage(
         return b'ERROR: could not find log file'
 
 
+async def _fetch_attempt_record(app, batch_id, job_id, attempt_id):
+    db: Database = app['db']
+    return await db.select_and_fetchone(
+        """
+SELECT attempts.start_time, attempts.end_time, instances.ip_address, instances.state AS instance_state
+FROM attempts
+LEFT JOIN instances ON attempts.instance_name = instances.name
+WHERE attempts.batch_id = %s AND attempts.job_id = %s AND attempts.attempt_id = %s
+""",
+        (batch_id, job_id, attempt_id),
+    )
+
+
 async def _get_job_container_log(
     app, batch_id, job_id, container, job_record, override_attempt_id=None
 ) -> Optional[bytes]:
-    if not has_resource_available(job_record):
+    batch_format_version = BatchFormatVersion(job_record['format_version'])
+    attempt_id = override_attempt_id if override_attempt_id is not None else attempt_id_from_spec(job_record)
+    if attempt_id is None:
         return None
-
-    state = job_record['state']
-
-    if override_attempt_id is not None:
-        use_worker = state == 'Running' and override_attempt_id == job_record['attempt_id']
-        attempt_id = override_attempt_id
-    else:
-        use_worker = state == 'Running'
-        attempt_id = attempt_id_from_spec(job_record)
-        if not (use_worker or (attempt_id is not None and state in complete_states)):
-            raise ValueError(
-                f'unexpected log fetch state: use_worker={use_worker}, attempt_id={attempt_id}, state={state}'
-            )
-
-    if use_worker:
-        return await _get_job_container_log_from_worker(
-            app[CommonAiohttpAppKeys.CLIENT_SESSION], batch_id, job_id, container, job_record['ip_address']
-        )
-    else:
+    attempt = await _fetch_attempt_record(app, batch_id, job_id, attempt_id)
+    if attempt is None:
+        return None
+    if attempt['end_time'] is not None:
         return await _read_job_container_log_from_cloud_storage(
-            app['file_store'],
-            BatchFormatVersion(job_record['format_version']),
-            batch_id,
-            job_id,
-            container,
-            attempt_id,
+            app['file_store'], batch_format_version, batch_id, job_id, container, attempt_id
         )
+    if attempt['start_time'] is not None and attempt['instance_state'] == 'active':
+        return await _get_job_container_log_from_worker(
+            app[CommonAiohttpAppKeys.CLIENT_SESSION], batch_id, job_id, container, attempt['ip_address']
+        )
+    return None
 
 
 async def _get_job_log(app, batch_id, job_id, override_attempt_id=None) -> Dict[str, Optional[bytes]]:
